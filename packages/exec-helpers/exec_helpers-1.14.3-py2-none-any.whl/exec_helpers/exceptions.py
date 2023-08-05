648485dd740a902d5037e4b4f159ea8acf714089
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:28 +0000

    Add package exec-helpers/1.14.3/exec_helpers-1.14.3-py2-none-any.whl

---
+++ packages/exec-helpers/exec_helpers-1.14.3-py2-none-any.whl/exec_helpers/exceptions.py
@@ -0,0 +1,308 @@
+#    Copyright 2018 Alexey Stepanov aka penguinolog.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""Package specific exceptions."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import unicode_literals
+
+# Standard Library
+import typing
+
+# Exec-Helpers Implementation
+from exec_helpers import proc_enums
+
+if typing.TYPE_CHECKING:  # pragma: no cover
+    from exec_helpers import exec_result  # noqa: F401  # pylint: disable=cyclic-import, unused-import
+
+__all__ = (
+    "ExecHelperError",
+    "ExecHelperNoKillError",
+    "ExecHelperTimeoutError",
+    "ExecCalledProcessError",
+    "CalledProcessError",
+    "ParallelCallProcessError",
+    "ParallelCallExceptions",
+)
+
+
+class ExecHelperError(Exception):
+    """Base class for all exceptions raised inside."""
+
+    __slots__ = ()
+
+
+class DeserializeValueError(ExecHelperError, ValueError):
+    """Deserialize impossible."""
+
+    __slots__ = ()
+
+
+class ExecCalledProcessError(ExecHelperError):
+    """Base class for process call errors."""
+
+    __slots__ = ()
+
+
+class ExecHelperTimeoutProcessError(ExecCalledProcessError):
+    """Timeout based errors."""
+
+    __slots__ = ("result", "timeout")
+
+    def __init__(
+        self, message, result, timeout
+    ):  # type: (typing.Union[str, typing.Text], exec_result.ExecResult, typing.Union[int, float]) -> None
+        """Exception for error on process calls.
+
+        :param message: exception message
+        :type message: str
+        :param result: execution result
+        :type result: exec_result.ExecResult
+        :param timeout: timeout for command
+        :type timeout: typing.Union[int, float]
+        """
+        super(ExecHelperTimeoutProcessError, self).__init__(message)
+        self.result = result
+        self.timeout = timeout
+
+    @property
+    def cmd(self):  # type: () -> typing.Union[str, typing.Text]
+        """Failed command."""
+        return self.result.cmd
+
+    @property
+    def stdout(self):  # type: () -> typing.Text
+        """Command stdout."""
+        return self.result.stdout_str
+
+    @property
+    def stderr(self):  # type: () -> typing.Text
+        """Command stderr."""
+        return self.result.stderr_str
+
+
+class ExecHelperNoKillError(ExecHelperTimeoutProcessError):
+    """Impossible to kill process.
+
+    .. versionadded:: 1.10.0
+    """
+
+    __slots__ = ()
+
+    def __init__(self, result, timeout):  # type: (exec_result.ExecResult, typing.Union[int, float]) -> None
+        """Exception for error on process calls.
+
+        :param result: execution result
+        :type result: exec_result.ExecResult
+        :param timeout: timeout for command
+        :type timeout: typing.Union[int, float]
+        """
+        message = (
+            "Wait for {result.cmd!r} during {timeout!s}s: no return code and no response on SIGTERM + SIGKILL signals!"
+            "\n"
+            "\tSTDOUT:\n"
+            "{result.stdout_brief!r}\n"
+            "\tSTDERR:\n"
+            "{result.stderr_brief!r}".format(result=result, timeout=timeout)
+        )
+        super(ExecHelperNoKillError, self).__init__(message, result=result, timeout=timeout)
+
+
+def make_timeout_error_message(
+    result, timeout
+):  # type: (exec_result.ExecResult, typing.Union[int, float]) -> typing.Text
+    """Make timeout failed message."""
+    return (
+        "Wait for {result.cmd!r} during {timeout!s}s: no return code!\n"
+        "\tSTDOUT:\n"
+        "{result.stdout_brief!r}\n"
+        "\tSTDERR:\n"
+        "{result.stderr_brief!r}".format(result=result, timeout=timeout)
+    )
+
+
+class ExecHelperTimeoutError(ExecHelperTimeoutProcessError):
+    """Execution timeout.
+
+    .. versionchanged:: 1.3.0 provide full result and timeout inside.
+    .. versionchanged:: 1.3.0 subclass ExecCalledProcessError
+    """
+
+    __slots__ = ()
+
+    def __init__(self, result, timeout):  # type: (exec_result.ExecResult, typing.Union[int, float]) -> None
+        """Exception for error on process calls.
+
+        :param result: execution result
+        :type result: exec_result.ExecResult
+        :param timeout: timeout for command
+        :type timeout: typing.Union[int, float]
+        """
+        super(ExecHelperTimeoutError, self).__init__(
+            make_timeout_error_message(result=result, timeout=timeout), result=result, timeout=timeout
+        )
+
+
+class CalledProcessError(ExecCalledProcessError):
+    """Exception for error on process calls."""
+
+    __slots__ = ("result", "expected")
+
+    def __init__(
+        self,
+        result,  # type: exec_result.ExecResult
+        expected=(proc_enums.EXPECTED,),  # type: typing.Iterable[typing.Union[int, proc_enums.ExitCodes]]
+    ):  # type: (...) -> None
+        """Exception for error on process calls.
+
+        :param result: execution result
+        :type result: exec_result.ExecResult
+        :param expected: expected return codes
+        :type expected: typing.Iterable[typing.Union[int, proc_enums.ExitCodes]]
+
+        .. versionchanged:: 1.1.1 - provide full result
+        .. versionchanged:: 1.10.0 Expected is not optional, defaults os dependent
+        """
+        self.result = result
+        self.expected = proc_enums.exit_codes_to_enums(expected)
+        message = (
+            "Command {result.cmd!r} returned exit code {result.exit_code} "
+            "while expected {expected}\n"
+            "\tSTDOUT:\n"
+            "{result.stdout_brief!r}\n"
+            "\tSTDERR:\n"
+            "{result.stderr_brief!r}".format(result=result, expected=self.expected)
+        )
+        super(CalledProcessError, self).__init__(message)
+
+    @property
+    def returncode(self):  # type: () -> typing.Union[int, proc_enums.ExitCodes]
+        """Command return code."""
+        return self.result.exit_code
+
+    @property
+    def cmd(self):  # type: () -> typing.Union[str, typing.Text]
+        """Failed command."""
+        return self.result.cmd
+
+    @property
+    def stdout(self):  # type: () -> typing.Text
+        """Command stdout."""
+        return self.result.stdout_str
+
+    @property
+    def stderr(self):  # type: () -> typing.Text
+        """Command stderr."""
+        return self.result.stderr_str
+
+
+class ParallelCallProcessError(ExecCalledProcessError):
+    """Exception during parallel execution."""
+
+    __slots__ = ("cmd", "errors", "results", "expected")
+
+    def __init__(
+        self,
+        command,  # type: typing.Union[str, typing.Text]
+        errors,  # type: typing.Dict[typing.Tuple[typing.Union[str, typing.Text], int], exec_result.ExecResult]
+        results,  # type: typing.Dict[typing.Tuple[typing.Union[str, typing.Text], int], exec_result.ExecResult]
+        expected=(proc_enums.EXPECTED,),  # type: typing.Iterable[typing.Union[int, proc_enums.ExitCodes]]
+        **kwargs  # type: typing.Any
+    ):  # type: (...) -> None
+        """Exception raised during parallel call as result of exceptions.
+
+        :param command: command
+        :type command: str
+        :param errors: results with errors
+        :type errors: typing.Dict[typing.Tuple[str, int], ExecResult]
+        :param results: all results
+        :type results: typing.Dict[typing.Tuple[str, int], ExecResult]
+        :param expected: expected return codes
+        :type expected: typing.Iterable[typing.Union[int, proc_enums.ExitCodes]]
+        :param _message: message override
+        :type _message: typing.Optional[str]
+
+        .. versionchanged:: 1.10.0 Expected is not optional, defaults os dependent
+        """
+        prep_expected = proc_enums.exit_codes_to_enums(expected)
+        message = kwargs.get("_message", None) or (
+            "Command {cmd!r} "
+            "returned unexpected exit codes on several hosts\n"
+            "Expected: {expected}\n"
+            "Got:\n"
+            "\t{errors}".format(
+                cmd=command,
+                expected=prep_expected,
+                errors="\n\t".join(
+                    "{host}:{port} - {code} ".format(host=host, port=port, code=result.exit_code)
+                    for (host, port), result in errors.items()
+                ),
+            )
+        )
+        super(ParallelCallProcessError, self).__init__(message)
+        self.cmd = command
+        self.errors = errors
+        self.results = results
+        self.expected = prep_expected
+
+
+class ParallelCallExceptions(ParallelCallProcessError):
+    """Exception raised during parallel call as result of exceptions."""
+
+    __slots__ = ("cmd", "exceptions")
+
+    def __init__(
+        self,
+        command,  # type: typing.Union[str, typing.Text]
+        exceptions,  # type: typing.Dict[typing.Tuple[typing.Union[str, typing.Text], int], Exception]
+        errors,  # type: typing.Dict[typing.Tuple[typing.Union[str, typing.Text], int], exec_result.ExecResult]
+        results,  # type: typing.Dict[typing.Tuple[typing.Union[str, typing.Text], int], exec_result.ExecResult]
+        expected=(proc_enums.EXPECTED,),  # type: typing.Tuple[typing.Union[int, proc_enums.ExitCodes], ...]
+        **kwargs  # type: typing.Any
+    ):  # type: (...) -> None
+        """Exception during parallel execution.
+
+        :param command: command
+        :type command: str
+        :param exceptions: Exceptions on connections
+        :type exceptions: typing.Dict[typing.Tuple[str, int], Exception]
+        :param errors: results with errors
+        :type errors: typing.Dict[typing.Tuple[str, int], ExecResult]
+        :param results: all results
+        :type results: typing.Dict[typing.Tuple[str, int], ExecResult]
+        :param expected: expected return codes
+        :type expected: typing.Iterable[typing.Union[int, proc_enums.ExitCodes]]
+        :param _message: message override
+        :type _message: typing.Optional[str]
+
+        .. versionchanged:: 1.10.0 Expected is not optional, defaults os dependent
+        """
+        prep_expected = proc_enums.exit_codes_to_enums(expected)
+        message = kwargs.get("_message", None) or (
+            "Command {cmd!r} "
+            "during execution raised exceptions: \n"
+            "\t{exceptions}".format(
+                cmd=command,
+                exceptions="\n\t".join(
+                    "{host}:{port} - {exc} ".format(host=host, port=port, exc=exc)
+                    for (host, port), exc in exceptions.items()
+                ),
+            )
+        )
+        super(ParallelCallExceptions, self).__init__(
+            command=command, errors=errors, results=results, expected=prep_expected, _message=message
+        )
+        self.cmd = command
+        self.exceptions = exceptions
---
+++ packages/exec-helpers/exec_helpers-1.14.3-py2-none-any.whl/exec_helpers/ssh_client.py
@@ -0,0 +1,118 @@
+#    Copyright 2018 Alexey Stepanov aka penguinolog.
+
+#    Copyright 2013 - 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""SSH client helper based on Paramiko. Extended API helpers."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import unicode_literals
+
+# Standard Library
+import logging
+import os
+import posixpath
+import typing  # pylint: disable=unused-import
+
+# Local Implementation
+from ._ssh_client_base import SSHClientBase
+
+__all__ = ("SSHClient",)
+
+logger = logging.getLogger(__name__)
+logging.getLogger("paramiko").setLevel(logging.WARNING)
+
+
+class SSHClient(SSHClientBase):
+    """SSH Client helper."""
+
+    __slots__ = ()
+
+    @staticmethod
+    def _path_esc(path):  # type: (typing.Union[str, typing.Text]) -> typing.Union[str, typing.Text]
+        """Escape space character in the path."""
+        return path.replace(" ", r"\ ")
+
+    def mkdir(self, path):  # type: (typing.Union[str, typing.Text]) -> None
+        """Run 'mkdir -p path' on remote.
+
+        :type path: str
+        """
+        if self.exists(path):
+            return
+        # noinspection PyTypeChecker
+        self.execute("mkdir -p {}\n".format(self._path_esc(path)))
+
+    def rm_rf(self, path):  # type: (typing.Union[str, typing.Text]) -> None
+        """Run 'rm -rf path' on remote.
+
+        :type path: str
+        """
+        # noinspection PyTypeChecker
+        self.execute("rm -rf {}".format(self._path_esc(path)))
+
+    def upload(self, source, target):  # type: (typing.Union[str, typing.Text], typing.Union[str, typing.Text]) -> None
+        """Upload file(s) from source to target using SFTP session.
+
+        :type source: str
+        :type target: str
+        """
+        self.logger.debug("Copying '%s' -> '%s'", source, target)
+
+        if self.isdir(target):
+            target = posixpath.join(target, os.path.basename(source))
+
+        source = os.path.expanduser(source)
+        if not os.path.isdir(source):
+            self._sftp.put(source, target)
+            return
+
+        for rootdir, _, files in os.walk(source):
+            targetdir = os.path.normpath(os.path.join(target, os.path.relpath(rootdir, source))).replace("\\", "/")
+
+            self.mkdir(targetdir)
+
+            for entry in files:
+                local_path = os.path.normpath(os.path.join(rootdir, entry))
+                remote_path = posixpath.join(targetdir, entry)
+                if self.exists(remote_path):
+                    self._sftp.unlink(remote_path)
+                self._sftp.put(local_path, remote_path)
+
+    def download(
+        self, destination, target
+    ):  # type: (typing.Union[str, typing.Text], typing.Union[str, typing.Text]) -> bool
+        """Download file(s) to target from destination.
+
+        :param destination: remote path
+        :type destination: str
+        :param target: local path
+        :type target: str
+        :return: downloaded file present on local filesystem
+        :rtype: bool
+        """
+        self.logger.debug("Copying '%s' -> '%s' from remote to local host", destination, target)
+
+        if os.path.isdir(target):
+            target = posixpath.join(target, os.path.basename(destination))
+
+        if not self.isdir(destination):
+            if self.exists(destination):
+                self._sftp.get(destination, target)
+            else:
+                self.logger.debug("Can't download %s because it doesn't exist", destination)
+        else:
+            self.logger.debug("Can't download %s because it is a directory", destination)
+        return os.path.exists(target)
