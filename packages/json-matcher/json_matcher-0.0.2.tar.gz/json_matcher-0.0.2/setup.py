4261ad83aa6ffb9cfa486d5bf726ccfa4b7a5c6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:44 +0000

    Add package json-matcher/0.0.2/json_matcher-0.0.2.tar.gz

---
+++ packages/json-matcher/json_matcher-0.0.2.tar.gz/json_matcher-0.0.2/setup.py
@@ -0,0 +1,40 @@
+#!/usr/env/bin python
+#-*- coding: utf-8 -*-
+import os
+import setuptools
+
+module_path = os.path.join(os.path.dirname(__file__), 'json_matcher/__init__.py')
+
+# scripts
+scripts = [ 'bin/json_match', 'bin/jrep' ]
+
+# packages
+packages = setuptools.find_packages(exclude=['tests'])
+
+setuptools.setup(
+    name="json_matcher",
+    version="0.0.2",
+    url="https://github.com/hosung-yim/json_matcher",
+    dowload_url="https://github.com/hosung-yim/json_matcher/archive/0.0.2.tar.gz",
+
+    author="Greg.YIM",
+    author_email="greg.yim@kakaocorp.com",
+
+    description="Match json with lucece-like query",
+    long_description=open('README.md').read(),
+
+    scripts=scripts,
+    packages=packages,
+    platforms='any',
+
+    install_requires=[ 'pydash', 'pyparsing', 'six' ],
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+)
