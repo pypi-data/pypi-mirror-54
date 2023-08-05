7b77d8205f1fccac5130d0b22d172ce5f471e2ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:12 +0000

    Add package juriscraper/1.26.12/juriscraper-1.26.12-py27-none-any.whl

---
+++ packages/juriscraper/juriscraper-1.26.12-py27-none-any.whl/juriscraper/opinions/united_states/state/alaskactapp.py
@@ -0,0 +1,15 @@
+"""
+Scraper for Alaska Court of Appeals
+ID: alaskactapp
+Court Short Name: Alaska Court of Appeals
+"""
+
+from . import alaska
+
+
+class Site(alaska.Site):
+    url_court_id = 'ap'
+
+    def __init__(self, *args, **kwargs):
+        super(Site, self).__init__(*args, **kwargs)
+        self.court_id = self.__module__
