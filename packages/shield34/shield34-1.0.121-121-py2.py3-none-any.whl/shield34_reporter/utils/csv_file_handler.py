8801476f1726d4fc2b8a14a2f9b9a7e813b9a195
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:14:11 +0000

    Add package shield34/1.0.121/shield34-1.0.121-121-py2.py3-none-any.whl

---
+++ packages/shield34/shield34-1.0.121-121-py2.py3-none-any.whl/shield34_reporter/utils/csv_file_handler.py
@@ -0,0 +1,15 @@
+import csv
+import io
+
+class CsvFileHandler:
+
+    @staticmethod
+    def write_csv_file(file_path, csv_rows, header):
+        with io.open(file_path, 'w', newline='', encoding='utf-8') as csvFile:
+        #with open(file_path, 'w', newline='') as csvFile:
+            writer = csv.writer(csvFile)
+            writer.writerow(header)
+            for csv_row in csv_rows:
+                writer.writerow(csv_row.to_array())
+
+        csvFile.close()
