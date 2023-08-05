8329f7c33585791811ce5665f5094dc3c3df81c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:00 +0000

    Add package sketchfab/0.0.5/sketchfab-0.0.5.tar.gz

---
+++ packages/sketchfab/sketchfab-0.0.5.tar.gz/sketchfab-0.0.5/sketchfab/__init__.py
@@ -0,0 +1,11 @@
+"""
+Python client API providing an easy way to manage you models on sketchfab.
+"""
+import sketchfab.clt
+
+VERSION = "0.0.5"
+"""Current version of the lib"""
+
+Client = clt.SFClient
+"""Shortcut to instantiate the client"""
+
