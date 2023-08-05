89b54bcf4a2cd759ec0bedb1a475abd9b1509fa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:41 +0000

    Add package ellipsis/0.1.0/ellipsis-0.1.0.tar.gz

---
+++ packages/ellipsis/ellipsis-0.1.0.tar.gz/ellipsis-0.1.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ellipsis",
+    version="0.1.0",
+    author="Daniel van der Maas",
+    author_email="daniel@ellipsis-earth.com",
+    description="package to interact with the Ellipsis API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ellipsis-earth/PythonPackage",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
