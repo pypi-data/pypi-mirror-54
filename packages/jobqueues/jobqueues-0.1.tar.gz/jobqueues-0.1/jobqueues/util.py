c0d435c2ff520e96d1bf31a54f074005f8f8f2ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:35 +0000

    Add package jobqueues/0.1/jobqueues-0.1.tar.gz

---
+++ packages/jobqueues/jobqueues-0.1.tar.gz/jobqueues-0.1/jobqueues/util.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+def ensurelist(tocheck, tomod=None):
+    """Convert np.ndarray and scalars to lists.
+
+    Lists and tuples are left as is. If a second argument is given,
+    the type check is performed on the first argument, and the second argument is converted.
+    """
+    if tomod is None:
+        tomod = tocheck
+    if isinstance(tocheck, np.ndarray):
+        return list(tomod)
+    if isinstance(tocheck, range):
+        return list(tocheck)
+    if not isinstance(tocheck, list) and not isinstance(tocheck, tuple):
+        return [tomod, ]
+    return tomod
