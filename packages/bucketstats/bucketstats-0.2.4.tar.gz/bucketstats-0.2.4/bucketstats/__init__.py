081d8f7a04ea437fd0af5ef46d70680516f5c7cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:09 +0000

    Add package bucketstats/0.2.4/bucketstats-0.2.4.tar.gz

---
+++ packages/bucketstats/bucketstats-0.2.4.tar.gz/bucketstats-0.2.4/bucketstats/__init__.py
@@ -0,0 +1,5 @@
+# __init__.py
+
+from bucketstats.probability import cmf, median, pmf, rcmf, rcumsum
+from bucketstats.survival_analysis import survival, kaplan_meier, nelson_aalen, var_nelson_aalen, var_kaplan_meier
+from bucketstats.util import safe_divide
---
+++ packages/bucketstats/bucketstats-0.2.4.tar.gz/bucketstats-0.2.4/bucketstats/util.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+
+
+#def safe_getitem(x, idx=0):
+#    """
+#    >>> safe_getitem(1)
+#    1
+#    >>> safe_getitem((1,2))
+#    1
+#    """
+#
+#    if '__getitem__' in dir(x):
+#        return x[idx]
+#    else:
+#        return x
+
+
+def safe_divide(numer: pd.Series, denom: pd.Series, fill) -> pd.Series:
+    """
+    Safely divide two series by replacing zero denom with specified fill value
+
+    >>> numer = pd.Series([1,1,1])
+    >>> denom = pd.Series([0,1,2])
+    >>> fill = 0
+    >>> safe_divide(numer, denom, fill)
+    0    0.0
+    1    1.0
+    2    0.5
+    dtype: float64
+    """
+
+    result = numer / denom
+    result[denom == 0] = fill
+    return result
