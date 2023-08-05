26d7f0d2ab921b17a5ea5e21bc33c983e7b240cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:46 +0000

    Add package reegis/0.1.0rc0/reegis-0.1.0rc0.tar.gz

---
+++ packages/reegis/reegis-0.1.0rc0.tar.gz/reegis-0.1.0rc0/setup.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python
+
+"""Setup module of reegis."""
+
+from setuptools import setup
+import os
+import reegis
+
+if not os.environ.get('READTHEDOCS') == 'True':
+    requirements = [
+        'pandas >= 0.21.0, < 0.25',
+        'demandlib',
+        'tables',
+        'shapely',
+        'pvlib < 0.7',
+        'geopandas < 0.5',
+        'requests',
+        'numpy < 1.16',
+        'workalendar',
+        'pyproj',
+        'pytz',
+        'windpowerlib < 0.3',
+        'python-dateutil',
+        'cython',
+        'Rtree',
+        'xlrd']
+else:
+    requirements = ['cycler']
+
+
+setup(name='reegis',
+      version=reegis.__version__,
+      author='Uwe Krien',
+      author_email='krien@uni-bremen.de',
+      description='Open geospatial data model',
+      package_dir={'reegis': 'reegis'},
+      install_requires=requirements)
