07b0a821f939f292242c6ad6e39b7da235fd8b2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:57 +0000

    Add package chemio/1.6.2/chemio-1.6.2-py2.py3-none-any.whl

---
+++ packages/chemio/chemio-1.6.2-py2.py3-none-any.whl/chemio/__init__.py
@@ -0,0 +1,14 @@
+"""
+CHEMIO
+"""
+
+from .main import read, write, preview, convert
+
+__version__ = '1.6.2'
+def version():
+    return __version__
+
+
+def _setdebug():
+    from . import main
+    main._setdebug()
