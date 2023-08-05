4afdb9717900699e7710e08ce888736a758dbf7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:58 +0000

    Add package Theos-python-functions/0.0.3/Theos-python-functions-0.0.3.tar.gz

---
+++ packages/Theos-python-functions/Theos-python-functions-0.0.3.tar.gz/Theos-python-functions-0.0.3/setup.py
@@ -0,0 +1,26 @@
+# python setup.py sdist bdist_wheel
+# twine check dist/*
+# twine upload --repository pypi dist/*
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Theos-python-functions",
+    version="0.0.3",
+    author="Theo van der Sluijs",
+    author_email="theo@vandersluijs.nl",
+    description="Some nice to have python functions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tvdsluijs/Theos-Python-Functions",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
