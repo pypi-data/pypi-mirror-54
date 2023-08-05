e7dfb9e4427f19edaa76ef2e48cdb681c11a3103
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:59 +0000

    Add package fetchai-ledger-api/0.9.0a2/fetchai_ledger_api-0.9.0a2-py3-none-any.whl

---
+++ packages/fetchai-ledger-api/fetchai_ledger_api-0.9.0a2-py3-none-any.whl/fetchai/ledger/api/tx.py
@@ -0,0 +1,16 @@
+from .common import ApiEndpoint
+
+
+class TransactionApi(ApiEndpoint):
+    def status(self, tx_digest):
+        """
+        Determines the status of the transaction at the node
+
+        :param tx_digest: The hex-encoded string of the target tx digest
+        :return:
+        """
+
+        url = '{}://{}:{}/api/status/tx/{}'.format(self.protocol, self.host, self.port, tx_digest)
+
+        response = self._session.get(url).json()
+        return response.get('status')
