92be5d5f7e48bec47ed2a7f7889d93ddea2557d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:43 +0000

    Add package akinaka/0.3.0/akinaka-0.3.0.tar.gz

---
+++ packages/akinaka/akinaka-0.3.0.tar.gz/akinaka-0.3.0/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="akinaka",
+    python_requires='>=3.3.0',
+    version="0.3.0",
+    author="Afraz",
+    author_email="afraz@olindata.com",
+    description="OlinData's aws cli Extras",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.olindata.com/olindata/akinaka",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'boto3',
+        'datetime',
+        'click',
+        'pyyaml',
+        'kubernetes'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    scripts=['akinaka.py'],
+    zip_safe=False
+)
