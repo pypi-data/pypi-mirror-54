79285fb28e8f6450a062c9c9e85125fb7ae6011b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:44 +0000

    Add package streamsx.endpoint/0.7.1/streamsx.endpoint-0.7.1.tar.gz

---
+++ packages/streamsx.endpoint/streamsx.endpoint-0.7.1.tar.gz/streamsx.endpoint-0.7.1/streamsx/endpoint/__init__.py
@@ -0,0 +1,41 @@
+# coding=utf-8
+# Licensed Materials - Property of IBM
+# Copyright IBM Corp. 2019
+
+"""
+Overview
+++++++++
+
+For details of implementing applications in Python
+for IBM Streams including IBM Cloud Pak for Data:
+
+  * `streamsx package documentation <https://streamsxtopology.readthedocs.io/en/stable>`_
+
+This package exposes SPL operators in the `com.ibm.streamsx.inetserver <https://ibmstreams.github.io/streamsx.inetserver/>`_ toolkit as Python methods.
+
+
+Sample
+++++++
+
+A simple example of a Streams application that provides an endpoint for json injection::
+
+    from streamsx.topology.topology import *
+    from streamsx.topology.context import submit
+    import streamsx.endpoint as endpoint
+
+    topo = Topology()
+
+    s1 = endpoint.inject(topo, context='sample', name='jsoninject', monitor='endpoint-sample')
+    s1.print()
+
+    # Use for IBM Streams including IBM Cloud Pak for Data
+    submit ('DISTRIBUTED', topo)
+
+
+"""
+
+__version__='0.7.1'
+
+__all__ = ['download_toolkit', 'inject', 'expose']
+from streamsx.endpoint._endpoint import download_toolkit, inject, expose
+
