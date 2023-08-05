b502feff2f061e2a63e4766c87a79b2505ddf8ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:19 +0000

    Add package jet-bridge-base/0.5.0/jet-bridge-base-0.5.0.tar.gz

---
+++ packages/jet-bridge-base/jet-bridge-base-0.5.0.tar.gz/jet-bridge-base-0.5.0/jet_bridge_base/serializers/model_description.py
@@ -0,0 +1,34 @@
+from jet_bridge_base import fields as fields_
+from jet_bridge_base.serializers.serializer import Serializer
+
+
+class ModelDescriptionFieldSerializer(Serializer):
+    name = fields_.CharField()
+    db_column = fields_.CharField()
+    field = fields_.CharField()
+    filterable = fields_.BooleanField()
+    null = fields_.BooleanField()
+    editable = fields_.BooleanField()
+    params = fields_.JSONField()
+    verbose_name = fields_.CharField(required=False)
+    required = fields_.BooleanField(required=False)
+
+
+class ModelDescriptionRelationSerializer(Serializer):
+    name = fields_.CharField()
+    related_model = fields_.JSONField()
+    field = fields_.CharField()
+    related_model_field = fields_.CharField()
+    through = fields_.JSONField()
+
+
+class ModelDescriptionSerializer(Serializer):
+    model = fields_.CharField()
+    db_table = fields_.CharField()
+    hidden = fields_.BooleanField()
+    primary_key_field = fields_.CharField()
+    fields = ModelDescriptionFieldSerializer(many=True)
+    relations = ModelDescriptionRelationSerializer(many=True)
+    verbose_name = fields_.CharField(required=False)
+    verbose_name_plural = fields_.CharField(required=False)
+
