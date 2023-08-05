2f1c9e5798cf21f95d3442eab108e2c1cab2cdfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:29 +0000

    Add package shield34/1.0.92/shield34-1.0.92-92-py3-none-any.whl

---
+++ packages/shield34/shield34-1.0.92-92-py3-none-any.whl/shield34_reporter/utils/reporter_proxy.py
@@ -0,0 +1,34 @@
+import os
+import platform
+
+from browsermobproxy import Server
+
+from shield34_reporter.auth.sdk_authentication import SdkAuthentication
+from shield34_reporter.utils.network_utils import NetworkUtils
+
+
+class ReporterProxy():
+
+    @staticmethod
+    def create_proxy():
+        if SdkAuthentication.is_authorized():
+            from shield34_reporter.container.run_report_container import RunReportContainer
+            block_run_report_container = RunReportContainer.get_current_block_run_holder()
+            if block_run_report_container.proxyServer is None:
+                ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+                path = os.path.join(ROOT_DIR, 'proxy', 'browsermob-proxy-2.1.4', 'bin')
+                if platform.system() == 'Windows':
+                    os.environ["PATH"] += os.pathsep + path
+                #    path = path + ".bat"
+                #path = '"%s"' % path
+                browser_mob_server = Server('browsermob-proxy', options={'port': NetworkUtils.get_random_port()})
+                #browser_mob_server.host = "127.0.0.1"
+                browser_mob_server.start()
+                proxy = browser_mob_server.create_proxy(params= {"trustAllServers": "true", "port": NetworkUtils.get_random_port(), "bindAddress": "127.0.0.1"})
+                block_run_report_container.proxyServer = proxy
+                return True
+            else :
+                # proxy already exists. !
+                return True
+        return False
+
