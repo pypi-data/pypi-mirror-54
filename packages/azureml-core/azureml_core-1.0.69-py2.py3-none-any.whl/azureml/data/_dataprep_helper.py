fa43ae93c91c57f260420688e1d8840095886d93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:58 +0000

    Add package azureml-core/1.0.69/azureml_core-1.0.69-py2.py3-none-any.whl

---
+++ packages/azureml-core/azureml_core-1.0.69-py2.py3-none-any.whl/azureml/data/_dataprep_helper.py
@@ -0,0 +1,40 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+
+"""Contains helper methods for dataprep."""
+
+
+def dataprep():
+    try:
+        import azureml.dataprep as _dprep
+        return _dprep
+    except ImportError:
+        raise ImportError(_dataprep_missing_error)
+
+
+def dataprep_fuse():
+    try:
+        import azureml.dataprep.fuse.dprepfuse as _dprep_fuse
+        return _dprep_fuse
+    except ImportError:
+        raise ImportError(_dataprep_missing_error)
+
+
+def ensure_dataflow(dataflow):
+    if not isinstance(dataflow, dataprep().Dataflow):
+        raise RuntimeError('dataflow must be instance of azureml.dataprep.Dataflow')
+
+
+def get_dataflow_for_execution(dataflow, action, source):
+    from copy import deepcopy
+    meta = deepcopy(dataflow._meta)
+    meta['activityApp'] = source
+    meta['activity'] = action
+    return dataprep().Dataflow(dataflow._engine_api, dataflow._steps, meta)
+
+
+_dataprep_missing_error = (
+    'Could not import package "azureml-dataprep". Please ensure it is installed by running: ' +
+    'pip install "azureml-dataprep[fuse,pandas]"'
+)
