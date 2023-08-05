4c614f979b1bc34212cb27dd88fff79ef6bb71c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:39 +0000

    Add package guillotina/5.0.23/guillotina-5.0.23-cp37-cp37m-macosx_10_14_x86_64.whl

---
+++ packages/guillotina/guillotina-5.0.23-cp37-cp37m-macosx_10_14_x86_64.whl/guillotina/files/dbfile.py
@@ -0,0 +1,35 @@
+from .field import BaseCloudFile
+from guillotina.interfaces import IDBFile
+from zope.interface import implementer
+
+
+@implementer(IDBFile)
+class DBFile(BaseCloudFile):
+    """File stored in a DB using blob storage"""
+
+    _blob = None
+
+    @property
+    def valid(self):
+        return self._blob is not None
+
+    def get_actual_size(self):
+        if self._blob is not None:
+            return self._blob.size
+        return 0
+
+    @property
+    def size(self):
+        if self._blob is not None:
+            return self._blob.size
+        return 0
+
+    @size.setter
+    def size(self, val):
+        pass
+
+    @property
+    def chunks(self) -> int:
+        if self._blob is not None:
+            return self._blob.chunks
+        return 0
---
+++ packages/guillotina/guillotina-5.0.23-cp37-cp37m-macosx_10_14_x86_64.whl/guillotina/tests/test_annotations.py
@@ -0,0 +1,41 @@
+from guillotina.annotations import AnnotationData
+from guillotina.content import create_content_in_container
+from guillotina.interfaces import IAnnotations
+from guillotina.tests.utils import login
+from guillotina.transactions import transaction
+from guillotina.utils import get_database
+
+import os
+import pytest
+
+
+pytest.mark.skipif(os.environ.get("DATABASE") == "cockroachdb", reason="Flaky cockroachdb test")
+
+
+async def test_create_annotation(db, guillotina_main):
+    db = await get_database("db")
+    login()
+
+    async with transaction(db=db):
+        container = await create_content_in_container(db, "Container", "container", title="Container")
+        ob = await create_content_in_container(container, "Item", "foobar")
+
+        annotations = IAnnotations(ob)
+        data = AnnotationData()
+        data["foo"] = "bar"
+        await annotations.async_set("foobar", data)
+
+    async with transaction(db=db):
+        container = await db.async_get("container")
+        ob = await container.async_get("foobar")
+        annotations = IAnnotations(ob)
+        assert "foobar" in (await annotations.async_keys())
+        await annotations.async_del("foobar")
+
+    async with transaction(db=db):
+        container = await db.async_get("container")
+        ob = await container.async_get("foobar")
+        annotations = IAnnotations(ob)
+        assert "foobar" not in (await annotations.async_keys())
+        await container.async_del("foobar")
+        await db.async_del("container")
