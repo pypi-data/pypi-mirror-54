56c7e018670d80a0ad2a6ffc2a7980edd88c6f38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:44 +0000

    Add package solstice-config/0.0.2/solstice-config-0.0.2.tar.gz

---
+++ packages/solstice-config/solstice-config-0.0.2.tar.gz/solstice-config-0.0.2/solstice/config/__init__.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Initialization module for solstice-config
+"""
+
+from __future__ import print_function, division, absolute_import
+
+__author__ = "Tomas Poveda"
+__license__ = "MIT"
+__maintainer__ = "Tomas Poveda"
+__email__ = "tpovedatd@gmail.com"
+
+import os
+import inspect
+
+import artellapipe.config as artella_cfg
+
+
+class SolsticeConfigs(artella_cfg.ArtellaConfigs, object):
+
+    def get_configurations_path(self):
+        """
+        Overrides base ArtellaConfigs get_configurations_path function
+        Returns path where tpNameIt module is stored
+        :return: str
+        """
+
+        try:
+            mod_dir = os.path.dirname(inspect.getframeinfo(inspect.currentframe()).filename)
+        except Exception:
+            try:
+                mod_dir = os.path.dirname(__file__)
+            except Exception:
+                try:
+                    import artellapipe
+                    mod_dir = artellapipe.__path__[0]
+                except Exception:
+                    return None
+
+        return mod_dir
