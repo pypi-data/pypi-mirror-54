c8cdb4bb85ccbdd2e183e4be9771a16ec89327f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:42 +0000

    Add package cron-schedule-triggers/0.0.2/cron-schedule-triggers-0.0.2.tar.gz

---
+++ packages/cron-schedule-triggers/cron-schedule-triggers-0.0.2.tar.gz/cron-schedule-triggers-0.0.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+      name='cron-schedule-triggers',
+      version='0.0.2',
+      description='Cron Schedule Triggers ~ A library for determining Quartz Cron schedule trigger dates.',
+      url='https://gitlab.com/dameon.andersen/cstriggers',
+      author='Dameon Andersen',
+      author_email='dameon.andersen@facteon.global',
+      packages=setuptools.find_packages(),
+      python_requires='>=3.6',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+      ],
+      licence="MIT",
+      zip_safe=False
+)
