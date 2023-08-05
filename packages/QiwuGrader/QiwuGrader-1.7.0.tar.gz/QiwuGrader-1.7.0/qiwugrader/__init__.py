f028e6e7b4862083d994a1a459ee85b6743e5aa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:20:56 +0000

    Add package QiwuGrader/1.7.0/QiwuGrader-1.7.0.tar.gz

---
+++ packages/QiwuGrader/QiwuGrader-1.7.0.tar.gz/QiwuGrader-1.7.0/qiwugrader/__init__.py
@@ -0,0 +1,9 @@
+__all__ = ['app', 'server', 'controller', 'grader', 'model']
+
+from qiwugrader import app
+from qiwugrader.app import main
+
+
+from qiwugrader.controller import *
+from qiwugrader.grader import *
+from qiwugrader.model import *
