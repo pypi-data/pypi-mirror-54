450593ebea3c5c69b8907d6bb3af1d4f645b018b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:14 +0000

    Add package kcw/1.4.3.3/kcw-1.4.3.3.tar.gz

---
+++ packages/kcw/kcw-1.4.3.3.tar.gz/kcw-1.4.3.3/kcw/utill/db/sqlite.py
@@ -0,0 +1,11 @@
+from kcw.config import sqlite as sqliteconfig
+import time,traceback
+import random
+dbconfig=sqliteconfig
+class sqlite:
+    def test(self):
+        pass
+    def connect(self,config):
+        return self
+    def table(self,table):
+        return self
