b21506edf7421f647086270c5fe958121de1f1c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:17 +0000

    Add package spkcspider/0.20.4/spkcspider-0.20.4-py3-none-any.whl

---
+++ packages/spkcspider/spkcspider-0.20.4-py3-none-any.whl/spkcspider/apps/spider/queryfilters.py
@@ -0,0 +1,196 @@
+__all__ = (
+    "filter_components", "filter_contents", "listed_variants_q",
+    "machine_variants_q", "active_protections_q"
+)
+
+from django.db.models import Q
+from django.conf import settings
+
+from spkcspider.constants import VariantType, ProtectionStateType
+
+_base_variants = ~(
+    (
+        ~Q(ctype__contains=VariantType.feature_connect) &
+        Q(ctype__contains=VariantType.component_feature)
+    ) |
+    Q(ctype__contains=VariantType.content_feature)
+)
+
+machine_variants_q = (
+    Q(ctype__contains=VariantType.machine) &
+    _base_variants  # such features cannot be created
+)
+
+active_protections_q = (
+    Q(state=ProtectionStateType.enabled) |
+    Q(state=ProtectionStateType.instant_fail)
+)
+
+
+listed_variants_q = (
+    _base_variants &
+    ~Q(ctype__contains=VariantType.unlisted)
+)
+
+
+def filter_components(searchlist, filter_unlisted=True, use_contents=True):
+    searchq = Q()
+    searchq_exc = Q()
+    notsearch = Q()
+
+    counter = 0
+    # against ddos
+    max_counter = settings.SPIDER_MAX_SEARCH_PARAMETERS
+
+    # list only unlisted if explicity requested or export is used
+    # ComponentPublicIndex doesn't allow unlisted in any case
+    # this is enforced by setting "is_special_user" to False
+    if filter_unlisted:
+        notsearch = ~Q(contents__info__contains="\x1eunlisted\x1e")
+
+    for item in searchlist:
+        if filter_unlisted and item == "_unlisted":
+            continue
+        if counter > max_counter:
+            break
+        counter += 1
+        if len(item) == 0:
+            continue
+        use_strict = False
+        if item.startswith("!!"):
+            _item = item[1:]
+        elif item.startswith("__"):
+            _item = item[1:]
+        elif item.startswith("!_"):
+            _item = item[2:]
+            use_strict = True
+        elif item.startswith("!"):
+            _item = item[1:]
+        elif item.startswith("_"):
+            _item = item[1:]
+            use_strict = True
+        else:
+            _item = item
+        qob = Q()
+        if use_strict:
+            if use_contents:
+                qob |= Q(contents__info__contains="\x1e%s\x1e" % _item)
+                # exclude unlisted from searchterms
+                qob &= notsearch
+        else:
+            qob |= Q(description__icontains=_item)
+        if _item == "index":
+            qob |= Q(strength=10)
+        elif use_strict:
+            qob |= Q(
+                name=_item,
+                strength__lt=10
+            )
+        else:
+            qob |= Q(
+                name__icontains=_item,
+                strength__lt=10
+            )
+        if item.startswith("!!"):
+            searchq |= qob
+        elif item.startswith("!"):
+            searchq_exc |= qob
+        else:
+            searchq |= qob
+    return (searchq & ~searchq_exc, counter)
+
+
+def filter_contents(
+    searchlist, idlist=None, filter_unlisted=True, feature_exception=True,
+    use_components=False
+):
+    searchq = Q()
+    searchq_exc = Q()
+
+    counter = 0
+    unlisted_active = False
+    # against ddos
+    max_counter = settings.SPIDER_MAX_SEARCH_PARAMETERS
+
+    for item in searchlist:
+        if filter_unlisted is True and item == "_unlisted":
+            continue
+        elif item == "_unlisted":
+            unlisted_active = True
+        if counter > max_counter:
+            break
+        counter += 1
+        if len(item) == 0:
+            continue
+        use_strict = False
+        negate = False
+        if item.startswith("!!"):
+            _item = item[1:]
+        elif item.startswith("__"):
+            _item = item[1:]
+        elif item.startswith("!_"):
+            _item = item[2:]
+            use_strict = True
+            negate = True
+        elif item.startswith("!"):
+            _item = item[1:]
+            negate = True
+        elif item.startswith("_"):
+            _item = item[1:]
+            use_strict = True
+        else:
+            _item = item
+        if use_strict:
+            qob = Q(name=_item)
+            qob |= Q(info__contains="\x1e%s\x1e" % _item)
+            # can exclude/include specific usercomponents names
+            if use_components:
+                qob |= Q(usercomponent__name=_item)
+        else:
+            qob = Q(name__icontains=_item)
+            qob |= Q(description__icontains=_item)
+            qob |= Q(info__icontains=_item)
+            # but don't be too broad with unspecific negation
+            #   only apply on contents
+            if use_components and not negate:
+                qob |= Q(usercomponent__name__icontains=_item)
+                qob |= Q(usercomponent__description__icontains=_item)
+        if negate:
+            searchq_exc |= qob
+        else:
+            searchq |= qob
+
+    if idlist:
+        # idlist contains int and str entries
+        try:
+            ids = map(lambda x: int(x), idlist)
+        except ValueError:
+            # deny any access in case of an incorrect id
+            ids = []
+
+        searchq &= (
+            Q(
+                id__in=ids
+            )
+        )
+    if not unlisted_active:
+        tmp = None
+        if filter_unlisted is True:
+            tmp = Q(info__contains="\x1eunlisted\x1e")
+        elif filter_unlisted is not False:
+            tmp = Q(
+                info__contains="\x1eunlisted\x1e",
+                priority__lte=filter_unlisted
+            )
+        if tmp:
+            if feature_exception:
+                tmp &= ~(
+                    Q(
+                        ctype__ctype__contains=VariantType.feature_connect
+                    ) &
+                    ~Q(
+                        ctype__ctype__contains=VariantType.unlisted
+                    )
+                )
+            searchq_exc |= tmp
+    return (searchq & ~searchq_exc, counter)
---
+++ packages/spkcspider/spkcspider-0.20.4-py3-none-any.whl/spkcspider/apps/spider_tags/fields.py
@@ -0,0 +1,246 @@
+__all__ = ["installed_fields", "generate_fields"]
+
+import logging
+import posixpath
+from django import forms
+from django.apps import apps
+from django.utils.translation import gettext, gettext_lazy
+
+from spkcspider.utils.fields import add_by_field
+
+from spkcspider.apps.spider.widgets import (
+    SubSectionStartWidget, SubSectionStopWidget
+)
+
+installed_fields = {}
+
+safe_default_fields = [
+    "BooleanField", "CharField", "ChoiceField", "MultipleChoiceField",
+    "DateField", "DateTimeField", "DecimalField", "DurationField",
+    "EmailField", "FilePathField", "FloatField", "GenericIPAddressField",
+    "ModelChoiceField", "ModelMultipleChoiceField", "SlugField", "TimeField",
+    "URLField"
+]
+for i in safe_default_fields:
+    installed_fields[i] = getattr(forms, i)
+
+
+@add_by_field(installed_fields, "__name__")
+class TextareaField(forms.CharField):
+    widget = forms.Textarea
+
+# extra attributes for fields:
+# limit_to_usercomponent = "<fieldname">: limit field name to associated uc
+# limit_to_user = "<fieldname">: limit field name to user of associated uc
+
+
+def localized_choices(obj):
+    def func(*, choices=(), **kwargs):
+        choices = map(lambda x: (x[0], gettext(x[1])), choices)
+        return obj(choices=choices, **kwargs)
+    return func
+
+
+installed_fields["LocalizedChoiceField"] = localized_choices(forms.ChoiceField)
+installed_fields["MultipleLocalizedChoiceField"] = \
+    localized_choices(forms.MultipleChoiceField)
+
+
+@add_by_field(installed_fields, "__name__")
+class UserContentRefField(forms.ModelChoiceField):
+    filter_strength_link = "associated_rel__usercomponent__strength__lte"
+    exclude_travel = (
+        "associated_rel__usercomponent__travel_protected__in",
+        "associated_rel__travel_protected__in",
+    )
+
+    # limit_to_uc: limit to usercomponent, if False to user
+    # True is strongly recommended to prevent info leak gadgets
+    def __init__(self, modelname, limit_to_uc=True, **kwargs):
+        from spkcspider.apps.spider.contents import BaseContent
+        if limit_to_uc:
+            self.filters_usercomponent = (
+                "associated_rel__usercomponent",
+                "associated_rel__referenced_by__usercomponent"
+            )
+        else:
+            self.filters_user = ("associated_rel__usercomponent__user",)
+
+        model = apps.get_model(modelname)
+        if not issubclass(model, BaseContent):
+            raise Exception("Not a content (inherit from BaseContent)")
+
+        kwargs["queryset"] = model.objects.filter(
+            **kwargs.pop("limit_choices_to", {})
+        )
+        super().__init__(**kwargs)
+
+    def tagdata_from_value(self, obj):
+        if obj:
+            return obj.pk
+        return None
+
+    def label_from_instance(self, obj):
+        return str(obj)
+
+
+@add_by_field(installed_fields, "__name__")
+class MultipleUserContentRefField(forms.ModelMultipleChoiceField):
+    filter_strength_link = "associated_rel__usercomponent__strength__lte"
+    exclude_travel = (
+        "associated_rel__usercomponent__travel_protected__in",
+        "associated_rel__travel_protected__in",
+    )
+
+    # limit_to_uc: limit to usercomponent, if False to user
+    # True is strongly recommended to prevent info leak gadgets
+    def __init__(self, modelname, limit_to_uc=True, **kwargs):
+        from spkcspider.apps.spider.contents import BaseContent
+        if limit_to_uc:
+            self.filters_usercomponent = (
+                "associated_rel__usercomponent",
+                "associated_rel__referenced_by__usercomponent"
+            )
+        else:
+            self.filters_user = ("associated_rel__usercomponent__user",)
+
+        model = apps.get_model(
+            modelname
+        )
+        if not issubclass(model, BaseContent):
+            raise Exception("Not a content (inherit from BaseContent)")
+
+        kwargs["queryset"] = model.objects.filter(
+            **kwargs.pop("limit_choices_to", {})
+        )
+        super().__init__(**kwargs)
+
+    def tagdata_from_value(self, query):
+        return list(query.values_list(
+            'pk', flat=True
+        ))
+
+    def label_from_instance(self, obj):
+        return str(obj)
+
+
+@add_by_field(installed_fields, "__name__")
+class AnchorField(forms.ModelChoiceField):
+    spkc_use_uriref = True
+    use_default_anchor = None
+    filter_strength_link = "usercomponent__strength__lte"
+    exclude_travel = (
+        "associated_rel__usercomponent__travel_protected__in",
+        "associated_rel__travel_protected__in",
+    )
+
+    # limit_to_uc: limit to usercomponent, if False to user
+    def __init__(
+        self, use_default_anchor=True, limit_to_uc=True, **kwargs
+    ):
+        from spkcspider.apps.spider.models import AssignedContent
+        _ = gettext_lazy
+        if limit_to_uc:
+            self.filters_usercomponent = ("usercomponent",)
+        else:
+            self.filters_user = ("usercomponent__user",)
+        self.use_default_anchor = use_default_anchor
+        if use_default_anchor:
+            kwargs.setdefault("required", False)
+        if use_default_anchor and not kwargs.get("empty_label", None):
+            kwargs["empty_label"] = _("(Use default anchor)")
+
+        # can also use Links to anchor as anchor
+        kwargs["queryset"] = AssignedContent.objects.filter(
+            info__contains="\x1eanchor\x1e",
+            **kwargs.pop("limit_choices_to", {})
+        )
+        super().__init__(**kwargs)
+
+    def to_python(self, value):
+        if value in self.empty_values:
+            return None
+        try:
+            key = self.to_field_name or 'pk'
+            value = self.queryset.get(**{key: value}).content
+        except (ValueError, TypeError, self.queryset.model.DoesNotExist):
+            raise forms.ValidationError(
+                self.error_messages['invalid_choice'], code='invalid_choice'
+            )
+        return value
+
+    def tagdata_from_value(self, obj):
+        if obj:
+            return obj.associated.id
+        return None
+
+    def label_from_instance(self, obj):
+        return str(obj.content)
+
+
+class StartSub(forms.Field):
+    widget = SubSectionStartWidget
+    hashable = False
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.widget.label = self.label
+
+
+class StopSub(forms.Field):
+    widget = SubSectionStopWidget
+    hashable = False
+
+
+def generate_fields(layout, prefix="", _base=None, _mainprefix=None):
+    if _base is None:
+        _base = []
+        _mainprefix = prefix
+    for i in layout:
+        item = i.copy()
+        item.setdefault("required", False)
+        try:
+            key, field = item.pop("key", None), item.pop("field", None)
+        except Exception:
+            logging.warning("Invalid item (no dict)", i)
+            continue
+        localize = item.pop("localize", False)
+        nonhashable = item.pop("nonhashable", False)
+        if not item.get("label"):
+            item["label"] = key.replace(_mainprefix, "", 1)
+
+        if localize:
+            item["label"] = gettext(item["label"])
+            if "help_text" in item:
+                item["help_text"] = gettext(item["help_text"])
+        if not key or "/" in key:
+            logging.warning("Invalid item (no key/contains /)", i)
+            continue
+        if isinstance(field, list):
+            new_prefix = posixpath.join(prefix, key)
+            item["required"] = False
+            item["initial"] = None
+            # by prefixing with _ invalidate prefix for tag recognition
+            _base.append((
+                "_{}_start".format(new_prefix),
+                StartSub(**item)
+            ))
+            generate_fields(
+                field, new_prefix, _base=_base, _mainprefix=_mainprefix
+            )
+            # by prefixing with _ invalidate prefix for tag recognition
+            _base.append((
+                "_{}_stop".format(new_prefix),
+                StopSub(**item)
+            ))
+        elif isinstance(field, str):
+            new_field = installed_fields.get(field, None)
+            if not new_field:
+                logging.warning("Invalid field specified: %s", field)
+            else:
+                new_field = new_field(**item)
+                setattr(new_field, "hashable", not nonhashable)
+                _base.append((posixpath.join(prefix, key), new_field))
+        else:
+            logging.warning("Invalid item", i)
+    return _base
