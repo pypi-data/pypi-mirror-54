e9528faf4d419794a9b5396ce5e2ad9fb9f7c0b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:12 +0000

    Add package quetzal-openapi-client/0.5.0/quetzal_openapi_client-0.5.0-py3-none-any.whl

---
+++ packages/quetzal-openapi-client/quetzal_openapi_client-0.5.0-py3-none-any.whl/quetzal/openapi_client/api/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from quetzal.openapi_client.api.authentication_api import AuthenticationApi
+from quetzal.openapi_client.api.data_api import DataApi
+from quetzal.openapi_client.api.public_api import PublicApi
+from quetzal.openapi_client.api.query_api import QueryApi
+from quetzal.openapi_client.api.workspace_api import WorkspaceApi
