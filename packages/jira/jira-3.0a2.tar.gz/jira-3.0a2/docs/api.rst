8c141cf493c994b4364469a7226cce204a995e33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:10 +0000

    Add package jira/3.0a2/jira-3.0a2.tar.gz

---
+++ packages/jira/jira-3.0a2.tar.gz/jira-3.0a2/docs/api.rst
@@ -0,0 +1,42 @@
+API Documentation
+*****************
+
+.. module:: jira
+
+.. contents:: Contents
+   :local:
+
+JIRA
+====
+
+.. autoclass:: JIRA
+
+Issue
+========
+
+.. autoclass:: Issue
+
+Priority
+========
+
+.. autoclass:: Priority
+
+Comment
+=======
+
+.. autoclass:: Comment
+
+Worklog
+=======
+
+.. autoclass:: Worklog
+
+Watchers
+========
+
+.. autoclass:: Watchers
+
+JIRAError
+=========
+
+.. autoclass:: JIRAError
---
+++ packages/jira/jira-3.0a2.tar.gz/jira-3.0a2/examples/greenhopper.py
@@ -0,0 +1,18 @@
+# This script shows how to use the client in anonymous mode
+# against jira.atlassian.com.
+from jira.client import GreenHopper
+
+# By default, the client will connect to a JIRA instance started from the Atlassian Plugin SDK
+# (see https://developer.atlassian.com/display/DOCS/Installing+the+Atlassian+Plugin+SDK for details).
+# Override this with the options parameter.
+# GreenHopper is a plugin in a JIRA instance
+options = {"server": "https://jira.atlassian.com"}
+gh = GreenHopper(options)
+
+# Get all boards viewable by anonymous users.
+boards = gh.boards()
+
+# Get the sprints in a specific board
+board_id = 441
+print("GreenHopper board: %s (%s)" % (boards[0].name, board_id))
+sprints = gh.sprints(board_id)
