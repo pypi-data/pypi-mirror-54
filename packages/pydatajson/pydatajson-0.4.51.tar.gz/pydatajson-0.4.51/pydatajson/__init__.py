ea363fe9814206a7ad46f712d639d22895c99359
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:49 +0000

    Add package pydatajson/0.4.51/pydatajson-0.4.51.tar.gz

---
+++ packages/pydatajson/pydatajson-0.4.51.tar.gz/pydatajson-0.4.51/pydatajson/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Módulo pydatajson
+Conjunto de herramientas para validar y manipular la información presente en
+el archivo `data.json` de un Portal de Datos
+"""
+
+from __future__ import absolute_import
+from .core import DataJson
+from .helpers import parse_repeating_time_interval
+from . import helpers
+import logging
+
+__author__ = """Datos Argentina"""
+__email__ = 'datos@modernizacion.gob.ar'
+__version__ = '0.4.51'
+
+"""
+Logger base para librería pydatajson
+https://docs.python.org/2/howto/logging.html#configuring-logging-for-a-library
+"""
+logger = logging.getLogger('pydatajson')
+logger.addHandler(logging.NullHandler())
