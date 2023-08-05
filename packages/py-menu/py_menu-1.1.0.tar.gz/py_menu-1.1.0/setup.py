8cd3e78e88ed9347ed10c02d00159c87a9be3124
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:51 +0000

    Add package py-menu/1.1.0/py_menu-1.1.0.tar.gz

---
+++ packages/py-menu/py_menu-1.1.0.tar.gz/py_menu-1.1.0/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+short_description = """\
+Two classes used for creating a command-line driven menu interface"""
+
+try:
+  with open("README.md", "r") as readme:
+    long_description = readme.read()
+except:
+  long_description = short_description
+
+classifiers = ["Programming Language :: Python :: 3",
+               "License :: OSI Approved :: MIT License",
+               "Operating System :: OS Independent",
+               "Development Status :: 5 - Production/Stable",
+               "Environment :: Console"]
+
+setuptools.setup(name = "py_menu",
+                 version = "1.1.0",
+                 author = "John Gresl",
+                 author_email = "j.gresl12@gmail.com",
+                 description = "Command-line driven menu interface in python",
+                 long_description = long_description,
+                 long_description_content_type = "text/markdown",
+                 url = "https://github.com/SyntaxVoid/py_menu",
+                 packages = ["py_menu"],
+                 package_dir = {"py_menu": "py_menu"},
+                 package_data = {"py_menu": ["examples/*"]},
+                 classifiers = classifiers,
+                 python_requires = ">=3.0")
