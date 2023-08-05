73c6ea5553c07eef2e28de2d07d879d4bf83610b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:37:06 +0000

    Add package LsBook/0.2.3/LsBook-0.2.3-py3-none-any.whl

---
+++ packages/LsBook/LsBook-0.2.3-py3-none-any.whl/LsBook/parse/parse_readme.py
@@ -0,0 +1,20 @@
+import os
+
+from ..models.book import Book
+from ..utils.error import error, file_not_found_error
+
+
+def readme_exist(book: Book):
+    """验证readme文件
+
+    :param book:
+    :return:
+    """
+    readme = os.path.join(book.book_path, "readme.md")
+    if not os.path.isfile(readme):
+        readme = os.path.join(book.book_path, "README.md")
+        if not os.path.isfile(readme):
+            file_not_found_error(readme, "书籍目录必须存在 readme.md")
+
+    if len(book.summary[0]['articles']) != 0 and book.summary[0]['articles'][0].get("ref", "").lower() != "readme.md":
+        error("目录一个链接必须为 readme.md")
