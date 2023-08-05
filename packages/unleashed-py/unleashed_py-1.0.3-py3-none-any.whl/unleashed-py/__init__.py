1bc734569a009af80c209415dd080c531188e2c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:56 +0000

    Add package unleashed-py/1.0.3/unleashed_py-1.0.3-py3-none-any.whl

---
+++ packages/unleashed-py/unleashed_py-1.0.3-py3-none-any.whl/unleashed-py/__init__.py
@@ -0,0 +1,10 @@
+"""unleashed-py - Python package to connect to the Unleashed Software inventory management API"""
+import sys, os
+__version__ = '0.0.12'
+__author__ = 'Jonathan Mucha <jonmucha@gmail.com>'
+sys.path.append(os.path.join(os.path.dirname(__file__)))
+
+# __all__ = []
+from .unleashed_py import UnleashedBase
+from .unleashed_py import Resource
+from .unleashed_py import EditableResource
