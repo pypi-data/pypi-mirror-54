27cc3ed921a420fe0ccb8a7fb8583b7ccdf08f22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:09 +0000

    Add package kielbasa/0.0.1/kielbasa-0.0.1.tar.gz

---
+++ packages/kielbasa/kielbasa-0.0.1.tar.gz/kielbasa-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="kielbasa",
+    version="0.0.1",
+    author="Josh Smith",
+    author_email="josh@joshingmachine.com",
+    description="All-Natural Casing Utilities",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/joshingmachine/kielbasa",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
