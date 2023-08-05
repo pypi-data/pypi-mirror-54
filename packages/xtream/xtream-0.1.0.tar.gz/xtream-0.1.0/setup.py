977cd8e3398ebe50bb753b65f32bddebee168f8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:43 +0000

    Add package xtream/0.1.0/xtream-0.1.0.tar.gz

---
+++ packages/xtream/xtream-0.1.0.tar.gz/xtream-0.1.0/setup.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    README = readme_file.read()
+
+with open('HISTORY.md') as history_file:
+    HISTORY = history_file.read()
+
+setup_args = dict(
+    name='xtream',
+    version='0.1.0',
+    description='nstream streaming platform',
+    long_description_content_type="text/markdown",
+    long_description=README + '\n\n' + HISTORY,
+    license='MIT',
+    packages=find_packages(),
+    author='Vladmir Impala',
+    author_email='your@email.com',
+    keywords=['NStream', 'IPTV Platform', 'Streaming Platform'],
+    url='https://github.com/xtream/xtream',
+    download_url='https://pypi.org/project/xtream/'
+)
+
+install_requires = [
+    "requests",
+        "docopt",
+        "requests",
+        "django",
+        "pytest",
+        "py-cpuinfo",
+        "celery",
+        "redis",
+        "psycopg2",
+        "dj-static",
+        "static3",
+        "gunicorn",
+        "django-heroku",
+        "uWSGI",
+        "mysql-connector-python",
+        "django-ratelimit",
+        "django-crispy-forms",
+        "iso8601"       
+]
+
+if __name__ == '__main__':
+    setup(**setup_args, install_requires=install_requires)
