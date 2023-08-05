5386bd40f8083f77a6539871e32a968f8cc4be29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:38:21 +0000

    Add package wasic/0.1.2/wasic-0.1.2.tar.gz

---
+++ packages/wasic/wasic-0.1.2.tar.gz/wasic-0.1.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='wasic',
+    version='0.1.2',
+    packages=['wasic'],
+    include_package_data=True,
+    entry_points={'console_scripts': [
+        'wasiar = wasic.wasiar:run',
+        'wasic = wasic.wasic:run',
+        'wasicc = wasic.wasicc:run',
+        'wasic++ = wasic.wasicc:run',
+        'wasiconfigure = wasic.wasiconfigure:run',
+        'wasild = wasic.wasild:run',
+        'wasimake = wasic.wasimake:run',
+        'wasirun = wasic.wasirun:run',
+        'wasinm = wasic.wasinm:run',
+        'wasiranlib = wasic.wasiranlib:run',
+    ]},
+)
