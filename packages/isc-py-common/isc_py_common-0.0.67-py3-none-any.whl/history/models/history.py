dedaa8cbf16475070f449c91849d25a9d906f768
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:14 +0000

    Add package isc-py-common/0.0.67/isc_py_common-0.0.67-py3-none-any.whl

---
+++ packages/isc-py-common/isc_py_common-0.0.67-py3-none-any.whl/history/models/history.py
@@ -0,0 +1,24 @@
+import logging
+
+from django.contrib.postgres.fields import JSONField
+from django.db.models import Model, DateTimeField
+from django.utils import timezone
+from isc_common.fields.code_field import CodeField, CodeStrictField
+from isc_common.fields.name_field import NameField
+
+logger = logging.getLogger(__name__)
+
+
+class History(Model):
+    date = DateTimeField(db_index=True, default=timezone.now)
+    method = CodeField()
+    path = CodeStrictField()
+    username = NameField()
+    fio = NameField()
+    data = JSONField(default=dict)
+
+    def __str__(self):
+        return f"method: {self.method} path: {self.path} data{self.data}"
+
+    class Meta:
+        verbose_name = 'History'
