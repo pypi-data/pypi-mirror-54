cb233f6f33dbc70a518d8a0ccc415ba56c961b41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:35 +0000

    Add package mc-sprite/0.0.22/mc_sprite-0.0.22.tar.gz

---
+++ packages/mc-sprite/mc_sprite-0.0.22.tar.gz/mc_sprite-0.0.22/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+setup(
+    name='mc_sprite',
+    version='0.0.22',
+    author='魔扣少儿编程_Hugn',
+    author_email='',
+    url ='https://www.coding4fun.com.cn/',
+    install_requires=['pygame>=1.9.4'
+                      ],
+    packages = ['GameObject','Engine','Timer'],
+    data_files=[('',['mc_sprite.py'])
+                ],
+    include_package_data = True, 
+    zip_safe=False,
+    )
