dce53d5881a59d5a1a6ef045f63cd411c982c6e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:08 +0000

    Add package echopy-lib/0.1.42/echopy_lib-0.1.42-py3-none-any.whl

---
+++ packages/echopy-lib/echopy_lib-0.1.42-py3-none-any.whl/echopy/echo.py
@@ -0,0 +1,21 @@
+from .echoapi import Api
+from .transaction import Transaction
+from .echobase.config import Config
+from .echobase.account import BrainKey
+
+
+class Echo:
+
+    def __init__(self):
+        self.api = Api()
+        self.config = Config()
+        self.brain_key = BrainKey
+
+    def create_transaction(self):
+        return Transaction(self.api)
+
+    def connect(self, url, debug=False):
+        self.api.connect(url, debug)
+
+    def disconnect(self):
+        self.api.disconnect()
---
+++ packages/echopy-lib/echopy_lib-0.1.42-py3-none-any.whl/echopy/echobase/config/object_types.py
@@ -0,0 +1,28 @@
+#: object_types
+class ObjectTypes:
+    def __init__(self):
+        self.NULL = 0
+        self.BASE = 1
+        self.ACCOUNT = 2
+        self.ASSET = 3
+        self.COMMITTEE_MEMBER = 4
+        self.PROPOSAL = 5
+        self.OPERATION_HISTORY = 6
+        self.VESTING_BALANCE = 7
+        self.BALANCE = 8
+        self.FROZEN_BALANCE = 9
+        self.COMMITTEE_FROZEN_BALANCE = 10
+        self.CONTRACT = 11
+        self.CONTRACT_RESULT = 12
+        self.BLOCK_RESULT = 13
+        self.ETH_ADDRESS = 14
+        self.DEPOSIT_ETH = 15
+        self.WITHDRAW_ETH = 16
+        self.ERC20_TOKEN = 17
+        self.DEPOSIT_ERC20_TOKEN = 18
+        self.WITHDRAW_ERC20_TOKEN = 19
+        self.BTC_ADDRESS = 20
+        self.BTC_INTERMEDIATE_DEPOSIT = 21
+        self.BTC_DEPOSIT = 22
+        self.BTC_WITHDRAW = 23
+        self.BTC_AGGREGATING = 24
