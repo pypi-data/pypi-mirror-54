4f6e07dd44f25e388f45fcb405b292d9837a787f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:40 +0000

    Add package lino-xl/19.10.3/lino-xl-19.10.3.tar.gz

---
+++ packages/lino-xl/lino-xl-19.10.3.tar.gz/lino-xl-19.10.3/lino_xl/lib/groups/fixtures/demo.py
@@ -0,0 +1,22 @@
+# -*- coding: UTF-8 -*-
+# Copyright 2017 Rumma & Ko Ltd
+#
+# License: BSD (see file COPYING for details)
+
+
+from lino.api import rt, _
+from lino.utils.mldbc import babel_named as named
+
+
+def objects():
+    Group = rt.models.groups.Group
+    User = rt.models.users.User
+    UserTypes = rt.models.users.UserTypes
+
+    yield named(Group, _("Hitchhiker's Guide to the Galaxy"))
+    yield named(Group, _("Star Trek"))
+    yield named(Group, _("Harry Potter"))
+
+    yield User(username="andy", user_type=UserTypes.user)
+    yield User(username="bert", user_type=UserTypes.user)
+    yield User(username="chloe", user_type=UserTypes.user)
