768eb3391dd367503adb7c25679ecd860b4ba8d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:35 +0000

    Add package wf-gqlpycgen/0.5.7/wf-gqlpycgen-0.5.7.tar.gz

---
+++ packages/wf-gqlpycgen/wf-gqlpycgen-0.5.7.tar.gz/wf-gqlpycgen-0.5.7/setup.py
@@ -0,0 +1,41 @@
+import os
+from setuptools import setup, find_packages
+
+BASEDIR = os.path.dirname(os.path.abspath(__file__))
+VERSION = open(os.path.join(BASEDIR, 'VERSION')).read().strip()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(BASEDIR))
+
+test_dependencies = [
+    'coverage',
+    'mock',
+    'nose',
+    'rednose'
+]
+
+setup(
+    name='wf-gqlpycgen',
+    version=VERSION,
+    packages=find_packages(),
+    include_package_data=True,
+    description='GraphQL Python CLient Generator',
+    long_description='Reads a GQL schema and turns it into a python library.',
+    url='https://github.com/Wildflowerschools/graphql-python-client-generator',
+    author='optimuspaul',
+    author_email='paul.decoursey@wildflowerschools.org',
+    install_requires=[
+        'Jinja2==2.10',
+        'click>=6.7',
+        'requests==2.21.0'
+    ],
+    tests_require = test_dependencies,
+    extras_require = {
+        'test': test_dependencies
+    },
+    entry_points={
+        'console_scripts': [
+            'gqlpycgen=gqlpycgencli:cli',
+        ],
+    }
+)
