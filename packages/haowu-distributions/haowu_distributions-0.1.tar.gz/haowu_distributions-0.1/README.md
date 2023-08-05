79a49f37cf445835da862eb22435e70a8d1ec7f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:53 +0000

    Add package haowu-distributions/0.1/haowu_distributions-0.1.tar.gz

---
+++ packages/haowu-distributions/haowu_distributions-0.1.tar.gz/haowu_distributions-0.1/README.md
@@ -0,0 +1,9 @@
+# haowu_distributions package
+
+
+Summary of this package
+
+
+# Files
+
+# Installation
---
+++ packages/haowu-distributions/haowu_distributions-0.1.tar.gz/haowu_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='haowu_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions'],
+      author='Hao Wu',
+      author_email='haowu@nyu.edu',
+      zip_safe=False)
