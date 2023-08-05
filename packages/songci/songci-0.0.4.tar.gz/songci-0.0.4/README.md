5e435e5114c461c293732a6acc27952ff1a2e805
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:37 +0000

    Add package songci/0.0.4/songci-0.0.4.tar.gz

---
+++ packages/songci/songci-0.0.4.tar.gz/songci-0.0.4/README.md
@@ -0,0 +1,122 @@
+
+<p align="center"> 
+    <img src="https://github.com/terbeznik/songci/blob/master/bin/logo.png">
+</p>
+
+#  songci
+
+## Meaning
+> Song Ci was a Chinese physician, judge, forensic medical scientist, anthropologist, and writer of the Southern Song dynasty. He was the first known anthropologist who wrote a groundbreaking book titled Collected Cases of Injustice Rectified.
+
+## Install
+```console
+$ pip install songci
+```
+
+## CLI
+### Single proxy
+
+```console
+$ songci -p 103.60.137.2:41630
+103.60.137.2:41630 http=failed https=elite
+```
+
+### Multiple proxies
+
+```console
+$ songci -p 118.174.220.133:50616 185.18.64.106:53281 180.254.90.87:8080
+180.254.90.87:8080 http=transparent https=elite evil
+118.174.220.133:50616 http=elite https=failed
+185.18.64.106:53281 http=failed https=failed
+```
+
+### Random proxies
+Use the built-in web scraper to get and check random proxies from the internet.
+
+```console
+$ songci -r 10
+118.99.93.6:8080 http=failed https=elite
+103.242.13.69:8082 http=elite https=failed
+103.36.35.135:8080 http=transparent https=elite
+125.26.41.220:4145 http=failed https=failed
+203.78.144.218:37190 http=failed https=elite
+62.201.230.37:48958 http=elite https=failed
+186.219.210.209:35215 http=failed https=failed
+37.32.36.29:1080 http=failed https=failed
+111.77.20.118:9000 http=failed https=failed
+181.143.223.139:4145 http=failed https=failed
+```
+
+### Output
+To write the complete check results use `-o`.
+
+```console
+$ songci -r 3 -o
+180.254.90.87:8080 http=transparent https=elite evil
+185.18.64.106:53281 http=elite https=elite
+118.174.220.133:50616 http=failed https=failed
+```
+
+Each check contains a json with **complete results** and a HTML file of each **content manipulation** found.
+
+```
+2019-10-19T10_34_30/
+|--- 180.254.90.87:8080 (http).html
+|--- summary.json
+```
+
+Structure of results inside `summary.json`
+
+```json
+{
+  "ip": "",
+  "port": "",
+  "check_headers": "",
+  "check_html": "",
+  "begin": "",
+  "end": ""
+}
+```
+
+### Additional parameters
+#### Threads
+`songci` uses threading to check multiple proxies at the same time. With `-t` you can set amount of threads.
+
+```console
+$ sonci -r 100 -t 10
+```
+
+### Timeout
+Each check contains multiple HTTP-Requests. Default timeout for each request is 60 seconds. You can change it with `--timeout`.
+
+```
+$ songci -r 100 --timeout 20
+```
+
+## Python
+
+### Single proxy
+
+```pycon
+>>> import songci
+>>> proxy = songci.get_proxy()
+>>> result = songci.check(proxy)
+171.6.73.253:8080 http=anonymous https=elite
+>>> result
+{'ip': '171.6.73.253', 'port': '8080', 'check_headers': {'http': {'status_code': 200, 'status_code_on_proxy': None, 'client_request_headers': {'User-Agent': 'python-requests/2.20.0', 'Accept-Encoding': 'gzip, deflate', 'Accept': '*/*', 'Connection': 'keep-alive'}, 'server_request_headers': {'args': {}, 'headers': {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8', 'Accept-Encoding': 'gzip', 'Accept-Language': 'zh-CN,zh;q=0.8,en;q=0.6', 'Cache-Control': 'max-age=0', 'Host': 'httpbin.org', 'If-Modified-Since': 'Sat, 19 Oct 2019 11:04:22 GMT', 'User-Agent': 'Mozilla/5.0 (compatible; WOW64; MSIE 10.0; Windows NT 6.2)', 'X-Proxy-Id': '438871588'}, 'origin': '183.89.1.147, 183.89.1.147', 'url': 'https://httpbin.org/get'}, 'anonymity_level': 2, 'response_headers': {'Content-Length': '527', 'Content-Type': 'application/json', 'Server': 'nginx', 'Last-Modified': 'Sat, 19 Oct 2019 11:05:20 GMT', 'Date': 'Sat, 19 Oct 2019 11:23:07 GMT'}, 'error': None}, 'https': {'status_code': 200, 'status_code_on_proxy': None, 'client_request_headers': {'User-Agent': 'python-requests/2.20.0', 'Accept-Encoding': 'gzip, deflate', 'Accept': '*/*', 'Connection': 'keep-alive'}, 'server_request_headers': {'args': {}, 'headers': {'Accept': '*/*', 'Accept-Encoding': 'gzip, deflate', 'Host': 'httpbin.org', 'User-Agent': 'python-requests/2.20.0'}, 'origin': '171.6.73.253, 171.6.73.253', 'url': 'https://httpbin.org/get'}, 'anonymity_level': 3, 'response_headers': {'Access-Control-Allow-Credentials': 'true', 'Access-Control-Allow-Origin': '*', 'Content-Encoding': 'gzip', 'Content-Type': 'application/json', 'Date': 'Sat, 19 Oct 2019 12:12:57 GMT', 'Referrer-Policy': 'no-referrer-when-downgrade', 'Server': 'nginx', 'X-Content-Type-Options': 'nosniff', 'X-Frame-Options': 'DENY', 'X-XSS-Protection': '1; mode=block', 'Content-Length': '182', 'Connection': 'keep-alive'}, 'error': None}}, 'check_html': {'http': {'status_code': 200, 'status_code_on_proxy': None, 'client_request_headers': {'User-Agent': 'python-requests/2.20.0', 'Accept-Encoding': 'gzip, deflate', 'Accept': '*/*', 'Connection': 'keep-alive'}, 'response_headers': {'Access-Control-Allow-Credentials': 'true', 'Access-Control-Allow-Origin': '*', 'Content-Type': 'text/html; charset=utf-8', 'Date': 'Sat, 19 Oct 2019 12:13:07 GMT', 'Referrer-Policy': 'no-referrer-when-downgrade', 'Server': 'nginx', 'X-Content-Type-Options': 'nosniff', 'X-Frame-Options': 'DENY', 'X-XSS-Protection': '1; mode=block', 'Content-Length': '3741', 'Age': '0'}, 'content_manipulation': None, 'error': None}, 'https': {'status_code': 200, 'status_code_on_proxy': None, 'client_request_headers': {'User-Agent': 'python-requests/2.20.0', 'Accept-Encoding': 'gzip, deflate', 'Accept': '*/*', 'Connection': 'keep-alive'}, 'response_headers': {'Access-Control-Allow-Credentials': 'true', 'Access-Control-Allow-Origin': '*', 'Content-Encoding': 'gzip', 'Content-Type': 'text/html; charset=utf-8', 'Date': 'Sat, 19 Oct 2019 12:13:07 GMT', 'Referrer-Policy': 'no-referrer-when-downgrade', 'Server': 'nginx', 'X-Content-Type-Options': 'nosniff', 'X-Frame-Options': 'DENY', 'X-XSS-Protection': '1; mode=block', 'Content-Length': '1936', 'Connection': 'keep-alive'}, 'content_manipulation': None, 'error': None}}, 'begin': '2019-10-19T12_12_42', 'end': '2019-10-19T12_13_08'}
+```
+
+### Multiple proxies
+
+```pycon
+>>> proxies = songci.get_proxies(n=100)
+>>> results = songci.check(proxies)
+>>> type(results)
+<class 'list'>
+```
+
+### Default parameters
+
+```python
+songci.check(threads=4, timeout=60)
+```
---
+++ packages/songci/songci-0.0.4.tar.gz/songci-0.0.4/songci/api.py
@@ -0,0 +1,233 @@
+import requests
+import socket
+import hashlib
+import json
+
+from multiprocessing.dummy import Pool as ThreadPool
+from tqdm import tqdm
+from colorama import init as init_colorama
+from colorama import Fore, Back, Style
+
+from . import helper
+
+THREADS = 4
+TIMEOUT = 60
+PROTOCOLS = ["http", "https"]
+
+init_colorama()
+ANONYMITY_LEVEL_OUTPUT = {
+    1: f"{Style.BRIGHT}transparent{Style.RESET_ALL}",
+    2: f"{Style.BRIGHT}{Fore.CYAN}anonymous{Style.RESET_ALL}",
+    3: f"{Style.BRIGHT}{Fore.GREEN}elite{Style.RESET_ALL}",
+    None: f"{Style.BRIGHT}{Fore.RED}failed{Style.RESET_ALL}"
+}
+CONTENT_MANIPULATION_OUTPUT = f"{Style.BRIGHT}{Back.RED}evil{Style.RESET_ALL}"
+
+
+class Check():
+
+    BAD_REQUEST_400 = "HTTP/1.1 400 Bad Request\r\n\r\n"
+    HTML_URL = "httpbin.org/html"
+    HEADERS_URL = "httpbin.org/get"
+    IP_URL = "httpbin.org/ip"
+    L4_HEADER = [
+        "Mt-Proxy-ID",
+        "Proxy-agent",
+        "Proxy-Connection",
+        "Surrogate-Capability",
+        "Via",
+        "Xroxy-Connection"
+    ]
+
+    def __init__(self,
+                 proxy,
+                 reference_html=None,
+                 reference_ip=None,
+                 protocols=PROTOCOLS,
+                 timeout=TIMEOUT):
+
+        self.protocols = protocols
+        self.timeout = timeout
+
+        try:
+            self.proxy = proxy.replace(" ", "")
+            self.ip, self.port = proxy.split(":")
+            socket.getaddrinfo(self.ip, self.port)
+        except Exception as e:
+            raise e
+
+        self.reference_html = reference_html
+        self.reference_ip = reference_ip
+        self.session = requests.Session()
+
+        self.result = {
+            "ip": self.ip,
+            "port": self.port,
+        }
+
+    def __str__(self):
+        return json.dumps(self.result, indent=4)
+
+    @property
+    def result_for_humans(self):
+        http = ANONYMITY_LEVEL_OUTPUT[self.result["check_headers"]["http"]["anonymity_level"]]
+        https = ANONYMITY_LEVEL_OUTPUT[self.result["check_headers"]["https"]["anonymity_level"]]
+
+        if self.result["check_html"]["http"]["content_manipulation"] or self.result["check_html"]["https"]["content_manipulation"]:
+            content_manipulation = CONTENT_MANIPULATION_OUTPUT
+        else:
+            content_manipulation = ""
+
+        cols = [
+            f"{self.ip}:{self.port}",
+            f"http={http}",
+            f"https={https}",
+            content_manipulation
+        ]
+
+        return f" ".join(cols)
+
+    def get_reference_html():
+        url = f"https://{Check.HTML_URL}"
+        r = requests.get(url)
+        return r.content
+
+    def get_reference_ip():
+        url = f"https://{Check.IP_URL}"
+        r = requests.get(url)
+        ip = r.json()["origin"]
+        ip = ip.replace(" ", "")
+        if "," in ip:
+            ip = ip.split(",")[0]
+        return ip
+
+    def anonymity_level(headers, reference_ip):
+        if reference_ip in str(headers):
+            return 1
+
+        for header in headers["headers"]:
+            if header in Check.L4_HEADER or header.lower()[:2] == "x-":
+                return 2
+
+        return 3
+
+    def run(self):
+        self.begin = helper.get_timestamp()
+
+        self.check_headers()
+        self.check_html()
+
+        self.end = helper.get_timestamp()
+
+        self.result["begin"] = self.begin
+        self.result["end"] = self.end
+
+    def check_headers(self):
+        self.result["check_headers"] = {}
+
+        if not self.reference_ip:
+            self.reference_ip = Check.get_reference_ip()
+
+        for protocol in self.protocols:
+            result = {
+                "status_code": None,
+                "status_code_on_proxy": None,
+                "client_request_headers": None,
+                "server_request_headers": None,
+                "anonymity_level": None,
+                "response_headers": None,
+                "error": None
+            }
+
+            self.session.proxies = {protocol: f"{protocol}://{self.proxy}"}
+            url = f"{protocol}://{Check.HEADERS_URL}"
+
+            try:
+                r = self.session.get(url, timeout=self.timeout)
+                result["status_code"] = r.status_code
+                if r.text == Check.BAD_REQUEST_400:
+                    result["status_code_on_proxy"] = 400
+                result["client_request_headers"] = dict(r.request.headers)
+                result["server_request_headers"] = r.json()
+                result["anonymity_level"] = Check.anonymity_level(
+                    result["server_request_headers"],
+                    self.reference_ip
+                )
+                result["response_headers"] = dict(r.headers)
+
+            except Exception as e:
+                result["error"] = str(type(e))
+
+            self.result["check_headers"][protocol] = result
+
+    def check_html(self):
+        self.result["check_html"] = {}
+
+        if not self.reference_html:
+            self.reference_html = Check.get_reference_html()
+
+        self.reference_html_hash = hashlib.md5(self.reference_html).digest()
+
+        for protocol in self.protocols:
+            result = {
+                "status_code": None,
+                "status_code_on_proxy": None,
+                "client_request_headers": None,
+                "response_headers": None,
+                "content_manipulation": None,
+                "error": None
+            }
+
+            self.session.proxies = {protocol: f"{protocol}://{self.proxy}"}
+            url = f"{protocol}://{Check.HTML_URL}"
+
+            try:
+                r = self.session.get(url, timeout=self.timeout)
+                result["status_code"] = r.status_code
+                result["client_request_headers"] = dict(r.request.headers)
+                result["response_headers"] = dict(r.headers)
+                if r.status_code == 200:
+                    html_hash = hashlib.md5(r.content).digest()
+                    if html_hash != self.reference_html_hash:
+                        if r.text == Check.BAD_REQUEST_400:
+                            result["status_code_on_proxy"] == 400
+                        else:
+                            result["content_manipulation"] = r.text
+            except Exception as e:
+                result["error"] = str(type(e))
+
+            self.result["check_html"][protocol] = result
+
+
+def check(proxy_data, threads=THREADS, timeout=TIMEOUT):
+    if isinstance(proxy_data, str):
+        proxies = [proxy_data]
+    elif isinstance(proxy_data, list):
+        proxies = proxy_data
+
+    if len(proxies) > 1:
+        threads = threads if len(proxies) > threads else len(proxies)
+        reference_html = Check.get_reference_html()
+        reference_ip = Check.get_reference_ip()
+
+        def worker(proxy):
+            c = Check(proxy,
+                      reference_html=reference_html,
+                      reference_ip=reference_ip,
+                      timeout=timeout)
+            c.run()
+            tqdm.write(c.result_for_humans)
+            return c.result
+
+        pool = ThreadPool(threads)
+        amount = len(proxy_data)
+        results = list(tqdm(pool.imap(worker, proxy_data), total=amount, desc="Checking proxies"))
+        pool.close()
+        pool.join()
+        return results
+    elif len(proxies) == 1:
+        proxy = proxies[0]
+        c = Check(proxy, timeout=timeout)
+        c.run()
+        print(c.result_for_humans)
+        return c.result
