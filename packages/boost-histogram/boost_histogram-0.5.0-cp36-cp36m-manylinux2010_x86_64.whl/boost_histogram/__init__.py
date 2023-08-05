f67300f7f2d46d5422c69a46aae5c9839cdd3537
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:35 +0000

    Add package boost-histogram/0.5.0/boost_histogram-0.5.0-cp36-cp36m-manylinux2010_x86_64.whl

---
+++ packages/boost-histogram/boost_histogram-0.5.0-cp36-cp36m-manylinux2010_x86_64.whl/boost_histogram/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import, division, print_function
+
+from ._hist import histogram
+
+from . import axis, storage, accumulators, algorithm, numpy
+
+from .utils import loc, rebin, project, indexed, underflow, overflow
+
+from .version import __version__
