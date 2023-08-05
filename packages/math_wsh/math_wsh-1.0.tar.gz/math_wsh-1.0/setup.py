e06cd72c8090fc8ce024da4232659593ac23f385
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:47 +0000

    Add package math_wsh/1.0/math_wsh-1.0.tar.gz

---
+++ packages/math_wsh/math_wsh-1.0.tar.gz/math_wsh-1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+name='math_wsh', # 对外我们模块的名字
+version='1.0', # 版本号
+description='这是第一个对外发布的模块，测试哦', #描述
+author='shaoweiqiang', # 作者
+author_email='shaoweiqiang@live.com',
+py_modules=['math_wsh.moo1'] # 要发布的模块
+)
