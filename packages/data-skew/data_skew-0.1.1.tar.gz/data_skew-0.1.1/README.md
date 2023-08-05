8e051e194aa360c8683458a1f1a9cfecde7a543c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:04 +0000

    Add package data-skew/0.1.1/data_skew-0.1.1.tar.gz

---
+++ packages/data-skew/data_skew-0.1.1.tar.gz/data_skew-0.1.1/README.md
@@ -0,0 +1,2 @@
+# data-skew
+Create pandas data frames with a specified data Bias
---
+++ packages/data-skew/data_skew-0.1.1.tar.gz/data_skew-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='data_skew',
+      version='0.1.1',
+      description='Create DataFrames based on a required bias',
+      url='http://github.com/JVoogt/data-skew',
+      author='J Voogt',
+      author_email='jvoogt1@outlook.com',
+      license='MIT',
+      packages=['data_skew'],
+      zip_safe=False)
