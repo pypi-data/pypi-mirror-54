65faabd694564ecee3aae321f854201588da6031
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:59 +0000

    Add package recognizers-text-sequence/1.0.2a0/recognizers_text_sequence-1.0.2a0-py3-none-any.whl

---
+++ packages/recognizers-text-sequence/recognizers_text_sequence-1.0.2a0-py3-none-any.whl/recognizers_sequence/sequence/__init__.py
@@ -0,0 +1,7 @@
+from .sequence_recognizer import *
+from .constants import *
+from .models import *
+from .extractors import *
+from .parsers import *
+from .english import *
+from .chinese import *
