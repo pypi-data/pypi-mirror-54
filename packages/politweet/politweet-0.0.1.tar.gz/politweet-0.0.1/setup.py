e8d729b6ea45823ace7f15d443ea4706e6c78937
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:59 +0000

    Add package politweet/0.0.1/politweet-0.0.1.tar.gz

---
+++ packages/politweet/politweet-0.0.1.tar.gz/politweet-0.0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="politweet",
+    version="0.0.1",
+    author="",
+    author_email="author@example.com",
+    description="library for processing italian political tweets",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
