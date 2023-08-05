8e471e2f72f0c4b2a60973fa2b2883cfa6e68a86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:37:34 +0000

    Add package smlmvis/0.0.3/smlmvis-0.0.3.tar.gz

---
+++ packages/smlmvis/smlmvis-0.0.3.tar.gz/smlmvis-0.0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+import versioneer
+
+requirements = [
+    'numpy', 'vtk', 'jupyter', 'pandas', 'requests', 'seaborn'
+]
+
+setup(
+    name='smlmvis',
+    license='GPLv3',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description="Superresolution visualization of 3D protein localization data from a range of microscopes",
+    author="Ben Cardoen",
+    author_email='bcardoen@sfu.ca',
+    url='https://github.com/bencardoen/smlmvis',
+    download_url='https://github.com/bencardoen/smlmvis/archive/v0.0.1.tar.gz',
+    packages=['smlmvis'],
+    install_requires=requirements,
+    keywords='smlmvis',
+    classifiers=[
+        'Programming Language :: Python :: 3.7',
+    ]
+)
