1af4f83f6dbb825244e47fd148370047116a73e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:46 +0000

    Add package touch-callable/0.0.1/touch-callable-0.0.1.tar.gz

---
+++ packages/touch-callable/touch-callable-0.0.1.tar.gz/touch-callable-0.0.1/touch_callable/front-end/build/precache-manifest.6d82dd892a70f930e84e2192c17a6907.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee949263c7ab9b385af9f982612a67f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3ae38ba71e7d32ffa53",
+    "url": "/static/css/2.8c467cd4.chunk.css"
+  },
+  {
+    "revision": "ed75f14f97bec29903db",
+    "url": "/static/css/main.6e6e8990.chunk.css"
+  },
+  {
+    "revision": "e3ae38ba71e7d32ffa53",
+    "url": "/static/js/2.05999534.chunk.js"
+  },
+  {
+    "revision": "ed75f14f97bec29903db",
+    "url": "/static/js/main.19e8b2da.chunk.js"
+  },
+  {
+    "revision": "a4a2c54e6b5e2d190c40",
+    "url": "/static/js/runtime~main.e543b2a5.js"
+  }
+]);
