0a3abb00ccf5c95f0d59b6ef147fab1722e90dfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:26 +0000

    Add package atuproot/0.2.0/atuproot-0.2.0-py3-none-any.whl

---
+++ packages/atuproot/atuproot-0.2.0-py3-none-any.whl/atuproot/EventBuilderConfigMaker.py
@@ -0,0 +1,60 @@
+import os
+import collections
+import uproot
+
+EventBuilderConfig = collections.namedtuple(
+    'EventBuilderConfig',
+    'inputPaths treeName start_block stop_block nevents_per_block dataset name branch_cache'
+)
+
+class EventBuilderConfigMaker(object):
+    def __init__(
+        self, nevents_per_block, treename_of_files_map={},
+        predetermined_nevents_in_file={}, branch_cache={},
+    ):
+        self.nevents_per_block = nevents_per_block
+        self._treename_of_files_map = treename_of_files_map
+
+        # Cache nevents in each file - getting nevents takes a while
+        self._nevents_in_file_cache = predetermined_nevents_in_file
+        self._branch_cache = branch_cache
+
+    def create_config_for(self, dataset, files, start, length):
+        config = EventBuilderConfig(
+            inputPaths = files,
+            treeName = dataset.tree,
+            start_block = start,
+            stop_block = length,
+            nevents_per_block = self.nevents_per_block,
+            dataset = dataset,
+            name = dataset.name,
+            branch_cache = self._branch_cache,
+        )
+        return config
+
+    def file_list_in(self, dataset, maxFiles):
+        if maxFiles < 0:
+            return dataset.files
+        return dataset.files[:min(maxFiles, len(dataset.files))]
+
+    def nevents_in_file(self, path):
+        if path in self._nevents_in_file_cache:
+            nevents = self._nevents_in_file_cache[path]
+            nblocks = int((nevents-1) / self.nevents_per_block + 1)
+        else:
+            # Try to open root file with standard memmap with uproot. Use
+            # localsource option if it fails
+            try:
+                rootfile = uproot.open(path)
+                tree = rootfile[self._treename_of_files_map[path]]
+            except mmap.error:
+                def localsource(path):
+                    return uproot.FileSource(path, **uproot.FileSource.defaults)
+                rootfile = uproot.open(
+                    path, localsource=lambda p: uproot.FileSource(p, **uproot.FileSource.defaults),
+                )
+                tree = rootfile[self._treename_of_files_map[path]]
+            nevents = tree.numentries
+            self._nevents_in_file_cache[path] = nevents
+            nblocks = int((nevents-1) / self.nevents_per_block + 1)
+        return nblocks
