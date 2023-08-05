ad921f0f1516dea6a8bf64051eced3e4449b1d95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:39:04 +0000

    Add package wot/1.0.8/wot-1.0.8.tar.gz

---
+++ packages/wot/wot-1.0.8.tar.gz/wot-1.0.8/setup.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+requirements = [
+        'numpy', 'pandas', 'h5py', 'anndata', 'scikit-learn', 'scipy', 'matplotlib', 'pegasuspy', 'POT'
+]
+
+extras_require = {
+}
+
+setup_requirements = [
+        'Cython'
+]
+
+test_requirements = [
+        'unittest'
+]
+
+setuptools.setup(
+    name='wot',
+    version='1.0.8',
+    description="Optimal transport for time-course single cell data",
+    author="WOT Team",
+    author_email='wot@broadinstitute.org',
+    url='https://github.com/broadinstitute/wot',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(include=['wot']),
+    include_package_data=True,
+    install_requires=requirements,
+    license="BSD license",
+    zip_safe=False,
+    keywords='wot',
+    classifiers=[
+            'License :: OSI Approved :: BSD License',
+            'Intended Audience :: Developers',
+            'Intended Audience :: Science/Research',
+            'Natural Language :: English',
+            'Operating System :: MacOS :: MacOS X',
+            'Operating System :: Microsoft :: Windows',
+            'Operating System :: POSIX :: Linux',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            'Topic :: Scientific/Engineering :: Bio-Informatics',
+            'Topic :: Scientific/Engineering :: Visualization',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements,
+    setup_requires=setup_requirements,
+    python_requires='>= 3',
+    entry_points={
+            'console_scripts': [
+                    'wot=wot.__main__:main'
+            ]
+    },
+)
