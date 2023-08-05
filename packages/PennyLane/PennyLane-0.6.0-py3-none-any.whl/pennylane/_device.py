01694efdacfc2a9e8dfebfb84ce1b2bbeba2d6b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:27 +0000

    Add package PennyLane/0.6.0/PennyLane-0.6.0-py3-none-any.whl

---
+++ packages/PennyLane/PennyLane-0.6.0-py3-none-any.whl/pennylane/_device.py
@@ -0,0 +1,425 @@
+# Copyright 2018 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+This module contains the :class:`Device` abstract base class.
+"""
+# pylint: disable=too-many-format-args
+import abc
+
+import numpy as np
+
+from pennylane.operation import Operation, Observable, Sample, Variance, Expectation, Tensor
+from .qnode import QuantumFunctionError
+
+
+class DeviceError(Exception):
+    """Exception raised by a :class:`~.pennylane._device.Device` when it encounters an illegal
+    operation in the quantum circuit.
+    """
+    pass
+
+
+class Device(abc.ABC):
+    """Abstract base class for PennyLane devices.
+
+    Args:
+        wires (int): number of subsystems in the quantum state represented by the device.
+            Default 1 if not specified.
+        shots (int): Number of circuit evaluations/random samples used to estimate
+            expectation values of observables. Defaults to 1000 if not specified.
+    """
+    #pylint: disable=too-many-public-methods
+    _capabilities = {} #: dict[str->*]: plugin capabilities
+    _circuits = {}     #: dict[str->Circuit]: circuit templates associated with this API class
+
+    def __init__(self, wires=1, shots=1000):
+        self.num_wires = wires
+        self._shots = 1000
+        self.shots = shots
+
+        self._op_queue = None
+        self._obs_queue = None
+        self._parameters = None
+
+    def __repr__(self):
+        """String representation."""
+        return "{}.\nInstance: ".format(self.__module__, self.__class__.__name__, self.name)
+
+    def __str__(self):
+        """Verbose string representation."""
+        return "{}\nName: \nAPI version: \nPlugin version: \nAuthor: ".format(self.name, self.pennylane_requires, self.version, self.author)
+
+    @abc.abstractproperty
+    def name(self):
+        """The full name of the device."""
+        raise NotImplementedError
+
+    @abc.abstractproperty
+    def short_name(self):
+        """Returns the string used to load the device."""
+        raise NotImplementedError
+
+    @abc.abstractproperty
+    def pennylane_requires(self):
+        """The current API version that the device plugin was made for."""
+        raise NotImplementedError
+
+    @abc.abstractproperty
+    def version(self):
+        """The current version of the plugin."""
+        raise NotImplementedError
+
+    @abc.abstractproperty
+    def author(self):
+        """The author(s) of the plugin."""
+        raise NotImplementedError
+
+    @abc.abstractproperty
+    def operations(self):
+        """Get the supported set of operations.
+
+        Returns:
+            set[str]: the set of PennyLane operation names the device supports
+        """
+        raise NotImplementedError
+
+    @abc.abstractproperty
+    def observables(self):
+        """Get the supported set of observables.
+
+        Returns:
+            set[str]: the set of PennyLane observable names the device supports
+        """
+        raise NotImplementedError
+
+    @property
+    def shots(self):
+        """Number of circuit evaluations/random samples used to estimate
+        expectation values of observables"""
+
+        return self._shots
+
+    @shots.setter
+    def shots(self, shots):
+        """Changes the number of shots.
+
+        Args:
+            shots (int): number of circuit evaluations/random samples used to estimate
+                expectation values of observables
+        """
+        if shots < 1:
+            raise DeviceError("The specified number of shots needs to be at least 1. Got {}.".format(shots))
+
+        self._shots = int(shots)
+
+    @classmethod
+    def capabilities(cls):
+        """Get the other capabilities of the plugin.
+
+        Measurements, batching etc.
+
+        Returns:
+            dict[str->*]: results
+        """
+        return cls._capabilities
+
+    def execute(self, queue, observables, parameters={}):
+        """Execute a queue of quantum operations on the device and then measure the given observables.
+
+        For plugin developers: Instead of overwriting this, consider implementing a suitable subset of
+        :meth:`pre_apply`, :meth:`apply`, :meth:`post_apply`, :meth:`pre_measure`,
+        :meth:`expval`, :meth:`var`, :meth:`sample`, :meth:`post_measure`, and :meth:`execution_context`.
+
+        Args:
+            queue (Iterable[~.operation.Operation]): operations to execute on the device
+            observables (Iterable[~.operation.Observable]): observables to measure and return
+            parameters (dict[int->list[(int, int)]]): Mapping from free parameter index to the list of
+                :class:`Operations <pennylane.operation.Operation>` (in the queue) that depend on it.
+                The first element of the tuple is the index of the Operation in the program queue,
+                the second the index of the parameter within the Operation.
+
+        Returns:
+            array[float]: measured value(s)
+        """
+        self.check_validity(queue, observables)
+        self._op_queue = queue
+        self._obs_queue = observables
+        self._parameters = {}
+        self._parameters.update(parameters)
+
+        results = []
+
+        with self.execution_context():
+            self.pre_apply()
+
+            for operation in queue:
+                self.apply(operation.name, operation.wires, operation.parameters)
+
+            self.post_apply()
+
+            self.pre_measure()
+
+            for obs in observables:
+                if obs.return_type is Expectation:
+                    results.append(self.expval(obs.name, obs.wires, obs.parameters))
+
+                elif obs.return_type is Variance:
+                    results.append(self.var(obs.name, obs.wires, obs.parameters))
+
+                elif obs.return_type is Sample:
+                    results.append(np.array(self.sample(obs.name, obs.wires, obs.parameters)))
+
+                elif obs.return_type is not None:
+                    raise QuantumFunctionError("Unsupported return type specified for observable {}".format(obs.name))
+
+            self.post_measure()
+
+            self._op_queue = None
+            self._obs_queue = None
+            self._parameters = None
+
+            # Ensures that a combination with sample does not put
+            # expvals and vars in superfluous arrays
+            if all(obs.return_type is Sample for obs in observables):
+                return np.asarray(results)
+            if any(obs.return_type is Sample for obs in observables):
+                return np.asarray(results, dtype="object")
+
+            return np.asarray(results)
+
+    @property
+    def op_queue(self):
+        """The operation queue to be applied.
+
+        Note that this property can only be accessed within the execution context
+        of :meth:`~.execute`.
+
+        Returns:
+            list[~.operation.Operation]
+        """
+        if self._op_queue is None:
+            raise ValueError("Cannot access the operation queue outside of the execution context!")
+
+        return self._op_queue
+
+    @property
+    def obs_queue(self):
+        """The observables to be measured and returned.
+
+        Note that this property can only be accessed within the execution context
+        of :meth:`~.execute`.
+
+        Returns:
+            list[~.operation.Observable]
+        """
+        if self._obs_queue is None:
+            raise ValueError("Cannot access the observable value queue outside of the execution context!")
+
+        return self._obs_queue
+
+    @property
+    def parameters(self):
+        """Mapping from free parameter index to the list of
+        :class:`Operations <~.Operation>` in the device queue that depend on it.
+
+        Note that this property can only be accessed within the execution context
+        of :meth:`~.execute`.
+
+        Returns:
+            dict[int->list[(int, int)]]: the first element of the tuple is the index
+            of the Operation in the program queue, the second the index of the parameter
+            within the Operation.
+        """
+        if self._parameters is None:
+            raise ValueError("Cannot access the free parameter mapping outside of the execution context!")
+
+        return self._parameters
+
+    def pre_apply(self):
+        """Called during :meth:`execute` before the individual operations are executed."""
+        pass
+
+    def post_apply(self):
+        """Called during :meth:`execute` after the individual operations have been executed."""
+        pass
+
+    def pre_measure(self):
+        """Called during :meth:`execute` before the individual observables are measured."""
+        pass
+
+    def post_measure(self):
+        """Called during :meth:`execute` after the individual observables have been measured."""
+        pass
+
+    def execution_context(self):
+        """The device execution context used during calls to :meth:`execute`.
+
+        You can overwrite this function to return a context manager in case your
+        quantum library requires that;
+        all operations and method calls (including :meth:`apply` and :meth:`expval`)
+        are then evaluated within the context of this context manager (see the
+        source of :meth:`.Device.execute` for more details).
+        """
+        # pylint: disable=no-self-use
+        class MockContext: # pylint: disable=too-few-public-methods
+            """Mock class as a default for the with statement in execute()."""
+            def __enter__(self):
+                pass
+            def __exit__(self, type, value, traceback):
+                pass
+
+        return MockContext()
+
+    def supports_operation(self, operation):
+        """Checks if an operation is supported by this device.
+
+        Args:
+            operation (Operation,str): operation to be checked
+
+        Returns:
+            bool: ``True`` iff supplied operation is supported
+        """
+        if isinstance(operation, type) and issubclass(operation, Operation):
+            return operation.__name__ in self.operations
+        if isinstance(operation, str):
+            return operation in self.operations
+
+        raise ValueError("The given operation must either be a pennylane.Operation class or a string.")
+
+    def supports_observable(self, observable):
+        """Checks if an observable is supported by this device.
+
+        Args:
+            operation (Observable,str): observable to be checked
+
+        Returns:
+            bool: ``True`` iff supplied observable is supported
+        """
+        if isinstance(observable, type) and issubclass(observable, Observable):
+            return observable.__name__ in self.observables
+        if isinstance(observable, str):
+            return observable in self.observables
+
+        raise ValueError("The given operation must either be a pennylane.Observable class or a string.")
+
+
+    def check_validity(self, queue, observables):
+        """Checks whether the operations and observables in queue are all supported by the device.
+
+        Args:
+            queue (Iterable[~.operation.Operation]): quantum operation objects which are intended
+                to be applied on the device
+            observables (Iterable[~.operation.Observable]): observables which are intended
+                to be evaluated on the device
+        """
+        for o in queue:
+            if o.name not in self.operations:
+                raise DeviceError("Gate {} not supported on device {}".format(o.name, self.short_name))
+
+        for o in observables:
+
+            if isinstance(o, Tensor):
+                if "tensor_observables" not in self.capabilities() or not self.capabilities()["tensor_observables"]:
+                    raise DeviceError("Tensor observables not supported on device {}".format(self.short_name))
+
+                for i in o.obs:
+                    if i.name not in self.observables:
+                        raise DeviceError("Observable {} not supported on device {}".format(i.name, self.short_name))
+            else:
+                if o.name not in self.observables:
+                    raise DeviceError("Observable {} not supported on device {}".format(o.name, self.short_name))
+
+    @abc.abstractmethod
+    def apply(self, operation, wires, par):
+        """Apply a quantum operation.
+
+        For plugin developers: this function should apply the operation on the device.
+
+        Args:
+            operation (str): name of the operation
+            wires (Sequence[int]): subsystems the operation is applied on
+            par (tuple): parameters for the operation
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def expval(self, observable, wires, par):
+        r"""Returns the expectation value of observable on specified wires.
+
+        Note: all arguments accept _lists_, which indicate a tensor
+        product of observables.
+
+        Args:
+            observable (str or list[str]): name of the observable(s)
+            wires (List[int] or List[List[int]]): subsystems the observable(s) is to be measured on
+            par (tuple or list[tuple]]): parameters for the observable(s)
+
+        Returns:
+            float: expectation value :math:`\expect{A} = \bra{\psi}A\ket{\psi}`
+        """
+        raise NotImplementedError
+
+    def var(self, observable, wires, par):
+        r"""Returns the variance of observable on specified wires.
+
+        Note: all arguments support _lists_, which indicate a tensor
+        product of observables.
+
+        Args:
+            observable (str or list[str]): name of the observable(s)
+            wires (List[int] or List[List[int]]): subsystems the observable(s) is to be measured on
+            par (tuple or list[tuple]]): parameters for the observable(s)
+
+        Returns:
+            float: variance :math:`\mathrm{var}(A) = \bra{\psi}A^2\ket{\psi} - \bra{\psi}A\ket{\psi}^2`
+        """
+        raise NotImplementedError("Returning variances from QNodes not currently supported by {}".format(self.short_name))
+
+    def sample(self, observable, wires, par):
+        """Return a sample of an observable.
+
+        The number of samples is determined by the value of ``Device.shots``,
+        which can be directly modified.
+
+        Note: all arguments support _lists_, which indicate a tensor
+        product of observables.
+
+        Args:
+            observable (str or list[str]): name of the observable(s)
+            wires (List[int] or List[List[int]]): subsystems the observable(s) is to be measured on
+            par (tuple or list[tuple]]): parameters for the observable(s)
+
+        Returns:
+            array[float]: samples in an array of dimension ``(n, num_wires)``
+        """
+        raise NotImplementedError("Returning samples from QNodes not currently supported by {}".format(self.short_name))
+
+    def probability(self):
+        """Return the full state probability of each computational basis state from the last run of the device.
+
+        Returns:
+            OrderedDict[tuple, float]: Dictionary mapping a tuple representing the state
+            to the resulting probability. The dictionary should be sorted such that the
+            state tuples are in lexicographical order.
+        """
+        raise NotImplementedError("Returning probability not currently supported by {}".format(self.short_name))
+
+    @abc.abstractmethod
+    def reset(self):
+        """Reset the backend state.
+
+        After the reset the backend should be as if it was just constructed.
+        Most importantly the quantum state is reset to its initial value.
+        """
+        raise NotImplementedError
---
+++ packages/PennyLane/PennyLane-0.6.0-py3-none-any.whl/pennylane/templates/embeddings.py
@@ -0,0 +1,225 @@
+# Copyright 2018 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+r"""
+This module provides quantum circuit architectures that can embed features into a quantum state.
+"""
+#pylint: disable-msg=too-many-branches,too-many-arguments,protected-access
+from collections.abc import Iterable
+from pennylane.ops import RX, RY, RZ, BasisState, Squeezing, Displacement, QubitStateVector
+import numpy as np
+
+
+def AngleEmbedding(features, wires, rotation='X'):
+    r"""
+    Encodes :math:`N` features into the rotation angles of :math:`n` qubits, where :math:`N \leq n`.
+
+    The rotations can be chosen as either :class:`~pennylane.ops.RX`, :class:`~pennylane.ops.RY`
+    or :class:`~pennylane.ops.RZ` gates, as defined by the ``rotation`` parameter:
+
+    * ``rotation='X'`` uses the features as angles of RX rotations
+
+    * ``rotation='Y'`` uses the features as angles of RY rotations
+
+    * ``rotation='Z'`` uses the features as angles of RZ rotations
+
+    The length of ``features`` has to be smaller or equal to the number of qubits. If there are fewer entries in
+    ``features`` than rotations, the circuit does not apply the remaining rotation gates.
+
+    This embedding method can also be used to encode a binary sequence into a basis state. For example, to prepare
+    basis state :math:`|0,1,1,0\rangle`, choose ``rotation='X'`` and use the
+    feature vector :math:`[0, \pi/2, \pi/2, 0]`. Alternatively, one can use the :mod:`BasisEmbedding()` template.
+
+    Args:
+        features (array): Input array of shape ``(N,)``, where N is the number of input features to embed,
+            with :math:`N\leq n`
+        wires (Sequence[int]): sequence of qubit indices that the template acts on
+
+    Keyword Args:
+        rotation (str): Type of rotations used
+    """
+
+    if not isinstance(wires, Iterable):
+        raise ValueError("Wires needs to be a list of wires that the embedding uses; got {}.".format(wires))
+
+    if len(features) > len(wires):
+        raise ValueError("Number of features to embed cannot be larger than number of wires, which is {}; "
+                         "got {}.".format(len(wires), len(features)))
+    if rotation == 'X':
+        for f, w in zip(features, wires):
+            RX(f, wires=w)
+
+    elif rotation == 'Y':
+        for f, w in zip(features, wires):
+            RY(f, wires=w)
+
+    elif rotation == 'Z':
+        for f, w in zip(features, wires):
+            RZ(f, wires=w)
+
+    else:
+        raise ValueError("Rotation has to be `X`, `Y` or `Z`; got {}.".format(rotation))
+
+
+def AmplitudeEmbedding(features, wires, pad=False, normalize=False):
+    r"""Encodes :math:`2^n` features into the amplitude vector of :math:`n` qubits.
+
+    If the total number of features to embed are less than the :math:`2^n` available amplitudes,
+    non-informative constants (zeros) can be padded to ``features``. To enable this, the argument
+    ``pad`` should be set to ``True``.
+
+    The L2-norm of ``features`` must be one. By default, AmplitudeEmbedding expects a normalized
+    feature vector. The argument ``normalize`` can be set to ``True`` to automatically normalize it.
+
+    .. note::
+
+        AmplitudeEmbedding uses PennyLane's :class:`~pennylane.ops.QubitStateVector` and only works in conjunction with
+        devices that implement this function.
+
+    Args:
+        features (array): input array of shape ``(2**n,)``
+        wires (Sequence[int]): sequence of qubit indices that the template acts on
+        pad (Boolean): controls the activation of the padding option
+        normalize (Boolean): controls the activation of automatic normalization
+    """
+
+    if not isinstance(wires, Iterable):
+        raise ValueError("Wires needs to be a list of wires that the embedding uses; got {}.".format(wires))
+
+    n_features = len(features)
+    n_amplitudes = 2**len(wires)
+
+    if n_amplitudes < n_features:
+        raise ValueError("AmplitudeEmbedding requires the size of feature vector to be smaller than or equal to 2**len(wires), which is {}; "
+                         "got {}.".format(n_amplitudes, n_features))
+
+    if pad and n_amplitudes >= n_features:
+        features = np.pad(features, (0, n_amplitudes-n_features), 'constant')
+
+    if  not pad and n_amplitudes != n_features:
+        raise ValueError("AmplitudeEmbedding with no padding requires a feature vector of size 2**len(wires), which is {}; "
+                         "got {}.".format(n_amplitudes, n_features))
+
+    if normalize and np.linalg.norm(features, 2) != 1:
+        features = features * (1/np.linalg.norm(features, 2))
+
+    if not normalize and np.linalg.norm(features, 2) != 1:
+        raise ValueError("AmplitudeEmbedding requires a normalized feature vector. The argument normalize can be set to True to automatically normalize it.")
+
+    QubitStateVector(features, wires=wires)
+
+
+def BasisEmbedding(features, wires):
+    r"""Encodes :math:`n` binary features into a basis state of :math:`n` qubits.
+
+    For example, for ``features=[0, 1, 0]``, the quantum system will be prepared in state :math:`|010 \rangle`.
+
+    .. note::
+
+        BasisEmbedding uses PennyLane's :class:`~pennylane.ops.BasisState` and only works in conjunction with
+        devices that implement this function.
+
+    Args:
+        features (array): Binary input array of shape ``(n, )``
+        wires (Sequence[int]): sequence of qubit indices that the template acts on
+    """
+    if not isinstance(wires, Iterable):
+        raise ValueError("Wires needs to be a list of wires that the embedding uses; got {}.".format(wires))
+
+    if len(features) > len(wires):
+        raise ValueError("Number of bits to embed cannot be larger than number of wires, which is {}; "
+                         "got {}.".format(len(wires), len(features)))
+    BasisState(features, wires=wires)
+
+
+def SqueezingEmbedding(features, wires, method='amplitude', c=0.1):
+    r"""Encodes :math:`N` features into the squeezing amplitudes :math:`r \geq 0` or phases :math:`\phi \in [0, 2\pi)`
+    of :math:`M` modes, where :math:`N\leq M`.
+
+    The mathematical definition of the squeezing gate is given by the operator
+
+    .. math::
+
+        S(z) = \exp\left(\frac{r}{2}\left(e^{-i\phi}\a^2 -e^{i\phi}{\ad}^{2} \right) \right),
+
+    where :math:`\a` and :math:`\ad` are the bosonic creation and annihilation operators.
+
+    ``features`` has to be an array of at most ``len(wires)`` floats. If there are fewer entries in
+    ``features`` than wires, the circuit does not apply the remaining squeezing gates.
+
+    Args:
+        features (array): Array of features of size (N,)
+        wires (Sequence[int]): sequence of mode indices that the template acts on
+
+    Keyword Args:
+        method (str): ``'phase'`` encodes the input into the phase of single-mode squeezing, while
+            ``'amplitude'`` uses the amplitude
+        c (float): value of the phase of all squeezing gates if ``execution='amplitude'``, or the
+            amplitude of all squeezing gates if ``execution='phase'``
+    """
+
+    if not isinstance(wires, Iterable):
+        raise ValueError("Wires needs to be a list of wires that the embedding uses; got {}.".format(wires))
+
+    if len(wires) < len(features):
+        raise ValueError("Number of features to embed cannot be larger than number of wires, which is {}; "
+                         "got {}.".format(len(wires), len(features)))
+
+    for idx, f in enumerate(features):
+        if method == 'amplitude':
+            Squeezing(f, c, wires=wires[idx])
+        elif method == 'phase':
+            Squeezing(c, f, wires=wires[idx])
+        else:
+            raise ValueError("Execution method '{}' not known. Has to be 'phase' or 'amplitude'.".format(method))
+
+
+def DisplacementEmbedding(features, wires, method='amplitude', c=0.1):
+    r"""Encodes :math:`N` features into the displacement amplitudes :math:`r` or phases :math:`\phi` of :math:`M` modes,
+     where :math:`N\leq M`.
+
+    The mathematical definition of the displacement gate is given by the operator
+
+    .. math::
+            D(\alpha) = \exp(r (e^{i\phi}\ad -e^{-i\phi}\a)),
+
+    where :math:`\a` and :math:`\ad` are the bosonic creation and annihilation operators.
+
+    ``features`` has to be an array of at most ``len(wires)`` floats. If there are fewer entries in
+    ``features`` than wires, the circuit does not apply the remaining displacement gates.
+
+    Args:
+        features (array): Array of features of size (N,)
+        wires (Sequence[int]): sequence of mode indices that the template acts on
+
+    Keyword Args:
+        method (str): ``'phase'`` encodes the input into the phase of single-mode displacement, while
+            ``'amplitude'`` uses the amplitude
+        c (float): value of the phase of all displacement gates if ``execution='amplitude'``, or
+            the amplitude of all displacement gates if ``execution='phase'``
+   """
+
+    if not isinstance(wires, Iterable):
+        raise ValueError("Wires needs to be a list of wires that the embedding uses; got {}.".format(wires))
+
+    if len(wires) < len(features):
+        raise ValueError("Number of features to embed cannot be larger than number of wires, which is {}; "
+                         "got {}.".format(len(wires), len(features)))
+
+    for idx, f in enumerate(features):
+        if method == 'amplitude':
+            Displacement(f, c, wires=wires[idx])
+        elif method == 'phase':
+            Displacement(c, f, wires=wires[idx])
+        else:
+            raise ValueError("Execution method '{}' not known. Has to be 'phase' or 'amplitude'.".format(method))
