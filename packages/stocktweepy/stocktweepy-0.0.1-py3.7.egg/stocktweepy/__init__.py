b4e1ec6818aea304aeea78c747feee867adaecb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:36 +0000

    Add package stocktweepy/0.0.1/stocktweepy-0.0.1-py3.7.egg

---
+++ packages/stocktweepy/stocktweepy-0.0.1-py3.7.egg/stocktweepy/__init__.py
@@ -0,0 +1,10 @@
+# StockTweepy
+# Copyright 2019 John Patrick Roach
+# See LICENSE for details.
+
+"""
+StockTweepy StockTwits API library
+"""
+__version__ = '0.0.1'
+__author__ = 'John Patrick Roach'
+__license__ = 'MIT'
