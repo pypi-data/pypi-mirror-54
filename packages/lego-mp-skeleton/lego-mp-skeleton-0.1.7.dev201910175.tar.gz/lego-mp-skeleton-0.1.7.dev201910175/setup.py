16f9369f9af7f8cb91aba7e7765fd6d952f2ed82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:49 +0000

    Add package lego-mp-skeleton/0.1.7.dev201910175/lego-mp-skeleton-0.1.7.dev201910175.tar.gz

---
+++ packages/lego-mp-skeleton/lego-mp-skeleton-0.1.7.dev201910175.tar.gz/lego-mp-skeleton-0.1.7.dev201910175/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+import os
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lego-mp-skeleton",
+    version="0.1.7" + os.environ['PACKAGE_BUILD'],
+    author="Lee Tzilantonis",
+    author_email="LTzilantonis@gmail.com",
+    description="Skeleton for the Lego EV3 MicroPython Port",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires="enum34",
+    url="https://github.com/klutzybubbles/lego-micropython-skeleton",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
