92344983919b0226c4f584b5b40e3cd23527624c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:23 +0000

    Add package uwsgiconf/0.19.0/uwsgiconf-0.19.0.tar.gz

---
+++ packages/uwsgiconf/uwsgiconf-0.19.0.tar.gz/uwsgiconf-0.19.0/uwsgiconf/contrib/django/uwsgify/management/commands/uwsgi_run.py
@@ -0,0 +1,33 @@
+from django.core.management.base import BaseCommand
+
+from ...toolbox import run_uwsgi, SectionMutator
+
+
+class Command(BaseCommand):
+
+    help = 'Runs uWSGI to serve your project'
+
+    def add_arguments(self, parser):
+
+        super(Command, self).add_arguments(parser)
+
+        parser.add_argument(
+            '--nostatic', action='store_false', dest='use_static_handler',
+            help='Tells uWSGI to NOT to serve static and media files.',
+        )
+        parser.add_argument(
+            '--compile', action='store_true', dest='compile',
+            help='Do not run just print out compiled uWSGI .ini configuration.',
+        )
+        parser.add_argument(
+            '--embedded', action='store_true', dest='embedded',
+            help='Do not create temporary config files and try to use resource files for configuration',
+        )
+
+    def handle(self, *args, **options):
+        mutator = SectionMutator.spawn(options=options)
+        run_uwsgi(
+            mutator.section,
+            compile_only=options['compile'],
+            embedded=options['embedded'],
+        )
