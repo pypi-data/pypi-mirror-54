f7ad830dccd5c073a3944ae218439d31f28a10ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:12 +0000

    Add package pw-generator/0.1.0/pw_generator-0.1.0-py3-none-any.whl

---
+++ packages/pw-generator/pw_generator-0.1.0-py3-none-any.whl/pwgen/charlists.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+#----------------------------------------------------------------------#
+charsets = ("123456789",
+    "ABCDEFGHJKLMNPQRSTUVWXYZ",
+    "abcdefghijkmnopqrstuvwxyz")
+
+separators = "-_!$&*+="
+
+#----------------------------------------------------------------------#
+if __name__ == '__main__':
+    print("Don't use it directly!")
