b2c60572e9b1af4a6b36e4801e8b8d90f5017d6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:10 +0000

    Add package riemann-ether/4.0.1/riemann_ether-4.0.1-py3-none-any.whl

---
+++ packages/riemann-ether/riemann_ether-4.0.1-py3-none-any.whl/ether/ether_types.py
@@ -0,0 +1,80 @@
+import asyncio
+
+from asyncio import Future
+from mypy_extensions import TypedDict
+from typing import Any, Dict, List, Optional, Tuple
+
+EthSig = Tuple[int, int, int]
+
+EthABI = List[Dict[str, Any]]
+
+
+class EthTx(TypedDict):
+    to: str
+    value: int
+    gas: int
+    gasPrice: int
+    nonce: int
+    data: bytes
+
+
+class SignedEthTx(EthTx):
+    v: int
+    s: int
+    r: int
+
+
+class UnsignedEthTx(EthTx):
+    chainId: int
+
+
+class EtherEvent(TypedDict):
+    address: str
+    blockHash: str
+    blockNumber: str
+    logIndex: str
+    removed: bool
+    topics: List[str]
+    transactionHash: str
+    transactionIndex: str
+
+
+class UnparsedEtherEvent(EtherEvent):
+    data: str
+
+
+class ParsedEtherEvent(EtherEvent):
+    data: Dict[str, Any]
+
+
+# we declare it in this style because "from" is a keyword
+Receipt = TypedDict(
+    'Receipt',
+    {
+        'blockHash': str,
+        'blockNumber': str,
+        'contractAddress': Optional[str],
+        'cumulativeGasUsed': str,
+        'from': str,
+        'gasUsed': str,
+        'logs': List[EtherEvent],
+        'logsBloom': str,
+        'status': str,
+        'to': str,
+        'transactionHash': str,
+        'transactionIndex': str
+
+    }
+)
+
+
+class RPCRequest(TypedDict):
+    method: str
+    params: List
+    fut: Future
+
+
+class RPCSubscription(TypedDict):
+    method: str
+    params: List
+    queue: asyncio.Queue
