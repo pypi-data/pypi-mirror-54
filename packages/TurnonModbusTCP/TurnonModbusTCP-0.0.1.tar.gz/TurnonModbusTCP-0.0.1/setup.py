c9bd2389f3ef9f297e6c4a4d03cde423f99c3b6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:07 +0000

    Add package TurnonModbusTCP/0.0.1/TurnonModbusTCP-0.0.1.tar.gz

---
+++ packages/TurnonModbusTCP/TurnonModbusTCP-0.0.1.tar.gz/TurnonModbusTCP-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+	name = "TurnonModbusTCP",
+	version = "0.0.1",
+	description = "Turnon-Tech for Franka Modbus",
+	long_description = "Turnon-Tech for Franka Modbus",
+	license = "MIT",
+
+	author = "Kevin Wang",
+	author_email = "n159951357753159@gmail.com",
+	url="http://pypi.org/user/n159951357753/",
+
+	packages = find_packages(),
+	platforms = "any",
+	install_requires = ["pyModbusTCP"]
+)
