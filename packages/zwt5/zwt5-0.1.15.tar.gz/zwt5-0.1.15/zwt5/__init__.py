3270ca49f03edb0c88f366e1385643f24b130b4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:35 +0000

    Add package zwt5/0.1.15/zwt5-0.1.15.tar.gz

---
+++ packages/zwt5/zwt5-0.1.15.tar.gz/zwt5-0.1.15/zwt5/__init__.py
@@ -0,0 +1,7 @@
+import numpy
+from .add import add
+from .division import division
+from .multiply import multiply
+from .subtract import subtract
+from .adv.sqrt import squareroot
+import pandas as pd
