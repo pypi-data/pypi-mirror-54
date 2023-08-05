5a1cfa28e7a86e2810849ad3548148afb35f2905
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:23 +0000

    Add package airport/0.0.1/airport-0.0.1.tar.gz

---
+++ packages/airport/airport-0.0.1.tar.gz/airport-0.0.1/setup.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+
+from setuptools import setup
+
+
+setup(
+    name='airport',
+    version='0.0.1',
+    author='Meteorix',
+    author_email='lxhustauto@gmail.com',
+    url='https://github.com/Meteorix',
+    description='airport library',
+    zip_safe=False,
+)
