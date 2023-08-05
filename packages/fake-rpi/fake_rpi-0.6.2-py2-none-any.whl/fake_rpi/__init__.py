b39b2cc36a89a0c4ea3d92988984be5c710f3f6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:53 +0000

    Add package fake-rpi/0.6.2/fake_rpi-0.6.2-py2-none-any.whl

---
+++ packages/fake-rpi/fake_rpi-0.6.2-py2-none-any.whl/fake_rpi/__init__.py
@@ -0,0 +1,13 @@
+from __future__ import print_function
+from . import serial
+import fake_rpi.smbus
+import fake_rpi.RPi
+from .Adafruit import LSM303
+from .wrappers import printf
+from .wrappers import toggle_print
+from . import picamera
+
+
+__author__ = 'Kevin Walchko'
+__license__ = 'MIT'
+__version__ = '0.6.2'
