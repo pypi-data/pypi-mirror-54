4068a4ad48a1fce0257d0fe69d33b6163d428df9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:45 +0000

    Add package tp-py-common/1.0.43/tp_py_common-1.0.43-py3-none-any.whl

---
+++ packages/tp-py-common/tp_py_common-1.0.43-py3-none-any.whl/tp/test/fixtures/db/seeds_test/Admin.86b1.sql
@@ -0,0 +1,3 @@
+INSERT INTO `admins` (`id`, `email`, `key`, `created_at`, `updated_at`, `password_digest`, `customer_id`, `location_id`, `first_name`, `last_name`, `pioneer_id`, `url_token`) 
+VALUES 
+('1','ben@truepill.co','5c104ce80745ca746ccb66e05392e9f5cc5af5d7','2017-11-17 02:37:51','2019-08-09 06:00:12','$2a$10$rGx6QuspUtWJqR.1kN.tY.r.YAraj2yx0aCVvnG1oPasyD7GAIdZu','3',NULL,'Ben ','Chang',NULL,'Gmawlfpp')
