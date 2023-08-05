d20d177668bcd609b8602ca972ca4d26fc84a95a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:20 +0000

    Add package falcontyping/0.2.6/falcontyping-0.2.6.tar.gz

---
+++ packages/falcontyping/falcontyping-0.2.6.tar.gz/falcontyping-0.2.6/setup.py
@@ -0,0 +1,31 @@
+"""Setup module."""
+from setuptools import setup
+
+with open('README.md', 'r', encoding='UTF-8') as file:
+    README = file.read()
+
+setup(
+    name='falcontyping',
+    version='0.2.6',
+    url='https://github.com/abdelrahman-t/falcontyping',
+    author='Abdurrahman Talaat <abdurrahman.talaat@gmail.com>',
+    author_email='abdurrahman.talaat@gmail.com',
+    packages=['falcontyping', 'falcontyping.base', 'falcontyping.middleware', 'falcontyping.typedjson'],
+    include_package_data=True,
+    python_requires='>=3.7.0',
+    install_requires=['falcon'],
+
+    description='Add type hints support to Falcon with Pydantic and Marshmallow integration',
+    long_description=README,
+    long_description_content_type='text/markdown',
+
+    keywords=['falcon', 'typing', 'hints', 'mypy', 'pydantic', 'marshmallow'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3.7',
+    ],
+
+    project_urls={
+        'Documentation': 'https://github.com/abdelrahman-t/falcontyping'
+    },
+)
