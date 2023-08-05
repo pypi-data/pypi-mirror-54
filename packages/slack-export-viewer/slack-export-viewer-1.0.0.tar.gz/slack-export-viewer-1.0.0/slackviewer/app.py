c4ea58dc99fbf6dc3734f3939942d6cd4e73091a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:05 +0000

    Add package slack-export-viewer/1.0.0/slack-export-viewer-1.0.0.tar.gz

---
+++ packages/slack-export-viewer/slack-export-viewer-1.0.0.tar.gz/slack-export-viewer-1.0.0/slackviewer/app.py
@@ -0,0 +1,89 @@
+import flask
+
+
+app = flask.Flask(
+    __name__,
+    template_folder="templates",
+    static_folder="static"
+)
+
+
+@app.route("/channel/<name>/")
+def channel_name(name):
+    messages = flask._app_ctx_stack.channels[name]
+    channels = list(flask._app_ctx_stack.channels.keys())
+    groups = list(flask._app_ctx_stack.groups.keys()) if flask._app_ctx_stack.groups else {}
+    dm_users = list(flask._app_ctx_stack.dm_users)
+    mpim_users = list(flask._app_ctx_stack.mpim_users)
+
+    return flask.render_template("viewer.html", messages=messages,
+                                 name=name.format(name=name),
+                                 channels=sorted(channels),
+                                 groups=sorted(groups) if groups else {},
+                                 dm_users=dm_users,
+                                 mpim_users=mpim_users,
+                                 no_sidebar=app.no_sidebar,
+                                 no_external_references=app.no_external_references)
+
+
+@app.route("/group/<name>/")
+def group_name(name):
+    messages = flask._app_ctx_stack.groups[name]
+    channels = list(flask._app_ctx_stack.channels.keys())
+    groups = list(flask._app_ctx_stack.groups.keys())
+    dm_users = list(flask._app_ctx_stack.dm_users)
+    mpim_users = list(flask._app_ctx_stack.mpim_users)
+
+    return flask.render_template("viewer.html", messages=messages,
+                                 name=name.format(name=name),
+                                 channels=sorted(channels),
+                                 groups=sorted(groups),
+                                 dm_users=dm_users,
+                                 mpim_users=mpim_users,
+                                 no_sidebar=app.no_sidebar,
+                                 no_external_references=app.no_external_references)
+
+
+@app.route("/dm/<id>/")
+def dm_id(id):
+    messages = flask._app_ctx_stack.dms[id]
+    channels = list(flask._app_ctx_stack.channels.keys())
+    groups = list(flask._app_ctx_stack.groups.keys())
+    dm_users = list(flask._app_ctx_stack.dm_users)
+    mpim_users = list(flask._app_ctx_stack.mpim_users)
+
+    return flask.render_template("viewer.html", messages=messages,
+                                 id=id.format(id=id),
+                                 channels=sorted(channels),
+                                 groups=sorted(groups),
+                                 dm_users=dm_users,
+                                 mpim_users=mpim_users,
+                                 no_sidebar=app.no_sidebar,
+                                 no_external_references=app.no_external_references)
+
+
+@app.route("/mpim/<name>/")
+def mpim_name(name):
+    messages = flask._app_ctx_stack.mpims[name]
+    channels = list(flask._app_ctx_stack.channels.keys())
+    groups = list(flask._app_ctx_stack.groups.keys())
+    dm_users = list(flask._app_ctx_stack.dm_users)
+    mpim_users = list(flask._app_ctx_stack.mpim_users)
+
+    return flask.render_template("viewer.html", messages=messages,
+                                 name=name.format(name=name),
+                                 channels=sorted(channels),
+                                 groups=sorted(groups),
+                                 dm_users=dm_users,
+                                 mpim_users=mpim_users,
+                                 no_sidebar=app.no_sidebar,
+                                 no_external_references=app.no_external_references)
+
+
+@app.route("/")
+def index():
+    channels = list(flask._app_ctx_stack.channels.keys())
+    if "general" in channels:
+        return channel_name("general")
+    else:
+        return channel_name(channels[0])
