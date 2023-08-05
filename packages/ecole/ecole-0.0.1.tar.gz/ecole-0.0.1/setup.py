f1e9a260645af89af7724b880d2a13e6e484a1d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:08 +0000

    Add package ecole/0.0.1/ecole-0.0.1.tar.gz

---
+++ packages/ecole/ecole-0.0.1.tar.gz/ecole-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="ecole",
+    version="0.0.1",
+    author="Antoine Prouvost",
+    license="No License",
+    url="https://github.com/ds4dm/ecole",
+    description="Extensible Combinatorial Optimization Learning Environments",
+    long_descritpion=(
+        "This package does not do anything yet but we intend on making good "
+        "use of the `ecole` name."
+    ),
+)
