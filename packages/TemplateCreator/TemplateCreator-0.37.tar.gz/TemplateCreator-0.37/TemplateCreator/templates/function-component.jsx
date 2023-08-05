ba70d4c478c7e8e0ad4303b412044c4f6d92ecb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:09 +0000

    Add package TemplateCreator/0.37/TemplateCreator-0.37.tar.gz

---
+++ packages/TemplateCreator/TemplateCreator-0.37.tar.gz/TemplateCreator-0.37/TemplateCreator/templates/function-component.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyledContainer } from './{{ folder_name }}.styles';
+import PropTypes from 'prop-types';
+
+const {{ component_name }} = (props) => {
+  return (
+    <StyledContainer>
+      {{ component_name }}
+    </StyledContainer>
+  );
+};
+
+{{ component_name }}.propTypes = {
+
+};
+
+{{ component_name }}.defaultProps = {
+
+};
+
+export default {{ component_name }};
