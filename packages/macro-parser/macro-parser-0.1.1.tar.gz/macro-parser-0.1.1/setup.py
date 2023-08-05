474703cfd47f9f1c4d652b84481c6a21967f2717
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:13 +0000

    Add package macro-parser/0.1.1/macro-parser-0.1.1.tar.gz

---
+++ packages/macro-parser/macro-parser-0.1.1.tar.gz/macro-parser-0.1.1/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+'''Set up package Iydon.
+'''
+import setuptools
+
+
+with open("README.md", "r") as f:
+	long_description = f.read()
+
+setuptools.setup(
+	name="macro-parser",
+	version="0.1.1",
+	author="Iydon Liang",
+	author_email="11711217@mail.sustech.edu.cn",
+	license='MIT License',
+	description="Macro parser",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/Iydon/macro-parser",
+	packages=setuptools.find_packages(),
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+	install_requires=[],
+	tests_require=[
+		'pytest',
+	],
+)
