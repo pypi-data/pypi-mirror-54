53483166c1dc2ad2017dbae5987f822dfc7c01bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:24 +0000

    Add package uedge/7.0.8.4rc1/uedge-7.0.8.4rc1.tar.gz

---
+++ packages/uedge/uedge-7.0.8.4rc1.tar.gz/uedge-7.0.8.4rc1/sptodp.h
@@ -0,0 +1,42 @@
+
+#if WORDSIZE == 32
+#define lsode dlsode
+#define b22in db22in
+#define bs2dr dbs2dr
+#define bs2vl dbs2vl
+#define bs2in dbs2in
+#define bs3in dbs3in
+#define bs3vl dbs3vl
+#define bsnak dbsnak
+#define isamax idamax_u
+#define sasum dasum_u
+#define saxpy daxpy_u
+#define scopy dcopy_u
+#define sdot ddot_u
+#define sgbco dgbco
+#define sgbfa dgbfa
+#define sgbsl dgbsl
+#define sgefa dgefa
+#define sgesl dgesl
+#define snrm2 dnrm2_u
+#define sscal dscal_u
+#define ssum dsum
+#define sswap dswap_u
+#define ISAMAX idamax_u
+#define SASUM dasum_u
+#define SAXPY daxpy_u
+#define SCOPY dcopy_u
+#define SDOT ddot_u
+#define SSCAL dscal_u
+#define SSWAP dswap_u
+#define SNRM2 dnrm2_u
+#define SSUM dsum
+#define SGBFA dgbfa
+#define SGBSL dgbsl
+#define SGEFA dgefa
+#define SGESL dgesl
+#define EWSET dewset
+#define XERRWV dxerrwv
+#define dfloat real
+#define float real
+#endif
