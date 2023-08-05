6d7b7c22fe639f99cc475584a86ad6299b332c25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:59 +0000

    Add package handright/5.2.0/handright-5.2.0.tar.gz

---
+++ packages/handright/handright-5.2.0.tar.gz/handright-5.2.0/handright/__init__.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+"""A lightweight Python library for simulating Chinese handwriting
+
+Vision: Reveal the nature of Chinese handwriting and use it to implement
+beautiful, simple and easy-to-use interfaces.
+
+Algorithm: Randomly perturb each character as a whole in horizontal position,
+vertical position and font size. Then, Randomly perturb each stroke of a
+character in horizontal position, vertical position and rotation angle.
+"""
+from handright._core import (
+    handwrite, Template, Error, LayoutError, BackgroundTooLargeError
+)
+
+__version__ = "5.2.0"
