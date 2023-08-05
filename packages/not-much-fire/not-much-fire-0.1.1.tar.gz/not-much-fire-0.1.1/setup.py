2e8b4d9026a4aab7c32befd1addf7719fd6447ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:46 +0000

    Add package not-much-fire/0.1.1/not-much-fire-0.1.1.tar.gz

---
+++ packages/not-much-fire/not-much-fire-0.1.1.tar.gz/not-much-fire-0.1.1/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['not_much_fire']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['appdirs>=1.4,<2.0',
+ 'click>=7.0,<8.0',
+ 'dbus-python>=1.2,<2.0',
+ 'notify2>=0.3.1,<0.4.0',
+ 'notmuch>=0.29.1,<0.30.0']
+
+entry_points = \
+{'console_scripts': ['not-much-fire = not_much_fire.cli:main']}
+
+setup_kwargs = {
+    'name': 'not-much-fire',
+    'version': '0.1.1',
+    'description': 'A simple Notmuch notification tool',
+    'long_description': None,
+    'author': 'Thore Weilbier',
+    'author_email': 'thore@weilbier.net',
+    'url': 'https://github.com/weilbith/not-much-fire',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
