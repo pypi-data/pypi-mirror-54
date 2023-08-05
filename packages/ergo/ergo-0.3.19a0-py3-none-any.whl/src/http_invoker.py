829f015219fb8ecadda8cfb25e83af832c7ce191
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:05 +0000

    Add package ergo/0.3.19a0/ergo-0.3.19a0-py3-none-any.whl

---
+++ packages/ergo/ergo-0.3.19a0-py3-none-any.whl/src/http_invoker.py
@@ -0,0 +1,31 @@
+from abc import ABC, abstractmethod
+
+from src.function_invocable import FunctionInvocable
+
+
+class HttpInvoker(ABC):
+    def __init__(self, invocable: FunctionInvocable) -> None:
+        super().__init__()
+        self._invocable = invocable
+        self._route: str = '/'
+        self._port: int = 80
+
+    @property
+    def route(self) -> str:
+        return self._route
+
+    @route.setter
+    def route(self, arg: str) -> None:
+        self._route = arg
+
+    @property
+    def port(self) -> int:
+        return self._port
+
+    @port.setter
+    def port(self, arg: int) -> None:
+        self._port = arg
+
+    @abstractmethod
+    def start(self) -> int:
+        raise NotImplementedError()
---
+++ packages/ergo/ergo-0.3.19a0-py3-none-any.whl/src/version.py
@@ -0,0 +1,9 @@
+VERSION = '0.3.19-alpha'
+
+
+def get_version() -> str:
+    return VERSION
+
+
+if __name__ == '__main__':
+    print(get_version())
