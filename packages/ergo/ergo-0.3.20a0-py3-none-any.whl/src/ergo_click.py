d01a89777072e35402be15a1d2bdd35c494ba83a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:13 +0000

    Add package ergo/0.3.20a0/ergo-0.3.20a0-py3-none-any.whl

---
+++ packages/ergo/ergo-0.3.20a0-py3-none-any.whl/src/ergo_click.py
@@ -0,0 +1,67 @@
+"""Summary.
+
+Attributes:
+    ERGO_CLI (TYPE): Description
+
+"""
+from typing import Tuple
+
+import click
+from click_default_group import \
+    DefaultGroup  # https://pypi.org/project/click-default-group/
+
+from src.ergo_cli import ErgoCli
+from src.ergo_cmd import ErgoCmd
+
+ERGO_CLI = ErgoCli()
+
+
+@click.group(cls=DefaultGroup, default='shell', default_if_no_args=True)
+def main() -> int:
+    """Summary."""
+
+
+@main.command()
+def shell() -> int:
+    """Summary.
+
+    Returns:
+        int: Description
+
+    """
+    ErgoCmd(ERGO_CLI).cmdloop()
+    return 0
+
+
+@main.command()
+@click.argument('ref', type=click.STRING)
+@click.argument('arg', nargs=-1)
+def run(ref: str, arg: Tuple[str]) -> int:
+    """Summary.
+
+    Args:
+        ref (str): Description
+        arg (Tuple[str]): Description
+
+    Returns:
+        int: Description
+
+    """
+    return ERGO_CLI.run(ref, *list(arg))
+
+
+@main.command()
+@click.argument('ref', type=click.STRING)
+@click.argument('arg', nargs=-1)
+def http(ref: str, arg: Tuple[str]) -> int:
+    """Summary.
+
+    Args:
+        ref (str): Description
+        arg (Tuple[str]): Description
+
+    Returns:
+        int: Description
+
+    """
+    return ERGO_CLI.http(ref, *list(arg))
