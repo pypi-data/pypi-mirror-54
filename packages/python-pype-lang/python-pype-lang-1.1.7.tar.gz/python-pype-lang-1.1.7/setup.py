f382d20fe81ec44fd650128f07fd1e2ee1d2f6f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:03 +0000

    Add package python-pype-lang/1.1.7/python-pype-lang-1.1.7.tar.gz

---
+++ packages/python-pype-lang/python-pype-lang-1.1.7.tar.gz/python-pype-lang-1.1.7/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+#with open("README.md", "r") as fh:
+#    LONG_DESCRIPTION=fh.read()
+
+setup(name='python-pype-lang',
+      version='1.1.7',
+      description='Python-integrated functional programming language - Provides easy-to-use pseudo-macros for common functional programming tasks such as maps, reduces, filters, conditionals, dictionary manipulations, and list manipulations..',
+      #long_description_content_type="text/markdown",
+      #long_description=LONG_DESCRIPTION,
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Operating System :: OS Independent',
+      ],
+      keywords='functional programming map reduce filter lambda conditional',
+      url='https://github.com/bbbdragon/python-pype-lang',
+      author='Bennett Charles Bullock',
+      author_email='bennettbullock36@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+          'numpy',
+          'astpretty',
+          'python-dateutil',
+      ],
+      include_package_data=True,
+      zip_safe=False)
