722b2f7e9fb34ae0da094c533f38d558a6c65eaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:32 +0000

    Add package GridFree/0.1.2.1/GridFree-0.1.2.1.tar.gz

---
+++ packages/GridFree/GridFree-0.1.2.1.tar.gz/GridFree-0.1.2.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup,find_packages
+
+with open('./GridFree/README.md',encoding='utf-8') as f:
+        longtext=f.read()
+
+
+setup(
+        name="GridFree",
+        version="0.1.2.1",
+        keywords=("pip3","python3 -m pip","GridFree","droneimage","yanghu"),
+        description="label plants",
+        #long_description="A pixel-level label plants in drone images.\n Implement Python version= 3.6.5,QGIS version = 3.4 \n *** Need to modify matplotlibrc file *** backend: TkAgg",
+        long_description=longtext,
+        long_description_content_type='text/markdown',
+        license = "MIT Licence",
+
+        url="http://css.wsu.edu/people/research-associates/yang-hu/",
+        author="yangh",
+        author_email="yang.hu@wsu.edu",
+
+        packages=find_packages(),
+        include_package_data=True,
+        platforms="any",
+        install_requires=["pillow","opencv-python","matplotlib","scipy","sklearn","scikit-image","tkinter","rasterio"],
+        python_requirements='>=3',
+        #include_package_data=True,
+        package_data={'GridFree':['*.tif','*.JPG','*.png']}
+        )
