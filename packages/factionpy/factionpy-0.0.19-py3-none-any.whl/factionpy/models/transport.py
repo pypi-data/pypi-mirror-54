182bc6328e64dfa1b132176a32ead7ec33be8269
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:31 +0000

    Add package factionpy/0.0.19/factionpy-0.0.19-py3-none-any.whl

---
+++ packages/factionpy/factionpy-0.0.19-py3-none-any.whl/factionpy/models/transport.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Boolean, Column, DateTime, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
+from factionpy.backend.database import Base
+
+from factionpy.models.agent import Agent
+
+class Transport(Base):
+    __tablename__ = "Transport"
+    Id = Column(Integer, primary_key=True)
+    Name = Column(String)
+    TransportType = Column(String)
+    Guid = Column(String)
+    Created = Column(DateTime)
+    LastCheckin = Column(DateTime)
+    Configuration = Column(String)
+    ApiKeyId = Column(Integer, ForeignKey('ApiKey.Id'), nullable=False)
+    Enabled = Column(Boolean)
+    Visible = Column(Boolean)
+    Agents = relationship('Agent', backref='Transport', lazy=True)
+    Payloads = relationship('Payload', backref='Transport', lazy=True)
+
+    def __repr__(self):
+        return '<Transport: %s>' % str(self.Id)
+
+
