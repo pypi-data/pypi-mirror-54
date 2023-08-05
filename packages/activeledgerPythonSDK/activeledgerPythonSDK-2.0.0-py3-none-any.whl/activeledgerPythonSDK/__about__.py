04e5a2d3ebe2b25bdec010d758f752c3ac318a5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:13:42 +0000

    Add package activeledgerPythonSDK/2.0.0/activeledgerPythonSDK-2.0.0-py3-none-any.whl

---
+++ packages/activeledgerPythonSDK/activeledgerPythonSDK-2.0.0-py3-none-any.whl/activeledgerPythonSDK/__about__.py
@@ -0,0 +1,21 @@
+# This file is dual licensed under the terms of the Apache License, Version
+# 2.0, and the BSD License. See the LICENSE file in the root of this repository
+# for complete details.
+
+from __future__ import absolute_import, division, print_function
+
+__all__ = [
+    "__title__", "__summary__","__version__", "__author__",
+    "__email__", "__license__", "__copyright__",
+]
+
+__title__ = "activeledgerPythonSDK"
+__summary__ = ("activeledgersdk is a package to use full functions of activeledger for Python developers")
+
+__version__ = "v2.0.0"
+
+__author__ = "activeledger"
+__email__ = "infor@activeledger.io"
+
+__license__ = "BSD or Apache License, Version 2.0"
+__copyright__ = "Copyright 2013-2017 {0}".format(__author__)
