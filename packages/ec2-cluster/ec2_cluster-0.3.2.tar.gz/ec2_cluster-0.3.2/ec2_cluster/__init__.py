cfc08f670bb14da3dc48924746e4b895bc29502d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:14 +0000

    Add package ec2-cluster/0.3.2/ec2_cluster-0.3.2.tar.gz

---
+++ packages/ec2-cluster/ec2_cluster-0.3.2.tar.gz/ec2_cluster-0.3.2/ec2_cluster/__init__.py
@@ -0,0 +1,2 @@
+name = "ec2-cluster"
+
---
+++ packages/ec2-cluster/ec2_cluster-0.3.2.tar.gz/ec2_cluster-0.3.2/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+        name="ec2_cluster",
+        version="0.3.2",
+        author="Armand McQueen",
+        author_email="armandmcqueen@gmail.com",
+        description="A tool for launching and running commands on a cluster of EC2 instances",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/armandmcqueen/ec2-cluster",
+        packages=setuptools.find_packages(),
+        include_package_data=True,
+        classifiers=[
+            "Programming Language :: Python :: 3.6"
+        ],
+        install_requires=[
+            'fabric2',
+            'boto3',
+            'pyyaml',
+            'tabulate',
+            'cryptography==2.4.2'
+        ],
+        scripts=['cli/ec3']
+)
