db76d41454a97768eff54a355a6d835a8eacbe0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:06 +0000

    Add package jitterentropy/0.1/jitterentropy-0.1.tar.gz

---
+++ packages/jitterentropy/jitterentropy-0.1.tar.gz/jitterentropy-0.1/README.md
@@ -0,0 +1,8 @@
+# What is this?
+
+Python wrapper for libjitterentropy
+
+# See also
+
+* [jitterentropy docs](http://www.chronox.de/jent.html)
+* [libjitterentropy](https://github.com/smuellerDD/jitterentropy-library)
---
+++ packages/jitterentropy/jitterentropy-0.1.tar.gz/jitterentropy-0.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import Extension
+from setuptools import setup
+
+jitterentropy = Extension(
+    "jitterentropy",
+    include_dirs=["/usr/local/include"],
+    libraries=["jitterentropy"],
+    sources=["jitterentropy.c"],
+)
+
+setup(
+    name="jitterentropy",
+    description="Use libjitterentropy to get random bytes",
+    url="https://github.com/swills/py-jitterentropy",
+    version="0.1",
+    author="Steve Wills",
+    author_email="steve@mouf.net",
+    license="LICENSE",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Utilities",
+        "Operating System :: POSIX :: BSD :: FreeBSD",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    ext_modules=[jitterentropy],
+    install_requires=[],
+)
