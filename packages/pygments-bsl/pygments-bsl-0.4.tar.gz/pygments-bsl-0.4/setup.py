61dfa17d2c2e04f0de9f8496f6c03acd8a272162
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:29 +0000

    Add package pygments-bsl/0.4/pygments-bsl-0.4.tar.gz

---
+++ packages/pygments-bsl/pygments-bsl-0.4.tar.gz/pygments-bsl-0.4/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pygments-bsl',
+    version='0.4',
+    packages=find_packages(),
+    author='Ingvar Vilkman',
+    author_email='zeegin@zeegin.com',
+    url='https://github.com/zeegin/pygments-bsl',
+    license='MIT License',
+    description='Pygments 1C (BSL) lexer',
+    classifiers=[
+        'Environment :: Plugins',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+    ],
+    install_requires=['pygments'],
+    entry_points={
+        'pygments.lexers': [
+            'bsl=pygments_bsl:BSLLexer'
+        ]
+    },
+    package_data={
+        '': ['LICENSE', '*.rst'],
+    }
+)
