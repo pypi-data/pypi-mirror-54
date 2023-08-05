3bef7b48fd9fbd25ea1b791bf087b1a8ea36d0d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:09 +0000

    Add package flagopt/0.0.0/flagopt-0.0.0.tar.gz

---
+++ packages/flagopt/flagopt-0.0.0.tar.gz/flagopt-0.0.0/flagopt/__init__.py
@@ -0,0 +1,155 @@
+import multidict
+
+from .general import *
+
+from . import helpers
+
+
+__all__ = (*general.__all__, 'snip', 'draw', 'trace')
+
+
+def snip(flags, value, parse = parse, strip = strip):
+
+    """
+    Get recursive multidict representation of arguments based on the flags.
+    """
+
+    keys = flags.keys()
+
+    store = multidict.MultiDict()
+
+    for (key, value) in parse(value, *keys):
+
+        value = strip(value)
+
+        if key is None:
+
+            if not value:
+
+                continue
+
+            key = next(iter(keys))
+
+        item = flags[key]
+
+        if isinstance(item, dict):
+
+            value = snip(item, value, parse, strip)
+
+        store.add(key, value)
+
+    return store
+
+
+empty = ' '
+
+
+clause = '()'
+
+
+variable = '[]'
+
+
+def draw(flags, empty = empty, clause = clause, variable = variable):
+
+    """
+    Draw description on how this flags expects.
+    """
+
+    store = []
+
+    for (key, value) in flags.items():
+
+        store.append(key)
+
+        if isinstance(value, dict):
+
+            ends = clause
+
+            value = draw(value)
+
+        else:
+
+            ends = variable
+
+        value = helpers.wrap(ends, value)
+
+        store.append(value)
+
+    return empty.join(store)
+
+
+ignore = '{}' + empty
+
+
+def trace(values, ignore = ignore, clause = clause, variable = variable):
+
+    """
+    Get flags from the string; used from drawing and snipping.
+    """
+
+    flags = {}
+
+    buffer = ''
+
+    level = 0
+
+    for value in values:
+
+        if value in ignore:
+
+            continue
+
+        if value == clause[0]:
+
+            try:
+
+                if not level:
+
+                    key = buffer
+
+                    buffer = ''
+
+                    continue
+
+            finally:
+
+                level += 1
+
+        if value == clause[1]:
+
+            level -= 1
+
+            if not level:
+
+                value = trace(buffer, ignore, clause, variable)
+
+                buffer = ''
+
+                flags[key] = value
+
+                continue
+
+        if not level:
+
+            if value == variable[0]:
+
+                key = buffer
+
+                buffer = ''
+
+                continue
+
+            if value == variable[1]:
+
+                value = buffer
+
+                buffer = ''
+
+                flags[key] = value
+
+                continue
+
+        buffer += value
+
+    return flags
---
+++ packages/flagopt/flagopt-0.0.0.tar.gz/flagopt-0.0.0/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open('README.md') as file:
+
+    readme = file.read()
+
+name = 'flagopt'
+
+version = '0.0.0'
+
+author = 'Exahilosys'
+
+url = f'https://github.com/{author}/{name}'
+
+setuptools.setup(
+    name = name,
+    version = version,
+    url = url,
+    packages = setuptools.find_packages(),
+    license = 'MIT',
+    description = 'Command line based argument parse framework.',
+    long_description = readme,
+    long_description_content_type = 'text/markdown',
+    install_requires = [
+        'multidict'
+    ]
+)
