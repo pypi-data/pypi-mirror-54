4d135dd8798490c9e5aa18e080ac5a3582dfa385
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:28 +0000

    Add package mocker-client/0.0.5dev1/mocker_client-0.0.5dev1.tar.gz

---
+++ packages/mocker-client/mocker_client-0.0.5dev1.tar.gz/mocker_client-0.0.5dev1/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+
+if __name__ == '__main__':
+
+    with open('README.rst', 'r') as f:
+        long_description = f.read()
+
+    setup(
+        name='mocker_client',
+        version='0.0.5dev1',
+        description='client for http mock server https://github.com/vilus/mocker',
+        long_description=long_description,
+        packages=['mocker_client'],
+        author='Shevchenko Vladimir',
+        url='https://github.com/vilus/mocker_client',
+        install_requires=['requests']
+    )
