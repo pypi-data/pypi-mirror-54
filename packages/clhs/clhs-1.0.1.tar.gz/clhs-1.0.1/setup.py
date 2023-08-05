7cae1b003a959d7a7b734f1424a0fd14ae580e93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:18 +0000

    Add package clhs/1.0.1/clhs-1.0.1.tar.gz

---
+++ packages/clhs/clhs-1.0.1.tar.gz/clhs-1.0.1/setup.py
@@ -0,0 +1,36 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+#import clhs
+
+classifiers = [
+    "Programming Language :: Python :: 3",
+    "Development Status :: 2 - Pre-Alpha",
+    "Intended Audience :: Science/Research",
+    "License :: OSI Approved :: MIT License",
+    "Natural Language :: English",
+    "Topic :: Scientific/Engineering",
+    "Topic :: Scientific/Engineering :: Astronomy"
+    ]
+
+with open("README.rst", "r") as fp:
+    long_description = fp.read()
+
+setup(
+    name="clhs",
+    #version=clhs.__version__,
+    #author=clhs.__author__,
+    #author_email=clhs.__email__,
+    version="1.0.1",
+    author="Erika Wagoner",
+    author_email="wagoner47+clhs@email.arizona.edu",
+    url="https://github.com/wagoner47/clhs_py",
+    py_modules=["clhs"],
+    description="Conditioned Latin Hypercube Sampling in Python",
+    long_description=long_description,
+    license="MIT",
+    classifiers=classifiers,
+    install_requires=['tqdm', 'numpy', 'pandas']
+    )
