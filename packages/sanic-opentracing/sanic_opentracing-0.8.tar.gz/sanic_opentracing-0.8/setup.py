9f25a88301f4abe0a9f86ec2573d840887d80e59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:13:59 +0000

    Add package sanic-opentracing/0.8/sanic_opentracing-0.8.tar.gz

---
+++ packages/sanic-opentracing/sanic_opentracing-0.8.tar.gz/sanic_opentracing-0.8/setup.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+import versioneer
+
+version = versioneer.get_version()
+setup(
+    name="sanic_opentracing",
+    cmdclass=versioneer.get_cmdclass(),
+    version=version,
+    url="https://github.com/shady-robot/sanic-opentracing/",
+    download_url="https://github.com/shady-robot/sanic-opentracing/tarball/" + version,
+    license="MIT",
+    author="Shady Robot",
+    author_email="darkercookies@gmail.com",
+    description="OpenTracing support for Sanic applications",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=["sanic_opentracing", "tests"],
+    platforms="any",
+    install_requires=["sanic", "opentracing>=2.0,<3"],
+    extras_require={
+        "tests": [
+            "coverage",
+            "flake8<3",  # see https://github.com/zheller/flake8-quotes/issues/29
+            "flake8-quotes",
+            "aiohttp",
+        ]
+    },
+    classifiers=[
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
