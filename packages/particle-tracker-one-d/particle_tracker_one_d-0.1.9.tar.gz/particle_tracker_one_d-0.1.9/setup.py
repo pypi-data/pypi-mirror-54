129ed62dcb401c4fe40a6a75542fb775297de76e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:14:26 +0000

    Add package particle-tracker-one-d/0.1.9/particle_tracker_one_d-0.1.9.tar.gz

---
+++ packages/particle-tracker-one-d/particle_tracker_one_d-0.1.9.tar.gz/particle_tracker_one_d-0.1.9/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+version = '0.1.9'
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+if __name__ == "__main__":
+
+    setup(name='particle_tracker_one_d',
+          version=version,
+          description='Particle tracking in Kymographs',
+          long_description=long_description,
+          long_description_content_type="text/markdown",
+          url='',
+          author='Johan Tenghamn',
+          author_email='tenghamn@chalmers.se',
+          license='MIT',
+          packages=find_packages(),
+          install_requires=requirements,
+          keywords='particle tracking nanochannel',
+          classifiers=['Development Status :: 4 - Beta', 'Programming Language :: Python :: 3.6', 'Intended Audience :: Science/Research',
+                       'Topic :: Scientific/Engineering :: Physics'],
+          zip_safe=False)
