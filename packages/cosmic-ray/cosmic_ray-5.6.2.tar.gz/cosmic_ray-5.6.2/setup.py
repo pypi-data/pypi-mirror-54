2fc8d22f5981895e6794845c171c06a39fc1fa3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:43 +0000

    Add package cosmic-ray/5.6.2/cosmic_ray-5.6.2.tar.gz

---
+++ packages/cosmic-ray/cosmic_ray-5.6.2.tar.gz/cosmic_ray-5.6.2/setup.py
@@ -0,0 +1,116 @@
+"""Setup for Cosmic Ray.
+"""
+import io
+import os
+
+from setuptools import setup, find_packages
+
+
+def local_file(*name):
+    "Find a file relative to this directory."
+    return os.path.join(os.path.dirname(__file__), *name)
+
+
+def read(name, **kwargs):
+    "Read the contents of a file."
+    with io.open(name, encoding=kwargs.get("encoding", "utf8")) as handle:
+        return handle.read()
+
+
+# This is unfortunately duplicated from scripts/cosmic_ray_tooling.py. I
+# couldn't find a way to use the original version and still have tox
+# work...hmmm...
+def read_version():
+    "Read the `(version-string, version-info)` from `src/cosmic_ray/version.py`."
+    version_file = local_file('src', 'cosmic_ray', 'version.py')
+    local_vars = {}
+    with open(version_file) as handle:
+        exec(handle.read(), {}, local_vars)  # pylint: disable=exec-used
+    return (local_vars['__version__'], local_vars['__version_info__'])
+
+
+LONG_DESCRIPTION = read(local_file('README.rst'), mode='rt')
+
+INSTALL_REQUIRES = [
+    'astunparse',
+    'decorator',
+    'docopt_subcommands>=3.0.0,<4.0.0',
+    'exit_codes',
+    'gitpython',
+    'parso',
+    'pathlib',
+    'qprompt',
+    'spor>=1.1.0',
+    'stevedore',
+    'toml',
+    'virtualenv',
+    'yattag',
+    'anybadge',
+]
+
+version = read_version()[0]
+
+setup(
+    name='cosmic_ray',
+    version=version,
+    packages=find_packages('src'),
+    author='Sixty North AS',
+    author_email='austin@sixty-north.com',
+    description='Mutation testing',
+    license='MIT License',
+    keywords='testing',
+    package_dir={'': 'src'},
+    url='http://github.com/sixty-north/cosmic-ray',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Testing',
+    ],
+    platforms='any',
+    include_package_data=True,
+    install_requires=INSTALL_REQUIRES,
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    extras_require={
+        'test': ['hypothesis', 'pytest', 'pytest-mock', 'tox'],
+        'dev': ['pylint', 'autopep8'],
+        'docs': ['sphinx', 'sphinx_rtd_theme'],
+        'celery4_engine': ['cosmic_ray_celery4_engine'],
+    },
+    entry_points={
+        'console_scripts': [
+            'cosmic-ray = cosmic_ray.cli:main',
+            'cr-html = cosmic_ray.tools.html:report_html',
+            'cr-report = cosmic_ray.tools.report:report',
+            'cr-badge = cosmic_ray.tools.badge:generate_badge',
+            'cr-rate = cosmic_ray.tools.survival_rate:format_survival_rate',
+            'cr-xml = cosmic_ray.tools.xml:report_xml',
+        ],
+        'cosmic_ray.test_runners': [
+            'unittest = cosmic_ray.testing.unittest_runner:UnittestRunner',
+        ],
+        'cosmic_ray.operator_providers': [
+            'core = cosmic_ray.operators.provider:OperatorProvider',
+        ],
+        'cosmic_ray.execution_engines': [
+            'local = cosmic_ray.execution.local:LocalExecutionEngine',
+
+        ],
+        'cosmic_ray.interceptors': [
+            'spor = cosmic_ray.interceptors.spor:intercept',
+            'pragma_no_mutate = cosmic_ray.interceptors.pragma_no_mutate:intercept',
+            'operators-filter = cosmic_ray.interceptors.operators_filter:intercept',
+        ],
+    },
+    long_description=LONG_DESCRIPTION,
+)
---
+++ packages/cosmic-ray/cosmic_ray-5.6.2.tar.gz/cosmic_ray-5.6.2/src/cosmic_ray/commands/init.py
@@ -0,0 +1,86 @@
+"Implementation of the 'init' command."
+import logging
+import uuid
+
+from cosmic_ray.ast import get_ast, Visitor
+import cosmic_ray.modules
+from cosmic_ray.plugins import get_interceptor, interceptor_names, get_operator
+from cosmic_ray.work_item import WorkItem
+
+log = logging.getLogger()
+
+
+class WorkDBInitVisitor(Visitor):
+    """An AST visitor that initializes a WorkDB for a specific module and operator.
+
+    The idea is to walk the AST looking for nodes that the operator can mutate.
+    As they're found, `activate` is called and this core adds new
+    WorkItems to the WorkDB. Use this core to populate a WorkDB by creating one
+    for each operator-module pair and running it over the module's AST.
+    """
+
+    def __init__(self, module_path, op_name, work_db, operator):
+        self.operator = operator
+        self.module_path = module_path
+        self.op_name = op_name
+        self.work_db = work_db
+        self.occurrence = 0
+
+    def visit(self, node):
+        for start, stop in self.operator.mutation_positions(node):
+            self._record_work_item(start, stop)
+        return node
+
+    def _record_work_item(self, start_pos, end_pos):
+        self.work_db.add_work_item(
+            WorkItem(
+                job_id=uuid.uuid4().hex,
+                module_path=str(self.module_path),
+                operator_name=self.op_name,
+                occurrence=self.occurrence,
+                start_pos=start_pos,
+                end_pos=end_pos))
+
+        self.occurrence += 1
+
+
+def init(module_paths, work_db, config):
+    """Clear and initialize a work-db with work items.
+
+    Any existing data in the work-db will be cleared and replaced with entirely
+    new work orders. In particular, this means that any results in the db are
+    removed.
+
+    Args:
+      module_paths: iterable of pathlib.Paths of modules to mutate.
+      work_db: A `WorkDB` instance into which the work orders will be saved.
+      config: The configuration for the new session.
+    """
+
+    operator_names = list(cosmic_ray.plugins.operator_names())
+
+    work_db.set_config(config=config)
+
+    work_db.clear()
+
+    for module_path in module_paths:
+        module_ast = get_ast(
+            module_path, python_version=config.python_version)
+
+        for op_name in operator_names:
+            operator = get_operator(op_name)(config.python_version)
+            visitor = WorkDBInitVisitor(module_path, op_name, work_db,
+                                        operator)
+            visitor.walk(module_ast)
+
+    enabled_interceptors = config.sub('interceptors').get('enabled', ())
+    apply_interceptors(work_db, enabled_interceptors, config)
+
+
+def apply_interceptors(work_db, enabled_interceptors, config):
+    """Apply each registered interceptor to the WorkDB."""
+    names = (name for name in interceptor_names() if name in enabled_interceptors)
+    for name in names:
+        sub_config = config.sub(name)
+        interceptor = get_interceptor(name)
+        interceptor(work_db, sub_config)
