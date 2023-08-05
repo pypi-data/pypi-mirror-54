dba0a619efb84fb4d91f2aedadd6b2e23a351154
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:38 +0000

    Add package django-environ-var/0.0/django-environ-var-0.0.tar.gz

---
+++ packages/django-environ-var/django-environ-var-0.0.tar.gz/django-environ-var-0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='django-environ-var',
+      version='0.0',
+      description='Declare Environment To Django Project',
+      url='https://github.com/datasik/django-environ-var',
+      author='Monirul Hasan',
+      author_email='89monirulhasan@gmail.com',
+      license='GPL',
+      packages=['django_environ_var'],
+      zip_safe=False)
