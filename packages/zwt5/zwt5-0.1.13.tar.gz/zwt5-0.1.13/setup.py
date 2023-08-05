55db40ecbbd8504d769bccc4b4237153c7816636
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:18 +0000

    Add package zwt5/0.1.13/zwt5-0.1.13.tar.gz

---
+++ packages/zwt5/zwt5-0.1.13.tar.gz/zwt5-0.1.13/setup.py
@@ -0,0 +1,17 @@
+# !/usr/bin/env python
+
+from setuptools import setup
+setup(name='zwt5',
+version='0.1.13',
+description='A silly package',
+author='Wentao Zheng',
+author_email='nkzhengwt@outlook.com',
+url='',
+packages=['zwt5', 'zwt5.adv'],
+install_requires=['numpy','pandas'],
+license = 'MIT',
+python_requires='>=3',
+package_data={
+    'zwt5': ['1.txt'],
+},
+)
---
+++ packages/zwt5/zwt5-0.1.13.tar.gz/zwt5-0.1.13/zwt5/__init__.py
@@ -0,0 +1,9 @@
+import numpy
+from .add import add
+from .division import division
+from .multiply import multiply
+from .subtract import subtract
+from .adv.sqrt import squareroot
+import pandas as pd
+a = pd.read_csv('zwt//1.txt')
+print(a)
