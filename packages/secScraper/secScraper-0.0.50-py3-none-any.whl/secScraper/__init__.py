e6e159d0b672c9676362ac2b8538fd3605d82e2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:14 +0000

    Add package secScraper/0.0.50/secScraper-0.0.50-py3-none-any.whl

---
+++ packages/secScraper/secScraper-0.0.50-py3-none-any.whl/secScraper/__init__.py
@@ -0,0 +1,18 @@
+from os.path import dirname, basename, isfile, join, abspath, split
+import glob
+
+path_glob = join(dirname(abspath(__file__)), '*.py')
+modules = glob.glob(path_glob)
+module_blacklist = [
+    '__init__.py',
+    'setup.py',
+    'Filing metrics.py',
+    'main_new_scores.py',
+    'ec2_scraping.py',
+    'csv_to_postgres.py'
+]
+
+__name__ = "secScraper"
+__version__ = "0.0.50"
+__all__ = [ basename(f)[:-3] for f in modules if isfile(f) and split(f)[1] not in module_blacklist]
+# print("[INFO] Loaded the following modules:\n", __all__)
