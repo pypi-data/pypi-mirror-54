988d41bd475a017e54159b1c157748ad93955bb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:22 +0000

    Add package spead2/2.0.2/spead2-2.0.2.tar.gz

---
+++ packages/spead2/spead2-2.0.2.tar.gz/spead2-2.0.2/.travis/install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e -v
+
+echo "TEST_PYTHON = ${TEST_PYTHON:=no}"
+echo "TEST_CXX = ${TEST_CXX:=no}"
+echo "COVERAGE = ${COVERAGE:=no}"
+echo "PYTHON = ${PYTHON:=python3}"
+echo "TRAVIS_OS_NAME = ${TRAVIS_OS_NAME:=linux}"
+echo "PYPY_VERSION = ${PYPY_VERSION:=5.9.0}"
+echo "CC = ${CC:=gcc}"
+echo "CXX = ${CXX:=g++}"
+
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    if [[ "$PYTHON" == "python3" ]]; then
+        brew update
+        brew upgrade python
+        pyvenv venv
+    fi
+elif [[ "$PYTHON" == pypy* ]]; then
+    curl -fSL https://bitbucket.org/pypy/pypy/downloads/${PYTHON}-v${PYPY_VERSION}-linux64.tar.bz2 | tar -jx
+    PY="$PWD/$PYTHON-v${PYPY_VERSION}-linux64/bin/pypy3"
+    virtualenv -p $PY venv
+else
+    virtualenv -p `which $PYTHON` venv
+fi
+
+source venv/bin/activate
+pip install -U pip setuptools wheel
+pip install -r requirements.txt
+if [ "$COVERAGE" = "yes" ]; then
+    pip install cpp-coveralls
+fi
