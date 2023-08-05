948b695591f964ce03c90014b76da3746de250e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:31 +0000

    Add package pydeclares/0.4/pydeclares-0.4.tar.gz

---
+++ packages/pydeclares/pydeclares-0.4.tar.gz/pydeclares-0.4/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pydeclares',
+    version='0.4',
+    author="heweitao",
+    author_email="675428202@qq.com",
+    description="A tool for provide format and serialize support to json, xml, form data and query string.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/phona/pydeclares",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
