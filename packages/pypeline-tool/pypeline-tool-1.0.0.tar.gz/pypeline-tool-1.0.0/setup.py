78dc37d7bc2667485d9f9ba0848dab3bb1143f18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:13 +0000

    Add package pypeline-tool/1.0.0/pypeline-tool-1.0.0.tar.gz

---
+++ packages/pypeline-tool/pypeline-tool-1.0.0.tar.gz/pypeline-tool-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name='pypeline-tool',
+      version='1.0.0',
+      description='Pipeline tool',
+      author='Ohrlando',
+      author_email='pythonicos@outlook.com',
+      url='https://github.com/Pythonicos/pypeline',
+      py_modules=['pypeline/__init__'],
+      license='MIT',
+      classifiers=['Programming Language :: Python :: 3'],
+      keywords='pipeline tasks chain',
+      python_requires='>=3')
