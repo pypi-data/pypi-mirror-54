174c36cb614f592656e6a71f6dfdef4cf83d6c1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:56 +0000

    Add package jbtech-utils-package/0.0.3/jbtech-utils-package-0.0.3.tar.gz

---
+++ packages/jbtech-utils-package/jbtech-utils-package-0.0.3.tar.gz/jbtech-utils-package-0.0.3/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open('README.md', 'r') as read_me:
+    description = read_me.read()
+
+setuptools.setup(
+    name='jbtech-utils-package',
+    version='0.0.3',
+    author='James Beringer',
+    author_email='jamberin@gmail.com',
+    description='Package for just basic utitilies',
+    long_description=description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/jamberin/jbtech-utils-package',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3.7',
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: Public Domain'
+    ]
+)
