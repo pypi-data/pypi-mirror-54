2917cb3c12054dba31e2d5d9b203528e285cf9a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:37 +0000

    Add package fomo/1.2.2/fomo-1.2.2.tar.gz

---
+++ packages/fomo/fomo-1.2.2.tar.gz/fomo-1.2.2/Fomo/fomo.py
@@ -0,0 +1,203 @@
+import json
+import requests
+
+try:
+    import ssl
+except ImportError:
+    print('error: no ssl support')
+    exit(1)
+
+
+class FomoClient:
+    """Fomo Client wrapper"""
+
+    def __init__(self, token):
+        """Initializes client with authorization token"""
+        #: Fomo API auth token
+        self.__token = token
+
+        #: Fomo API endpoint
+        self.__endpoint = 'https://api.fomo.com'
+
+        #: SDK version
+        self.__version__ = '1.2.2'
+
+    def get_event(self, event_id):
+        """Get Fomo Event
+        @:param event_id: Fomo Event ID
+        :return: :class:`FomoEvent <FomoEvent>` object
+        :rtype: FomoEvent
+        """
+        return json.loads(self.__makeRequest('/api/v1/applications/me/events/' + repr(event_id), 'GET'))
+
+    def get_events(self, size=30, page=1):
+        """Get Fomo events
+        :return: :class:`FomoEventList <FomoEventList>` object
+        :rtype: FomoEventList
+        """
+        return json.loads(self.__makeRequest('/api/v1/applications/me/events?per=' + repr(size) + '&page=' + repr(page), 'GET'))
+
+    def get_events_with_meta(self, size=30, page=1):
+        """Get Fomo events with meta data
+        :return: :class:`FomoEventsWithMeta <FomoEventsWithMeta>` object
+        :rtype: FomoEventsWithMeta
+        """
+        response = self.__makeRequest(
+            '/api/v1/applications/me/events?per=' + repr(size) + '&page=' + repr(page) + '&show_meta=true', 'GET')
+        return json.loads(response)
+
+    def delete_event(self, event_id):
+        """Delete Fomo Event
+        @:param event_id: string Fomo Event ID
+        :return: :class:`FomoDeleteMessageResponse <FomoDeleteMessageResponse>` object
+        :rtype: FomoDeleteMessageResponse
+        """
+        return json.loads(self.__makeRequest('/api/v1/applications/me/events/' + repr(event_id), 'DELETE'))
+
+    def create_event(self, event):
+        """Create Fomo Event
+        @:param event: Fomo event to create
+        @type event: FomoEventBasic
+        :return: :class:`FomoEvent <FomoEvent>` object
+        :rtype: FomoEvent
+        """
+        return json.loads(self.__makeRequest('/api/v1/applications/me/events', 'POST', FomoEventWrapper(event)))
+
+    def update_event(self, event):
+        """Update Fomo Event
+        @:param event: Fomo event to update
+        @type event: FomoEvent
+        :return: :class:`FomoEvent <FomoEvent>` object
+        :rtype: FomoEvent
+        """
+        return json.loads(self.__makeRequest('/api/v1/applications/me/events/' + repr(event['id']), 'PATCH', FomoEventWrapper(event)))
+
+    def __makeRequest(self, path, method, data=None):
+        """Make Fomo Authorized API request
+        @:param path: API path
+        @type path: string
+        @:param method: HTTP method
+        @type method: string
+        @:param data: Dictionary of data to be send to server
+        @type data: FomoEventWrapper
+        """
+        headers = {
+            'Authorization': 'Token ' + self.__token,
+            'User-Agent': 'Fomo/Python/' + self.__version__
+        }
+        if data is not None:
+            headers['Content-Type'] = 'application/json'
+
+        response = requests.request(method, self.__endpoint + path,
+                                    data=data.to_JSON() if data is not None else None,
+                                    headers=headers)
+        return response.text
+
+
+class FomoEventCustomAttribute(object):
+    """Custom event attribute"""
+
+    def __init__(self, key, value):
+        """Creates new custom attribute object"""
+        #: Custom attribute key
+        self.key = key
+
+        #: Custom attribute value
+        self.value = value
+
+    def __str__(self):
+        """Dumps JSON serialized object"""
+        return json.dumps(self.__dict__, indent=4)
+
+
+class FomoEventBasic(object):
+    """Fomo basic event"""
+
+    def __init__(self, event_type_id="", event_type_tag="", url="", first_name="", email_address="", ip_address="", city="", province="", country="",
+                 title="", external_id="",
+                 image_url="", custom_event_fields_attributes=None):
+        """Create a new Fomo basic event"""
+
+        #: Event type unique ID (optional|required if event_type_tag = '')
+        if custom_event_fields_attributes is None:
+            custom_event_fields_attributes = []
+        self.event_type_id = event_type_id
+
+        #: Event type tag (optional|required if event_type_id = '')
+        self.event_type_tag = event_type_tag
+
+        #: Url to redirect on the event click. Size range: 0..255 (required)
+        self.url = url
+
+        #: First name of the person on the event. Size range: 0..255
+        self.first_name = first_name
+
+        #: Email address of the person on the event. Size range: 0..255
+        self.email_address = email_address
+
+        #: IP address of the person on the event. Size range: 0..255
+        self.ip_address = ip_address
+
+        #: City where the event happened. Size range: 0..255
+        self.city = city
+
+        #: Province where the event happened. Size range: 0..255
+        self.province = province
+
+        #: Country where the event happened ISO-2 standard. Size range: 0..255
+        self.country = country
+
+        #: Title of the event. Size range: 0..255
+        self.title = title
+
+        #: Url of the image to be displayed. Size range: 0..255
+        self.image_url = image_url
+
+        #: A list of :class:`FomoEventCustomAttribute <FomoEventCustomAttribute>` objects for custom event fields
+        self.custom_event_fields_attributes = custom_event_fields_attributes
+
+    def add_custom_event_field(self, key, value):
+        """Add custom event field"""
+        self.custom_event_fields_attributes.append(FomoEventCustomAttribute(key, value))
+
+    def __str__(self):
+        """Dumps JSON serialized object"""
+        return json.dumps(self.__dict__, indent=4)
+
+    @classmethod
+    def from_json(cls, json_str):
+        """Parse object from JSON"""
+        json_dict = json.loads(json_str)
+        return cls(**json_dict)
+
+    def to_JSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__,
+                          sort_keys=True)
+
+    def __repr__(self):
+        """Dumps itself to dictionary"""
+        return json.dumps(self.__dict__)
+
+class FomoEventWrapper(object):
+    def __init__(self, event=None):
+        """Initializes Fomo Event wrapper"""
+        #: Event
+        self.event = event
+
+    def __str__(self):
+        """Dumps JSON serialized object"""
+        return json.dumps(self.__dict__, indent=4)
+
+    @classmethod
+    def from_json(cls, json_str):
+        """Parse object from JSON"""
+        json_dict = json.loads(json_str)
+        return cls(**json_dict)
+
+    def __repr__(self):
+        """Dumps itself to dictionary"""
+        return json.dumps(self.__dict__)
+
+    def to_JSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__,
+                          sort_keys=True)
---
+++ packages/fomo/fomo-1.2.2.tar.gz/fomo-1.2.2/README.rst
@@ -0,0 +1,125 @@
+Fomo Python SDK
+===============
+
+*Fomo Python SDK* is the official SDK wrapper for the `Fomo API service <https://fomo.com>`_
+
+API docs: `https://docs.fomo.com <https://docs.fomo.com>`_
+
+Requirements
+------------
+
+- Python version [2.7+, 3.x+]
+- pyOpenSSL / OpenSSL with SNI support
+- Python module `requests <http://python-requests.org>`_
+
+Installation
+------------
+
+Install the latest version with
+
+.. code-block:: bash
+
+    $ pip install fomo
+
+
+Manual User Installation
+------------------------
+
+Download `Fomo/fomo.py <https://github.com/usefomo/fomo-python-sdk/blob/master/Fomo/fomo.py>`_ and include the file in your Python project.
+
+Check out our examples in `tests/test.py <https://github.com/usefomo/fomo-python-sdk/blob/master/tests/test.py>`_, quick usage examples:
+
+Basic Usage
+-----------
+
+Initialize Fomo client:
+
+.. code-block:: python
+
+    import Fomo
+    client = Fomo.FomoClient('<token>')  # Auth token can be found Fomo application admin dashboard (App -> API Access)
+
+
+Create a new event with template name:
+
+.. code-block:: python
+
+    event = Fomo.FomoEventBasic()
+    event.event_type_tag = 'new_order'  # Event type tag is found on Fomo dashboard (Templates -> Template name)
+    event.email_address = 'ryan.kulp@fomo.com' # used to fetch Gravatar for notification image
+    event.title = 'Test event'
+    event.city = 'San Francisco'
+    event.url = 'https://www.fomo.com'
+
+    # Add event custom attribute value
+    event.add_custom_event_field('variable_name', 'value')
+
+    created_event = client.create_event(event)
+    print(created_event)
+
+or with template ID:
+
+.. code-block:: python
+
+    event = Fomo.FomoEventBasic()
+    event.event_type_id = '183'  # Event type ID is found on Fomo dashboard (Templates -> Template ID)
+    event.title = 'Test event'
+    event.city = 'San Francisco'
+    event.url = 'https://www.fomo.com'
+
+    # Add event custom attribute value
+    event.add_custom_event_field('variable_name', 'value')
+
+    created_event = client.create_event(event)
+    print(created_event)
+
+
+Fetch an event:
+
+.. code-block:: python
+
+    event = client.get_event('<event ID>')
+    print(event)
+
+
+Get events:
+
+.. code-block:: python
+
+    events = client.get_events(30, 1)
+    print(events)
+
+
+Get events with meta data:
+
+.. code-block:: python
+
+    data = client.get_events_with_meta(30, 1)
+    print(data['events'])
+    print(data['meta']['per_page'])
+    print(data['meta']['page'])
+    print(data['meta']['total_count'])
+    print(data['meta']['total_pages'])
+
+
+Delete an event:
+
+.. code-block:: python
+
+    client.delete_event('<event ID>')
+
+
+Update an event:
+
+.. code-block:: python
+
+    event['first_name'] = 'Donny'
+    event['custom_event_fields_attributes'] = { 'amount': 19.99 }
+
+    updated_event = client.update_event(event)
+    print(updated_event)
+
+Support
+-------
+
+If you have questions, email us at `hello@fomo.com <mailto:hello@fomo.com>`_.
