d98bcb4f2844d57e552c15a55a3e8d228502e220
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:07 +0000

    Add package flattentool/0.9.0/flattentool-0.9.0.tar.gz

---
+++ packages/flattentool/flattentool-0.9.0.tar.gz/flattentool-0.9.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+import sys
+
+install_requires = ['jsonref', 'schema', 'openpyxl>=2.6', 'six', 'pytz', 'xmltodict', 'lxml']
+
+if sys.version < '3':
+    install_requires.append('unicodecsv')
+
+setup(
+    name='flattentool',
+    version='0.9.0',
+    author='Open Data Services',
+    author_email='code@opendataservices.coop',
+    packages=['flattentool'],
+    scripts=['flatten-tool'],
+    url='https://github.com/OpenDataServices/flatten-tool',
+    license='MIT',
+    description='Tools for generating CSV and other flat versions of the structured data',
+    install_requires=install_requires,
+    extras_require = {
+        'HTTP': ['requests']
+    }
+)
