f84d74bd988a13a2e02557cf427c96323b2e2b4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:52 +0000

    Add package pynidm/0.2.dev0/pynidm-0.2.dev0.tar.gz

---
+++ packages/pynidm/pynidm-0.2.dev0.tar.gz/pynidm-0.2.dev0/nidm/version.py
@@ -0,0 +1,62 @@
+from __future__ import absolute_import, division, print_function
+import os.path
+
+# Format expected by setup.py and doc/source/conf.py: string of form "X.Y.Z"
+_version_major = 0
+_version_minor = 2
+_version_micro = ''  # use '' for first of series, number for 1 and above
+_version_extra = 'dev'
+# _version_extra = ''  # Uncomment this for full releases
+
+# Construct full version string from these.
+_ver = [_version_major, _version_minor]
+if _version_micro:
+    _ver.append(_version_micro)
+if _version_extra:
+    _ver.append(_version_extra)
+
+__version__ = '.'.join(map(str, _ver))
+
+CLASSIFIERS = ["Development Status :: 3 - Alpha",
+               "Environment :: Console",
+               "Intended Audience :: Science/Research",
+               "License :: OSI Approved :: Apache Software License",
+               "Operating System :: MacOS :: MacOS X",
+               "Operating System :: POSIX :: Linux",
+               "Programming Language :: Python :: 3",
+               "Topic :: Scientific/Engineering"]
+
+# Description should be a one-liner:
+# TODO
+description = "NIDM: a Python NIDM library"
+# Long description will go up on the pypi page
+long_description = """
+NIDM
+========
+HERE should be longer description of tha package TODO!
+License
+=======
+``nidm`` is licensed under the terms of the Apache License 2.0. See the file
+"LICENSE" for information on the history of this software, terms & conditions
+for usage, and a DISCLAIMER OF ALL WARRANTIES.
+"""
+
+NAME = "pynidm"
+MAINTAINER = "INCF-NIDASH developers"
+MAINTAINER_EMAIL = "incf-nidash-nidm@googlegroups.com"
+DESCRIPTION = description
+LONG_DESCRIPTION = long_description
+URL = "https://github.com/incf-nidash/PyNIDM"
+DOWNLOAD_URL = ""
+LICENSE = "Apache License 2.0"
+AUTHOR = "INCF-NIDASH developers"
+AUTHOR_EMAIL = "incf-nidash-nidm@googlegroups.com"
+MAJOR = _version_major
+MINOR = _version_minor
+MICRO = _version_micro
+VERSION = __version__
+#REQUIRES = ["prov", "rdflib", "graphviz", "pydotplus", "pydot", "validators", "requests", "fuzzywuzzy", "pygithub",
+#            "pandas", "pybids", "duecredit", "pytest", "graphviz", "click", "ontquery"]
+INSTALL_REQUIRES = ["prov", "rdflib", "graphviz", "pydotplus", "pydot", "validators", "requests", "fuzzywuzzy", "pygithub",
+            "pandas", "pybids", "duecredit", "pytest", "graphviz", "click", "ontquery","rdflib-jsonld","pyld", "neurdflib"]
+SCRIPTS = ["bin/nidm_query", "bin/BIDSMRI2NIDM", "bin/CSV2NIDM","bin/nidm_utils"]
