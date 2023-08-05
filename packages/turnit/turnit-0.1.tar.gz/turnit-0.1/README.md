933986a5f7355cb9f19438af170de2e9a0aafc51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:12 +0000

    Add package turnit/0.1/turnit-0.1.tar.gz

---
+++ packages/turnit/turnit-0.1.tar.gz/turnit-0.1/README.md
@@ -0,0 +1,7 @@
+# Turnit: minimize support structures for 3D printing
+
+Turnit is a program that, given an .STL file describing an object, orients it
+in a way that would minimize the amount of support structure needed to
+3D-print it.
+
+![Turnit](images/turnit_example.png)
---
+++ packages/turnit/turnit-0.1.tar.gz/turnit-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='turnit',
+      version='0.1',
+      description='Orient STL files to minimize supports',
+      long_description=open('README.md').read(),
+      url='http://github.com/nickc92/turnit',
+      author='Nick C',
+      author_email='nickcholy@gmail.com',
+      license='MIT',
+      packages=['turnit'],
+      install_requires=['matplotlib', 'scipy', 'numpy', 'numpy-stl', 'rtree'],
+      scripts=['bin/turnit'],
+      zip_safe=False)
