4c4f845887cc6db84b85a0d29d395befd91f10dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:17 +0000

    Add package giotto-learn/0.1a0/giotto_learn-0.1a0-cp36-cp36m-win_amd64.whl

---
+++ packages/giotto-learn/giotto_learn-0.1a0-cp36-cp36m-win_amd64.whl/giotto/base.py
@@ -0,0 +1,72 @@
+"""Base class for transforming and resampling"""
+# License: Apache 2.0
+
+
+class TransformerResamplerMixin:
+    """Mixin class for all transformers resamplers in giotto."""
+
+    _estimator_type = 'transformer_resampler'
+
+    def fit_transform(self, X, y=None, **fit_params):
+        """Fit to data, then transform it.
+        Fits transformer to X and y with optional parameters fit_params
+        and returns a transformed version of X.
+
+        Parameters
+        ----------
+        X : numpy array of shape [n_samples, n_features]
+            Training set.
+        y : numpy array of shape [n_samples]
+            Target values.
+
+        Returns
+        -------
+        X_new : numpy array of shape [n_samples, n_features_new]
+            Transformed array.
+        """
+        # non-optimized default implementation; override when a better
+        # method is possible for a given clustering algorithm
+        if y is None:
+            # fit method of arity 1 (unsupervised transformation)
+            return self.fit(X, **fit_params).transform(X)
+        else:
+            # fit method of arity 2 (supervised transformation)
+            return self.fit(X, y, **fit_params).transform(X, y)
+
+    def transform_resample(self, X, y):
+        """Fit to data, then transform it.
+        Fits transformer to X and y with optional parameters fit_params
+        and returns a transformed version of X.
+
+        Parameters
+        ----------
+        X : numpy array of shape [n_samples, n_features]
+            Training set.
+        y : numpy array of shape [n_samples]
+            Target values.
+
+        Returns
+        -------
+        X_new : numpy array of shape [n_samples, n_features_new]
+            Transformed array.
+        """
+        return self.transform(X), self.resample(y, X)
+
+    def fit_transform_resample(self, X, y, **fit_params):
+        """Fit to data, then transform it.
+        Fits transformer to X and y with optional parameters fit_params
+        and returns a transformed version of X.
+
+        Parameters
+        ----------
+        X : numpy array of shape [n_samples, n_features]
+            Training set.
+        y : numpy array of shape [n_samples]
+            Target values.
+
+        Returns
+        -------
+        X_new : numpy array of shape [n_samples, n_features_new]
+            Transformed array.
+        """
+        return self.fit(X, y, **fit_params).transform_resample(X, y)
