6c446d62534c0f364de95480f87c8f8ecf5abe73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:32 +0000

    Add package ole-py/1.1.2/ole-py-1.1.2.tar.gz

---
+++ packages/ole-py/ole-py-1.1.2.tar.gz/ole-py-1.1.2/setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import find_packages, setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='ole-py',
+    version='1.1.2',
+    url='https://github.com/hallazzang/ole-py',
+    license='MIT',
+    author='Hanjun Kim',
+    author_email='hallazzang@gmail.com',
+    description='Lightweight Microsoft OLE file parser in pure Python',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    include_package_data=True,
+    packages=find_packages(),
+    python_requires='>=3.5',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
