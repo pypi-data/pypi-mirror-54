1de2cfb7e418b698e7ccbdc359e303d6381dffda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:38 +0000

    Add package padmet/3.2.2/padmet-3.2.2.tar.gz

---
+++ packages/padmet/padmet-3.2.2.tar.gz/padmet-3.2.2/setup.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Definition of setup function for setuptools module."""
+
+# Standard imports
+import sys
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+################################################################################
+
+class PyTest(TestCommand):
+    """Call tests with the custom 'python setup.py test' command."""
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main()
+        sys.exit(errno)
+
+################################################################################
+ 
+setup(
+ 
+    name='padmet',
+    version="3.2.2",
+ 
+    packages=find_packages(),
+ 
+    author="Meziane AITE",
+    author_email="meziane.aite@inria.fr",
+ 
+    description="Padmet package for metabolic network",
+ 
+    long_description=open('README.md', encoding='utf8').read(),
+ 
+    
+    install_requires= ["docopt==0.6.2","python-libsbml==5.18.0","cobra==0.14.0","biopython==1.70"],
+ 
+    include_package_data=True,
+ 
+    url='https://github.com/AuReMe/padmet',
+ 
+    classifiers=[
+        "Programming Language :: Python",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved",
+        "Natural Language :: French",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+)
