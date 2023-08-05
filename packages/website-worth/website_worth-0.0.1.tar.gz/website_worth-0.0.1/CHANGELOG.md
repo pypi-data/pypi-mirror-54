2787f7bf5f2ef604ed648dbf55b19abfd7836cdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:17 +0000

    Add package website-worth/0.0.1/website_worth-0.0.1.tar.gz

---
+++ packages/website-worth/website_worth-0.0.1.tar.gz/website_worth-0.0.1/CHANGELOG.md
@@ -0,0 +1,11 @@
+Changelog
+=========
+
+Unreleased
+----------
+
+0.0.1 (2019-10-17)
+------------------
+
+* First release on PyPI.
+* Add https://www.webuka.com support.
---
+++ packages/website-worth/website_worth-0.0.1.tar.gz/website_worth-0.0.1/website_worth/calcs.py
@@ -0,0 +1,68 @@
+import click
+import requests
+from bs4 import BeautifulSoup
+from requests import codes
+
+from website_worth.constants import SCRAPERS
+
+
+class BaseConfigScraper(object):
+    NAME = ''
+
+    def get(self, url):
+        """
+        Get source of calculated site
+        :return: string
+        """
+        raise NotImplementedError
+
+    def scrape(self, content):
+        """
+        Get data of calculated site
+        :return: dict
+        """
+        raise NotImplementedError
+
+    def run(self, url):
+        content = self.get(url)
+        data = self.scrape(content)
+        click.echo("=== Data of '{}'".format(url))
+        click.echo("=== Using '{}' calculator".format(self.NAME))
+        for name, value in data.items():
+            click.echo(" - {}: {}".format(name, value))
+
+
+class WebukaScraper(BaseConfigScraper):
+    URL = "http://www.webuka.com/files/apps/worth/websiteworth.php"
+    NAME = 'https://webuka.com'
+
+    def get(self, url):
+        req = requests.post(self.URL, data={
+            "www": url
+        })
+        if req.status_code is codes.OK:
+            return req.content
+        else:
+            req.raise_for_status()
+
+    def scrape(self, content):
+        data = dict()
+        soup = BeautifulSoup(content, "html.parser")
+        groups = soup.find_all("hgroup")
+        for group in groups:
+            name = group.find("h3").text
+            value = group.find("p").text
+            data[name] = value
+        return data
+
+
+def get_scraper(app_name):
+    """
+    Return scraper based on app name.
+    :param app_name: String
+    :return: parser object
+    """
+    app_parser_name = SCRAPERS[app_name]
+    parser = [subcls for subcls in BaseConfigScraper.__subclasses__() if
+              subcls.__name__ == app_parser_name][0]
+    return parser()  # return scraper object instead of class
