4c2b23ef9e6d5a859e8d3be8aad995225f0a59a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:37 +0000

    Add package getUserName/0.1/getUserName-0.1.tar.gz

---
+++ packages/getUserName/getUserName-0.1.tar.gz/getUserName-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='getUserName',
+      version='0.1',
+      description='Gets system Username',
+      author='karthik',
+      author_email='patkarkarthik@gmail.com',
+      license='kar',
+      packages=['getUserName'],
+      zip_safe=False)
