27dbc45bb448be6d4850c4d8b91d2f0245f79540
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:54 +0000

    Add package syj-prob-distributions/0.1/syj-prob-distributions-0.1.tar.gz

---
+++ packages/syj-prob-distributions/syj-prob-distributions-0.1.tar.gz/syj-prob-distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='syj-prob-distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['syj-prob-distributions'],
+      author='Syed Muhammad Haider Jafri',
+      zip_safe=False)
