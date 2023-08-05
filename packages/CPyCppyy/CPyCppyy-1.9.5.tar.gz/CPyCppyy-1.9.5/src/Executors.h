01af10a96c385a0f42a8ab0a2cb831d3c772a0d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:08 +0000

    Add package CPyCppyy/1.9.5/CPyCppyy-1.9.5.tar.gz

---
+++ packages/CPyCppyy/CPyCppyy-1.9.5.tar.gz/CPyCppyy-1.9.5/src/Executors.h
@@ -0,0 +1,36 @@
+#ifndef CPYCPPYY_EXECUTORS_H
+#define CPYCPPYY_EXECUTORS_H
+
+// Standard
+#include <string>
+
+
+namespace CPyCppyy {
+
+struct CallContext;
+
+class Executor {
+public:
+    virtual ~Executor() {}
+    virtual PyObject* Execute(
+        Cppyy::TCppMethod_t, Cppyy::TCppObject_t, CallContext*) = 0;
+    virtual bool HasState() { return false; }
+};
+
+// special case needed for CPPSetItem
+class RefExecutor : public Executor {
+public:
+    RefExecutor() : fAssignable(nullptr) {}
+    virtual bool SetAssignable(PyObject*);
+    virtual bool HasState() { return true; }
+
+protected:
+    PyObject* fAssignable;
+};
+
+// create executor from fully qualified type
+Executor* CreateExecutor(const std::string& fullType);
+
+} // namespace CPyCppyy
+
+#endif // !CPYCPPYY_EXECUTORS_H
