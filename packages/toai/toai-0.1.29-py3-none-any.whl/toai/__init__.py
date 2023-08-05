99a33bff2f1d25aeedfe4fb837225b0923129e02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:31 +0000

    Add package toai/0.1.29/toai-0.1.29-py3-none-any.whl

---
+++ packages/toai/toai-0.1.29-py3-none-any.whl/toai/__init__.py
@@ -0,0 +1,2 @@
+VERSION = (0, 1, 29)
+__version__ = ".".join([str(x) for x in VERSION])
---
+++ packages/toai/toai-0.1.29-py3-none-any.whl/toai/data/__init__.py
@@ -0,0 +1,6 @@
+# pylama:ignore=W0611
+
+from .DataContainer import DataContainer
+from .DataParams import DataParams
+from .Dataset import Dataset
+from .utils import split_df
