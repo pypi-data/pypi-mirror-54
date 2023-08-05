8f5f987f2c101cf13825a7104db482a54357ecf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:21 +0000

    Add package audio-metadata/0.6.0/audio_metadata-0.6.0-py3-none-any.whl

---
+++ packages/audio-metadata/audio_metadata-0.6.0-py3-none-any.whl/audio_metadata/formats/vorbis.py
@@ -0,0 +1,58 @@
+__all__ = ['VorbisComments', 'VorbisPicture']
+
+import struct
+from collections import defaultdict
+
+from .models import Picture, Tags
+from .tables import ID3PictureType
+from ..utils import datareader
+
+
+# TODO: Number frames.
+class VorbisComments(Tags):
+	@datareader
+	@classmethod
+	def load(cls, data):
+		vendor_length = struct.unpack('I', data.read(4))[0]
+		vendor = data.read(vendor_length).decode('utf-8', 'replace')
+		num_comments = struct.unpack('I', data.read(4))[0]
+
+		fields = defaultdict(list)
+
+		for _ in range(num_comments):
+			length = struct.unpack('I', data.read(4))[0]
+			comment = data.read(length).decode('utf-8', 'replace')
+
+			if '=' in comment:  # pragma: nobranch
+				field, value = comment.split('=', 1)
+
+				fields[field.lower()].append(value)
+
+		return cls(**fields, _vendor=vendor)
+
+
+class VorbisPicture(Picture):
+	@datareader
+	@classmethod
+	def load(cls, data):
+		type_, mime_length = struct.unpack('>2I', data.read(8))
+		mime_type = data.read(mime_length).decode('utf-8', 'replace')
+
+		desc_length = struct.unpack('>I', data.read(4))[0]
+		description = data.read(desc_length).decode('utf-8', 'replace')
+
+		width, height, depth, colors = struct.unpack('>4I', data.read(16))
+
+		data_length = struct.unpack('>I', data.read(4))[0]
+		data = data.read(data_length)
+
+		return cls(
+			type=ID3PictureType(type_),
+			mime_type=mime_type,
+			description=description,
+			width=width,
+			height=height,
+			depth=depth,
+			colors=colors,
+			data=data
+		)
