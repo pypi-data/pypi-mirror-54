5f6d1ae45710adb3c6c47e51f9e8e7039e754d04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:15 +0000

    Add package outerspace/0.2.3/outerspace-0.2.3.tar.gz

---
+++ packages/outerspace/outerspace-0.2.3.tar.gz/outerspace-0.2.3/outerspace/util.py
@@ -0,0 +1,32 @@
+from PIL import Image
+from io import BytesIO
+import base64
+from collections import OrderedDict
+
+
+class objdict(OrderedDict):
+    def __getattr__(self, name):
+        if name in self:
+            return self[name]
+        else:
+            raise AttributeError("No such attribute: " + name)
+
+    def __setattr__(self, name, value):
+        self[name] = value
+
+    def __delattr__(self, name):
+        if name in self:
+            del self[name]
+        else:
+            raise AttributeError("No such attribute: " + name)
+
+
+def array2d_to_html_img(nparr, image_mode='L', resize=None, style=''):
+    img = Image.fromarray(nparr, image_mode)
+    if resize is not None:
+        img = img.resize(resize)
+    bytesio = BytesIO()
+    img.save(bytesio, format='PNG')
+    data = base64.b64encode(bytesio.getvalue()).decode()
+    html = f'<img src="data:image/png;base64,{data}" style="{style}"/>'
+    return html
