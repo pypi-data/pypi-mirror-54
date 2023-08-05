467e8f37e4880cc094f303259cf343076f4d7186
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:50 +0000

    Add package tf-utils/1.0.2/tf_utils-1.0.2-py3-none-any.whl

---
+++ packages/tf-utils/tf_utils-1.0.2-py3-none-any.whl/tf_utils/gail_utils.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+
+def conjugate_gradient(f_Ax, b, cg_iters=10, residual_tol=1e-10):
+    p = b.copy()
+    r = b.copy()
+    x = np.zeros_like(b)
+    rdotr = r.dot(r)
+    for i in range(cg_iters):
+        z = f_Ax(p)
+        v = rdotr / p.dot(z)
+        x += v * p
+        r -= v * z
+        newrdotr = r.dot(r)
+        mu = newrdotr / rdotr
+        p = r + mu * p
+        rdotr = newrdotr
+        if rdotr < residual_tol:
+            break
+    return x
