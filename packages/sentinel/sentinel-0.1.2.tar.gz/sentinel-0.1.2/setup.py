d89525b98f3e55dff612cc7f0f4660a6587b829c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:21 +0000

    Add package sentinel/0.1.2/sentinel-0.1.2.tar.gz

---
+++ packages/sentinel/sentinel-0.1.2.tar.gz/sentinel-0.1.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+from sentinel import __version__ as VERSION
+from codecs import open
+
+with open('README.rst', encoding='UTF-8') as readme:
+    long_description = readme.read()
+
+setup(
+    name='sentinel',
+    version=VERSION,
+    url='https://github.com/eddieantonio/sentinel',
+    license='MIT',
+    author='Eddie Antonio Santos',
+    author_email='easantos@ualberta.ca',
+    description='Create sentinel and singleton objects',
+    long_description=long_description,
+    py_modules=['sentinel'],
+    platforms='any',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python',
+    ],
+    download_url = 'https://github.com/eddieantonio/sentinel/tarball/v' + VERSION,
+)
