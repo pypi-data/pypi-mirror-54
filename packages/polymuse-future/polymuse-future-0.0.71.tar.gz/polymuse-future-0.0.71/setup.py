49d5d425474082acffffce8781392051b08c2eee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:55 +0000

    Add package polymuse-future/0.0.71/polymuse-future-0.0.71.tar.gz

---
+++ packages/polymuse-future/polymuse-future-0.0.71.tar.gz/polymuse-future-0.0.71/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="polymuse-future",
+    version="0.0.71",
+    # scripts = ['MIDI.py', 'mutils.py', 'rmidi.py', 'sound.py'],
+    packages=['polymuse', 'midis', 'h5_models'],
+    author="rushike",
+    author_email="rushike.ab1@gmail.com",
+    description="Polymuse",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rushike/polymuse-future",
+    install_requires=['numpy', 'magenta', 'keras', 'pyfiglet', 'midi2audio', 'googledrivedownloader'],
+    # packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+
+    entry_points={
+          'console_scripts': [
+            #   'main = main:main',
+            #   'midiplay = midi2audio:main_play'
+          ]
+      },
+)
