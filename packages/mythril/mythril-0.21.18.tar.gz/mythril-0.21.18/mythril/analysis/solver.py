fed5c96673c725ad978f3b81de5573a0f839cee8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:17 +0000

    Add package mythril/0.21.18/mythril-0.21.18.tar.gz

---
+++ packages/mythril/mythril-0.21.18.tar.gz/mythril-0.21.18/mythril/analysis/solver.py
@@ -0,0 +1,218 @@
+"""This module contains analysis module helpers to solve path constraints."""
+from functools import lru_cache
+from typing import Dict, Tuple, Union
+from z3 import sat, unknown, FuncInterp
+import z3
+
+from mythril.analysis.analysis_args import analysis_args
+from mythril.laser.ethereum.state.global_state import GlobalState
+from mythril.laser.ethereum.state.constraints import Constraints
+from mythril.laser.ethereum.transaction import BaseTransaction
+from mythril.laser.smt import UGE, Optimize, symbol_factory
+from mythril.laser.ethereum.time_handler import time_handler
+from mythril.exceptions import UnsatError
+from mythril.laser.ethereum.transaction.transaction_models import (
+    ContractCreationTransaction,
+)
+import logging
+
+log = logging.getLogger(__name__)
+
+# LRU cache works great when used in powers of 2
+@lru_cache(maxsize=2 ** 23)
+def get_model(constraints, minimize=(), maximize=(), enforce_execution_time=True):
+    """
+
+    :param constraints:
+    :param minimize:
+    :param maximize:
+    :param enforce_execution_time: Bool variable which enforces --execution-timeout's time
+    :return:
+    """
+    s = Optimize()
+    timeout = analysis_args.solver_timeout
+    if enforce_execution_time:
+        timeout = min(timeout, time_handler.time_remaining() - 500)
+        if timeout <= 0:
+            raise UnsatError
+    s.set_timeout(timeout)
+    for constraint in constraints:
+        if type(constraint) == bool and not constraint:
+            raise UnsatError
+
+    constraints = [constraint for constraint in constraints if type(constraint) != bool]
+
+    for constraint in constraints:
+        s.add(constraint)
+    for e in minimize:
+        s.minimize(e)
+    for e in maximize:
+        s.maximize(e)
+
+    result = s.check()
+    if result == sat:
+        return s.model()
+    elif result == unknown:
+        log.debug("Timeout encountered while solving expression using z3")
+    raise UnsatError
+
+
+def pretty_print_model(model):
+    """ Pretty prints a z3 model
+
+    :param model:
+    :return:
+    """
+    ret = ""
+
+    for d in model.decls():
+        if type(model[d]) == FuncInterp:
+            condition = model[d].as_list()
+            ret += "%s: %s\n" % (d.name(), condition)
+            continue
+
+        try:
+            condition = "0x%x" % model[d].as_long()
+        except:
+            condition = str(z3.simplify(model[d]))
+
+        ret += "%s: %s\n" % (d.name(), condition)
+
+    return ret
+
+
+def get_transaction_sequence(
+    global_state: GlobalState, constraints: Constraints
+) -> Dict:
+    """Generate concrete transaction sequence.
+
+    :param global_state: GlobalState to generate transaction sequence for
+    :param constraints: list of constraints used to generate transaction sequence
+    """
+
+    transaction_sequence = global_state.world_state.transaction_sequence
+
+    concrete_transactions = []
+
+    tx_constraints, minimize = _set_minimisation_constraints(
+        transaction_sequence, constraints.copy(), [], 5000, global_state.world_state
+    )
+
+    try:
+        model = get_model(tx_constraints, minimize=minimize)
+    except UnsatError:
+        raise UnsatError
+
+    # Include creation account in initial state
+    # Note: This contains the code, which should not exist until after the first tx
+    initial_world_state = transaction_sequence[0].world_state
+    initial_accounts = initial_world_state.accounts
+
+    for transaction in transaction_sequence:
+        concrete_transaction = _get_concrete_transaction(model, transaction)
+        concrete_transactions.append(concrete_transaction)
+
+    min_price_dict = {}  # type: Dict[str, int]
+    for address in initial_accounts.keys():
+        min_price_dict[address] = model.eval(
+            initial_world_state.starting_balances[
+                symbol_factory.BitVecVal(address, 256)
+            ].raw,
+            model_completion=True,
+        ).as_long()
+
+    concrete_initial_state = _get_concrete_state(initial_accounts, min_price_dict)
+
+    steps = {"initialState": concrete_initial_state, "steps": concrete_transactions}
+
+    return steps
+
+
+def _get_concrete_state(initial_accounts: Dict, min_price_dict: Dict[str, int]):
+    """ Gets a concrete state """
+    accounts = {}
+    for address, account in initial_accounts.items():
+        # Skip empty default account
+
+        data = dict()  # type: Dict[str, Union[int, str]]
+        data["nonce"] = account.nonce
+        data["code"] = account.code.bytecode
+        data["storage"] = str(account.storage)
+        data["balance"] = hex(min_price_dict.get(address, 0))
+        accounts[hex(address)] = data
+    return {"accounts": accounts}
+
+
+def _get_concrete_transaction(model: z3.Model, transaction: BaseTransaction):
+    """ Gets a concrete transaction from a transaction and z3 model"""
+    # Get concrete values from transaction
+    address = hex(transaction.callee_account.address.value)
+    value = model.eval(transaction.call_value.raw, model_completion=True).as_long()
+    caller = "0x" + (
+        "%x" % model.eval(transaction.caller.raw, model_completion=True).as_long()
+    ).zfill(40)
+
+    input_ = ""
+    if isinstance(transaction, ContractCreationTransaction):
+        address = ""
+        input_ += transaction.code.bytecode
+
+    input_ += "".join(
+        [
+            hex(b)[2:] if len(hex(b)) % 2 == 0 else "0" + hex(b)[2:]
+            for b in transaction.call_data.concrete(model)
+        ]
+    )
+
+    # Create concrete transaction dict
+    concrete_transaction = dict()  # type: Dict[str, str]
+    concrete_transaction["input"] = "0x" + input_
+    concrete_transaction["value"] = "0x%x" % value
+    # Fixme: base origin assignment on origin symbol
+    concrete_transaction["origin"] = caller
+    concrete_transaction["address"] = "%s" % address
+
+    return concrete_transaction
+
+
+def _set_minimisation_constraints(
+    transaction_sequence, constraints, minimize, max_size, world_state
+) -> Tuple[Constraints, tuple]:
+    """ Set constraints that minimise key transaction values
+
+    Constraints generated:
+    - Upper bound on calldata size
+    - Minimisation of call value's and calldata sizes
+
+    :param transaction_sequence: Transaction for which the constraints should be applied
+    :param constraints: The constraints array which should contain any added constraints
+    :param minimize: The minimisation array which should contain any variables that should be minimised
+    :param max_size: The max size of the calldata array
+    :return: updated constraints, minimize
+    """
+    for transaction in transaction_sequence:
+        # Set upper bound on calldata size
+        max_calldata_size = symbol_factory.BitVecVal(max_size, 256)
+        constraints.append(UGE(max_calldata_size, transaction.call_data.calldatasize))
+
+        # Minimize
+        minimize.append(transaction.call_data.calldatasize)
+        minimize.append(transaction.call_value)
+        constraints.append(
+            UGE(
+                symbol_factory.BitVecVal(1000000000000000000000, 256),
+                world_state.starting_balances[transaction.caller],
+            )
+        )
+
+    for account in world_state.accounts.values():
+        # Lazy way to prevent overflows and to ensure "reasonable" balances
+        # Each account starts with less than 100 ETH
+        constraints.append(
+            UGE(
+                symbol_factory.BitVecVal(100000000000000000000, 256),
+                world_state.starting_balances[account.address],
+            )
+        )
+
+    return constraints, tuple(minimize)
---
+++ packages/mythril/mythril-0.21.18.tar.gz/mythril-0.21.18/mythril/laser/ethereum/transaction/symbolic.py
@@ -0,0 +1,153 @@
+"""This module contains functions setting up and executing transactions with
+symbolic values."""
+import logging
+
+from mythril.disassembler.disassembly import Disassembly
+from mythril.laser.ethereum.cfg import Node, Edge, JumpType
+from mythril.laser.ethereum.state.account import Account
+from mythril.laser.ethereum.state.calldata import SymbolicCalldata
+from mythril.laser.ethereum.state.world_state import WorldState
+from mythril.laser.ethereum.transaction.transaction_models import (
+    MessageCallTransaction,
+    ContractCreationTransaction,
+    get_next_transaction_id,
+    BaseTransaction,
+)
+from mythril.laser.smt import symbol_factory, Or, BitVec
+
+log = logging.getLogger(__name__)
+
+CREATOR_ADDRESS = 0xAFFEAFFEAFFEAFFEAFFEAFFEAFFEAFFEAFFEAFFE
+ATTACKER_ADDRESS = 0xDEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEF
+
+ACTOR_ADDRESSES = [
+    symbol_factory.BitVecVal(0xAFFEAFFEAFFEAFFEAFFEAFFEAFFEAFFEAFFEAFFE, 256),
+    symbol_factory.BitVecVal(0xDEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEF, 256),
+    symbol_factory.BitVecVal(0xAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA, 256),
+]
+
+
+def execute_message_call(laser_evm, callee_address: BitVec) -> None:
+    """Executes a message call transaction from all open states.
+
+    :param laser_evm:
+    :param callee_address:
+    """
+    # TODO: Resolve circular import between .transaction and ..svm to import LaserEVM here
+    open_states = laser_evm.open_states[:]
+    del laser_evm.open_states[:]
+
+    for open_world_state in open_states:
+        if open_world_state[callee_address].deleted:
+            log.debug("Can not execute dead contract, skipping.")
+            continue
+
+        next_transaction_id = get_next_transaction_id()
+        transaction = MessageCallTransaction(
+            world_state=open_world_state,
+            identifier=next_transaction_id,
+            gas_price=symbol_factory.BitVecSym(
+                "gas_price{}".format(next_transaction_id), 256
+            ),
+            gas_limit=8000000,  # block gas limit
+            origin=symbol_factory.BitVecSym(
+                "origin{}".format(next_transaction_id), 256
+            ),
+            caller=symbol_factory.BitVecSym(
+                "sender_{}".format(next_transaction_id), 256
+            ),
+            callee_account=open_world_state[callee_address],
+            call_data=SymbolicCalldata(next_transaction_id),
+            call_value=symbol_factory.BitVecSym(
+                "call_value{}".format(next_transaction_id), 256
+            ),
+        )
+        _setup_global_state_for_execution(laser_evm, transaction)
+
+    laser_evm.exec()
+
+
+def execute_contract_creation(
+    laser_evm, contract_initialization_code, contract_name=None, world_state=None
+) -> Account:
+    """Executes a contract creation transaction from all open states.
+
+    :param laser_evm:
+    :param contract_initialization_code:
+    :param contract_name:
+    :return:
+    """
+    # TODO: Resolve circular import between .transaction and ..svm to import LaserEVM here
+    del laser_evm.open_states[:]
+
+    world_state = world_state or WorldState()
+    open_states = [world_state]
+    new_account = None
+    for open_world_state in open_states:
+        next_transaction_id = get_next_transaction_id()
+        # call_data "should" be '[]', but it is easier to model the calldata symbolically
+        # and add logic in codecopy/codesize/calldatacopy/calldatasize than to model code "correctly"
+        transaction = ContractCreationTransaction(
+            world_state=open_world_state,
+            identifier=next_transaction_id,
+            gas_price=symbol_factory.BitVecSym(
+                "gas_price{}".format(next_transaction_id), 256
+            ),
+            gas_limit=8000000,  # block gas limit
+            origin=symbol_factory.BitVecSym(
+                "origin{}".format(next_transaction_id), 256
+            ),
+            code=Disassembly(contract_initialization_code),
+            caller=symbol_factory.BitVecVal(CREATOR_ADDRESS, 256),
+            contract_name=contract_name,
+            call_data=None,
+            call_value=symbol_factory.BitVecSym(
+                "call_value{}".format(next_transaction_id), 256
+            ),
+        )
+        _setup_global_state_for_execution(laser_evm, transaction)
+        new_account = new_account or transaction.callee_account
+    laser_evm.exec(True)
+
+    return new_account
+
+
+def _setup_global_state_for_execution(laser_evm, transaction: BaseTransaction) -> None:
+    """Sets up global state and cfg for a transactions execution.
+
+    :param laser_evm:
+    :param transaction:
+    """
+    # TODO: Resolve circular import between .transaction and ..svm to import LaserEVM here
+    global_state = transaction.initial_global_state()
+    global_state.transaction_stack.append((transaction, None))
+
+    global_state.mstate.constraints.append(
+        Or(*[transaction.caller == actor for actor in ACTOR_ADDRESSES])
+    )
+
+    new_node = Node(
+        global_state.environment.active_account.contract_name,
+        function_name=global_state.environment.active_function_name,
+    )
+    if laser_evm.requires_statespace:
+        laser_evm.nodes[new_node.uid] = new_node
+
+    if transaction.world_state.node:
+        if laser_evm.requires_statespace:
+            laser_evm.edges.append(
+                Edge(
+                    transaction.world_state.node.uid,
+                    new_node.uid,
+                    edge_type=JumpType.Transaction,
+                    condition=None,
+                )
+            )
+
+        global_state.mstate.constraints += transaction.world_state.node.constraints
+        new_node.constraints = global_state.mstate.constraints.as_list
+
+    global_state.world_state.transaction_sequence.append(transaction)
+    global_state.node = new_node
+    new_node.states.append(global_state)
+    laser_evm.work_list.append(global_state)
