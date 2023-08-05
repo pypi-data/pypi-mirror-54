dc26c43515d2328adf8db79379fed4c08e8abc67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:01 +0000

    Add package pizdets/0.11/pizdets-0.11.tar.gz

---
+++ packages/pizdets/pizdets-0.11.tar.gz/pizdets-0.11/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pizdets',
+      version='0.11',
+      description='PMachine',
+      url='https://github.com/catalin-rusnac/pizdets',
+      author='me',
+      author_email='crusnac@ist.ac.at',
+      license='MIT',
+      packages=['pizdets',"pizdets.pm"],
+      zip_safe=True)
