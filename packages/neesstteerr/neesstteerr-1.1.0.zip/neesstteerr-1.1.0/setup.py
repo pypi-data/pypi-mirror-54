2ca54f4c27bdc46e8320aee45c52bd380f570298
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:07 +0000

    Add package neesstteerr/1.1.0/neesstteerr-1.1.0.zip

---
+++ packages/neesstteerr/neesstteerr-1.1.0.zip/neesstteerr-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'neesstteerr',
+	version = '1.1.0',
+	py_modules = ['neesstteerr'],
+	author = 'zhjj',
+	author_email = '553848705@qq.com',
+	url = 'http://www.zhjj.com',
+	description = 'A simple printer of nested lists',
+)
