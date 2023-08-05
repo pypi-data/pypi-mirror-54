98ea0f4aa82baa094a80cf80b070b684ef308164
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:45 +0000

    Add package Hextapus/0.0.1/Hextapus-0.0.1.tar.gz

---
+++ packages/Hextapus/Hextapus-0.0.1.tar.gz/Hextapus-0.0.1/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+with open("Readme.md", "r") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+    name = "Hextapus",
+    version = "0.0.1",
+    author = "OddSox",
+)    
