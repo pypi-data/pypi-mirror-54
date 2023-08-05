bc1c88cf82312246db59eec399ecb34bb34ec482
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:10 +0000

    Add package shipmnts-utils/0.0.47/shipmnts_utils-0.0.47-py3-none-any.whl

---
+++ packages/shipmnts-utils/shipmnts_utils-0.0.47-py3-none-any.whl/shipmnts_utils/misc/download.py
@@ -0,0 +1,17 @@
+import os
+
+import requests
+
+
+def serve_request_write_content(url, destination):
+    r = requests.get(url, allow_redirects=True)
+    open(destination + "/" + url.split("/")[-1], "wb").write(r.content)
+
+
+def download_file(source, destination, multiple=False):
+    os.makedirs(destination, exist_ok=True)
+    if multiple:
+        for url in source:
+            serve_request_write_content(url=url, destination=destination)
+    else:
+        serve_request_write_content(url=source, destination=destination)
