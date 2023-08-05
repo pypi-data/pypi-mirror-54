42ea3f074012e12938e8071017b9c497c528668b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:44 +0000

    Add package alteruphono/0.0.1.dev0/alteruphono-0.0.1.dev0-py3-none-any.whl

---
+++ packages/alteruphono/alteruphono-0.0.1.dev0-py3-none-any.whl/alteruphono/__init__.py
@@ -0,0 +1,10 @@
+# __init__.py
+
+# Version of the alteruphono package
+__version__ = "0.0.1dev"
+__author__ = "Tiago Tresoldi"
+__email__ = "tresoldi@shh.mpg.de"
+
+# Build the namespace
+from alteruphono.sound_changer import apply_rule
+from alteruphono import utils
---
+++ packages/alteruphono/alteruphono-0.0.1.dev0-py3-none-any.whl/alteruphono/__main__.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+# Import Python standard libraries
+import random
+
+# Import 3rd party libraries
+from pyclts import TranscriptionSystem
+
+# Import our library
+import alteruphono
+
+
+def main():
+    # import enki
+    # import csv
+    # import random
+
+    # import yvyra
+    # import os.path
+
+    # TODO: write a reader
+    rules = alteruphono.utils.read_sound_changes()
+    bipa = TranscriptionSystem("bipa")
+    sclasses = alteruphono.utils.read_sound_classes()
+    features = alteruphono.utils.read_features()
+
+    # param = {}
+    # vocab = tuple(enki.random_words(10, param))
+    vocab = ("m a m a", "p e p e", "d i t i")
+
+    for i in range(100):
+        rule = random.choice(rules)
+
+        new_vocab = tuple(
+            [
+                alteruphono.apply_rule(word, rule, bipa, sclasses, features)
+                for word in vocab
+            ]
+        )
+
+        if vocab != new_vocab:
+            print(i, rule)
+            print("==", vocab)
+            print("==", new_vocab)
+            print()
+
+
+if __name__ == "__main__":
+    main()
