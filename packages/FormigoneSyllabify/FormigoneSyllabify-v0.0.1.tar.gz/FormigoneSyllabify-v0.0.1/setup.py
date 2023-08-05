7f2759cf11b01f96f74f45ff63ba3234bf59d7b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:14:19 +0000

    Add package FormigoneSyllabify/v0.0.1/FormigoneSyllabify-v0.0.1.tar.gz

---
+++ packages/FormigoneSyllabify/FormigoneSyllabify-v0.0.1.tar.gz/FormigoneSyllabify-v0.0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='FormigoneSyllabify',
+    version='v0.0.1',
+    packages=['formigone', ],
+    license='MIT',
+    author='Rodrigo Silveira',
+    author_email='webmaster@rodrigo-silveira.com',
+    url='https://github.com/formigone/syllabify',
+
+    download_url='https://github.com/formigone/syllabify/archive/v0.0.1.tar.gz',
+)
