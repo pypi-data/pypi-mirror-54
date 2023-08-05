62eedb9f5d42709b7f61f8439a1d82e8c300c3e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:39 +0000

    Add package logwrap/6.3.3/logwrap-6.3.3.tar.gz

---
+++ packages/logwrap/logwrap-6.3.3.tar.gz/logwrap-6.3.3/logwrap/__init__.pyx
@@ -0,0 +1,59 @@
+#    Copyright 2016-2019 Alexey Stepanov aka penguinolog
+##
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import pkg_resources
+
+from logwrap.repr_utils import PrettyFormat, PrettyRepr, PrettyStr, pretty_repr, pretty_str
+
+from logwrap.log_wrap import LogWrap
+from logwrap.log_wrap import logwrap, BoundParameter, bind_args_kwargs
+
+from logwrap.log_on_access import LogOnAccess
+
+cpdef tuple __all__ = (
+    "LogWrap",
+    "logwrap",
+    "PrettyFormat",
+    "PrettyRepr",
+    "PrettyStr",
+    "pretty_repr",
+    "pretty_str",
+    "BoundParameter",
+    "bind_args_kwargs",
+    "LogOnAccess",
+)
+
+cpdef str __version__
+
+try:
+    __version__ = pkg_resources.get_distribution(__name__).version
+except pkg_resources.DistributionNotFound:
+    # package is not installed, try to get from SCM
+    try:
+        import setuptools_scm  # type: ignore
+
+        __version__ = setuptools_scm.get_version()
+    except ImportError:
+        setuptools_scm = None
+
+cpdef str __author__ = "Alexey Stepanov"
+cpdef str __author_email__ = "penguinolog@gmail.com"
+cpdef dict __maintainers__ = {
+    "Alexey Stepanov": "penguinolog@gmail.com",
+    "Antonio Esposito": "esposito.cloud@gmail.com",
+    "Dennis Dmitriev": "dis-xcom@gmail.com",
+}
+cpdef str __url__ = "https://github.com/python-useful-helpers/logwrap"
+cpdef str __description__ = "Decorator for logging function arguments and return value by human-readable way"
+cpdef str __license__ = "Apache License, Version 2.0"
