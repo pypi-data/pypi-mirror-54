d89e9223215b19e5574b47ba6fc962628fceddf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:41 +0000

    Add package ynitdb/0.0.1/ynitdb-0.0.1.tar.gz

---
+++ packages/ynitdb/ynitdb-0.0.1.tar.gz/ynitdb-0.0.1/ynitdb/__init__.py
@@ -0,0 +1,26 @@
+import json
+from pathlib import Path
+
+class Database():
+    def __init__(self, path=None):
+        self._dict = {}
+        if(path):
+            self.path = path
+        else:
+            self.path = str(Path.home()) + "/db.ynit"
+        self.read()
+
+    def read(self):
+        with open(self.path, "r") as f:
+            self._dict = json.load(f)
+
+    def write(self):
+        with open(self.path, "w") as f:
+            f.write(json.dumps(self._dict))
+
+    def __getitem__(self, key):
+        return self._dict[key]
+
+    def __setitem__(self, key, value):
+        self._dict[key] = value
+        self.write()
