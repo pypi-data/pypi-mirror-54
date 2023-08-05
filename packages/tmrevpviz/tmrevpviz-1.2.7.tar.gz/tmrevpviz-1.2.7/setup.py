4f2633de9d7ebae4a126c5db6817ae9141354dea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:20 +0000

    Add package tmrevpviz/1.2.7/tmrevpviz-1.2.7.tar.gz

---
+++ packages/tmrevpviz/tmrevpviz-1.2.7.tar.gz/tmrevpviz-1.2.7/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+ name='tmrevpviz',
+ version='1.2.7',
+ license='MIT',
+ packages=[],
+ package_dir={'' : 'src'},
+ entry_points={
+    'console_scripts': [
+        'tmrevpviz-run=tmrevpviz:run',
+        'tmrevpviz-generate=generate:run'
+    ]
+},
+ py_modules=['tmrevpviz', 'Incident', 'generate'],
+ classifiers=[
+      'Environment :: Console',
+      'Operating System :: Microsoft :: Windows',
+      'Programming Language :: Python',
+  ],
+  install_requires=['datetime',
+                    'matplotlib',
+                    'numpy',
+                    'pandas',
+                    'statistics',
+                    'xlsxwriter']
+)
