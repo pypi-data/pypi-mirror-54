0941a0f2addb70a110c0053d0b5a5304704df9cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:59 +0000

    Add package artellapipe-launcher/0.0.5/artellapipe-launcher-0.0.5.tar.gz

---
+++ packages/artellapipe-launcher/artellapipe-launcher-0.0.5.tar.gz/artellapipe-launcher-0.0.5/artellapipe/launcher/core/__init__.py
@@ -0,0 +1,7 @@
+order = [
+    'artellapipe.launcher.core.defines',
+    'artellapipe.launcher.core.config',
+    'artellapipe.launcher.core.updater',
+    'artellapipe.launcher.core.dccselector',
+    'artellapipe.launcher.core.launcher',
+]
