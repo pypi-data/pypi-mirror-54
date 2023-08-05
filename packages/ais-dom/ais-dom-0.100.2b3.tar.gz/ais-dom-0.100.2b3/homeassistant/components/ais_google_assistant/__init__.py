737230b365ab15c8797e3b3777423112abf238bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:27 +0000

    Add package ais-dom/0.100.2b3/ais-dom-0.100.2b3.tar.gz

---
+++ packages/ais-dom/ais-dom-0.100.2b3.tar.gz/ais-dom-0.100.2b3/homeassistant/components/ais_google_assistant/__init__.py
@@ -0,0 +1,6 @@
+"""
+Support for interacting with Spotify.
+
+For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/media_player.spotify/
+"""
