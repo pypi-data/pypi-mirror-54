cebd85a2cd78b49ff77d9650e1eb6bad2e4e2b50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:03 +0000

    Add package greek-normalisation/0.2.1/greek-normalisation-0.2.1.tar.gz

---
+++ packages/greek-normalisation/greek-normalisation-0.2.1.tar.gz/greek-normalisation-0.2.1/README.md
@@ -0,0 +1,18 @@
+# greek-normalisation
+
+[![Build Status](https://travis-ci.org/jtauber/greek-normalisation.svg)](https://travis-ci.org/jtauber/greek-normalisation)
+[![Coverage Status](https://coveralls.io/repos/github/jtauber/greek-normalisation/badge.svg?branch=master)](https://coveralls.io/github/jtauber/greek-normalisation?branch=master)
+
+utilities for validating and normalising Ancient Greek text
+
+For more of my work on Ancient Greek, see <http://jktauber.com/>.
+
+## Installation
+
+```
+pip install greek-normalisation
+```
+
+## Documentation / Tests
+
+See `tests.rst` for usage examples.
---
+++ packages/greek-normalisation/greek-normalisation-0.2.1.tar.gz/greek-normalisation-0.2.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+
+setup(
+    name="greek-normalisation",
+    version="0.2.1",
+    description="Python 3 utilities for validating and normalising Ancient Greek text",
+    url="http://github.com/jtauber/greek-normalisation",
+    author="James Tauber",
+    author_email="jtauber@jtauber.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    packages=["greek_normalisation"],
+    zip_safe=False,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Text Processing :: Linguistic",
+    ],
+)
