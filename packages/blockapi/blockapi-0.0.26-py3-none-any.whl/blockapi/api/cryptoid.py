f6fb00d2f716ad5645122403ed755741c0422cb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:39 +0000

    Add package blockapi/0.0.26/blockapi-0.0.26-py3-none-any.whl

---
+++ packages/blockapi/blockapi-0.0.26-py3-none-any.whl/blockapi/api/cryptoid.py
@@ -0,0 +1,47 @@
+from blockapi.services import (
+    BlockchainAPI
+)
+
+
+class CryptoIDAPI(BlockchainAPI):
+    """
+    Multi coins
+    API docs: https://chainz.cryptoid.info/api.dws
+    Explorer: https://chainz.cryptoid.info
+    """
+
+    active = True
+
+    symbol = None
+    base_url = 'http://chainz.cryptoid.info'
+    rate_limit = 0
+    coef = 1e-8
+    max_items_per_page = None
+    page_offset_step = None
+    confirmed_num = None
+
+    supported_requests = {
+        'get_balance': '/{symbol}/api.dws?q=getbalance&a={address}&key={api_key}',
+    }
+
+    def get_balance(self):
+        response = self.request('get_balance',
+                                api_key=self.api_key,
+                                symbol=self.symbol.lower(),
+                                address=self.address)
+
+        return response * self.coef
+
+
+class CryptoIDLitecoinAPI(CryptoIDAPI):
+    symbol = 'LTC'
+
+
+class CryptoIDDashAPI(CryptoIDAPI):
+    symbol = 'DASH'
+    coef = 1
+
+
+class CryptoIDGroestlcoinAPI(CryptoIDAPI):
+    symbol = 'GRS'
+    coef = 1
