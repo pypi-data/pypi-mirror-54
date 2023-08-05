3ba5793dc0c0be94400dac2b0720daae4d8eb53c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:47 +0000

    Add package selective-linter/1.0.0/selective_linter-1.0.0-py3-none-any.whl

---
+++ packages/selective-linter/selective_linter-1.0.0-py3-none-any.whl/selective_linter/output.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import hashlib
+import os
+
+class LintError:
+
+    def __init__(self, error):
+        split_error = error.split(":")
+        self.file = split_error[0]
+        self.line = split_error[1]
+        self.character = split_error[2]
+        self.error_type = split_error[3]
+        self.description = split_error[4]
+        aux_length = len(":".join(split_error[:5])) + 1
+        self.code = error[aux_length:]
+
+    def __str__(self):
+        return self.file + ":" + self.line + ": warning:" + self.description
