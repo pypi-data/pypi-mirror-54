744ca419653a3659ac449e9eaab9243f1d781e96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:44 +0000

    Add package jk-asyncio-syncasync/0.2019.10.19/jk_asyncio_syncasync-0.2019.10.19.tar.gz

---
+++ packages/jk-asyncio-syncasync/jk_asyncio_syncasync-0.2019.10.19.tar.gz/jk_asyncio_syncasync-0.2019.10.19/jk_asyncio_syncasync/__init__.py
@@ -0,0 +1,24 @@
+﻿
+
+
+__version__ = "0.2019.10.19"
+
+
+from .asyncio_sync import call_sync, make_async
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ packages/jk-asyncio-syncasync/jk_asyncio_syncasync-0.2019.10.19.tar.gz/jk_asyncio_syncasync-0.2019.10.19/jk_asyncio_syncasync/asyncio_sync.py
@@ -0,0 +1,40 @@
+﻿
+
+
+__version__ = "0.2019.10.19"
+
+
+
+import concurrent.futures
+import asyncio
+
+
+
+
+executor = concurrent.futures.ThreadPoolExecutor(max_workers=32)
+loop = asyncio.get_event_loop()
+
+
+
+async def call_sync(synchroneousCallable, *args, **kwargs):
+	assert callable(synchroneousCallable)
+
+	result = await loop.run_in_executor(executor, synchroneousCallable, *args, **kwargs)
+	return result
+#
+
+
+
+
+
+def make_async(synchroneousCallable):
+	assert callable(synchroneousCallable)
+
+	async def func_wrapper(*args, **kwargs):
+		result = await loop.run_in_executor(executor, synchroneousCallable, *args, **kwargs)
+		return result
+	#
+
+	return func_wrapper
+#
+
