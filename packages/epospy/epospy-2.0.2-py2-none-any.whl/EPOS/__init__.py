6633948fe8d2a762953c7302da9e91c0a72d3d88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:37:52 +0000

    Add package epospy/2.0.2/epospy-2.0.2-py2-none-any.whl

---
+++ packages/epospy/epospy-2.0.2-py2-none-any.whl/EPOS/__init__.py
@@ -0,0 +1,12 @@
+__shortversion__= u'2.0'
+__version__= u'2.0.2' # [N!]N(.N)*[{a|b|rc}N][.postN][.devN]
+
+__all__ = ['epos','fitparameters','kepler','rv','run','population','plot','occurrence',
+	'fitfunctions','pfmodel','massradius','regression','multi','analytics','save',
+	'scripts', 'cgs']
+
+import kepler, rv, run, plot, occurrence, population
+import fitfunctions, pfmodel, regression, massradius, multi, analytics, save
+import scripts, cgs
+
+from classes import epos, fitparameters
