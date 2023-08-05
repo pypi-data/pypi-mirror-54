4b26cea1cd8ca6093a9a80df263410d7b11f3bd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:13 +0000

    Add package flask-2-ui5-py/0.0.1/flask_2_ui5_py-0.0.1-py3-none-any.whl

---
+++ packages/flask-2-ui5-py/flask_2_ui5_py-0.0.1-py3-none-any.whl/flask_2_ui5_py/__init__.py
@@ -0,0 +1,5 @@
+from .json_util import make_dict_values_json_serializable, make_list_items_json_serializable, treat_non_serializable
+from .message_util import get_message, MessageType, throw_exception
+
+name = "msg_flsak_2_ui5_py"
+
