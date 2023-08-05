ee7cb7fc7636b4600e9947d500dabf0790ea132f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:14 +0000

    Add package mitos/2.0.2/mitos-2.0.2.tar.gz

---
+++ packages/mitos/mitos-2.0.2.tar.gz/mitos-2.0.2/mitos/__init__.py
@@ -0,0 +1,5 @@
+name = "mitos"
+__version__ = "2.0.2"
+
+__all__ = ['bedfile', 'blast', 'codon', 'draw', 'extprog', 'feature', 'gb', 'gfffile', 'mergefeature', 'mitfi',
+           'mito', 'mitofile', 'rna', 'sequence', 'tax', 'tbl', 'trna', 'update', 'webserver']
