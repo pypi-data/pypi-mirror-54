2df4bdaa2ecfb084625b8d4d23f394725a0ec159
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:47 +0000

    Add package sphero-sdk/0.3.4.post1/sphero_sdk-0.3.4.post1.tar.gz

---
+++ packages/sphero-sdk/sphero_sdk-0.3.4.post1.tar.gz/sphero_sdk-0.3.4.post1/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from setuptools import find_packages
+from distutils.core import setup
+
+setup(
+	name='sphero_sdk',
+	version='0.3.4.post1',  # TODO: unify with __version__.py
+	description='Sphero SDK to run on Raspberry Pi using Python',
+	author='Anthony Vizcarra',
+	author_email='Anthony@sphero.com',
+	url='https://github.com/sphero-inc/sphero-sdk-raspberrypi-python',
+	packages=find_packages(include=['sphero_sdk*'])
+)
