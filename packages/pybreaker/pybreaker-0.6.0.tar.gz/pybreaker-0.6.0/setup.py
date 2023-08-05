bcddf735e716f85f8dc80ab31ca9ed94d73ccf7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:51 +0000

    Add package pybreaker/0.6.0/pybreaker-0.6.0.tar.gz

---
+++ packages/pybreaker/pybreaker-0.6.0.tar.gz/pybreaker-0.6.0/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='pybreaker',
+    version='0.6.0',
+    description='Python implementation of the Circuit Breaker pattern',
+    long_description=open('README.rst', 'r').read(),
+    keywords=['design', 'pattern', 'circuit', 'breaker', 'integration'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries',
+    ],
+    platforms=[
+        'Any',
+    ],
+    license='BSD',
+    author='Daniel Fernandes Martins',
+    author_email='daniel.tritone@gmail.com',
+    url='http://github.com/danielfm/pybreaker',
+    package_dir={'': 'src'},
+    py_modules=['pybreaker'],
+    install_requires=[
+        'six',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    test_suite='tests',
+    tests_require=['mock', 'fakeredis==0.16.0', 'redis==2.10.6', 'tornado'],
+)
