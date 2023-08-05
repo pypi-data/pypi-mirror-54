e60a4257e9cdb7e989030fd6fb4cd2dd83818afa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:38:42 +0000

    Add package neurocombat-sklearn/0.1.2a0/neurocombat-sklearn-0.1.2a0.tar.gz

---
+++ packages/neurocombat-sklearn/neurocombat-sklearn-0.1.2a0.tar.gz/neurocombat-sklearn-0.1.2a0/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='neurocombat-sklearn',
+    version='0.1.2a',
+    author='Walter Hugo Lopez Pinaya',
+    description='Harmonizing neuroimaging data across sites. Implementation of neurocombat using sklearn format',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Warvito/combat-sklearn",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
