bf4c04342afc4314da61613b4945ed6a9823cebc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:44 +0000

    Add package tp-py-common/1.0.42/tp_py_common-1.0.42-py3-none-any.whl

---
+++ packages/tp-py-common/tp_py_common-1.0.42-py3-none-any.whl/tp/test/db/seeds_test/Patient.d332.sql
@@ -0,0 +1,4 @@
+INSERT INTO `patients` (`id`, `name`, `dob`, `url_token`, `created_at`, `updated_at`, `user_id`, `customer_id`, `pioneer_rx_id`, `ssn`, `drivers_license_number`, `dob_obj`, `first_name`, `last_name`, `created_by`, `gender`, `hipaa_packet_accepted`, `middle_name`, `email`, `phone`, `zip`) 
+VALUES 
+('10000001','Keira Langworth','20180829','1e53e5','2019-10-10 23:40:56','2019-10-10 23:40:56',NULL,NULL,NULL,NULL,NULL,'2018-08-29 00:00:00','Keira','Langworth',NULL,'male',NULL,NULL,NULL,NULL,NULL),
+('10000003','Sanford Grant','19920922','aafe1c','2019-10-10 23:40:56','2019-10-10 23:40:56',NULL,NULL,NULL,NULL,NULL,'1992-09-22 00:00:00','Sanford','Grant',NULL,'female',NULL,NULL,NULL,NULL,NULL)
