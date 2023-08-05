127605eb36bbc757ea18187fc52ddb948c74df27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:26 +0000

    Add package fmcw/3.2.2/fmcw-3.2.2-py3-none-any.whl

---
+++ packages/fmcw/fmcw-3.2.2-py3-none-any.whl/fmcw/__init__.py
@@ -0,0 +1,10 @@
+from os.path import dirname, basename, isfile, join, abspath, split
+import glob
+
+path_glob = join(dirname(abspath(__file__)), '*.py')
+modules = glob.glob(path_glob)
+module_blacklist = ['__init__.py', 'setup.py', '190903_rt_fmcw3.py']
+
+__name__ = "fmcw"
+__version__ = "3.2.2"
+__all__ = [basename(f)[:-3] for f in modules if isfile(f) and split(f)[1] not in module_blacklist]
