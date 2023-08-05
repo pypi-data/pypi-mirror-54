147929c1e3e4672cb7b4faf12e31fec8d4a7987c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:47 +0000

    Add package py-dwarf/0.2.2b1/py-dwarf-0.2.2b1.tar.gz

---
+++ packages/py-dwarf/py-dwarf-0.2.2b1.tar.gz/py-dwarf-0.2.2b1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+about = {}
+with open("pydwarf/__about__.py") as fp:
+    exec(fp.read(), about)
+
+setup(name=about["__title__"],
+      version=about["__version__"],
+      description=about["__summary__"],
+      url="https://github.com/zcutlip/py-dwarf-parser",
+      packages=find_packages(),
+      python_requires='>=3.7',
+      install_requires=[]
+      )
