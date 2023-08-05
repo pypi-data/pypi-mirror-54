075443f5f5c061997000d4b3fd83806b53dd8713
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:59 +0000

    Add package xtream/0.5.3/xtream-0.5.3.tar.gz

---
+++ packages/xtream/xtream-0.5.3.tar.gz/xtream-0.5.3/setup.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    README = readme_file.read()
+
+with open('HISTORY.md') as history_file:
+    HISTORY = history_file.read()
+
+setup_args = dict(
+    name='xtream',
+    version='0.5.3',
+    description='nstream streaming platform',
+    long_description_content_type="text/markdown",
+    long_description=README + '\n\n' + HISTORY,
+    license='MIT',
+    packages=find_packages(),
+    author='Vladmir Impala',
+    author_email='your@email.com',
+    keywords=['NStream', 'IPTV Platform', 'Streaming Platform'],
+    url='https://github.com/xtream/xtream',
+    download_url='https://pypi.org/project/xtream/',
+    namespace_packages=[],
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'nstream = xtream.main:main'
+        ]
+    },
+)
+
+install_requires = [
+        "cliff",
+        "requests",
+        "requests",
+        "django",
+        "py-cpuinfo",
+        "celery",
+        "redis",
+        "dj-static",
+        "static3",
+        "gunicorn",
+        "uWSGI",
+        "django-ratelimit",
+        "django-crispy-forms",
+        "iso8601"       
+]
+
+if __name__ == '__main__':
+    setup(**setup_args, install_requires=install_requires)
