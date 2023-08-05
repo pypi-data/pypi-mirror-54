f1b384a9753744161fc61723a19082f0abde5138
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:28 +0000

    Add package pipdeps/0.0.4/pipdeps-0.0.4.tar.gz

---
+++ packages/pipdeps/pipdeps-0.0.4.tar.gz/pipdeps-0.0.4/setup.py
@@ -0,0 +1,41 @@
+"""
+pipdeps setup
+"""
+from distutils.core import setup
+import setuptools  # noqa
+#from setuptools import setup, find_packages
+
+setup(
+    name='pipdeps',
+
+    description='Pipdeps shows/upgrades outdated packages with respect to existing \
+                     dependencies.',
+    classifiers=[
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+
+    author='IT4Innovations',
+    author_email='support@it4i.cz',
+    url='https://code.it4i.cz/sccs/pip-deps',
+    license='GPLv3+',
+    packages=setuptools.find_packages(),
+    namespace_packages=['pipdeps'],
+    include_package_data=True,
+    zip_safe=False,
+    version_format='{tag}',
+    long_description_markdown_filename='README.md',
+    setup_requires=['mustache', 'pystache', 'setuptools-git-version', 'setuptools-markdown'],
+    install_requires=[
+        'packaging',
+        'tabulate',
+        'wheel',
+    ],
+    entry_points={
+        'console_scripts': [
+            'pipdeps = pipdeps.pipdeps:main',
+        ]
+    }
+)
