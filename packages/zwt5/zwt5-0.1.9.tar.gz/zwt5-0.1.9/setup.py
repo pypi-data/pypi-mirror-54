1afdeecdc1e72633762bdd265eacede95ddc54a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:33 +0000

    Add package zwt5/0.1.9/zwt5-0.1.9.tar.gz

---
+++ packages/zwt5/zwt5-0.1.9.tar.gz/zwt5-0.1.9/setup.py
@@ -0,0 +1,14 @@
+# !/usr/bin/env python
+
+from setuptools import setup
+setup(name='zwt5',
+version='0.1.9',
+description='A silly package',
+author='Wentao Zheng',
+author_email='nkzhengwt@outlook.com',
+url='',
+packages=['zwt5', 'zwt5.adv'],
+install_requires=['numpy','pandas'],
+license = 'MIT',
+python_requires='>=3',
+)
