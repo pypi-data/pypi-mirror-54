fa278f67bf30e7d364ce4f3cef134315a401ebd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:59 +0000

    Add package nc-time-axis/1.2.0/nc_time_axis-1.2.0-py3-none-any.whl

---
+++ packages/nc-time-axis/nc_time_axis-1.2.0-py3-none-any.whl/nc_time_axis/tests/integration/test_plot.py
@@ -0,0 +1,46 @@
+"""Integration test for plotting data with non-gregorian calendar."""
+
+from __future__ import (absolute_import, division, print_function)
+from six.moves import (filter, input, map, range, zip)  # noqa
+
+import unittest
+
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt  # nopep8
+import cftime  # nopep8
+import numpy as np  # nopep8
+
+import nc_time_axis  # nopep8
+
+
+class Test(unittest.TestCase):
+    def setUp(self):
+        # Make sure we have no unclosed plots from previous tests before
+        # generating this one.
+        plt.close('all')
+
+    def tearDown(self):
+        # If a plotting test bombs out it can leave the current figure
+        # in an odd state, so we make sure it's been disposed of.
+        plt.close('all')
+
+    def test_360_day_calendar_CalendarDateTime(self):
+        datetimes = [cftime.datetime(1986, month, 30)
+                     for month in range(1, 6)]
+        cal_datetimes = [nc_time_axis.CalendarDateTime(dt, '360_day')
+                         for dt in datetimes]
+        line1, = plt.plot(cal_datetimes)
+        result_ydata = line1.get_ydata()
+        np.testing.assert_array_equal(result_ydata, cal_datetimes)
+
+    def test_360_day_calendar_raw_dates(self):
+        datetimes = [cftime.Datetime360Day(1986, month, 30)
+                     for month in range(1, 6)]
+        line1, = plt.plot(datetimes)
+        result_ydata = line1.get_ydata()
+        np.testing.assert_array_equal(result_ydata, datetimes)
+
+
+if __name__ == "__main__":
+    unittest.main()
