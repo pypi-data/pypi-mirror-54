0cbdcbecf12e8145986acd95577c3bc75eb71d7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:19 +0000

    Add package spkcspider/0.20.2/spkcspider-0.20.2.tar.gz

---
+++ packages/spkcspider/spkcspider-0.20.2.tar.gz/spkcspider-0.20.2/spkcspider/apps/spider/templatetags/spider_base.py
@@ -0,0 +1,114 @@
+from django import template
+from django.urls import reverse
+from django.utils import timezone
+from django.urls.exceptions import NoReverseMatch
+
+register = template.Library()
+
+
+@register.filter()
+def is_not_or_space(value):
+    if not value:
+        return True
+    return isinstance(value, str) and value.isspace()
+
+
+@register.simple_tag(takes_context=True)
+def shall_show_content(context, content, priority=0):
+    return (
+        not content.is_hidden or
+        (context["request"].is_owner and content.priority > priority) or
+        "_unlisted" in context["request"].GET.getlist("search") or
+        "_unlisted" in context["request"].POST.getlist("search")
+    )
+
+
+@register.simple_tag(takes_context=True)
+def themed_media(context):
+    # TODO: implement, just stub
+    # uc = context.get("source", context["uc"])
+    # media = uc.theme.replace(context.get("media"))
+    return context.get("media")
+
+
+@register.simple_tag(takes_context=True)
+def current_url(context):
+    try:
+        return reverse(
+            "{}:{}".format(
+                context["request"].resolver_match.namespace,
+                context["request"].resolver_match.url_name,
+            ),
+            args=context["request"].resolver_match.args,
+            kwargs=context["request"].resolver_match.kwargs
+        )
+    except NoReverseMatch:
+        return reverse(
+            context["request"].resolver_match.url_name,
+            args=context["request"].resolver_match.args,
+            kwargs=context["request"].resolver_match.kwargs
+        )
+
+
+@register.simple_tag(takes_context=True)
+def list_own_content(context):
+    ucname = "index"
+    if context["request"].user.is_authenticated:
+        return context["request"].user.usercomponent_set.get(
+            name=ucname
+        ).get_absolute_url()
+    return ""
+
+
+@register.simple_tag(takes_context=True)
+def update_component(context, name):
+    if context["request"].user.is_authenticated:
+        uc = context["request"].user.usercomponent_set.only("token").get(
+            name=name
+        )
+        return reverse("spider_base:ucomponent-update", kwargs={
+            "token": uc.token
+        })
+    return ""
+
+
+@register.simple_tag(takes_context=True)
+def view_component(context, name):
+    if context["request"].user.is_authenticated:
+        uc = context["request"].user.usercomponent_set.only("token").get(
+            name=name
+        )
+        return reverse("spider_base:ucontent-list", kwargs={
+            "token": uc.token
+        })
+    return ""
+
+
+@register.simple_tag(takes_context=True)
+def reverse_get(context, name, **kwargs):
+    """ Works only if hostpart and sanitized_GET is available """
+    return "{}{}?{}".format(
+        context["hostpart"],
+        reverse(name, kwargs=kwargs),
+        context["sanitized_GET"]
+    )
+
+
+@register.simple_tag()
+def concat_string(*args):
+    return "".join(args)
+
+
+@register.simple_tag()
+def expires_delta(expires):
+    return expires-timezone.now()
+
+
+@register.simple_tag()
+def token_expires(usercomponent, token):
+    return token.created + usercomponent.token_duration
+
+
+@register.simple_tag(takes_context=True)
+def active_feature_names(context):
+    return context["active_features"].values_list("name", flat=True)
---
+++ packages/spkcspider/spkcspider-0.20.2.tar.gz/spkcspider-0.20.2/spkcspider/apps/spider_accounts/views.py
@@ -0,0 +1,103 @@
+__all__ = (
+    "SignupView", "LoginView", "LogoutView", "UpdateView",
+    "PasswordChangeView", "PasswordChangeDoneView", "SignupDoneView"
+)
+
+from django.http import HttpResponseRedirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.views import (
+    LoginView as _LoginView,
+    LogoutView as _LogoutView,
+    PasswordChangeView as _PasswordChangeView,
+    PasswordChangeDoneView as _PasswordChangeDoneView
+)
+from django.views.generic.edit import FormView, UpdateView
+from django.views.generic.base import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.decorators.cache import never_cache
+from django.utils.decorators import method_decorator
+from django.urls import reverse_lazy
+from django.contrib.auth import get_user_model
+from django.conf import settings
+
+from .forms import SignupForm, UserUpdateForm
+from spkcspider.apps.spider.forms import SpiderAuthForm
+from spkcspider.apps.spider.views import DefinitionsMixin
+
+# Create your views here.
+
+
+class PasswordChangeView(DefinitionsMixin, _PasswordChangeView):
+    pass
+
+
+class PasswordChangeDoneView(DefinitionsMixin, _PasswordChangeDoneView):
+    pass
+
+
+class SignupView(DefinitionsMixin, FormView):
+    template_name = 'registration/signup.html'
+    form_class = SignupForm
+    success_url = reverse_lazy("auth:signup_done")
+
+    def form_valid(self, form):
+        form.save()
+        username = form.cleaned_data.get('username')
+        raw_password = form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=raw_password)
+        login(self.request, user)
+        return HttpResponseRedirect(self.get_success_url())
+
+    def get_success_url(self):
+        if "next" in self.request.GET:
+            return self.request.GET["next"]
+        return self.success_url
+
+
+class SignupDoneView(DefinitionsMixin, TemplateView):
+    template_name = 'registration/thanks.html'
+
+
+class LoginView(DefinitionsMixin, _LoginView):
+    form_class = SpiderAuthForm
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return HttpResponseRedirect(self.get_success_url())
+        return super().get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        """Security check complete. Log the user in."""
+        # backend for loging in, select the first
+        backend = settings.AUTHENTICATION_BACKENDS[0]
+        login(
+            self.request,
+            form.get_user(),
+            backend
+        )
+        return HttpResponseRedirect(self.get_success_url())
+
+    def form_invalid(self, form):
+        form.reset_protections()
+        return super().form_invalid(form)
+
+
+class LogoutView(DefinitionsMixin, _LogoutView):
+    @method_decorator(never_cache)
+    def dispatch(self, request, *args, **kwargs):
+        ret = super().dispatch(request, *args, **kwargs)
+        ret["Clear-Site-Data"] = "*"
+        return ret
+
+
+class UpdateView(DefinitionsMixin, LoginRequiredMixin, UpdateView):
+    success_url = reverse_lazy('auth:profile')
+    template_name = 'registration/profile.html'
+    form_class = UserUpdateForm
+
+    def get_object(self, queryset=None):
+        # clone user object
+        return get_user_model().objects.get(pk=self.request.user.pk)
