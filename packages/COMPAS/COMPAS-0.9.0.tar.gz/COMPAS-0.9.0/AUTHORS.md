abe419a8f2826f30a4b89d8a3356ab3de225f3bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:14:16 +0000

    Add package COMPAS/0.9.0/COMPAS-0.9.0.tar.gz

---
+++ packages/COMPAS/COMPAS-0.9.0.tar.gz/COMPAS-0.9.0/AUTHORS.md
@@ -0,0 +1,21 @@
+# Credits
+
+## Project Lead
+
+- Tom Van Mele <<van.mele@arch.ethz.ch>> [@brgcode](https://github.com/brgcode), [@tomvanmele](https://github.com/tomvanmele)
+
+## Main Developers
+
+- Gonzalo Casas <<casas@arch.ethz.ch>> [@gonzalocasas](https://github.com/gonzalocasas)
+- Andrew Liew <<a.liew@sheffield.ac.uk>> [@andrewliew](https://github.com/andrewliew)
+- Tomás Méndez Echenagucia <<mendez@arch.ethz.ch>> [@tmsmendez](https://github.com/tmsmendez)
+- Matthias Rippmann <<rippmann@arch.ethz.ch>> [@Rippmann](https://github.com/Rippmann)
+- Romana Rust <<rust@arch.ethz.ch>> [@romanarust](https://github.com/romanarust)
+
+## Contributors
+
+- Kathrin Dörfler <<doerfler@arch.ethz.ch>> [@Kathrin3010](https://github.com/Kathrin3010)
+- Robin Oval <<oval@arch.ethz.ch>> [@robin-oval](https://github.com/robin-oval)
+- Rafael Pastrana <<pastrana@arch.ethz.ch>> [@arpastrana](https://github.com/arpastrana)
+- Achilleas Xydis <<xydis@arch.ethz.ch>> [@Achillx](https://github.com/Achillx)
+- Elitsa Dimitrova <<e.dimitrova77@gmail.com>> [@elidim](https://github.com/elidim)
---
+++ packages/COMPAS/COMPAS-0.9.0.tar.gz/COMPAS-0.9.0/src/compas_rhino/install.py
@@ -0,0 +1,151 @@
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+
+import io
+import importlib
+import os
+import sys
+
+import compas_rhino
+
+from compas._os import system
+from compas._os import remove_symlinks
+from compas._os import create_symlinks
+
+
+__all__ = ['install']
+
+
+INSTALLABLE_PACKAGES = ['compas', 'compas_rhino']
+
+if system == 'win32':
+    INSTALLABLE_PACKAGES.append('compas_ghpython')
+
+
+def _get_package_path(package):
+    return os.path.abspath(os.path.dirname(package.__file__))
+
+
+def _get_bootstrapper_data(compas_bootstrapper):
+    data = {}
+
+    if not os.path.exists(compas_bootstrapper):
+        return data
+
+    content = io.open(compas_bootstrapper, encoding='utf8').read()
+    exec(content, data)
+
+    return data
+
+
+def install(version=None, packages=None):
+    """Install COMPAS for Rhino.
+
+    Parameters
+    ----------
+    version : {'5.0', '6.0'}, optional
+        The version number of Rhino.
+        Default is ``'6.0'``.
+    packages : list of str, optional
+        List of packages to install or None to use default package list.
+        Default is ``['compas', 'compas_rhino', 'compas_ghpython']``.
+
+    Examples
+    --------
+    .. code-block:: python
+
+        >>> import compas_rhino
+        >>> compas_rhino.install('6.0')
+
+    .. code-block:: python
+
+        $ python -m compas_rhino.install -v 6.0
+
+    """
+    if version not in ('5.0', '6.0'):
+        version = '6.0'
+
+    if system == 'win32':
+        print('Installing COMPAS packages to Rhino {0} IronPython lib:'.format(version))
+    elif system == 'darwin':
+        print('Installing COMPAS packages to Rhino IronPython lib.')
+
+    if not packages:
+        packages = INSTALLABLE_PACKAGES
+
+    ipylib_path = compas_rhino._get_ironpython_lib_path(version)
+    print('IronPython location: {}'.format(ipylib_path))
+    print()
+
+    results = []
+    symlinks = []
+    exit_code = 0
+
+    for package in packages:
+        package_path = _get_package_path(importlib.import_module(package))
+        symlink_path = os.path.join(ipylib_path, package)
+        symlinks.append((package_path, symlink_path))
+
+    removal_results = remove_symlinks([link[1] for link in symlinks])
+
+    for package, success in zip(packages, removal_results):
+        if not success:
+            results.append((package, 'ERROR: Cannot remove symlink, try to run as administrator.'))
+
+    create_results = create_symlinks(symlinks)
+
+    for package, success in zip(packages, create_results):
+        result = 'OK' if success else 'ERROR: Cannot create symlink, try to run as administrator.'
+        results.append((package, result))
+
+    if not all(create_results):
+        exit_code = -1
+
+    if exit_code == -1:
+        results.append(('compas_bootstrapper', 'WARNING: One or more packages failed, will not install bootstrapper, try uninstalling first'))
+    else:
+        # Take either the CONDA environment directory or the current Python executable's directory
+        python_directory = os.environ.get('CONDA_PREFIX', None) or os.path.dirname(sys.executable)
+        environment_name = os.environ.get('CONDA_DEFAULT_ENV', '')
+        compas_bootstrapper = os.path.join(ipylib_path, 'compas_bootstrapper.py')
+
+        try:
+            bootstrapper_data = _get_bootstrapper_data(compas_bootstrapper)
+            installed_packages = bootstrapper_data.get('INSTALLED_PACKAGES', [])
+            installed_packages = list(set(installed_packages + list(packages)))
+
+            with open(compas_bootstrapper, 'w') as f:
+                f.write('ENVIRONMENT_NAME = r"{}"\n'.format(environment_name))
+                f.write('PYTHON_DIRECTORY = r"{}"\n'.format(python_directory))
+                f.write('INSTALLED_PACKAGES = {}'.format(repr(installed_packages)))
+                results.append(('compas_bootstrapper', 'OK'))
+        except:  # noqa: E722
+            results.append(('compas_bootstrapper', 'ERROR: Could not create compas_bootstrapper to auto-determine Python environment'))
+
+    for package, status in results:
+        print('   {} {}'.format(package.ljust(20), status))
+
+        if status != 'OK':
+            exit_code = -1
+
+    print('\nCompleted.')
+    sys.exit(exit_code)
+
+
+# ==============================================================================
+# Main
+# ==============================================================================
+
+if __name__ == "__main__":
+
+    import argparse
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-v', '--version', choices=['5.0', '6.0'], default='6.0', help="The version of Rhino to install the packages in.")
+    parser.add_argument('-p', '--packages', nargs='+', help="The packages to install.")
+
+    args = parser.parse_args()
+
+    install(version=args.version, packages=args.packages)
