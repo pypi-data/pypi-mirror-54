c70eb76f0d3a410b51b9ce31096ac3b2b659fb23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:43 +0000

    Add package dgPre/0.1.3/dgPre-0.1.3.tar.gz

---
+++ packages/dgPre/dgPre-0.1.3.tar.gz/dgPre-0.1.3/setup.py
@@ -0,0 +1,31 @@
+#-*- encoding: UTF-8 -*-
+from setuptools import setup, find_packages
+"""
+打包的用的setup必须引入，
+"""
+
+VERSION = '0.1.3'
+
+setup(name='dgPre',
+      version=VERSION,
+      description="a tiny and smart cli player of douyutv,ximalayad,anmu based on Python",
+      long_description='just enjoy',
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='python douyu danmu danmu.fm terminal',
+      author='viton',
+      author_email='viton@gmail.com',
+      url='https://github.com/viton/doumu.fm',
+      license='MIT',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+        'requests',
+        'click'
+      ],
+      entry_points={
+        'console_scripts':[
+            'sgpre = dgPre.Yuepre'
+        ]
+      },
+)
