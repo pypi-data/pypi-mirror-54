38c9d813883d18327555183e71e14801c58e2ecf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:37 +0000

    Add package shaka-streamer/0.3.0/shaka_streamer-0.3.0-py3-none-any.whl

---
+++ packages/shaka-streamer/shaka_streamer-0.3.0-py3-none-any.whl/streamer/controller_node.py
@@ -0,0 +1,270 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Top-level module API.
+
+If you'd like to import Shaka Streamer as a Python module and build it into
+your own application, this is the top-level API you can use for that.  You may
+also want to look at the source code to the command-line front end script
+`shaka-streamer`.
+"""
+
+
+import os
+import re
+import shutil
+import string
+import subprocess
+import tempfile
+import uuid
+
+from . import bitrate_configuration
+from . import cloud_node
+from . import external_command_node
+from . import input_configuration
+from . import node_base
+from . import output_stream
+from . import packager_node
+from . import pipeline_configuration
+from . import transcoder_node
+
+# Alias a few classes to avoid repeating namespaces later.
+BitrateConfig = bitrate_configuration.BitrateConfig
+
+InputConfig = input_configuration.InputConfig
+InputType = input_configuration.InputType
+MediaType = input_configuration.MediaType
+
+AudioOutputStream = output_stream.AudioOutputStream
+TextOutputStream = output_stream.TextOutputStream
+VideoOutputStream = output_stream.VideoOutputStream
+
+PipelineConfig = pipeline_configuration.PipelineConfig
+StreamingMode = pipeline_configuration.StreamingMode
+
+
+class ControllerNode(object):
+  """Controls all other nodes and manages shared resources."""
+
+  def __init__(self):
+    global_temp_dir = tempfile.gettempdir()
+
+    # The docs state that if any of prefix, suffix, or dir are specified, all
+    # must be specified (and not None).  Create a temp dir of our own, inside
+    # the global temp dir, and with a name that indicates who made it.
+    self._temp_dir = tempfile.mkdtemp(
+        dir=global_temp_dir, prefix='shaka-live-', suffix='')
+
+    self._nodes = []
+
+  def __del__(self):
+    # Clean up named pipes by removing the temp directory we placed them in.
+    shutil.rmtree(self._temp_dir)
+
+  def __enter__(self):
+    return self
+
+  def __exit__(self, *unused_args):
+    self.stop()
+
+  def _create_pipe(self):
+    """Create a uniquely-named named pipe in the node's temp directory.
+
+    Raises:
+      RuntimeError: If the platform doesn't have mkfifo.
+    Returns:
+      The path to the named pipe, as a string.
+    """
+
+    # TODO(#8): mkfifo only works on Unix.  We would need a special case for a
+    # Windows port some day.
+
+    if not hasattr(os, 'mkfifo'):
+      raise RuntimeError('Platform not supported due to lack of mkfifo')
+
+    # Since the tempfile module creates actual files, use uuid to generate a
+    # filename, then call mkfifo to create the named pipe.
+    unique_name = str(uuid.uuid4())
+    path = os.path.join(self._temp_dir, unique_name)
+
+    readable_by_owner_only = 0o600  # Unix permission bits
+    os.mkfifo(path, mode=readable_by_owner_only)
+
+    return path
+
+  def start(self, output_dir,
+            input_config_dict, pipeline_config_dict,
+            bitrate_config_dict={},
+            bucket_url=None):
+    """Create and start all other nodes.
+
+    :raises: `RuntimeError` if the controller has already started.
+    :raises: :class:`streamer.configuration.ConfigError` if the configuration is
+             invalid.
+    """
+
+    if self._nodes:
+      raise RuntimeError('Controller already started!')
+
+    # Check that ffmpeg version is 4.1 or above.
+    _check_version('FFmpeg', ['ffmpeg', '-version'], (4, 1))
+
+    # Check that ffprobe version (used for autodetect features) is 4.1 or above.
+    _check_version('ffprobe', ['ffprobe', '-version'], (4, 1))
+
+    # Check that Shaka Packager version is 2.1 or above.
+    _check_version('Shaka Packager', ['packager', '-version'], (2, 1))
+
+    if bucket_url:
+      # Check that the Google Cloud SDK is at least v254, which introduced
+      # gsutil 4.40 with an important rsync bug fix.
+      # https://cloud.google.com/sdk/docs/release-notes
+      # https://github.com/GoogleCloudPlatform/gsutil/blob/master/CHANGES.md
+      # This is only required if the user asked for upload to cloud storage.
+      _check_version('Google Cloud SDK', ['gcloud', '--version'], (254, 0, 0))
+
+      # Also, make sure the user is logged in and can access the destination.
+      cloud_node.CloudNode.check_access(bucket_url)
+
+    # Define resolutions and bitrates before parsing other configs.
+    bitrate_config = BitrateConfig(bitrate_config_dict)
+
+    # Now that the definitions have been parsed, register the maps of valid
+    # resolutions and channel layouts so that InputConfig and PipelineConfig
+    # can be validated accordingly.
+    bitrate_configuration.Resolution.set_map(bitrate_config.video_resolutions)
+    bitrate_configuration.ChannelLayout.set_map(
+        bitrate_config.audio_channel_layouts)
+
+    input_config = InputConfig(input_config_dict)
+    pipeline_config = PipelineConfig(pipeline_config_dict)
+    self._pipeline_config = pipeline_config
+
+    outputs = []
+    for input in input_config.inputs:
+      # External command inputs need to be processed by an additional node
+      # before being transcoded.  In this case, the input doesn't have a
+      # filename that FFmpeg can read, so we generate an intermediate pipe for
+      # that node to write to.  TranscoderNode will then instruct FFmpeg to
+      # read from that pipe for this input.
+      if input.input_type == InputType.EXTERNAL_COMMAND:
+        command_output = self._create_pipe()
+        self._nodes.append(external_command_node.ExternalCommandNode(
+            input.name, command_output))
+        input.set_pipe(command_output)
+
+      if input.media_type == MediaType.AUDIO:
+        for codec in pipeline_config.audio_codecs:
+          outputs.append(AudioOutputStream(self._create_pipe(),
+                                           input,
+                                           codec,
+                                           pipeline_config.channels))
+
+      elif input.media_type == MediaType.VIDEO:
+        for codec in pipeline_config.video_codecs:
+          for output_resolution in pipeline_config.resolutions:
+            # Only going to output lower or equal resolution videos.
+            # Upscaling is costly and does not do anything.
+            if input.resolution < output_resolution:
+              continue
+
+            outputs.append(VideoOutputStream(self._create_pipe(),
+                                             input,
+                                             codec,
+                                             output_resolution))
+
+      elif input.media_type == MediaType.TEXT:
+        outputs.append(TextOutputStream(input))
+
+    self._nodes.append(transcoder_node.TranscoderNode(input_config,
+                                                      pipeline_config,
+                                                      outputs))
+
+    self._nodes.append(packager_node.PackagerNode(pipeline_config,
+                                                  output_dir,
+                                                  outputs))
+
+    if bucket_url:
+      cloud_temp_dir = os.path.join(self._temp_dir, 'cloud')
+      os.mkdir(cloud_temp_dir)
+
+      self._nodes.append(cloud_node.CloudNode(output_dir,
+                                              bucket_url,
+                                              cloud_temp_dir,
+                                              self.is_vod()))
+
+    for node in self._nodes:
+      node.start()
+    return self
+
+  def check_status(self):
+    """Checks the status of all the nodes.
+
+    :rtype: streamer.node_base.ProcessStatus
+
+    If one node is errored, this returns Errored; otherwise if one node is
+    finished, this returns Finished; this only returns Running if all nodes are
+    running.  If there are no nodes, this returns Finished.
+    """
+    if not self._nodes:
+      return node_base.ProcessStatus.Finished
+
+    value = max(node.check_status().value for node in self._nodes)
+    return node_base.ProcessStatus(value)
+
+  def stop(self):
+    """Stop all nodes."""
+    status = self.check_status()
+    for node in self._nodes:
+      node.stop(status)
+    self._nodes = []
+
+  def is_vod(self):
+    """Returns True if the pipeline is running in VOD mode.
+
+    :rtype: bool
+    """
+
+    return self._pipeline_config.streaming_mode == StreamingMode.VOD
+
+class VersionError(Exception):
+  """A version error for one of Shaka Streamer's external dependencies.
+
+  Raised when a dependency (like FFmpeg) is missing or not new enough to work
+  with Shaka Streamer.  See also :doc:`prerequisites`.
+  """
+
+  pass
+
+def _check_version(name, command, minimum_version):
+  min_version_string = '.'.join(str(x) for x in minimum_version)
+
+  version_error_string = (
+      name + ' not installed! Please install version ' + min_version_string +
+      ' or higher of ' + name + '.')
+  try:
+    version_string = str(subprocess.check_output(command))
+  except (subprocess.CalledProcessError, OSError) as e:
+    if isinstance(e, subprocess.CalledProcessError):
+      print(e.stdout, file=sys.stderr)
+    raise VersionError(version_error_string) from None
+
+  version_match = re.search(r'([0-9]+)\.([0-9]+)\.([0-9]+)', version_string)
+
+  if version_match == None:
+    raise VersionError(name + ' version not found in string output!')
+
+  version = (int(version_match.group(1)), int(version_match.group(2)))
+  if version < minimum_version:
+    raise VersionError(version_error_string)
---
+++ packages/shaka-streamer/shaka_streamer-0.3.0-py3-none-any.whl/streamer/packager_node.py
@@ -0,0 +1,176 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""A module that feeds information from two named pipes into shaka-packager."""
+
+import os
+import subprocess
+
+from . import input_configuration
+from . import node_base
+from . import pipeline_configuration
+
+# Alias a few classes to avoid repeating namespaces later.
+MediaType = input_configuration.MediaType
+
+ManifestFormat = pipeline_configuration.ManifestFormat
+StreamingMode = pipeline_configuration.StreamingMode
+
+
+INIT_SEGMENT = {
+  MediaType.AUDIO: '{dir}/audio_{language}_{channels}c_{bitrate}_init.{format}',
+  MediaType.VIDEO: '{dir}/video_{resolution_name}_{bitrate}_init.{format}',
+  MediaType.TEXT: '{dir}/text_{language}_init.{format}',
+}
+
+MEDIA_SEGMENT = {
+  MediaType.AUDIO: '{dir}/audio_{language}_{channels}c_{bitrate}_$Number$.{format}',
+  MediaType.VIDEO: '{dir}/video_{resolution_name}_{bitrate}_$Number$.{format}',
+  MediaType.TEXT: '{dir}/text_{language}_$Number$.{format}',
+}
+
+SINGLE_SEGMENT = {
+  MediaType.AUDIO: '{dir}/audio_{language}_{channels}c_{bitrate}.{format}',
+  MediaType.VIDEO: '{dir}/video_{resolution_name}_{bitrate}.{format}',
+  MediaType.TEXT: '{dir}/text_{language}.{format}',
+}
+
+class SegmentError(Exception):
+  """Raise when segment is incompatible with format."""
+  pass
+
+
+class PackagerNode(node_base.PolitelyWaitOnFinishMixin, node_base.NodeBase):
+
+  def __init__(self, pipeline_config, output_dir, output_streams):
+    super().__init__()
+    self._pipeline_config = pipeline_config
+    self._output_dir = output_dir
+    self._segment_dir = os.path.join(output_dir, pipeline_config.segment_folder)
+    self._output_streams = output_streams
+
+  def start(self):
+    args = [
+        'packager',
+    ]
+
+    args += [self._setup_stream(stream) for stream in self._output_streams]
+
+    args += [
+        # Segment duration given in seconds.
+        '--segment_duration', str(self._pipeline_config.segment_size),
+    ]
+
+    if self._pipeline_config.streaming_mode == StreamingMode.LIVE:
+      args += [
+          # Number of seconds the user can rewind through backwards.
+          '--time_shift_buffer_depth',
+          str(self._pipeline_config.availability_window),
+          # Number of segments preserved outside the current live window.
+          '--preserved_segments_outside_live_window', '1',
+          # Number of seconds of content encoded/packaged that is ahead of the
+          # live edge.
+          '--suggested_presentation_delay',
+          str(self._pipeline_config.presentation_delay),
+          # Number of seconds between manifest updates.
+          '--minimum_update_period',
+          str(self._pipeline_config.update_period),
+      ]
+
+    args += self._setup_manifest_format()
+
+    if self._pipeline_config.encryption.enable:
+      args += self._setup_encryption()
+
+    stdout = None
+    if self._pipeline_config.debug_logs:
+      # Log by writing all Packager output to a file.  Unlike the logging
+      # system in ffmpeg, this will stop any Packager output from getting to
+      # the screen.
+      stdout = open('PackagerNode.log', 'w')
+
+    self._process = self._create_process(args,
+                                         stderr=subprocess.STDOUT,
+                                         stdout=stdout)
+
+  def _setup_stream(self, stream):
+    dict = {
+        'in': stream.pipe,
+        'stream': stream.type.value,
+    }
+
+    # Note: Shaka Packager will not accept 'und' as a language, but Shaka
+    # Player will fill that in if the language metadata is missing from the
+    # manifest/playlist.
+    if stream.input.language and stream.input.language != 'und':
+      dict['language'] = stream.input.language
+
+    if self._pipeline_config.segment_per_file:
+      dict['init_segment'] = stream.fill_template(
+          INIT_SEGMENT[stream.type],
+          dir=self._segment_dir)
+      dict['segment_template'] = stream.fill_template(
+          MEDIA_SEGMENT[stream.type],
+          dir=self._segment_dir)
+    else:
+      dict['output'] = stream.fill_template(
+          SINGLE_SEGMENT[stream.type],
+          dir=self._segment_dir)
+
+    # The format of this argument to Shaka Packager is a single string of
+    # key=value pairs separated by commas.
+    return ','.join(key + '=' + value for key, value in dict.items())
+
+  def _setup_manifest_format(self):
+    args = []
+    if ManifestFormat.DASH in self._pipeline_config.manifest_format:
+      if self._pipeline_config.streaming_mode == StreamingMode.VOD:
+        args += [
+            '--generate_static_mpd',
+        ]
+      args += [
+          # Generate DASH manifest file.
+          '--mpd_output',
+          os.path.join(self._output_dir, self._pipeline_config.dash_output),
+      ]
+    if ManifestFormat.HLS in self._pipeline_config.manifest_format:
+      if self._pipeline_config.streaming_mode == StreamingMode.LIVE:
+        args += [
+            '--hls_playlist_type', 'LIVE',
+        ]
+      else:
+        args += [
+            '--hls_playlist_type', 'VOD',
+        ]
+      args += [
+          # Generate HLS playlist file(s).
+          '--hls_master_playlist_output',
+          os.path.join(self._output_dir, self._pipeline_config.hls_output),
+      ]
+    return args
+
+  def _setup_encryption(self):
+    # Sets up encryption of content.
+    args = [
+      '--enable_widevine_encryption',
+      '--key_server_url', self._pipeline_config.encryption.key_server_url,
+      '--content_id', self._pipeline_config.encryption.content_id,
+      '--signer', self._pipeline_config.encryption.signer,
+      '--aes_signing_key', self._pipeline_config.encryption.signing_key,
+      '--aes_signing_iv', self._pipeline_config.encryption.signing_iv,
+      '--protection_scheme',
+      self._pipeline_config.encryption.protection_scheme.value,
+      '--clear_lead', str(self._pipeline_config.encryption.clear_lead),
+    ]
+    return args
