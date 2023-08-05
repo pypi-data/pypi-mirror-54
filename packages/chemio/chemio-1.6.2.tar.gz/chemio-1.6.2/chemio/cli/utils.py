1d2e9afe9d80697b2608ca76e65e3947a0a49583
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:58 +0000

    Add package chemio/1.6.2/chemio-1.6.2.tar.gz

---
+++ packages/chemio/chemio-1.6.2.tar.gz/chemio-1.6.2/chemio/cli/utils.py
@@ -0,0 +1,36 @@
+"""
+
+
+
+args utils are stored here.
+
+
+"""
+
+
+
+import re
+
+
+
+
+def parse_args_data(args_data):
+    data = dict()
+    if args_data:
+        for d in args_data:
+            match = re.match('^(.*?)=(.*)$', d)
+            if match:
+                key, val = match[1], match[2]
+                data[key] = val
+            else:
+                raise ValueError("args data: d not a=b format")
+
+    import pdb; pdb.set_trace()
+    print('data:', data)
+    return data
+
+
+
+
+
+
