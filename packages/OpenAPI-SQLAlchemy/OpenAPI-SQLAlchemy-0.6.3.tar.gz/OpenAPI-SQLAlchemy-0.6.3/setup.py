a8ffbfb4d95cb4c352fb211e2a323317423d3721
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:36 +0000

    Add package OpenAPI-SQLAlchemy/0.6.3/OpenAPI-SQLAlchemy-0.6.3.tar.gz

---
+++ packages/OpenAPI-SQLAlchemy/OpenAPI-SQLAlchemy-0.6.3.tar.gz/OpenAPI-SQLAlchemy-0.6.3/setup.py
@@ -0,0 +1,48 @@
+"""Setup package."""
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+setuptools.setup(
+    name="OpenAPI-SQLAlchemy",
+    version="0.6.3",
+    author="David Andersson",
+    author_email="anderssonpublic@gmail.com",
+    description="Maps an OpenAPI schema to SQLAlchemy models.",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jdkandersson/OpenAPI-SQLAlchemy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "Natural Language :: English",
+        "Topic :: Database",
+    ],
+    python_requires=">=3.7",
+    install_requires=["SQLAlchemy>=1.3.6", "typing-extensions>=3.7.4"],
+    extras_require={
+        "dev": [
+            "pytest",
+            "tox",
+            "tox-pyenv",
+            "pylint",
+            "pytest-cov",
+            "pytest-flakes",
+            "mypy",
+            "pydocstyle",
+            "black",
+            "pre-commit",
+            "isort",
+            "PyYAML",
+            "Sphinx",
+            "doc8",
+        ]
+    },
+)
