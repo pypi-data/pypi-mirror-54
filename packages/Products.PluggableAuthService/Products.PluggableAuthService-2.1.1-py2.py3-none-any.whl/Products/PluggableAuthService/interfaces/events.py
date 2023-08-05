996981d9f9fc96187b16e5feb004f1ccea27036d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:29 +0000

    Add package Products.PluggableAuthService/2.1.1/Products.PluggableAuthService-2.1.1-py2.py3-none-any.whl

---
+++ packages/Products.PluggableAuthService/Products.PluggableAuthService-2.1.1-py2.py3-none-any.whl/Products/PluggableAuthService/interfaces/events.py
@@ -0,0 +1,54 @@
+from zope.interface import Attribute
+from zope.interface import Interface
+
+
+class IPASEvent(Interface):
+    """An event related to a PAS principal.
+    """
+
+    principal = Attribute('The subject of the event.')
+
+
+class IPrincipalCreatedEvent(IPASEvent):
+    """A new principal has been created.
+    """
+
+
+class IUserLoggedInEvent(IPASEvent):
+    """ A user logged in.
+    """
+
+
+class IUserLoggedOutEvent(IPASEvent):
+    """ A user logged out.
+    """
+
+
+class IPrincipalDeletedEvent(IPASEvent):
+    """A user has been removed.
+    """
+
+
+class ICredentialsUpdatedEvent(IPASEvent):
+    """A principal has changed her password.
+
+    Sending this event will cause a PAS user folder to trigger its active
+    credential update plugins.
+    """
+    password = Attribute('The new password')
+
+
+class IPropertiesUpdatedEvent(IPASEvent):
+    """A principals properties have been updated.
+    """
+    properties = Attribute('List of modified property ids')
+
+
+class IGroupCreatedEvent(IPASEvent):
+    """A group has been created.
+    """
+
+
+class IGroupDeletedEvent(IPASEvent):
+    """A group has been removed.
+    """
