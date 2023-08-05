ec42ab90740d33fcd2c725a122de90bcbd4addc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:05 +0000

    Add package FlipException/0.5/FlipException-0.5-py3-none-any.whl

---
+++ packages/FlipException/FlipException-0.5-py3-none-any.whl/flipexception/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from .flipexception import FlipException
---
+++ packages/FlipException/FlipException-0.5-py3-none-any.whl/flipexception/flipexception.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import upsidedown
+
+__version__ = 0.5
+
+class FlipException(Exception):
+
+    @staticmethod
+    def flip_text(message=None):
+        if not message:
+            message = "┻━┻"
+        else:
+            message = upsidedown.transform(message)
+
+        return str("(╯°□°）╯︵ " + message)
+
+
+    def __init__(self, message=None):
+        message = self.flip_text(message)
+        super(Exception, self).__init__(message)
