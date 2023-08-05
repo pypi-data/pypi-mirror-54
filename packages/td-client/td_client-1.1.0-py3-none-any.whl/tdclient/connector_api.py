fffc0941b202da089dd76c22e4c824f786a5f6af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:26 +0000

    Add package td-client/1.1.0/td_client-1.1.0-py3-none-any.whl

---
+++ packages/td-client/td_client-1.1.0-py3-none-any.whl/tdclient/connector_api.py
@@ -0,0 +1,243 @@
+#!/usr/bin/env python
+
+import json
+
+from .util import create_url
+
+
+class ConnectorAPI:
+    """Access Data Connector API which handles Data Connector.
+
+    This class is inherited by :class:`tdclient.api.API`.
+    """
+
+    def connector_guess(self, job):
+        """Guess the Data Connector configuration
+
+        Args:
+            job (dict): :class:`dict` representation of `seed.yml`
+
+        Returns:
+             :class:`dict`
+        """
+        headers = {"content-type": "application/json; charset=utf-8"}
+        payload = json.dumps(job).encode("utf-8") if isinstance(job, dict) else job
+        with self.post("/v3/bulk_loads/guess", payload, headers=headers) as res:
+            code, body = res.status, res.read()
+            if code != 200:
+                self.raise_error("DataConnector configuration guess failed", res, body)
+            return self.checked_json(body, [])
+
+    def connector_preview(self, job):
+        """Show the preview of the Data Connector job.
+
+        Args:
+            job (dict): :class:`dict` representation of `load.yml`
+
+        Returns:
+             :class:`dict`
+        """
+        headers = {"content-type": "application/json; charset=utf-8"}
+        payload = json.dumps(job).encode("utf-8") if isinstance(job, dict) else job
+        with self.post("/v3/bulk_loads/preview", payload, headers=headers) as res:
+            code, body = res.status, res.read()
+            if code != 200:
+                self.raise_error("DataConnector job preview failed", res, body)
+            return self.checked_json(body, [])
+
+    def connector_issue(self, db, table, job):
+        """Create a Data Connector job.
+
+        Args:
+            db (str): name of the database to perform connector job
+            table (str): name of the table to perform connector job
+            job (dict): :class:`dict` representation of `load.yml`
+
+        Returns:
+             str: job Id
+        """
+        headers = {"content-type": "application/json; charset=utf-8"}
+        params = dict(job)
+        params["database"] = db
+        params["table"] = table
+        payload = json.dumps(params).encode("utf-8")
+        with self.post(
+            create_url("/v3/job/issue/bulkload/{db}", db=db), payload, headers=headers
+        ) as res:
+            code, body = res.status, res.read()
+            if code != 200:
+                self.raise_error("DataConnector job issuing failed", res, body)
+            js = self.checked_json(body, ["job_id"])
+            return str(js["job_id"])
+
+    def connector_list(self):
+        """Show the list of available Data Connector sessions.
+
+        Returns:
+             :class:`list`
+        """
+        with self.get("/v3/bulk_loads") as res:
+            code, body = res.status, res.read()
+            if code != 200:
+                self.raise_error("DataConnectorSession list retrieve failed", res, body)
+            # cannot use `checked_json` since `GET /v3/bulk_loads` returns an array
+            return json.loads(body.decode("utf-8"))
+
+    def connector_create(self, name, database, table, job, params=None):
+        """Create a Data Connector session.
+
+        Args:
+            name (str): name of the connector job
+            database (str): name of the database to perform connector job
+            table (str): name of the table to perform connector job
+            job (dict): :class:`dict` representation of `load.yml`
+            params (dict, optional): Extra parameters
+
+                - config (str):
+                     Embulk configuration as JSON format.
+                     See also https://www.embulk.org/docs/built-in.html#embulk-configuration-file-format
+                - cron (str, optional):
+                     Schedule of the query.
+                     {``"@daily"``, ``"@hourly"``, ``"10 * * * *"`` (custom cron)}
+                     See also: https://support.treasuredata.com/hc/en-us/articles/360001451088-Scheduled-Jobs-Web-Console
+                - delay (int, optional):
+                     A delay ensures all buffered events are imported
+                     before running the query. Default: 0
+                - database (str):
+                     Target databse for the Data Connector session
+                - name (str):
+                     Name of the Data Connector session
+                - table (str):
+                     Target table for the Data Connector session
+                - time_column (str, optional):
+                     Column in the table for registering config.out.time
+                - timezone (str):
+                     Timezone for scheduled Data Connector session.
+                     See here for list of supported timezones https://gist.github.com/frsyuki/4533752
+
+        Returns:
+             :class:`dict`
+        """
+        headers = {"content-type": "application/json; charset=utf-8"}
+        params = {} if params is None else dict(params)
+        params.update(job)
+        params["name"] = name
+        params["database"] = database
+        params["table"] = table
+        payload = json.dumps(params).encode("utf-8")
+        with self.post("/v3/bulk_loads", payload, headers=headers) as res:
+            code, body = res.status, res.read()
+            if code != 200:
+                self.raise_error(
+                    "DataConnectorSession: %s created failed" % (name,), res, body
+                )
+            return self.checked_json(body, [])
+
+    def connector_show(self, name):
+        """Show a specific Data Connector session information.
+
+        Args:
+            name (str): name of the connector job
+
+        Returns:
+             :class:`dict`
+        """
+        with self.get(create_url("/v3/bulk_loads/{name}", name=name)) as res:
+            code, body = res.status, res.read()
+            if code != 200:
+                self.raise_error(
+                    "DataConnectorSession: %s retrieve failed" % (name,), res, body
+                )
+            return self.checked_json(body, [])
+
+    def connector_update(self, name, job):
+        """Update a specific Data Connector session.
+
+        Args:
+          name (str): name of the connector job
+          job (dict): :class:`dict` representation of `load.yml`.
+              For detailed format, see also: https://www.embulk.org/docs/built-in.html#embulk-configuration-file-format
+
+        Returns:
+             :class:`dict`
+        """
+        headers = {"content-type": "application/json; charset=utf-8"}
+        payload = json.dumps(job).encode("utf-8")
+        with self.put(
+            create_url("/v3/bulk_loads/{name}", name=name),
+            payload,
+            len(payload),
+            headers=headers,
+        ) as res:
+            code, body = res.status, res.read()
+            if code != 200:
+                self.raise_error(
+                    "DataConnectorSession: %s update failed" % (name,), res, body
+                )
+            return self.checked_json(body, [])
+
+    def connector_delete(self, name):
+        """Delete a Data Connector session.
+
+        Args:
+            name (str): name of the connector job
+
+        Returns:
+             :class:`dict`
+        """
+        with self.delete(create_url("/v3/bulk_loads/{name}", name=name)) as res:
+            code, body = res.status, res.read()
+            if code != 200:
+                self.raise_error(
+                    "DataConnectorSession: %s delete failed" % (name,), res, body
+                )
+            return self.checked_json(body, [])
+
+    def connector_history(self, name):
+        """Show the list of the executed jobs information for the Data Connector job.
+
+        Args:
+            name (str): name of the connector job
+
+        Returns:
+             :class:`list`
+        """
+        with self.get(create_url("/v3/bulk_loads/{name}/jobs", name=name)) as res:
+            code, body = res.status, res.read()
+            if code != 200:
+                self.raise_error(
+                    "history of DataConnectorSession: %s retrieve failed" % (name,),
+                    res,
+                    body,
+                )
+            return json.loads(body.decode("utf-8"))
+
+    def connector_run(self, name, **kwargs):
+        """Create a job to execute Data Connector session.
+
+        Args:
+            name (str): name of the connector job
+            **kwargs (optional): Extra parameters.
+
+                - scheduled_time (int):
+                    Time in Unix epoch format that would be set as
+                    `TD_SCHEDULED_TIME`.
+                - domain_key (str):
+                    Job domain key which is assigned to a single job.
+
+        Returns:
+             :class:`dict`
+        """
+        headers = {"content-type": "application/json; charset=utf-8"}
+        payload = json.dumps(kwargs).encode("utf-8")
+        with self.post(
+            create_url("/v3/bulk_loads/{name}/jobs", name=name),
+            payload,
+            headers=headers,
+        ) as res:
+            code, body = res.status, res.read()
+            if code != 200:
+                self.raise_error(
+                    "DataConnectorSession: %s job create failed" % (name,), res, body
+                )
+            return self.checked_json(body, [])
---
+++ packages/td-client/td_client-1.1.0-py3-none-any.whl/tdclient/util.py
@@ -0,0 +1,93 @@
+import io
+from urllib.parse import quote as urlquote
+
+import msgpack
+
+
+def create_url(tmpl, **values):
+    """Create url with values
+
+    Args:
+        tmpl (str): url template
+        values (dict): values for url
+    """
+    quoted_values = {k: urlquote(str(v)) for k, v in values.items()}
+    return tmpl.format(**quoted_values)
+
+
+def parse_csv_value(s):
+    """Parse and convert value to suitable types
+
+    Args:
+        s (str): value on csv
+    Returns:
+        Suitable value (int, float, str, bool or None)
+    """
+    try:
+        return int(s)
+    except (OverflowError, ValueError):
+        try:
+            return float(s)
+        except (OverflowError, ValueError):
+            pass
+    lower = s.lower()
+    if lower in ("false", "true"):
+        return "true" == lower
+    elif lower in ("", "none", "null"):
+        return None
+    else:
+        return s
+
+
+def create_msgpack(items):
+    """Create msgpack streaming bytes from list
+
+    Args:
+        items (list of dict): target list
+
+    Returns:
+        Converted msgpack streaming (bytes)
+
+    Examples:
+
+        >>> t1 = int(time.time())
+        >>> l1 = [{"a": 1, "b": 2, "time": t1}, {"a":3, "b": 6, "time": t1}]
+        >>> create_msgpack(l1)
+        ``b'\x83\xa1a\x01\xa1b\x02\xa4time\xce]\xa5X\xa1\x83\xa1a\x03\xa1b\x06\xa4time\xce]\xa5X\xa1'``
+    """
+    stream = io.BytesIO()
+    packer = msgpack.Packer()
+    for item in items:
+        try:
+            mp = packer.pack(item)
+        except (OverflowError, ValueError):
+            packer.reset()
+            mp = packer.pack(normalized_msgpack(item))
+        stream.write(mp)
+
+    return stream.getvalue()
+
+
+def normalized_msgpack(value):
+    """Convert int to str if overflow
+
+    Args:
+        value (int, float, str, bool or None): value to be normalized
+
+    Returns:
+        Normalized value
+    """
+    if isinstance(value, (list, tuple)):
+        return [normalized_msgpack(v) for v in value]
+    elif isinstance(value, dict):
+        return dict(
+            [(normalized_msgpack(k), normalized_msgpack(v)) for (k, v) in value.items()]
+        )
+
+    if isinstance(value, int):
+        if -(1 << 63) < value < (1 << 64):
+            return value
+        else:
+            return str(value)
+    else:
+        return value
