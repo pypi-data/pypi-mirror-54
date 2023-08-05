1570fcfd34a89878f8272c6e6751975d726868a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:26 +0000

    Add package yawrap/0.4.8/yawrap-0.4.8-py2.py3-none-any.whl

---
+++ packages/yawrap/yawrap-0.4.8-py2.py3-none-any.whl/yawrap/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+from ._engine import Doc
+from ._navrap import NavedYawrap
+from ._sourcer import HEAD, BODY_BEGIN, BODY_END, EmbedCss, EmbedJs, LinkCss, LinkJs, ExternalCss, ExternalJs
+from ._yawrap import Yawrap
+from .clerap import Ya4, Ya5
