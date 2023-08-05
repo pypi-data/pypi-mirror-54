d88e913f5dded3129795e6600ffae3161b11d7df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:48 +0000

    Add package ChiantiPy/0.8.2/ChiantiPy-0.8.2-py3-none-any.whl

---
+++ packages/ChiantiPy/ChiantiPy-0.8.2-py3-none-any.whl/ChiantiPy/__init__.py
@@ -0,0 +1,8 @@
+"""
+ChiantiPy - CHIANTI Python package Calculates various aspects of emission lines
+and continua from the CHIANTI atomic database for astrophysical spectroscopy.
+"""
+
+from . import  version
+__version_info__ = version.__version_info__
+__version__ = version.__version__
