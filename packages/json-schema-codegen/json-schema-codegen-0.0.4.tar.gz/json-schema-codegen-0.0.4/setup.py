f3df3080ac6255ef9defc28678177a06c3ddd775
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:05 +0000

    Add package json-schema-codegen/0.0.4/json-schema-codegen-0.0.4.tar.gz

---
+++ packages/json-schema-codegen/json-schema-codegen-0.0.4.tar.gz/json-schema-codegen-0.0.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='json-schema-codegen',
+      version='0.0.4',
+      url='http://github.com/pearmaster/json-schema-codegen',
+      author='Jacob Brunson',
+      author_email='pypi@jacobbrunson.com',
+      description="Generates C++ code from JSON-Schema",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      license='GPLv2',
+      packages=['jsonschemacodegen', 'jsonschemacodegen.templates.cpp'],
+      package_data={
+            'jsonschemacodegen.templates.cpp': ['*.jinja2']
+      },
+      zip_safe=False,
+      install_requires=[
+          'jinja2',
+          'stringcase',
+      ],
+      include_package_data=True,
+      python_requires='>=3.7',
+)
