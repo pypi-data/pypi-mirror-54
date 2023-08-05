ea4da214808950f169a69f2a837b0919dc4c46ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:24 +0000

    Add package hasasia/1.0.2/hasasia-1.0.2.tar.gz

---
+++ packages/hasasia/hasasia-1.0.2.tar.gz/hasasia-1.0.2/hasasia/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for hasasia."""
+
+__author__ = """Jeffrey S. Hazboun"""
+__email__ = 'jeffrey.hazboun@gmail.com'
+__version__ = '1.0.2'
+
+citation = 'Hazboun, J.S., Romano, J.R., Smith, T.L. '
+citation += 'Realistic sensitivity curves for pulsar timing arrays. '
+citation += 'arxiv:1907.04341'
+__cite__ = citation
