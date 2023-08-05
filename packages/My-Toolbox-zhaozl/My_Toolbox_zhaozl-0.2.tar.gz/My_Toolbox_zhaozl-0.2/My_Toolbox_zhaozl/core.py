13d5d985ccbe0601b42ac13359f25d463a9d4504
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:03 +0000

    Add package My-Toolbox-zhaozl/0.2/My_Toolbox_zhaozl-0.2.tar.gz

---
+++ packages/My-Toolbox-zhaozl/My_Toolbox_zhaozl-0.2.tar.gz/My_Toolbox_zhaozl-0.2/My_Toolbox_zhaozl/core.py
@@ -0,0 +1,28 @@
+'''
+This toolbox contens some basic developing methods,
+including:
+integrated printing method;
+'''
+import numpy as np
+np.set_printoptions(threshold=np.inf, linewidth=np.inf)
+
+
+class Print_Tool:
+    '''
+    By inputing [name] and [value] of a Var, to output the basic info.
+    '''
+    @staticmethod
+    def Integrated_Print(name, value):
+
+        print(' 【变量名  name 】', name, '\n',
+              '【数  值 value 】', value, '\n',
+              '【尺  寸 shape 】', np.shape(value), '\n',
+              '【类  型  type 】', type(value))
+        try:
+            print(
+                ' 【最大值  max  】', max(value), '\n',
+                '【最小值  min  】', min(value), '\n',
+                '【均  值  mean 】', np.mean(value), '\n',
+                '【中  值 median】', np.median(value), '\n')
+        except:
+            pass
