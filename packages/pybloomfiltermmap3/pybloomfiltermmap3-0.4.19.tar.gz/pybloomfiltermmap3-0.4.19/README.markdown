c10a9fdc0ecd192eaf3e37b3661ce44f0327af89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:18 +0000

    Add package pybloomfiltermmap3/0.4.19/pybloomfiltermmap3-0.4.19.tar.gz

---
+++ packages/pybloomfiltermmap3/pybloomfiltermmap3-0.4.19.tar.gz/pybloomfiltermmap3-0.4.19/README.markdown
@@ -0,0 +1,56 @@
+# pybloomfiltermmap3
+
+`pybloomfiltermmap3` is a `Python 3` compatible fork of `pybloomfiltermmap` by @axiak.
+
+The goal of `pybloomfiltermmap3` is simple: to provide a fast, simple, scalable, correct library for Bloom Filters in Python.
+
+[![Build Status](https://travis-ci.org/PrashntS/pybloomfiltermmap3.svg?branch=master)](https://travis-ci.org/PrashntS/pybloomfiltermmap3)
+[![PyPI](https://img.shields.io/pypi/v/pybloomfiltermmap3.svg)](https://pypi.python.org/pypi/pybloomfiltermmap3)
+[![PyPI](https://img.shields.io/pypi/dw/pybloomfiltermmap3.svg)](https://pypi.python.org/pypi/pybloomfiltermmap3)
+[![PyPI](https://img.shields.io/pypi/pyversions/pybloomfiltermmap3.svg)](https://pypi.python.org/pypi/pybloomfiltermmap3)
+
+
+## Quickstart
+
+After you install, the interface to use is a cross between a file
+interface and a ste interface. As an example:
+```python
+    >>> import pybloomfilter
+    >>> fruit = pybloomfilter.BloomFilter(100000, 0.1, '/tmp/words.bloom')
+    >>> fruit.update(('apple', 'pear', 'orange', 'apple'))
+    >>> len(fruit)
+    3
+    >>> 'mike' in fruit
+    False
+    >>> 'apple' in fruit
+    True
+```
+
+To create an in-memory filter, ditch the file location.
+```python
+    >>> cakes = pybloomfilter.BloomFilter(10000, 0.1)
+```
+*Caveates*: It is currently not possible to persist this filter later.
+
+## Docs
+
+Follow the *official* docs for `pybloomfiltermmap`. http://axiak.github.io/pybloomfiltermmap/
+
+## Install
+
+Please have `Cython` installed. Please note that this version is **specifically** meant for Python 3. In case you need Python 2, please see https://github.com/axiak/pybloomfiltermmap.
+
+To install:
+
+```shell
+    $ pip install cython
+    $ pip install pybloomfiltermmap3
+```
+
+and you should be set.
+
+
+## License
+
+See the LICENSE file. It's under the MIT License.
+
---
+++ packages/pybloomfiltermmap3/pybloomfiltermmap3-0.4.19.tar.gz/pybloomfiltermmap3-0.4.19/setup.py
@@ -0,0 +1,67 @@
+import os
+import sys
+
+from setuptools import setup, Extension
+
+try:
+  from Cython.Distutils import build_ext
+except ImportError:
+  print("""Cannot find Cython!
+  Cython is required to correctly build pyBloomFilter's C extensions.
+  In most cases, running the following command should be sufficient:
+    $ pip install Cython
+
+  Exception: ImportError
+  """)
+  exit()
+
+here = os.path.dirname(__file__)
+# Get the long description from the README file
+with open(os.path.join(here, 'README.markdown'), encoding='utf-8') as fp:
+  long_description = fp.read()
+
+
+ext_files = [
+  'src/mmapbitarray.c',
+  'src/bloomfilter.c',
+  'src/md5.c',
+  'src/primetester.c',
+  'src/MurmurHash3.c',
+  'src/pybloomfilter.pyx'
+]
+
+print("info: Building from Cython")
+
+ext_modules = [
+  Extension("pybloomfilter", ext_files, libraries=['crypto'])
+]
+
+if sys.version_info[0] < 3:
+  raise SystemError('This Package is for Python Version 3 and above.')
+
+setup(
+  name='pybloomfiltermmap3',
+  version="0.4.19",
+  author="Michael Axiak, Rob Stacey, Prashant Sinha",
+  author_email="prashant@noop.pw",
+  url="https://github.com/prashnts/pybloomfiltermmap3",
+  description="A Bloom filter (bloomfilter) for Python 3 built on mmap",
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  license="MIT License",
+  test_suite='tests.test_all',
+  install_requires=['Cython'],
+  ext_modules=ext_modules,
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: C',
+    'Programming Language :: Cython',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+  ],
+  cmdclass={'build_ext': build_ext}
+)
