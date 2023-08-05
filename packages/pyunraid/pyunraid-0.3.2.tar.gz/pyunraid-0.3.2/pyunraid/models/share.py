7a175f50ab88fe3c241dfb3bf059aba3e6da177e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:11 +0000

    Add package pyunraid/0.3.2/pyunraid-0.3.2.tar.gz

---
+++ packages/pyunraid/pyunraid-0.3.2.tar.gz/pyunraid-0.3.2/pyunraid/models/share.py
@@ -0,0 +1,19 @@
+class Share():
+
+    def __init__(self):
+        self.type = ''
+        self.status = ''
+        self.name = ''
+        self.comment = ''
+        self.smb_security = ''
+        self.nfs_security = ''
+        self.afp_security = None
+        self.free_size = 0
+
+
+    def size(self):
+        pass
+
+
+    def path(self, path = '/'):
+        pass
