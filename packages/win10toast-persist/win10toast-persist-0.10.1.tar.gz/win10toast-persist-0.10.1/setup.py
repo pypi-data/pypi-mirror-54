797dfd721ca696bd927182a1e3fbfe9888e679eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:15:56 +0000

    Add package win10toast-persist/0.10.1/win10toast-persist-0.10.1.tar.gz

---
+++ packages/win10toast-persist/win10toast-persist-0.10.1.tar.gz/win10toast-persist-0.10.1/setup.py
@@ -0,0 +1,47 @@
+
+from operator import attrgetter
+from os import path
+
+from setuptools import setup
+
+
+def read(fname):
+    return open(path.join(path.dirname(__file__), fname)).read()
+
+
+def from_here(relative_path):
+    return path.join(path.dirname(__file__), relative_path)
+
+
+setup(
+    name="win10toast-persist",
+    version="0.10.1",
+    install_requires=[
+        "pypiwin32",
+        "setuptools"
+    ],
+    packages=["win10toast_persist"],
+    license="BSD",
+    url="https://github.com/tnthieding/Windows-10-Toast-Notifications",
+    download_url='',
+    description=(
+        "An easy-to-use Python library for displaying "
+        "Windows 10 Toast Notifications with support for "
+        "persisting notifications."
+    ),
+    include_package_data=True,
+    package_data={
+        '': ['*.txt'],
+        'win10toast_persist': ['data/*.ico'],
+    },
+    long_description=read('README.rst'),
+    author="Jithu R. Jacob and Tyler N. Thieding",
+    author_email="python@thieding.com",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        'Operating System :: Microsoft',
+        'Environment :: Win32 (MS Windows)',
+        "License :: OSI Approved :: MIT License",
+    ],
+)
