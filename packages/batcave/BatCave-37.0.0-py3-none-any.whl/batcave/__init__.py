155d346a378b9ad0b81cc74c909b41df085079da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:33 +0000

    Add package batcave/37.0.0/BatCave-37.0.0-py3-none-any.whl

---
+++ packages/batcave/BatCave-37.0.0-py3-none-any.whl/batcave/__init__.py
@@ -0,0 +1,19 @@
+﻿'Module initializer and generic utilities'
+__all__ = ('__title__', '__summary__', '__uri__',
+           '__version__', '__buildname__', '__builddate__',
+           '__author__', '__email__',
+           '__license__', '__copyright__')
+
+__title__ = 'BatCave'
+__summary__ = 'Python Programming Toolkit'
+__uri__ = 'https://pypi.org/project/batcave/'
+
+__version__ = '37.0.0'
+__buildname__ = 'BatCave_37.0.0_4'
+__builddate__ = '2019-10-15 14:23:06.050065'
+
+__author__ = 'Jeff Smith'
+__email__ = 'web@pobox.com'
+
+__license__ = 'MIT'
+__copyright__ = 'Copyright (c) 2019 Jeffery G. Smith'
