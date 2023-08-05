fc77bd1ccd417eb22e3e3903d7d715ae276b3db1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:27 +0000

    Add package bareasgi/3.2.1/bareasgi-3.2.1-py3-none-any.whl

---
+++ packages/bareasgi/bareasgi-3.2.1-py3-none-any.whl/bareasgi/basic_router/__init__.py
@@ -0,0 +1,10 @@
+"""Basic routing support
+"""
+
+from .http_router import BasicHttpRouter
+from .web_socket_router import BasicWebSocketRouter
+
+__all__ = [
+    "BasicHttpRouter",
+    "BasicWebSocketRouter"
+]
