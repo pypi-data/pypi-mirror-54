102d0fe101d657fc89867e32f8778f2258a4f433
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:44 +0000

    Add package module-package/0.009/module_package-0.009.tar.gz

---
+++ packages/module-package/module_package-0.009.tar.gz/module_package-0.009/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup (
+	name = 'module_package',
+	author= 'VISHNU',
+	author_email='vishnu.r@ignitarium.com',
+	version = '0.009',
+	py_modules=['module_file']
+)
