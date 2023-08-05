b8c2d83285a41365167950b4f63e0fed14518c52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:46 +0000

    Add package pretix-telephone/2.4.0/pretix_telephone-2.4.0-py3-none-any.whl

---
+++ packages/pretix-telephone/pretix_telephone-2.4.0-py3-none-any.whl/pretix_telephone/__init__.py
@@ -0,0 +1,20 @@
+from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy
+
+
+class PluginApp(AppConfig):
+    name = 'pretix_telephone'
+    verbose_name = 'Pretix Phone Number Field'
+
+    class PretixPluginMeta:
+        name = ugettext_lazy('Pretix Phone Number Field')
+        author = 'Felix Rindt'
+        description = ugettext_lazy('This plugin adds a contact question asking for the telephone number.')
+        visible = True
+        version = '2.4.0'
+
+    def ready(self):
+        from . import signals  # NOQA
+
+
+default_app_config = 'pretix_telephone.PluginApp'
