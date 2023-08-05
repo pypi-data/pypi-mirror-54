b0522ff6e6dc16c6358c904b48bc48f8f4f724ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:23 +0000

    Add package hdptest/0.1/hdptest-0.1.tar.gz

---
+++ packages/hdptest/hdptest-0.1.tar.gz/hdptest-0.1/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='hdptest',
+    version='0.1',
+    scripts=[],
+    author="Ismael Figueroa",
+    author_email="ifigueroap@gmail.com",
+    description="Mecanismo simple para tests en Jupyter Notebooks",
+    url="https://github.com/ifigueroap/hdptest",
+    packages=setuptools.find_packages()
+)
