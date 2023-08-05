bbe1e43b0291b429d03742cc421c36af510a0cd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:41 +0000

    Add package me-nd-distributions/0.1/me-nd-distributions-0.1.tar.gz

---
+++ packages/me-nd-distributions/me-nd-distributions-0.1.tar.gz/me-nd-distributions-0.1/README.md
@@ -0,0 +1,4 @@
+# me-nd-distributions package
+
+Package to calculcate Gaussian and Binomial distributions and plot them
+
---
+++ packages/me-nd-distributions/me-nd-distributions-0.1.tar.gz/me-nd-distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='me-nd-distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['me-nd-distributions'],
+      zip_safe=False)
