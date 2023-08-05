947aeb9577e62f9a51477c069f932c91ffcc6f66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:03 +0000

    Add package ward/0.4.0a0/ward-0.4.0a0.tar.gz

---
+++ packages/ward/ward-0.4.0a0.tar.gz/ward-0.4.0a0/ward/test_result.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+from enum import Enum, auto
+from typing import Optional
+
+from ward.test import Test
+
+
+class TestOutcome(Enum):
+    PASS = auto()
+    FAIL = auto()
+    SKIP = auto()
+
+
+@dataclass
+class TestResult:
+    test: Test
+    outcome: TestOutcome
+    error: Optional[Exception]
+    message: str
