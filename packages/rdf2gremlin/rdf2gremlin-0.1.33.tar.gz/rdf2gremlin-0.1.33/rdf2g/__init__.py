e56b4007a350570bbcdff6599ee34f9930b0bcc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:53 +0000

    Add package rdf2gremlin/0.1.33/rdf2gremlin-0.1.33.tar.gz

---
+++ packages/rdf2gremlin/rdf2gremlin-0.1.33.tar.gz/rdf2gremlin-0.1.33/rdf2g/__init__.py
@@ -0,0 +1,17 @@
+"""
+__init__.py
+Date: 13.10.19
+Author: Eugeniu Costetchi
+Email: costezki.eugen@gmail.com
+"""
+
+import logging
+from rdf2g.operations import *
+
+__version__ = "0.1.33"
+
+level = logging.INFO
+
+logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s',
+                    level=level)
+logging.getLogger().setLevel(level)
