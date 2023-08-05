573c9f2b2d8ccf0270ecb9939e90b8c2cae58c87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:04 +0000

    Add package awkward1/0.1.17/awkward1-0.1.17-cp35-cp35m-manylinux1_i686.whl

---
+++ packages/awkward1/awkward1-0.1.17-cp35-cp35m-manylinux1_i686.whl/awkward1/operations/convert.py
@@ -0,0 +1,29 @@
+# BSD 3-Clause License; see https://github.com/jpivarski/awkward-1.0/blob/master/LICENSE
+
+import numbers
+
+import numpy
+
+import awkward1.util
+import awkward1.layout
+
+def tolist(array):
+    if array is None or isinstance(array, (bool, str, bytes, numbers.Number)):
+        return array
+
+    elif isinstance(array, numpy.ndarray):
+        return array.tolist()
+
+    elif isinstance(array, awkward1.layout.FillableArray):
+        return [tolist(x) for x in array]
+
+    elif isinstance(array, awkward1.layout.NumpyArray):
+        return numpy.asarray(array).tolist()
+
+    elif isinstance(array, awkward1.layout.Content):
+        return [tolist(x) for x in array]
+
+    else:
+        raise TypeError("unrecognized array type: {0}".format(repr(array)))
+
+__all__ = [x for x in list(globals()) if not x.startswith("_") and x not in ("awkward1", "numpy")]
