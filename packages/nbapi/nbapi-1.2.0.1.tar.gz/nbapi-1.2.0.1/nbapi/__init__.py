83e3a53f9674772917187aed22d27eff524226e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:18 +0000

    Add package nbapi/1.2.0.1/nbapi-1.2.0.1.tar.gz

---
+++ packages/nbapi/nbapi-1.2.0.1.tar.gz/nbapi-1.2.0.1/nbapi/__init__.py
@@ -0,0 +1,39 @@
+import asyncio,requests
+from random import randint
+
+#Note: Using this API you are going to be using a API that MAY (unlikely) But MAY send NSFW results.
+#If that happens, DM my owner (LazyNeko#5644), or join the Neko Support Server from the website.
+
+#Nothing much yet :3
+
+#                   #-------------------------------------------------#
+#                       To join: Copy the below link.  
+#                       https://discord.gg/RauzUYK
+#                   #--------------------------------------------------#
+
+#You may edit this code to your needs. I dont mind ;3
+#TODO: make requests smaller (DONE)
+#TODO: make "hug"&"pat" animated&static endpoint! (PAT FINISHED, HUG WILL BE DONE LATER)
+class random():
+    def anime():
+        return f"http://neko-bot.net/anime/{str(randint(int(1),int(str(requests.get('http://neko-bot.net/info/totalanime.txt').text))))}.png"
+    def neko():
+
+                
+            return f"http://neko-bot.net/nekos/{str(randint(int(1),int(str(requests.get('http://neko-bot.net/info/totalnekos.txt').text))))}.png"
+    class pat():
+        def animated():
+            return f"http://neko-bot.net/pats/animated/{str(randint(int(1),int(str(requests.get('http://neko-bot.net/info/totalpats.txt').text))))}.png"
+        def static():
+            return f"http://neko-bot.net/pats/static/{str(randint(int(1),int(str(requests.get('http://neko-bot.net/info/totalpats_a.txt').text))))}.png"
+    class hug():
+        def animated():
+            return f"http://neko-bot.net/hugs/animated/{str(randint(int(1),int(str(requests.get('http://neko-bot.net/info/totalpats.txt').text))))}.png"#NOT FINISHED
+        def static():
+            return f"http://neko-bot.net/hugs/static/{str(randint(int(1),int(str(requests.get('http://neko-bot.net/info/totalpats.txt').text))))}.png"#NOT FINISHED
+class search():
+    def neko(num):
+        return 'http://neko-bot.net/nekos/neko'+str(num)+'.png'
+    def anime(num):
+        return 'http://neko-bot.net/posts/post'+str(num)+'.png'
+#print(random.pat() + random.neko() + random.anime())   #DEBUG
---
+++ packages/nbapi/nbapi-1.2.0.1.tar.gz/nbapi-1.2.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.txt", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nbapi",
+    version="1.2.0.1",
+    author="LazyNeko",
+    author_email="nekobot.help@gmail.com",
+    description="A small anime API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lazyneko1/nbapi",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
