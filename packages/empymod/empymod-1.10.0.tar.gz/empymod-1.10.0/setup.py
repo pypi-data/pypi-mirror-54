856efb2e3babedb0dc70ca08ef5baf5414ae5758
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:00 +0000

    Add package empymod/1.10.0/empymod-1.10.0.tar.gz

---
+++ packages/empymod/empymod-1.10.0.tar.gz/empymod-1.10.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+readme = open('README.rst').read()
+
+description = 'Open-source full 3D electromagnetic modeller for 1D VTI media'
+
+setup(
+    name='empymod',
+    version='1.10.0',
+    description=description,
+    long_description=readme,
+    author='The empymod Developers',
+    author_email='dieter@werthmuller.org',
+    url='https://empymod.github.io',
+    download_url='https://github.com/empymod/empymod/tarball/v1.10.0',
+    license='Apache License V2.0',
+    packages=['empymod', 'empymod.scripts'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    install_requires=[
+        'numpy',
+        'scipy>=1.0.0',
+    ],
+)
