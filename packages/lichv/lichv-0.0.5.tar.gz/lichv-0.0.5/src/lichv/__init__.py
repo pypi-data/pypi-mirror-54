21493ba4728674f6e174acc26b67108e58f9e89c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:09 +0000

    Add package lichv/0.0.5/lichv-0.0.5.tar.gz

---
+++ packages/lichv/lichv-0.0.5.tar.gz/lichv-0.0.5/src/lichv/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+from .browser import *
+from .mongodb import *
+from .mysqldb import *
+from .sms import *
+from .wechat import *
+ 
+__version__ = '0.0.5'
+__license__ = 'MIT'
