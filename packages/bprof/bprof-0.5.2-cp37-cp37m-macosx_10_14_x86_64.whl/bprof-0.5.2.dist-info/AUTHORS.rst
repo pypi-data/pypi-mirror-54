779f4817a485c590c44738f43caea76cf4979c58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:24 +0000

    Add package bprof/0.5.2/bprof-0.5.2-cp37-cp37m-macosx_10_14_x86_64.whl

---
+++ packages/bprof/bprof-0.5.2-cp37-cp37m-macosx_10_14_x86_64.whl/bprof-0.5.2.dist-info/AUTHORS.rst
@@ -0,0 +1,13 @@
+=======
+Credits
+=======
+
+Development Lead
+----------------
+
+* Joel Frederico <joelfrederico@gmail.com>
+
+Contributors
+------------
+
+None yet. Why not be the first?
---
+++ packages/bprof/bprof-0.5.2-cp37-cp37m-macosx_10_14_x86_64.whl/bprof/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for bprof."""
+
+__author__ = """Joel Frederico"""
+__email__ = 'joelfrederico@gmail.com'
+__version__ = '0.5.2'
+
+from ._bprof import start, stop, dump
