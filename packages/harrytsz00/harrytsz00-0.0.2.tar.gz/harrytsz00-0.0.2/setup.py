ec2ac31b6e3329e2baa919af66338829fddaa220
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:00 +0000

    Add package harrytsz00/0.0.2/harrytsz00-0.0.2.tar.gz

---
+++ packages/harrytsz00/harrytsz00-0.0.2.tar.gz/harrytsz00-0.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='harrytsz00',
+      version='0.0.2',
+      description='string to date',
+      url='https://github.com/BigDataFounder/string2date',
+      author='harrytsz',
+      author_email='taosizhan@gmail.com',
+      license='MIT',
+      packages=['string2date'])
