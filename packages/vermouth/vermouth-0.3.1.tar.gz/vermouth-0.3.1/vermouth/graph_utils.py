24fe42164e4d319fd701727d3aceba4ce7ab359a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:58 +0000

    Add package vermouth/0.3.1/vermouth-0.3.1.tar.gz

---
+++ packages/vermouth/vermouth-0.3.1.tar.gz/vermouth-0.3.1/vermouth/graph_utils.py
@@ -0,0 +1,278 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright 2018 University of Groningen
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import itertools
+import networkx as nx
+
+from .utils import maxes, first_alpha
+
+
+def add_element_attr(molecule):
+    """
+    Adds an element attribute to every node in `molecule`, based on that node's
+    atomname attribute.
+
+    Parameters
+    ----------
+    molecule: networkx.Graph
+        The graph of which nodes should get an element attribute.
+
+    Raises
+    ------
+    ValueError
+        If no element could be guessed for a node.
+
+    """
+    for node_idx in molecule:
+        node = molecule.nodes[node_idx]
+        if 'element' not in node:
+            try:
+                element = first_alpha(node['atomname'])
+            except KeyError:
+                raise ValueError('Cannot guess the element of atom {}: '
+                                 'the node has no atom name.'
+                                 .format(node_idx))
+            except ValueError:
+                raise ValueError('Cannot guess the element of atom {}: '
+                                 'the atom name has no alphabetic charater.'
+                                 .format(node_idx))
+            node['element'] = element
+
+
+def categorical_cartesian_product(graph1, graph2, attributes=tuple()):
+    product = nx.Graph()  # FIXME graphtype?
+    for idx1, idx2 in itertools.product(graph1, graph2):
+        node1 = graph1.nodes[idx1]
+        node2 = graph2.nodes[idx2]
+        if all((attr in node1 and attr in node2 and node1[attr] == node2[attr])
+               or (attr not in node1 and attr not in node2) for attr in attributes):
+            attrs = {}
+            for attr in set(node1.keys()) | set(node2.keys()):
+                attrs[attr] = (node1.get(attr, None), node2.get(attr, None))
+            product.add_node((idx1, idx2), **attrs)
+    return product
+
+
+def categorical_modular_product(graph1, graph2, attributes=tuple()):
+    product = categorical_cartesian_product(graph1, graph2, attributes)
+    for (graph1_node1, graph2_node1), (graph1_node2, graph2_node2) in itertools.combinations(product.nodes(), 2):
+        graph1_nodes = graph1_node1, graph1_node2
+        graph2_nodes = graph2_node1, graph2_node2
+        both_edge = graph1.has_edge(*graph1_nodes) and graph2.has_edge(*graph2_nodes)
+        neither_edge = not graph1.has_edge(*graph1_nodes) and\
+                       not graph2.has_edge(*graph2_nodes)
+        # Effectively: not (graph1.has_edge(graph1_node1, graph1_node2) xor graph2.has_edge(graph2_node1, graph2_node2))
+        if graph1_node1 != graph1_node2 and graph2_node1 != graph2_node2 and\
+                (both_edge or neither_edge):
+            attrs = {}
+            if both_edge:
+                g1_edge_keys = set(graph1.edges[graph1_nodes].keys())
+                g2_edge_keys = set(graph2.edges[graph2_nodes].keys())
+                for attr in g1_edge_keys | g2_edge_keys:
+                    attrs[attr] = (graph1.edges[graph1_nodes].get(attr, None),
+                                   graph2.edges[graph2_nodes].get(attr, None))
+            product.add_edge((graph1_node1, graph2_node1), (graph1_node2, graph2_node2), **attrs)
+    return product
+
+
+def categorical_maximum_common_subgraph(graph1, graph2, attributes=tuple()):
+    product = categorical_modular_product(graph1, graph2, attributes)
+    cliques = nx.find_cliques(product)
+    # cliques is an iterator which will return a *lot* of items. So make sure
+    # we never turn it into a full list.
+    largest = maxes(cliques, key=len)
+    matches = [dict(clique) for clique in largest]
+    return matches
+
+
+def blockmodel(G, partitions, **attrs):
+    """
+    Analogous to networkx.blockmodel, but can deal with incomplete partitions,
+    and assigns ``attrs`` to nodes.
+
+    Parameters
+    ----------
+    G: networkx.Graph
+        The graph to partition
+    parititions: collections.abc.Iterable[collections.abc.Iterable]
+        Each element contains the node indices that construct the new node.
+    **attrs: dict[str, collections.abc.Iterable]
+        Attributes to assign to new nodes. Attribute values are assigned to the
+        new nodes in order.
+
+    Returns
+    -------
+    networkx.Graph
+        A new graph where every node is a subgraph as specified by partitions.
+        Node attributes:
+
+            :graph: Subgraph of constructing nodes.
+            :nnodes: Number of nodes in ``graph``.
+            :nedges: Number of edges in ``graph``.
+            :density: Density of ``graph``.
+            :attrs.keys(): As specified by ``**attrs``.
+    """
+    # TODO: Change this to use nx.quotient_graph.
+    attrs = {key: list(val) for key, val in attrs.items()}
+    CG_mol = nx.Graph()
+    for bead_idx, idxs in enumerate(partitions):
+        bd = G.subgraph(idxs)
+        CG_mol.add_node(bead_idx)
+        CG_mol.nodes[bead_idx]['graph'] = bd
+        # TODO: CoM instead of CoG
+#        CG_mol.nodes[bead_idx]['position'] = np.mean([bd.nodes[idx]['position'] for idx in bd], axis=0)
+        for k, vals in attrs.items():
+            CG_mol.nodes[bead_idx][k] = vals[bead_idx]
+
+        CG_mol.nodes[bead_idx]['nnodes'] = bd.number_of_nodes()
+        CG_mol.nodes[bead_idx]['nedges'] = bd.number_of_edges()
+        CG_mol.nodes[bead_idx]['density'] = nx.density(bd)
+
+    block_mapping = {}
+    for n in CG_mol:
+        nodes_in_block = CG_mol.nodes[n]['graph'].nodes()
+        block_mapping.update(dict.fromkeys(nodes_in_block, n))
+
+    for u, v, d in G.edges(data=True):
+        try:
+            bmu = block_mapping[u]
+            bmv = block_mapping[v]
+        except KeyError:
+            # Atom not represented
+            continue
+        if bmu == bmv:  # no self loops
+            continue
+        # For graphs and digraphs add single weighted edge
+        weight = d.get('weight', 1.0)  # default to 1 if no weight specified
+        if CG_mol.has_edge(bmu, bmv):
+            CG_mol[bmu][bmv]['weight'] += weight
+        else:
+            CG_mol.add_edge(bmu, bmv, weight=weight)
+    return CG_mol
+
+
+def rate_match(residue, bead, match):
+    """
+    A helper function which rates how well ``match`` describes the isomorphism
+    between ``residue`` and ``bead`` based on the number of matching atomnames.
+
+
+    Parameters
+    ----------
+    residue : networkx.Graph
+        A graph. Required node attributes:
+
+            :atomname: The name of an atom.
+
+    bead : networkx.Graph
+        A subgraph of ``residue`` where the isomorphism is described by ``match``.
+        Required node attributes:
+
+            :atomname: The name of an atom.
+
+
+    Returns
+    -------
+    int
+        The number of entries in match where the atomname in ``residue`` matches
+        the atomname in ``bead``.
+    """
+    return sum(residue.nodes[rdx].get('atomname') == bead.nodes[bdx].get('atomname')
+               for rdx, bdx in match.items())
+
+
+def make_residue_graph(mol):
+    """
+    Creates a graph with one node per residue; as identified by the tuple
+    (chain identifier, residue index, residue name).
+
+    Parameters
+    ----------
+    mol: networkx.Graph
+        The atomistic graph. Required node attributes:
+
+            :chain: The chain identifier.
+            :resid: The residue index.
+            :resname: The residue name.
+
+    Returns
+    -------
+    networkx.Graph
+        A graph with one node per residue. Node attributes:
+
+            :chain: The chain identifier.
+            :graph: The atomistic subgraph.
+            :density: The density of ``graph``.
+            :nedges: The number of edges in ``graph``.
+            :nnodes: The number of nodes in ``graph``.
+            :resid: The residue index.
+            :resname: The residue name.
+            :atomname: The residue name.
+    """
+    def keyfunc(node_idx):
+        return mol.nodes[node_idx]['chain'], mol.nodes[node_idx]['resid'], mol.nodes[node_idx]['resname']
+    nodes = sorted(mol.nodes, key=keyfunc)
+    keys = []
+    grps = []
+    for key, grp in itertools.groupby(nodes, keyfunc):
+        keys.append(key)
+        grps.append(list(grp))
+    if keys:
+        chain, resids, resnames = map(list, zip(*keys))
+    else:
+        chain, resids, resnames = [], [], []
+    res_graph = blockmodel(mol, grps, chain=chain, resid=resids,
+                           resname=resnames, atomname=resnames)
+    return res_graph
+
+
+# We can't inherit from nx.isomorphism.GraphMatcher to override
+# `semantic_feasibility`. That implementation will clobber this one's method.
+class MappingGraphMatcher(nx.isomorphism.isomorphvf2.GraphMatcher):
+    def __init__(self, *args, edge_match=None, node_match=None, **kwargs):
+        self.edge_match = edge_match
+        self.node_match = node_match
+        super().__init__(*args, **kwargs)
+        self.G1_adj = self.G1.adj
+
+    def semantic_feasibility(self, G1_node, G2_node):
+        """
+        Returns True if mapping G1_node to G2_node is semantically feasible.
+        Adapted from networkx.algorithms.isomorphism.vf2userfunc._semantic_feasibility.
+        """
+        # Make sure the nodes match
+        if self.node_match is not None:
+            nm = self.node_match(self.G1.nodes[G1_node], self.G2.nodes[G2_node])
+            if not nm:
+                return False
+
+        # Make sure the edges match
+        if self.edge_match is not None:
+
+            # Cached lookups
+            core_1 = self.core_1
+            edge_match = self.edge_match
+
+            for neighbor in self.G1_adj[G1_node]:
+                # G1_node is not in core_1, so we must handle R_self separately
+                if neighbor == G1_node:
+                    if not edge_match(G1_node, G1_node, G2_node, G2_node):
+                        return False
+                elif neighbor in core_1:
+                    if not edge_match(G1_node, neighbor, G2_node, core_1[neighbor]):
+                        return False
+            # syntactic check has already verified that neighbors are symmetric
+        return True
---
+++ packages/vermouth/vermouth-0.3.1.tar.gz/vermouth-0.3.1/vermouth/processors/make_bonds.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright 2018 University of Groningen
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Provides a processor that can add edges to a graph based on geometric criteria.
+"""
+
+
+import networkx as nx
+import numpy as np
+
+from .. import KDTree
+from ..molecule import Molecule
+from .processor import Processor
+
+# Van der Waals radii from A. Bondi, J. Phys. Chem., 68, 441-452, 1964.
+# https://doi.org/10.1021/j100785a001
+# For hydrogen, we use R.S. Rowland & R. Taylor, J.Phys.Chem., 100, 7384-7391, 1996.
+# https://doi.org/10.1021/jp953141
+VDW_RADII = {  # in nm
+    'H': 0.120,
+    'He': 0.140,
+    'C': 0.170,
+    'N': 0.155,
+    'O': 0.152,
+    'F': 0.147,
+    'Ne': 0.154,
+    'Si': 0.210,
+    'P': 0.180,
+    'S': 0.180,
+    'Cl': 0.175,
+    'Ar': 0.188,
+    'As': 0.185,
+    'Se': 1.90,
+    'Br': 0.185,
+    'Kr': 0.202,
+    'Te': 0.206,
+    'I': 0.198,
+    'Xe': 0.216,
+}
+#VALENCES = {'H': 1, 'C': 4, 'N': 3, 'O': 2, 'S': 6}
+
+
+def bonds_from_distance(system, fudge=1.2):
+    """
+    Creates edges between nodes of molecules in system based on a distance
+    criterion. Nodes in system must have `position` and `element` attributes.
+    The possible distance between nodes is determined by values in
+    `VDW_RADII`.
+
+    Notes
+    -----
+    Elements that are not in `VDW_RADII` do not make bonds.
+
+    Parameters
+    ----------
+    system: :class:`~vermouth.system.System`
+        The system in which to add edges.
+    fudge: :class:`~numbers.Number`
+        Increase the allowed distance by this factor.
+
+    Returns
+    -------
+    :class:`networkx.Graph`
+        A new graph where edges are added between nodes that are within a
+        certain distance from each other. It is probably disconnected.
+    """
+    system = nx.disjoint_union_all(system.molecules)
+    # We filter out the nodes for which we do not know the radius. Indeed, we
+    # consider these nodes cannot make bonds. The filtering is done before we
+    # enter the KDTree; we only provide to the KDTree the position of the nodes
+    # that could make a bond. `idx_to_nodenum` make the link between the
+    # indices in the `positions` array, and the node keys in the `system`
+    # graph.
+    idx_to_nodenum = {
+        idx: n
+        for idx, n in enumerate(
+            subn
+            for subn in system
+            if system.nodes[subn].get('element') in VDW_RADII
+        )
+    }
+
+    # We also make a set of all nodes that have gotten bonds from CONECT records
+    # (in case of PDB input). Later on we only make new bonds between pairs of
+    # atoms if at least one of those does *not* have a CONECT record. If both
+    # have CONECT records the bond between them should also have been specified.
+    has_conect = {idx for idx in system if system[idx]}
+    max_dist = max(
+        VDW_RADII[node.get('element')]
+        for node in system.nodes.values()
+        if node.get('element') in VDW_RADII
+    )
+    positions = np.array([
+        node['position']
+        for node in system.nodes.values()
+        if node.get('element') in VDW_RADII
+    ], dtype=float)
+    tree = KDTree(positions)
+    pairs = tree.sparse_distance_matrix(tree, max_dist * fudge)
+
+    for (idx1, idx2), dist in pairs.items():
+        if idx1 >= idx2 or (idx1 in has_conect and idx2 in has_conect):
+            continue
+        node_idx1 = idx_to_nodenum[idx1]
+        node_idx2 = idx_to_nodenum[idx2]
+        atom1 = system.nodes[node_idx1]
+        atom2 = system.nodes[node_idx2]
+        element1 = atom1['element']
+        element2 = atom2['element']
+
+        bond_distance = 0.5 * (VDW_RADII[element1] + VDW_RADII[element2])
+        if dist <= bond_distance * fudge:
+            system.add_edge(node_idx1, node_idx2, distance=dist)
+    return system
+
+
+class MakeBonds(Processor):
+    def run_system(self, system):
+        if not system.molecules:
+            # No molecules means nothing to do.
+            return
+        mols = bonds_from_distance(system)
+        system.molecules = list(map(Molecule, (mols.subgraph(mol)
+                                               for mol in nx.connected_components(mols))))
+        # Restore the force field in each molecule. Setting the force field
+        # at the system level propagates it to all the molecules.
+        system.force_field = system.force_field
