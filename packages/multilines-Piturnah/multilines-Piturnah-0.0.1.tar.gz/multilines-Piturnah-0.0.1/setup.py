2f8e723f2b91d72fccd49a7d4ed85100d3ddd8b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:51 +0000

    Add package multilines-Piturnah/0.0.1/multilines-Piturnah-0.0.1.tar.gz

---
+++ packages/multilines-Piturnah/multilines-Piturnah-0.0.1.tar.gz/multilines-Piturnah-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="multilines-Piturnah",
+    version="0.0.1",
+    author="Piturnah",
+    author_email="peterhebden6@gmail.com",
+    description="Beautification tools for text based devs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Piturnah/multilines",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
