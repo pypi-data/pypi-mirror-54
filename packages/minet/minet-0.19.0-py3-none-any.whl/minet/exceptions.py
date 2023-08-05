f6b489b350d3760ed49993ac45c8a6c7f8123d09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:41 +0000

    Add package minet/0.19.0/minet-0.19.0-py3-none-any.whl

---
+++ packages/minet/minet-0.19.0-py3-none-any.whl/minet/exceptions.py
@@ -0,0 +1,33 @@
+# =============================================================================
+# Minet Custom Exceptions
+# =============================================================================
+#
+# Collection of handy custom exceptions.
+#
+
+
+# General errors
+class UnknownEncodingError(Exception):
+    pass
+
+
+# Miscellaneous HTTP errors
+class InvalidURLError(Exception):
+    pass
+
+
+# Redirection errors
+class MaxRedirectsError(Exception):
+    pass
+
+
+class InfiniteRedirectsError(Exception):
+    pass
+
+
+class SelfRedirectError(InfiniteRedirectsError):
+    pass
+
+
+class InvalidRedirectError(Exception):
+    pass
