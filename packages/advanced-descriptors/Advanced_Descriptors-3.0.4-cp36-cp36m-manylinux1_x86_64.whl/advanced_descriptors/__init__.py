bda255947dcc865a392632c9b5b184d1c87d00c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:08 +0000

    Add package advanced-descriptors/3.0.4/Advanced_Descriptors-3.0.4-cp36-cp36m-manylinux1_x86_64.whl

---
+++ packages/advanced-descriptors/Advanced_Descriptors-3.0.4-cp36-cp36m-manylinux1_x86_64.whl/advanced_descriptors/__init__.py
@@ -0,0 +1,44 @@
+#    Copyright 2017 - 2019 Alexey Stepanov aka penguinolog
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""Advanced descriptors for special cases."""
+
+import pkg_resources
+
+from .advanced_property import AdvancedProperty
+from .log_on_access import LogOnAccess
+from .separate_class_method import SeparateClassMethod
+
+__all__ = ("SeparateClassMethod", "AdvancedProperty", "LogOnAccess")
+
+try:
+    __version__ = pkg_resources.get_distribution(__name__).version
+except pkg_resources.DistributionNotFound:
+    # package is not installed, try to get from SCM
+    try:
+        import setuptools_scm  # type: ignore
+
+        __version__ = setuptools_scm.get_version()
+    except ImportError:
+        setuptools_scm = None
+
+__author__ = "Alexey Stepanov"
+__author_email__ = "penguinolog@gmail.com"
+__maintainers__ = {
+    "Alexey Stepanov": "penguinolog@gmail.com",
+    "Antonio Esposito": "esposito.cloud@gmail.com",
+    "Dennis Dmitriev": "dis-xcom@gmail.com",
+}
+__url__ = "https://github.com/python-useful-helpers/advanced-descriptors"
+__description__ = "Advanced descriptors for special cases."
+__license__ = "Apache License, Version 2.0"
