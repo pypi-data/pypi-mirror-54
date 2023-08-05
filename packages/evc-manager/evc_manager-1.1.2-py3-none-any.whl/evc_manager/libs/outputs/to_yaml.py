0a83681596b248898b06a0fc622d5018ac9f2cd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:20 +0000

    Add package evc-manager/1.1.2/evc_manager-1.1.2-py3-none-any.whl

---
+++ packages/evc-manager/evc_manager-1.1.2-py3-none-any.whl/evc_manager/libs/outputs/to_yaml.py
@@ -0,0 +1,30 @@
+""" YAML module. Used to read and write in YAML format """
+
+from datetime import datetime
+from yaml import dump
+import yaml
+from libs.core.cli import CliOptions
+
+
+class MyDumper(yaml.Dumper):  # pylint: disable=R0901
+    """ Ignore aliases when printing dictionaries using YAML """
+    def ignore_aliases(self, _data):  # pylint: disable=W0221
+        return True
+
+
+def print_yaml(ctks):
+    """ Exported function to print in YAML """
+    print("Number of Circuits: %s " % len(ctks))
+
+    evcs = dict()
+    evcs['evcs'] = ctks
+    evcs['version'] = '1.0'
+
+    now = datetime.now()
+    now.strftime("%Y/%m/%d %H:%M:%S")
+    evcs['date'] = now
+
+    with open(CliOptions().destination_file, 'w') as yaml_file:
+        dump(evcs, yaml_file, default_flow_style=False, Dumper=MyDumper)
+        print("List of circuits saved on file %s: " %
+              CliOptions().destination_file)
