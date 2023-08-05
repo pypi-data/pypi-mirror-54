53681534aa393cd9356c7dbaf20043c67d389c91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:17 +0000

    Add package factionpy/0.0.4/factionpy-0.0.4-py3-none-any.whl

---
+++ packages/factionpy/factionpy-0.0.4-py3-none-any.whl/factionpy/models/user_role.py
@@ -0,0 +1,11 @@
+from factionpy.backend.database import db
+
+
+class UserRole(db.Model):
+    __tablename__ = "UserRole"
+    Id = db.Column(db.Integer, primary_key=True)
+    Name = db.Column(db.String, nullable=False, unique=True)
+    Users = db.relationship('User', backref='UserRole', lazy=True)
+
+    def __repr__(self):
+        return '<Role: %s>' % str(self.Id)
