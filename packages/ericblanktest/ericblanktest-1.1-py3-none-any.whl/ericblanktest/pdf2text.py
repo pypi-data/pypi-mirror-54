96bb39c2d3a3d8d9047e798ea7e362e5d08448a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:31 +0000

    Add package ericblanktest/1.1/ericblanktest-1.1-py3-none-any.whl

---
+++ packages/ericblanktest/ericblanktest-1.1-py3-none-any.whl/ericblanktest/pdf2text.py
@@ -0,0 +1,15 @@
+""" This module provides functions to convert PDF to text """
+
+
+def convert():
+    """
+    Convert a pdf to text
+
+    Parameters:
+    path (str): The path to the PDF file
+
+    Returns:
+    str: The content of the PDF file 
+    """
+
+    print("pdf2text")
