299483a8cc8273b5e8ee196485f03ecb521d3456
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:58 +0000

    Add package pyade-python/1.1/pyade-python-1.1.tar.gz

---
+++ packages/pyade-python/pyade-python-1.1.tar.gz/pyade-python-1.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+import setuptools
+
+setup(
+    name='pyade-python',
+    version='1.1',
+    packages=['pyade'],
+    url='https://github.com/xKuZz/pyade',
+    license='MIT',
+    author='xKuZz',
+    author_email='davidcr96@gmail.com',
+    description='A Python Advanced Differential Evolution algorithms library',
+)
