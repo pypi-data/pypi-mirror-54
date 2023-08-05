b1a0ebff55df843c1ce73d0c6048f40a1096b188
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:54 +0000

    Add package FinMesh/1.1/FinMesh-1.1-py3-none-any.whl

---
+++ packages/FinMesh/FinMesh-1.1-py3-none-any.whl/FinMesh/iex/_common.py
@@ -0,0 +1,25 @@
+import os
+import requests
+
+def append_iex_token(url):
+    sandboxState = os.getenv('SANDBOX')
+    if sandboxState is True:
+        token = os.getenv('IEX_SANDBOX_TOKEN')
+    else:
+        token = os.getenv('IEX_TOKEN')
+    return f"{url}&token={token}"
+
+def get_iex_json_request(url, vprint=False):
+    url = append_iex_token(url)
+    if vprint: print(f"Making request: {url}")
+    result = requests.get(url)
+    if vprint: print(f"Request status code: {result.status_code}")
+    if result.status_code != 200:
+        raise BaseException(result.text)
+    result = result.json()
+    return result
+
+def replace_url_var(url, **kwargs):
+    for key, value in kwargs.items():
+        url = url.replace('{' + key + '}', value)
+    return url
