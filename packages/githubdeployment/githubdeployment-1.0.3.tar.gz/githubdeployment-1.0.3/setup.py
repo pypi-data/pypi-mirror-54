a01773fb16ea576f3c498952a3a85c7cf0f2ef1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:33 +0000

    Add package githubdeployment/1.0.3/githubdeployment-1.0.3.tar.gz

---
+++ packages/githubdeployment/githubdeployment-1.0.3.tar.gz/githubdeployment-1.0.3/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="githubdeployment",
+    version="1.0.3",
+    author="Komron Miralizoda",
+    author_email="miralizoda.komron@gmail.com",
+    description="Simple tool for autodeploying github projects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/komron-m/github-deployment",
+    packages=setuptools.find_packages(),
+    install_requires=[
+            'gitpython==3.0.3',
+            'requests==2.22.0',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
