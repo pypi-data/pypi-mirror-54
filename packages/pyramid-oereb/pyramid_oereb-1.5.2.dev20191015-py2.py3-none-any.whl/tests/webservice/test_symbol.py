7d414dcc5195df242767a1dab36cf0f672c1f9b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:22 +0000

    Add package pyramid-oereb/1.5.2.dev20191015/pyramid_oereb-1.5.2.dev20191015-py2.py3-none-any.whl

---
+++ packages/pyramid-oereb/pyramid_oereb-1.5.2.dev20191015-py2.py3-none-any.whl/tests/webservice/test_symbol.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import pytest
+from pyramid.httpexceptions import HTTPNotFound
+from pyramid.response import Response
+
+from tests.mockrequest import MockRequest
+from pyramid_oereb.views.webservice import Symbol
+
+
+def test_get_image():
+    request = MockRequest()
+    request.matchdict.update({
+        'theme_code': 'ContaminatedSites',
+        'view_service_id': '1',
+        'type_code': 'CodeA'
+    })
+    webservice = Symbol(request)
+    result = webservice.get_image()
+    assert isinstance(result, Response)
+    if sys.version_info.major == 2:
+        assert result.body == '1'
+    else:
+        assert result.body == b'1'
+
+
+def test_get_image_invalid():
+    request = MockRequest()
+    request.matchdict.update({
+        'theme_code': 'ContaminatedSites',
+        'view_service_id': '1',
+        'type_code': 'notExisting'
+    })
+    webservice = Symbol(request)
+    with pytest.raises(HTTPNotFound):
+        webservice.get_image()
