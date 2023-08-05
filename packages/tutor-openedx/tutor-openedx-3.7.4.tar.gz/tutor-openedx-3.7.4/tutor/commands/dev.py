7d308ab860dc2ba5f043796df9bc968ad4c52f31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:47 +0000

    Add package tutor-openedx/3.7.4/tutor-openedx-3.7.4.tar.gz

---
+++ packages/tutor-openedx/tutor-openedx-3.7.4.tar.gz/tutor-openedx-3.7.4/tutor/commands/dev.py
@@ -0,0 +1,152 @@
+import subprocess
+
+import click
+
+from .. import env as tutor_env
+from .. import opts
+from .. import utils
+
+
+@click.group(help="Run Open edX platform with development settings")
+def dev():
+    pass
+
+
+@click.command(
+    help="Run a command in one of the containers",
+    context_settings={"ignore_unknown_options": True},
+)
+@opts.root
+@opts.edx_platform_path
+@opts.edx_platform_settings
+@click.argument("service")
+@click.argument("command", default=None, required=False)
+@click.argument("args", nargs=-1)
+def run(root, edx_platform_path, edx_platform_settings, service, command, args):
+    run_command = [service]
+    if command:
+        run_command.append(command)
+    if args:
+        run_command += args
+    port = service_port(service)
+    docker_compose_run_with_port(
+        root, edx_platform_path, edx_platform_settings, port, *run_command
+    )
+
+
+@click.command(
+    help="Exec a command in a running container",
+    context_settings={"ignore_unknown_options": True},
+)
+@opts.root
+@click.argument("service")
+@click.argument("command")
+@click.argument("args", nargs=-1)
+def execute(root, service, command, args):
+    exec_command = ["exec", service, command]
+    if args:
+        exec_command += args
+    docker_compose(root, *exec_command)
+
+
+@click.command(help="Run a development server")
+@opts.root
+@opts.edx_platform_path
+@opts.edx_platform_settings
+@click.argument("service", type=click.Choice(["lms", "cms"]))
+def runserver(root, edx_platform_path, edx_platform_settings, service):
+    port = service_port(service)
+    from .. import fmt
+
+    fmt.echo_info(
+        "The {} service will be available at http://localhost:{}".format(service, port)
+    )
+    docker_compose_run_with_port(
+        root,
+        edx_platform_path,
+        edx_platform_settings,
+        port,
+        service,
+        "./manage.py",
+        service,
+        "runserver",
+        "0.0.0.0:{}".format(port),
+    )
+
+
+@click.command(help="Stop a running development platform")
+@opts.root
+def stop(root):
+    docker_compose(root, "rm", "--stop", "--force")
+
+
+@click.command(help="Watch for changes in your themes and recompile assets when needed")
+@opts.root
+@opts.edx_platform_path
+@opts.edx_platform_settings
+def watchthemes(root, edx_platform_path, edx_platform_settings):
+    docker_compose_run(
+        root,
+        edx_platform_path,
+        edx_platform_settings,
+        "--no-deps",
+        "lms",
+        "openedx-assets",
+        "watch-themes",
+        "--env",
+        "dev",
+    )
+
+
+def docker_compose_run_with_port(
+    root, edx_platform_path, edx_platform_settings, port, *command
+):
+    docker_compose_run(
+        root,
+        edx_platform_path,
+        edx_platform_settings,
+        "-p",
+        "{port}:{port}".format(port=port),
+        *command
+    )
+
+
+def docker_compose_run(root, edx_platform_path, edx_platform_settings, *command):
+    run_command = [
+        "run",
+        "--rm",
+        "-e",
+        "SETTINGS={}".format(edx_platform_settings),
+        "--volume={}:/openedx/themes".format(
+            tutor_env.pathjoin(root, "build", "openedx", "themes")
+        ),
+    ]
+    if edx_platform_path:
+        run_command += [
+            "--volume={}:/openedx/edx-platform".format(edx_platform_path),
+            "-e",
+            "USERID={}".format(subprocess.check_output(["id", "-u"]).strip().decode()),
+        ]
+    run_command += command
+    docker_compose(root, *run_command)
+
+
+def docker_compose(root, *command):
+    return utils.docker_compose(
+        "-f",
+        tutor_env.pathjoin(root, "local", "docker-compose.yml"),
+        "--project-name",
+        "tutor_dev",
+        *command
+    )
+
+
+def service_port(service):
+    return 8000 if service == "lms" else 8001
+
+
+dev.add_command(run)
+dev.add_command(execute, name="exec")
+dev.add_command(runserver)
+dev.add_command(stop)
+dev.add_command(watchthemes)
---
+++ packages/tutor-openedx/tutor-openedx-3.7.4.tar.gz/tutor-openedx-3.7.4/tutor/templates/apps/openedx/settings/partials/common_lms.py
@@ -0,0 +1,65 @@
+{% include "apps/openedx/settings/partials/common_all.py" %}
+
+######## Common LMS settings
+
+# Load module store settings from config files
+update_module_store_settings(MODULESTORE, doc_store_settings=DOC_STORE_CONFIG)
+
+# Set uploaded media file path
+MEDIA_ROOT = "/openedx/media/"
+
+# Video settings
+VIDEO_IMAGE_SETTINGS["STORAGE_KWARGS"]["location"] = MEDIA_ROOT
+VIDEO_TRANSCRIPTS_SETTINGS["STORAGE_KWARGS"]["location"] = MEDIA_ROOT
+
+# Change syslog-based loggers which don't work inside docker containers
+LOGGING["handlers"]["local"] = {
+    "class": "logging.handlers.WatchedFileHandler",
+    "filename": os.path.join(LOG_DIR, "all.log"),
+    "formatter": "standard",
+}
+LOGGING["handlers"]["tracking"] = {
+    "level": "DEBUG",
+    "class": "logging.handlers.WatchedFileHandler",
+    "filename": os.path.join(LOG_DIR, "tracking.log"),
+    "formatter": "standard",
+}
+LOGGING["loggers"]["tracking"]["handlers"] = ["console", "local", "tracking"]
+
+EMAIL_USE_SSL = {{ SMTP_USE_SSL }}
+
+# Fix media files paths
+VIDEO_IMAGE_SETTINGS["STORAGE_KWARGS"]["location"] = MEDIA_ROOT
+VIDEO_TRANSCRIPTS_SETTINGS["STORAGE_KWARGS"]["location"] = MEDIA_ROOT
+PROFILE_IMAGE_BACKEND["options"]["location"] = os.path.join(
+    MEDIA_ROOT, "profile-images/"
+)
+
+ORA2_FILEUPLOAD_BACKEND = "filesystem"
+ORA2_FILEUPLOAD_ROOT = "/openedx/data/ora2"
+ORA2_FILEUPLOAD_CACHE_NAME = "ora2-storage"
+
+GRADES_DOWNLOAD = {
+    "STORAGE_TYPE": "",
+    "STORAGE_KWARGS": {
+        "base_url": "/media/grades/",
+        "location": "/openedx/media/grades",
+    },
+}
+
+LOCALE_PATHS.append("/openedx/locale")
+
+# JWT is authentication for other openedx services
+JWT_AUTH["JWT_ISSUER"] = "{{ JWT_COMMON_ISSUER }}"
+JWT_AUTH["JWT_AUDIENCE"] = "{{ JWT_COMMON_AUDIENCE }}"
+JWT_AUTH["JWT_SECRET_KEY"] = "{{ JWT_COMMON_SECRET_KEY }}"
+JWT_AUTH["JWT_PRIVATE_SIGNING_JWK"] = None
+
+# Create folders if necessary
+for folder in [LOG_DIR, MEDIA_ROOT, STATIC_ROOT_BASE, ORA2_FILEUPLOAD_ROOT]:
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+{{ patch("openedx-lms-common-settings") }}
+
+######## End of common LMS settings
