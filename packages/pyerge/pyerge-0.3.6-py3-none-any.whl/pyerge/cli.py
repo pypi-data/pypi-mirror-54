665355cc64dac5d382c92ebc9cb8a76d4d86057a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:29 +0000

    Add package pyerge/0.3.6/pyerge-0.3.6-py3-none-any.whl

---
+++ packages/pyerge/pyerge-0.3.6-py3-none-any.whl/pyerge/cli.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python3.6
+"""Various tools to emerge and to show status for conky."""
+from argparse import ArgumentParser, Namespace
+from logging import basicConfig, DEBUG, info, error
+from typing import List
+
+from pyerge import tmerge, utils, glsa, __version__
+
+basicConfig(format='%(asctime)s | %(levelname)-6s | %(message)s', level=DEBUG)
+
+
+def run_parser():
+    """
+    Function to collect command line arguments.
+
+    Construct main object with correct set of parameters.
+    """
+    parser = ArgumentParser(description='Emerge in temporary RAM disk')
+    parser.add_argument('-s', '--size', action='store', dest='size',
+                        default='4G', help='Size or RAM disk, default 4G')
+    parser.add_argument('-l', '--check_local', action='store_true', dest='local',
+                        default=False, help='check locally')
+    parser.add_argument('-d', '--deep_clean', action='store_true', dest='deep',
+                        default=False, help='run deep clean after emerge')
+    parser.add_argument('-w', '--world', action='store_true', dest='world',
+                        default=False, help='run emerge -NDu @world')
+    parser.add_argument('-r', '--pretend_world', action='store_true', dest='pretend_world',
+                        default=False, help='run emerge -pvNDu @world')
+    parser.add_argument('-v', '--verbose', action='count', dest='verbose',
+                        default=0, help='Increase output verbosity')
+    parser.add_argument('-e', '--elements', action='store', dest='elements', type=int,
+                        default='5', help='number of elements')
+    parser.add_argument('-V', '--version', action='version', version='%(prog)s ' + __version__)
+    parser.add_argument('action', help='check, emerge, glsa_list or glsa_test')
+    opts, emerge_opts = parser.parse_known_args()
+    if opts.action not in ['check', 'emerge', 'glsa_list', 'glsa_test']:
+        error(f'Wrong options: {opts} {emerge_opts}')
+        exit()
+
+    if opts.world:
+        emerge_opts = ['-NDu', '@world']
+    if opts.pretend_world:
+        emerge_opts = ['-pvNDu', '@world']
+    if opts.verbose:
+        info(f'Pyerge version: {__version__}')
+    opts.online = utils.is_internet_connected(opts.verbose)
+    if opts.action in ('glsa_list', 'glsa_test'):
+        run_glsa(opts)
+        exit()
+
+    if not tmerge.is_portage_running():
+        utils.set_portage_tmpdir()
+        handling_mounting(opts)
+        run_emerge(emerge_opts, opts)
+        run_check(opts)
+        utils.unmounttmpfs(opts.size, opts.verbose)
+    else:
+        if opts.verbose:
+            info('emerge already running!')
+
+
+def handling_mounting(opts: Namespace) -> None:
+    """
+    Handling mounting temporary file fistem with requestes size.
+
+    :param opts: cli arguments
+    """
+    if not opts.action == 'check' and (not opts.local or not opts.pretend_world):
+        if not utils.is_tmpfs_mounted():
+            utils.mounttmpfs(opts.size, opts.verbose)
+        elif utils.size_of_mounted_tmpfs() != utils.convert2blocks(opts.size):
+            utils.remounttmpfs(opts.size, opts.verbose)
+        else:
+            if opts.verbose:
+                info('tmpfs is already mounted with requested size!')
+
+
+def run_emerge(emerge_opts: List[str], opts: Namespace) -> None:
+    """
+    Run update of system.
+
+    :param emerge_opts: list of arguments for emege
+    :param opts: cli arguments
+    """
+    if opts.action == 'emerge' and opts.online:
+        ret_code = tmerge.emerge(emerge_opts, opts.verbose, build=True)
+        tmerge.post_emerge(emerge_opts, opts.verbose, ret_code)
+        if opts.deep:
+            tmerge.deep_clean(emerge_opts, opts.verbose, ret_code)
+
+
+def run_check(opts: Namespace) -> None:
+    """
+    Run checking system updates.
+
+    :param opts: cli arguments
+    """
+    if opts.action == 'check' and (opts.online or opts.local):
+        tmerge.check_upd(opts.local, opts.verbose)
+
+
+def run_glsa(opts: Namespace) -> None:
+    """
+    Run gGLSA module to test or to list.
+
+    :param opts: cli arguments
+    """
+    if opts.online:
+        try:
+            attr = getattr(glsa, opts.action)
+        except AttributeError:
+            pass
+        else:
+            print(attr(opts))
---
+++ packages/pyerge/pyerge-0.3.6-py3-none-any.whl/pyerge/utils.py
@@ -0,0 +1,172 @@
+"""Various tools to emerge and to show status for conky."""
+from logging import warning, info, debug
+from os import system, environ
+from re import search
+from shlex import split
+from subprocess import Popen, PIPE  # nosec
+from typing import Union, Tuple
+
+from pyerge import server, portage_tmpdir
+
+
+def run_cmd(cmd: str, use_system=False) -> Tuple[bytes, bytes]:
+    """
+    Run any system command.
+
+    If use_system is set cmd is run via os.system and function
+    return RC from comand as bytes and b''.
+    If use_system is not set (default) cmd is run via subprocess.Popen and
+    function return cmd stdout and stderr as bytes.
+
+    :param cmd: command string
+    :param use_system: os.system use insted of subprocess
+    :return: tuple of bytes with output and error
+    """
+    if use_system:
+        ret_code = system(cmd)  # nosec
+        out, err = str(ret_code).encode(), b''
+    else:
+        out, err = Popen(split(cmd), stdout=PIPE, stderr=PIPE).communicate()  # nosec
+    return out, err
+
+
+def mounttmpfs(size: str, verbose: int) -> None:
+    """
+    Mount directory with size as tmp file system in RAM.
+
+    :param size: with unit K, M, G
+    :param verbose: be verbose
+    """
+    if verbose:
+        info(f'Mounting {size} of memory to {portage_tmpdir}')
+    if verbose > 1:
+        debug(f'sudo mount -t tmpfs -o size={size},nr_inodes=1M tmpfs {portage_tmpdir}')
+    run_cmd(f'sudo mount -t tmpfs -o size={size},nr_inodes=1M tmpfs {portage_tmpdir}')
+
+
+def unmounttmpfs(size: str, verbose: int) -> None:
+    """
+    Unmount directory from RAM.
+
+    :param size: with unit K, M, G
+    :param verbose: be verbose
+    """
+    if verbose:
+        info(f'Unmounting {size} of memory from {portage_tmpdir}')
+    if verbose > 1:
+        debug(f'sudo umount -f {portage_tmpdir}')
+    run_cmd(f'sudo umount -f {portage_tmpdir}')
+
+
+def remounttmpfs(size: str, verbose: int) -> None:
+    """
+    Re-mount directory with size as tmp file system in RAM.
+
+    :param size: with unit K, M, G
+    :param verbose: be verbose
+    """
+    if verbose:
+        info(f'Remounting {size} of memory to {portage_tmpdir}')
+    if verbose > 1:
+        debug(f'sudo umount -f {portage_tmpdir}')
+    run_cmd(f'sudo umount -f {portage_tmpdir}')
+    if verbose > 1:
+        debug(f'sudo mount -t tmpfs -o size={size},nr_inodes=1M tmpfs {portage_tmpdir}')
+    run_cmd(f'sudo mount -t tmpfs -o size={size},nr_inodes=1M tmpfs {portage_tmpdir}')
+
+
+def is_internet_connected(verbose: int) -> bool:
+    """
+    Check if there is connection to internet.
+
+    :param verbose: be verbose
+    :return: True is connected, False otherwise
+    """
+    cmd, _ = run_cmd(f'ping -W1 -c1 {server}')
+    match = search(b'[1].*, [1].*, [0]%.*,', cmd)
+    if match is not None:
+        if verbose:
+            info('There is internet connecton or not needed')
+        return True
+    warning('No internet connection!')
+    return False
+
+
+def size_of_mounted_tmpfs() -> int:
+    """
+    Return size of mounted directory.
+
+    :return: size in bytes as intiger
+    """
+    df_cmd, _ = run_cmd('df')
+    match = search(r'(tmpfs\s*)(\d+)(\s*.*%s)' % portage_tmpdir, df_cmd.decode())
+    if match is not None:
+        return int(match.group(2))
+    return 0
+
+
+def is_tmpfs_mounted() -> bool:
+    """
+    Check if portage temp dir is mounted.
+
+    :return: True is mounted, False otherwise
+    """
+    mount_cmd, _ = run_cmd('mount')
+    match = search(r'(tmpfs on\s+)(%s)(\s+type tmpfs)' % portage_tmpdir, mount_cmd.decode())
+    return bool(match is not None and match.group(2) == portage_tmpdir)
+
+
+def convert2blocks(size: str) -> int:
+    """
+    Convert size with unit into system blocks (used in i.e. df/mounts commands).
+
+    :param size: with units K, M, G
+    :return: size in kB
+    """
+    try:
+        return int(float(size))
+    except ValueError:
+        pass
+    match = search(r'(?i)(\d+)([KMG])', size)
+    if match.group(2).upper() == 'K':
+        return int(match.group(1))
+    if match.group(2).upper() == 'M':
+        return int(match.group(1)) * 1024
+    if match.group(2).upper() == 'G':
+        return int(match.group(1)) * 1024 * 1024
+
+
+def delete_content(fname: Union[str, bytes, int]) -> None:
+    """
+    Clean-up file content.
+
+    :param fname: path to file as string
+    """
+    with open(fname, 'w'):
+        pass
+
+
+def set_portage_tmpdir() -> str:
+    """Set system variable."""
+    if not environ.get('PORTAGE_TMPDIR', ''):
+        environ['PORTAGE_TMPDIR'] = portage_tmpdir
+    return environ['PORTAGE_TMPDIR']
+
+
+def e_eta() -> str:
+    """
+    Return estimated time of compilation of current package.
+
+    :return: time until compilation ends
+    :rtype: str
+    """
+    output, _ = run_cmd('genlop -cn')
+    out = output.decode()
+    eta = ''
+    if search(r'Error.*no working merge found', out):
+        eta = 'Unknown'
+
+    match = search(r'ETA:\s(.*)\.', out)
+    if match is not None:
+        eta = match.group(1)
+    return eta
