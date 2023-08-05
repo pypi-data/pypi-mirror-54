3df263fbaf80e01905dd26e813a7f0928c2eeb06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:39 +0000

    Add package keras-augm-layer/1.0.2/keras_augm_layer-1.0.2.tar.gz

---
+++ packages/keras-augm-layer/keras_augm_layer-1.0.2.tar.gz/keras_augm_layer-1.0.2/setup.py
@@ -0,0 +1,21 @@
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
+setup(
+    name='keras_augm_layer',
+    version='1.0.2',
+    author='Roman Sol (ZFTurbo)',
+    packages=['keras_augm_layer', ],
+    url='https://github.com/ZFTurbo/Keras-augmentation-layer',
+    description='Keras implementation of layer which performs augmentations of images using GPU.',
+    long_description='Keras implementation of layer which performs augmentations of images using GPU.'
+                     'More details: https://github.com/ZFTurbo/Keras-augmentation-layer',
+    install_requires=[
+        "numpy",
+        "pandas",
+        "tensorflow>=1.9",
+        "keras",
+    ],
+)
