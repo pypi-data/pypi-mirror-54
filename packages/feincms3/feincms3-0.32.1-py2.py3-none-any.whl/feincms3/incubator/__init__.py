a259d5a9133bfaf1bff57e9fed6d0255d90e1961
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:03 +0000

    Add package feincms3/0.32.1/feincms3-0.32.1-py2.py3-none-any.whl

---
+++ packages/feincms3/feincms3-0.32.1-py2.py3-none-any.whl/feincms3/incubator/__init__.py
@@ -0,0 +1,8 @@
+import warnings
+
+warnings.warn(
+    "The feincms3.incubator module contains code which is subject to change"
+    " and/or removal without any prior warning.",
+    Warning,
+    stacklevel=2,
+)
