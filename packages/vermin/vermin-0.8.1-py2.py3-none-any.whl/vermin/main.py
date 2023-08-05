a52b81996660357dc669074983ec4743a1cb3b2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:27 +0000

    Add package vermin/0.8.1/vermin-0.8.1-py2.py3-none-any.whl

---
+++ packages/vermin/vermin-0.8.1-py2.py3-none-any.whl/vermin/main.py
@@ -0,0 +1,79 @@
+import sys
+from os.path import abspath
+
+from .config import Config
+from .printing import nprint
+from .detection import detect_paths
+from .processor import Processor
+from .arguments import Arguments
+
+def version_strings(vers):
+  return ", ".join(str(v) for v in vers)
+
+def main():
+  config = Config.get()
+
+  args = Arguments(sys.argv[1:]).parse()
+  if "usage" in args:
+    Arguments.print_usage()
+    sys.exit(args["code"])
+
+  if args["code"] != 0:
+    sys.exit(args["code"])
+
+  processes = args["processes"]
+  targets = args["targets"]
+
+  # Detect paths, remove duplicates, and sort for deterministic results.
+  nprint("Detecting python files..")
+  paths = [abspath(p) for p in args["paths"]]
+  paths = list(set(detect_paths(paths, args["hidden"])))
+  paths.sort()
+
+  amount = len(paths)
+  if amount == 0:
+    print("No files specified to analyze!")
+    sys.exit(1)
+
+  msg = "Analyzing"
+  if amount > 1:
+    msg += " {} files".format(amount)
+  nprint("{} using {} processes..".format(msg, processes))
+
+  try:
+    processor = Processor()
+    (mins, incomp, unique_versions) = processor.process(paths, processes)
+  except KeyboardInterrupt:
+    print("Aborting..")
+    sys.exit(1)
+
+  if incomp and not config.ignore_incomp():
+    nprint("Note: Some files had incompatible versions so the results might not be correct!")
+
+  incomps = []
+  reqs = []
+  for i in range(len(mins)):
+    ver = mins[i]
+    if ver is None:
+      incomps.append(i + 2)
+    elif ver is not None and ver != 0:
+      reqs.append(ver)
+
+  if len(reqs) == 0 and len(incomps) == 0:
+    print("No known reason found that it will not work with 2+ and 3+.")
+    print("Please report if it does not: https://github.com/netromdk/vermin/issues/")
+    if config.lax_mode():
+      print("Tip: Try without using lax mode for more thorough analysis.")
+
+  if len(reqs) > 0:
+    print("Minimum required versions: {}".format(version_strings(reqs)))
+  if len(incomps) > 0:
+    print("Incompatible versions:     {}".format(version_strings(incomps)))
+  if args["versions"] and len(unique_versions) > 0:
+    print("Version range:             {}".format(version_strings(unique_versions)))
+
+  if len(targets) > 0 and targets != reqs:
+    print("Target versions not met:   {}".format(version_strings(targets)))
+    sys.exit(1)
+
+  sys.exit(0)
---
+++ packages/vermin/vermin-0.8.1-py2.py3-none-any.whl/vermin/utility.py
@@ -0,0 +1,50 @@
+from .config import Config
+
+def reverse_range(object):
+  """Yields reverse range of object: list(reverse_range([1, 2, 3])) -> [2, 1, 0]."""
+  return range(len(object) - 1, -1, -1)
+
+def dotted_name(names):
+  """Returns a dotted name of a list of strings, integers, lists, and tuples."""
+
+  # It will just return the value instead of "b.a.d" if input was "bad"!
+  if isinstance(names, str):
+    return names
+
+  resolved = []
+  for name in names:
+    if isinstance(name, str):
+      resolved.append(name)
+    elif isinstance(name, int):
+      resolved.append(str(name))
+    elif isinstance(name, list) or isinstance(name, tuple):
+      resolved += name
+    else:
+      assert False
+  return ".".join(resolved)
+
+class InvalidVersionException(BaseException):
+  pass
+
+def combine_versions(list1, list2):
+  assert len(list1) == len(list2)
+  assert len(list1) == 2
+  if not Config.get().ignore_incomp() and\
+    ((list1[0] is None and list1[1] is not None and list2[0] is not None and list2[1] is None) or
+     (list1[0] is not None and list1[1] is None and list2[0] is None and list2[1] is not None)):
+    raise InvalidVersionException("Versions could not be combined: {} and {}".format(list1, list2))
+  res = []
+  for i in range(len(list1)):
+    v1 = list1[i]
+    v2 = list2[i]
+    if v1 == 0 and v2 == 0:
+      res.append(0)
+    elif v1 == 0:
+      res.append(v2)
+    elif v2 == 0:
+      res.append(v1)
+    elif v1 is None or v2 is None:
+      res.append(None)
+    else:
+      res.append(max(v1, v2))
+  return res
