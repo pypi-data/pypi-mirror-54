c3154b7c7db31721919d3bb2f104df17d7f3c429
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:44 +0000

    Add package tp-py-common/1.0.42/tp-py-common-1.0.42.tar.gz

---
+++ packages/tp-py-common/tp-py-common-1.0.42.tar.gz/tp-py-common-1.0.42/tp/test/db/seeds_test/BillOfPacking.1a9d.sql
@@ -0,0 +1,6 @@
+INSERT INTO `bill_of_packings` (`id`, `name`, `description`, `customer_id`, `url_token`, `active`, `status`, `created_at`, `updated_at`, `new_patient`) 
+VALUES 
+('92','Apostrophe Base Package',NULL,'1','706e3bb36ac0330c7b1e',true,NULL,'2019-03-20 16:24:22','2019-03-20 16:24:22',false),
+('95','Apostrophe Finasteride 30',NULL,'1','d40022d0defc8e0d9230',true,NULL,'2019-03-20 16:34:03','2019-03-20 16:34:03',false),
+('96','Apostrophe Finasteride 90',NULL,'1','96b85197caaba201257a',true,NULL,'2019-03-20 16:36:34','2019-03-20 16:36:34',false),
+('97','Apostrophe Latisse',NULL,'1','7980f7baa17339f7b2b7',true,NULL,'2019-03-20 16:49:12','2019-03-20 16:49:12',false)
