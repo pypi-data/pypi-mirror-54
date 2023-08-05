ae4e13aa5003d1ce61fc3a089b41eeebf1d78bdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:36 +0000

    Add package trackpy/0.4.2/trackpy-0.4.2.tar.gz

---
+++ packages/trackpy/trackpy-0.4.2.tar.gz/trackpy-0.4.2/setup.py
@@ -0,0 +1,41 @@
+import os
+import versioneer
+from setuptools import setup
+
+
+try:
+    descr = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+except IOError:
+    descr = ''
+
+try:
+    from pypandoc import convert
+    descr = convert(descr, 'rst', format='md')
+except ImportError:
+    pass
+
+
+# In some cases, the numpy include path is not present by default.
+# Let's try to obtain it.
+try:
+    import numpy
+except ImportError:
+    ext_include_dirs = []
+else:
+    ext_include_dirs = [numpy.get_include(),]
+
+setup_parameters = dict(
+    name = "trackpy",
+    version = versioneer.get_version(),
+    cmdclass = versioneer.get_cmdclass(),
+    description = "particle-tracking toolkit",
+    author = "Trackpy Contributors",
+    author_email = "daniel.b.allan@gmail.com",
+    url = "https://github.com/soft-matter/trackpy",
+    install_requires = ['numpy>=1.8', 'scipy>=0.12', 'six>=1.8',
+	                'pandas>=0.13', 'pyyaml', 'matplotlib'],
+    packages = ['trackpy', 'trackpy.refine', 'trackpy.linking', 'trackpy.locate_functions'],
+    long_description = descr,
+)
+
+setup(**setup_parameters)
