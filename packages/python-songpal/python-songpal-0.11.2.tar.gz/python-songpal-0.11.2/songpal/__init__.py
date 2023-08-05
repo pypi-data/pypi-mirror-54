14b1aa8bbb8470a16fab3b715d57c5a092d7fa6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:15:09 +0000

    Add package python-songpal/0.11.2/python-songpal-0.11.2.tar.gz

---
+++ packages/python-songpal/python-songpal-0.11.2.tar.gz/python-songpal-0.11.2/songpal/__init__.py
@@ -0,0 +1,10 @@
+# flake8: noqa
+from songpal.common import SongpalException
+from songpal.device import Device
+from songpal.notification import (
+    Notification,
+    ConnectChange,
+    ContentChange,
+    PowerChange,
+    VolumeChange,
+)
