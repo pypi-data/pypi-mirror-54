bed92d2d08b7a3b2b3da58ac63e505065a80c52e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:34 +0000

    Add package konduit/0.1.1/konduit-0.1.1.tar.gz

---
+++ packages/konduit/konduit-0.1.1.tar.gz/konduit-0.1.1/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='konduit',
+    version='0.1.1',
+    packages=find_packages(),
+    install_requires=[
+        'requests>=2.22.0',
+        'numpy<=1.16.4',  # For compatibility with python 2
+        'pyarrow==0.13.0',
+        'requests-toolbelt>=0.9.1',
+        'pandas<=0.24.2',  # For compatibility with python 2
+        'Cython',
+        'pydl4j',
+        'pydatavec'
+    ],
+    extras_require={
+        'tests': ['pytest', 'pytest-pep8', 'pytest-cov', 'mock'],
+        'codegen': ['jsonschema2popo']
+    },
+    include_package_data=True,
+    license='Apache',
+    description='konduit: Enterprise Runtime for Machine Learning Models',
+    long_description='konduit: Enterprise Runtime for Machine Learning Models',
+    author='Max Pumperla',
+    author_email='max@skymind.global',
+    url='https://github.com/KonduitAI/konduit-serving',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Environment :: Console',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3'
+    ]
+)
---
+++ packages/konduit/konduit-0.1.1.tar.gz/konduit-0.1.1/tests/test_transform_process.py
@@ -0,0 +1,78 @@
+from konduit import *
+from konduit.json_utils import json_with_type
+from konduit.server import Server
+from konduit.client import Client
+from konduit.utils import is_port_in_use
+
+import random
+import time
+import json
+from jnius import autoclass
+
+
+def test_build_tp():
+    TransformProcessBuilder = autoclass(
+        'org.datavec.api.transform.TransformProcess$Builder')
+    TransformProcess = autoclass('org.datavec.api.transform.TransformProcess')
+    StringJava = autoclass("java.lang.String")
+
+    SchemaBuilder = autoclass(
+        'org.datavec.api.transform.schema.Schema$Builder')
+    schema = SchemaBuilder().addColumnString(StringJava('first')).build()
+    tp = TransformProcessBuilder(schema).appendStringColumnTransform(
+        StringJava("first"), StringJava("two")).build()
+
+    tp_json = tp.toJson()
+    from_json = TransformProcess.fromJson(StringJava(tp_json))
+    json_tp = json.dumps(tp_json)
+    as_python_json = json.loads(tp_json)
+    transform_process = TransformProcessPipelineStep(
+        transform_processes={'default': as_python_json},
+        input_names=['default'],
+        output_names=['default'],
+        input_schemas={'default': ['String']},
+        output_schemas={'default': ['String']},
+        input_column_names={'default': ['first']},
+        output_column_names={'default': ['first']}
+    )
+
+    input_names = ['default']
+    output_names = ['default']
+    port = random.randint(1000, 65535)
+    serving_config = ServingConfig(http_port=port,
+                                   input_data_type='JSON',
+                                   output_data_type='JSON',
+                                   log_timings=True)
+
+    inference_config = InferenceConfiguration(serving_config=serving_config,
+                                              pipeline_steps=[transform_process])
+    as_json = json_with_type(inference_config)
+    inference_configuration_java_class = autoclass(
+        'ai.konduit.serving.InferenceConfiguration')
+    config = inference_configuration_java_class.fromJson(
+        StringJava(json.dumps(as_json)))
+
+    server = Server(config=inference_config,
+                    extra_start_args='-Xmx8g',
+                    jar_path='konduit.jar')
+    server.start()
+    print('Process started. Sleeping 10 seconds.')
+    client = Client(input_names=input_names,
+                    output_names=output_names,
+                    return_output_type='JSON',
+                    input_type='JSON',
+                    endpoint_output_type='RAW',
+                    url='http://localhost:' + str(port))
+
+    data_input = {'first': 'value'}
+
+    time.sleep(30)
+    assert is_port_in_use(port)
+
+    try:
+        predicted = client.predict(data_input)
+        print(predicted)
+        server.stop()
+    except Exception as e:
+        print(e)
+        server.stop()
