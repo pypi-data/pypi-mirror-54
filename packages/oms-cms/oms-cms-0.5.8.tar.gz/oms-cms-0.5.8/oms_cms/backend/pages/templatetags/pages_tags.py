87b3cdfede170019ffd4299ccf82a5eab81334a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:37 +0000

    Add package oms-cms/0.5.8/oms-cms-0.5.8.tar.gz

---
+++ packages/oms-cms/oms-cms-0.5.8.tar.gz/oms-cms-0.5.8/oms_cms/backend/pages/templatetags/pages_tags.py
@@ -0,0 +1,24 @@
+from django import template
+
+from oms_cms.backend.pages.models import Pages, BlockPage
+
+register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def for_pages(context):
+    """Вывод страниц"""
+    return Pages.objects.filter(published=True, lang=context["request"].LANGUAGE_CODE)
+
+
+@register.simple_tag(takes_context=True)
+def for_block_page(context, page):
+    """Вывод списка блоков страниц"""
+    return BlockPage.objects.filter(page=page, page__lang=context["request"].LANGUAGE_CODE)
+
+
+@register.simple_tag(takes_context=True)
+def get_block_page(context, page):
+    """Вывод блоков страниц"""
+    blocks = BlockPage.objects.filter(page=page, page__lang=context["request"].LANGUAGE_CODE)
+    return {block.name: block for block in blocks}
