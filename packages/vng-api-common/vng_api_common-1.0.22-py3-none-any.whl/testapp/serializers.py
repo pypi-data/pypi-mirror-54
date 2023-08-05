93d97efdcea31fbc93afa124c3dec70085d043a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:20 +0000

    Add package vng-api-common/1.0.22/vng_api_common-1.0.22-py3-none-any.whl

---
+++ packages/vng-api-common/vng_api_common-1.0.22-py3-none-any.whl/testapp/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from testapp.models import Group, Person
+
+from vng_api_common.serializers import GegevensGroepSerializer
+
+
+class AddressSerializer(GegevensGroepSerializer):
+    class Meta:
+        model = Person
+        gegevensgroep = "address"
+
+
+class PersonSerializer(serializers.ModelSerializer):
+    address = AddressSerializer(allow_null=True)
+
+    class Meta:
+        model = Person
+        fields = ("address", "name")
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    person = PersonSerializer(many=True)
+
+    class Meta:
+        model = Group
+        fields = ("person",)
