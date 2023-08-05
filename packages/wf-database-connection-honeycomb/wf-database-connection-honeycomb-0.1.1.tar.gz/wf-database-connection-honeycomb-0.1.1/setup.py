53f92d0a634746b7ebe5e8f54294a42e9dccd5cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:57 +0000

    Add package wf-database-connection-honeycomb/0.1.1/wf-database-connection-honeycomb-0.1.1.tar.gz

---
+++ packages/wf-database-connection-honeycomb/wf-database-connection-honeycomb-0.1.1.tar.gz/wf-database-connection-honeycomb-0.1.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='wf-database-connection-honeycomb',
+    packages=find_packages(),
+    version='0.1.1',
+    include_package_data=True,
+    description='An implementation of the database_connection API using Wildflower\'s Honeycomb database',
+    long_description=open('README.md').read(),
+    url='https://github.com/WildflowerSchools/database_connection_honeycomb',
+    author='Theodore Quinn',
+    author_email='ted.quinn@wildflowerschools.org',
+    install_requires=[
+        'wf-database-connection>=0.1.0',
+        'wildflower-honeycomb-sdk>=0.7.2',
+        'python-dateutil>=2.8.0'
+    ],
+    keywords=['database'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ]
+)
