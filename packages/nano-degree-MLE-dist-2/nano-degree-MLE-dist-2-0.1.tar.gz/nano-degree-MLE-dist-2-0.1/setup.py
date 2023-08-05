54add37842d77939130aa40e52281c784c11fc4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:01 +0000

    Add package nano-degree-MLE-dist-2/0.1/nano-degree-MLE-dist-2-0.1.tar.gz

---
+++ packages/nano-degree-MLE-dist-2/nano-degree-MLE-dist-2-0.1.tar.gz/nano-degree-MLE-dist-2-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='nano-degree-MLE-dist-2',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      zip_safe=False)
