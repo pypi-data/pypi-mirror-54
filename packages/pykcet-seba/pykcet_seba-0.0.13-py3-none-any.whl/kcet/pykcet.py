1c05f552c25d9d995d4777bb8af289e75f3f261d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:43 +0000

    Add package pykcet-seba/0.0.13/pykcet_seba-0.0.13-py3-none-any.whl

---
+++ packages/pykcet-seba/pykcet_seba-0.0.13-py3-none-any.whl/kcet/pykcet.py
@@ -0,0 +1,6 @@
+class pykcet:
+    def __init__(self):
+        self.text=["Good Luck"]
+    def kprint(self):
+        return "KAMARAJ COLLEGE OF ENGINEERING AND TECHNOLOGY        An Autonomous Institution           "
+       
