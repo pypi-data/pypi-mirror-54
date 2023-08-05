4cfdc6776897f484db68d681323176b0a7166a70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:01 +0000

    Add package magnelPy/0.0.6/magnelPy-0.0.6-py3-none-any.whl

---
+++ packages/magnelPy/magnelPy-0.0.6-py3-none-any.whl/magnelPy/__init__.py
@@ -0,0 +1,31 @@
+"""
+magnelPy
+========
+Provides
+  1. Support methods for data handling
+  2. Probabilistic / Reliability methods and tools
+  3. Structural Fire Engineering methods and tools (tbc)
+
+Available subpackages
+---------------------
+admin
+	Basic functions for data handling : read-write to excel etc.
+stochVar
+	Standard formatting of stochastic variables.
+statFunc
+	Probabilistic distribution evaluations in standardized format.
+sampling
+	Sampling procedures.
+
+Documentation
+-------------
+The documentation is incomplete and under development.
+
+Code snippets are indicated by three greater-than signs:
+  >>> x = 42
+  >>> x = x + 1
+
+Use the built-in ``help`` function to view a function's docstring:
+  >>> help(statFunc.Finv_Uniform)
+
+"""
