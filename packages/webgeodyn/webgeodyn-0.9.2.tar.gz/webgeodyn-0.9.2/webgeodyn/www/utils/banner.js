d1c0d814ea6a8289e1a4e8d3bd2eaf16281a1ddb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:47 +0000

    Add package webgeodyn/0.9.2/webgeodyn-0.9.2.tar.gz

---
+++ packages/webgeodyn/webgeodyn-0.9.2.tar.gz/webgeodyn-0.9.2/webgeodyn/www/utils/banner.js
@@ -0,0 +1,31 @@
+class Banner {
+    constructor(parent_div_name, message, type, closing_button=true)
+    {
+        this.parentDiv = $(parent_div_name);
+        this.type = type+'-banner';
+
+        this.createBanner(message);
+        if(closing_button) {
+            this.createCloseButton();
+        }
+    }
+
+    createBanner(message) {
+        $("<div>").attr({'class': '_flexcontainer', 'id': this.type})
+            .append($("<span>").attr('class', '_flex').html(message))
+            .prependTo(this.parentDiv);
+        if (this.canClose) {
+            banner_div.append();
+            $('#info-banner .close_button').on('click', function () {
+            })
+        }
+    }
+
+    createCloseButton() {
+        let bannerDiv = this.parentDiv.children('#'+this.type);
+        bannerDiv.append($("<span>").attr('class', '_flexfixedsize _flex-end close_button').text('X'));
+        bannerDiv.children('.close_button').on('click', function(){
+            bannerDiv.slideUp();
+        });
+    }
+}
