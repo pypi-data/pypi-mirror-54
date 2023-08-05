d6f01883d803fb6dc7f12af2623eacd738cd9904
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:17 +0000

    Add package flairstreamlined/1.1/flairstreamlined-1.1.tar.gz

---
+++ packages/flairstreamlined/flairstreamlined-1.1.tar.gz/flairstreamlined-1.1/flair/models/__init__.py
@@ -0,0 +1,1 @@
+from .text_classification_model import TextClassifier
---
+++ packages/flairstreamlined/flairstreamlined-1.1.tar.gz/flairstreamlined-1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='flairstreamlined',
+    python_requires='>3.6',
+    version='1.1',
+    description='streamlined version of flair to use with aws lambda',
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    license='MIT',
+    packages=find_packages(),
+    install_requires = [
+        'torch',
+        'requests',
+        'segtok',
+        'gensim',
+        'tqdm'
+        ]
+    )
