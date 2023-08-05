246e2023181285b4582488bab1ab201d11231f69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:17 +0000

    Add package zwt5/0.1.11/zwt5-0.1.11.tar.gz

---
+++ packages/zwt5/zwt5-0.1.11.tar.gz/zwt5-0.1.11/zwt5/__init__.py
@@ -0,0 +1,9 @@
+import numpy
+from .add import add
+from .division import division
+from .multiply import multiply
+from .subtract import subtract
+from .adv.sqrt import squareroot
+import pandas as pd
+a = pd.read_csv('.//1.txt')
+print(a)
