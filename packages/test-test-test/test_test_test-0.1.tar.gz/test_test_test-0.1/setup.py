f7eb34d2d792ebe72bef7dc97d227cb13077ca66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:00 +0000

    Add package test-test-test/0.1/test_test_test-0.1.tar.gz

---
+++ packages/test-test-test/test_test_test-0.1.tar.gz/test_test_test-0.1/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+#with open("README.md", "r") as fh:
+#    long_description = fh.read()
+setuptools.setup(
+     name='test_test_test',  
+     version='0.1',
+     scripts=['email_permutations'] ,
+     author="Pratik Dani",
+     author_email="danipratik91@gmail.com",
+#     long_description=long_description,
+   long_description_content_type="text/markdown",
+#     url="https://github.com/javatechy/dokr",
+     packages=setuptools.find_packages(),
+ )
