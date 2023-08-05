3f92e0352b04fff2115dd81bac1853089723964b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:50 +0000

    Add package ginpar/0.5.0/ginpar-0.5.0-py3-none-any.whl

---
+++ packages/ginpar/ginpar-0.5.0-py3-none-any.whl/ginpar/config.py
@@ -0,0 +1,7 @@
+p5_path = "https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.9.0/p5.min.js"
+delaunator_path = "https://unpkg.com/delaunator@4.0.1/delaunator.min.js"
+
+libraries = [
+    "https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.9.0/p5.min.js",
+    "https://unpkg.com/delaunator@4.0.1/delaunator.min.js"
+]
