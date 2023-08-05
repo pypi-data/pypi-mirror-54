5494a1106d1d62f9857a4b5ead227506d493b580
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:33 +0000

    Add package mati/0.1.8/mati-0.1.8-py3-none-any.whl

---
+++ packages/mati/mati-0.1.8-py3-none-any.whl/mati/resources/__init__.py
@@ -0,0 +1,6 @@
+__all__ = ['AccessToken', 'Identity', 'Resource', 'UserValidationData']
+
+from .access_tokens import AccessToken
+from .base import Resource
+from .identities import Identity
+from .user_verification_data import UserValidationData
