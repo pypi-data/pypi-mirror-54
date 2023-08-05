afac26e3182a93d1b1474b912b01342d634dcdeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:56 +0000

    Add package logModel/0.0.1/logModel-0.0.1.tar.gz

---
+++ packages/logModel/logModel-0.0.1.tar.gz/logModel-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+setup(
+    name='logModel',  # package
+    version='0.0.1',
+    description='Log_Model',
+    long_description='args: debug, info, warn, error, critical',
+    author='donglidunyin',
+    author_email='donglidunyin@163.com',
+    install_requires=[],
+    packages=find_packages(),
+)
