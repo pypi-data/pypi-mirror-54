257a41e6882fefcefa5b1295fa03200528b94606
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:58 +0000

    Add package volumetric/0.0.2/volumetric-0.0.2.tar.gz

---
+++ packages/volumetric/volumetric-0.0.2.tar.gz/volumetric-0.0.2/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="volumetric",
+    version="0.0.2",
+    author="volatilityfoundation",
+    author_email="info@volatilityfoundation.org",
+    description="Volumetric is a web-based GUI for the volatility3 library. DUMMY PACKAGE. DO NOT USE.",
+    long_description="This is a dummy package. The official project website is available at https://github.com/volatilityfoundation/volumetric",
+    url="https://github.com/volatilityfoundation/volumetric",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5.3',
+)
