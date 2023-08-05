50a4930a461fed3625488ac778aa06192954d879
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:20 +0000

    Add package Payton/0.0.2.34/Payton-0.0.2.34.tar.gz

---
+++ packages/Payton/Payton-0.0.2.34.tar.gz/Payton-0.0.2.34/payton/scene/geometry/__init__.py
@@ -0,0 +1,41 @@
+# pylama:ignore=W
+"""
+Payton main geometry module
+
+Geometry module holds the basic geometry shapes. They are all inherited
+from `payton.scene.geometry.base.Object` class. They are as simple as possible.
+
+Their face informations are generated at the initialization but vertex array
+object is not generated until it arrives in render pipeline.
+
+* Object
+  * Material (`payton.scene.material`)
+    * Shader (`payton.scene.shader`)
+"""
+from payton.scene.geometry.base import Object, Line
+from payton.scene.geometry.mesh import Mesh
+from payton.scene.geometry.cylinder import Cylinder
+from payton.scene.geometry.cube import Cube
+from payton.scene.geometry.plane import Plane, MatrixPlane
+from payton.scene.geometry.sphere import Sphere
+from payton.scene.geometry.point import PointCloud
+from payton.scene.geometry.md2 import MD2
+from payton.scene.geometry.wavefront import Wavefront
+from payton.scene.geometry.ragdoll import RagDoll, Joint
+
+
+__all__ = [
+    "Object",
+    "Line",
+    "Mesh",
+    "Cylinder",
+    "Cube",
+    "Plane",
+    "MatrixPlane",
+    "Sphere",
+    "PointCloud",
+    "MD2",
+    "Wavefront",
+    "RagDoll",
+    "Joint",
+]
