1c69cf33af6bdd3b331ea6ff5962dfc1d360f20c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:41 +0000

    Add package python-masscan/0.1.6/python-masscan-0.1.6.tar.gz

---
+++ packages/python-masscan/python-masscan-0.1.6.tar.gz/python-masscan-0.1.6/masscan/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""Init file for the masscan bindings."""
+from .masscan import *
+
+__author__ = 'MyKings (xsseroot@gmail.com)'
+__version__ = '0.1.6'
+__last_modification__ = '2019.10.15'
+
