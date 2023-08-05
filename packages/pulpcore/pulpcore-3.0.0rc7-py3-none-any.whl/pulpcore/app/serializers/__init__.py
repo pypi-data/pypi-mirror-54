09a087d5601dc3a2425e9ab45bd6e1892faab839
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:48 +0000

    Add package pulpcore/3.0.0rc7/pulpcore-3.0.0rc7-py3-none-any.whl

---
+++ packages/pulpcore/pulpcore-3.0.0rc7-py3-none-any.whl/pulpcore/app/serializers/__init__.py
@@ -0,0 +1,58 @@
+# Load order: base, fields, all others.
+# - fields can import directly from base if needed
+# - all can import directly from base and fields if needed
+from .base import (  # noqa
+    AsyncOperationResponseSerializer,
+    DetailIdentityField,
+    DetailRelatedField,
+    IdentityField,
+    ModelSerializer,
+    NestedIdentityField,
+    NestedRelatedField,
+    RelatedField,
+    validate_unknown_fields,
+)
+from .content import (  # noqa
+    ArtifactSerializer,
+    ContentChecksumSerializer,
+    MultipleArtifactContentSerializer,
+    NoArtifactContentSerializer,
+    SingleArtifactContentSerializer,
+)
+from .fields import (  # noqa
+    BaseURLField,
+    ContentRelatedField,
+    LatestVersionField,
+    SecretCharField,
+    SingleContentArtifactField,
+    relative_path_validator,
+)
+from .progress import ProgressReportSerializer  # noqa
+from .publication import (  # noqa
+    BaseDistributionSerializer,
+    ContentGuardSerializer,
+    PublicationDistributionSerializer,
+    PublicationSerializer,
+    RepositoryVersionDistributionSerializer,
+)
+from .repository import (  # noqa
+    ExporterSerializer,
+    PublisherSerializer,
+    RemoteSerializer,
+    RepositorySerializer,
+    RepositorySyncURLSerializer,
+    RepositoryVersionCreateSerializer,
+    RepositoryVersionSerializer,
+)
+from .task import (  # noqa
+    MinimalTaskSerializer,
+    TaskCancelSerializer,
+    TaskSerializer,
+    WorkerSerializer,
+)
+from .upload import (  # noqa
+    UploadChunkSerializer,
+    UploadCommitSerializer,
+    UploadSerializer,
+    UploadDetailSerializer
+)
---
+++ packages/pulpcore/pulpcore-3.0.0rc7-py3-none-any.whl/pulpcore/tests/functional/api/using_plugin/test_pagination.py
@@ -0,0 +1,54 @@
+# coding=utf-8
+"""Tests related to file plugin pagination."""
+import unittest
+from random import sample
+
+from pulp_smash import api, config
+from pulp_smash.pulp3.constants import REPO_PATH
+from pulp_smash.pulp3.utils import gen_repo, get_versions
+
+from pulpcore.tests.functional.api.using_plugin.constants import (
+    FILE_CONTENT_PATH,
+    FILE_MANY_FIXTURE_COUNT,
+    FILE_MANY_FIXTURE_MANIFEST_URL,
+)
+from pulpcore.tests.functional.api.using_plugin.utils import populate_pulp
+from pulpcore.tests.functional.api.using_plugin.utils import set_up_module as setUpModule  # noqa
+
+
+class PaginationTestCase(unittest.TestCase):
+    """Test file plugin pagination.
+
+    This test case assumes that Pulp returns 100 elements in each page of
+    results. This is configurable, but the current default set by all known
+    Pulp installers.
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        """Create class-wide variables."""
+        cls.cfg = config.get_config()
+        cls.client = api.Client(cls.cfg, api.page_handler)
+
+    def test_file_content(self):
+        """Test pagination for repository versions."""
+        # Add content to Pulp, create a repo, and add content to repo. We
+        # sample 21 contents, because with page_size set to 10, this produces 3
+        # pages, where the three three pages have unique combinations of values
+        # for the "previous" and "next" links.
+        populate_pulp(self.cfg, url=FILE_MANY_FIXTURE_MANIFEST_URL)
+        sample_size = min(FILE_MANY_FIXTURE_COUNT, 21)
+        contents = sample(self.client.get(FILE_CONTENT_PATH), sample_size)
+        repo = self.client.post(REPO_PATH, gen_repo())
+        self.addCleanup(self.client.delete, repo['pulp_href'])
+
+        for content in contents:
+            self.client.post(
+                repo['versions_href'],
+                {'add_content_units': [content['pulp_href']]}
+            )
+
+        # Verify pagination works for getting repo versions.
+        repo = self.client.get(repo['pulp_href'])
+        repo_versions = get_versions(repo, {'page_size': 10})
+        self.assertEqual(len(repo_versions), sample_size, repo_versions)
