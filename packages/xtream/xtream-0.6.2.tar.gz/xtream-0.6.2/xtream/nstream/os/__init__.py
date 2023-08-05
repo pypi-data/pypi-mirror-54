14ada5078479aac29864fb54c9da49c1eff08ee2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:18 +0000

    Add package xtream/0.6.2/xtream-0.6.2.tar.gz

---
+++ packages/xtream/xtream-0.6.2.tar.gz/xtream-0.6.2/xtream/nstream/os/__init__.py
@@ -0,0 +1,4 @@
+from ubuntu import *
+from centos import *
+from windows import *
+from .base import *
---
+++ packages/xtream/xtream-0.6.2.tar.gz/xtream-0.6.2/xtream/nstream/packages/__init__.py
@@ -0,0 +1,64 @@
+NGINX_SRC="http://nginx.org/download/nginx-1.9.9.tar.gz"
+NGINX_RTMP="https://github.com/arut/nginx-rtmp-module/archive/master.zip"
+PCRE="https://ftp.pcre.org/pub/pcre/pcre-8.43.tar.gz"
+OPENSSL="https://www.openssl.org/source/openssl-1.1.1d.tar.gz"
+ZLIB="https://www.zlib.net/zlib-1.2.11.tar.gz"
+
+
+
+
+CONFIGURE_CENTOS="""
+./configure \
+--prefix=/etc/nginx \
+--sbin-path=/usr/sbin/nginx \
+--modules-path=/usr/lib64/nginx/modules \
+--conf-path=/etc/nginx/nginx.conf \
+--error-log-path=/var/log/nginx/error.log \
+--pid-path=/var/run/nginx.pid \
+--lock-path=/var/run/nginx.lock \
+--user=nginx \
+--group=nginx \
+--build=CentOS \
+--builddir=nginx-1.17.4 \
+--with-select_module \
+--with-poll_module \
+--with-threads \
+--with-file-aio \
+--with-http_ssl_module \
+--with-http_v2_module \
+--with-http_realip_module \
+--with-http_addition_module \
+--with-http_xslt_module=dynamic \
+--with-http_image_filter_module=dynamic \
+--with-http_sub_module \
+--with-http_dav_module \
+--with-http_flv_module \
+--with-http_mp4_module \
+--with-http_gunzip_module \
+--with-http_gzip_static_module \
+--with-http_auth_request_module \
+--with-http_random_index_module \
+--with-http_secure_link_module \
+--with-http_degradation_module \
+--with-http_slice_module \
+--with-http_stub_status_module \
+--http-log-path=/var/log/nginx/access.log \
+--http-client-body-temp-path=/var/cache/nginx/client_temp \
+--http-proxy-temp-path=/var/cache/nginx/proxy_temp \
+--http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp \
+--http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp \
+--http-scgi-temp-path=/var/cache/nginx/scgi_temp \
+--with-mail=dynamic \
+--with-mail_ssl_module \
+--with-stream=dynamic \
+--with-stream_ssl_module \
+--with-stream_realip_module \
+--with-stream_ssl_preread_module \
+--with-compat \
+--with-pcre=../pcre-8.42 \
+--with-pcre-jit \
+--with-zlib=../zlib-1.2.11 \
+--with-openssl=../openssl-1.1.0h \
+--with-openssl-opt=no-nextprotoneg \
+--with-debug
+"""
