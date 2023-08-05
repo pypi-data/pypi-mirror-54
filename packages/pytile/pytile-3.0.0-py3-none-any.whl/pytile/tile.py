68477e8e084cd110b98bf888f7f2f2d73d130e62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:21 +0000

    Add package pytile/3.0.0/pytile-3.0.0-py3-none-any.whl

---
+++ packages/pytile/pytile-3.0.0-py3-none-any.whl/pytile/tile.py
@@ -0,0 +1,37 @@
+"""Define endpoints for interacting with Tiles."""
+from typing import Awaitable, Callable, List, Optional
+from uuid import UUID
+
+
+class Tile:  # pylint: disable=too-few-public-methods
+    """Define "Tile" endpoints."""
+
+    def __init__(
+        self,
+        request: Callable[..., Awaitable[dict]],
+        *,
+        user_uuid: Optional[UUID] = None,
+    ) -> None:
+        """Initialize."""
+        self._request: Callable[..., Awaitable[dict]] = request
+        self._user_uuid: Optional[UUID] = user_uuid
+
+    async def all(self, whitelist: list = None, show_inactive: bool = False) -> list:
+        """Get all Tiles for a user's account."""
+        list_data: dict = await self._request(
+            "get", f"users/{self._user_uuid}/user_tiles"
+        )
+        tile_uuid_list: List[UUID] = [
+            tile["tile_uuid"]
+            for tile in list_data["result"]
+            if not whitelist or tile["tileType"] in whitelist
+        ]
+
+        tile_data: dict = await self._request(
+            "get", "tiles", params=[("tile_uuids", uuid) for uuid in tile_uuid_list]
+        )
+        return [
+            tile
+            for tile in tile_data["result"].values()
+            if show_inactive or tile["visible"] is True
+        ]
