c0e3297e99a67b7fccdd4d4c065d73f226502b98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:40 +0000

    Add package fattmerchant/1.1.3/fattmerchant-1.1.3.tar.gz

---
+++ packages/fattmerchant/fattmerchant-1.1.3.tar.gz/fattmerchant-1.1.3/README.md
@@ -0,0 +1,11 @@
+# Fattmerchant Python Client
+
+This package aims to provide assitance while interacting with the
+[Fattmerchant](https://fattmerchant.com/api-documentation/) rest API.
+
+This is the official python client which is supported by fattmerchant and bugs/issues/feature-requests can be posted on the official [github](https://github.com/fattmerchantorg/fattmerchant_python_client) page.
+
+## Python client documentation at [readthedocs](https://fattmerchant-python-sdk.readthedocs.io/en/latest/)
+
+build status
+[![Documentation Status](https://readthedocs.org/projects/fattmerchant-python-client/badge/?version=latest)](https://fattmerchant-python-client.readthedocs.io/en/latest/?badge=latest)
---
+++ packages/fattmerchant/fattmerchant-1.1.3.tar.gz/fattmerchant-1.1.3/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fattmerchant",
+    version="1.1.3",
+    author="Austin Burns",
+    author_email="austin.burns@fattmerchant.com",
+    description="Python client for Fattmerchant",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/fattmerchantorg/fattmerchant_python_client",
+    packages=setuptools.find_packages(),
+    package_data={
+        "": ["*.config", "*.txt"],
+    },
+    install_requires=["ddt", "requests"],
+    python_requires='>=2.7',
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
