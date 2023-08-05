477d2c59cf14770170dbad3afa3aaefe8e1b5c1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:50 +0000

    Add package dagster/0.6.2rc1/dagster-0.6.2rc1.tar.gz

---
+++ packages/dagster/dagster-0.6.2rc1.tar.gz/dagster-0.6.2rc1/dagster/__init__.py
@@ -0,0 +1,179 @@
+from dagster.core.definitions import (
+    CompositeSolidDefinition,
+    DependencyDefinition,
+    EventMetadataEntry,
+    ExecutionTargetHandle,
+    ExecutorDefinition,
+    ExpectationResult,
+    Failure,
+    InputDefinition,
+    JsonMetadataEntryData,
+    LoggerDefinition,
+    Materialization,
+    ModeDefinition,
+    MultiDependencyDefinition,
+    Output,
+    OutputDefinition,
+    PathMetadataEntryData,
+    PipelineDefinition,
+    PresetDefinition,
+    RepositoryDefinition,
+    ResourceDefinition,
+    ScheduleDefinition,
+    SolidDefinition,
+    SolidInvocation,
+    SystemStorageData,
+    SystemStorageDefinition,
+    TextMetadataEntryData,
+    TypeCheck,
+    UrlMetadataEntryData,
+    composite_solid,
+    executor,
+    lambda_solid,
+    logger,
+    pipeline,
+    resource,
+    schedules,
+    solid,
+    system_storage,
+)
+from dagster.core.engine.init import InitExecutorContext
+from dagster.core.errors import (
+    DagsterExecutionStepExecutionError,
+    DagsterExecutionStepNotFoundError,
+    DagsterInvalidConfigError,
+    DagsterInvalidDefinitionError,
+    DagsterInvariantViolationError,
+    DagsterResourceFunctionError,
+    DagsterStepOutputNotFoundError,
+    DagsterTypeCheckError,
+    DagsterUserCodeExecutionError,
+)
+from dagster.core.events import DagsterEventType
+from dagster.core.execution.api import (
+    execute_pipeline,
+    execute_pipeline_iterator,
+    execute_pipeline_with_preset,
+)
+from dagster.core.execution.config import RunConfig
+from dagster.core.execution.context.init import InitResourceContext
+from dagster.core.execution.context.logger import InitLoggerContext
+from dagster.core.execution.plan.objects import StepKind
+from dagster.core.execution.results import PipelineExecutionResult, SolidExecutionResult
+from dagster.core.storage.file_manager import FileHandle, LocalFileHandle
+from dagster.core.storage.init import InitSystemStorageContext
+from dagster.core.types import (
+    Any,
+    Bool,
+    Dict,
+    Field,
+    Float,
+    Int,
+    List,
+    NamedDict,
+    Nothing,
+    Optional,
+    Path,
+    PermissiveDict,
+    String,
+    input_hydration_config,
+    output_materialization_config,
+)
+from dagster.core.types.config import ConfigScalar, ConfigType, Enum, EnumValue
+from dagster.core.types.decorator import as_dagster_type, dagster_type
+from dagster.core.types.marshal import SerializationStrategy
+from dagster.core.types.runtime import RuntimeType, define_python_dagster_type
+from dagster.utils import file_relative_path
+from dagster.utils.test import execute_solid, execute_solids_within_pipeline
+
+from .version import __version__
+
+__all__ = [
+    # Definition
+    'CompositeSolidDefinition',
+    'DependencyDefinition',
+    'EventMetadataEntry',
+    'ExecutorDefinition',
+    'TextMetadataEntryData',
+    'UrlMetadataEntryData',
+    'PathMetadataEntryData',
+    'JsonMetadataEntryData',
+    'ExpectationResult',
+    'Failure',
+    'Field',
+    'InputDefinition',
+    'LoggerDefinition',
+    'Materialization',
+    'ModeDefinition',
+    'OutputDefinition',
+    'PipelineDefinition',
+    'RepositoryDefinition',
+    'ResourceDefinition',
+    'Output',
+    'SolidDefinition',
+    'SolidInvocation',
+    'SystemStorageDefinition',
+    # Decorators
+    'composite_solid',
+    'executor',
+    'lambda_solid',
+    'logger',
+    'pipeline',
+    'resource',
+    'solid',
+    'system_storage',
+    # Execution
+    'execute_pipeline_iterator',
+    'execute_pipeline',
+    'DagsterEventType',
+    'InitExecutorContext',
+    'InitLoggerContext',
+    'InitResourceContext',
+    'InitSystemStorageContext',
+    'PipelineExecutionResult',
+    'RunConfig',
+    'SolidExecutionResult',
+    'SystemStorageData',
+    # Errors
+    'DagsterExecutionStepExecutionError',
+    'DagsterExecutionStepNotFoundError',
+    'DagsterInvalidConfigError',
+    'DagsterInvalidDefinitionError',
+    'DagsterInvariantViolationError',
+    'DagsterResourceFunctionError',
+    'DagsterTypeCheckError',
+    'DagsterUserCodeExecutionError',
+    'DagsterStepOutputNotFoundError',
+    # Utilities
+    'execute_solid',
+    'execute_solids_within_pipeline',
+    'file_relative_path',
+    # types
+    'Any',
+    'Bool',
+    'input_hydration_config',
+    'Dict',
+    'Enum',
+    'EnumValue',
+    'Float',
+    'Int',
+    'List',
+    'NamedDict',
+    'Optional',
+    'output_materialization_config',
+    'Path',
+    'PermissiveDict',
+    'String',
+    'SerializationStrategy',
+    'Nothing',
+    # type creation
+    'as_dagster_type',
+    'dagster_type',
+    'define_python_dagster_type',
+    'RuntimeType',
+    'ConfigType',
+    'ConfigScalar',
+    # file things
+    'FileHandle',
+    'LocalFileHandle',
+]
