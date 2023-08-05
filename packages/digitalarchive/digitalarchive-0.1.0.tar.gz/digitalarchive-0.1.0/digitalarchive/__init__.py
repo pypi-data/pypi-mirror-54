3ebbb871b2308bd031e88064a7b8200141ea4191
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:18 +0000

    Add package digitalarchive/0.1.0/digitalarchive-0.1.0.tar.gz

---
+++ packages/digitalarchive/digitalarchive-0.1.0.tar.gz/digitalarchive-0.1.0/digitalarchive/__init__.py
@@ -0,0 +1,24 @@
+"""
+A python API and ORM for the Wilson Center's Digital Archive of historical documents.
+"""
+# __init__.py
+__version__ = "0.1.0"
+
+# Import DA models for convenience.
+from .models import (
+    Subject,
+    Language,
+    Transcript,
+    Translation,
+    MediaFile,
+    Contributor,
+    Donor,
+    Coverage,
+    Collection,
+    Repository,
+    Publisher,
+    Type,
+    Right,
+    Classification,
+    Document,
+)
