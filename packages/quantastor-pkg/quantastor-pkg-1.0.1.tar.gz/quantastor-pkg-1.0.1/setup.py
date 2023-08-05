db94d0d4b34e4cbce58d640b24b28e85ac3343d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:34 +0000

    Add package quantastor-pkg/1.0.1/quantastor-pkg-1.0.1.tar.gz

---
+++ packages/quantastor-pkg/quantastor-pkg-1.0.1.tar.gz/quantastor-pkg-1.0.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description_txt = fh.read()
+
+setuptools.setup(
+    name='quantastor-pkg',  
+    version='1.0.1',
+    scripts=['quantastor/qs_client.py'],
+    author="Seth Cagampang",
+    author_email="seth.cagampang@osnexus.com",
+    description="QuantaStor REST python library",
+    long_description=long_description_txt,
+    long_description_content_type="text/markdown",
+    url="https://github.com/OSNEXUS/QSPyClient",
+    packages=setuptools.find_packages(),
+    install_requires=['urllib3',],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+
+ )
