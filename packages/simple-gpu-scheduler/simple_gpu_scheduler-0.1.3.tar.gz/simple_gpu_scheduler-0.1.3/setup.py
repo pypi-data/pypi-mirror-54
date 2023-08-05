3eda9eac779192d6aec6accf5c310dd9921d4db7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:58 +0000

    Add package simple-gpu-scheduler/0.1.3/simple_gpu_scheduler-0.1.3.tar.gz

---
+++ packages/simple-gpu-scheduler/simple_gpu_scheduler-0.1.3.tar.gz/simple_gpu_scheduler-0.1.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+def read_readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(
+    name='simple_gpu_scheduler',
+    version='0.1.3',
+    description='A simple scheduler for running commands on multiple GPUs.',
+    long_description=read_readme(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/ExpectationMax/simple_gpu_scheduler',
+    author='Max Horn',
+    author_email='maexlich@gmail.com',
+    packages=['simple_gpu_scheduler'],
+    python_requires='>3.6',
+    classifiers=[
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
+    ],
+    entry_points={
+      'console_scripts': [
+          'simple_gpu_scheduler=simple_gpu_scheduler.scheduler:main',
+          'simple_hypersearch=simple_gpu_scheduler.hypersearch:generate_commands'
+      ],
+    },
+    zip_safe=False
+)
