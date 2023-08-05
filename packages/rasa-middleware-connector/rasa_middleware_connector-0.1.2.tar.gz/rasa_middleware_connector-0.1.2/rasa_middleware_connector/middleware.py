c013fefe2d7e6cf5056ada3ee1b1f61f55116e2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:09 +0000

    Add package rasa-middleware-connector/0.1.2/rasa_middleware_connector-0.1.2.tar.gz

---
+++ packages/rasa-middleware-connector/rasa_middleware_connector-0.1.2.tar.gz/rasa_middleware_connector-0.1.2/rasa_middleware_connector/middleware.py
@@ -0,0 +1,52 @@
+from rasa.core.channels.channel import UserMessage
+from typing import Callable, Text, Any, Dict
+
+class BaseMiddleware:
+
+    """
+    Interface for Middlewares
+    """
+
+    next = None
+    is_output = False
+
+    def set_next(self, next: Callable[[UserMessage], None], is_output):
+
+        """
+        Sets the next middleware to call.
+        """
+
+        self.next = next
+        self.is_output = is_output
+
+    async def compute(self, *args):
+        
+        """
+        This method sends the message to the correct function.
+        """
+
+        if self.is_output:
+            self.output_compute(*args)
+        else:
+            self.input_compute(*args)
+
+    async def input_compute(self, message: UserMessage):
+
+        """
+        This method process a input message, encapsulated in a UserMessage object 
+
+        When done, 'await self.next(message)' should be called.
+        """
+
+        raise NotImplementedError()
+
+    
+    async def output_compute(self, recipient_id: Text, message: Dict[Text, Any]):
+        
+        """
+        This method process a output message. 
+
+        When done, 'await self.next(recipient_id, message)' should be called.
+        """
+
+        raise NotImplementedError()
