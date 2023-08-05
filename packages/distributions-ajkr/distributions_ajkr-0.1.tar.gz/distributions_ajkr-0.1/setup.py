9cd1d8a47a02bce7d21150a7b675929c7eeab6fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:04 +0000

    Add package distributions-ajkr/0.1/distributions_ajkr-0.1.tar.gz

---
+++ packages/distributions-ajkr/distributions_ajkr-0.1.tar.gz/distributions_ajkr-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_ajkr',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_ajkr'],
+      zip_safe=False)
