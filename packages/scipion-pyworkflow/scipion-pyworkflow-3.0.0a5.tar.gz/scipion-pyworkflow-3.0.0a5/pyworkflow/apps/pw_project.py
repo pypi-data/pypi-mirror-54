08c5b633f38cefbb312aba0df58af54e8c7aeb35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:49 +0000

    Add package scipion-pyworkflow/3.0.0a5/scipion-pyworkflow-3.0.0a5.tar.gz

---
+++ packages/scipion-pyworkflow/scipion-pyworkflow-3.0.0a5.tar.gz/scipion-pyworkflow-3.0.0a5/pyworkflow/apps/pw_project.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# **************************************************************************
+# *
+# * Authors:     J.M. De la Rosa Trevin (jmdelarosa@cnb.csic.es)
+# *
+# * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
+# *
+# * This program is free software; you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License as published by
+# * the Free Software Foundation; either version 2 of the License, or
+# * (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program; if not, write to the Free Software
+# * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+# * 02111-1307  USA
+# *
+# *  All comments concerning this program package may be sent to the
+# *  e-mail address 'scipion@cnb.csic.es'
+# *
+# **************************************************************************
+"""
+Launch main project window 
+"""
+
+from __future__ import print_function
+
+import sys
+import os
+
+from pyworkflow.project import Manager
+from pyworkflow.gui.project import ProjectWindow
+import pyworkflow.utils as pwutils
+
+
+if __name__ == '__main__':
+
+    # Add callback for remote debugging if available.
+    try:
+        from rpdb2 import start_embedded_debugger
+        from signal import signal, SIGUSR2
+        signal(SIGUSR2, lambda sig, frame: start_embedded_debugger('a'))
+    except ImportError:
+        pass
+
+    if len(sys.argv) > 1:
+        manager = Manager()
+        projName = os.path.basename(sys.argv[1])
+
+        # Handle special name 'here' to create a project
+        # from the current directory
+        if projName == 'here':
+            cwd = os.environ['SCIPION_CWD']
+            print("\nYou are trying to create a project here:",
+                  pwutils.cyan(cwd))
+
+            if os.listdir(cwd):
+                print(pwutils.red('\nWARNING: this folder is not empty!!!'))
+            key = input("\nDo you want to create a project here? [y/N]?")
+
+            if key.lower().strip() != 'y':
+                print("\nAborting...")
+                sys.exit(0)
+            else:
+                print("\nCreating project....")
+                projName = os.path.basename(cwd)
+                projDir = os.path.dirname(cwd)
+                proj = manager.createProject(projName, location=projDir)
+
+        elif projName == 'last':  # Get last project
+            projects = manager.listProjects()
+            if not projects:
+                sys.exit("No projects yet, cannot open the last one.")
+            projName = projects[0].projName
+            
+        projPath = manager.getProjectPath(projName)
+        # try:
+        projWindow = ProjectWindow(projPath)
+        # except Exception as e:
+        #     # Print any exception
+        #     print("ERROR: At pw_project.py loading Project %s.\n"
+        #           "       Message: %s\n" % (projPath, e))
+        #
+        #     import traceback
+        #     traceback.print_exc(file=sys.stderr)
+        #
+        #     sys.exit(e)
+
+        projWindow.show()
+    else:
+        print("usage: pw_project.py PROJECT_NAME")
---
+++ packages/scipion-pyworkflow/scipion-pyworkflow-3.0.0a5.tar.gz/scipion-pyworkflow-3.0.0a5/pyworkflow/tests/test_protocol_execution.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+# **************************************************************************
+# *
+# * Authors:     J.M. De la Rosa Trevin (delarosatrevin@scilifelab.se) [1]
+# *
+# * [1] SciLifeLab, Stockholm University
+# *
+# * This program is free software: you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License as published by
+# * the Free Software Foundation, either version 3 of the License, or
+# * (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# *
+# *  All comments concerning this program package may be sent to the
+# *  e-mail address 'scipion@cnb.csic.es'
+# *
+# **************************************************************************
+
+import pyworkflow.tests as pwtests
+import pyworkflow.mapper as pwmapper
+import pyworkflow.protocol as pwprot
+
+import mock_domain as mod
+import mock_domain.protocols as modprot
+
+
+# TODO: this test seems not to be finished.
+class TestProtocolExecution(pwtests.BaseTest):
+    
+    @classmethod
+    def setUpClass(cls):
+        pwtests.setupTestOutput(cls)
+    
+    def test_StepExecutor(self):
+        """Test the list with several Complex"""
+        fn = self.getOutputPath("protocol.sqlite")
+        print("Writing to db: %s" % fn)
+
+        # Discover objects and protocols
+        mapperDict = mod.Domain.getMapperDict()
+
+        # Check that the protocol has associated package
+        mapper = pwmapper.SqliteMapper(fn, mapperDict)
+        prot = modprot.SleepingProtocol(mapper=mapper, n=2,
+                                        workingDir=self.getOutputPath(''))
+        domain = prot.getClassDomain()
+        domain.printInfo()
+
+        prot.setStepsExecutor(pwprot.StepExecutor(hostConfig=None))
+        prot.run()
+        mapper.commit()
+        mapper.close()
+
+        self.assertEqual(prot._steps[0].getStatus(), pwprot.STATUS_FINISHED)
+        
+        mapper2 = pwmapper.SqliteMapper(fn, mapperDict)
+        prot2 = mapper2.selectById(prot.getObjId())
+        
+        self.assertEqual(prot.endTime.get(), prot2.endTime.get())
