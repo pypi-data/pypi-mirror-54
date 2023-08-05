e71ea03c3b70f8f790aa524decefd602916c202a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:21 +0000

    Add package flatlibfr/0.0.1.dev4/flatlibfr-0.0.1.dev4-py3.7.egg

---
+++ packages/flatlibfr/flatlibfr-0.0.1.dev4-py3.7.egg/flatlibfr/__init__.py
@@ -0,0 +1,16 @@
+"""
+    This file is part of flatlib - (C) FlatAngle
+    Author: João Ventura (flatangleweb@gmail.com)
+    
+    flatlibfr translation of const by Stéphane Bressani (s.bressani@bluewin.ch)
+
+"""
+
+import os
+
+
+__version__ = '0.0.1.dev4'
+
+# Library and resource paths
+PATH_LIB = os.path.dirname(__file__) + os.sep
+PATH_RES = PATH_LIB + 'resources' + os.sep
---
+++ packages/flatlibfr/flatlibfr-0.0.1.dev4-py3.7.egg/flatlibfr/dignities/__init__.py
@@ -0,0 +1,9 @@
+"""
+    This file is part of flatlib - (C) FlatAngle
+    Author: João Ventura (flatangleweb@gmail.com)
+    flatlibfr translation of const by Stéphane Bressani (s.bressani@bluewin.ch)
+    
+    This subpackage implements the Essential and
+    Accidental dignities.
+  
+"""
