8e7106c7ab3a62b55e0d0c4ed9e7dd7cc7362539
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:14:13 +0000

    Add package pytest-level/0.1.2/pytest_level-0.1.2-py3-none-any.whl

---
+++ packages/pytest-level/pytest_level-0.1.2-py3-none-any.whl/pytest_level.py
@@ -0,0 +1,31 @@
+import pytest
+
+def pytest_configure(config):
+    config.addinivalue_line(
+        "markers",
+        "level(lvl): Don't run the test if `lvl` is higher than current --level."
+    )
+
+def pytest_addoption(parser):
+    parser.addoption('--level', type=int)
+
+
+@pytest.fixture
+def level(config):
+    return config.getoption('--level')
+
+
+def pytest_collection_modifyitems(session, config, items):
+    selected_level = config.getoption('--level')
+    if selected_level == None:
+        return
+
+    deselected_items = set()
+    for item in items:
+        for marker in item.iter_markers('level'):
+            [marker_level] = marker.args
+            if marker_level > selected_level:
+                deselected_items.add(item)
+    selected_items = [item for item in items if item not in deselected_items]
+    items[:] = selected_items
+    config.hook.pytest_deselected(items=deselected_items)
