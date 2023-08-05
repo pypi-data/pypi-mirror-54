0ef14866e0c5e4c76871ed2437ca371bf510158b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:13 +0000

    Add package dgPre/0.0.1/dgPre-0.0.1.tar.gz

---
+++ packages/dgPre/dgPre-0.0.1.tar.gz/dgPre-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='dgPre',
+      version='0.0.1',
+      description='print name',
+      url='https://github.dgPre/BigDataFounder/string2date',
+      author='viton',
+      author_email='viton@gmail.dgPre',
+      license='MIT',
+      packages=['dgPre'])
