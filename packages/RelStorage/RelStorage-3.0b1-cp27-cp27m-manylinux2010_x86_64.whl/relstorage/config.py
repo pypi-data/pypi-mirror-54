6d2b5079b066d1f39a10fcd33ee244945726ab88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:53 +0000

    Add package RelStorage/3.0b1/RelStorage-3.0b1-cp27-cp27m-manylinux2010_x86_64.whl

---
+++ packages/RelStorage/RelStorage-3.0b1-cp27-cp27m-manylinux2010_x86_64.whl/relstorage/config.py
@@ -0,0 +1,77 @@
+##############################################################################
+#
+# Copyright (c) 2008 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""ZConfig directive implementations for binding RelStorage to Zope"""
+from __future__ import absolute_import
+
+from relstorage.options import Options
+from relstorage.storage import RelStorage
+
+
+class BaseConfig(object):
+
+    def __init__(self, config):
+        self.config = config
+        self.name = config.getSectionName()
+
+class RelStorageFactory(BaseConfig):
+    """Open a storage configured via ZConfig"""
+    def open(self):
+        config = self.config
+        # Hoist the driver setting to the section we really want it.
+        config.driver = config.adapter.config.driver
+        # But don't remove it or otherwise mutate the config object;
+        # that would prevent us from being correctly opened again.
+        #config.adapter.config.driver = None
+        options = Options.copy_valid_options(config)
+        adapter = config.adapter.create(options)
+        return RelStorage(adapter, name=config.name, options=options)
+
+
+class PostgreSQLAdapterFactory(BaseConfig):
+    def create(self, options):
+        from .adapters.postgresql import PostgreSQLAdapter
+        return PostgreSQLAdapter(
+            dsn=self.config.dsn,
+            options=options)
+
+
+class OracleAdapterFactory(BaseConfig):
+    def create(self, options):
+        from .adapters.oracle import OracleAdapter
+        config = self.config
+        return OracleAdapter(
+            user=config.user,
+            password=config.password,
+            dsn=config.dsn,
+            options=options)
+
+
+class MySQLAdapterFactory(BaseConfig):
+    def create(self, options):
+        from .adapters.mysql import MySQLAdapter
+        params = {}
+        for key in self.config.getSectionAttributes():
+            if key == 'driver':
+                continue
+            value = getattr(self.config, key)
+            if value is not None:
+                params[key] = value
+        return MySQLAdapter(options=options, **params)
+
+
+class Sqlite3AdapterFactory(BaseConfig):
+    def create(self, options):
+        from .adapters.sqlite.adapter import Sqlite3Adapter
+        return Sqlite3Adapter(path=self.config.path,
+                              options=options)
