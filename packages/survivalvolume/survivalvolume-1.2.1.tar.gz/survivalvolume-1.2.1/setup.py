1db4792895965fd9ff10da97b02041e145c8b8dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:09 +0000

    Add package survivalvolume/1.2.1/survivalvolume-1.2.1.tar.gz

---
+++ packages/survivalvolume/survivalvolume-1.2.1.tar.gz/survivalvolume-1.2.1/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='survivalvolume',
+    version='1.2.1',
+    author='Matthew Wakefield',
+    author_email='matthew.wakefield@unimelb.edu.au',
+    install_requires = [
+      'setuptools',
+      'lifelines>=0.22.8',
+      'matplotlib',
+      'mpld3',
+      'numpy',
+      'pandas',
+      'scipy',
+      'xlrd',
+    ],
+    packages=['survivalvolume',
+              ],
+    zip_safe = True,
+    include_package_data = True,
+    url='https://git@bitbucket.org/genomematt/SurvivalVolume.git',
+    license='GPLv3',
+    description='Plotting tools for survival data',
+    long_description='',
+    classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+          'Operating System :: POSIX',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Topic :: Scientific/Engineering :: Bio-Informatics',
+    ],
+
+)
