6ec68c1e3cd94ae8c2a996c01536462ba84c9151
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:31 +0000

    Add package suanpan/0.9.97/suanpan-0.9.97.tar.gz

---
+++ packages/suanpan/suanpan-0.9.97.tar.gz/suanpan-0.9.97/suanpan/g.py
@@ -0,0 +1,23 @@
+# coding=utf-8
+from __future__ import absolute_import, print_function
+
+import os
+
+from suanpan.objects import Context
+
+g = Context(
+    userId=os.environ.get("SP_USER_ID"),
+    appId=os.environ.get("SP_APP_ID"),
+    nodeId=os.environ.get("SP_NODE_ID"),
+    nodeGroup=os.environ.get("SP_NODE_GROUP", "default"),
+    host=os.environ.get("SP_HOST"),
+    hostTls=os.environ.get("SP_HOST_TLS") in ("true", "True"),
+    apiHost=os.environ.get("SP_API_HOST"),
+    apiHostTls=os.environ.get("SP_API_HOST_TLS") in ("true", "True"),
+    affinity=os.environ.get("SP_AFFINITY"),
+    accessKey=os.environ.get("SP_ACCESS_KEY"),
+    accessSecret=os.environ.get("SP_ACCESS_SECRET"),
+    userIdHeaderField=os.environ.get("SP_USER_ID_HEADER_FIELD", "x-sp-user-id"),
+    userSignatureHeaderField=os.environ.get("SP_USER_SIGNATURE_HEADER_FIELD", "x-sp-signature"),
+    userSignVersionHeaderField=os.environ.get("SP_USER_SIGN_VERSION_HEADER_FIELD", "x-sp-sign-version"),
+)
