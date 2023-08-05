dd7f2e265743e5e9f38da9e238feb4a0ee13904b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:14 +0000

    Add package jedopo-py/0.0.5/jedopo-py-0.0.5.tar.gz

---
+++ packages/jedopo-py/jedopo-py-0.0.5.tar.gz/jedopo-py-0.0.5/jedopo/poc.py
@@ -0,0 +1,7 @@
+from jedopo import VERSION
+from click import command, version_option
+
+@command()
+@version_option()
+def main():
+    pass
---
+++ packages/jedopo-py/jedopo-py-0.0.5.tar.gz/jedopo-py-0.0.5/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+from setuptools import find_packages, setup
+
+
+project = "jedopo-py"
+version = "0.0.5"
+
+
+setup(
+    name=project,
+    version=version,
+    author="Jeremiah Porten",
+    author_email="jeremiah@gmail.com",
+    description="A poc package",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/wheremiah",
+    packages=find_packages(),
+    python_requires='>=3.6',
+    install_requires=[
+        "click>=6.7",
+    ],
+    entry_points={
+        "console_scripts": [
+            "jedopo = jedopo.poc:main",
+        ],
+    },
+)
