b08ca0c075c3bf8e25099a309714151f78b72482
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:43 +0000

    Add package SimiLab/0.0.5.17.dev0/SimiLab-0.0.5.17.dev0.tar.gz

---
+++ packages/SimiLab/SimiLab-0.0.5.17.dev0.tar.gz/SimiLab-0.0.5.17.dev0/setup.py
@@ -0,0 +1,39 @@
+import setuptools
+import sys
+import os
+import shutil
+import glob
+import subprocess
+
+from distutils.command.sdist import sdist
+
+# This directory
+dir_setup = os.path.dirname(os.path.realpath(__file__))
+
+
+with open("similab\README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="SimiLab", #Este es el nombre que figura en PyPI
+    version="0.0.5.17dev",
+    author="CID",
+    author_email="author@example.com",
+    description="NPL Package in development",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords='NLP corpus meaning',
+    url="https://github.com/CID-ITBA",
+    packages=["similab"],#setuptools.find_packages(),
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+          'numpy',
+          'scipy',
+      ],
+    python_requires='>=3.6',
+)
