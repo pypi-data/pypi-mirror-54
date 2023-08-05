52c3433a68565dd484b396f050481cbc651636a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:23 +0000

    Add package iterfilesystem/1.3.1/iterfilesystem-1.3.1-py3.6.egg

---
+++ packages/iterfilesystem/iterfilesystem-1.3.1-py3.6.egg/iterfilesystem/__init__.py
@@ -0,0 +1,12 @@
+"""
+    Top-level package for iterfilesystem.
+"""
+
+
+__author__ = 'Jens Diemer'
+__email__ = 'python@jensdiemer.de'
+__version__ = '1.3.1'
+
+
+def get_module_version():
+    return __version__
