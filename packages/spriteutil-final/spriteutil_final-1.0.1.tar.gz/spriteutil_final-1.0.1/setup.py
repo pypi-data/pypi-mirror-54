2cdabb4991ad044ca32b7d1615de3289905602bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:49 +0000

    Add package spriteutil-final/1.0.1/spriteutil_final-1.0.1.tar.gz

---
+++ packages/spriteutil-final/spriteutil_final-1.0.1.tar.gz/spriteutil_final-1.0.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="spriteutil_final",
+    version="1.0.1",
+    author="Long Lam Duc",
+    author_email="long.lam@f4.intek.edu.vn",
+    description="A sprite detection package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/intek-training-jsc/sprite-detection-longlamduc`",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "numpy==1.17.2",
+        "Pillow==6.2.0",
+    ],
+    python_requires='>=3.6',
+)
