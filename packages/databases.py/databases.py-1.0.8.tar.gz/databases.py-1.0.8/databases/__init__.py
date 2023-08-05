7e452a5184c2d7872a6cd0e0b28d17e62f4ad4a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:33 +0000

    Add package databases.py/1.0.8/databases.py-1.0.8.tar.gz

---
+++ packages/databases.py/databases.py-1.0.8.tar.gz/databases.py-1.0.8/databases/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+'''
+databases.py
+------------
+
+Easily create and manipulate databases with databases.py
+
+:license: - MIT, see LICENSE for more details.
+:copyright: (c) Caio Alexandre
+'''
+
+__title__ = 'databases'
+__author__ = 'Caio Alexandre'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2019 Caio Alexandre'
+__version__ = '1.0.8'
+
+from . import sqlite3
+
+from .errors import *
