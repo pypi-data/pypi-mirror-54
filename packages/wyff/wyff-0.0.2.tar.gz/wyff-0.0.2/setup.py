2e58fa12665f7b24ff31532a027d7f68b5cb7288
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:56 +0000

    Add package wyff/0.0.2/wyff-0.0.2.tar.gz

---
+++ packages/wyff/wyff-0.0.2.tar.gz/wyff-0.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "wyff",
+    version = "0.0.2",
+    keywords = ("pip", "wyf"),
+    description = "my first package",
+    long_description = "wyf for python",
+    license = "MIT Licence",
+
+    url = "http://github.com/wangfeng22",
+    author = "wyf",
+    author_email = "2246521484@qq.com",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = []
+)
