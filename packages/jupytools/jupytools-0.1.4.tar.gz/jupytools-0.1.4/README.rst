49931db8904d72cd0be68b9b06e0bfe483162a54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:02 +0000

    Add package jupytools/0.1.4/jupytools-0.1.4.tar.gz

---
+++ packages/jupytools/jupytools-0.1.4.tar.gz/jupytools-0.1.4/README.rst
@@ -0,0 +1,11 @@
+Jupytools
+=========
+
+Convenience tools to simplify Jupyter development.
+
+Features
+--------
+
+* Exporting Jupyter notebook cells annotated with `#export` commentary
+* Automatically choose the implementation of `set_trace` function depending where the script is running
+* Extend interpreter search path to include directories with additional utilities to import in notebook
---
+++ packages/jupytools/jupytools-0.1.4.tar.gz/jupytools-0.1.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+exec(open('jupytools/version.py').read())
+
+setup(
+    name='jupytools',
+    version=__version__,
+    maintainer='devforfu',
+    description='A set of utilities making Jupyter development a bit easier',
+    long_description=open('README.rst', 'r').read(),
+    long_description_content_type='text/x-rst',
+    url='https://github.com/devforfu/jupytools',
+    download_url=f'https://github.com/devforfu/jupytools/archive/v{__version__}.tar.gz',
+    install_requires=['jupyter'],
+    packages=find_packages()
+)
