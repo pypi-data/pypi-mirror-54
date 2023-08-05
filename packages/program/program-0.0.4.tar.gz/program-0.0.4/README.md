03daccc1faef8856459186581cc0c0612376bf90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:29 +0000

    Add package program/0.0.4/program-0.0.4.tar.gz

---
+++ packages/program/program-0.0.4.tar.gz/program-0.0.4/README.md
@@ -0,0 +1,1 @@
+A warning to be more careful when pulling in supply chain dependencies. Prints a link to a supply chain security paper.
---
+++ packages/program/program-0.0.4.tar.gz/program-0.0.4/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as readme:
+    long_description = readme.read()
+
+setuptools.setup(
+    name="program",
+    version="0.0.4",
+    author="Aditya Saky",
+    author_email="aditya@saky.in",
+    description="A warning to be more careful when pulling in dependencies",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/adityasaky/program",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
