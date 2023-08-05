bb0ea00681cd05014f7d7620d28c5ed8c8d9315c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:11 +0000

    Add package harrytsz02/0.0.1/harrytsz02-0.0.1.tar.gz

---
+++ packages/harrytsz02/harrytsz02-0.0.1.tar.gz/harrytsz02-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='harrytsz02',
+      version='0.0.1',
+      description='string to date',
+      author='harrytsz',
+      author_email='taosizhan@gmail.com',
+      license='MIT',
+      packages=['helloworld'])
