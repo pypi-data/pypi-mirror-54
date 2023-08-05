7a575eed934df2eeb761ee22c5ea9d72cd6ef145
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:37:19 +0000

    Add package anvil101/1.0.1/anvil101-1.0.1.tar.gz

---
+++ packages/anvil101/anvil101-1.0.1.tar.gz/anvil101-1.0.1/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup 
+setup(
+    name='anvil101',    # This is the name of your PyPI-package.
+    version='1.0.1',                          # Update the version number for new releases
+    scripts=['anvil101.py']                  # The name of your scipt, and also the command you'll be using for calling it
+)
