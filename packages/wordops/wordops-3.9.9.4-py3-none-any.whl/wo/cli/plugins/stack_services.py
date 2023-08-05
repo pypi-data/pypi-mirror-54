25b9ee3a9a38bd39fdf5d945141b598769260c0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:15 +0000

    Add package wordops/3.9.9.4/wordops-3.9.9.4-py3-none-any.whl

---
+++ packages/wordops/wordops-3.9.9.4-py3-none-any.whl/wo/cli/plugins/stack_services.py
@@ -0,0 +1,444 @@
+import os
+
+from cement.core.controller import CementBaseController, expose
+
+from wo.core.aptget import WOAptGet
+from wo.core.logging import Log
+from wo.core.services import WOService
+from wo.core.variables import WOVar
+
+
+class WOStackStatusController(CementBaseController):
+    class Meta:
+        label = 'stack_services'
+        stacked_on = 'stack'
+        stacked_type = 'embedded'
+        description = 'Check the stack status'
+
+    @expose(help="Start stack services")
+    def start(self):
+        """Start services"""
+        services = []
+        pargs = self.app.pargs
+        if not (pargs.nginx or pargs.php or
+                pargs.php73 or
+                pargs.mysql or
+                pargs.redis or
+                pargs.fail2ban or
+                pargs.proftpd or
+                pargs.netdata):
+            pargs.nginx = True
+            pargs.php = True
+            pargs.mysql = True
+            pargs.fail2ban = True
+            pargs.netdata = True
+
+        if pargs.nginx:
+            if (WOAptGet.is_installed(self, 'nginx-custom')):
+                services = services + ['nginx']
+            else:
+                Log.info(self, "Nginx is not installed")
+
+        if pargs.php:
+            if WOAptGet.is_installed(self, 'php7.2-fpm'):
+                services = services + ['php7.2-fpm']
+            else:
+                Log.info(self, "PHP7.2-FPM is not installed")
+            if WOAptGet.is_installed(self, 'php7.3-fpm'):
+                services = services + ['php7.3-fpm']
+            else:
+                Log.info(self, "PHP7.3-FPM is not installed")
+
+        if pargs.php73:
+            if WOAptGet.is_installed(self, 'php7.3-fpm'):
+                services = services + ['php7.3-fpm']
+            else:
+                Log.info(self, "PHP7.3-FPM is not installed")
+
+        if pargs.mysql:
+            if ((WOVar.wo_mysql_host == "localhost") or
+                    (WOVar.wo_mysql_host == "127.0.0.1")):
+                if (WOAptGet.is_installed(self, 'mysql-server') or
+                    WOAptGet.is_installed(self, 'percona-server-server-5.6') or
+                        WOAptGet.is_installed(self, 'mariadb-server')):
+                    services = services + ['mysql']
+                else:
+                    Log.info(self, "MySQL is not installed")
+            else:
+                Log.warn(self, "Remote MySQL found, "
+                         "Unable to check MySQL service status")
+
+        if pargs.redis:
+            if WOAptGet.is_installed(self, 'redis-server'):
+                services = services + ['redis-server']
+            else:
+                Log.info(self, "Redis server is not installed")
+
+        if pargs.fail2ban:
+            if WOAptGet.is_installed(self, 'fail2ban'):
+                services = services + ['fail2ban']
+            else:
+                Log.info(self, "fail2ban is not installed")
+
+        # proftpd
+        if pargs.proftpd:
+            if WOAptGet.is_installed(self, 'proftpd-basic'):
+                services = services + ['proftpd']
+            else:
+                Log.info(self, "ProFTPd is not installed")
+
+        # netdata
+        if pargs.netdata:
+            if (os.path.isdir("/opt/netdata") or
+                    os.path.isdir("/etc/netdata")):
+                services = services + ['netdata']
+            else:
+                Log.info(self, "Netdata is not installed")
+
+        for service in services:
+            Log.debug(self, "Starting service: {0}".format(service))
+            WOService.start_service(self, service)
+
+    @expose(help="Stop stack services")
+    def stop(self):
+        """Stop services"""
+        services = []
+        pargs = self.app.pargs
+        if not (pargs.nginx or pargs.php or
+                pargs.php73 or
+                pargs.mysql or
+                pargs.fail2ban or
+                pargs.netdata or
+                pargs.proftpd or
+                pargs.redis):
+            pargs.nginx = True
+            pargs.php = True
+            pargs.mysql = True
+
+        # nginx
+        if pargs.nginx:
+            if (WOAptGet.is_installed(self, 'nginx-custom')):
+                services = services + ['nginx']
+            else:
+                Log.info(self, "Nginx is not installed")
+
+        # php7.2
+        if pargs.php:
+            if WOAptGet.is_installed(self, 'php7.2-fpm'):
+                services = services + ['php7.2-fpm']
+            else:
+                Log.info(self, "PHP7.2-FPM is not installed")
+
+            if WOAptGet.is_installed(self, 'php7.3-fpm'):
+                services = services + ['php7.3-fpm']
+            else:
+                Log.info(self, "PHP7.3-FPM is not installed")
+
+        # php7.3
+        if pargs.php73:
+            if WOAptGet.is_installed(self, 'php7.3-fpm'):
+                services = services + ['php7.3-fpm']
+            else:
+                Log.info(self, "PHP7.3-FPM is not installed")
+
+        # mysql
+        if pargs.mysql:
+            if ((WOVar.wo_mysql_host == "localhost") or
+                    (WOVar.wo_mysql_host == "127.0.0.1")):
+                if (WOAptGet.is_installed(self, 'mysql-server') or
+                    WOAptGet.is_installed(self, 'percona-server-server-5.6') or
+                        WOAptGet.is_installed(self, 'mariadb-server')):
+                    services = services + ['mysql']
+                else:
+                    Log.info(self, "MySQL is not installed")
+            else:
+                Log.warn(self, "Remote MySQL found, "
+                         "Unable to check MySQL service status")
+
+        # redis
+        if pargs.redis:
+            if WOAptGet.is_installed(self, 'redis-server'):
+                services = services + ['redis-server']
+            else:
+                Log.info(self, "Redis server is not installed")
+
+        # fail2ban
+        if pargs.fail2ban:
+            if WOAptGet.is_installed(self, 'fail2ban'):
+                services = services + ['fail2ban']
+            else:
+                Log.info(self, "fail2ban is not installed")
+
+        # proftpd
+        if pargs.proftpd:
+            if WOAptGet.is_installed(self, 'proftpd-basic'):
+                services = services + ['proftpd']
+            else:
+                Log.info(self, "ProFTPd is not installed")
+
+        # netdata
+        if pargs.netdata:
+            if (os.path.isdir("/opt/netdata") or
+                    os.path.isdir("/etc/netdata")):
+                services = services + ['netdata']
+            else:
+                Log.info(self, "Netdata is not installed")
+
+        for service in services:
+            Log.debug(self, "Stopping service: {0}".format(service))
+            WOService.stop_service(self, service)
+
+    @expose(help="Restart stack services")
+    def restart(self):
+        """Restart services"""
+        services = []
+        pargs = self.app.pargs
+        if not (pargs.nginx or pargs.php or
+                pargs.php73 or
+                pargs.mysql or
+                pargs.netdata or
+                pargs.proftpd or
+                pargs.redis or
+                pargs.fail2ban):
+            pargs.nginx = True
+            pargs.php = True
+            pargs.mysql = True
+            pargs.netdata = True
+
+        if pargs.nginx:
+            if (WOAptGet.is_installed(self, 'nginx-custom')):
+                services = services + ['nginx']
+            else:
+                Log.info(self, "Nginx is not installed")
+
+        if pargs.php:
+            if WOAptGet.is_installed(self, 'php7.2-fpm'):
+                services = services + ['php7.2-fpm']
+            else:
+                Log.info(self, "PHP7.2-FPM is not installed")
+
+            if WOAptGet.is_installed(self, 'php7.3-fpm'):
+                services = services + ['php7.3-fpm']
+            else:
+                Log.info(self, "PHP7.3-FPM is not installed")
+
+        if pargs.php73:
+            if WOAptGet.is_installed(self, 'php7.3-fpm'):
+                services = services + ['php7.3-fpm']
+            else:
+                Log.info(self, "PHP7.3-FPM is not installed")
+
+        if pargs.mysql:
+            if ((WOVar.wo_mysql_host == "localhost") or
+                    (WOVar.wo_mysql_host == "127.0.0.1")):
+                if ((WOAptGet.is_installed(self, 'mysql-server') or
+                     WOAptGet.is_installed(self,
+                                           'percona-server-server-5.6') or
+                        WOAptGet.is_installed(self, 'mariadb-server'))):
+                    services = services + ['mysql']
+                else:
+                    Log.info(self, "MySQL is not installed")
+            else:
+                Log.warn(self, "Remote MySQL found, "
+                         "Unable to check MySQL service status")
+
+        if pargs.redis:
+            if WOAptGet.is_installed(self, 'redis-server'):
+                services = services + ['redis-server']
+            else:
+                Log.info(self, "Redis server is not installed")
+
+        if pargs.fail2ban:
+            if WOAptGet.is_installed(self, 'fail2ban'):
+                services = services + ['fail2ban']
+            else:
+                Log.info(self, "fail2ban is not installed")
+
+        # proftpd
+        if pargs.proftpd:
+            if WOAptGet.is_installed(self, 'proftpd-basic'):
+                services = services + ['proftpd']
+            else:
+                Log.info(self, "ProFTPd is not installed")
+
+        # netdata
+        if pargs.netdata:
+            if (os.path.isdir("/opt/netdata") or
+                    os.path.isdir("/etc/netdata")):
+                services = services + ['netdata']
+            else:
+                Log.info(self, "Netdata is not installed")
+
+        for service in services:
+            Log.debug(self, "Restarting service: {0}".format(service))
+            WOService.restart_service(self, service)
+
+    @expose(help="Get stack status")
+    def status(self):
+        """Status of services"""
+        services = []
+        pargs = self.app.pargs
+        if not (pargs.nginx or pargs.php or
+                pargs.php73 or
+                pargs.mysql or
+                pargs.netdata or
+                pargs.proftpd or
+                pargs.redis or
+                pargs.fail2ban):
+            pargs.nginx = True
+            pargs.php = True
+            pargs.mysql = True
+            pargs.fail2ban = True
+            pargs.netdata = True
+
+        if pargs.nginx:
+            if (WOAptGet.is_installed(self, 'nginx-custom')):
+                services = services + ['nginx']
+            else:
+                Log.info(self, "Nginx is not installed")
+
+        if pargs.php:
+            if WOAptGet.is_installed(self, 'php7.2-fpm'):
+                services = services + ['php7.2-fpm']
+            else:
+                Log.info(self, "PHP7.2-FPM is not installed")
+
+            if WOAptGet.is_installed(self, 'php7.3-fpm'):
+                services = services + ['php7.3-fpm']
+            else:
+                Log.info(self, "PHP7.3-FPM is not installed")
+
+        if pargs.php73:
+            if WOAptGet.is_installed(self, 'php7.3-fpm'):
+                services = services + ['php7.3-fpm']
+            else:
+                Log.info(self, "PHP7.3-FPM is not installed")
+
+        if pargs.mysql:
+            if ((WOVar.wo_mysql_host == "localhost") or
+                    (WOVar.wo_mysql_host == "127.0.0.1")):
+                if (WOAptGet.is_installed(self, 'mysql-server') or
+                    WOAptGet.is_installed(self, 'percona-server-server-5.6') or
+                        WOAptGet.is_installed(self, 'mariadb-server')):
+                    services = services + ['mysql']
+                else:
+                    Log.info(self, "MySQL is not installed")
+            else:
+                Log.warn(self, "Remote MySQL found, "
+                         "Unable to check MySQL service status")
+
+        if pargs.redis:
+            if WOAptGet.is_installed(self, 'redis-server'):
+                services = services + ['redis-server']
+            else:
+                Log.info(self, "Redis server is not installed")
+
+        if pargs.fail2ban:
+            if WOAptGet.is_installed(self, 'fail2ban'):
+                services = services + ['fail2ban']
+            else:
+                Log.info(self, "fail2ban is not installed")
+
+        # proftpd
+        if pargs.proftpd:
+            if WOAptGet.is_installed(self, 'proftpd-basic'):
+                services = services + ['proftpd']
+            else:
+                Log.info(self, "ProFTPd is not installed")
+
+        # netdata
+        if pargs.netdata:
+            if (os.path.isdir("/opt/netdata") or
+                    os.path.isdir("/etc/netdata")):
+                services = services + ['netdata']
+            else:
+                Log.info(self, "Netdata is not installed")
+
+        for service in services:
+            if WOService.get_service_status(self, service):
+                Log.info(self, "{0:10}:  {1}".format(service, "Running"))
+
+    @expose(help="Reload stack services")
+    def reload(self):
+        """Reload service"""
+        services = []
+        pargs = self.app.pargs
+        if not (pargs.nginx or pargs.php or
+                pargs.php73 or
+                pargs.mysql or
+                pargs.netdata or
+                pargs.proftpd or
+                pargs.redis or
+                pargs.fail2ban):
+            pargs.nginx = True
+            pargs.php = True
+            pargs.mysql = True
+            pargs.fail2ban = True
+
+        if pargs.nginx:
+            if (WOAptGet.is_installed(self, 'nginx-custom') or
+                    WOAptGet.is_installed(self, 'nginx-mainline')):
+                services = services + ['nginx']
+            else:
+                Log.info(self, "Nginx is not installed")
+
+        if pargs.php:
+            if WOAptGet.is_installed(self, 'php7.2-fpm'):
+                services = services + ['php7.2-fpm']
+            else:
+                Log.info(self, "PHP7.2-FPM is not installed")
+
+            if WOAptGet.is_installed(self, 'php7.3-fpm'):
+                services = services + ['php7.3-fpm']
+            else:
+                Log.info(self, "PHP7.3-FPM is not installed")
+
+        if pargs.php73:
+            if WOAptGet.is_installed(self, 'php7.3-fpm'):
+                services = services + ['php7.3-fpm']
+            else:
+                Log.info(self, "PHP7.3-FPM is not installed")
+
+        if pargs.mysql:
+            if ((WOVar.wo_mysql_host == "localhost") or
+                    (WOVar.wo_mysql_host == "127.0.0.1")):
+                if (WOAptGet.is_installed(self, 'mysql-server') or
+                    WOAptGet.is_installed(self, 'percona-server-server-5.6') or
+                        WOAptGet.is_installed(self, 'mariadb-server')):
+                    services = services + ['mysql']
+                else:
+                    Log.info(self, "MySQL is not installed")
+            else:
+                Log.warn(self, "Remote MySQL found, "
+                         "Unable to check MySQL service status")
+
+        if pargs.redis:
+            if WOAptGet.is_installed(self, 'redis-server'):
+                services = services + ['redis-server']
+            else:
+                Log.info(self, "Redis server is not installed")
+
+        if pargs.fail2ban:
+            if WOAptGet.is_installed(self, 'fail2ban'):
+                services = services + ['fail2ban']
+            else:
+                Log.info(self, "fail2ban is not installed")
+
+        # proftpd
+        if pargs.proftpd:
+            if WOAptGet.is_installed(self, 'proftpd-basic'):
+                services = services + ['proftpd']
+            else:
+                Log.info(self, "ProFTPd is not installed")
+
+        # netdata
+        if pargs.netdata:
+            if (os.path.isdir("/opt/netdata") or
+                    os.path.isdir("/etc/netdata")):
+                services = services + ['netdata']
+            else:
+                Log.info(self, "Netdata is not installed")
+
+        for service in services:
+            Log.debug(self, "Reloading service: {0}".format(service))
+            WOService.reload_service(self, service)
---
+++ packages/wordops/wordops-3.9.9.4-py3-none-any.whl/wo/core/acme.py
@@ -0,0 +1,161 @@
+import csv
+import os
+
+import requests
+
+from wo.core.fileutils import WOFileUtils
+from wo.core.git import WOGit
+from wo.core.logging import Log
+from wo.core.shellexec import WOShellExec
+from wo.core.variables import WOVar
+
+
+class WOAcme:
+    """Acme.sh utilities for WordOps"""
+
+    wo_acme_exec = ("/etc/letsencrypt/acme.sh --config-home "
+                    "'/etc/letsencrypt/config'")
+
+    def export_cert(self):
+        """Export acme.sh csv certificate list"""
+        if not WOShellExec.cmd_exec(
+                self, "{0} ".format(WOAcme.wo_acme_exec) +
+                "--list --listraw > /var/lib/wo/cert.csv"):
+            Log.error(self, "Unable to export certs list")
+
+    def setupletsencrypt(self, acme_domains, acmedata):
+        """Issue SSL certificates with acme.sh"""
+        all_domains = '\' -d \''.join(acme_domains)
+        wo_acme_dns = acmedata['acme_dns']
+        keylenght = acmedata['keylength']
+        if acmedata['dns'] is True:
+            acme_mode = "--dns {0}".format(wo_acme_dns)
+            validation_mode = "DNS mode with {0}".format(wo_acme_dns)
+            if acmedata['dnsalias'] is True:
+                acme_mode = acme_mode + \
+                    " --challenge-alias {0}".format(acmedata['acme_alias'])
+        else:
+            acme_mode = "-w /var/www/html"
+            validation_mode = "Webroot challenge"
+            Log.debug(self, "Validation : Webroot mode")
+
+        Log.info(self, "Validation mode : {0}".format(validation_mode))
+        Log.wait(self, "Issuing SSL cert with acme.sh")
+        if not WOShellExec.cmd_exec(
+                self, "{0} ".format(WOAcme.wo_acme_exec) +
+                "--issue -d '{0}' {1} -k {2} -f"
+                .format(all_domains, acme_mode, keylenght)):
+            Log.failed(self, "Issuing SSL cert with acme.sh")
+            if acmedata['dns'] is True:
+                Log.error(
+                    self, "Please make sure your properly "
+                    "set your DNS API credentials for acme.sh")
+                return False
+            else:
+                Log.error(
+                    self, "Your domain is properly configured "
+                    "but acme.sh was unable to issue certificate.\n"
+                    "You can find more informations in "
+                    "/var/log/wo/wordops.log")
+                return False
+        else:
+            Log.valide(self, "Issuing SSL cert with acme.sh")
+            return True
+
+    def deploycert(self, wo_domain_name):
+        if not os.path.isfile('/etc/letsencrypt/renewal/{0}_ecc/fullchain.cer'
+                              .format(wo_domain_name)):
+            Log.error(self, 'Certificate not found. Deployment canceled')
+
+        Log.debug(self, "Cert deployment for domain: {0}"
+                  .format(wo_domain_name))
+
+        try:
+            Log.wait(self, "Deploying SSL cert")
+            if WOShellExec.cmd_exec(
+                self, "mkdir -p {0}/{1} && {2} --install-cert -d {1} --ecc "
+                "--cert-file {0}/{1}/cert.pem --key-file {0}/{1}/key.pem "
+                "--fullchain-file {0}/{1}/fullchain.pem "
+                "--ca-file {0}/{1}/ca.pem --reloadcmd \"nginx -t && "
+                "service nginx restart\" "
+                .format(WOVar.wo_ssl_live,
+                        wo_domain_name, WOAcme.wo_acme_exec)):
+                Log.valide(self, "Deploying SSL cert")
+            else:
+                Log.failed(self, "Deploying SSL cert")
+                Log.error(self, "Unable to deploy certificate")
+
+            if os.path.isdir('/var/www/{0}/conf/nginx'
+                             .format(wo_domain_name)):
+
+                sslconf = open("/var/www/{0}/conf/nginx/ssl.conf"
+                               .format(wo_domain_name),
+                               encoding='utf-8', mode='w')
+                sslconf.write(
+                    "listen 443 ssl http2;\n"
+                    "listen [::]:443 ssl http2;\n"
+                    "ssl_certificate     {0}/{1}/fullchain.pem;\n"
+                    "ssl_certificate_key     {0}/{1}/key.pem;\n"
+                    "ssl_trusted_certificate {0}/{1}/ca.pem;\n"
+                    "ssl_stapling_verify on;\n"
+                    .format(WOVar.wo_ssl_live, wo_domain_name))
+                sslconf.close()
+
+            if not WOFileUtils.grep(self, '/var/www/22222/conf/nginx/ssl.conf',
+                                    '/etc/letsencrypt'):
+                Log.info(self, "Securing WordOps backend with current cert")
+                sslconf = open("/var/www/22222/conf/nginx/ssl.conf",
+                               encoding='utf-8', mode='w')
+                sslconf.write("ssl_certificate     {0}/{1}/fullchain.pem;\n"
+                              "ssl_certificate_key     {0}/{1}/key.pem;\n"
+                              "ssl_trusted_certificate {0}/{1}/ca.pem;\n"
+                              "ssl_stapling_verify on;\n"
+                              .format(WOVar.wo_ssl_live, wo_domain_name))
+                sslconf.close()
+
+            WOGit.add(self, ["/etc/letsencrypt"],
+                      msg="Adding letsencrypt folder")
+
+        except IOError as e:
+            Log.debug(self, str(e))
+            Log.debug(self, "Error occured while generating "
+                      "ssl.conf")
+        return 0
+
+    def check_dns(self, acme_domains):
+        """Check if a list of domains point to the server IP"""
+        server_ip = requests.get('http://v4.wordops.eu/').text
+        for domain in acme_domains:
+            domain_ip = requests.get('http://v4.wordops.eu/dns/{0}/'
+                                     .format(domain)).text
+            if(not domain_ip == server_ip):
+                Log.warn(
+                    self, "{0} is not pointing to your server IP"
+                    .format(domain))
+                Log.error(
+                    self, "You have to add the "
+                    "proper DNS record", False)
+                return False
+        else:
+            Log.debug(self, "DNS record are properly set")
+            return True
+
+    def cert_check(self, wo_domain_name):
+        """Check certificate existance with acme.sh and return Boolean"""
+        WOAcme.export_cert(self)
+        # define new csv dialect
+        csv.register_dialect('acmeconf', delimiter='|')
+        # open file
+        certfile = open('/var/lib/wo/cert.csv', mode='r', encoding='utf-8')
+        reader = csv.reader(certfile, 'acmeconf')
+        for row in reader:
+            # check if domain exist
+            if wo_domain_name in row[0]:
+                # check if cert expiration exist
+                if not row[3] == '':
+                    cert_exist = True
+                    break
+        else:
+            cert_exist = False
+        certfile.close()
+        return cert_exist
