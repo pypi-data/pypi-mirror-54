42273701e371025798417072dc82762ba08f8c0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:45 +0000

    Add package retaggr/2.3.1/retaggr-2.3.1-py3-none-any.whl

---
+++ packages/retaggr/retaggr-2.3.1-py3-none-any.whl/retaggr/engines/__init__.py
@@ -0,0 +1,8 @@
+from .base import Engine
+from .base import ImageResult
+
+from .danbooru import Danbooru
+from .e621 import E621
+from .iqdb import Iqdb
+from .paheal import Paheal
+from .saucenao import SauceNao
