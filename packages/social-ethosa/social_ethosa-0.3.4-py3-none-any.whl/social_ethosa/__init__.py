bbd352637cbba34bba74ed20049fc8ad1aac8be2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:38 +0000

    Add package social-ethosa/0.3.4/social_ethosa-0.3.4-py3-none-any.whl

---
+++ packages/social-ethosa/social_ethosa-0.3.4-py3-none-any.whl/social_ethosa/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# author: ethosa
+
+__version__ = "0.2.61"
+
+from .net import *
+from .botwrapper import *
+from .otherUtils import *
---
+++ packages/social-ethosa/social_ethosa-0.3.4-py3-none-any.whl/social_ethosa/net/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# author: ethosa
+
+from .vkcom import *
+from .jdoodlecom import *
+from .tracemoe import *
+from .tpdne import *
+from .yandex import *
+from .yummyanimeclub import *
+from .bloggercom import *
+from .pastebincom import *
