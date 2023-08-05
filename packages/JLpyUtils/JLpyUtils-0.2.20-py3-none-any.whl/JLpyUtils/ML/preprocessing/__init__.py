a8b14135719b7caa3acfcb53c8aab31ada320b5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:28 +0000

    Add package JLpyUtils/0.2.20/JLpyUtils-0.2.20-py3-none-any.whl

---
+++ packages/JLpyUtils/JLpyUtils-0.2.20-py3-none-any.whl/JLpyUtils/ML/preprocessing/__init__.py
@@ -0,0 +1,11 @@
+"""
+Functions related to preprocessing/feature engineering for machine learning.
+"""
+from . import LabelEncode
+from . import Impute
+from . import Scale
+from . import OneHotEncode
+from ._feat_eng_pipe import feat_eng_pipe
+from ._CorrCoeff import CorrCoeffThreshold
+from . import log
+        
