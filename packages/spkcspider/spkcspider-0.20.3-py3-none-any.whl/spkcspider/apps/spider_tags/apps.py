56a5e7906276aec5477472dea3727f758be6275e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:52 +0000

    Add package spkcspider/0.20.3/spkcspider-0.20.3-py3-none-any.whl

---
+++ packages/spkcspider/spkcspider-0.20.3-py3-none-any.whl/spkcspider/apps/spider_tags/apps.py
@@ -0,0 +1,25 @@
+__all__ = ["SpiderTagsConfig"]
+
+from django.apps import AppConfig, apps
+from .signals import UpdateDefaultLayouts
+from spkcspider.utils.settings import extract_app_dicts
+from spkcspider.apps.spider.signals import update_dynamic
+
+
+class SpiderTagsConfig(AppConfig):
+    name = 'spkcspider.apps.spider_tags'
+    label = 'spider_tags'
+    spider_url_path = 'spidertags/'
+    verbose_name = 'spkcspider tags'
+
+    def ready(self):
+        from .fields import installed_fields
+        for app in apps.get_app_configs():
+            installed_fields.update(
+                extract_app_dicts(app, "spider_tag_fields")
+            )
+
+        update_dynamic.connect(
+            UpdateDefaultLayouts,
+            dispatch_uid="update_default_layouts"
+        )
