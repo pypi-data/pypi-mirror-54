7bff657b09b87486ae00e516319a90395d202cb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:23 +0000

    Add package bybit-ws/0.1.8/bybit-ws-0.1.8.tar.gz

---
+++ packages/bybit-ws/bybit-ws-0.1.8.tar.gz/bybit-ws-0.1.8/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bybit-ws",
+    version="0.1.8",
+    author="Simon",
+    author_email="simon.zhang@bybit.com",
+    description="bybit websocket client",
+    url="https://github.com/bybit-exchange/api-connectors/tree/master/official-ws/python",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'websocket-client==0.53.0',
+    ]
+)
