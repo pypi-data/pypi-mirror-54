b309574c491071cf05a4a2950b1af4f1d4808bc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:33 +0000

    Add package pydov/0.3.0/pydov-0.3.0-py2.py3-none-any.whl

---
+++ packages/pydov/pydov-0.3.0-py2.py3-none-any.whl/pydov/util/dovutil.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+"""Module grouping utility functions for DOV XML services."""
+
+from owslib.etree import etree
+from pydov.util.errors import XmlParseError
+import pydov
+
+
+def get_remote_url(url):
+    """Request the URL from the remote service and return its contents.
+
+    Parameters
+    ----------
+    url : str
+        URL to download.
+
+    Returns
+    -------
+    xml : bytes
+        The raw XML data as bytes.
+
+    """
+
+    request = pydov.session.get(url, timeout=pydov.request_timeout)
+    request.encoding = 'utf-8'
+    return request.text.encode('utf8')
+
+
+def get_xsd_schema(url):
+    """Request the XSD schema from DOV webservices and return it.
+
+    Parameters
+    ----------
+    url : str
+        URL of the XSD schema to download.
+
+    Returns
+    -------
+    xml : bytes
+        The raw XML data of this XSD schema as bytes.
+
+    """
+    return get_remote_url(url)
+
+
+def get_dov_xml(url):
+    """Request the XML from the remote DOV webservices and return it.
+
+    Parameters
+    ----------
+    url : str
+        URL of the DOV object to download.
+
+    Returns
+    -------
+    xml : bytes
+        The raw XML data of this DOV object as bytes.
+
+    """
+    return get_remote_url(url)
+
+
+def parse_dov_xml(xml_data):
+    """Parse the given XML data into an ElementTree.
+
+    Parameters
+    ----------
+    xml_data : bytes
+        The raw XML data of a DOV object as bytes.
+
+    Returns
+    -------
+    tree : etree.ElementTree
+        Parsed XML tree of the DOV object.
+
+    """
+    try:
+        parser = etree.XMLParser(ns_clean=True, recover=True)
+    except TypeError:
+        parser = etree.XMLParser()
+
+    try:
+        tree = etree.fromstring(xml_data, parser=parser)
+        return tree
+    except Exception:
+        raise XmlParseError("Failed to parse XML record.")
