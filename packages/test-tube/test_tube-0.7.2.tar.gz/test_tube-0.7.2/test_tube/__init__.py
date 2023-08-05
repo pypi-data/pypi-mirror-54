7b477e900260b05a846bfecb317b33819be4069f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:48 +0000

    Add package test-tube/0.7.2/test_tube-0.7.2.tar.gz

---
+++ packages/test-tube/test_tube-0.7.2.tar.gz/test_tube-0.7.2/test_tube/__init__.py
@@ -0,0 +1,8 @@
+"""
+Experiment logger module
+"""
+
+from .argparse_hopt import HyperOptArgumentParser
+from .log import Experiment
+from .hyperopt import HyperParamOptimizer
+from .hpc import SlurmCluster
