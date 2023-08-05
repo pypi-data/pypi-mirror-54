0848d3621432170a325f3101bc5543d34b613fe3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:00 +0000

    Add package pystickynote/1.0.1/pystickynote-1.0.1-py3-none-any.whl

---
+++ packages/pystickynote/pystickynote-1.0.1-py3-none-any.whl/pystickynote/paths.py
@@ -0,0 +1,4 @@
+import os
+
+CONFIG_PATH = os.path.realpath(os.path.expanduser('~') + '/.config/pystickynote/pysn.conf')
+NOTES_PATH = os.path.realpath(os.path.expanduser('~') + '/.config/pystickynote/notes.json')
