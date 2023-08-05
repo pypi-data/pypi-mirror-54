950680405c56ba42deda025275df1331b439e766
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:56 +0000

    Add package wyff/0.0.2/wyff-0.0.2-py3-none-any.whl

---
+++ packages/wyff/wyff-0.0.2-py3-none-any.whl/wyff/wyff.py
@@ -0,0 +1,15 @@
+def me():
+    print("you kown my big name?")
+
+def zhuangbi():
+    print("If you don't feel the white privileges,congratulations,you are enjoying the benifits of it.")
+
+def say(p):
+    print(p)
+
+def al():
+    while(1):
+        s = input()
+        print(s)
+
+
