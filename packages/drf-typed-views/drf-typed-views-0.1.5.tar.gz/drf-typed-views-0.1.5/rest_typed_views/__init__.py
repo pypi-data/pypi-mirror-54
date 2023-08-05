3d78428cccc3eb42c0ff24996ddbbc8d9924fe61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:04 +0000

    Add package drf-typed-views/0.1.5/drf-typed-views-0.1.5.tar.gz

---
+++ packages/drf-typed-views/drf-typed-views-0.1.5.tar.gz/drf-typed-views-0.1.5/rest_typed_views/__init__.py
@@ -0,0 +1,22 @@
+from .decorators import typed_action, typed_api_view
+from .param_settings import ParamSettings
+
+
+def Query(*args, **kwargs):
+    return ParamSettings("query_param", *args, **kwargs)
+
+
+def Path(*args, **kwargs):
+    return ParamSettings("path", *args, **kwargs)
+
+
+def CurrentUser(*args, **kwargs):
+    return ParamSettings("current_user", *args, **kwargs)
+
+
+def Body(*args, **kwargs):
+    return ParamSettings("body", *args, **kwargs)
+
+
+def Param(*args, **kwargs):
+    return ParamSettings(*args, **kwargs)
