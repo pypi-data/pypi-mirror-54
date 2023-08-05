0f341f4c6c84fb5a5099fc1d698243d8f516a201
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:15 +0000

    Add package cotohappy/0.0.1.2/cotohappy-0.0.1.2-py3-none-any.whl

---
+++ packages/cotohappy/cotohappy-0.0.1.2-py3-none-any.whl/cotohappy/__init__.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3.8
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 20 12:00:00 2019
+        
+COTOHA API for Python3.8
+@author: Nicolas Toba Nozomi
+@id: 278mt
+"""
+
+"""
+CotohapPy
+=====
+
+Provides
+  1. 
+  2.
+
+How to use the documentation
+----------------------------
+
+"""
+
+__version__ = '0.0.1.2'
+__author__  = 'Nicolas Toba Nozomi'
+__licence__ = 'MIT'
+
+from cotohappy.api import API
+from cotohappy.error import CotohapPyError
