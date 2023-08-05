7822a78ff9874b2243030736bf11c85e0d065f65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:27 +0000

    Add package matplotlib2tikz/0.7.6/matplotlib2tikz-0.7.6.tar.gz

---
+++ packages/matplotlib2tikz/matplotlib2tikz-0.7.6.tar.gz/matplotlib2tikz-0.7.6/setup.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+#
+import codecs
+import os
+
+from setuptools import setup, find_packages
+
+# https://packaging.python.org/single_source_version/
+base_dir = os.path.abspath(os.path.dirname(__file__))
+about = {}
+with open(os.path.join(base_dir, "matplotlib2tikz", "__about__.py"), "rb") as f:
+    exec(f.read(), about)
+
+
+def read(fname):
+    return codecs.open(os.path.join(base_dir, fname), encoding="utf-8").read()
+
+
+setup(
+    name="matplotlib2tikz",
+    version=about["__version__"],
+    packages=find_packages(),
+    url="https://github.com/nschloe/matplotlib2tikz",
+    author=about["__author__"],
+    author_email=about["__email__"],
+    install_requires=["matplotlib >= 1.4.0", "numpy", "Pillow", "six"],
+    extras_require={
+        "all": ["pipdate >= 0.3.0, <0.4.0"],
+        "update": ["pipdate >= 0.3.0, <0.4.0"],
+    },
+    description="Convert matplotlib figures into TikZ/PGFPlots",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    license=about["__license__"],
+    classifiers=[
+        about["__status__"],
+        about["__license__"],
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Multimedia :: Graphics :: Graphics Conversion",
+        "Topic :: Scientific/Engineering :: Visualization",
+    ],
+)
---
+++ packages/matplotlib2tikz/matplotlib2tikz-0.7.6.tar.gz/matplotlib2tikz-0.7.6/test/test_marker.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+#
+from helpers import assert_equality
+
+
+def plot():
+    from matplotlib import pyplot as plt
+    import numpy as np
+
+    fig, ax = plt.subplots()
+    with plt.style.context(("ggplot")):
+        t = np.linspace(0, 2 * np.pi, 11)
+        s = np.sin(t)
+        c = np.cos(t)
+        ax.plot(t, s, "ko-", mec="r", markevery=20)
+        ax.plot(t, c, "ks--", mec="r", markevery=20)
+        ax.set_xlim(t[0], t[-1])
+        ax.set_xlabel("t")
+        ax.set_ylabel("y")
+        ax.set_title("Simple plot")
+        ax.grid(True)
+    return fig
+
+
+def test():
+    assert_equality(plot, __file__[:-3] + "_reference.tex")
+    return
