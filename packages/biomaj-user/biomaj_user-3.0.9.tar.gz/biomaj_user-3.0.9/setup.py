d730e530d45a663c3420005f675fdea2afc45268
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:12 +0000

    Add package biomaj-user/3.0.9/biomaj_user-3.0.9.tar.gz

---
+++ packages/biomaj-user/biomaj_user-3.0.9.tar.gz/biomaj_user-3.0.9/setup.py
@@ -0,0 +1,64 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup
+
+from distutils.command.install import install
+import os
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.md')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.txt')) as f:
+    CHANGES = f.read()
+
+
+config = {
+    'description': 'BioMAJ user management library',
+    'long_description': README + '\n\n' + CHANGES,
+    'author': 'Olivier Sallou',
+    'url': 'http://biomaj.genouest.org',
+    'download_url': 'http://biomaj.genouest.org',
+    'author_email': 'olivier.sallou@irisa.fr',
+    'version': '3.0.9',
+     'classifiers': [
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        # Indicate who your project is intended for
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4'
+    ],
+    'install_requires': [
+                         'ldap3>=2.2.4',
+                         'pymongo',
+                         'py-bcrypt',
+                         'flask',
+                         'PyYAML',
+                         'tabulate',
+                         'python-consul',
+                         'biomaj_core'
+                        ],
+    'tests_require': ['nose', 'mock'],
+    'test_suite': 'nose.collector',
+    'packages': find_packages(),
+    'include_package_data': True,
+    'scripts': ['bin/biomaj-users.py'],
+    'name': 'biomaj_user'
+}
+
+setup(**config)
