d9380ce8bdec2431251d1e0311c4ef810a69b0e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:09 +0000

    Add package data-extractor/0.5.0.dev1/data_extractor-0.5.0.dev1-py3-none-any.whl

---
+++ packages/data-extractor/data_extractor-0.5.0.dev1-py3-none-any.whl/data_extractor/abc.py
@@ -0,0 +1,183 @@
+"""
+:mod:`abc` -- Abstract Base Classes.
+====================================
+"""
+# Standard Library
+import inspect
+import warnings
+
+from abc import abstractmethod
+from typing import Any, Dict, List, Tuple, Union
+
+# Local Folder
+from .utils import sentinel
+
+
+class ComplexExtractorMeta(type):
+    """
+    Complex Extractor Meta Class.
+    """
+
+    def __init__(cls, name: str, bases: Tuple[type], attr_dict: Dict[str, Any]):
+        super().__init__(name, bases, attr_dict)
+        field_names: List[str] = []
+
+        __init_args = inspect.getfullargspec(getattr(cls, "__init__")).args
+
+        for key, attr in attr_dict.items():
+            if isinstance(type(attr), ComplexExtractorMeta):
+                # can't using data_extractor.utils.is_complex_extractor here,
+                # because AbstractComplexExtractor which being used in it
+                # bases on ComplexExtractorMeta.
+                if key in __init_args[1:]:
+                    # Item's attribute overwrites
+                    # the 'Item.__init__' parameters except first parameter.
+                    exc_args = None
+                    frame = inspect.currentframe()
+                    assert frame is not None, (
+                        "If running in an implementation "
+                        "without Python stack frame support, "
+                        "this function returns None."
+                    )
+                    try:
+                        outer_frame = frame.f_back
+
+                        filename = outer_frame.f_code.co_filename
+                        firstlineno = outer_frame.f_lineno
+                        firstline_idx = firstlineno - 1
+                        lines = None
+                        try:
+                            lines, _ = inspect.findsource(outer_frame)
+                        except OSError:
+                            # can't get the source code from python repl
+                            pass
+
+                        if lines is not None:
+                            start_index = inspect.indentsize(lines[firstline_idx])
+                            for lineno, line in enumerate(
+                                lines[firstline_idx + 1 :], start=firstlineno + 1
+                            ):
+                                # iterate line in the code block body
+                                cur_index = inspect.indentsize(line)
+                                if cur_index <= start_index:
+                                    # reach end of the code block,
+                                    # use code block firstlineno as SyntaxError.lineno
+                                    line = lines[firstline_idx]
+                                    lineno = firstlineno
+                                    break
+
+                                if line.lstrip().startswith(key):
+                                    # find the line as SyntaxError.text
+                                    break
+
+                            else:
+                                # reach EOF,
+                                # use code block firstlineno as SyntaxError.lineno
+                                line = lines[firstline_idx]
+                                lineno = firstlineno
+
+                            offset = inspect.indentsize(line)
+                            line = line.strip()
+                            exc_args = (filename, lineno, offset, line)
+                        else:
+                            line = f"{key}={attr!r}"
+
+                    finally:
+                        del outer_frame
+                        del frame
+
+                    err_msg = (
+                        f"{line!r} overwriten "
+                        f"the parameter {key!r} of '{name}.__init__' method. "
+                        f"Please using the optional parameter name={key!r} "
+                        "in {attr!r} to avoid overwriting parameter name."
+                    )
+                    if exc_args is not None:
+                        raise SyntaxError(err_msg, exc_args)
+                    else:
+                        raise SyntaxError(err_msg)
+
+                field_names.append(key)
+
+        cls._field_names = field_names
+
+
+class AbstractSimpleExtractor:
+    """
+    Abstract Simple Extractor Clase.
+
+    :param expr: Extractor selector expression.
+    :type expr: str
+    """
+
+    def __init__(self, expr: str):
+        self.expr = expr
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({self.expr!r})"
+
+    @abstractmethod
+    def extract(self, element: Any) -> Any:
+        """
+        Extract data or subelement from element.
+
+        :param element: The target data node element.
+        :type element: Any
+
+        :returns: Data or subelement.
+        :rtype: Any
+
+        :raises ~data_extractor.exceptions.ExprError: Extractor Expression Error.
+        """
+        raise NotImplementedError
+
+    def extract_first(self, element: Any, default: Any = sentinel) -> Any:
+        """
+        Extract the first data or subelement from `extract` method call result.
+
+        :param element: The target data node element.
+        :type element: Any
+        :param default: Default value when not found. \
+            Default: :data:`data_extractor.utils.sentinel`.
+        :type default: Any, optional
+
+        :returns: Data or subelement.
+        :rtype: Any
+
+        :raises ~data_extractor.exceptions.ExtractError: \
+            Thrown by extractor extracting wrong data.
+        """
+        rv = self.extract(element)
+        if not isinstance(rv, list):
+            warnings.warn(
+                f"{self!r} can't extract first item from result {rv!r}", UserWarning
+            )
+            return rv
+
+        if not rv:
+            if default is sentinel:
+                from .exceptions import ExtractError
+
+                raise ExtractError(self, element)
+
+            return default
+
+        return rv[0]
+
+
+class AbstractComplexExtractor(metaclass=ComplexExtractorMeta):
+    """
+    Abstract Complex Extractor Clase.
+
+    Its metaclass is :class:`data_extractor.abc.ComplexExtractorMeta`
+    """
+
+
+AbstractExtractors = Union[AbstractSimpleExtractor, AbstractComplexExtractor]
+
+__all__ = (
+    "AbstractComplexExtractor",
+    "AbstractExtractors",
+    "AbstractSimpleExtractor",
+    "ComplexExtractorMeta",
+)
