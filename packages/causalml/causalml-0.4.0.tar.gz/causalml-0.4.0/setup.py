a85af75d8b40981ffab3833d99477a046bd28828
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:56 +0000

    Add package causalml/0.4.0/causalml-0.4.0.tar.gz

---
+++ packages/causalml/causalml-0.4.0.tar.gz/causalml-0.4.0/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+from setuptools.extension import Extension
+from Cython.Build import cythonize
+import numpy as np
+import causalml
+
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+
+with open("requirements.txt") as f:
+    requirements = f.readlines()
+
+extensions = [
+    Extension("causalml.inference.tree.causaltree",
+              ["causalml/inference/tree/causaltree.pyx"],
+              libraries = [],
+              include_dirs = [np.get_include()],
+              extra_compile_args=["-O3"]
+    )
+]
+
+packages = find_packages()
+
+setup(
+    name="causalml",
+    version=causalml.__version__,
+    author="Huigang Chen, Totte Harinen, Jeong-Yoon Lee, Mike Yung, Zhenyu Zhao",
+    author_email="",
+    description="Python Package for Uplift Modeling and Causal Inference with Machine Learning Algorithms",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/uber-common/causalml",
+    packages=packages,
+    python_requires=">=2.7",
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=requirements,
+    ext_modules=cythonize(extensions),
+    include_dirs=[np.get_include()]
+)
