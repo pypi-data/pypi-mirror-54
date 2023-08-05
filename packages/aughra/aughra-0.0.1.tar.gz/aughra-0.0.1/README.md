fe284ed4d6019b435471dec1f1041dff648485f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:01 +0000

    Add package aughra/0.0.1/aughra-0.0.1.tar.gz

---
+++ packages/aughra/aughra-0.0.1.tar.gz/aughra-0.0.1/README.md
@@ -0,0 +1,63 @@
+<div align="center">
+  <img src="https://www.frootlab.org/images/fig/aughra.svg" width=350px>
+</div>
+
+Aughra
+======
+
+[![Building Status](https://travis-ci.org/frootlab/aughra.svg?branch=master)](https://travis-ci.org/frootlab/aughra)
+[![Documentation Status](https://readthedocs.org/projects/aughra/badge/?version=latest)](https://aughra.readthedocs.io/en/latest/?badge=latest)
+[![PIP Version](https://badge.fury.io/py/aughra.svg)](https://badge.fury.io/py/aughra)
+
+*Aughra* is a planned root directory for the [Vivid Code](https://www.frootlab.org/projects/vivid.html) framework.
+Aughra is intended to deploy information about public [Vivid Stores](https://www.frootlab.org/projects/vivid/store.html)
+to allow an automatic configuration of [Vivid Nodes](https://www.frootlab.org/projects/vivid/node.html).
+
+Aughra is open source, based on the [Python](https://www.python.org/)
+programming language and actively developed as part of the
+[Vivid Code](https://www.frootlab.org/vivid) framework at
+[Frootlab](https://www.frootlab.org).
+
+Current Development Status
+--------------------------
+
+Aughra currently is in early *Planning* stage. This means, that the projects
+directives, requirements and goals are mostly not yet settled or implemented.
+
+Installation
+------------
+
+Comprehensive information and installation support is provided within the
+[online manual](https://aughra.readthedocs.io/en/latest/). If you already have a
+Python environment configured on your computer, you can install the latest
+distributed version by using pip:
+
+    $ pip install aughra
+
+Documentation
+-------------
+
+The latest Aughra documentation is available as an [online
+manual](https://aughra.readthedocs.io/en/latest/) and for download in the
+formats [PDF](https://readthedocs.org/projects/aughra/downloads/pdf/latest/),
+[EPUB](https://readthedocs.org/projects/aughra/downloads/epub/latest/) and
+[HTML](https://readthedocs.org/projects/aughra/downloads/htmlzip/latest/).
+
+Contribute
+----------
+
+Contributors are very welcome! Feel free to report bugs and feature requests to
+the [issue tracker](https://github.com/frootlab/aughra/issues) provided by
+GitHub. Currently, as the Frootlab Developers team still is growing, we do not
+provide any Contribution Guide Lines to collaboration partners. However, if you
+are interested to join the team, we would be glad, to receive an informal
+[application](mailto:application@frootlab.org).
+
+License
+-------
+
+Aughra is open source and available free for any use under the
+[GPLv3](https://www.gnu.org/licenses/gpl.html) license:
+
+    © 2019 Frootlab Developers:
+      Patrick Michl <patrick.michl@frootlab.org>
---
+++ packages/aughra/aughra-0.0.1.tar.gz/aughra-0.0.1/setup.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2019 Frootlab
+#
+# This file is part of Frootlab Aughra, https://www.frootlab.org/aughra
+#
+#  Aughra is free software: you can redistribute it and/or modify it under the
+#  terms of the GNU General Public License as published by the Free Software
+#  Foundation, either version 3 of the License, or (at your option) any later
+#  version.
+#
+#  Aughra is distributed in the hope that it will be useful, but WITHOUT ANY
+#  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+#  A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#  You should have received a copy of the GNU General Public License along with
+#  Aughra. If not, see <http://www.gnu.org/licenses/>.
+#
+"""Setuptools based installation."""
+
+__copyright__ = '2019 Frootlab'
+__license__ = 'GPLv3'
+__docformat__ = 'google'
+__author__ = 'Frootlab Developers'
+__email__ = 'contact@frootlab.org'
+__authors__ = ['Patrick Michl <patrick.michl@frootlab.org>']
+
+import pathlib
+import re
+import setuptools
+
+def install() -> None:
+    """Setuptools based installation script."""
+
+    # Parse top level module for attributes
+    text = pathlib.Path('./aughra/__init__.py').read_text()
+    pattern = r"^[ ]*__([^\d\W]\w*)__[ ]*=[ ]*['\"]([^'\"]*)['\"]"
+    matches = re.finditer(pattern, text, re.M)
+    pkg = {str(m.group(1)): str(m.group(2)) for m in matches}
+
+    # Install package
+    setuptools.setup(
+        name='aughra',
+        version=pkg['version'],
+        description=pkg['description'],
+        long_description=pathlib.Path('.', 'README.md').read_text(),
+        long_description_content_type='text/markdown',
+        classifiers=[
+            'Development Status :: 1 - Planning',
+            'Intended Audience :: Developers',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+            'Programming Language :: Python :: 3',
+    		'Programming Language :: Python :: 3.7',
+            'Operating System :: OS Independent',
+            'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+            'Topic :: Software Development :: Libraries :: Python Modules'],
+        keywords=(
+            'catalog '
+            'catalog-software '),
+        url=pkg['url'],
+        author=pkg['author'],
+        author_email=pkg['email'],
+        license=pkg['license'],
+        packages=setuptools.find_packages(exclude=['docs', 'tests']),
+        package_dir={
+            'aughra': 'aughra'},
+        python_requires='>=3.7',
+        install_requires=[
+            'numpy>=1.15',
+            'hup>=0.9.2']
+    )
+
+if __name__ == '__main__':
+    install()
