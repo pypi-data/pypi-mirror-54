b175864f6548faac8f6d266952b4e9ed5ce168be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:17 +0000

    Add package khnum/0.0.3/khnum-0.0.3-py3-none-any.whl

---
+++ packages/khnum/khnum-0.0.3-py3-none-any.whl/khnum/__init__.py
@@ -0,0 +1,10 @@
+'''
+human readable numbers
+'''
+
+__author__ = 'steve@siznax.net'
+__version__ = '0.0.3'
+
+
+from .datapoint import Datapoint as num
+from .khnum import cnum, hnum, pretty
---
+++ packages/khnum/khnum-0.0.3-py3-none-any.whl/khnum/datapoint.py
@@ -0,0 +1,33 @@
+'''
+Datapoint class
+~~~~~~~~~~~~~~~
+'''
+
+from . import khnum
+
+
+class Datapoint():
+
+    '''
+    represents a human readable number
+    '''
+
+    def __init__(self, num, units='d'):
+        '''
+        returns Datapoint instance
+        '''
+        self.update(num, units)
+
+    def __str__(self):
+        return khnum.pretty(dict(self.__dict__))
+
+    def update(self, num, units='d'):
+        '''
+        update datapoint value with number and (optional) khnum units
+        '''
+        self.type = str(type(num))
+        self._num = num
+        self.cnum = khnum.cnum(num)
+        self.hnum = khnum.hnum(num, units)
+        self.line = '{} ({})'.format(self.hnum, self.cnum)
+        self.units = units
