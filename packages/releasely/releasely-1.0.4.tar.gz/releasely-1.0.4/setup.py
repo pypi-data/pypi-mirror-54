771b5630aa8d0c72c88a6727ce03b24093d5a0b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:52 +0000

    Add package releasely/1.0.4/releasely-1.0.4.tar.gz

---
+++ packages/releasely/releasely-1.0.4.tar.gz/releasely-1.0.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="releasely",
+    version="1.0.4",
+    package_dir={"": "src"},
+    packages=find_packages("src"),
+    install_requires=[
+        "bumpversion",
+    ],
+    extras_require={
+        'testing': [
+            'pytest',
+            'pytest-cov',
+        ],
+        'linting': [
+            'isort',
+            'black',
+        ]
+    },
+    entry_points={
+        "console_scripts": [
+            "releasely = releasely.__main__:main"
+        ]
+    },
+)
