d5ab6769aec9e63a3d222438ef05ead1c519386d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:44 +0000

    Add package BDPapi/0.0.2/BDPapi-0.0.2.tar.gz

---
+++ packages/BDPapi/BDPapi-0.0.2.tar.gz/BDPapi-0.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages      
+
+setup(
+    name = "BDPapi",   
+    version = "0.0.2",  
+    keywords = ("pip", "BDPapi"),
+    description = "BDP api",
+    long_description = "BDP api",
+    license = "MIT Licence",
+
+    url = "https://github.com/haizhiml/BDP.git",     
+    author = "blue",
+    author_email = "xuguozheng@haizhi.com",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = ["numpy", "requests"]
+)
