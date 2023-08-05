632ee64c57262c5218b3398b3f1ada680e65e5f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:05 +0000

    Add package async-notifications/0.1.11/async_notifications-0.1.11.tar.gz

---
+++ packages/async-notifications/async_notifications-0.1.11.tar.gz/async_notifications-0.1.11/async_notifications/models.py
@@ -0,0 +1,59 @@
+from django.db import models
+
+from django.conf import settings
+# Create your models here.
+from django.utils.translation import ugettext_lazy as _
+from django.utils.encoding import python_2_unicode_compatible
+
+
+@python_2_unicode_compatible
+class EmailNotification(models.Model):
+    subject = models.CharField(max_length=500, verbose_name=_("Subject"))
+    message = models.TextField(verbose_name=_("Message"))
+    # coma separated
+    recipient = models.TextField(verbose_name=_("Recipient list"))
+    enqueued = models.BooleanField(default=True,
+                                   verbose_name=_("Enqueued"))
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True,
+                             blank=True, verbose_name=_("User"), on_delete=models.CASCADE)
+    sended = models.BooleanField(default=False, verbose_name=_("Sended"))
+    file = models.FileField(upload_to='email/%Y/%M', null=True,
+                            blank=True, verbose_name=_("File"))
+    problems = models.BooleanField(default=False, verbose_name=_("Problems"))
+    create_datetime = models.DateTimeField(auto_now_add=True,
+                                           verbose_name=_("Create datetime"))
+
+    def __str__(self):
+        return "(%s) %s" % (_("sended") if self.sended else _("Not sended"),
+                            self.subject)
+
+    class Meta:
+        verbose_name = _("Email notification")
+        verbose_name_plural = _("Email notifications")
+
+
+@python_2_unicode_compatible
+class EmailTemplate(models.Model):
+    code = models.CharField(max_length=50, verbose_name=_("Code"))
+    subject = models.CharField(max_length=500, verbose_name=_("Subject"))
+    message = models.TextField(verbose_name=_("Message"))
+
+    def __str__(self):
+        return "(%s) %s" % (self.code, self.subject)
+
+    class Meta:
+        verbose_name = _("Email template")
+        verbose_name_plural = _("Email templates")
+
+
+@python_2_unicode_compatible
+class TemplateContext(models.Model):
+    code = models.CharField(max_length=50, verbose_name=_("Code"))
+    context_dic = models.TextField(verbose_name=_("Context dictionary"))
+
+    def __str__(self):
+        return self.code
+
+    class Meta:
+        verbose_name = _("Context of template")
+        verbose_name_plural = _("Context of template")
---
+++ packages/async-notifications/async_notifications-0.1.11.tar.gz/async_notifications-0.1.11/async_notifications/utils.py
@@ -0,0 +1,83 @@
+# encoding: utf-8
+from __future__ import unicode_literals
+
+'''
+Created on 20/12/2015
+
+@author: luisza
+'''
+
+from django.template import Context, Template
+from .models import EmailTemplate, EmailNotification
+from django.apps import apps
+from django.template.exceptions import TemplateDoesNotExist
+import six
+
+
+def hexify(text):
+    return "".join([str(hex(ord(x))).replace("0x", "") for x in text])
+
+
+def unhexify(text):
+    if "@" in text:
+        return text
+    return "".join([chr(int(text[x] + text[x + 1], base=16))
+                    for x in range(0, len(text), 2)])
+
+def _get_template(code):
+    template = None
+    if type(code) == six.text_type or type(code) == six.binary_type:
+        code = [code]
+    for _code in code:
+        template = EmailTemplate.objects.filter(code=_code).first()
+        if template:
+            break
+    if template is None:
+        raise TemplateDoesNotExist()
+    return template
+
+
+def send_email_from_template(code, recipient,
+                             context={},
+                             enqueued=True,
+                             user=None,
+                             upfile=None):
+
+
+    template = _get_template(code)
+    if user is not None:
+        context['user'] = user
+
+    if type(recipient) is list or type(recipient) is tuple:
+        recipient = ",".join(recipient)
+
+    subject = Template(template.subject)
+    message = Template(template.message)
+    c = Context(context)
+    email = EmailNotification(subject=subject.render(c),
+                              message=message.render(c),
+                              recipient=recipient,
+                              enqueued=enqueued
+                              )
+    if user is not None:
+        email.user = user
+    if upfile is not None:
+        email.file = upfile
+
+    email.save()
+
+
+def extract_emails(text):
+    if type(text) == str:
+        mail_list = text.replace(
+            "[", "").replace("]", "").replace("'", "").split(",")
+    else:
+        mail_list = text
+
+    emails = [unhexify(x.strip()) for x in mail_list]
+    return emails
+
+
+def get_model(model_name):
+    app_name, model = model_name.split(".")
+    return apps.get_model(app_name, model)
