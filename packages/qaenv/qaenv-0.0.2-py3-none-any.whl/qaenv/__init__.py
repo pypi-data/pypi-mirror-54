3fb093547c9ce9988bbad5c41bd04f39c219883f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:21 +0000

    Add package qaenv/0.0.2/qaenv-0.0.2-py3-none-any.whl

---
+++ packages/qaenv/qaenv-0.0.2-py3-none-any.whl/qaenv/__init__.py
@@ -0,0 +1,24 @@
+import os
+
+
+__author__ = 'yutiansut'
+__version__ = '0.0.2'
+
+
+mongo_ip = os.getenv('MONGODB', '127.0.0.1')
+mongo_port = 27017
+mongo_uri = 'mongodb://{}:{}'.format(mongo_ip, mongo_port)
+
+
+eventmq_ip = os.getenv('QAPUBSUB_IP', '127.0.0.1')
+eventmq_port = os.getenv('QAPUBSUB_PORT', 5672)
+eventmq_username = os.getenv('QAPUBSUB_USER', 'admin')
+eventmq_password = os.getenv('QAPUBSUB_PWD', 'admin')
+eventmq_amqp = 'pyamqp://{}:{}@{}:{}//'.format(
+    eventmq_username, eventmq_password, eventmq_ip, eventmq_port)
+
+
+
+clickhouse_ip =  os.getenv('CLICKHOUSE_IP', '127.0.0.1')
+
+redis_ip = os.getenv('REDIS_IP', '127.0.0.1')
