409c3a869ed9a57d316e5a290fabf864248d97c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:42 +0000

    Add package medicine-utils/0.1.1/medicine-utils-0.1.1.tar.gz

---
+++ packages/medicine-utils/medicine-utils-0.1.1.tar.gz/medicine-utils-0.1.1/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+from os.path import join, dirname
+
+import medicine_utils
+
+setuptools.setup(
+    name='medicine-utils',
+    packages=setuptools.find_packages(),
+    version=medicine_utils.__version__,
+    license='MIT',
+    url='https://bitbucket.org/med-logic/medicine-utils',
+    long_description=open(join(dirname(__file__), 'README.md')).read(),
+    long_description_content_type="text/markdown"
+)
