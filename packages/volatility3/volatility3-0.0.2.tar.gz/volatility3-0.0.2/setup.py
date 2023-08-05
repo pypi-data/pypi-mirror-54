083ecd667a7756dd3b1271f8a1ad0aa5d368a8b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:58 +0000

    Add package volatility3/0.0.2/volatility3-0.0.2.tar.gz

---
+++ packages/volatility3/volatility3-0.0.2.tar.gz/volatility3-0.0.2/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="volatility3",
+    version="0.0.2",
+    author="volatilityfoundation",
+    author_email="info@volatilityfoundation.org",
+    description="Volatility 3: The volatile memory extraction framework. DUMMY PACKAGE. DO NOT USE.",
+    long_description="This is a dummy package. The official project website is available at https://github.com/volatilityfoundation/volatility3",
+    url="https://github.com/volatilityfoundation/volatility3",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5.3',
+)
