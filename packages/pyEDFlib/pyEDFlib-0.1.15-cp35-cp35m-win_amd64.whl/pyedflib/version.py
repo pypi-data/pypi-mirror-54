9d55cf65e1863b640a04f7f9e25cd121ae9895c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:28 +0000

    Add package pyEDFlib/0.1.15/pyEDFlib-0.1.15-cp35-cp35m-win_amd64.whl

---
+++ packages/pyEDFlib/pyEDFlib-0.1.15-cp35-cp35m-win_amd64.whl/pyedflib/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM pyedflib SETUP.PY
+short_version = '0.1.15'
+version = '0.1.15'
+full_version = '0.1.15'
+git_revision = '3b2f936f67936f1fea16d2e0106fa870e587d167'
+release = True
+
+if not release:
+    version = full_version
