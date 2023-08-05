2bfc69fec4b24f297d6d355cce65e2a720e8b292
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:53 +0000

    Add package wdl-lsp/0.0.72/wdl-lsp-0.0.72.tar.gz

---
+++ packages/wdl-lsp/wdl-lsp-0.0.72.tar.gz/wdl-lsp-0.0.72/setup.py
@@ -0,0 +1,40 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='wdl-lsp',
+    author='Broad Institute',
+    description='Language Server Protocol (LSP) implementation for Workflow Definition Language (WDL)',
+    license='BSD 3-clause "New" or "Revised" License',
+    url='https://github.com/broadinstitute/wdl-ide',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Software Development :: Interpreters',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    python_requires='>=3.7.0',
+    packages=find_packages(
+        exclude=[
+            'tests',
+        ],
+    ),
+    setup_requires=[
+        'setuptools_scm',
+    ],
+    use_scm_version={
+        'root': '..',
+        'relative_to': __file__,
+    },
+    install_requires=[
+        'cromwell-tools == 2.3.0',
+        'miniwdl == 0.5.1',
+        'pygls == 0.8.1',
+    ],
+    entry_points={
+        'console_scripts': ['wdl-lsp = wdl_lsp.__main__:main'],
+    },
+)
