d58968aeebe0901f3b9aa2d9e0e6047ec69153e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:43 +0000

    Add package xtream/0.1.0/xtream-0.1.0-py2.py3-none-any.whl

---
+++ packages/xtream/xtream-0.1.0-py2.py3-none-any.whl/xtream/__init__.py
@@ -0,0 +1,21 @@
+"""NStream v0.1.
+Usage:
+  nstream ship new <name>...
+  nstream ship <name> move <x> <y> [--speed=<kn>]
+  nstream ship shoot <x> <y>
+  nstream mine (set|remove) <x> <y> [--moored|--drifting]
+  nstream -h | --help
+  nstream --version
+Options:
+  -h --help     Show this screen.
+  --version     Show version.
+  --speed=<kn>  Speed in knots [default: 10].
+  --moored      Moored (anchored) mine.
+  --drifting    Drifting mine.
+"""
+from docopt import docopt
+
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__, version='NStream v0.1')
+    print(arguments)
