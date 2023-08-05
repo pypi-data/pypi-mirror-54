f5bd4d9f14fd4d2c8e4b4850c7add183e6d3602c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:07 +0000

    Add package print-custum-module/1.0/print_custum_module-1.0.tar.gz

---
+++ packages/print-custum-module/print_custum_module-1.0.tar.gz/print_custum_module-1.0/print_custum_module.py
@@ -0,0 +1,3 @@
+def print_list(lst):
+        for elt	in lst:
+            print(elt)
---
+++ packages/print-custum-module/print_custum_module-1.0.tar.gz/print_custum_module-1.0/setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(
+	name='print_custum_module',
+	version='1.0',
+	py_modules=['print_custum_module']
+)
