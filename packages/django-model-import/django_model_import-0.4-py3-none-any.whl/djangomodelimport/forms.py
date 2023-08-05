cdef9a7800f8ca25268e8f652ca24849dbe0b45b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:26 +0000

    Add package django-model-import/0.4/django_model_import-0.4-py3-none-any.whl

---
+++ packages/django-model-import/django_model_import-0.4-py3-none-any.whl/djangomodelimport/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+
+from .magic import CachedChoiceFieldFormMixin, FlatRelatedFieldFormMixin, JSONFieldFormMixin
+
+
+class ImporterModelForm(JSONFieldFormMixin, FlatRelatedFieldFormMixin, CachedChoiceFieldFormMixin, forms.ModelForm):
+    """ Extends the ModelForm to prime our caches and tweaks the validation
+    routines to ensure we are not doing too many queries with our cached fields.
+    """
+    def __init__(self, data, caches, *args, **kwargs):
+        self.caches = caches
+        super().__init__(data, *args, **kwargs)
+
+    def validate_unique(self):
+        pass
