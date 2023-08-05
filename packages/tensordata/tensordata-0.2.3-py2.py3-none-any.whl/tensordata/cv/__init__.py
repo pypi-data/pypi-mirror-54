56daec8ce38df143a13c50a81fccca331e6cc8ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:42 +0000

    Add package tensordata/0.2.3/tensordata-0.2.3-py2.py3-none-any.whl

---
+++ packages/tensordata/tensordata-0.2.3-py2.py3-none-any.whl/tensordata/cv/__init__.py
@@ -0,0 +1,7 @@
+from tensordata.cv._cifar import *
+from tensordata.cv._mnist import *
+from tensordata.cv._mnist_kuzushiji import *
+from tensordata.cv._mnist_tibetan import *
+from tensordata.cv._coil import *
+from tensordata.cv._caltech import *
+from tensordata.cv._stl10 import *
