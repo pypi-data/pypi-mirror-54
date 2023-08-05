48473461b2a0575c01f1a064b8d6cfd1920b52e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:10 +0000

    Add package events-sdk/0.2/events_sdk-0.2-py2-none-any.whl

---
+++ packages/events-sdk/events_sdk-0.2-py2-none-any.whl/src/publish.py
@@ -0,0 +1,11 @@
+class Publish():
+
+    def publish_kinesis(self,data,context):
+        pass
+
+    def publish_cloudwatch(self, data, context):
+        pass
+
+    def publish_slack(self,data,context):
+        pass
+
---
+++ packages/events-sdk/events_sdk-0.2-py2-none-any.whl/src/subscribe.py
@@ -0,0 +1,3 @@
+class Subscribe():
+    def subscribe(self):
+        pass
