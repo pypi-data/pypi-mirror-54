a5b3163b8f1b4deb26a221629ea7ea43af085e5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:44 +0000

    Add package dimp/0.5.1/dimp-0.5.1.tar.gz

---
+++ packages/dimp/dimp-0.5.1.tar.gz/dimp-0.5.1/dimp/protocol/text.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+#
+#   DIMP : Decentralized Instant Messaging Protocol
+#
+#                                Written in 2019 by Moky <albert.moky@gmail.com>
+#
+# ==============================================================================
+# MIT License
+#
+# Copyright (c) 2019 Albert Moky
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ==============================================================================
+
+from dkd import Content, ContentType
+from dkd.content import message_content_classes
+
+
+class TextContent(Content):
+    """
+        Text Message Content
+        ~~~~~~~~~~~~~~~~~~~~
+
+        data format: {
+            type : 0x01,
+            sn   : 123,
+
+            text : "..."
+        }
+    """
+
+    #
+    #   text
+    #
+    @property
+    def text(self) -> str:
+        return self.get('text')
+
+    @text.setter
+    def text(self, value: str):
+        if value is None:
+            self.pop('text', None)
+        else:
+            self['text'] = value
+
+    #
+    #   Factory
+    #
+    @classmethod
+    def new(cls, text: str) -> Content:
+        content = {
+            'type': ContentType.Text,
+            'text': text,
+        }
+        return TextContent(content)
+
+
+# register content class
+message_content_classes[ContentType.Text] = TextContent
