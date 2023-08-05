0c02c24152b305fe5ffff43ce60d08ebaf4f74c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:39 +0000

    Add package factionpy/0.0.5/factionpy-0.0.5-py3-none-any.whl

---
+++ packages/factionpy/factionpy-0.0.5-py3-none-any.whl/factionpy/models/language.py
@@ -0,0 +1,7 @@
+from factionpy.backend.database import db
+
+
+class Language(db.Model):
+    __tablename__ = "Language"
+    Id = db.Column(db.Integer, primary_key=True)
+    Name = db.Column(db.String)
