be1b0d0a918cab5e92f67632e0923429d53a494f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:49 +0000

    Add package mavsdk/0.3.0/mavsdk-0.3.0-py3-none-macosx_10_9_x86_64.whl

---
+++ packages/mavsdk/mavsdk-0.3.0-py3-none-macosx_10_9_x86_64.whl/mavsdk/plugins/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from .gimbal import *
+from .core import *
+from .calibration import *
+from .camera import *
+from .param import *
+from .mission import *
+from .info import *
+from .action import *
+from .offboard import *
+from .telemetry import *
