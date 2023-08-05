492f515f6051a30385893ffff9c16503f036b2aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:16 +0000

    Add package zhangyx-test-1/0.20/zhangyx_test_1-0.20.tar.gz

---
+++ packages/zhangyx-test-1/zhangyx_test_1-0.20.tar.gz/zhangyx_test_1-0.20/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="zhangyx_test_1",
+    version="0.20",
+    description="My test module",
+    author="Zhang Yexing",
+    url="http://www.csdn.net",
+    license="LGPL",
+    packages= find_packages(),
+)
---
+++ packages/zhangyx-test-1/zhangyx_test_1-0.20.tar.gz/zhangyx_test_1-0.20/zhangyx_test_1/test.py
@@ -0,0 +1,6 @@
+import os
+
+# print(os.environ.keys())
+def print_char():
+    print("import test.py successful!")    
+
