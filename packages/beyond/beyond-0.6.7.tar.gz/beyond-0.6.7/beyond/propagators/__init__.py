ee05430ebe40cc987e9a7fd86aac01c603538cec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:14:22 +0000

    Add package beyond/0.6.7/beyond-0.6.7.tar.gz

---
+++ packages/beyond/beyond-0.6.7.tar.gz/beyond-0.6.7/beyond/propagators/__init__.py
@@ -0,0 +1,26 @@
+"""This module defines the propagators availables
+"""
+
+from ..errors import UnknownPropagatorError
+
+
+def get_propagator(name):
+    """Retrieve a named propagator
+
+    Args:
+        name (str): Name of the desired propagator
+    Return:
+        Propagator class
+    """
+
+    from .sgp4 import Sgp4
+    from .sgp4beta import Sgp4Beta
+    from .kepler import Kepler
+
+    scope = locals().copy()
+    scope.update(globals())
+
+    if name not in scope:
+        raise UnknownPropagatorError(name)
+
+    return scope[name]
