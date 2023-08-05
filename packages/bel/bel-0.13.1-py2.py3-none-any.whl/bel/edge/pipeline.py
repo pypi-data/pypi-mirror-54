f3a3ac3cad76dea2b0fafc949c27ffce2ed1b31d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:33 +0000

    Add package bel/0.13.1/bel-0.13.1-py2.py3-none-any.whl

---
+++ packages/bel/bel-0.13.1-py2.py3-none-any.whl/bel/edge/pipeline.py
@@ -0,0 +1,251 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Usage:  program.py <customer>
+
+"""
+
+import copy
+import datetime
+import itertools
+import json
+import os.path
+import urllib
+
+import requests
+import structlog
+
+import bel.db.arangodb as arangodb
+import bel.edge.edges
+import bel.nanopub.files as files
+import bel.utils as utils
+
+log = structlog.getLogger(__name__)
+
+client = arangodb.get_client()
+edgestore_db = arangodb.get_edgestore_handle(client)
+
+edges_coll_name = arangodb.edgestore_edges_name
+nodes_coll_name = arangodb.edgestore_nodes_name
+
+
+def get_edges_for_nanopub(nanopub_id):
+    query = f"""
+        FOR edge IN edges
+            FILTER edge.nanopub_id == "{nanopub_id}"
+            LIMIT 1
+            RETURN edge
+    """
+    try:
+        result = [edge for edge in edgestore_db.aql.execute(query)]
+        return result[0]
+    except Exception:
+        return None
+
+
+def process_nanopub(
+    nanopub_url, orthologize_targets: list = [], overwrite: bool = False, token: str = None
+):
+
+    log.info("Processing nanopub", nanopub_url=nanopub_url)
+
+    url_comps = urllib.parse.urlparse(nanopub_url)
+    nanopub_id = os.path.basename(url_comps.path)
+    # domain = url_comps.netloc
+
+    start_time = datetime.datetime.now()
+
+    # collect nanopub
+    headers = {}
+    if token:
+        headers = {"Authorization": f"Bearer {token}"}
+
+    r = requests.get(nanopub_url, headers=headers)
+
+    nanopub = r.json()
+    log.debug(f"Nanopub {nanopub_url}", nanopub=nanopub)
+
+    assertions = []
+    for assertion in nanopub["nanopub"].get("assertions", []):
+        assertions.append(f"{assertion['subject']} {assertion['relation']} {assertion['object']}")
+
+    if not nanopub:
+        return {
+            "msg": f"Could not GET nanopub: {nanopub_url}",
+            "edges_cnt": 0,
+            "assertions_cnt": 0,
+            "assertions": assertions,
+            "success": False,
+            "errors": [],
+        }
+
+    nanopub["source_url"] = nanopub_url
+
+    end_time1 = datetime.datetime.now()
+    delta_ms = f"{(end_time1 - start_time).total_seconds() * 1000:.1f}"
+    log.debug("Timing - Get nanopub", delta_ms=delta_ms)
+
+    # Is nanopub in edge newer than from queue? If so, skip
+    if not overwrite:
+        # collect one edge for nanopub from edgestore
+        edge = get_edges_for_nanopub(nanopub_id)
+        if edge:
+            # check if edge nanopub is newer
+            # log.info("Nanopub to Edge comparison", nanopub=nanopub, edge=edge)
+            if edge["metadata"].get("gd_updateTS", None):
+                if nanopub["nanopub"]["metadata"]["gd_updateTS"] <= edge["metadata"]["gd_updateTS"]:
+                    return {"msg": "Nanopub older than edge nanopub", "success": True, "e": ""}
+
+    end_time2 = datetime.datetime.now()
+    delta_ms = f"{(end_time2 - end_time1).total_seconds() * 1000:.1f}"
+    log.debug("Timing - Get edge to check nanopub", delta_ms=delta_ms)
+
+    results = bel.edge.edges.nanopub_to_edges(nanopub, orthologize_targets=orthologize_targets)
+
+    end_time3 = datetime.datetime.now()
+    delta_ms = f"{(end_time3 - end_time2).total_seconds() * 1000:.1f}"
+
+    if results["success"]:
+
+        load_edges_into_db(nanopub_id, nanopub["source_url"], edges=results["edges"])
+
+        end_time4 = datetime.datetime.now()
+        delta_ms = f"{(end_time4 - end_time3).total_seconds() * 1000:.1f}"
+        log.debug("Timing - Load edges into edgestore", delta_ms=delta_ms)
+
+        delta_ms = f"{(end_time4 - start_time).total_seconds() * 1000:.1f}"
+        log.debug("Timing - Process nanopub into edges", delta_ms=delta_ms)
+
+        return {
+            "msg": f"Loaded {len(results['edges'])} edges into edgestore",
+            "edges_cnt": len(results["edges"]),
+            "assertions_cnt": len(nanopub["nanopub"]["assertions"]),
+            "assertions": assertions,
+            "success": True,
+            "errors": results["errors"],
+        }
+
+    else:
+        return {
+            "msg": f'Could not process nanopub into edges - error: {results["errors"]}',
+            "edges_cnt": 0,
+            "assertions_cnt": len(nanopub["nanopub"]["assertions"]),
+            "assertions": assertions,
+            "success": False,
+            "errors": results["errors"],
+        }
+
+
+def load_edges_into_db(
+    nanopub_id: str,
+    nanopub_url: str,
+    edges: list = [],
+    edges_coll_name: str = edges_coll_name,
+    nodes_coll_name: str = nodes_coll_name,
+):
+    """Load edges into Edgestore"""
+
+    start_time = datetime.datetime.now()
+
+    # Clean out edges for nanopub in edgestore
+    query = f"""
+        FOR edge IN {edges_coll_name}
+            FILTER edge.nanopub_id == "{nanopub_id}"
+            REMOVE edge IN edges
+    """
+
+    try:
+        edgestore_db.aql.execute(query)
+    except Exception as e:
+        log.debug(f"Could not remove nanopub-related edges: {query}  msg: {e}")
+
+    end_time1 = datetime.datetime.now()
+    delta_ms = f"{(end_time1 - start_time).total_seconds() * 1000:.1f}"
+    log.debug("Timing - Delete edges for nanopub", delta_ms=delta_ms)
+
+    # Collect edges and nodes to load into arangodb
+    node_list, edge_list = [], []
+    for doc in edge_iterator(edges=edges):
+
+        log.error("Edge to be inserted", doc=doc)
+
+        if doc[0] == "nodes":
+            node_list.append(doc[1])
+        else:
+            edge_list.append(doc[1])
+
+    end_time2 = datetime.datetime.now()
+    delta_ms = f"{(end_time2 - end_time1).total_seconds() * 1000:.1f}"
+    log.debug("Timing - Collect edges and nodes", delta_ms=delta_ms)
+
+    try:
+        results = edgestore_db.collection(edges_coll_name).import_bulk(
+            edge_list, on_duplicate="replace", halt_on_error=False
+        )
+    except Exception as e:
+        log.error(f"Could not load edges  msg: {e}")
+
+    end_time3 = datetime.datetime.now()
+    delta_ms = f"{(end_time3 - end_time2).total_seconds() * 1000:.1f}"
+    log.debug("Timing - Load edges into edgestore", delta_ms=delta_ms)
+
+    try:
+        results = edgestore_db.collection(nodes_coll_name).import_bulk(
+            node_list, on_duplicate="replace", halt_on_error=False
+        )
+    except Exception as e:
+        log.error(f"Could not load nodes  msg: {e}")
+
+    end_time4 = datetime.datetime.now()
+    delta_ms = f"{(end_time4 - end_time3).total_seconds() * 1000:.1f}"
+    log.debug("Timing - Load nodes into edgestore", delta_ms=delta_ms)
+
+
+def edge_iterator(edges=[], edges_fn=None):
+    """Yield documents from edge for loading into ArangoDB"""
+
+    for edge in itertools.chain(edges, files.read_edges(edges_fn)):
+
+        subj = copy.deepcopy(edge["edge"]["subject"])
+        subj_id = str(utils._create_hash_from_doc(subj))
+        subj["_key"] = subj_id
+        obj = copy.deepcopy(edge["edge"]["object"])
+
+        obj_id = str(utils._create_hash_from_doc(obj))
+        obj["_key"] = obj_id
+        relation = copy.deepcopy(edge["edge"]["relation"])
+
+        relation["_from"] = f"nodes/{subj_id}"
+        relation["_to"] = f"nodes/{obj_id}"
+
+        # Create edge _key
+        relation_hash = copy.deepcopy(relation)
+        relation_hash.pop("edge_dt", None)
+        relation_hash.pop("edge_hash", None)
+        relation_hash.pop("nanopub_dt", None)
+        relation_hash.pop("nanopub_url", None)
+        relation_hash.pop("subject_canon", None)
+        relation_hash.pop("object_canon", None)
+        relation_hash.pop("public_flag", None)
+        relation_hash.pop("metadata", None)
+
+        relation_id = str(utils._create_hash_from_doc(relation_hash))
+        relation["_key"] = relation_id
+
+        if edge.get("nanopub_id", None):
+            if "metadata" not in relation:
+                relation["metadata"] = {}
+            relation["metadata"]["nanopub_id"] = edge["nanopub_id"]
+
+        yield ("nodes", subj)
+        yield ("nodes", obj)
+        yield ("edges", relation)
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/bel/bel-0.13.1-py2.py3-none-any.whl/bel/nanopub/validate.py
@@ -0,0 +1,216 @@
+import re
+from typing import Tuple
+
+import structlog
+
+import bel.db.elasticsearch
+import bel.lang.belobj
+from bel.Config import config
+
+log = structlog.getLogger(__name__)
+
+es = bel.db.elasticsearch.get_client()
+
+
+def convert_msg_to_html(msg):
+    """Convert \n into a <BR> for an HTML formatted message"""
+
+    msg = re.sub("\n", "<br />", msg, flags=re.MULTILINE)
+    return msg
+
+
+def validate(nanopub: dict, error_level: str = "WARNING") -> Tuple[str, str, str]:
+    """Validate Nanopub
+
+    Error Levels are similar to log levels - selecting WARNING includes both
+    WARNING and ERROR, selecting ERROR just includes ERROR
+
+    The validation result is a list of objects containing
+        {
+            'level': 'Warning|Error',
+            'section': 'Assertion|Annotation|Structure',
+            'label': '{Error|Warning}-{Assertion|Annotation|Structure}',  # to be used for faceting in Elasticsearch
+            'index': idx,  # Index of Assertion or Annotation in Nanopub - starts at 0
+            'msg': msg,  # Error or Warning message
+        }
+
+    Args:
+        nanopub: nanopub record starting with nanopub...
+        level: return WARNING or just ERROR?  defaults to warnings and errors
+    Returns:
+        list(tuples): [{'level': 'Warning', 'section': 'Assertion', 'label': 'Warning-Assertion', 'index': 0, 'msg': <msg>}]
+
+    """
+
+    # Validation results
+    validation_results = []
+
+    bel_version = config["bel"]["lang"]["default_bel_version"]
+
+    # Structural checks
+    try:
+        if not isinstance(nanopub["nanopub"]["assertions"], list):
+            msg = "Assertions must be a list/array"
+            validation_results.append(
+                {
+                    "level": "Error",
+                    "section": "Structure",
+                    "label": "Error-Structure",
+                    "msg": msg,
+                    "msg_html": msg,
+                }
+            )
+    except Exception as e:
+        msg = 'Missing nanopub["nanopub"]["assertions"]'
+        validation_results.append(
+            {
+                "level": "Error",
+                "section": "Structure",
+                "label": "Error-Structure",
+                "msg": msg,
+                "msg_html": msg,
+            }
+        )
+
+    try:
+        if "name" in nanopub["nanopub"]["type"] and "version" in nanopub["nanopub"]["type"]:
+            pass
+        if nanopub["nanopub"]["type"]["name"].upper() == "BEL":
+            bel_version = nanopub["nanopub"]["type"]["version"]
+
+    except Exception as e:
+        msg = 'Missing or badly formed type - must have nanopub["nanopub"]["type"] = {"name": <name>, "version": <version}'
+        validation_results.append(
+            {
+                "level": "Error",
+                "section": "Structure",
+                "label": "Error-Structure",
+                "msg": msg,
+                "msg_html": msg,
+            }
+        )
+
+    try:
+        for key in ["uri", "database", "reference"]:
+            if key in nanopub["nanopub"]["citation"]:
+                break
+        else:
+            msg = (
+                'nanopub["nanopub"]["citation"] must have either a uri, database or reference key.'
+            )
+            validation_results.append(
+                {
+                    "level": "Error",
+                    "section": "Structure",
+                    "label": "Error-Structure",
+                    "msg": msg,
+                    "msg_html": msg,
+                }
+            )
+    except Exception as e:
+        msg = 'nanopub["nanopub"] must have a "citation" key with either a uri, database or reference key.'
+        validation_results.append(
+            {
+                "level": "Error",
+                "section": "Structure",
+                "label": "Error-Structure",
+                "msg": msg,
+                "msg_html": msg,
+            }
+        )
+
+    # Assertion checks
+    if "assertions" in nanopub["nanopub"]:
+        for idx, assertion in enumerate(nanopub["nanopub"]["assertions"]):
+            bo = bel.lang.belobj.BEL(bel_version, config["bel_api"]["servers"]["api_url"])
+            belstr = f'{assertion.get("subject")} {assertion.get("relation", "")} {assertion.get("object", "")}'
+            belstr = belstr.replace("None", "")
+            try:
+                messages = (
+                    bo.parse(belstr)
+                    .semantic_validation(error_level=error_level)
+                    .validation_messages
+                )
+
+                for message in messages:
+                    (level, msg) = message
+                    if error_level == "ERROR":
+                        if level == "ERROR":
+                            validation_results.append(
+                                {
+                                    "level": f"{level.title()}",
+                                    "section": "Assertion",
+                                    "label": f"{level.title()}-Assertion",
+                                    "index": idx,
+                                    "msg": msg,
+                                    "msg_html": convert_msg_to_html(msg),
+                                }
+                            )
+                    else:
+                        validation_results.append(
+                            {
+                                "level": f"{level.title()}",
+                                "section": "Assertion",
+                                "label": f"{level.title()}-Assertion",
+                                "index": idx,
+                                "msg": msg,
+                                "msg_html": convert_msg_to_html(msg),
+                            }
+                        )
+
+            except Exception as e:
+                msg = f"Could not parse: {belstr}"
+                validation_results.append(
+                    {
+                        "level": "Error",
+                        "section": "Assertion",
+                        "label": "Error-Assertion",
+                        "index": idx,
+                        "msg": msg,
+                        "msg_html": msg,
+                    }
+                )
+                log.exception(f"Could not parse: {belstr}")
+
+    # Annotation checks
+    if error_level == "WARNING":
+        for idx, annotation in enumerate(nanopub["nanopub"].get("annotations", [])):
+            term_type = annotation["type"]
+            term_id = annotation["id"]
+            # term_label = annotation['label']
+            log.info(f"Annotation: {term_type}  ID: {term_id}")
+
+            search_body = {
+                "_source": ["src_id", "id", "name", "label", "annotation_types"],
+                "query": {"term": {"id": term_id}},
+            }
+
+            results = es.search(index="terms", doc_type="term", body=search_body)
+            if len(results["hits"]["hits"]) > 0:
+                result = results["hits"]["hits"][0]["_source"]
+                if term_type not in result["annotation_types"]:
+                    msg = f'Annotation type: {term_type} for {term_id} does not match annotation types in database: {result["annotation_types"]}'
+                    validation_results.append(
+                        {
+                            "level": "Warning",
+                            "section": "Annotation",
+                            "index": idx,
+                            "label": "Warning-Annotation",
+                            "msg": msg,
+                            "msg_html": msg,
+                        }
+                    )
+            else:
+                msg = f"Annotation term: {term_id} not found in database"
+                validation_results.append(
+                    {
+                        "level": "Warning",
+                        "section": "Annotation",
+                        "index": idx,
+                        "label": "Warning-Annotation",
+                        "msg": msg,
+                        "msg_html": msg,
+                    }
+                )
+
+    return validation_results
