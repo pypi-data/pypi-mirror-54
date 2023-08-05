fac216f73c9d72a95bbcc2c4249e2aa491f6d68f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:20 +0000

    Add package legomena/1.2.0/legomena-1.2.0.tar.gz

---
+++ packages/legomena/legomena-1.2.0.tar.gz/legomena-1.2.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="legomena",
+    version="1.2.0",
+    description="Tool for exploring types, tokens, and n-legomena relationships in text.",
+    license="MIT",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    py_modules=["legomena"],
+    author="Victor Davis",
+    author_email="vadsql@gmail.com",
+    url="https://github.com/VictorDavis/legomena",
+    install_requires=["mpmath", "nltk", "numpy", "pandas", "scipy"],
+    python_requires=">=3.5",
+)
