431c04744c4801c32ce042401141288d23d63268
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:56 +0000

    Add package onespacemedia-cms/4.0.10/onespacemedia-cms-4.0.10.tar.gz

---
+++ packages/onespacemedia-cms/onespacemedia-cms-4.0.10.tar.gz/onespacemedia-cms-4.0.10/cms/apps/pages/tests/test_admin_destructive.py
@@ -0,0 +1,32 @@
+import sys
+
+from django.conf import settings
+from django.contrib import admin
+from django.test import TestCase
+
+from ..models import Country, CountryGroup, Page
+
+
+class TestArticleAdminBase(TestCase):
+
+    def test_article_admin(self):
+        NEW_MIDDLEWARE_CLASSES = (
+            'cms.middleware.LocalisationMiddleware',
+        ) + tuple(settings.MIDDLEWARE_CLASSES)
+
+        self.assertNotIn(Country, admin.site._registry)
+        self.assertNotIn(CountryGroup, admin.site._registry)
+
+        with self.settings(MIDDLEWARE_CLASSES=NEW_MIDDLEWARE_CLASSES):
+            module = sys.modules['cms.apps.pages.admin']
+            del sys.modules['cms.apps.pages.admin']
+
+            admin.site.unregister(Page)
+
+            from ..admin import page_admin
+            assert page_admin
+
+            self.assertIn(Country, admin.site._registry)
+            self.assertIn(CountryGroup, admin.site._registry)
+
+            sys.modules['cms.apps.pages.admin'] = module
