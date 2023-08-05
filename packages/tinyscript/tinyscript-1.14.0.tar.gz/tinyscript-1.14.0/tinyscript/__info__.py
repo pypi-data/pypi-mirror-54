2f98c7ef0b36a1ad394c33a8785d64f268b2116e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:08 +0000

    Add package tinyscript/1.14.0/tinyscript-1.14.0.tar.gz

---
+++ packages/tinyscript/tinyscript-1.14.0.tar.gz/tinyscript-1.14.0/tinyscript/__info__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""Tinyscript package information.
+
+"""
+
+__author__    = "Alexandre D'Hondt"
+__version__   = "1.14.0"
+__copyright__ = "© 2019 A. D'Hondt"
+__license__   = "AGPLv3 (http://www.gnu.org/licenses/agpl.html)"
+
