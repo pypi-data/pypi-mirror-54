a9f7ecc88b7684dd915eca65275d311bd63b28e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:28 +0000

    Add package Collectfast/1.3.0/Collectfast-1.3.0-py3-none-any.whl

---
+++ packages/Collectfast/Collectfast-1.3.0-py3-none-any.whl/collectfast/tests/no_preload_metadata.py
@@ -0,0 +1,10 @@
+from storages.backends.s3boto3 import S3Boto3Storage
+
+
+class NPM(S3Boto3Storage):
+    """
+    Dummy class for testing that collectfast warns about overriding the
+    `preload_metadata` attribute.
+    """
+
+    preload_metadata = False
