c8b42b3d64f8ceeaa56d6e3a5d6fe9b20a056382
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:00 +0000

    Add package sky-chaostoolkit-prometheus/0.3.4/sky-chaostoolkit-prometheus-0.3.4.tar.gz

---
+++ packages/sky-chaostoolkit-prometheus/sky-chaostoolkit-prometheus-0.3.4.tar.gz/sky-chaostoolkit-prometheus-0.3.4/CHANGELOG.md
@@ -0,0 +1,35 @@
+# Changelog
+
+## [Unreleased][]
+
+[Unreleased]: https://github.com/chaostoolkit/chaostoolkit-prometheus/compare/0.3.0...HEAD
+
+### Added
+
+-   Added `requirements-dev.txt` for development dependencies to match template.
+
+## [0.3.0][] - 2017-12-17
+
+[0.3.0]: https://github.com/chaostoolkit/chaostoolkit-prometheus/compare/0.2.0...0.3.0
+
+### Changed
+
+- Timezone aware date parsing
+- More logging
+- Updated to chaostoolkit-lib
+
+## [0.2.0][] - 2017-12-08
+
+[0.2.0]: https://github.com/chaostoolkit/chaostoolkit-prometheus/compare/0.1.0...0.2.0
+
+### Changed
+
+-   Matching new format from release 0.6.0 of chaostoolkit-lib
+
+## [0.1.0][] - 2017-10-16
+
+[0.1.0]: https://github.com/chaostoolkit/chaostoolkit-prometheus/tree/0.1.0
+
+### Added
+
+-   Initial release
---
+++ packages/sky-chaostoolkit-prometheus/sky-chaostoolkit-prometheus-0.3.4.tar.gz/sky-chaostoolkit-prometheus-0.3.4/chaosprom/probes.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+from typing import Any, Dict
+
+import dateparser
+from logzero import logger
+import maya
+import requests
+import jsonpath
+
+from chaoslib.exceptions import FailedActivity
+from chaoslib.types import Configuration, Secrets
+
+__all__ = ["query", "query_interval"]
+
+
+def query(query: str, when: str = None, timeout: float = None,
+          configuration: Configuration = None,
+          secrets: Secrets = None) -> Dict[str, Any]:
+    """
+    Run an instant query against a Prometheus server and returns its result
+    as-is.
+    """
+    base = (configuration or {}).get(
+        "prometheus_base_url", "http://10.119.8.142:9090")
+    url = "{base}/api/v1/query".format(base=base)
+
+    params = {"query": query}
+
+    if timeout is not None:
+        params["timeout"] = timeout
+
+    if when:
+        when_dt = dateparser.parse(when, settings={
+            'RETURN_AS_TIMEZONE_AWARE': True})
+        if not when_dt:
+            raise FailedActivity("failed to parse '{s}'".format(s=when))
+        params["time"] = maya.MayaDT.from_datetime(when_dt).rfc3339()
+
+    logger.debug("Querying with: {q}".format(q=params))
+
+    r = requests.get(
+        url, headers={"Accept": "application/json"}, params=params)
+
+    if r.status_code != 200:
+        raise FailedActivity(
+            "Prometheus query {q} failed: {m}".format(q=str(params), m=r.text))
+
+    return r.json()
+
+
+def query_interval(query: str, start: str, end: str, step: int = 1,
+                   timeout: float = None, configuration: Configuration = None,
+                   secrets: Secrets = None) -> Dict[str, Any]:
+    """
+    Run a range query against a Prometheus server and returns its result as-is.
+
+    The `start` and `end` arguments can be a RFC 3339 date or expressed more
+    colloquially such as `"5 minutes ago"`.
+    """
+    base = (configuration or {}).get(
+        "prometheus_base_url", "http://localhost:9090")
+    url = "{base}/api/v1/query_range".format(base=base)
+
+    params = {"query": query}
+
+    if timeout is not None:
+        params["timeout"] = timeout
+
+    if step:
+        params["step"] = step
+
+    start_dt = dateparser.parse(start, settings={
+        'RETURN_AS_TIMEZONE_AWARE': True})
+    if not start_dt:
+        raise FailedActivity("failed to parse '{s}'".format(s=start))
+    params["start"] = maya.MayaDT.from_datetime(start_dt).rfc3339()
+
+    end_dt = dateparser.parse(end, settings={
+        'RETURN_AS_TIMEZONE_AWARE': True})
+    if not end_dt:
+        raise FailedActivity("failed to parse '{s}'".format(s=end))
+    params["end"] = maya.MayaDT.from_datetime(end_dt).rfc3339()
+
+    logger.debug("Querying with: {q}".format(q=params))
+
+    r = requests.get(
+        url, headers={"Accept": "application/json"}, params=params)
+
+    if r.status_code != 200:
+        raise FailedActivity(
+            "Prometheus query range {q} failed: {m}".format(
+                q=str(params), m=r.text))
+    
+    return r.json()
+
+
+def query_alerts_exist_for_job(job: str) -> Dict[str, Any]:
+    """
+    Run an instant query for alerts for a particular job against a Prometheus server 
+    and returns its result as-is.
+    """
+    result=query("ALERTS")
+
+    alerts=jsonpath.jsonpath(result, "$.data.result[*][?(@.job=='{j}')]".format(j=job))
+
+    if alerts:
+            logger.debug("Alert(s) exist for {j}: {m}".format(j=job,m=alerts))
+            return True
+    return False
+
+query_alerts_exist_for_job("AWS_Emissary")
