f7b24c8dacea7645be8ec07fff061c2f3582045b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:01 +0000

    Add package glide/0.2.2/glide-0.2.2-py3-none-any.whl

---
+++ packages/glide/glide-0.2.2-py3-none-any.whl/glide/__init__.py
@@ -0,0 +1,8 @@
+"""Glide package"""
+from glide.core import *
+from glide.extract import *
+from glide.filter import *
+from glide.transform import *
+from glide.load import *
+from glide.pipelines import *
+from .version import __version__
