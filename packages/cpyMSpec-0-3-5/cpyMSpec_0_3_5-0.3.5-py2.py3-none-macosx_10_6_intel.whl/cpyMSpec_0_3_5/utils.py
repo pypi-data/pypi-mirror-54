13eb2a72fa74c9426e1270ab4a30ac4caa258da3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:48 +0000

    Add package cpyMSpec-0-3-5/0.3.5/cpyMSpec_0_3_5-0.3.5-py2.py3-none-macosx_10_6_intel.whl

---
+++ packages/cpyMSpec-0-3-5/cpyMSpec_0_3_5-0.3.5-py2.py3-none-macosx_10_6_intel.whl/cpyMSpec_0_3_5/utils.py
@@ -0,0 +1,20 @@
+import platform
+import os.path
+import cffi
+
+def full_filename(fn):
+    return os.path.abspath(os.path.join(os.path.dirname(__file__), fn))
+
+def shared_lib(name):
+    suffixes = {'Windows': 'dll', 'Linux': 'so', 'Darwin': 'dylib'}
+    return 'lib' + name + '.' + suffixes[platform.system()]
+
+def init_ffi():
+    ffi = cffi.FFI()
+    ffi.cdef(open(full_filename("ims.h")).read())
+    return ffi
+
+def load_shared_lib(ffi):
+    return ffi.dlopen(full_filename(shared_lib("ms_cffi_0_3_5")))
+
+VERSION = "0.3.5"
