713b1ffb6936bc601d791f97faec52a5aa0883cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:19 +0000

    Add package cloudnetpy/1.0.5/cloudnetpy-1.0.5.tar.gz

---
+++ packages/cloudnetpy/cloudnetpy-1.0.5.tar.gz/cloudnetpy-1.0.5/cloudnetpy/constants.py
@@ -0,0 +1,16 @@
+"""Constants used in Cloudnet processing."""
+
+# Triple point of water
+T0 = 273.16
+
+# Ratio of the molecular weight of water vapor to dry air
+mw_ratio = 0.62198
+
+# Specific heat capacity of air at around 275K (J kg-1 K-1)
+specific_heat = 1004
+
+# Latent heat of evaporation (J kg-1)
+latent_heat = 2.26e6
+
+# Specific gas constant for dry  air (J kg-1 K-1)
+Rs = 287.058
