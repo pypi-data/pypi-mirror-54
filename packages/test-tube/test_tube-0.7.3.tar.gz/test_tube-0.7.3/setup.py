7ec1bdf1abb14b1fe60c58b899f614360deda3c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:17 +0000

    Add package test-tube/0.7.3/test_tube-0.7.3.tar.gz

---
+++ packages/test-tube/test_tube-0.7.3.tar.gz/test_tube-0.7.3/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+from setuptools import find_packages, setup
+
+version = '0.7.3'
+
+setup(
+    name='test_tube',
+    packages=find_packages(),
+    version=version,
+    description='Experiment logger and visualizer',
+    author='William Falcon',
+    install_requires=[
+        'pandas>=0.20.3',
+        'numpy>=1.13.3',
+        'imageio>=2.3.0',
+        'tb-nightly==1.15.0a20190708',
+        'torch>=1.1.0',
+        'future'
+    ],
+    author_email='will@hacstudios.com',
+    url='https://github.com/williamFalcon/test_tube',
+    download_url='https://github.com/williamFalcon/test_tube/archive/{}.tar.gz'.format(version),
+    keywords=[
+        'testing',
+        'machine learning',
+        'deep learning',
+        'prototyping',
+        'experimenting',
+        'modeling',
+    ],
+)
