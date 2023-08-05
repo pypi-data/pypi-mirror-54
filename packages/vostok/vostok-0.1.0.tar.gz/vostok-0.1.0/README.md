2e07235ded21fa766d3f24a3274f93f15713b9bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:16 +0000

    Add package vostok/0.1.0/vostok-0.1.0.tar.gz

---
+++ packages/vostok/vostok-0.1.0.tar.gz/vostok-0.1.0/README.md
@@ -0,0 +1,3 @@
+# Vostok
+
+A python changelog parser for Keep a Changelog format https://keepachangelog.com/en/1.0.0/
---
+++ packages/vostok/vostok-0.1.0.tar.gz/vostok-0.1.0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="vostok",
+    version="0.1.0",
+    author="vitorbaraujo",
+    author_email="vitornga15@gmail.com",
+    description="A chagelog parser for Keep a Changelog format",
+    long_description=long_description,
+    url="https://github.com/vitorbaraujo/vostok",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6'
+)
