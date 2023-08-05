23efd871d80d4f57e7a0d08d8d548e8386d47a45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:13 +0000

    Add package meta-lists/0.1.7/meta_lists-0.1.7-py3-none-any.whl

---
+++ packages/meta-lists/meta_lists-0.1.7-py3-none-any.whl/meta_lists/models.py
@@ -0,0 +1,25 @@
+from edc_list_data.model_mixins import ListModelMixin
+
+
+class Symptoms(ListModelMixin):
+    pass
+
+
+class ArvRegimens(ListModelMixin):
+    pass
+
+
+class OffstudyReasons(ListModelMixin):
+    pass
+
+
+class OiProphylaxis(ListModelMixin):
+    pass
+
+
+class BaselineSymptoms(ListModelMixin):
+    pass
+
+
+class DiabetesSymptoms(ListModelMixin):
+    pass
