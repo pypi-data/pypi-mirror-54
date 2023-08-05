64a25c165be3574279a00485a78a9df8898120e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:08 +0000

    Add package galaxy-importer/0.1.3b10/galaxy_importer-0.1.3b10.tar.gz

---
+++ packages/galaxy-importer/galaxy_importer-0.1.3b10.tar.gz/galaxy_importer-0.1.3b10/galaxy_importer/loaders.py
@@ -0,0 +1,234 @@
+# (c) 2012-2019, Ansible by Red Hat
+#
+# This file is part of Ansible Galaxy
+#
+# Ansible Galaxy is free software: you can redistribute it and/or modify
+# it under the terms of the Apache License as published by
+# the Apache Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# Ansible Galaxy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# Apache License for more details.
+#
+# You should have received a copy of the Apache License
+# along with Galaxy.  If not, see <http://www.apache.org/licenses/>.
+
+import abc
+from copy import deepcopy
+import json
+import logging
+import os
+from pathlib import Path
+import re
+from subprocess import Popen, PIPE
+
+from galaxy_importer import constants
+from galaxy_importer import exceptions as exc
+from galaxy_importer import schema
+from galaxy_importer.utils import markup as markup_utils
+
+
+default_logger = logging.getLogger(__name__)
+
+ANSIBLE_DOC_SUPPORTED_TYPES = [
+    'become', 'cache', 'callback', 'cliconf', 'connection',
+    'httpapi', 'inventory', 'lookup', 'shell', 'module', 'strategy', 'vars']
+ANSIBLE_DOC_KEYS = ['doc', 'metadata', 'examples', 'return']
+ANSIBLE_LINT_EXCEPTION_RETURN_CODE = 1
+
+
+class ContentLoader(metaclass=abc.ABCMeta):
+
+    def __init__(self, content_type, rel_path, root, logger=None):
+        """
+        :param content_type: Content type.
+        :param rel_path: Path to content file or dir, relative to root path.
+        :param root: Collection root path.
+        :param logger: Optional logger instance.
+        """
+        self.content_type = content_type
+        self.rel_path = rel_path
+        self.root = root
+        self.name = self._make_name()
+
+        self.doc_strings = None
+        self.description = None
+        self.readme_file = None
+        self.readme_html = None
+
+        self.log = logger or default_logger
+        self._validate_name()
+
+    @abc.abstractmethod
+    def load(self):
+        """Loads data from content inside collection.
+        :return: Content object."""
+        pass
+
+    @abc.abstractmethod
+    def _make_name(self):
+        """Returns content name generated from it's path."""
+        pass
+
+    def _validate_name(self):
+        if not re.match(constants.CONTENT_NAME_REGEXP, self.name):
+            raise exc.ContentNameError(
+                f'{self.content_type.value} name invalid format: {self.name}')
+
+    def _log_loading(self):
+        self.log.info(' ')
+        self.log.info(
+            f'===== LOADING {self.content_type.name}: {self.name} =====')
+
+
+class PluginLoader(ContentLoader):
+    def load(self):
+        self._log_loading()
+        self.doc_strings = self._get_doc_strings()
+
+        return schema.Content(
+            name=self.name,
+            content_type=self.content_type,
+            doc_strings=self.doc_strings,
+        )
+
+    def _make_name(self):
+        return os.path.splitext(os.path.basename(self.rel_path))[0]
+
+    def _get_doc_strings(self):
+        if self.content_type.value not in ANSIBLE_DOC_SUPPORTED_TYPES:
+            return None
+
+        self.log.info('Getting doc strings via ansible-doc')
+        json_output = self._run_ansible_doc()
+
+        if not json_output:
+            return
+        data = json.loads(json_output)
+        if self.name not in data.keys():
+            self.log.error(f'No "{self.name}" key in ansible-doc output')
+            return None
+
+        data = self._transform_doc_strings(data)
+        return {
+            key: data[self.name].get(key, None)
+            for key in ANSIBLE_DOC_KEYS
+        }
+
+    def _transform_doc_strings(self, data):
+        """Transform data meant for UI tables into format suitable for UI."""
+
+        def dict_to_named_list(dict_of_dict):
+            """Return new list of dicts for given dict of dicts."""
+            return [
+                {'name': key, **deepcopy(dict_of_dict[key])} for
+                key in dict_of_dict.keys()
+            ]
+
+        def handle_nested_tables(obj, table_key):
+            """Recurse over dict to replace nested tables with updated format."""
+            if table_key in obj.keys() and isinstance(obj[table_key], dict):
+                obj[table_key] = dict_to_named_list(obj[table_key])
+                for row in obj[table_key]:
+                    handle_nested_tables(row, table_key)
+
+        doc = data[self.name].get('doc')
+        if doc and 'options' in doc.keys() and isinstance(doc['options'], dict):
+            doc['options'] = dict_to_named_list(doc['options'])
+            for d in doc['options']:
+                handle_nested_tables(d, table_key='suboptions')
+
+        ret = data[self.name].get('return')
+        if ret and isinstance(ret, dict):
+            data[self.name]['return'] = dict_to_named_list(ret)
+            for d in data[self.name]['return']:
+                handle_nested_tables(d, table_key='contains')
+
+        return data
+
+    def _run_ansible_doc(self):
+        cmd = [
+            'ansible-doc',
+            '--type', self.content_type.value,
+            '-M', os.path.dirname(self.rel_path),
+            self.name,
+            '--json']
+        self.log.debug('CMD: {}'.format(' '.join(cmd)))
+        proc = Popen(cmd, cwd=self.root, stdout=PIPE, stderr=PIPE)
+        stdout, stderr = proc.communicate()
+        if proc.returncode:
+            self.log.error(f'Error running ansible-doc: {stderr}')
+            return None
+        return stdout
+
+
+class RoleLoader(ContentLoader):
+    def load(self):
+        self._log_loading()
+        for line in self._lint_role(self.rel_path):
+            self.log.warning(line)
+        self._get_readme()
+        self._get_metadata_description()
+
+        return schema.Content(
+            name=self.name,
+            content_type=self.content_type,
+            description=self.description,
+            readme_file=self.readme_file,
+            readme_html=self.readme_html,
+        )
+
+    def _make_name(self):
+        return os.path.basename(self.rel_path)
+
+    def _lint_role(self, path):
+        self.log.info('Linting role via ansible-lint')
+        cmd = [
+            'ansible-lint', path,
+            '-p',
+            '-x', 'metadata',
+        ]
+        self.log.debug('CMD: ' + ' '.join(cmd))
+        proc = Popen(
+            cmd,
+            cwd=self.root,
+            encoding='utf-8',
+            stdout=PIPE,
+        )
+
+        for line in proc.stdout:
+            # shorten linter message filepath to last 3 parts of path
+            # /tmp/tmp_zyx/roles/role_test1/tasks/main.yml:19: [E201] Trail...
+            line_list = line.split(' ')
+            rel_path = os.path.join(*Path(line_list[0]).parts[-3:])
+            line_list[0] = rel_path
+            line = ' '.join(line_list)
+            yield line.strip()
+
+        # returncode 1 is app exception, 0 is no linter err, 2 is linter err
+        if proc.wait() == ANSIBLE_LINT_EXCEPTION_RETURN_CODE:
+            yield 'Exception running ansible-lint, could not complete linting'
+
+    def _get_readme(self):
+        self.log.info('Getting role readme')
+        readme = markup_utils.get_readme_doc_file(
+            os.path.join(self.root, self.rel_path))
+        if not readme:
+            raise exc.ContentLoadError('No role readme found.')
+        self.readme_file = readme.name
+        self.readme_html = markup_utils.get_html(readme)
+
+    def _get_metadata_description(self):
+        self.log.info('Getting role description')
+        pass
+
+
+def get_loader_cls(content_type):
+    if content_type.category == constants.ContentCategory.ROLE:
+        return RoleLoader
+    elif content_type.category in [constants.ContentCategory.PLUGIN,
+                                   constants.ContentCategory.MODULE]:
+        return PluginLoader
+    return None
---
+++ packages/galaxy-importer/galaxy_importer-0.1.3b10.tar.gz/galaxy_importer-0.1.3b10/tests/test_loaders.py
@@ -0,0 +1,260 @@
+# (c) 2012-2019, Ansible by Red Hat
+#
+# This file is part of Ansible Galaxy
+#
+# Ansible Galaxy is free software: you can redistribute it and/or modify
+# it under the terms of the Apache License as published by
+# the Apache Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# Ansible Galaxy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# Apache License for more details.
+#
+# You should have received a copy of the Apache License
+# along with Galaxy.  If not, see <http://www.apache.org/licenses/>.
+
+import attr
+import os
+import pytest
+import re
+import shutil
+import tempfile
+from unittest import mock
+
+from galaxy_importer import constants
+from galaxy_importer import exceptions as exc
+from galaxy_importer import loaders
+from galaxy_importer import schema
+
+
+ANSIBLE_DOC_OUTPUT = """
+    {"my_sample_module": {
+        "doc": {
+            "description": ["Sample module for testing."],
+            "short_description": "Sample module for testing",
+            "version_added": "2.8",
+            "options": {
+                "exclude": {
+                    "description": ["This is the message to send..."],
+                    "required": "true"
+                },
+                "use_new": {
+                    "description": ["Control is passed..."],
+                    "version_added": "2.7",
+                    "default": "auto"
+                }
+            }
+        },
+        "examples": null,
+        "metadata": null,
+        "return": {
+            "message": {
+                "description": "The output message the sample module generates"
+            },
+            "original_message": {
+                "description": "The original name param that was passed in",
+                "type": "str"
+            }
+        }
+    }}
+"""
+
+
+@pytest.fixture
+def loader_module():
+    return loaders.PluginLoader(
+        content_type=constants.ContentType.MODULE,
+        rel_path='plugins/modules/my_sample_module.py',
+        root='/tmp/tmpiskt5e2n')
+
+
+@pytest.fixture
+def loader_role():
+    return loaders.RoleLoader(
+        content_type=constants.ContentType.ROLE,
+        rel_path='roles/my_sample_role',
+        root=None)
+
+
+def test_get_loader_cls():
+    res = loaders.get_loader_cls(constants.ContentType.MODULE)
+    assert issubclass(res, loaders.PluginLoader)
+    assert not issubclass(res, loaders.RoleLoader)
+
+    res = loaders.get_loader_cls(constants.ContentType.ROLE)
+    assert issubclass(res, loaders.RoleLoader)
+    assert not issubclass(res, loaders.PluginLoader)
+
+
+def test_init_plugin_loader(loader_module):
+    assert loader_module.name == 'my_sample_module'
+
+
+def test_init_role_loader(loader_role):
+    assert loader_role.name == 'my_sample_role'
+
+
+def test_bad_plugin_name():
+    with pytest.raises(exc.ContentNameError):
+        loaders.PluginLoader(
+            content_type=constants.ContentType.MODULE,
+            rel_path='plugins/modules/bad-name-dashes.py',
+            root='/tmp/tmpiskt5e2n')
+
+
+def test_bad_role_name():
+    with pytest.raises(exc.ContentNameError):
+        loaders.RoleLoader(
+            content_type=constants.ContentType.ROLE,
+            rel_path='roles/bad-name-dashes',
+            root='/tmp/tmpiskt5e2n')
+
+
+@mock.patch.object(loaders.PluginLoader, '_get_doc_strings')
+def test_plugin_loader_annotated_type(mocked_get_doc_strings, loader_module):
+    mocked_get_doc_strings.return_value = None
+    assert loader_module.name == 'my_sample_module'
+    res = loader_module.load()
+    mocked_get_doc_strings.assert_called_once()
+    assert isinstance(res, schema.Content)
+    assert isinstance(
+        res.content_type, attr.fields(schema.Content).content_type.type)
+
+
+@mock.patch('galaxy_importer.loaders.Popen')
+def test_run_ansible_doc(mocked_popen, loader_module):
+    mocked_popen.return_value.communicate.return_value = (
+        'expected output', '')
+    mocked_popen.return_value.returncode = 0
+    res = loader_module._run_ansible_doc()
+    assert res == 'expected output'
+
+
+@mock.patch('galaxy_importer.loaders.Popen')
+def test_run_ansible_doc_exception(mocked_popen, loader_module):
+    mocked_popen.return_value.communicate.return_value = (
+        'output', 'error that causes exception')
+    mocked_popen.return_value.returncode = 1
+    res = loader_module._run_ansible_doc()
+    assert not res
+
+
+@mock.patch.object(loaders.PluginLoader, '_run_ansible_doc')
+def test_get_doc_strings(mocked_run_ansible_doc, loader_module):
+    mocked_run_ansible_doc.return_value = ANSIBLE_DOC_OUTPUT
+    doc_strings = loader_module._get_doc_strings()
+
+    assert doc_strings['doc']['version_added'] == '2.8'
+    assert doc_strings['doc']['description'] == \
+        ['Sample module for testing.']
+
+
+def test_ansible_doc_unsupported_type():
+    loader_action = loaders.PluginLoader(
+        content_type=constants.ContentType.ACTION_PLUGIN,
+        rel_path='plugins/action/my_plugin.py',
+        root='/tmp/tmpiskt5e2n')
+    assert constants.ContentType.ACTION_PLUGIN.value not in \
+        loaders.ANSIBLE_DOC_SUPPORTED_TYPES
+    assert not loader_action._get_doc_strings()
+
+
+@mock.patch.object(loaders.PluginLoader, '_run_ansible_doc')
+def test_load(mocked_run_ansible_doc, loader_module):
+    mocked_run_ansible_doc.return_value = ANSIBLE_DOC_OUTPUT
+    res = loader_module.load()
+    assert isinstance(res, schema.Content)
+    assert res.name == 'my_sample_module'
+    assert res.content_type == constants.ContentType.MODULE
+    assert res.readme_file is None
+    assert res.readme_html is None
+    assert res.description == 'Sample module for testing'
+    assert res.doc_strings['doc']['version_added'] == '2.8'
+
+
+@mock.patch.object(loaders.PluginLoader, '_run_ansible_doc')
+def test_ansible_doc_no_output(mocked_run_ansible_doc, loader_module):
+    mocked_run_ansible_doc.return_value = ''
+    loader_module._get_doc_strings()
+    assert loader_module.doc_strings is None
+
+
+@mock.patch.object(loaders.PluginLoader, '_run_ansible_doc')
+def test_ansible_doc_missing_key(mocked_run_ansible_doc, loader_module):
+    ansible_doc_output = """
+        {"wrong_key_name": {
+            "doc": {
+                "description": [
+                    "Sample module for testing."
+                ]
+            }
+        }}
+    """
+    mocked_run_ansible_doc.return_value = ansible_doc_output
+    loader_module._get_doc_strings()
+    assert loader_module.doc_strings is None
+
+
+ANSIBLELINT_TASK_OK = """---
+- name: Add mongodb repo apt_key
+  become: true
+  apt_key: keyserver=hkp
+  until: result.rc == 0
+"""
+
+ANSIBLELINT_TASK_SUDO_WARN = """---
+- name: edit vimrc
+  sudo: true
+  lineinfile:
+    path: /etc/vimrc
+    line: '# added via ansible'
+"""
+
+
+@pytest.fixture
+def temp_root():
+    try:
+        tmp = tempfile.mkdtemp()
+        yield tmp
+    finally:
+        shutil.rmtree(tmp)
+
+
+def test_ansiblelint_file(loader_role):
+    with tempfile.NamedTemporaryFile('w') as fp:
+        fp.write(ANSIBLELINT_TASK_SUDO_WARN)
+        fp.flush()
+        res = list(loader_role._lint_role(fp.name))
+    assert 'deprecated sudo' in ' '.join(res).lower()
+
+
+def test_ansiblelint_role(temp_root, loader_role):
+    task_dir = os.path.join(temp_root, 'tasks')
+    os.makedirs(task_dir)
+    with open(os.path.join(task_dir, 'main.yml'), 'w') as fp:
+        fp.write(ANSIBLELINT_TASK_SUDO_WARN)
+        fp.flush()
+        res = list(loader_role._lint_role(temp_root))
+    assert 'deprecated sudo' in ' '.join(res).lower()
+    lint_output_path = re.compile(r'^tmp.+\/tasks\/main\.yml.+$')
+    assert re.match(lint_output_path, res[0])
+
+
+def test_ansiblelint_role_no_warn(temp_root, loader_role):
+    task_dir = os.path.join(temp_root, 'tasks')
+    os.makedirs(task_dir)
+    with open(os.path.join(task_dir, 'main.yml'), 'w') as fp:
+        fp.write(ANSIBLELINT_TASK_OK)
+        fp.flush()
+        res = list(loader_role._lint_role(temp_root))
+    assert res == []
+
+
+@mock.patch('galaxy_importer.loaders.Popen')
+def test_ansible_lint_exception(mocked_popen, loader_role):
+    mocked_popen.return_value.stdout = ''
+    mocked_popen.return_value.wait.return_value = 1
+    res = list(loader_role._lint_role('.'))
+    assert 'Exception running ansible-lint' in res[0]
