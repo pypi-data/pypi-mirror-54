4a78a353ec0fc392f4d8df616f49ecedeee4ff47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:13:09 +0000

    Add package packaging-tutorial-ramon/0.0.1/packaging_tutorial_ramon-0.0.1.tar.gz

---
+++ packages/packaging-tutorial-ramon/packaging_tutorial_ramon-0.0.1.tar.gz/packaging_tutorial_ramon-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='packaging_tutorial_ramon',
+    version='0.0.1',
+    packages=['example_pkg'],
+    url='http://ramon.wang',
+    license='MIT License',
+    author='ramon',
+    author_email='ramon_wang@hotmail.com',
+    description='Purly testing packaging.'
+)
