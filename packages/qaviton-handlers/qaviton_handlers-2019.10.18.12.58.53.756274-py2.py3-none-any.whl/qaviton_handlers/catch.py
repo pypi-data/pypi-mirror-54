bdbed4432496a66ae97b3c4232103f07fd7f3272
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:46 +0000

    Add package qaviton-handlers/2019.10.18.12.58.53.756274/qaviton_handlers-2019.10.18.12.58.53.756274-py2.py3-none-any.whl

---
+++ packages/qaviton-handlers/qaviton_handlers-2019.10.18.12.58.53.756274-py2.py3-none-any.whl/qaviton_handlers/catch.py
@@ -0,0 +1,85 @@
+from logging import Logger
+from qaviton_handlers.utils.error import Error
+
+
+class Catch:
+    """in cases where you find yourself needing to ignore errors now so you can handle them later
+    usage:
+        from qaviton_handlers.catch import Catch
+        catch = Catch(store=True)
+
+        # catch an error
+        try:
+            1 + '1'
+        except Exception as e:
+            catch(e)
+
+        # a cleaner syntax
+        with catch:
+            1 + '1'
+            2 + '2'
+
+        # ignore the error
+        with Catch():
+            5 * 'e'
+
+        print(f"caught {catch.count} errors")
+        print(f"caught first {catch.first}")
+        print(f"caught last {catch.last}")
+    """
+    def __init__(self, exceptions=Exception, store=False, log: Logger = None):
+        """ :param exceptions: an exception or tuple of exceptions to catch """
+        self.exceptions = exceptions if isinstance(exceptions, tuple) else (exceptions,)
+        self.log = log
+        self.store = store
+        self.stack = set()
+
+    def __call__(self, error):
+        if type(error) in self.exceptions:
+            if self.store:
+                return self.handler(error)
+            return
+        raise error
+
+    def __len__(self):
+        return len(self.stack)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type:
+            self.handler(exc_val)
+            return True
+
+    @property
+    def count(self):
+        return len(self)
+
+    @property
+    def first(self):
+        return min(self.stack)
+
+    @property
+    def last(self):
+        return max(self.stack)
+
+    def handler(self, e):
+        self.stack.add(Error(e))
+        return self
+
+    def clean(self):
+        self.__init__()
+
+    def raise_e(self, error=None):
+        if error:
+            self(error)
+            raise error
+        for error in self.stack:
+            raise error.e
+
+    def raise_first(self):
+        raise self.first.e
+
+    def raise_last(self):
+        raise self.last.e
---
+++ packages/qaviton-handlers/qaviton_handlers-2019.10.18.12.58.53.756274-py2.py3-none-any.whl/qaviton_handlers/try_functions.py
@@ -0,0 +1,36 @@
+from logging import Logger
+
+
+def handle(f, handler, exceptions=Exception, logger: Logger = None):
+    try:
+        return f()
+    except exceptions as e:
+        if logger:
+            logger.exception(e)
+        return handler()
+
+
+def try_to(f, *args, exceptions=Exception, logger: Logger = None, kwargs: dict = None):
+    try:
+        return f(*args, **kwargs if kwargs else {})
+    except exceptions as e:
+        if logger:
+            logger.exception(e)
+        return e
+
+
+def try_or_none(f, *args, exceptions=Exception, logger: Logger = None, kwargs: dict = None):
+    try:
+        return f(*args, **kwargs if kwargs else {})
+    except exceptions as e:
+        if logger:
+            logger.exception(e)
+        return
+
+
+def multi_try(*f, exceptions=Exception, logger: Logger = None):
+    return try_to(lambda: [F() for F in f], exceptions=exceptions, logger=logger)
+
+
+def multi_try_no_break(*f, exceptions=Exception, logger: Logger = None):
+    return [try_to(F, exceptions=exceptions, logger=logger) for F in f]
