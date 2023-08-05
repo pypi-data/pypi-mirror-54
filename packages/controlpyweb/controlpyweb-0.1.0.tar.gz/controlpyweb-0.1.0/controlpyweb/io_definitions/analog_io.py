e85936ab6c034ca2e502fc76e1de1b90edc07eb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:22 +0000

    Add package controlpyweb/0.1.0/controlpyweb-0.1.0.tar.gz

---
+++ packages/controlpyweb/controlpyweb-0.1.0.tar.gz/controlpyweb-0.1.0/controlpyweb/io_definitions/analog_io.py
@@ -0,0 +1,39 @@
+"""
+Module Analog IO
+The classes within this module, AnalogIn and AnalogOut represent analog IO or registers within the Web IO module. They
+provide specific implementation details for dealing with each.
+"""
+
+from controlpyweb.io_definitions.single_io import SingleIO
+from controlpyweb.io_definitions.io_out import IOOut
+from controlpyweb.abstract_reader_writer import AbstractReaderWriter
+
+
+class AnalogIn(SingleIO):
+    def __init__(self, name: str, addr: str, default: float = 0.0,
+                 reader: AbstractReaderWriter = None, *args, **kwargs):
+        super().__init__(name, addr, default, reader, *args, **kwargs)
+
+    @staticmethod
+    def _convert_type(value):
+        return float(value)
+
+    def __mod__(self, other):
+        if hasattr(other, 'value'):
+            other = other.value
+        return self.value % other
+
+
+class AnalogOut(IOOut, AnalogIn):
+    def __init__(self, name: str, addr: str, default: float = 0.0,
+                 reader: AbstractReaderWriter = None, *args, **kwargs):
+        super().__init__(name, addr, default, reader, *args, **kwargs)
+
+    @staticmethod
+    def _convert_type(value):
+        return float(value)
+
+    def __mod__(self, other):
+        if hasattr(other, 'value'):
+            other = other.value
+        return self.value % other
---
+++ packages/controlpyweb/controlpyweb-0.1.0.tar.gz/controlpyweb-0.1.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="controlpyweb",
+    version="0.1.0",
+    author="Steve Jackson",
+    author_email="washad@gmail.com",
+    description="A project to facilitate easy read/write to the ControlByWeb line of Automation/SCADA IO products.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/washad/ControlPyWeb",
+    packages=setuptools.find_packages(),
+    install_requires=['wheel', 'requests', 'str2bool'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
