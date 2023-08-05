4bfe90117df277a3909f0ab765f3b5a2450bff05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:29 +0000

    Add package httpstan/0.10.1/httpstan-0.10.1-cp37-cp37m-macosx_10_6_intel.whl

---
+++ packages/httpstan/httpstan-0.10.1-cp37-cp37m-macosx_10_6_intel.whl/httpstan/__main__.py
@@ -0,0 +1,26 @@
+"""Top-level script environment for httpstan.
+
+``python3 -m httpstan`` starts a server listening on ``localhost``.
+
+As httpstan is intended for use alongside a frontend, the frontend
+will typically do everything in ``main()`` function.
+"""
+import sys
+import time
+
+import httpstan.main
+
+
+def main() -> None:
+    server = httpstan.main.Server()
+    server.start()
+    print(f"httpstan serving on {server.host}:{server.port}", file=sys.stderr)
+    try:
+        while True:
+            time.sleep(0.1)
+    finally:
+        server.stop()
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/httpstan/httpstan-0.10.1-cp37-cp37m-macosx_10_6_intel.whl/httpstan/routes.py
@@ -0,0 +1,48 @@
+"""Routes for httpstan.
+
+Routes for the HTTP server are defined here.
+"""
+import json
+
+import apispec
+import aiohttp.web
+
+import httpstan
+import httpstan.schemas as schemas
+import httpstan.views as views
+
+
+def setup_routes(app: aiohttp.web.Application) -> None:
+    """Add routes to Application.
+
+    Arguments:
+        app (aiohttp.Application): Application to which routes should be added.
+
+    """
+    app.router.add_get("/v1/health", views.handle_health)
+    app.router.add_post("/v1/models", views.handle_models)
+    app.router.add_post("/v1/models/{model_id}/params", views.handle_show_params)
+    app.router.add_post("/v1/models/{model_id}/fits", views.handle_create_fit)
+    app.router.add_get("/v1/models/{model_id}/fits/{fit_id}", views.handle_get_fit)
+    app.router.add_get("/v1/operations/{operation_id}", views.handle_get_operation)
+
+
+def openapi_spec() -> str:
+    """Return OpenAPI (fka Swagger) spec for API."""
+    spec = apispec.APISpec(
+        title="httpstan API", version=httpstan.__version__, plugins=["apispec.ext.marshmallow"]
+    )
+    spec.add_path(path="/v1/health", view=views.handle_health)
+    spec.add_path(path="/v1/models", view=views.handle_models)
+    spec.add_path(path="/v1/models/{model_id}/params", view=views.handle_show_params)
+    spec.add_path(path="/v1/models/{model_id}/fits", view=views.handle_create_fit)
+    spec.add_path(path="/v1/models/{model_id}/fits/{fit_id}", view=views.handle_get_fit)
+    spec.add_path(path="/v1/operations/{operation_id}", view=views.handle_get_operation)
+    spec.definition("CreateModelRequest", schema=schemas.CreateModelRequest)
+    spec.definition("CreateFitRequest", schema=schemas.CreateFitRequest)
+    spec.definition("Status", schema=schemas.Status)
+    spec.definition("Fit", schema=schemas.Fit)
+    spec.definition("Model", schema=schemas.Model)
+    spec.definition("Operation", schema=schemas.Operation)
+    spec.definition("Parameter", schema=schemas.Parameter)
+    return json.dumps(spec.to_dict())
