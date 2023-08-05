984051f100014c1779200fbe097067cd104a5eb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:24 +0000

    Add package stellargraph/0.7.3/stellargraph-0.7.3.tar.gz

---
+++ packages/stellargraph/stellargraph-0.7.3.tar.gz/stellargraph-0.7.3/stellargraph/__init__.py
@@ -0,0 +1,43 @@
+"""
+Stellar Machine Learning Library
+
+"""
+
+__all__ = [
+    "data",
+    "layer",
+    "mapper",
+    "utils",
+    "StellarDiGraph",
+    "StellarGraph",
+    "__version__",
+]
+
+# Version
+from .version import __version__
+
+# Import modules
+from stellargraph import mapper, layer, utils
+
+# Top-level imports
+from stellargraph.core.graph import StellarGraph, StellarDiGraph
+from stellargraph.core.schema import GraphSchema
+from stellargraph.utils.calibration import TemperatureCalibration, IsotonicCalibration
+from stellargraph.utils.calibration import (
+    plot_reliability_diagram,
+    expected_calibration_error,
+)
+from stellargraph.utils.ensemble import Ensemble
+
+# Custom layers for keras deserialization:
+custom_keras_layers = {
+    "GraphConvolution": layer.GraphConvolution,
+    "GraphAttention": layer.GraphAttention,
+    "GraphAttentionSparse": layer.GraphAttentionSparse,
+    "SqueezedSparseConversion": layer.SqueezedSparseConversion,
+    "MeanAggregator": layer.graphsage.MeanAggregator,
+    "MaxPoolingAggregator": layer.graphsage.MaxPoolingAggregator,
+    "MeanPoolingAggregator": layer.graphsage.MeanPoolingAggregator,
+    "AttentionalAggregator": layer.graphsage.AttentionalAggregator,
+    "MeanHinAggregator": layer.hinsage.MeanHinAggregator,
+}
