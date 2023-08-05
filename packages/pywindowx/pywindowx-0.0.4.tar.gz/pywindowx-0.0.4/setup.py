b77b596f2df91e33f8b35ff78a047a74cd704423
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:36 +0000

    Add package pywindowx/0.0.4/pywindowx-0.0.4.tar.gz

---
+++ packages/pywindowx/pywindowx-0.0.4.tar.gz/pywindowx-0.0.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='pywindowx',
+    author='Marcin Miklitz',
+    author_email='marcin.miklitz@gmail.com',
+    url='https://www.github.com/marcinmiklitz/pywindow',
+    version='0.0.4',
+    packages=['pywindow'],
+    install_requires=[
+        'numpy',
+        'scipy',
+        'scikit-learn',
+    ],
+)
