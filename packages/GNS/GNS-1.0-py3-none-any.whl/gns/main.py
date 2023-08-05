448e184ff0b11adce92399d391e973f3f6949e3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:12 +0000

    Add package GNS/1.0/GNS-1.0-py3-none-any.whl

---
+++ packages/GNS/GNS-1.0-py3-none-any.whl/gns/main.py
@@ -0,0 +1,13 @@
+#import standard modules 
+
+#import custom modules
+import nested_run
+import plotting
+import input
+###########main function
+
+def main():
+	#call functions in nested_run module here
+
+if __name__ == '__main__': 
+	main()
