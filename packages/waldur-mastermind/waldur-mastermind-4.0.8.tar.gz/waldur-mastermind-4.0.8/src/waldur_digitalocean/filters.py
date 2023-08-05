eeeac3cd1799d4126f8adf819d80efd95369f5d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:23 +0000

    Add package waldur-mastermind/4.0.8/waldur-mastermind-4.0.8.tar.gz

---
+++ packages/waldur-mastermind/waldur-mastermind-4.0.8.tar.gz/waldur-mastermind-4.0.8/src/waldur_digitalocean/filters.py
@@ -0,0 +1,33 @@
+from django_filters import OrderingFilter, CharFilter
+
+from waldur_core.structure import filters as structure_filters
+
+from . import models
+
+
+class ImageFilter(structure_filters.BaseServicePropertyFilter):
+
+    o = OrderingFilter(fields=('distribution', 'type'))
+
+    class Meta(object):
+        model = models.Image
+        fields = structure_filters.BaseServicePropertyFilter.Meta.fields + ('distribution', 'type')
+
+
+class SizeFilter(structure_filters.BaseServicePropertyFilter):
+
+    class Meta(object):
+        model = models.Size
+        fields = structure_filters.BaseServicePropertyFilter.Meta.fields + ('cores', 'ram', 'disk')
+
+
+class RegionFilter(structure_filters.BaseServicePropertyFilter):
+    class Meta(structure_filters.BaseServicePropertyFilter.Meta):
+        model = models.Region
+
+
+class DropletFilter(structure_filters.BaseResourceFilter):
+    external_ip = CharFilter(name='ip_address')
+
+    class Meta(structure_filters.BaseResourceFilter.Meta):
+        model = models.Droplet
