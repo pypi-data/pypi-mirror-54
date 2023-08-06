a9833606dd5f04f8c4fff08d63287586567f7d9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:03 +0000

    Add package edxval/1.1.29/edxval-1.1.29.tar.gz

---
+++ packages/edxval/edxval-1.1.29.tar.gz/edxval-1.1.29/edxval/migrations/0003_coursevideo_is_hidden.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from __future__ import absolute_import
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('edxval', '0002_data__default_profiles'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='coursevideo',
+            name='is_hidden',
+            field=models.BooleanField(default=False, help_text=b'Hide video for course.'),
+        ),
+    ]
---
+++ packages/edxval/edxval-1.1.29.tar.gz/edxval-1.1.29/edxval/migrations/0010_add_video_as_foreign_key.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from __future__ import absolute_import
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('edxval', '0009_auto_20171127_0406'),
+    ]
+
+    operations = [
+        migrations.AlterUniqueTogether(
+            name='videotranscript',
+            unique_together=set([('language_code',)]),
+        ),
+        migrations.RemoveField(
+            model_name='videotranscript',
+            name='video_id',
+        ),
+        migrations.AddField(
+            model_name='videotranscript',
+            name='video',
+            field=models.ForeignKey(related_name='video_transcripts', to='edxval.Video', null=True, on_delete = models.CASCADE),
+        ),
+        migrations.AlterUniqueTogether(
+            name='videotranscript',
+            unique_together=set([('video', 'language_code')]),
+        ),
+    ]
