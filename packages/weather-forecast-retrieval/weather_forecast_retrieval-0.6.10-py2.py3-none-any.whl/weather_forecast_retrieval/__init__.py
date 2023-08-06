896375b34d59dc498de9bb6d514ee031161675d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:06 +0000

    Add package weather-forecast-retrieval/0.6.10/weather_forecast_retrieval-0.6.10-py2.py3-none-any.whl

---
+++ packages/weather-forecast-retrieval/weather_forecast_retrieval-0.6.10-py2.py3-none-any.whl/weather_forecast_retrieval/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Weather Forecast Retrieval."""
+
+__author__ = """Scott Havens"""
+__email__ = 'scott.havens@ars.usda.gov'
+__version__ = '0.6.10'
+
+from . import hrrr
+from . import rap
+from . import utils
+from . import hrrr_archive
