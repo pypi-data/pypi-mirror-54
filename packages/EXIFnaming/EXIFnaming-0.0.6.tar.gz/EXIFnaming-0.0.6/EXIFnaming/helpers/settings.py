e751049f4d76aac71d639586a20e786e1efc838b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:10 +0000

    Add package EXIFnaming/0.0.6/EXIFnaming-0.0.6.tar.gz

---
+++ packages/EXIFnaming/EXIFnaming-0.0.6.tar.gz/EXIFnaming-0.0.6/EXIFnaming/helpers/settings.py
@@ -0,0 +1,11 @@
+includeSubdirs = True
+encoding_format = "latin"
+image_types = (".JPG", ".jpg", ".tif", ".tiff", ".hdr", ".exr", ".ufo", ".fpx", ".RW2", ".Raw")
+video_types = (".MP4", ".mp4")
+hdr_program = "franzis HDR projects"
+panorama_program = "Hugin"
+photographer = "Marco Volkert"
+standard_kamera = "DMC-TZ101"
+googlemaps_api_key = ""
+## if unset is same directory as settings
+exiftool_directory = r""
