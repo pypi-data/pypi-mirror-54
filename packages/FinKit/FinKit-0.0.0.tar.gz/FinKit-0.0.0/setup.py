365ba48b316841fd6311f4170813bfd63c4b5a32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:38 +0000

    Add package FinKit/0.0.0/FinKit-0.0.0.tar.gz

---
+++ packages/FinKit/FinKit-0.0.0.tar.gz/FinKit-0.0.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="FinKit",
+    version="0.0.0",
+    description="",
+    long_description="",
+    author="",
+    author_email="",
+    maintainer="Maximiliano Greco",
+    maintainer_email="mmngreco@gmail.com",
+    url=None,
+    download_url=None,
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.5",
+)
