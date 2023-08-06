e8ce5b364c750a69b96ceed2b8d6c537915f292e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:46 +0000

    Add package b2luigi/0.4.3/b2luigi-0.4.3.tar.gz

---
+++ packages/b2luigi/b2luigi-0.4.3.tar.gz/b2luigi-0.4.3/tests/core/test_settings.py
@@ -0,0 +1,61 @@
+import json
+import os
+
+from ..helpers import B2LuigiTestCase
+
+import b2luigi
+
+
+class TaskTestCase(B2LuigiTestCase):
+    def setUp(self):
+        super().setUp()
+
+        b2luigi.clear_setting("my_setting")
+
+    def test_set_by_function(self):
+        self.assertRaises(ValueError, b2luigi.get_setting, "my_setting")
+
+        self.assertEqual("default", b2luigi.get_setting("my_setting", "default"))
+
+        b2luigi.set_setting("my_setting", "my value")
+
+        self.assertEqual("my value", b2luigi.get_setting("my_setting"))
+        self.assertEqual("my value", b2luigi.get_setting("my_setting", "default"))
+
+    def test_set_by_file(self):
+        with open("settings.json", "w") as f:
+            json.dump({"my_setting": "my file value"}, f)
+
+        self.assertEqual("my file value", b2luigi.get_setting("my_setting"))
+
+        b2luigi.set_setting("my_setting", "my value")
+
+        self.assertEqual("my value", b2luigi.get_setting("my_setting"))
+
+    def test_set_by_parent_file(self):
+        os.mkdir("child_path")
+        os.chdir("child_path")
+
+        with open("../settings.json", "w") as f:
+            json.dump({"my_setting": "my parent value"}, f)
+
+        self.assertEqual("my parent value", b2luigi.get_setting("my_setting"))
+
+        with open("settings.json", "w") as f:
+            json.dump({"my_setting": "my child value"}, f)
+
+        self.assertEqual("my child value", b2luigi.get_setting("my_setting"))
+
+    def test_set_by_task_or_file(self):
+        with open("settings.json", "w") as f:
+            json.dump({"my_setting": "my file value"}, f)
+
+        b2luigi.set_setting("my_second_setting", "my value")
+
+        task = b2luigi.Task()
+        setattr(task, "my_third_setting", "my task value")
+
+        self.assertEqual("my file value", b2luigi.get_setting("my_setting", task=task))
+        self.assertEqual("my value", b2luigi.get_setting("my_second_setting", task=task))
+        self.assertEqual("my task value", b2luigi.get_setting("my_third_setting", task=task))
+        
