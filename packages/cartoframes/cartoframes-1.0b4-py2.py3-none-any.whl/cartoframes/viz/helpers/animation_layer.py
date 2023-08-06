eb547897cf67f956ac17b0964e79144486827a56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:55 +0000

    Add package cartoframes/1.0b4/cartoframes-1.0b4-py2.py3-none-any.whl

---
+++ packages/cartoframes/cartoframes-1.0b4-py2.py3-none-any.whl/cartoframes/viz/helpers/animation_layer.py
@@ -0,0 +1,76 @@
+from __future__ import absolute_import
+
+from .. import defaults
+from ..layer import Layer
+
+
+def animation_layer(
+        source, value, title='', duration=None, fade=None, size=None,
+        color=None, opacity=None, stroke_color=None, stroke_width=None,
+        widget_type='time-series', description=''):
+    """Helper function for quickly creating an animated map.
+
+    Args:
+        source (:py:class:`Dataset <cartoframes.data.Dataset>` or str): Dataset
+          or text representing a table or query associated with user account.
+        value (str): Column to symbolize by.
+        title (str, optional): Title of widget.
+        color (str, optional): Hex value, rgb expression, or other valid
+          CARTO VL color. Default is '#EE5D5A' for point geometries,
+          '#4CC8A3' for lines and #826DBA for polygons.
+        size (int, optional): Size of point or line features.
+        opacity (int, optional): Opacity value for point color and line features.
+          Default is '0.8'.
+        stroke_width (int, optional): Size of the stroke on point features.
+        stroke_color (str, optional): Color of the stroke on point features.
+          Default is '#222'.
+        widget_type (str, optional): Type of animation widget: "animation" or "time-series".
+          The default is "time-series".
+        description (str, optional): Description text placed under the widget title.
+        fade (string, optional): Animation fade with the format: "(fade in, fade out)". Default is (1, 1).
+
+    Returns:
+        cartoframes.viz.Layer: Layer styled by `value`. Includes Widget `value`.
+    """
+
+    return Layer(
+        source,
+        style={
+            'point': {
+                'width': '{0}'.format(
+                    size or defaults.STYLE['point']['width']),
+                'color': 'opacity({0}, {1})'.format(
+                    color or '#EE4D5A', opacity or '0.8'),
+                'strokeWidth': '{0}'.format(
+                    stroke_width or defaults.STYLE['point']['strokeWidth']),
+                'strokeColor': '{0}'.format(
+                    stroke_color or defaults.STYLE['point']['strokeColor']),
+                'filter': 'animation(linear(${0}), {1}, fade{2})'.format(
+                    value, duration or 20, fade or '(1, 1)')
+            },
+            'line': {
+                'width': '{0}'.format(
+                    size or defaults.STYLE['line']['width']),
+                'color': 'opacity({0}, {1})'.format(
+                    color or '#4CC8A3', opacity or '0.8'),
+                'filter': 'animation(linear(${0}), {1}, fade{2})'.format(
+                    value, duration or 20, fade or '(1, 1)')
+            },
+            'polygon': {
+                'color': 'opacity({0}, {1})'.format(
+                    color or defaults.STYLE['polygon']['color'], opacity or '0.9'),
+                'strokeColor': '{0}'.format(
+                    stroke_color or defaults.STYLE['polygon']['strokeColor']),
+                'strokeWidth': '{0}'.format(
+                    stroke_width or defaults.STYLE['polygon']['strokeWidth']),
+                'filter': 'animation(linear(${0}), {1}, fade{2})'.format(
+                    value, duration or 20, fade or '(1, 1)')
+            }
+        },
+        widgets=[{
+            'type': widget_type,
+            'value': value,
+            'title': title,
+            'description': description
+        }]
+    )
---
+++ packages/cartoframes/cartoframes-1.0b4-py2.py3-none-any.whl/cartoframes/viz/source.py
@@ -0,0 +1,144 @@
+from __future__ import absolute_import
+
+from . import defaults
+from ..data import Dataset
+from ..data.dataset.registry.base_dataset import BaseDataset
+from ..utils.utils import get_query_bounds, get_geodataframe_bounds, encode_geodataframe
+
+
+class SourceType:
+    QUERY = 'Query'
+    GEOJSON = 'GeoJSON'
+
+
+class Source(object):
+    """Source
+
+    Args:
+        data (str, geopandas.GeoDataFrame, pandas.DataFrame,
+          :py:class:`Dataset <cartoframes.data.Dataset>` ): a table name,
+          SQL query, GeoJSON file, GeoDataFrame, DataFrame, or Dataset object.
+        credentials (:py:class:`Credentials <cartoframes.auth.Credentials>`, optional):
+          A Credentials instance. If not provided, the credentials will be automatically
+          obtained from the default credentials if available.
+        bounds (dict or list, optional): a dict with `west`, `south`, `east`, `north`
+          keys, or an array of floats in the following structure: [[west,
+          south], [east, north]]. If not provided the bounds will be automatically
+          calculated to fit all features.
+
+    Example:
+
+        Table name.
+
+        .. code::
+
+            from cartoframes.auth import set_default_credentials
+            from cartoframes.viz import Source
+
+            set_default_credentials('your_user_name', 'your api key')
+
+            Source('table_name')
+
+        SQL query.
+
+        .. code::
+
+            from cartoframes.auth import set_default_credentials
+            from cartoframes.viz import Source
+
+            set_default_credentials('your_user_name', 'your api key')
+
+            Source('SELECT * FROM table_name')
+
+        GeoJSON file.
+
+        .. code::
+
+            from cartoframes.viz import Source
+
+            Source('path/to/file.geojson')
+
+        Dataset object.
+
+        .. code::
+
+            from cartoframes.viz import Source
+            from cartoframes.data import Dataset
+
+            set_default_credentials('your_user_name', 'your api key')
+
+            ds = Dataset('table_name')
+
+            Source(ds)
+
+        Setting the credentials.
+
+        .. code::
+
+            from cartoframes.auth import Credentials
+            from cartoframes.viz import Source
+
+            credentials = Credentials('your_user_name', 'your api key')
+
+            Source('table_name', credentials)
+
+        Setting the bounds.
+
+        .. code::
+
+            from cartoframes.auth import set_default_credentials
+            from cartoframes.viz import Source
+
+            set_default_credentials('your_user_name', 'your api key')
+
+            bounds = {
+                'west': -10,
+                'east': 10,
+                'north': -10,
+                'south': 10
+            }
+
+            Source('table_name', bounds=bounds)
+    """
+
+    def __init__(self, data, credentials=None, bounds=None, schema=None):
+        if isinstance(data, Dataset):
+            self.dataset = data
+        else:
+            self.dataset = Dataset(data, credentials, schema)
+
+        self._init_source_dataset(bounds)
+
+    def get_geom_type(self):
+        return self.dataset.compute_geom_type() or BaseDataset.GEOM_TYPE_POINT
+
+    def get_credentials(self):
+        credentials = self.dataset.credentials
+        if credentials:
+            return {
+                # CARTO VL requires a username but CARTOframes allows passing only the base_url.
+                # That's why 'user' is used by default if username is empty.
+                'username': credentials.username or 'user',
+                'api_key': credentials.api_key,
+                'base_url': credentials.base_url
+            }
+        else:
+            return defaults.CREDENTIALS
+
+    def _init_source_dataset(self, bounds):
+        if self.dataset.is_local():
+            gdf = self.dataset.get_geodataframe()
+            self.type = SourceType.GEOJSON
+            self.query = encode_geodataframe(gdf)
+            self.bounds = bounds or self._compute_geojson_bounds(gdf)
+        else:
+            self.type = SourceType.QUERY
+            self.query = self.dataset.get_query()
+            self.bounds = bounds or self._compute_query_bounds()
+
+    def _compute_query_bounds(self):
+        context = self.dataset._strategy._context
+        return get_query_bounds(context, self.query)
+
+    def _compute_geojson_bounds(self, gdf):
+        return get_geodataframe_bounds(gdf)
