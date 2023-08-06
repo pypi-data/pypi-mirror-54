bf47415aa991f7814283a0748db5a22979c37f87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:48 +0000

    Add package liquipediapy2/2.0.1/liquipediapy2-2.0.1.tar.gz

---
+++ packages/liquipediapy2/liquipediapy2-2.0.1.tar.gz/liquipediapy2-2.0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r",encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='liquipediapy2',
+    version = '2.0.1',
+    description= 'api for liquipedia.net',
+    author = 'c00kie17',
+    author_email = 'anshul1708@gmail.com',
+    url = 'https://github.com/LiuZHolmes/liquipediapy',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'beautifulsoup4>=4.6.3',
+        'requests>=2.20.1',
+        'urllib3>=1.23',
+        'lxml>=4.2.4'
+
+    ],
+)
