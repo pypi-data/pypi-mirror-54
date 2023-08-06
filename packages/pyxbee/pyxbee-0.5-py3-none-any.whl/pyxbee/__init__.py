965f40490e99dd4c08a1686e333d02ddc1eeedf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:17:07 +0000

    Add package pyxbee/0.5/pyxbee-0.5-py3-none-any.whl

---
+++ packages/pyxbee/pyxbee-0.5-py3-none-any.whl/pyxbee/__init__.py
@@ -0,0 +1,16 @@
+from .base import Bike, Client, Server, Taurus
+from .packet import Packet
+from .exception import *
+
+__all__ = [
+    'Bike',
+    'Client',
+    'Server',
+    'Taurus',
+    'Packet',
+    'base',
+    'packet',
+    'exception'
+]
+
+__version__ = '0.5'
