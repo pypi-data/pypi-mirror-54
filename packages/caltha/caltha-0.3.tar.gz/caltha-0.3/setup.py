b881608138e9d62337dcfb9e893469af2930e6af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:07 +0000

    Add package caltha/0.3/caltha-0.3.tar.gz

---
+++ packages/caltha/caltha-0.3.tar.gz/caltha-0.3/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+# Author: Jasper Boom
+
+# Prequisites:
+# - conda install -c bioconda vsearch=2.14.1
+
+# Imports
+import setuptools
+
+with open("README.md", "r") as oisReadme:
+    strDescription = oisReadme.read()
+
+setuptools.setup(
+    name="caltha",
+    version="0.3",
+    description="A python package to process UMI tagged mixed amplicon metabarcoding data.",
+    long_description=strDescription,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JasperBoom/caltha",
+    author="Jasper Boom",
+    author_email="jboom@infernum.nl",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Scientific/Engineering :: Bio-Informatics"
+    ],
+    keywords="UMI Metabarcoding",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.7",
+    install_requires=[
+        "pandas>=0.25.1"
+    ],
+    project_urls={
+        "Source": "https://github.com/JasperBoom/caltha/tree/master/src",
+        "Tracker": "https://github.com/JasperBoom/caltha/issues",
+        "Documentation": "https://jasperboom.github.io/caltha/"
+    },
+    scripts=["src/caltha"]
+)
---
+++ packages/caltha/caltha-0.3.tar.gz/caltha-0.3/src/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# Author: Jasper Boom
+
+# Prequisites:
+# - conda install -c bioconda vsearch=2.14.1
+
+name = "caltha"
