c167e5e3a335f2d181f25ec8f73ab29c16996cdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:58 +0000

    Add package rammon/0.2.0/rammon-0.2.0.tar.gz

---
+++ packages/rammon/rammon-0.2.0.tar.gz/rammon-0.2.0/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="rammon",
+    version="0.2.0",
+    author="Jack Adamson",
+    author_email="jack@mrfluffybunny.com",
+    description="A low-memory alert daemon",
+    install_requires=["pydbus", "python-daemon", "psutil", "dataclasses"],
+    entry_points={"console_scripts": ["rammon = rammon.__main__:main"]},
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jackadamson/rammon",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+    ],
+    python_requires=">=3.6",
+)
