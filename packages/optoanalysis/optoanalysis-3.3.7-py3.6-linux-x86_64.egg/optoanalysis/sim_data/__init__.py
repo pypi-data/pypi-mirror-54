82393d9d8360440edb55480039adfe29442dde2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:38 +0000

    Add package optoanalysis/3.3.7/optoanalysis-3.3.7-py3.6-linux-x86_64.egg

---
+++ packages/optoanalysis/optoanalysis-3.3.7-py3.6-linux-x86_64.egg/optoanalysis/sim_data/__init__.py
@@ -0,0 +1,15 @@
+"""
+optoanalysis.sim_data
+=====================
+
+Functions for generating simulated data.
+
+"""
+
+# init file
+
+from .sim_data import *
+#from .sim_data_deprecated import *
+
+
+
