b8981d41c9abbb1ea779bcf67b88a68e9015a530
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:09 +0000

    Add package balabala/7.7.7/balabala-7.7.7.tar.gz

---
+++ packages/balabala/balabala-7.7.7.tar.gz/balabala-7.7.7/balabala/__init__.py
@@ -0,0 +1,21 @@
+'''
+@File       :   __init__.py
+@Author     :   Jiang Fubang
+@Time       :   2019-10-28 17:18
+@Version    :   1.0
+@Contact    :   luckybang@163.com
+@Dect       :   None
+'''
+
+import JFB
+
+def str2dict(s):
+    eachs = s.split("\n")
+    d = {}
+    for each in eachs:
+        if each.strip():
+            each_list = each.split(":")
+            key = "".join([item.strip() for item in each_list[:-1]])
+            value = each_list[-1].strip()
+            d[key] = value
+    return d
---
+++ packages/balabala/balabala-7.7.7.tar.gz/balabala-7.7.7/balabala/code1.py
@@ -0,0 +1,8 @@
+'''
+@File       :   file1.py
+@Author     :   Jiang Fubang
+@Time       :   2019-10-28 17:17
+@Version    :   1.0
+@Contact    :   luckybang@163.com
+@Dect       :   None
+'''
