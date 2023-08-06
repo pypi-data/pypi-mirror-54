45fa342d9d763afc4b44edc13e3b7eb8e7b955e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:21 +0000

    Add package pyro-api/0.1.1/pyro-api-0.1.1.tar.gz

---
+++ packages/pyro-api/pyro-api-0.1.1.tar.gz/pyro-api-0.1.1/pyroapi/__init__.py
@@ -0,0 +1,12 @@
+from pyroapi.dispatch import distributions, handlers, infer, ops, optim, pyro, pyro_backend, register_backend
+
+__all__ = [
+    'distributions',
+    'handlers',
+    'infer',
+    'ops',
+    'optim',
+    'pyro',
+    'pyro_backend',
+    'register_backend',
+]
