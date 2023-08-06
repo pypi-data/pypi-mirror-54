5475c9fb35fea812818ebdee41cbd497a89e10ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:17:45 +0000

    Add package fileparse-tobiasli/1.0.5/fileparse-tobiasli-1.0.5.tar.gz

---
+++ packages/fileparse-tobiasli/fileparse-tobiasli-1.0.5.tar.gz/fileparse-tobiasli-1.0.5/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='fileparse-tobiasli',
+                 version='1.0.5',
+                 description='Tools for parsing text content and creating data models for the content found.',
+                 author='Tobias Litherland',
+                 author_email='tobiaslland@gmail.com',
+                 url='https://github.com/tobiasli/fileparse',
+                 packages=['fileparse'],
+                 long_description=long_description,
+                 long_description_content_type="text/markdown",
+                 classifiers=[
+                     "Programming Language :: Python :: 3",
+                     "License :: OSI Approved :: MIT License",
+                     "Operating System :: OS Independent",
+                 ],
+                 )
