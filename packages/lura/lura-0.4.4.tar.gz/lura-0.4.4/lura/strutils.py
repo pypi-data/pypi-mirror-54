16937316518051a5595260014c7beede4bbf478e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:14 +0000

    Add package lura/0.4.4/lura-0.4.4.tar.gz

---
+++ packages/lura/lura-0.4.4.tar.gz/lura-0.4.4/lura/strutils.py
@@ -0,0 +1,17 @@
+import os
+import subprocess
+from distutils.util import strtobool
+
+shjoin = subprocess.list2cmdline
+
+def prefix(string, prefix, linesep=os.linesep):
+  'Return `string` with each line prefixed with `prefix`.'
+
+  return linesep.join(f'{prefix}{line}' for line in string.split(linesep))
+
+def as_bool(val):
+  'Use `strtobool` to parse `str`s into `bool`s.'
+
+  if val == '':
+    return False
+  return bool(strtobool(val)) if isinstance(val, str) else bool(val)
