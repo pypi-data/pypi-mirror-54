1c7c8445d192755e7782001af3b40223997bae18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:44 +0000

    Add package hm-render-landez/0.0.2/hm-render-landez-0.0.2.tar.gz

---
+++ packages/hm-render-landez/hm-render-landez-0.0.2.tar.gz/hm-render-landez-0.0.2/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    README = fh.read()
+
+with open('requirements.txt', 'r', encoding='utf-8') as f:
+    requirements = f.read().splitlines()
+
+setuptools.setup(
+    name="hm-render-landez",
+    version="0.0.2",
+    license='GNU General Public License (GNU GPL v3 or above)',
+    author="Roel Derickx",
+    author_email="hikingmap.pypi@derickx.be",
+    description="Render a map for a given area to paper using landez and mapnik",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/roelderickx/hm-render-landez",
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.7',
+    entry_points={
+        'console_scripts': ['hm-render-landez = hm_render_landez.hm_render_landez:main']
+    },
+    classifiers=[
+        'Environment :: Console',
+        'Topic :: Scientific/Engineering :: GIS',
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+    ],
+)
+
