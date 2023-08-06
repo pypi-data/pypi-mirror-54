85d7bc06ffcf3e55173f34596afd143fe97c526e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:38 +0000

    Add package batcave/37.1.2/BatCave-37.1.2.tar.gz

---
+++ packages/batcave/BatCave-37.1.2.tar.gz/BatCave-37.1.2/batcave/__init__.py
@@ -0,0 +1,19 @@
+﻿'Module initializer and generic utilities'
+__all__ = ('__title__', '__summary__', '__uri__',
+           '__version__', '__buildname__', '__builddate__',
+           '__author__', '__email__',
+           '__license__', '__copyright__')
+
+__title__ = 'BatCave'
+__summary__ = 'Python Programming Toolkit'
+__uri__ = 'https://gitlab.com/arisilon/batcave/'
+
+__version__ = '37.1.2'
+__buildname__ = 'BatCave_37.1.2_0'
+__builddate__ = '2019-10-29 15:27:31.797784'
+
+__author__ = 'Jeff Smith'
+__email__ = 'web@pobox.com'
+
+__license__ = 'MIT'
+__copyright__ = 'Copyright (c) 2019 Jeffery G. Smith'
