6344ad654341083ebd5ad1ac78a824dee36d8882
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:42 +0000

    Add package royalnet/5.0a74/royalnet-5.0a74-py3-none-any.whl

---
+++ packages/royalnet/royalnet-5.0a74-py3-none-any.whl/royalnet/packs/royal/commands/__init__.py
@@ -0,0 +1,54 @@
+# Imports go here!
+from .ciaoruozi import CiaoruoziCommand
+from .color import ColorCommand
+from .cv import CvCommand
+from .diario import DiarioCommand
+from .rage import RageCommand
+from .reminder import ReminderCommand
+from .ship import ShipCommand
+from .smecds import SmecdsCommand
+from .videochannel import VideochannelCommand
+from .dnditem import DnditemCommand
+from .dndspell import DndspellCommand
+from .trivia import TriviaCommand
+from .matchmaking import MatchmakingCommand
+from .pause import PauseCommand
+from .play import PlayCommand
+from .playmode import PlaymodeCommand
+from .queue import QueueCommand
+from .skip import SkipCommand
+from .summon import SummonCommand
+from .youtube import YoutubeCommand
+from .soundcloud import SoundcloudCommand
+from .zawarudo import ZawarudoCommand
+from .emojify import EmojifyCommand
+
+# Enter the commands of your Pack here!
+available_commands = [
+    CiaoruoziCommand,
+    ColorCommand,
+    CvCommand,
+    DiarioCommand,
+    RageCommand,
+    ReminderCommand,
+    ShipCommand,
+    SmecdsCommand,
+    VideochannelCommand,
+    DnditemCommand,
+    DndspellCommand,
+    TriviaCommand,
+    MatchmakingCommand,
+    PauseCommand,
+    PlayCommand,
+    PlaymodeCommand,
+    QueueCommand,
+    SkipCommand,
+    SummonCommand,
+    YoutubeCommand,
+    SoundcloudCommand,
+    ZawarudoCommand,
+    EmojifyCommand,
+]
+
+# Don't change this, it should automatically generate __all__
+__all__ = [command.__class__.__qualname__ for command in available_commands]
