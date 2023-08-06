48ed0075ed689dc5199e999c864d6a2a57289aea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:51 +0000

    Add package scout-apm/2.6.1/scout_apm-2.6.1-cp36-cp36m-manylinux1_x86_64.whl

---
+++ packages/scout-apm/scout_apm-2.6.1-cp36-cp36m-manylinux1_x86_64.whl/scout_apm/compat.py
@@ -0,0 +1,74 @@
+# coding=utf-8
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import datetime as dt
+import sys
+
+string_type = str if sys.version_info[0] >= 3 else basestring  # noqa: F821
+text_type = str if sys.version_info[0] >= 3 else unicode  # noqa: F821
+
+# Python 2 (and very early 3.x) didn't have ContextDecorator, so define it for ourselves
+if sys.version_info < (3, 2):
+    import functools
+
+    class ContextDecorator(object):
+        def __call__(self, f):
+            @functools.wraps(f)
+            def decorated(*args, **kwds):
+                with self:
+                    return f(*args, **kwds)
+
+            return decorated
+
+
+else:
+    from contextlib import ContextDecorator
+
+try:
+    # Python 3.x
+    import queue
+except ImportError:
+    # Python 2.x
+    import Queue as queue
+
+# datetime_to_timestamp converts a naive UTC datetime to a unix timestamp
+if sys.version_info >= (3, 3):
+
+    def datetime_to_timestamp(datetime_obj):
+        return datetime_obj.replace(tzinfo=dt.timezone.utc).timestamp()
+
+
+else:
+    _EPOCH = dt.datetime(1970, 1, 1)
+
+    def datetime_to_timestamp(datetime_obj):
+        return (datetime_obj - _EPOCH).total_seconds()
+
+
+def text(value, encoding="utf-8", errors="strict"):
+    """
+    Convert a value to str on Python 3 and unicode on Python 2.
+    """
+    if isinstance(value, text_type):
+        return value
+    elif isinstance(value, bytes):
+        return text_type(value, encoding, errors)
+    else:
+        return text_type(value)
+
+
+try:
+    from urllib.parse import urlencode
+except ImportError:
+    from urllib import urlencode
+
+
+__all__ = [
+    "ContextDecorator",
+    "datetime_to_timestamp",
+    "queue",
+    "string_type",
+    "text",
+    "text_type",
+    "urlencode",
+]
---
+++ packages/scout-apm/scout_apm-2.6.1-cp36-cp36m-manylinux1_x86_64.whl/scout_apm/core/metadata.py
@@ -0,0 +1,66 @@
+# coding=utf-8
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import datetime as dt
+import logging
+import sys
+from os import getpid
+
+from scout_apm.core.commands import ApplicationEvent
+from scout_apm.core.context import AgentContext
+
+logger = logging.getLogger(__name__)
+
+
+class AppMetadata(object):
+    @classmethod
+    def report(cls):
+        event = ApplicationEvent(
+            event_type="scout.metadata",
+            event_value=cls.data(),
+            source="Pid: " + str(getpid()),
+            timestamp=dt.datetime.utcnow(),
+        )
+        AgentContext.socket().send(event)
+
+    @classmethod
+    def data(cls):
+        config = AgentContext.instance.config
+        try:
+            data = {
+                "language": "python",
+                "version": "{}.{}.{}".format(*sys.version_info[:3]),
+                "server_time": dt.datetime.utcnow().isoformat() + "Z",
+                "framework": config.value("framework"),
+                "framework_version": config.value("framework_version"),
+                "environment": "",
+                "app_server": config.value("app_server"),
+                "hostname": config.value("hostname"),
+                "database_engine": "",  # Detected
+                "database_adapter": "",  # Raw
+                "application_name": "",  # Environment.application_name,
+                "libraries": cls.get_python_packages_versions(),
+                "paas": "",
+                "application_root": config.value("application_root"),
+                "scm_subdirectory": config.value("scm_subdirectory"),
+                "git_sha": config.value("revision_sha"),
+            }
+        except Exception as e:
+            logger.debug("Exception in AppMetadata: %r", e)
+            data = {}
+
+        return data
+
+    @classmethod
+    def get_python_packages_versions(cls):
+        try:
+            import pkg_resources
+        except ImportError:
+            return []
+
+        return list(
+            sorted(
+                (distribution.project_name, distribution.version)
+                for distribution in pkg_resources.working_set
+            )
+        )
