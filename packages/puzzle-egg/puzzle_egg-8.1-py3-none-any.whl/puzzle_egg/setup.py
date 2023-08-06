04ade25ea553666b995375f1b0f2ee0b3eae2f78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:33 +0000

    Add package puzzle-egg/8.1/puzzle_egg-8.1-py3-none-any.whl

---
+++ packages/puzzle-egg/puzzle_egg-8.1-py3-none-any.whl/puzzle_egg/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='puzzle_egg',
+    packages=['puzzle_egg'],
+    description='Puzzle for something nice',
+    version='8.1',
+    package_data={'puzzle_egg': ['data/bomb-icon.png','data/kitties.txt','data/kitties2.txt','data/letter_1.png', 'data/letter_2.png', 'data/letter_3.png', 'data/letter_4.png','data/letter_5.png', 'data/letter_6.png', 'data/letter_7.png', 'data/letter_8.png', 'data/letter_9.png', 'data/letter_10.png']},
+    url='',
+    author='Alice Bom',
+    author_email='alice.bom@avanade.com',
+    keywords=['puzzle','main','configuration'],
+    install_requires=[
+    ]
+)
