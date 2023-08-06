8afb5a2a634fe199a143a0ec8a322f5ff725ca99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:20 +0000

    Add package resumableds/0.4.5/resumableds-0.4.5.tar.gz

---
+++ packages/resumableds/resumableds-0.4.5.tar.gz/resumableds-0.4.5/resumableds/__init__.py
@@ -0,0 +1,7 @@
+from .resumableds import RdsProject
+from .resumableds import RdsFs
+from .resumableds import PandasObjectHasher
+
+__version__ = '0.4.5'
+__author__ = "David Riedel"
+__author_email__ = "systemverwalter@gmail.com"
