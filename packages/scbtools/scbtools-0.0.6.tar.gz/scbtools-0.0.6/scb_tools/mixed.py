e78d7342354f154d2d12a5215fa61a31ad8626d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:10 +0000

    Add package scbtools/0.0.6/scbtools-0.0.6.tar.gz

---
+++ packages/scbtools/scbtools-0.0.6.tar.gz/scbtools-0.0.6/scb_tools/mixed.py
@@ -0,0 +1,6 @@
+import random
+#-------------num 返回的数字个数 start 起始 end 终止---------
+def create_random(num:int,star=-100,end=100)->list:
+    raw_list=range(star,end)
+    result=random.sample(raw_list,num)
+    return result
