b07d0b580e3fe8311249ba7f70e3697af8420d9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:17:54 +0000

    Add package pst-format/1.0.0/pst-format-1.0.0.tar.gz

---
+++ packages/pst-format/pst-format-1.0.0.tar.gz/pst-format-1.0.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='pst-format',
+	version='1.0.0',
+	packages=find_packages(),
+	scripts=['pst'],
+	description='Plain Structured Text - data format suitable for I/O & command line',
+	author='Peter Kuma',
+	author_email='peter.kuma@fastmail.com',
+	py_modules=['pst'],
+	license="Public Domain",
+	url='https://github.com/peterkuma/pst',
+	classifiers=[
+		'Development Status :: 5 - Production/Stable',
+		'Environment :: Console',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 3',
+		'Topic :: Text Processing :: Markup',
+		'Topic :: Utilities',
+	]
+)
