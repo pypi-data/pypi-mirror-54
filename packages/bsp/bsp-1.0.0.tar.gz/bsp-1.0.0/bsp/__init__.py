313a2f2f2884ddf96fb0bcaded592bed36c69189
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:50 +0000

    Add package bsp/1.0.0/bsp-1.0.0.tar.gz

---
+++ packages/bsp/bsp-1.0.0.tar.gz/bsp-1.0.0/bsp/__init__.py
@@ -0,0 +1,1 @@
+from bsp.bsp import encode, decode
---
+++ packages/bsp/bsp-1.0.0.tar.gz/bsp-1.0.0/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bsp",
+    version="1.0.0",
+    author="Ayon Lee",
+    author_email="i@hyurl.com",
+    description="Basic Socket Protocol",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hyurl/bsp-py",
+    packages=[
+        "bsp"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
