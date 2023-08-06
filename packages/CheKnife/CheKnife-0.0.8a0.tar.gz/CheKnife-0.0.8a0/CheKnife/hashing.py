52e0389cbc48166fb46068196399d7cc14730380
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:58 +0000

    Add package CheKnife/0.0.8a0/CheKnife-0.0.8a0.tar.gz

---
+++ packages/CheKnife/CheKnife-0.0.8a0.tar.gz/CheKnife-0.0.8a0/CheKnife/hashing.py
@@ -0,0 +1,42 @@
+import hashlib
+from base64 import b64encode, b64decode
+
+from functools import partial
+
+
+def textmd5sum(text):
+    md5sum = hashlib.md5()
+    hash_seed = text.encode('utf-8')
+    md5sum.update(hash_seed)
+    md5string = str(md5sum.hexdigest())
+    return md5string
+
+
+def textsha256(text):
+    sha256 = hashlib.sha256()
+    sha256.update(text.encode('utf-8'))
+    return str(sha256.hexdigest())
+
+
+def filemsha256(file_path):
+    with open(file_path, 'rb') as file_handler:
+        sha256 = hashlib.sha256()
+        for part in iter(partial(file_handler.read, 128), b''):
+            sha256.update(part)
+    return sha256.hexdigest()
+
+
+def filemd5sum(file_path):
+    with open(file_path, 'rb') as file_handler:
+        md5sum = hashlib.md5()
+        for part in iter(partial(file_handler.read, 128), b''):
+            md5sum.update(part)
+    return md5sum.hexdigest()
+
+
+def base64(text):
+    return b64encode(text)
+
+
+def base64decode(text):
+    return b64decode(text)
---
+++ packages/CheKnife/CheKnife-0.0.8a0.tar.gz/CheKnife-0.0.8a0/README.rst
@@ -0,0 +1,38 @@
+CheKnife
+========
+
+Python utilities compilation.
+
+-  Free software: MIT license
+
+Install
+=======
+
+::
+
+   pip install CheKnife
+
+Packages
+========
+
+| `CheKnife.pki <docs/CheKnife_pki.md>`__
+| `CheKnife.django.choiceutils <docs/CheKnife.choiceutils.md>`__
+| `CheKnife.hashing <docs/hashing.md>`__
+| `CheKnife.logs <docs/CheKnife.logs.md>`__
+| `CheKnife.files <docs/CheKnife.files.md>`__
+| `CheKnife.security <docs/CheKnife.security>`__
+| `CheKnife.ssl <docs/CheKnife.ssl.md>`__
+
+Tests
+=====
+
+.. code:: bash
+
+   nosetests --with-coverage --cover-inclusive --cover-package=CheKnife --cover-html
+
+Upload to PyPi
+==============
+
+.. code:: bash
+
+   python setup.py sdist upload -r pypi
