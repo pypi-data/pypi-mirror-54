4a067522c955be01bc2a36aa2bb8a2ea49a89402
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:17:56 +0000

    Add package test-pkg2-doubleyoung18/0.0.2/test-pkg2-doubleyoung18-0.0.2.tar.gz

---
+++ packages/test-pkg2-doubleyoung18/test-pkg2-doubleyoung18-0.0.2.tar.gz/test-pkg2-doubleyoung18-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="test-pkg2-doubleyoung18",
+    version="0.0.2",
+    author="doubleyoung18",
+    author_email="doubleyoung18@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/doubleyoung18",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
