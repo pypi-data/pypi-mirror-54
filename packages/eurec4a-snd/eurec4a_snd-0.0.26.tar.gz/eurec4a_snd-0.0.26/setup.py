ca513f3b78745134585fdb1b50d4e7c21350cd3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:09 +0000

    Add package eurec4a-snd/0.0.26/eurec4a_snd-0.0.26.tar.gz

---
+++ packages/eurec4a-snd/eurec4a_snd-0.0.26.tar.gz/eurec4a_snd-0.0.26/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+import versioneer
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="eurec4a_snd",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    author="eurec4a folks",
+    author_email="",
+    description="Common EUREC4A sounding standard",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/observingClouds/eurec4a_snd",
+    packages=setuptools.find_packages(),
+    setup_requires=['setuptools-git-version'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=['pillow>=6.0.0', 'matplotlib>=3.1.0', 'basemap>=1.2.0',
+                      'numpy>=1.14.0', 'netCDF4>=1.4.0', 'eccodes>=2.13.0'],
+    entry_points={'console_scripts':
+                    ['sounding_converter=eurec4a_snd.L1_bufr:main',
+                     'sounding_visualize=eurec4a_snd.make_quicklooks_rs41:main']},
+    package_data={"eurec4a_snd": ["examples/data/*", "config/meta_information_template.ini"]}
+)
