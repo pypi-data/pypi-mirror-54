10cf073311531478b71959ddeb023137db1ae0e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:25 +0000

    Add package colouredprinter/0.0.3/colouredprinter-0.0.3.tar.gz

---
+++ packages/colouredprinter/colouredprinter-0.0.3.tar.gz/colouredprinter-0.0.3/colouredprinter/color/ColorConstant.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from enum import Enum
+
+
+# constant values of fonts's colors
+class FrontColor(Enum):
+
+    DEFAULT = ''
+
+    BLACK = '30'
+
+    RED = '31'
+
+    GREEN = '32'
+
+    YELLOW = '33'
+
+    BLUE = '34'
+
+    DARKORCHILD = '35'
+
+    CYAN = '36'
+
+    WHITE = '37'
+
+
+class BackgroundColor(Enum):
+
+    DEFAULT = ''
+
+    BLACK = '40'
+
+    RED = '41'
+
+    GREEN = '42'
+
+    YELLOW = '43'
+
+    BLUE = '44'
+
+    DARKORCHILD = '45'
+
+    CYAN = '46'
+
+    WHITE = '47'
