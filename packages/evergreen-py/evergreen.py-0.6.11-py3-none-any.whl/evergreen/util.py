a4abe968e765effb54a9b21b6144028b59d0e641
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:58 +0000

    Add package evergreen-py/0.6.11/evergreen.py-0.6.11-py3-none-any.whl

---
+++ packages/evergreen-py/evergreen.py-0.6.11-py3-none-any.whl/evergreen/util.py
@@ -0,0 +1,65 @@
+"""Useful utilities for interacting with Evergreen."""
+from datetime import datetime
+
+EVG_DATETIME_FORMAT = '%Y-%m-%dT%H:%M:%S.%fZ'
+EVG_SHORT_DATETIME_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
+EVG_DATE_FORMAT = '%Y-%m-%d'
+EVG_DATE_INPUT_FORMAT = '"%Y-%m-%dT%H:%M:%S.000Z"'
+
+
+def parse_evergreen_datetime(evg_date):
+    """
+    Convert an evergreen datetime string into a datetime object.
+
+    :param evg_date: String to convert to a datetime.
+    :return datetime version of date.
+    """
+    if not evg_date:
+        return None
+    if type(evg_date) in [int, float]:
+        return datetime.fromtimestamp(evg_date)
+    return datetime.strptime(evg_date, EVG_DATETIME_FORMAT)
+
+
+def parse_evergreen_short_datetime(evg_date):
+    """
+    Convert an evergreen datetime string into a datetime object.
+
+    :param evg_date: String to convert to a datetime.
+    :return datetime version of date.
+    """
+    if not evg_date:
+        return None
+    return datetime.strptime(evg_date, EVG_SHORT_DATETIME_FORMAT)
+
+
+def format_evergreen_datetime(when):
+    """
+    Convert a datetime object into an evergreen consumable string.
+
+    :param when: datetime to convert.
+    :return: string evergreen can understand.
+    """
+    return when.strftime(EVG_DATE_INPUT_FORMAT)
+
+
+def evergreen_input_to_output(input_date):
+    """
+    Convert a date from evergreen to a date to send back to evergreen.
+
+    :param input_date: date to convert.
+    :return: date to send to evergreen.
+    """
+    return format_evergreen_datetime(parse_evergreen_datetime(input_date))
+
+
+def parse_evergreen_date(evg_date):
+    """
+    Convert an evergreen date string into a date object.
+
+    :param evg_date: String to convert to a date.
+    :return: date version of date.
+    """
+    if not evg_date:
+        return None
+    return datetime.strptime(evg_date, EVG_DATE_FORMAT).date()
