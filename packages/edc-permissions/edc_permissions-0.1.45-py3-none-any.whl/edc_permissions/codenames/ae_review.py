ebb28c14de1c1e55a275e30d078fe0e80e1366d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:05 +0000

    Add package edc-permissions/0.1.45/edc_permissions-0.1.45-py3-none-any.whl

---
+++ packages/edc-permissions/edc_permissions-0.1.45-py3-none-any.whl/edc_permissions/codenames/ae_review.py
@@ -0,0 +1,4 @@
+from .ae import ae
+
+
+ae_review = [c for c in ae if ('view_' in c or 'nav_' in c)]
---
+++ packages/edc-permissions/edc_permissions-0.1.45-py3-none-any.whl/edc_permissions/codenames/rando.py
@@ -0,0 +1,8 @@
+
+rando_tuples = [
+    ("edc_randomization.display_assignment",
+     "Can display randomization assignment"),
+    ("edc_randomization.view_randomizationlist", "Can view randomization list"),
+]
+
+rando = [c[0] for c in rando_tuples]
