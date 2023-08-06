f2d5487232436fa9374038dbcad726f18c9013a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:16 +0000

    Add package aldjemy/0.10.1/aldjemy-0.10.1.tar.gz

---
+++ packages/aldjemy/aldjemy-0.10.1.tar.gz/aldjemy-0.10.1/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import os.path
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setupconf = dict(
+    name='aldjemy',
+    version="0.10.1",
+    license='BSD',
+    url='https://github.com/Deepwalker/aldjemy/',
+    author='Mihail Krivushin',
+    author_email='krivushinme@gmail.com',
+    description=('SQLAlchemy to Django integration library'),
+    long_description = read('README.rst'),
+    packages = find_packages(),
+
+    install_requires = ['sqlalchemy>0.7.1'],
+
+    classifiers = [
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        ],
+    )
+
+if __name__ == '__main__':
+    setup(**setupconf)
