27e5e6bf3c92587e15698a8c94330bd901320a27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:21 +0000

    Add package janis-pipelines.runner/0.7.0/janis-pipelines.runner-0.7.0.tar.gz

---
+++ packages/janis-pipelines.runner/janis-pipelines.runner-0.7.0.tar.gz/janis-pipelines.runner-0.7.0/janis_runner/data/enums/taskstatus.py
@@ -0,0 +1,48 @@
+from enum import Enum
+
+
+class TaskStatus(Enum):
+    PROCESSING = "processing"
+    QUEUED = "queued"
+    RUNNING = "running"
+    COMPLETED = "completed"
+    FAILED = "failed"
+    ABORTED = "aborted"
+    DRY_RUN = "dry-run"
+
+    @staticmethod
+    def all():
+        return [t for t in TaskStatus]
+
+    @staticmethod
+    def final_states():
+        return [
+            TaskStatus.COMPLETED,
+            TaskStatus.FAILED,
+            TaskStatus.ABORTED,
+            TaskStatus.DRY_RUN,
+        ]
+
+    def __str__(self):
+        __str = {
+            TaskStatus.PROCESSING.value: "Processing",
+            TaskStatus.QUEUED.value: "Queued",
+            TaskStatus.RUNNING.value: "Running",
+            TaskStatus.COMPLETED.value: "Completed",
+            TaskStatus.FAILED.value: "Failed",
+            TaskStatus.ABORTED.value: "Terminated",
+            TaskStatus.DRY_RUN.value: "Dry run",
+        }
+        return __str[self.value]
+
+    def symbol(self):
+        __str = {
+            TaskStatus.PROCESSING.value: "...",
+            TaskStatus.QUEUED.value: " ",
+            TaskStatus.RUNNING.value: "~",
+            TaskStatus.COMPLETED.value: "✓",
+            TaskStatus.FAILED.value: "!",
+            TaskStatus.ABORTED.value: "T",
+            TaskStatus.DRY_RUN.value: "DR",
+        }
+        return __str[self.value]
