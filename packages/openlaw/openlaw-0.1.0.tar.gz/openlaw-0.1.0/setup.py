8266c55ed05e3b3233bfa36022a929a012dc57be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:43 +0000

    Add package openlaw/0.1.0/openlaw-0.1.0.tar.gz

---
+++ packages/openlaw/openlaw-0.1.0.tar.gz/openlaw-0.1.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['openlaw']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'openlaw',
+    'version': '0.1.0',
+    'description': 'A metapacke for OpenLaw Israel projects',
+    'long_description': None,
+    'author': 'Yehuda Deutsch',
+    'author_email': 'yeh@uda.co.il',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
