9eac0421aaae76eceb04a90b5e33ac1eace3d706
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:53 +0000

    Add package dival/0.3.3/dival-0.3.3-py3-none-any.whl

---
+++ packages/dival/dival-0.3.3-py3-none-any.whl/dival/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import os
+with open(os.path.join(os.path.split(__file__)[0], '..',
+                       'VERSION')) as version_f:
+    __version__ = version_f.read().strip()
+
+from .config import CONFIG, get_config, set_config
+from .data import DataPairs
+from .datasets import Dataset
+from .datasets.standard import get_standard_dataset
+from .reconstructors import (Reconstructor, IterativeReconstructor,
+                             LearnedReconstructor)
+from .measure import Measure
+from .evaluation import TaskTable
+
+
+__all__ = ['CONFIG', 'get_config', 'set_config',
+           'DataPairs',
+           'Dataset', 'get_standard_dataset',
+           'Reconstructor', 'IterativeReconstructor', 'LearnedReconstructor',
+           'Measure',
+           'TaskTable']
