bc6fbfd76ab74437cd5ceb75c57a0cdcb4dca837
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:47 +0000

    Add package mdk/1.1/mdk-1.1.tar.gz

---
+++ packages/mdk/mdk-1.1.tar.gz/mdk-1.1/src/errors.py
@@ -0,0 +1,35 @@
+"""Define exceptions used by mdk"""
+
+class AuthenticationError(Exception):
+    """Raised when an API authentication attempt fails"""
+    def __init__(self, message):
+        super(AuthenticationError, self).__init__(message)
+        self.message = message
+
+
+class ConfigError(Exception):
+    """Raised when a config file cannot be used by mdk as expected"""
+    def __init__(self, message):
+        super(ConfigError, self).__init__(message)
+        self.message = message
+
+
+class InitializationError(Exception):
+    """Raised when a config file cannot be used by mdk as expected"""
+    def __init__(self, message):
+        super(InitializationError, self).__init__(message)
+        self.message = message
+
+
+class RequestError(Exception):
+    """Raised when an API authentication attempt fails"""
+    def __init__(self, message):
+        super(RequestError, self).__init__(message)
+        self.message = message
+
+
+class ServiceError(Exception):
+    """Raised when something goes wrong with a docker-compose service"""
+    def __init__(self, message):
+        super(ServiceError, self).__init__(message)
+        self.message = message
