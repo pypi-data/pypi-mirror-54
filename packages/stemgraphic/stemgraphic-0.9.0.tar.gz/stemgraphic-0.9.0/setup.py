dd4e1c634336f72524574dbcb87670efdb352406
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:58 +0000

    Add package stemgraphic/0.9.0/stemgraphic-0.9.0.tar.gz

---
+++ packages/stemgraphic/stemgraphic-0.9.0.tar.gz/stemgraphic-0.9.0/setup.py
@@ -0,0 +1,38 @@
+import io
+import os
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with io.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
+with io.open(os.path.join(here, 'VERSION')) as f:
+    version = f.read()
+
+setup(name='stemgraphic',
+      version=version,
+      install_requires=[
+          "docopt",
+          "matplotlib",
+          "pandas",
+          #"python-levenshtein",
+          "seaborn"
+      ],
+      include_package_data=True,
+      scripts=['bin/stem','bin/stem.bat'],
+      description='Graphic and text stem-and-leaf plots',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='http://github.com/dionresearch/stemgraphic',
+      author='Francois Dion',
+      author_email='francois.dion@gmail.com',
+      license='MIT',
+      packages=['stemgraphic'],
+      zip_safe=False,
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+      ],
+      )
---
+++ packages/stemgraphic/stemgraphic-0.9.0.tar.gz/stemgraphic-0.9.0/stemgraphic/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""stem_graphic
+
+Package implementing a complete toolkit for text and a graphical stem-and-leaf plots and other visualizations adapted
+to stem-and-leaf pair values, such as heatmaps and sunburst charts.
+
+It also handles very large data sets through scaling, sampling, trimming and other techniques.
+
+See research paper ( http://artchiv.es/pydata2016/stemgraphic ) for more technical details.
+
+A command line utility was installed along with the package, allowing to process excel or csv
+files. See: stem -h
+"""
+from .aliases import stem_hist, stem_kde, stem_line, stem_dot, stem_symmetric_dot
+from .graphic import stem_graphic, heatmap
+from .text import stem_data, stem_text, heatmatrix
+from .helpers import dd
+from .stopwords import *
