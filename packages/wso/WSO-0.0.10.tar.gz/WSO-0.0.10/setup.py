e1b3e054287e9e17086fe72daec9bc79862c6b0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:46 +0000

    Add package wso/0.0.10/WSO-0.0.10.tar.gz

---
+++ packages/wso/WSO-0.0.10.tar.gz/WSO-0.0.10/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="WSO",
+    version="0.0.10",
+    author="Rory Murdock",
+    author_email="rory@itmatic.com.au",
+    description="A Library for interacting with WSO UEM",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rorymurdock/WSO-UEM-Py",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        # "Development Status :: 5 - Production/Stable",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+          'reqREST',
+    ],
+)
