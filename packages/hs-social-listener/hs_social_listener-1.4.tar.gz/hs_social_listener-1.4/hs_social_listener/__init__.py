e8e6348519593c7f26c78990af96c179785f7ac2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:18 +0000

    Add package hs-social-listener/1.4/hs_social_listener-1.4.tar.gz

---
+++ packages/hs-social-listener/hs_social_listener-1.4.tar.gz/hs_social_listener-1.4/hs_social_listener/__init__.py
@@ -0,0 +1,12 @@
+"""
+Author: James Londal
+Copyright: Copyright 2019, Hearts & Science
+License: GNU AFFERO GENERAL PUBLIC LICENSE
+Version: 1.0.0
+Maintainer: James Londal
+"""
+
+
+
+
+__version__ = "0.1"
---
+++ packages/hs-social-listener/hs_social_listener-1.4.tar.gz/hs_social_listener-1.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='hs_social_listener',
+      version='1.4',
+      description='A library to analyse conversations on social media',
+      url='https://hearts-science.com',
+      author='James Londal',
+      license='GNU AFFERO GENERAL PUBLIC LICENSE',
+      install_requires=[
+         'twint',
+         'pandas',
+         'numpy'
+      ],
+      packages=['hs_social_listener'],
+      zip_safe=False,
+      include_package_data=True
+)
