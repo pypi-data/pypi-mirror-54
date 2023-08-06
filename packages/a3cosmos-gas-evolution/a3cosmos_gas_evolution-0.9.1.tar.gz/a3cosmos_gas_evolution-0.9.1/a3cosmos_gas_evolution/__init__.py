a08954f07e839b9ade28e7baf2862df839c2130b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:14 +0000

    Add package a3cosmos-gas-evolution/0.9.1/a3cosmos_gas_evolution-0.9.1.tar.gz

---
+++ packages/a3cosmos-gas-evolution/a3cosmos_gas_evolution-0.9.1.tar.gz/a3cosmos_gas_evolution-0.9.1/a3cosmos_gas_evolution/__init__.py
@@ -0,0 +1,28 @@
+name = 'a3cosmos_gas_evolution'
+
+
+
+
+
+# 
+# import functions
+# 
+#from .Common_Python_Code import calc_galaxy_main_sequence
+#from .Common_Python_Code import calc_gas_fraction
+#from .Common_Python_Code import calc_gas_depletion_time
+#from .Common_Python_Code import apply_cosmology
+#cosmo = apply_cosmology.cosmo
+
+
+
+
+
+
+from .__main__ import help
+
+from .__main__ import load_all_modules
+
+load_all_modules()
+
+from .__main__ import *
+
