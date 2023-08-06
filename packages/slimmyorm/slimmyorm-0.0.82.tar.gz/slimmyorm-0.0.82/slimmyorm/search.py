85264712fec3d4915d00154471a1e631bb339184
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:40 +0000

    Add package slimmyorm/0.0.82/slimmyorm-0.0.82.tar.gz

---
+++ packages/slimmyorm/slimmyorm-0.0.82.tar.gz/slimmyorm-0.0.82/slimmyorm/search.py
@@ -0,0 +1,29 @@
+from .select import Select
+
+
+def search(self, table, name=False, db_id=False, where=False):
+    slct = Select('*', table, multi=True, db_id=db_id, name=name, where=where)
+    data = slct.all()
+    if data == []:
+        return {"Error": f"'{name}' not found when searching"}
+    return data
+
+
+def search_high_low(table, column, name=False, high=False, low=False):
+    # Ensure at least one has a value
+    if not isinstance(high, int) and not isinstance(low, int):
+        return {"Error": "Please provide a high or low value"}
+
+    high_limit = f"{column} <= {high}" if high else ""
+    low_limit = f"{column} >= {low}" if low else ""
+
+    if high_limit and low_limit:  # If both have a value
+        where = f"{high_limit} AND {low_limit}"
+    else:
+        where = high_limit + low_limit  # If one is empty string
+
+    data = Select('*', table, search=True, name=name, where=where).all()
+
+    if data == []:
+        return {"Error": f"'{name}' not found when searching"}
+    return data
