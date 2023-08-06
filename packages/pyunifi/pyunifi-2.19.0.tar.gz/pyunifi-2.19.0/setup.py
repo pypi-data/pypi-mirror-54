736897cad180c5e248520c79540db3d1ebf210e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:03 +0000

    Add package pyunifi/2.19.0/pyunifi-2.19.0.tar.gz

---
+++ packages/pyunifi/pyunifi-2.19.0.tar.gz/pyunifi-2.19.0/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='pyunifi',
+      version='2.19.0',
+      description='API for Ubiquity Networks UniFi controller',
+      author='Caleb Dunn',
+      author_email='finish.06@gmail.com',
+      url='https://github.com/finish06/unifi-api',
+      packages=['pyunifi'],
+      # Need time to update scripts
+      # scripts=['unifi-low-snr-reconnect', 'unifi-ls-clients',
+      #          'unifi-save-statistics', 'unifi-log-roaming'],
+      classifiers=[],
+      install_requires=['requests'],
+      )
