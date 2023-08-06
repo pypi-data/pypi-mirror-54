daa9c25940dd5f540f1dc9cf6851006acfd7c1ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:34 +0000

    Add package kuttyPy/1.0.9/kuttyPy-1.0.9-py3-none-any.whl

---
+++ packages/kuttyPy/kuttyPy-1.0.9-py3-none-any.whl/kuttyPyGui/utilities/Qt.py
@@ -0,0 +1,23 @@
+## 'Qt' is a local module; it is intended mainly to cover up the differences
+## between PyQt4 and PyQt5.
+import os
+
+ENVIRON = os.environ['CSMCA_QT_LIB']
+
+if ENVIRON=='PyQt5':
+	from PyQt5 import QtGui,QtCore,QtWidgets
+	try: from PyQt5 import QtSvg
+	except: pass
+	try:
+		#from PyQt5.QtWebEngineWidgets import QWebEngineView as QWebView
+		from PyQt5.QtWebKitWidgets import QWebView# , QWebPage
+	except:
+		QWebView = None
+elif ENVIRON == 'PyQt4':
+	from PyQt4 import QtGui,QtCore
+	from PyQt4 import QtGui as QtWidgets
+	#try:
+	#	from PyQt4.QtWebKitWidgets import QWebView# , QWebPage
+	#except:
+	#	print('webview unavailable')
+	
