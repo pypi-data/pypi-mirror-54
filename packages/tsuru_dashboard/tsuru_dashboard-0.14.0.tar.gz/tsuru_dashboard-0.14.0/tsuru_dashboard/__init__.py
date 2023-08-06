f6af95ed503755c5fdbdabbba6d110c250e55039
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:38 +0000

    Add package tsuru_dashboard/0.14.0/tsuru_dashboard-0.14.0.tar.gz

---
+++ packages/tsuru_dashboard/tsuru_dashboard-0.14.0.tar.gz/tsuru_dashboard-0.14.0/tsuru_dashboard/__init__.py
@@ -0,0 +1,4 @@
+from tsuru_dashboard import apps  # NOQA
+
+
+__version__ = "0.14.0"
---
+++ packages/tsuru_dashboard/tsuru_dashboard-0.14.0.tar.gz/tsuru_dashboard-0.14.0/tsuru_dashboard/apps/urls.py
@@ -0,0 +1,30 @@
+from django.conf.urls import url
+
+import views
+
+
+urlpatterns = [
+    url(r'^$', views.ListApp.as_view(), name='list-app'),
+    url(r'^list.json$', views.ListAppJson.as_view(), name='list-app-json'),
+    url(r'^create/$', views.CreateApp.as_view(), name='create-app'),
+    url(r'^(?P<app_name>[\w-]+).json$',
+        views.AppDetailJson.as_view(), name='detail-app-json'),
+    url(r'^(?P<app_name>[\w-]+)/$',
+        views.AppDetail.as_view(), name='detail-app'),
+    url(r'^(?P<name>[\w-]+)/remove/$',
+        views.RemoveApp.as_view(), name='remove_app'),
+    url(r'^(?P<name>[\w-]+)/unlock/$', views.Unlock.as_view(), name='unlock-app'),
+    url(r'^(?P<app_name>[\w-]+)/log/stream/$', views.LogStream.as_view(), name='app-log-stream'),
+    url(r'^(?P<app_name>[\w-]+)/log/$', views.AppLog.as_view(),
+        name='app_log'),
+    url(r'^(?P<app_name>[\w-]+)/settings/$', views.Settings.as_view(),
+        name='app-settings'),
+    url(r'^(?P<app_name>[\w-]+)/deploys/$', views.ListDeploy.as_view(), name='app-deploys'),
+    url(r'^(?P<app_name>[\w-]+)/deploys/(?P<deploy>[\s\w@\.-]+)/$',
+        views.DeployInfo.as_view(), name='app-deploy'),
+    url(r'^(?P<app_name>[\w-]+)/rollback/(?P<image>.+)/$',
+        views.AppRollback.as_view(), name='app-rollback'),
+    url(r'^(?P<app_name>[\w-]+)/events/$', views.EventList.as_view(), name='app-events'),
+    url(r'^(?P<app_name>[\w-]+)/events/(?P<uuid>[\s\w@\.-]+)/$',
+        views.EventInfo.as_view(), name='app-event'),
+]
