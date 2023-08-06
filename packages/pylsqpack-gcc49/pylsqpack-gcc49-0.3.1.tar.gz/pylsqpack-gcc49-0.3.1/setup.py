965b9174d4ff538f273a9191335f1a1e12edcab6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:53 +0000

    Add package pylsqpack-gcc49/0.3.1/pylsqpack-gcc49-0.3.1.tar.gz

---
+++ packages/pylsqpack-gcc49/pylsqpack-gcc49-0.3.1.tar.gz/pylsqpack-gcc49-0.3.1/setup.py
@@ -0,0 +1,52 @@
+import os.path
+
+import setuptools
+import sysconfig
+
+root_dir = os.path.abspath(os.path.dirname(__file__))
+readme_file = os.path.join(root_dir, 'README.rst')
+with open(readme_file, encoding='utf-8') as f:
+    long_description = f.read()
+
+extra_compile_args = ['-std=c11']
+extra_compile_args += sysconfig.get_config_var('CFLAGS').split()
+
+setuptools.setup(
+    name='pylsqpack-gcc49',
+    version='0.3.1',
+    description='Python wrapper for the ls-qpack QPACK library',
+    long_description=long_description,
+    url='https://github.com/madricas/pylsqpack',
+    author='Madricas',
+    author_email='madricas@outlook.fr',
+    license='BSD',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        "Topic :: Internet :: WWW/HTTP",
+    ],
+    ext_modules=[
+        setuptools.Extension(
+            'pylsqpack',
+            include_dirs=[
+                'vendor/ls-qpack',
+                'vendor/ls-qpack/deps/xxhash/',
+            ],
+            sources=[
+                'src/module.c',
+                'vendor/ls-qpack/lsqpack.c',
+                'vendor/ls-qpack/deps/xxhash/xxhash.c',
+            ],
+            extra_compile_args=extra_compile_args,
+        ),
+    ],
+)
