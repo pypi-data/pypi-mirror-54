8d0c751f502125044bda2aa20e5e907fe3fc5245
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:14 +0000

    Add package jupyter-wysiwyg/19.10/jupyter-wysiwyg-19.10.tar.gz

---
+++ packages/jupyter-wysiwyg/jupyter-wysiwyg-19.10.tar.gz/jupyter-wysiwyg-19.10/README.md
@@ -0,0 +1,25 @@
+# WYSIWYG HTML/Markdown Editor for Jupyter Notebook
+
+This is an nbextension that enables WYSIWYG editing functionality for HTML/Markdown cells in Jupyter.
+
+## Installing
+
+This extension can be installed through either PIP or conda.
+
+> pip install jupyter-wysiwyg
+
+> conda install -c genepattern jupyter-wysiwyg
+
+## Enabling
+
+To enable this extension in Jupyter Notebook 5.2 and earlier you will need to run the following command lines. 
+
+In Jupyter Notebook 5.3 and later, this is automatic and will not be necessary.
+
+> jupyter nbextension install --py jupyter_wysiwyg
+
+> jupyter nbextension enable --py jupyter_wysiwyg
+
+## Using the Editor
+
+To use the WYSIWYG editor, first create a markdown cell, then select the cell. You should now see two buttons appear on the left side of the cell. These buttons will have the "Rich Text Editing" and "Run Cell" tooltips. Click the "Rich Text Editing" button to launch the editor.
---
+++ packages/jupyter-wysiwyg/jupyter-wysiwyg-19.10.tar.gz/jupyter-wysiwyg-19.10/setup.py
@@ -0,0 +1,73 @@
+from setuptools import setup
+import os
+
+
+# Read version and other metadata from file
+__version__ = '19.10'
+
+
+with open('README.md') as f:
+    long_description = f.read()
+
+
+def get_data_files():
+    directory = 'jupyter_wysiwyg/static'
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            alt_path = path.replace('jupyter_wysiwyg/static', 'share/jupyter/nbextensions/jupyter_wysiwyg')
+            paths.append(
+                (alt_path, [os.path.join(path, filename)],)
+            )
+
+    paths.append(('etc/jupyter/nbconfig/notebook.d', ['jupyter_wysiwyg.json']),)
+    return paths
+
+
+setup(name='jupyter-wysiwyg',
+      packages=['jupyter_wysiwyg'],
+      version=__version__,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      description='WYSIWYG editing functionality for markdown/HTML cells in Jupyter',
+      license='BSD',
+      author='Thorin Tabor',
+      author_email='thorin@broadinstitute.org',
+      url='https://github.com/genepattern/jupyter-wysiwyg/tree/master/wysiwyg',
+      download_url='https://github.com/genepattern/jupyter-wysiwyg/archive/' + __version__ + '.tar.gz',
+      keywords=['genepattern', 'wysiwyg', 'ipython', 'jupyter'],
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Science/Research',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: BSD License',
+          'Programming Language :: Python',
+          'Framework :: Jupyter',
+      ],
+      install_requires=[
+          'jupyter',
+          'notebook>=4.2.0',
+      ],
+      package_dir={'jupyter_wysiwyg': 'jupyter_wysiwyg'},
+      package_data={'jupyter_wysiwyg': ['static/*.js',
+                                        'static/tinymce/*.js',
+                                        'static/tinymce/*.md',
+                                        'static/tinymce/*.css',
+                                        'static/tinymce/*/*.js',
+                                        'static/tinymce/*/*.css',
+                                        'static/tinymce/*/*.png',
+                                        'static/tinymce/*/*.gif',
+                                        'static/tinymce/*/*/*.js',
+                                        'static/tinymce/*/*/*.css',
+                                        'static/tinymce/*/*/*.png',
+                                        'static/tinymce/*/*/*.gif',
+                                        'static/tinymce/*/*/*/*.js',
+                                        'static/tinymce/*/*/*/*.css',
+                                        'static/tinymce/*/*/*/*.png',
+                                        'static/tinymce/*/*/*/*.gif',
+                                        'static/tinymce/*/*/*/*/*.js'
+                                        'static/tinymce/*/*/*/*/*.css'
+                                        'static/tinymce/*/*/*/*/*.png'
+                                        'static/tinymce/*/*/*/*/*.gif']},
+      data_files=get_data_files(),
+      )
