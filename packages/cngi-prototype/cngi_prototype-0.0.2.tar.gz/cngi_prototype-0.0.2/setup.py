4abc9f57a4c0046ba7984ade6e6788638329cc2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:30 +0000

    Add package cngi-prototype/0.0.2/cngi_prototype-0.0.2.tar.gz

---
+++ packages/cngi-prototype/cngi_prototype-0.0.2.tar.gz/cngi_prototype-0.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+import os
+import sys
+
+with open('README.rst', encoding='utf-8') as fid:
+    long_description = fid.read()
+
+setup(
+    name='cngi_prototype',
+    version='0.0.2',
+    description='casa next generation infrastructure prototype',
+    long_description=long_description,
+    author='National Radio Astronomy Observatory',
+    author_email='casa-feedback@nrao.edu',
+    url='https://github.com/casangi/cngi_prototype',
+    license='Apache-2.0',
+    packages=find_packages(),
+    install_requires=['numpy',
+                      'dask',
+                      'distributed',
+                      'pyarrow',
+                      'bokeh',
+                      'pandas'],
+)
