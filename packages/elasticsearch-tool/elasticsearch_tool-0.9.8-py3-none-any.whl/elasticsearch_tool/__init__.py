f9a0fe13c3bbd047b3d5af07372d19fb1f0b225a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:01 +0000

    Add package elasticsearch-tool/0.9.8/elasticsearch_tool-0.9.8-py3-none-any.whl

---
+++ packages/elasticsearch-tool/elasticsearch_tool-0.9.8-py3-none-any.whl/elasticsearch_tool/__init__.py
@@ -0,0 +1,11 @@
+"""
+author: Haiton https://www.liushx.com/ 642641850@qq.com
+thanks to https://es.xiaoleilu.com/
+"""
+VERSION = (0, 9, 8)
+__version__ = VERSION
+__versionstr__ = '.'.join(map(str, VERSION))
+
+
+from .elements.document import Doc, Fields
+from .base_element.operate import NOT, OR, datetime_tool
