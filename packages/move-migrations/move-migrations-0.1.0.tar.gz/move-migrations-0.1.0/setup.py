e5ab4a70b46b38689678a94e4e076ed3ae3bb21a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:13 +0000

    Add package move-migrations/0.1.0/move-migrations-0.1.0.tar.gz

---
+++ packages/move-migrations/move-migrations-0.1.0.tar.gz/move-migrations-0.1.0/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['move_migrations']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['prompt>=0.4.1,<0.5.0']
+
+entry_points = \
+{'console_scripts': ['move-migrations = move_migrations:move_migrations.main']}
+
+setup_kwargs = {
+    'name': 'move-migrations',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Your Name',
+    'author_email': 'you@example.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
