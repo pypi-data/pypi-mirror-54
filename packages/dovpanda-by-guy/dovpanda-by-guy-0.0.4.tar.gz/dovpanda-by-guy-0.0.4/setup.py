ee13d70eddcee87592e84663702a7454ebcbb4e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:16:51 +0000

    Add package dovpanda-by-guy/0.0.4/dovpanda-by-guy-0.0.4.tar.gz

---
+++ packages/dovpanda-by-guy/dovpanda-by-guy-0.0.4.tar.gz/dovpanda-by-guy-0.0.4/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dovpanda-by-guy",
+    version="0.0.4",
+    author="guysmoilov",
+    description="Directions overlay for working with pandas in an analysis environment",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/guysmoilov/dovpanda",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
