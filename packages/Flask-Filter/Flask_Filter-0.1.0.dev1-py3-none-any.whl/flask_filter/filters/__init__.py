6aac47dbb00f4a6c46ba9f4fc3500b3bac9cb7d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:52 +0000

    Add package Flask-Filter/0.1.0.dev1/Flask_Filter-0.1.0.dev1-py3-none-any.whl

---
+++ packages/Flask-Filter/Flask_Filter-0.1.0.dev1-py3-none-any.whl/flask_filter/filters/__init__.py
@@ -0,0 +1,22 @@
+from .filters import (
+    LTFilter,
+    LTEFilter,
+    EqualsFilter,
+    GTFilter,
+    GTEFilter,
+    InFilter,
+    NotEqualsFilter,
+    LikeFilter
+)
+
+
+FILTERS = [
+    LTFilter,
+    LTEFilter,
+    EqualsFilter,
+    GTFilter,
+    GTEFilter,
+    InFilter,
+    NotEqualsFilter,
+    LikeFilter
+]
