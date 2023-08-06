b143ee3761b029ccafdb4508e65c1607b644a059
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:01 +0000

    Add package frgfm-package/0.1.0/frgfm_package-0.1.0-py3-none-any.whl

---
+++ packages/frgfm-package/frgfm_package-0.1.0-py3-none-any.whl/my_package/__init__.py
@@ -0,0 +1,2 @@
+from my_package import my_functions
+from .version import __version__
---
+++ packages/frgfm-package/frgfm_package-0.1.0-py3-none-any.whl/my_package/my_functions.py
@@ -0,0 +1,15 @@
+#!usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+def hello(name):
+    """My function description
+
+    Args:
+        name (str): user name
+
+    Returns:
+        str: Welcome message
+    """
+
+    return f'Hello {name}'
