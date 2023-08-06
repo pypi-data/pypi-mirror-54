c2bcb2c6cbca2791873d10d042bbca1e67bb155b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:48 +0000

    Add package usim/0.4.1/usim-0.4.1.tar.gz

---
+++ packages/usim/usim-0.4.1.tar.gz/usim-0.4.1/usim_pytest/test_types/test_pipe.py
@@ -0,0 +1,10 @@
+# from usim import Pipe, Scope
+
+from ..utility import via_usim, assertion_mode
+
+
+class TestPipe:
+    @via_usim
+    async def test_transfer_uncontested(self):
+        pass
+        # pipe = Pipe(throughput=2)
