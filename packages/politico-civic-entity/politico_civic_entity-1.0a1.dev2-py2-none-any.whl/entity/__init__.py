8450f1144e64d83f686642fccac7d656aae72690
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:04 +0000

    Add package politico-civic-entity/1.0a1.dev2/politico_civic_entity-1.0a1.dev2-py2-none-any.whl

---
+++ packages/politico-civic-entity/politico_civic_entity-1.0a1.dev2-py2-none-any.whl/entity/__init__.py
@@ -0,0 +1,3 @@
+__version__ = (1, "0a1", "dev2")
+
+default_app_config = "entity.apps.EntityConfig"
---
+++ packages/politico-civic-entity/politico_civic_entity-1.0a1.dev2-py2-none-any.whl/entity/admin/person.py
@@ -0,0 +1,51 @@
+# Imports from Django.
+from django.contrib import admin
+
+
+# Imports from entity.
+from entity.models import PersonImage
+
+
+class PersonImageInline(admin.StackedInline):
+    model = PersonImage
+    extra = 0
+
+
+class PersonAdmin(admin.ModelAdmin):
+    inlines = [PersonImageInline]
+
+    fieldsets = (
+        (
+            "Name",
+            {
+                "fields": (
+                    "first_name",
+                    "middle_name",
+                    "last_name",
+                    "suffix",
+                    "full_name",
+                )
+            },
+        ),
+        (
+            "Demographics",
+            {
+                "fields": (
+                    "gender",
+                    "race",
+                    "nationality",
+                    "state_of_residence",
+                )
+            },
+        ),
+        (None, {"fields": ("birth_date", "death_date")}),
+        (
+            "Descriptions",
+            {"fields": ("summary", "description", "identifiers", "links")},
+        ),
+        ("Record locators", {"fields": ("id", "uid", "slug")}),
+    )
+    readonly_fields = ("id", "uid", "slug")
+
+    search_fields = ("first_name", "last_name")
+    ordering = ("last_name",)
