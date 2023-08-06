7965b74f60e2ac5a728aa16b2a959801cd2a77a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:04 +0000

    Add package eagerpy/0.1.1/eagerpy-0.1.1.tar.gz

---
+++ packages/eagerpy/eagerpy-0.1.1.tar.gz/eagerpy-0.1.1/eagerpy/__init__.py
@@ -0,0 +1,9 @@
+from os.path import join, dirname
+
+with open(join(dirname(__file__), "VERSION")) as f:
+    __version__ = f.read().strip()
+
+from .tensor import PyTorchTensor  # noqa: F401
+from .tensor import TensorFlowTensor  # noqa: F401
+from .tensor import NumPyTensor  # noqa: F401
+from .framework import *  # noqa: F401,F403
