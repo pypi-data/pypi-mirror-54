5e99be6f6f17693a6e2f60f6c57c5811f72820b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:37 +0000

    Add package idenajamajkechceszzarobicbanke/0.1.0/idenajamajkechceszzarobicbanke-0.1.0.tar.gz

---
+++ packages/idenajamajkechceszzarobicbanke/idenajamajkechceszzarobicbanke-0.1.0.tar.gz/idenajamajkechceszzarobicbanke-0.1.0/hello/__init__.py
@@ -0,0 +1,13 @@
+powitanie = "Hej hej kup se klej!"
+
+
+
+
+#pip install idenajamajkechceszzarobicbanke
+
+#python
+#import hello
+#from hello import powitanie
+#print(hello.powitanie)
+
+#/Users/karolajanowska/PycharmProjects/wysylanie_super_paczki
---
+++ packages/idenajamajkechceszzarobicbanke/idenajamajkechceszzarobicbanke-0.1.0.tar.gz/idenajamajkechceszzarobicbanke-0.1.0/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+from setuptools import find_packages, setup
+
+setup(
+    name='idenajamajkechceszzarobicbanke',
+    version='0.1.0',
+    author='Karolina',
+    author_email='doyaca@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    description='To jest opis do paczki'
+)
