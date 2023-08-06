c207b5f67025a01843ddaf6a984e80c898f2c7fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:44 +0000

    Add package hyvr/1.0.2/hyvr-1.0.2-cp37-cp37m-win_amd64.whl

---
+++ packages/hyvr/hyvr-1.0.2-cp37-cp37m-win_amd64.whl/hyvr/__main__.py
@@ -0,0 +1,19 @@
+import sys
+import hyvr
+import argparse
+
+parser = argparse.ArgumentParser(description="HyVR: Turning your geofantasy into reality!")
+parser.add_argument('--overwrite', action='store_true',
+        help='whether to overwrite the old run directory')
+parser.add_argument('filename', default=0, action="store", nargs='?',
+        help='HyVR config file. Defaults to a testcase.')
+args = parser.parse_args()
+
+
+if args.overwrite == False:
+    args.overwrite = None
+try:
+    hyvr.run(args.filename, args.overwrite)
+except Exception as e:
+    print("Something went wrong:", e, file=sys.stderr)
+    sys.exit(1)
