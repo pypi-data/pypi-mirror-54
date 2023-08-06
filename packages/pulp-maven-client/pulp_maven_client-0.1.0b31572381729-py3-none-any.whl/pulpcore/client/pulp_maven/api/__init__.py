bd1308bfb6428f4c57140c532902bfea5f1cdd8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:11 +0000

    Add package pulp-maven-client/0.1.0b31572381729/pulp_maven_client-0.1.0b31572381729-py3-none-any.whl

---
+++ packages/pulp-maven-client/pulp_maven_client-0.1.0b31572381729-py3-none-any.whl/pulpcore/client/pulp_maven/api/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from pulpcore.client.pulp_maven.api.content_artifact_api import ContentArtifactApi
+from pulpcore.client.pulp_maven.api.distributions_maven_api import DistributionsMavenApi
+from pulpcore.client.pulp_maven.api.remotes_maven_api import RemotesMavenApi
