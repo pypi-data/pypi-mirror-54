edc0f4ff354756ec9094d070e0f7611aab927dae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:01 +0000

    Add package flambe/0.4.6/flambe-0.4.6-py3-none-any.whl

---
+++ packages/flambe/flambe-0.4.6-py3-none-any.whl/flambe/metric/__init__.py
@@ -0,0 +1,15 @@
+from flambe.metric.metric import Metric
+from flambe.metric.loss.cross_entropy import MultiLabelCrossEntropy
+from flambe.metric.loss.nll_loss import MultiLabelNLLLoss
+from flambe.metric.dev.accuracy import Accuracy
+from flambe.metric.dev.perplexity import Perplexity
+from flambe.metric.dev.auc import AUC
+from flambe.metric.dev.binary import BinaryPrecision
+from flambe.metric.dev.binary import BinaryRecall
+from flambe.metric.dev.binary import BinaryAccuracy
+
+
+__all__ = ['Metric',
+           'Accuracy', 'AUC', 'Perplexity',
+           'MultiLabelCrossEntropy', 'MultiLabelNLLLoss',
+           'BinaryPrecision', 'BinaryRecall', 'BinaryAccuracy']
