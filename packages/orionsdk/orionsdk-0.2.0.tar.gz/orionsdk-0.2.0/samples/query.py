d345c6b41ce49b4520a829ec9f923875dd0f7108
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:25 +0000

    Add package orionsdk/0.2.0/orionsdk-0.2.0.tar.gz

---
+++ packages/orionsdk/orionsdk-0.2.0.tar.gz/orionsdk-0.2.0/samples/query.py
@@ -0,0 +1,20 @@
+import requests
+from orionsdk import SwisClient
+
+npm_server = 'localhost'
+username = 'admin'
+password = ''
+
+verify = False
+if not verify:
+    from requests.packages.urllib3.exceptions import InsecureRequestWarning
+    requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+
+swis = SwisClient(npm_server, username, password)
+
+print("Query Test:")
+results = swis.query("SELECT TOP 3 NodeID, DisplayName FROM Orion.Nodes")
+
+for row in results['results']:
+    print("{NodeID:<5}: {DisplayName}".format(**row))
