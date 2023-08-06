6bdd9239f9e9e037f856d04478ddf1183b2ee115
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:02 +0000

    Add package test_publish/1.0/test_publish-1.0.tar.gz

---
+++ packages/test_publish/test_publish-1.0.tar.gz/test_publish-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name="test_publish",
+    version="1.0",
+    description="this is my first demo",
+    author="test_publish",
+    author_email="777@qq.com",
+    py_modules=["test_publish.modle1", "test_publish.modle2"]
+)
