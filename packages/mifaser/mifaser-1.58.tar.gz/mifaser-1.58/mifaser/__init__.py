bea8b468e37487588d21494d25f71447f94f516e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:56 +0000

    Add package mifaser/1.58/mifaser-1.58.tar.gz

---
+++ packages/mifaser/mifaser-1.58.tar.gz/mifaser-1.58/mifaser/__init__.py
@@ -0,0 +1,14 @@
+name = "mifaser"
+__author__ = 'mmiller'
+__version__ = '1.58'
+__releasedate__ = '10/25/19'
+__all__ = [
+    'alignment',
+    'annotation',
+    'bionode',
+    'caller',
+    'cli',
+    'external',
+    'run',
+    'translate'
+]
