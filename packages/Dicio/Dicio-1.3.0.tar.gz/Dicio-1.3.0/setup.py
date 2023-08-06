5622f6d8464cdb0679d00e1d8f2d041305d36770
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:16:50 +0000

    Add package Dicio/1.3.0/Dicio-1.3.0.tar.gz

---
+++ packages/Dicio/Dicio-1.3.0.tar.gz/Dicio-1.3.0/setup.py
@@ -0,0 +1,35 @@
+import pathlib
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    name='Dicio',
+    version='1.3.0',
+    author='Felipe Pontes',
+    author_email='felipemfpontes@gmail.com',
+    packages=['dicio'],
+    test_suite='tests',
+    url='https://github.com/felipemfp/dicio',
+    license='MIT License',
+    description='Unofficial Python API for Dicio.',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Internet :: WWW/HTTP :: Browsers',
+        'Topic :: Internet :: WWW/HTTP :: Indexing/Search',
+        'Topic :: Utilities',
+    ],
+)
