a54ed363a26f131bd8f5d48e72f4d76818bc45c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:43 +0000

    Add package scikit-hubness/0.21.0a9/scikit_hubness-0.21.0a9-py3-none-any.whl

---
+++ packages/scikit-hubness/scikit_hubness-0.21.0a9-py3-none-any.whl/skhubness/reduction/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""
+The :mod:`skhubness.reduction` package provides methods for hubness reduction.
+"""
+
+from .base import NoHubnessReduction
+from .mutual_proximity import MutualProximity
+from .local_scaling import LocalScaling
+from .dis_sim import DisSimLocal
+# from .shared_neighbors import SharedNearestNeighbors, SimhubIn
+hubness_algorihtms = ['mp',
+                      'ls',
+                      'dsl',
+                      ]
+
+
+__all__ = ['NoHubnessReduction',
+           'LocalScaling',
+           'MutualProximity',
+           'DisSimLocal',
+           # 'SharedNearestNeighbors',
+           # 'SimhubIn',
+           'hubness_algorihtms',
+           ]
