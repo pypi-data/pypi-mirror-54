af7512ecb6d44ee6d9c546ea71f53acc44690890
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:42 +0000

    Add package simplified-scrapy/0.0.15/simplified_scrapy-0.0.15-py2.py3-none-any.whl

---
+++ packages/simplified-scrapy/simplified_scrapy-0.0.15-py2.py3-none-any.whl/simplified_scrapy/downloader.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+#coding=utf-8
+import json,re,importlib
+import sys
+if sys.version_info.major == 2:
+  from core.request_helper import requestPost,requestGet
+else:
+  from .core.request_helper import requestPost,requestGet
+  
+
+# from yiye_common.module_helper import import_module
+def execDownload(url,ssp):
+  headers = url.get('cookie')
+  if(not headers):
+    headers = url.get('header')
+  head=None
+  if(headers):
+    head=json.loads(headers)
+  if(url.get('requestMethod')):
+    method = url.get('requestMethod').lower()
+    if(method == 'post'):
+      return requestPost(url['url'],url.get('postData'),head,url.get('useIp')==1,ssp)
+    elif(method == 'render'):
+      return ssp.renderUrl(url)
+    elif(method == 'custom'):
+      return ssp.customDown(url)
+  return _requestGet(url,ssp,head)
+  # return requestGet(url['url'],head,url.get('useIp')==1,ssp)
+
+def _requestPost(url,ssp,header):
+  if(url.get('request_timeout')):
+    return requestPost(url['url'],url.get('postData'),header,url.get('useIp')==1,ssp,url.get('request_timeout'))
+  else:
+    return requestPost(url['url'],url.get('postData'),header,url.get('useIp')==1,ssp)
+
+def _requestGet(url,ssp,header):
+  if(url.get('request_timeout')):
+    return requestGet(url['url'],header,url.get('useIp')==1,ssp,url.get('request_timeout'))
+  else:
+    return requestGet(url['url'],header,url.get('useIp')==1,ssp)
