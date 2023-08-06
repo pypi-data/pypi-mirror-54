c737e090836158a8763afccc59a6771f6c022c7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:57 +0000

    Add package gil/1.14.0.0/gil-1.14.0.0.tar.gz

---
+++ packages/gil/gil-1.14.0.0.tar.gz/gil-1.14.0.0/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import distutils.core
+
+if __name__ == "__main__":
+    distutils.core.setup(
+        name="gil",
+        version="1.14.0.0",
+        author="Ivan Shynkarenka",
+        author_email="chronoxor@gmail.com",
+        url="https://github.com/chronoxor/gil",
+        download_url = "https://github.com/chronoxor/gil/archive/1.14.0.0.tar.gz",
+        description="Gil (git links) tool",
+        long_description="Gil (git links) tool allows to describe and manage complex git repositories dependencies with cycles and cross references",
+        license="MIT License",
+        scripts=["gil.py", "gil.bat", "gil"],
+        platforms="Linux, OSX, Windows",
+        classifiers=[
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: POSIX",
+            "Programming Language :: Python :: 3"
+        ]
+    )
