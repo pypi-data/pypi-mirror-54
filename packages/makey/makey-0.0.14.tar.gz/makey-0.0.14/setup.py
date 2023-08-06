7a714211122fb18a6c6cc4c851eede4e8c9d56e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:02 +0000

    Add package makey/0.0.14/makey-0.0.14.tar.gz

---
+++ packages/makey/makey-0.0.14.tar.gz/makey-0.0.14/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['makey']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['plumbum>=1.6.7,<2.0.0']
+
+entry_points = \
+{'console_scripts': ['makey = makey.__main__:main']}
+
+setup_kwargs = {
+    'name': 'makey',
+    'version': '0.0.14',
+    'description': 'Make libraries from GIT/url/tarball with CMAKE, and install using checkinstall.',
+    'long_description': None,
+    'author': 'Angus Hollands',
+    'author_email': 'goosey15@gmail.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
