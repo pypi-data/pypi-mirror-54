00b55b17177dd828df826586e8f8d1e2117f8528
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:18:09 +0000

    Add package pyfoster/0.1.0/pyfoster-0.1.0.tar.gz

---
+++ packages/pyfoster/pyfoster-0.1.0.tar.gz/pyfoster-0.1.0/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['pyfoster']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'pyfoster',
+    'version': '0.1.0',
+    'description': 'Python Utility Functions on Steroid',
+    'long_description': None,
+    'author': 'rednafi',
+    'author_email': 'redowan.nafi@gmail.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
