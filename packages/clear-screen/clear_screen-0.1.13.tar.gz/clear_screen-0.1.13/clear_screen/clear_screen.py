6a97333b337956a8f2946bf47616e4ecfc635822
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:11 +0000

    Add package clear-screen/0.1.13/clear_screen-0.1.13.tar.gz

---
+++ packages/clear-screen/clear_screen-0.1.13.tar.gz/clear_screen-0.1.13/clear_screen/clear_screen.py
@@ -0,0 +1,13 @@
+from subprocess import call
+from sys import platform
+
+
+def clear():
+    if platform not in ('win32', 'cygwin'):
+        command = 'clear'
+    else:
+        command = 'cls'
+    try:
+        call(command, shell=True)
+    except OSError as e:
+        print(type(e).__name__, e)
