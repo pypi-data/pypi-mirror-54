f05699401b6381c9937094159323e3499e7a5f49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:44 +0000

    Add package sntk/0.5.10/sntk-0.5.10.tar.gz

---
+++ packages/sntk/sntk-0.5.10.tar.gz/sntk-0.5.10/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='sntk',
+      version='0.5.10',
+      description='Sequence Neural ToolKit',
+      long_description='',
+      author='Valentin Perret',
+      author_email='perret.valentin@gmail.com',
+      url='https://github.com/otosystems/sntk',
+      license='',
+      install_requires=[],
+      extras_require={},
+      packages=[])
