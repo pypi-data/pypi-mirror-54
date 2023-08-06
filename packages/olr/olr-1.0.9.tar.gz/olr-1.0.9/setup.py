ad2d194432f78e3d4b03448a36e1e600d54386ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:20 +0000

    Add package olr/1.0.9/olr-1.0.9.tar.gz

---
+++ packages/olr/olr-1.0.9.tar.gz/olr-1.0.9/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="olr",
+    version="1.0.9",
+    author="Mathew Fok",
+    author_email="mfok@stevens.edu",
+    description="olr: Optimal Linear Regression",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://www.github.com/MatHatter",
+    packages=setuptools.find_packages(),
+    install_requires=['pandas', 'numpy'],
+    data_files=[('olr', ['oildata.csv'])],
+    py_modules=['__init__', 'olr_function'],
+    classifers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License",
+        "Operating System :: OS Independent",
+    ],
+)
+    
