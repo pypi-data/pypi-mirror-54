47af2acfa1f1626bbc3c2c14cb9e79bc88841c11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:11 +0000

    Add package oauth-token-cache/0.1.0/oauth-token-cache-0.1.0.tar.gz

---
+++ packages/oauth-token-cache/oauth-token-cache-0.1.0.tar.gz/oauth-token-cache-0.1.0/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['oauth_token_cache']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['redis>=3.3,<4.0', 'requests>=2.22,<3.0']
+
+setup_kwargs = {
+    'name': 'oauth-token-cache',
+    'version': '0.1.0',
+    'description': 'Easily obtain and cache OAuth 2.0 JWT tokens from Auth0.',
+    'long_description': None,
+    'author': 'Nikolai Gulatz',
+    'author_email': 'nikolai.gulatz@posteo.de',
+    'url': 'https://github.com/NikolaiGulatz/oauth-token-cache',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
