f0099c06cc529461a6e2c9e93b5a3cf97a5b3736
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:21 +0000

    Add package shield34/1.0.148/shield34-1.0.148-148-py2.py3-none-any.whl

---
+++ packages/shield34/shield34-1.0.148-148-py2.py3-none-any.whl/shield34_reporter/listeners/robot_listener.py
@@ -0,0 +1,72 @@
+from shield34_reporter.auth.sdk_authentication import SdkAuthentication
+from shield34_reporter.exceptions import Shield34PropertiesFileNotFoundException, \
+    Shield34PropertiesSyntaxIncorrect, Shield34LoginFailedException
+from .shield34_listener import Shield34Listener
+from shield34_reporter.overrider.selenium_overrider import SeleniumOverrider
+from robot.api import logger
+
+
+class RobotListener(object):
+
+    ROBOT_LISTENER_API_VERSION = 3
+
+    shield34_listener = Shield34Listener()
+
+    def __init__(self):
+        self.ROBOT_LIBRARY_LISTENER = self
+        try:
+            logger.console("Searching for shield34.ini file...")
+            SdkAuthentication.login()
+            logger.console("Logged in successfully to Shield34")
+        except Shield34PropertiesFileNotFoundException as e:
+            logger.warn(e)
+        except Shield34PropertiesSyntaxIncorrect as e:
+            logger.warn(e)
+        except Shield34LoginFailedException as e:
+            logger.warn(e)
+
+    def start_suite(self, data, result):
+        try:
+            SeleniumOverrider.override()
+            self.shield34_listener.on_suite_start(suite_name=data.name)
+        except Shield34PropertiesFileNotFoundException as e:
+            logger.warn(e)
+        except Shield34PropertiesSyntaxIncorrect as e:
+            logger.warn(e)
+        except Shield34LoginFailedException as e:
+            logger.warn(e)
+
+    def end_suite(self, data, result):
+        pass
+
+    def start_test(self, test, result):
+        try:
+            self.shield34_listener.on_test_start(test_name=test.name, test_class_name=test.longname)
+        except Shield34PropertiesFileNotFoundException as e:
+            logger.warn(e)
+        except Shield34PropertiesSyntaxIncorrect as e:
+            logger.warn(e)
+        except Shield34LoginFailedException as e:
+            logger.warn(e)
+
+    def end_test(self, data, result):
+        try:
+            if result.status == 'PASS':
+                self.shield34_listener.on_test_success()
+                self.shield34_listener.on_test_finish()
+
+            elif result.status == 'FAIL':
+                self.shield34_listener.on_test_failure(result)
+                self.shield34_listener.on_test_finish()
+
+            elif result.status == 'SKIPPED':
+                self.shield34_listener.on_test_skipped()
+                self.shield34_listener.on_test_finish()
+        except Shield34PropertiesFileNotFoundException as e:
+            logger.warn(e)
+        except Shield34PropertiesSyntaxIncorrect as e:
+            logger.warn(e)
+        except Shield34LoginFailedException as e:
+            logger.warn(e)
+
+
