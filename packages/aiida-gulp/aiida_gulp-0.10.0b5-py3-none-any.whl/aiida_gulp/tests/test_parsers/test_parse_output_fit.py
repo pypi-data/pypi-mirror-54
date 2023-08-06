87cb1ec8463722f3e98321615c76d3c1c19355e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:51 +0000

    Add package aiida-gulp/0.10.0b5/aiida_gulp-0.10.0b5-py3-none-any.whl

---
+++ packages/aiida-gulp/aiida_gulp-0.10.0b5-py3-none-any.whl/aiida_gulp/tests/test_parsers/test_parse_output_fit.py
@@ -0,0 +1,16 @@
+from aiida_gulp.tests import open_resource_text
+from aiida_gulp.parsers.raw.parse_output_fit import parse_file
+
+
+def test_parse_file_lj(data_regression):
+    with open_resource_text("gulp", "fit_lj_fes", "main.gout") as handle:
+        data, exit_code = parse_file(handle)
+    data_regression.check(data)
+    assert exit_code is None
+
+
+def test_parse_file_reaxff(data_regression):
+    with open_resource_text("gulp", "fit_reaxff_fes", "main.gout") as handle:
+        data, exit_code = parse_file(handle)
+    data_regression.check(data)
+    assert exit_code is None
