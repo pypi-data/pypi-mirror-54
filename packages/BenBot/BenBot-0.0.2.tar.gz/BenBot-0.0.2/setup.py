721d231066baa4d08ea44c970848a9ffb3c670aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:43 +0000

    Add package BenBot/0.0.2/BenBot-0.0.2.tar.gz

---
+++ packages/BenBot/BenBot-0.0.2.tar.gz/BenBot-0.0.2/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="BenBot",
+    version="0.0.2",
+    author="xMistt",
+    description="Python wrapper for BenBot API.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/xMistt/BenBot",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
