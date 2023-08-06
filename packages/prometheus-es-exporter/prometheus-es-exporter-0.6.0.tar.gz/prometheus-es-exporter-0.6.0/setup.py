9cb7891e28236783b74520c2ad8df18151e8054a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:49 +0000

    Add package prometheus-es-exporter/0.6.0/prometheus-es-exporter-0.6.0.tar.gz

---
+++ packages/prometheus-es-exporter/prometheus-es-exporter-0.6.0.tar.gz/prometheus-es-exporter-0.6.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='prometheus-es-exporter',
+    version='0.6.0',
+    description='Elasticsearch query Prometheus exporter',
+    url='https://github.com/Braedon/prometheus-es-exporter',
+    author='Braedon Vickers',
+    author_email='braedon.vickers@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Topic :: System :: Monitoring',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='monitoring prometheus exporter elasticsearch',
+    packages=find_packages(exclude=['tests']),
+    install_requires=[
+        'elasticsearch',
+        'jog',
+        'prometheus-client'
+    ],
+    entry_points={
+        'console_scripts': [
+            'prometheus-es-exporter=prometheus_es_exporter:main',
+        ],
+    },
+)
