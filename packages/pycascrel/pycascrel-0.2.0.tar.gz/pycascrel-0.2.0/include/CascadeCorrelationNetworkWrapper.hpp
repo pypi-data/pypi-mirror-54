aeec41895856b60d4edd44d15f60b84f0c2eb899
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:17:01 +0000

    Add package pycascrel/0.2.0/pycascrel-0.2.0.tar.gz

---
+++ packages/pycascrel/pycascrel-0.2.0.tar.gz/pycascrel-0.2.0/include/CascadeCorrelationNetworkWrapper.hpp
@@ -0,0 +1,33 @@
+#ifndef PYCASCREL_CASCADECORRELATIONNETWORKWRAPPER_HPP
+#define PYCASCREL_CASCADECORRELATIONNETWORKWRAPPER_HPP
+
+#include "common.hpp"
+
+#include "CascadeCorrelationNetwork.hpp"
+#include "History.hpp"
+
+class CascadeCorrelationNetworkWrapper {
+public:
+    explicit CascadeCorrelationNetworkWrapper(
+            cascrel::CascadeCorrelationNetwork&& ccn);
+
+    cascrel::History train(const Eigen::Ref<const cascrel::RowMatrix>& x,
+                           const Eigen::Ref<const cascrel::RowMatrix>& y,
+                           unsigned int patience, cascrel::Scalar tolerance,
+                           unsigned int maxHidden, cascrel::Scalar maxLoss,
+                           unsigned int safetyEpochLimit);
+
+    cascrel::RowVector evaluate(const Eigen::Ref<const cascrel::RowMatrix>& x,
+                                const Eigen::Ref<const cascrel::RowMatrix>& y) const;
+
+    cascrel::RowMatrix predict(const Eigen::Ref<const cascrel::RowMatrix>& x);
+
+    cascrel::SizeType getNumHidden() const;
+
+    void setLogLevel(cascrel::LogLevel log_level);
+
+private:
+    cascrel::CascadeCorrelationNetwork mWrappedNetwork;
+};
+
+#endif //PYCASCREL_CASCADECORRELATIONNETWORKWRAPPER_HPP
