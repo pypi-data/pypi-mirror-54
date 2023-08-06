8e7b140a242de7bf90357aa2aac5205c821d4370
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:23 +0000

    Add package gearbest-api/0.0.3/gearbest_api-0.0.3.tar.gz

---
+++ packages/gearbest-api/gearbest_api-0.0.3.tar.gz/gearbest_api-0.0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    readme = f.read()
+
+setup(
+    name='gearbest_api',
+    version='0.0.3',
+    description='Retrieve info from gearbest api.',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    url='https://github.com/matteobaldelli/python-gearbest-api',
+    license='MIT',
+    author='Matteo Baldelli',
+    author_email='baldelli.matteo2@google.com',
+    packages=['gearbest_api'],
+    install_requires=['requests'],
+    python_requires='>=3.7',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
