87743254f746f0580073381fa6b3296ec928b80b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:09 +0000

    Add package puzzle-egg/5.1/puzzle_egg-5.1-py3-none-any.whl

---
+++ packages/puzzle-egg/puzzle_egg-5.1-py3-none-any.whl/puzzle_egg/azure_key_vault.py
@@ -0,0 +1,21 @@
+#.NOTES
+#------------------------------------------------------
+# Date          	: 30-4-2019
+# Latest update     : 14-5-2019
+# Script name       : azure-key-vault.py
+# Description       : 
+# Created by        : Alice Bom
+# Copyright         : @2019 
+# Comments          : - 
+#------------------------------------------------------
+
+
+#import sys
+#temporary
+#import examples.tempparameters as tp
+
+def printtest():
+    print("werkt")
+
+
+
