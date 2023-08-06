5a51760167133885722b1ae590b05696bc3b7751
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:12 +0000

    Add package RoCheck/0.2.1/RoCheck-0.2.1.tar.gz

---
+++ packages/RoCheck/RoCheck-0.2.1.tar.gz/RoCheck-0.2.1/setup.py
@@ -0,0 +1,55 @@
+from setuptools import setup
+
+setup(
+    name='RoCheck',
+    version='0.2.1',
+    packages=['RoCheck'],
+    install_requires=[    # 依赖列表
+        'requests'
+    ],
+    author_email = 'kwok6140@outlook.com',
+    author = "Kwok6140",
+    license='MIT',
+    long_description='''
+# RoCheck
+
+This is a modules ported from grilme99's RoCheck which is written in node.js
+
+[Link to his github](https://github.com/grilme99/RoCheck)
+
+# How to install
+
+pip install RoCheck
+
+# Get started
+
+import RoCheck
+
+Checks = RoCheck.auth("ur cookie")
+
+print(Checks.get_ip('place_id', 'job_id'))
+
+print(Checks.verify_ip('4038727744',str(Checks.get_ip('place_id', 'job_id'))))
+
+> Output: 128.116.6.27 (Different depend on the server the game is running on)
+> Output: True (Return true if the ip was from roblox and that place.)
+
+# Support
+
+> If you would like support with this module then message me on Discord (Kwok6140#2416)
+
+> -Kwok6140
+
+
+# Future update
+
+We are probably gonna get more function than the original one.
+Current planning:
+> Verify if ip is from Roblox Server (Done)
+
+
+
+(check grilme99 or jacob out he's cool
+''',
+    long_description_content_type='text/markdown'
+)
