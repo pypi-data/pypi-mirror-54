15f3e25c8445b3fadc78684ddfdefe83ebddb0ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:26 +0000

    Add package PhilippeCantrelle2ADatavizAutreAvecCarteTest2/0.0.1/PhilippeCantrelle2ADatavizAutreAvecCarteTest2-0.0.1.tar.gz

---
+++ packages/PhilippeCantrelle2ADatavizAutreAvecCarteTest2/PhilippeCantrelle2ADatavizAutreAvecCarteTest2-0.0.1.tar.gz/PhilippeCantrelle2ADatavizAutreAvecCarteTest2-0.0.1/philcantrel/__init__.py
@@ -0,0 +1,5 @@
+from .Data_viz_a_packager_propre import Viz
+from .module_carte import plot_geo_time_value
+from .module_carte import plot_geo_time_value_une_image
+from .module_carte import gif
+from .test_graphe_version_py import Test
