244bf7782be05dd944846834b7157b91830b2221
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:19 +0000

    Add package ganify/1.0.0/ganify-1.0.0.tar.gz

---
+++ packages/ganify/ganify-1.0.0.tar.gz/ganify-1.0.0/setup.py
@@ -0,0 +1,24 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path('setup.py').parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="ganify",
+    version="1.0.0",
+    description="An Easy way to use GANs for data augmentation",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/arnonbruno/ganify",
+    author="Arnon Bruno",
+    author_email="asantos.quantum@gmail.com",
+    packages=['model', 'discriminator', 'generator', 'utilities'],
+    include_package_data=True,
+    install_requires=['tensorflow>=2.0.0', 'pandas>=0.25',
+                      'numpy>=1.16', 'scikit-learn>=0.21', 'matplotlib>=3.1'],
+)
