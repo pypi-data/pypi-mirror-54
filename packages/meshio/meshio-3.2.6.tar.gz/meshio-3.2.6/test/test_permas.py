d14969a014666e9ee0863b785a410470ab41f1e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:44 +0000

    Add package meshio/3.2.6/meshio-3.2.6.tar.gz

---
+++ packages/meshio/meshio-3.2.6.tar.gz/meshio-3.2.6/test/test_permas.py
@@ -0,0 +1,22 @@
+import pytest
+
+import helpers
+import meshio
+
+
+@pytest.mark.parametrize(
+    "mesh",
+    [helpers.tri_mesh, helpers.quad_mesh, helpers.tri_quad_mesh, helpers.tet_mesh],
+)
+def test_io(mesh):
+    helpers.write_read(meshio._permas.write, meshio._permas.read, mesh, 1.0e-15)
+    return
+
+
+def test_generic_io():
+    helpers.generic_io("test.post")
+    helpers.generic_io("test.post.gz")
+    # With additional, insignificant suffix:
+    helpers.generic_io("test.0.post")
+    helpers.generic_io("test.0.post.gz")
+    return
