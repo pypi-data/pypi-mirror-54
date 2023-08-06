a2087e420cdac1ef050ec2b95fbb2cb6ee96106c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:28 +0000

    Add package flask-assembly/0.0.0/flask_assembly-0.0.0-py2-none-any.whl

---
+++ packages/flask-assembly/flask_assembly-0.0.0-py2-none-any.whl/flasik/skel/create-view/views.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Flasik Views
+"""
+from flasik import (Flasik,
+                    set_page_context,
+                    get_config,
+                    abort,
+                    url_for,
+                    redirect,
+                    models,
+                    request,
+                    response,
+                    functions,
+                    utils)
+
+# ------------------------------------------------------------------------------
+
+@request.route("/%ROUTE%/")
+class Index(Flasik):
+
+    def index(self):
+        set_page_context(title="Hello World", description="Under Construction")
+        return
+
+    @response.cors()
+    @response.json 
+    def api(self):
+        return {
+            "date": functions.utc_now(),
+            "location": "NC"
+        }
