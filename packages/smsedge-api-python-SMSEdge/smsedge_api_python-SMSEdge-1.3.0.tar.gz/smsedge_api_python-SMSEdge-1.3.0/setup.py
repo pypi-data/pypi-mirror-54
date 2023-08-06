f2369a3126060b440c23756d4fb011411c288887
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:13 +0000

    Add package smsedge-api-python-SMSEdge/1.3.0/smsedge_api_python-SMSEdge-1.3.0.tar.gz

---
+++ packages/smsedge-api-python-SMSEdge/smsedge_api_python-SMSEdge-1.3.0.tar.gz/smsedge_api_python-SMSEdge-1.3.0/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="smsedge_api_python-SMSEdge", # Replace with your own username
+    version="1.3.0",
+    author="Mimon Copitman",
+    author_email="mimon@smsedge.io",
+    description="SMSEdge API package for Python development",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SMSEdge/API-Python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+    python_requires='>=3.6',
+)
