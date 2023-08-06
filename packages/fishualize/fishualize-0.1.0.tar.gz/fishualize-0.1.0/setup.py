0a1932ed2e24b7240e1bbab5e3e9dbfafcd38ab5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:34 +0000

    Add package fishualize/0.1.0/fishualize-0.1.0.tar.gz

---
+++ packages/fishualize/fishualize-0.1.0.tar.gz/fishualize-0.1.0/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fishualize",
+    version="0.1.0",
+    author="Dori Grijseels",
+    author_email="dgrijseels@outlook.com",
+    description="Python version of the fishualize package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DoriMG/py_fishualize",
+    packages=setuptools.find_packages(),
+	install_requires=[
+		'pandas>=0.19.0',
+        'numpy>=1.13.0',
+		'matplotlib>=2.0.0',
+      ],
+	include_package_data=True ,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
