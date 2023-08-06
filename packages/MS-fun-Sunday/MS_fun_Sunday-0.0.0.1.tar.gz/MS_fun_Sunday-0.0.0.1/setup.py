189ee624ac0fb8bcebd57c08bfd0d5141700c6cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:35 +0000

    Add package MS-fun-Sunday/0.0.0.1/MS_fun_Sunday-0.0.0.1.tar.gz

---
+++ packages/MS-fun-Sunday/MS_fun_Sunday-0.0.0.1.tar.gz/MS_fun_Sunday-0.0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='MS_fun_Sunday',
+    version='0.0.0.1',
+    author='MS_ET',
+    packages=find_packages(),
+    include_package_data=True,
+    description="Time flies when you don't know what you're doing"
+)
