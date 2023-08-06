e46155277a14659f22c99d70f3eb4cd7df397fab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:17 +0000

    Add package tf-tagger/0.0.5/tf_tagger-0.0.5-py3-none-any.whl

---
+++ packages/tf-tagger/tf_tagger-0.0.5-py3-none-any.whl/tf_tagger/models/embed.py
@@ -0,0 +1,25 @@
+
+import os
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+
+import numpy as np
+import tensorflow as tf
+import tensorflow_addons as tfa
+
+
+class Embed(tf.keras.Model):
+
+    def __init__(self, embedding_size, vocab_size):
+        super(Embed, self).__init__(self)
+        model = tf.keras.Sequential()
+        model.add(tf.keras.layers.Embedding(
+            vocab_size,
+            embedding_size
+        ))
+        self.model = model
+
+    def call(self, inputs):
+        x = inputs
+        x = self.model(inputs)
+        return x
+
