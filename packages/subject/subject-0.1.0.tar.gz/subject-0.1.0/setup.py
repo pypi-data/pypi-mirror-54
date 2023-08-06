872e9148bd849a4da8600fabcdd83887e79c87d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:10 +0000

    Add package subject/0.1.0/subject-0.1.0.tar.gz

---
+++ packages/subject/subject-0.1.0.tar.gz/subject-0.1.0/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+    name='subject',
+    version='0.1.0',
+    description='Django Subject',
+    author='Cochise Ruhulessin',
+    author_email='cochise.ruhulessin@digitalcitizen.nl',
+    url='https://www.digitalcitizen.nl',
+    install_requires=[],
+    packages=find_packages()
+)
