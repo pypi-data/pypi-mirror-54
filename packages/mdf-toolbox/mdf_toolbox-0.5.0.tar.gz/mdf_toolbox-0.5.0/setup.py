f45ad6c878e5887eace0c295f7016a925de4b87a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:58 +0000

    Add package mdf-toolbox/0.5.0/mdf_toolbox-0.5.0.tar.gz

---
+++ packages/mdf-toolbox/mdf_toolbox-0.5.0.tar.gz/mdf_toolbox-0.5.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+setup(
+    name='mdf_toolbox',
+    version='0.5.0',
+    packages=['mdf_toolbox'],
+    description='Materials Data Facility Python utilities',
+    long_description=("Toolbox is the Materials Data Facility Python package"
+                      " containing utility functions and other tools."),
+    install_requires=[
+        "fair-research-login>=0.1.5",
+        "globus_nexus_client>=0.2.8",
+        "globus-sdk>=1.7.0",
+        "requests>=2.18.4"
+    ],
+    python_requires=">=3.4",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+    ],
+    keywords=[
+        "MDF",
+        "Materials Data Facility",
+        "materials science",
+        "utility"
+    ],
+    license="Apache License, Version 2.0",
+    url="https://github.com/materials-data-facility/toolbox"
+)
