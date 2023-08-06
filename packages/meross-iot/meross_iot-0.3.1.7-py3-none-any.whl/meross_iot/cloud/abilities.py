b30621985291de0bec4399f9c9f307dd827a4177
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:04 +0000

    Add package meross-iot/0.3.1.7/meross_iot-0.3.1.7-py3-none-any.whl

---
+++ packages/meross-iot/meross_iot-0.3.1.7-py3-none-any.whl/meross_iot/cloud/abilities.py
@@ -0,0 +1,22 @@
+# Common abilities
+ALL = 'Appliance.System.All'
+ABILITY = 'Appliance.System.Ability'
+REPORT = 'Appliance.System.Report'
+ONLINE = 'Appliance.System.Online'
+WIFI_LIST = 'Appliance.Config.WifiList'
+DEBUG = 'Appliance.System.Debug'
+TRACE = 'Appliance.Config.Trace'
+
+# Power plug/bulbs abilities
+TOGGLE = 'Appliance.Control.Toggle'
+TOGGLEX = 'Appliance.Control.ToggleX'
+TRIGGER = 'Appliance.Control.Trigger'
+TRIGGERX = 'Appliance.Control.TriggerX'
+ELECTRICITY = 'Appliance.Control.Electricity'
+CONSUMPTIONX = 'Appliance.Control.ConsumptionX'
+
+# Garage opener abilities
+GARAGE_DOOR_STATE = 'Appliance.GarageDoor.State'
+
+# Bulbs-only abilities
+LIGHT = 'Appliance.Control.Light'
