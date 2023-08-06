3ad845b2b9c7a789855037b29727b66b6dec3607
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:31 +0000

    Add package qcelemental/0.11.0/qcelemental-0.11.0.tar.gz

---
+++ packages/qcelemental/qcelemental-0.11.0.tar.gz/qcelemental-0.11.0/qcelemental/models/__init__.py
@@ -0,0 +1,14 @@
+try:
+    import pydantic
+except ImportError:  # pragma: no cover
+    raise ImportError("Python module pydantic not found. Solve by installing it: "
+                      "`conda install pydantic -c conda-forge` or `pip install pydantic`")
+
+from . import types
+from .align import AlignmentMill
+from .basemodels import AutodocBaseSettings, ProtoModel
+from .basis import BasisSet
+from .common_models import ComputeError, FailedOperation, Provenance
+from .molecule import Molecule
+from .procedures import Optimization, OptimizationInput
+from .results import Result, ResultInput, ResultProperties
