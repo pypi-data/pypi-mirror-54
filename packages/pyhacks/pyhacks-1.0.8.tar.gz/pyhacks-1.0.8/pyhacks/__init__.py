8e65c34bece24aa8eb13d3fda15a170eccb55752
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:16:30 +0000

    Add package pyhacks/1.0.8/pyhacks-1.0.8.tar.gz

---
+++ packages/pyhacks/pyhacks-1.0.8.tar.gz/pyhacks-1.0.8/pyhacks/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+"""
+Pyhacks library
+~~~~~~~~~~~~~~~~~~~~~
+
+:copyright: (c) 2019 by Barak Tawily.
+"""
+
+__title__ = 'pyhacks'
+__version__ = '1.0.7'
+__build__ = 0x01007
+__author__ = 'Barak Tawily'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2019 Barak Tawily'
+
+from .QueueThreads import QueueThreads
+from .Parser import Parser
+from .Logger import Logger
