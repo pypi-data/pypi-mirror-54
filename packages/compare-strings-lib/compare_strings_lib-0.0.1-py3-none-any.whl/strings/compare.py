76c4752c1682c2adedba1c5f6a5983a37bec048e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:25 +0000

    Add package compare-strings-lib/0.0.1/compare_strings_lib-0.0.1-py3-none-any.whl

---
+++ packages/compare-strings-lib/compare_strings_lib-0.0.1-py3-none-any.whl/strings/compare.py
@@ -0,0 +1,30 @@
+from .custom_exceptions import InvalidValues
+
+
+def compare(str1, str2):
+    '''compare strings.
+
+    params:
+    str1 -- string version 1
+    str2 -- string version 2
+    the strings are a dot seperated integers.
+
+    return:
+    1 if str1 = str2
+    0 if str1 > str1
+    -1 if str1 < str2
+
+    raise invalidValues exception if non integers value provided
+    '''
+    try:
+        str_v1 = float(''.join(str1.split('.')).replace(' ', ''))
+        str_v2 = float(''.join(str2.split('.')).replace(' ', ''))
+        if str_v2 == str_v1 and len(str1) == len(str2):
+            return 1
+        elif str_v1 > str_v2 or len(str1) > len(str2):
+            return 0
+        else:
+            return -1
+    except Exception:
+        raise InvalidValues(
+            f'invalid params. can not compare {str1} to {str2}')
