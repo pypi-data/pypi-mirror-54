e513db59e7014b8f0a860d74d7b7a310cc74a9e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:20 +0000

    Add package easydbio/0.1.1/easydbio-0.1.1-py3-none-any.whl

---
+++ packages/easydbio/easydbio-0.1.1-py3-none-any.whl/easydbio/__init__.py
@@ -0,0 +1,39 @@
+import requests
+
+name = "easydb-io"
+
+class DB:
+    def __init__(self, config):
+        if not config:
+            raise Exception("Config is required")
+
+        db = config.get("database")
+        token = config.get("token")
+        if not db:
+            raise Exception("Database must be provided")
+
+        if not token:
+            raise Exception("Token must be provided")
+        self.database = db
+        self.token = token
+
+    def Get(self, key):
+        r = requests.get('https://app.easydb.io/database/{}/{}'.format(self.database, key))
+        return r.json()
+
+    def Put(self, key, value):
+        data = {
+            "token": self.token,
+            "value": value
+        }
+        r = requests.post('https://app.easydb.io/database/{}/{}'.format(self.database, key), json=data)
+    
+    def Delete(self, key):
+        data = {
+            "token": self.token,
+        }
+        r = requests.delete('https://app.easydb.io/database/{}/{}'.format(self.database, key), json=data)
+        
+    def List(self):
+        r = requests.get('https://app.easydb.io/database/{}'.format(self.database))
+        return r.json()
