2165be9990c438b3e5789f94f94d83b34ea35bae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:46 +0000

    Add package useragentxxc/1.1.0/useragentxxc-1.1.0.tar.gz

---
+++ packages/useragentxxc/useragentxxc-1.1.0.tar.gz/useragentxxc-1.1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(name='useragentxxc',
+      version='1.1.0',
+      description='Random generation of User agent for PC and mobile',
+      url='https://github.com/clearDreamXXC/user-agent',
+      author='clearDreamXXC',
+      author_email='1586616064@qq.com',
+      license='MIT',
+      packages=['useragentxxc'])
