4f72aafae0a698cb9fdcc6d76b78a605b7b979ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:03 +0000

    Add package blahblahblah/0.1/blahblahblah-0.1.tar.gz

---
+++ packages/blahblahblah/blahblahblah-0.1.tar.gz/blahblahblah-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='blahblahblah',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['blahblahblah'],
+      zip_safe=False)
