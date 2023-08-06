ee39598d7fc53b2a80f1359868087f9fffbd2614
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:21 +0000

    Add package loman/0.3.0/loman-0.3.0.tar.gz

---
+++ packages/loman/loman-0.3.0.tar.gz/loman-0.3.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='loman',
+    version='0.3.0',
+    packages=['loman'],
+    url='https://github.com/janusassetallocation/loman',
+    license='BSD',
+    author='Ed Parcell',
+    author_email='edparcell@gmail.com',
+    description='Loman tracks state of computations, and the dependencies between them, allowing full and partial recalculations.',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
+    install_requires=['six', 'dill', 'pydotplus', 'networkx', 'pandas', 'matplotlib'],
+)
