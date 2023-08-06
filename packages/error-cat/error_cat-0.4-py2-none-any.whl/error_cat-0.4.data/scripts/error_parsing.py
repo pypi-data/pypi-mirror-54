16208fd6b62817bcbf5d774ca36c082364430dd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:05 +0000

    Add package error-cat/0.4/error_cat-0.4-py2-none-any.whl

---
+++ packages/error-cat/error_cat-0.4-py2-none-any.whl/error_cat-0.4.data/scripts/error_parsing.py
@@ -0,0 +1,29 @@
+# A module to parse python tracebacks
+
+## This module needs to be made more defensive
+
+#strip bad characters
+def strip_bad(string):
+    bad = ['(', ')', '&', '*', '^', '$', '<', '>', '\\', '/']
+    return ''.join([ch for ch in string if ch not in bad]).strip()
+
+
+
+
+def format_error(err):
+    
+    split_err = err.split('\n')
+
+    if split_err[-2].find('SyntaxError') != -1:
+
+        error_type = strip_bad(split_err[-2])
+        error_info = strip_bad(split_err[-5])
+        
+    else:
+        
+        error_type = strip_bad(split_err[-2])
+        error_info = strip_bad(split_err[-4])
+
+    return error_info + '--' + error_type
+
+
