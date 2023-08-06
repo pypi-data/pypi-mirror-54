f39ff43d0325962bdd1a62bb1c07dcbf3ef3fb6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:17:53 +0000

    Add package fastemc/0.0.1/fastemc-0.0.1.tar.gz

---
+++ packages/fastemc/fastemc-0.0.1.tar.gz/fastemc-0.0.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fastemc",
+    version="0.0.1",
+    author="James Rowland",
+    author_email="rowland.208@gmail.com",
+    description="FastEMC is a method for dimensionality reduction.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rowland-208/fastemc",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+)
