e486d9c52da5488eb870630116df3c31f3f1ad6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:09 +0000

    Add package elliesImgurUploader/0.2/elliesImgurUploader-0.2.tar.gz

---
+++ packages/elliesImgurUploader/elliesImgurUploader-0.2.tar.gz/elliesImgurUploader-0.2/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+setup(
+    name='elliesImgurUploader',
+    packages = ['elliesImgurUploader'],
+    version='0.2',
+    license='idgaf',
+    description='A small library for uploading imgur albums',
+    author='Ellie Mae Schipper',
+    url='https://github.com/lifesgood123/elliesImgurUploader',
+    download_url="https://github.com/Lifesgood123/elliesImgurUploader/archive/0.2.tar.gz",
+    install_requires=[
+        'requests',
+        'tqdm'
+    ],
+    classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3.7',
+  ]
+)
