3516b203f37ae1227bb1ce684265b301b9ab8e85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:49 +0000

    Add package raspend/1.4.0/raspend-1.4.0.tar.gz

---
+++ packages/raspend/raspend-1.4.0.tar.gz/raspend-1.4.0/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#  
+#  License: MIT
+#  
+#  Copyright (c) 2019 Joerg Beckers
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="raspend",
+    version="1.4.0",
+    author="Joerg Beckers",
+    author_email="pypi@jobe-software.de",
+    description="A small and easy to use HTTP backend framework for the Raspberry Pi which is ideal for small to medium-sized home automation projects.",
+    license="MIT",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jobe3774/raspend.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Home Automation",
+    ],
+    python_requires='>=3.5',
+)
