d67a9c568ccd2a64c45e150265c9147411976137
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:56 +0000

    Add package musket-text/0.42/musket_text-0.42.tar.gz

---
+++ packages/musket-text/musket_text-0.42.tar.gz/musket_text-0.42/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+import setuptools
+setup(name='musket_text',
+      version='0.42',
+      description='Common parts of my pipelines',
+      url='https://github.com/petrochenko-pavel-a/musket_core',
+      author='Petrochenko Pavel',
+      author_email='petrochenko.pavel.a@gmail.com',
+      license='MIT',
+      packages=setuptools.find_packages(),
+      include_package_data=True,
+      dependency_links=['https://github.com/aleju/imgaug'],
+      install_requires=["numpy", "scipy","Pillow", "cython","pandas","matplotlib", "scikit-image","keras>=2.2.4","imageio",
+"opencv-python",
+"h5py",
+"tqdm",
+"segmentation_models", "lightgbm", "async_promises"],
+      zip_safe=False)
