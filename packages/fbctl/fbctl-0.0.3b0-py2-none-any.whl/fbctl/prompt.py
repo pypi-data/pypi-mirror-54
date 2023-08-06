49ae8634aeda113a2258f6f66e2050bf7c729016
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:15 +0000

    Add package fbctl/0.0.3b0/fbctl-0.0.3b0-py2-none-any.whl

---
+++ packages/fbctl/fbctl-0.0.3b0-py2-none-any.whl/fbctl/prompt.py
@@ -0,0 +1,27 @@
+from config import get_cur_cluster_id
+
+
+def get_cli_prompt(user):
+    """Return cli prompt
+
+    :param user: user name
+    :return: prompt string
+    """
+    cluster_id = get_cur_cluster_id(allow_empty_id=True)
+    if cluster_id < 0:
+        cluster_id = '-'
+    return '{}@flashbase:{}>'.format(user, cluster_id)
+    # return '%s@flashbase:%d>' % (user, cluster_id)
+
+
+def get_sql_prompt(user):
+    """Return sql prompt
+
+    :param user: user name
+    :return: prompt string
+    """
+    cluster_id = get_cur_cluster_id(allow_empty_id=True)
+    if cluster_id < 0:
+        cluster_id = '-'
+    return '({}){}@flashbase:sql>'.format(cluster_id, user)
+    # return '%s@flashbase:sql:%s>' % (user, cluster_id)
