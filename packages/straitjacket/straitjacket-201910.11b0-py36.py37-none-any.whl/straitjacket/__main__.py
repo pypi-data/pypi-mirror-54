ecba55a1ebeb9a02b6e7c067ec41b13d481b040c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:26 +0000

    Add package straitjacket/201910.11b0/straitjacket-201910.11b0-py36.py37-none-any.whl

---
+++ packages/straitjacket/straitjacket-201910.11b0-py36.py37-none-any.whl/straitjacket/__main__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# This file is part of the straitjacket project
+# https://gitlab.com/mbarkhau/straitjacket
+#
+# Copyright (c) 2019 Manuel Barkhau (mbarkhau@gmail.com) - MIT License
+# SPDX-License-Identifier: MIT
+
+import os
+import sys
+
+from . import sjfmt
+
+
+# To enable pretty tracebacks:
+#   echo "export ENABLE_BACKTRACE=1;" >> ~/.bashrc
+if os.environ.get('ENABLE_BACKTRACE') == '1':
+    import backtrace
+
+    backtrace.hook(align=True, strip_path=True, enable_on_envvar_only=True)
+
+
+if __name__ == '__main__':
+    sjfmt.main()
+    sys.exit(0)
