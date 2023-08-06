3b96895eba3da2cf6581a20574fba74e4e129ec6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:59 +0000

    Add package blogger-cli/1.2.1/blogger_cli-1.2.1-py3-none-any.whl

---
+++ packages/blogger-cli/blogger_cli-1.2.1-py3-none-any.whl/blogger_cli/commands/cmd_serve.py
@@ -0,0 +1,48 @@
+import os
+import socketserver
+import http.server
+
+import click
+from blogger_cli.cli import pass_context
+
+
+@click.command("serve", short_help="Serve your blog locally")
+@click.argument("blog", required=False)
+@click.option("--port", "-p", "port", type=int, default=8000)
+@click.option("-d", "--dir", "dir", help="Folder path to serve. Default: blog_dir")
+@click.option("-v", "--verbose", is_flag=True, help="Enable verbosity")
+@pass_context
+def cli(ctx, blog, port, dir, verbose):
+    ctx.verbose = verbose
+    ctx.vlog("\n:: GOT blog:", blog, "port:", str(port), "(", type(port), ")")
+
+    if not blog:
+        blog = ctx.default_blog
+        ctx.vlog(":: No blog name given using default blog:", str(blog))
+    if not blog:
+        ctx.log("Use blogger serve <blogname> or set a default blog in configs")
+        ctx.log("ERROR: Missing required argument 'BLOG' ")
+        raise SystemExit()
+
+    blog_dir = ctx.config.read(key=blog + ":blog_dir")
+    if dir:
+        blog_dir = dir
+
+    if not blog_dir:
+        ctx.log("No blog_dir set blog_dir in config or use --dir option")
+        ctx.log("ERROR: No folder specified")
+        raise SystemExit()
+
+    blog_dir = os.path.abspath(os.path.expanduser(blog_dir))
+    ctx.vlog(":: Got blog_dir:", blog_dir)
+    ctx.log(":: Serving at http://localhost:" + str(port) + "/")
+    ctx.log(":: Exit using CTRL+C")
+    serve_locally(blog_dir, port)
+
+
+def serve_locally(dir, PORT):
+    web_dir = dir
+    os.chdir(web_dir)
+    Handler = http.server.SimpleHTTPRequestHandler
+    httpd = socketserver.TCPServer(("", PORT), Handler)
+    httpd.serve_forever()
