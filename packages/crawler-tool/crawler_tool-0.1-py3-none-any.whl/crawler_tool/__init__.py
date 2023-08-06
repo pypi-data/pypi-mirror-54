e0fc8d09d08a2db07e0efde5e38e42920b3fd052
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:37 +0000

    Add package crawler-tool/0.1/crawler_tool-0.1-py3-none-any.whl

---
+++ packages/crawler-tool/crawler_tool-0.1-py3-none-any.whl/crawler_tool/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+__version__ = "0.1"
+
+# 布隆过滤器
+from crawler_tool.bloomfilter import BloomFilter
+# 获取User-Agent
+from crawler_tool.fack_header import UserAgent
---
+++ packages/crawler-tool/crawler_tool-0.1-py3-none-any.whl/crawler_tool/fack_header.py
@@ -0,0 +1,78 @@
+# -*-coding: utf-8 -*-
+
+win_user_gent = [
+    # Windows10 / Chrome 75.0.3770.142
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36",
+    # Windows10 / Firefox 69.0b15
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:69.0) Gecko/20100101 Firefox/69.0",
+    # Windows10 / Opera 63.0.3368.43
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36 OPR/63.0.3368.43",
+    # Windows10 / Edge 44.18362.1.0
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.18362",
+    # Windows10 / IE 11.10000.18362.0
+    "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; LCTE; rv:11.0) like Gecko",
+    # Windows10 x64 / Safari 5.1.4（7534.54.16）
+    "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/534.54.16 (KHTML, like Gecko) Version/5.1.4 Safari/534.54.16",
+    # Windows10 / QQ浏览器 10.5（3739）
+    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.25 Safari/537.36 Core/1.70.3722.400 QQBrowser/10.5.3739.400",
+    # Windows10 / 360安全浏览器 10.0.1977.0
+    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36 QIHU 360SE",
+    # Windows10 / 360极速浏览器 11.0.2179.0
+    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36 QIHU 360EE",
+    # Windows10 / UC浏览器 6.2.3964.2
+    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 UBrowser/6.2.3964.2 Safari/537.36",
+    # Windows10 / 搜狗浏览器 8.5.10.31270
+    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 SE 2.X MetaSr 1.0",
+    # Windows10 / 猎豹浏览器 6.5.115.19331.8001
+    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.98 Safari/537.36 LBBROWSER",
+    # Windows10 / 傲游浏览器 5.2.7.5000
+    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.79 Safari/537.36",
+    # Windows10 / 2345加速浏览器 10.1.0.19399
+    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3947.100 Safari/537.36",
+]
+
+android_user_gent = [
+    # Android / Chrome 76.0.3809.111
+    "Mozilla/5.0 (Linux; Android 7.1.1; OPPO R9sk) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.111 Mobile Safari/537.36"
+    # Android / Firefox 68.0.2
+    "Mozilla/5.0 (Android 7.1.1; Mobile; rv:68.0) Gecko/68.0 Firefox/68.0",
+    # Android / Opera 53.0.2569.141117
+    "Mozilla/5.0 (Linux; Android 7.1.1; OPPO R9sk Build/NMF26F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Mobile Safari/537.36 OPR/53.0.2569.141117",
+    # Android / Edge 42.0.2.3819
+    "Mozilla/5.0 (Linux; Android 7.1.1; OPPO R9sk) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.90 Mobile Safari/537.36 EdgA/42.0.2.3819",
+    # Android / QQ浏览器 9.6.1.5190
+    "Mozilla/5.0 (Linux; U; Android 7.1.1; zh-cn; OPPO R9sk Build/NMF26F) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/66.0.3359.126 MQQBrowser/9.6 Mobile Safari/537.36",
+    # Android / OPPO浏览器 10.5.1.2_2c91537
+    "Mozilla/5.0 (Linux; U; Android 7.1.1; zh-cn; OPPO R9sk Build/NMF26F) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/70.0.3538.80 Mobile Safari/537.36 OppoBrowser/10.5.1.2",
+    # Android / 360浏览器 8.2.0.162
+    "Mozilla/5.0 (Linux; Android 7.1.1; OPPO R9sk Build/NMF26F; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/62.0.3202.97 Mobile Safari/537.36",
+    # Android / 360极速浏览器 1.0.100.1078
+    "Mozilla/5.0 (Linux; Android 7.1.1; OPPO R9sk Build/NMF26F) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/70.0.3538.80 Mobile Safari/537.36 360 Alitephone Browser (1.5.0.90/1.0.100.1078) mso_sdk(1.0.0)",
+    # Android / UC浏览器 12.6.0.1040
+    "Mozilla/5.0 (Linux; U; Android 7.1.1; zh-CN; OPPO R9sk Build/NMF26F) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/57.0.2987.108 UCBrowser/12.6.0.1040 Mobile Safari/537.36",
+    # Android / 猎豹浏览器 5.12.3
+    "Mozilla/5.0 (Linux; Android 7.1.1; OPPO R9sk Build/NMF26F; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/70.0.3538.80 Mobile Safari/537.36 LieBaoFast/5.12.3",
+    # Android / 百度浏览器 7.19
+    "Mozilla/5.0 (Linux; Android 7.1.1; OPPO R9sk Build/NMF26F; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/48.0.2564.116 Mobile Safari/537.36 T7/9.1 baidubrowser/7.19.13.0 (Baidu; P1 7.1.1)",
+    # Android / 搜狗浏览器 5.22.8.71677
+    "Mozilla/5.0 (Linux; Android 7.1.1; OPPO R9sk Build/NMF26F; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/68.0.3440.106 Mobile Safari/537.36 AWP/2.0 SogouMSE,SogouMobileBrowser/5.22.8",
+    # Android / 2345浏览器 11.0.1
+    "Mozilla/5.0 (Linux; Android 7.1.1; OPPO R9sk Build/NMF26F; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/70.0.3538.80 Mobile Safari/537.36 Mb2345Browser/11.0.1",
+]
+
+import random
+
+class UserAgent():
+    def __init__(self):
+        self.win_user_agents = win_user_gent
+        self.android_user_gent = android_user_gent
+
+    def random_header(self, system="windows"):
+        if system == "windows":
+            user_agent = random.choice(self.win_user_agents)
+        elif system == "android":
+            user_agent = random.choice(self.android_user_gent)
+        else:
+            user_agent = random.choice(self.win_user_agents)
+        return user_agent
+
