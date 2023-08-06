6c16f1cc1ade1226dd154e33a7864823c1a65892
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:34 +0000

    Add package TR-pypi-project/1.0.0/TR_pypi_project-1.0.0.tar.gz

---
+++ packages/TR-pypi-project/TR_pypi_project-1.0.0.tar.gz/TR_pypi_project-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'TR_pypi_project',
+    version = '1.0.0',
+    include_package_data = True,
+    package_dir = {'':'.'},
+    description = 'Hello it is printing hello'
+)
