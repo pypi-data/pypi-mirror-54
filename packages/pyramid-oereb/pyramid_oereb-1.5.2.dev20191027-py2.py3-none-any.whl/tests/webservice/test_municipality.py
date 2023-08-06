6c49fbf99a389238cc3239f393bbd68cb4a42545
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:16:52 +0000

    Add package pyramid-oereb/1.5.2.dev20191027/pyramid_oereb-1.5.2.dev20191027-py2.py3-none-any.whl

---
+++ packages/pyramid-oereb/pyramid_oereb-1.5.2.dev20191027-py2.py3-none-any.whl/tests/webservice/test_municipality.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import pytest
+from pyramid.httpexceptions import HTTPNotFound
+from pyramid.response import Response
+
+from tests.mockrequest import MockRequest
+from pyramid_oereb.views.webservice import Municipality
+
+
+def test_get_image():
+    request = MockRequest()
+    request.matchdict.update({
+        'fosnr': '1234'
+    })
+    webservice = Municipality(request)
+    result = webservice.get_image()
+    assert isinstance(result, Response)
+    if sys.version_info.major == 2:
+        assert result.body == 'abcdefg'
+    else:
+        assert result.body == b'abcdefg'
+
+
+def test_get_image_invalid():
+    request = MockRequest()
+    request.matchdict.update({
+        'fosnr': '0'
+    })
+    webservice = Municipality(request)
+    with pytest.raises(HTTPNotFound):
+        webservice.get_image()
