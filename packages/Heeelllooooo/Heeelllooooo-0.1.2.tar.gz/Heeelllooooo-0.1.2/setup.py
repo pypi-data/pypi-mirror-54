b51ceb2c559aa0ea97ec42485d2bf105ed7bccd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:40 +0000

    Add package Heeelllooooo/0.1.2/Heeelllooooo-0.1.2.tar.gz

---
+++ packages/Heeelllooooo/Heeelllooooo-0.1.2.tar.gz/Heeelllooooo-0.1.2/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+from setuptools import find_packages, setup
+
+setup(
+    name='Heeelllooooo',
+    version='0.1.2',
+    author='Marcin Trzaska',
+    author_email='maniu190.94@o2.pl',
+    packages=find_packages(),
+    include_package_data=True,
+    description='Pokochasz tą paczke od pierwszych chwil <3 <3 <3'
+)
