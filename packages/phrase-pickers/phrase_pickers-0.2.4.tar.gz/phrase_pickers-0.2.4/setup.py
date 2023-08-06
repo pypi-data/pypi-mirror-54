8a772a82d8942aa23a38fad9c2f9d8300e962961
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:53 +0000

    Add package phrase-pickers/0.2.4/phrase_pickers-0.2.4.tar.gz

---
+++ packages/phrase-pickers/phrase_pickers-0.2.4.tar.gz/phrase_pickers-0.2.4/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+from subprocess import check_call
+import os
+
+
+setup(name='phrase_pickers',
+      version='0.2.4',
+      description='NLP helpers to parse noun or verb phrases in a sentence using spacy parse tree.',
+      url='https://github.com/LocusAnalytics/phrase_pickers',
+      author='CAC',
+      author_email='cac@locus.co',
+      license='Proprietary',
+      packages=['phrase_pickers'],
+      package_data={
+          'phrase_pickers': [
+              os.path.join('res', 'morpho_semantic_words.csv.gz'),
+              os.path.join('res', 'stopwords.txt.gz'),
+              os.path.join('res', 'undesirable_words.json.gz')
+          ]
+      },
+      install_requires=['spacy==2.1.3'],
+      zip_safe=False,
+      python_requires=">=3.6")
+
+try:
+    import spacy
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    print('Downloading language model for the spaCy POS tagger\n'
+          "(don't worry, this will only happen once)")
+    from spacy.cli import download
+    download("en_core_web_sm")
