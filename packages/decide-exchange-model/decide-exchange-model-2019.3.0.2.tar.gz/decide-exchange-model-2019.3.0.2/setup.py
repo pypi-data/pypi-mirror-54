094a4090ac8bc4c5efb3ea1d98c821b506d9be84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:17 +0000

    Add package decide-exchange-model/2019.3.0.2/decide-exchange-model-2019.3.0.2.tar.gz

---
+++ packages/decide-exchange-model/decide-exchange-model-2019.3.0.2.tar.gz/decide-exchange-model-2019.3.0.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+from decide import __version__
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="decide-exchange-model",
+    version=__version__,
+    packages=find_packages(),
+    url="https://github.com/foarsitter/decide-exchange-model",
+    license="GPL-3.0",
+    author="jelmert",
+    author_email="info@jelmert.nl",
+    description="Model of collective decision making",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    entry_points={
+        "console_scripts": ["decide-cli=decide.cli:main"],
+        "gui_scripts": ["decide-gui=decide.gui:main"],
+    },
+    # data_files=[('data/input', ['data/input/kopenhagen.csv', 'data/input/CoP21.csv'])],
+    python_requires=">=3.6",
+    install_requires=[
+        "blinker==1.4",
+        "environs==6.0.0",
+        "matplotlib==3.0.2",
+        "requests==2.21.0",
+        "typesystem==0.2.2"
+    ],
+)
