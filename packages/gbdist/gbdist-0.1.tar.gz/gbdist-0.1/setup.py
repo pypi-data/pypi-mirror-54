736e43d5eb84018308a33a92b7720d2aee04257d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:16:42 +0000

    Add package gbdist/0.1/gbdist-0.1.tar.gz

---
+++ packages/gbdist/gbdist-0.1.tar.gz/gbdist-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='gbdist',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['gbdist'],
+      author='Rahul Khairnar',
+      zip_safe=False)
