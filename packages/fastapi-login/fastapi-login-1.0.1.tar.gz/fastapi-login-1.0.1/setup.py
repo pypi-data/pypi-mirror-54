dce91c80b1a98f70fd0989fdf3f4ae0e1fc0b538
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:26 +0000

    Add package fastapi-login/1.0.1/fastapi-login-1.0.1.tar.gz

---
+++ packages/fastapi-login/fastapi-login-1.0.1.tar.gz/fastapi-login-1.0.1/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+with open('requirements.txt', 'r') as f:
+    packages = [pkg.split('==')[0] for pkg in f.readlines()]
+
+
+setuptools.setup(
+    name="fastapi-login",
+    version="1.0.1",
+    author="Max Rausch-Dupont",
+    author_email="maxrd79@gmail.com",
+    descritpion="Flask-Login like package for FastAPI",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MushroomMaula/fastapi_login",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    packages=setuptools.find_packages(),
+    install_requires=packages,
+    zip_safe=False,
+    include_package_data=True
+)
