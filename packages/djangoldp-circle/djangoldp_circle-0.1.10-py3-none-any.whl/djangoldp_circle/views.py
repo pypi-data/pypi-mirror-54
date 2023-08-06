1961eec27580249c4a73610f2459aab1a4ad257d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:08 +0000

    Add package djangoldp-circle/0.1.10/djangoldp_circle-0.1.10-py3-none-any.whl

---
+++ packages/djangoldp-circle/djangoldp_circle-0.1.10-py3-none-any.whl/djangoldp_circle/views.py
@@ -0,0 +1,10 @@
+from djangoldp.views import LDPViewSet
+from .models import Circle
+
+
+class CirclesJoinableViewset(LDPViewSet):
+    model = Circle
+
+    def get_queryset(self):
+        return super().get_queryset() \
+                      .exclude(team__id=self.request.user.id)
