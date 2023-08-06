453f4cd0ba2c8187d2089865c13302311c37babf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:14 +0000

    Add package pyscaffoldext-dsproject/0.4/pyscaffoldext-dsproject-0.4.tar.gz

---
+++ packages/pyscaffoldext-dsproject/pyscaffoldext-dsproject-0.4.tar.gz/pyscaffoldext-dsproject-0.4/CHANGELOG.rst
@@ -0,0 +1,36 @@
+=========
+Changelog
+=========
+
+Version 0.4
+===========
+
+- Add a `.gitignore` also in the `data` folder
+- Changed wording in `README.md`
+
+Version 0.3.1
+=============
+
+- Fixed wrong folder name in README.md template
+
+Version 0.3
+===========
+
+- Added a configs folder
+
+Version 0.2.1
+=============
+
+- Fixed some docs
+- Adjusted line lengths
+- Some cosmetic changes
+
+Version 0.2
+===========
+
+- Reworked the general layout some more
+
+Version 0.1
+===========
+
+- Initial commit
---
+++ packages/pyscaffoldext-dsproject/pyscaffoldext-dsproject-0.4.tar.gz/pyscaffoldext-dsproject-0.4/src/pyscaffoldext/dsproject/templates/__init__.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+import string
+from pkg_resources import resource_string
+
+from .. import __version__ as dsproject_version
+
+
+def get_template(name):
+    """Retrieve the template by name
+
+    Args:
+        name: name of template
+
+    Returns:
+        :obj:`string.Template`: template
+    """
+    file_name = "{name}.template".format(name=name)
+    data = resource_string("pyscaffoldext.dsproject.templates",
+                           file_name)
+    return string.Template(data.decode("UTF-8"))
+
+
+def gitignore_all(opts):
+    """gitignore file that ignores just everything
+
+    Ignore everything except of this gitignore file.
+
+    Args:
+        opts (dict): given options, see :obj:`create_project` for
+            an extensive list.
+
+    Returns:
+        str: file content as string
+    """
+    template = get_template("gitignore_all")
+    return template.safe_substitute(opts)
+
+
+def gitignore_data(opts):
+    """gitignore file that ignores almost everything
+
+    Ignore everything except of gitignore also in sub directories.
+
+    Args:
+        opts (dict): given options, see :obj:`create_project` for
+            an extensive list.
+
+    Returns:
+        str: file content as string
+    """
+    template = get_template("gitignore_data")
+    return template.safe_substitute(opts)
+
+
+def environment_yaml(opts):
+    """Environment.yaml with some basic libraries
+
+    Args:
+        opts (dict): given options, see :obj:`create_project` for
+            an extensive list.
+
+    Returns:
+        str: file content as string
+    """
+    template = get_template("environment_yaml")
+    return template.safe_substitute(opts)
+
+
+def readme_md(opts):
+    """Adds a basic README.md
+
+    Args:
+        opts (dict): given options, see :obj:`create_project` for
+            an extensive list.
+
+    Returns:
+        str: file content as string
+    """
+    template = get_template("readme_md")
+    opts['pkg'] = opts['package'].ljust(19)
+    opts['dsproject_version'] = dsproject_version
+    return template.safe_substitute(opts)
+
+
+def template_ipynb(opts):
+    """Adds a template Jupyter notebook
+
+    Args:
+        opts (dict): given options, see :obj:`create_project` for
+            an extensive list.
+
+    Returns:
+        str: file content as string
+    """
+    template = get_template("template_ipynb")
+    return template.safe_substitute(opts)
+
+
+def train_model_py(opts):
+    """Adds a template python experiment
+
+    Args:
+        opts (dict): given options, see :obj:`create_project` for
+            an extensive list.
+
+    Returns:
+        str: file content as string
+    """
+    template = get_template("train_model_py")
+    return template.safe_substitute(opts)
