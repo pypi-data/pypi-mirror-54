363cadf9f5984de9b6c3731ab4b1b3466a24cd25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:16 +0000

    Add package dl-frame/0.0.0.8/dl_frame-0.0.0.8-py3-none-any.whl

---
+++ packages/dl-frame/dl_frame-0.0.0.8-py3-none-any.whl/infer/test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+#encoding=utf8
+ 
+
+class demoxxx(object):
+    '''
+    demo
+    '''
+    def func_demo(args):
+        '''
+        Args:
+            + kargs:dict
+        Returns:
+            xxx
+        '''
+        pass
