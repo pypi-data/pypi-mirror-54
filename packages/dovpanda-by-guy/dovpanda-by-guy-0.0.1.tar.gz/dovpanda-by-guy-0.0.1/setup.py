ffe60ece3c00b956e721e59c2ae9f2f3396a4e78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:16:49 +0000

    Add package dovpanda-by-guy/0.0.1/dovpanda-by-guy-0.0.1.tar.gz

---
+++ packages/dovpanda-by-guy/dovpanda-by-guy-0.0.1.tar.gz/dovpanda-by-guy-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dovpanda-by-guy",
+    version="0.0.1",
+    author="Dean Langsam",
+    author_email="deanla@gmail.com",
+    description="Directions overlay for working with pandas in an analysis environment",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/guysmoilov/dovpanda",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
