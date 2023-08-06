4b450b9270d5b2ae8db4aef34b729ade58d83771
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:02 +0000

    Add package GUISpices/1.2.0/GUISpices-1.2.0-py3-none-any.whl

---
+++ packages/GUISpices/GUISpices-1.2.0-py3-none-any.whl/GUISpices/UIWindow.py
@@ -0,0 +1,37 @@
+from PySide2.QtWidgets import QShortcut
+from PySide2.QtCore import QFile, QObject, Qt
+from PySide2.QtUiTools import QUiLoader
+from PySide2.QtGui import QIcon, QKeySequence
+import PySide2.QtXml  # DO NOT REMOVE, needed by QUiLoader
+
+
+class UIWindow(QObject):
+    def __init__(self, ui_file, window_icon: str = None, parent=None, frameless: bool = False):
+        super(UIWindow, self).__init__(parent)
+
+        ui_file = QFile(ui_file)
+        ui_file.open(QFile.ReadOnly)
+        loader = QUiLoader()
+        self.window = loader.load(ui_file)
+        ui_file.close()
+
+        if window_icon: self.window.setWindowIcon(QIcon(window_icon))
+
+        if frameless:
+            self.window.setWindowFlags(Qt.FramelessWindowHint)
+
+    def add_shortcut(self, key_sequence: str, action: callable):
+        QShortcut(QKeySequence(key_sequence.upper()), self.window, action)
+
+    def show(self):
+        self.window.show()
+
+
+if __name__ == '__main__':
+    from PySide2.QtWidgets import QApplication
+    import sys
+
+    app = QApplication()
+    main_form = UIWindow('examples/adobe.ui', "examples/icon.png")
+    main_form.show()
+    sys.exit(app.exec_())
