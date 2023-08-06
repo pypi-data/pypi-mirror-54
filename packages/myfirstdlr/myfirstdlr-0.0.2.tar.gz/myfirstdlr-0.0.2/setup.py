92baab5cbd16072673d46b8f42523e2c71407f9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:58 +0000

    Add package myfirstdlr/0.0.2/myfirstdlr-0.0.2.tar.gz

---
+++ packages/myfirstdlr/myfirstdlr-0.0.2.tar.gz/myfirstdlr-0.0.2/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+  name='myfirstdlr',
+  version='0.0.2',
+  author='whoami',
+  author_email='wai@yy.com',
+  description=u'testing...',
+  url='https://google.com',
+  entry_points={
+    'console_scripts': [
+      'foo=myfirstdlr:foo'
+    ]
+  }
+)
