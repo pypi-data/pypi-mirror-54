41ca7dd0da8420b4d78c9721073d1cf7faf4bfbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:06 +0000

    Add package baizhanMath01/1.0/baizhanMath01-1.0.tar.gz

---
+++ packages/baizhanMath01/baizhanMath01-1.0.tar.gz/baizhanMath01-1.0/setup.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+from distutils.core import setup
+setup( name='baizhanMath01', # 对外我们模块的名字
+       version='1.0', # 版本号
+       description='这是第一个对外发布的模块，测试哦', #描述
+       author='XPeng', # 作者
+       author_email='1500779233@qq.com',
+       py_modules=['baizhanMath2.demo1','baizhanMath2.demo2'] # 要发布的模块
+)
