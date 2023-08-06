223164aa6e5a61cfaa32239a46f710110714cc73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:40 +0000

    Add package mateusz-hello-application/1.0.2/mateusz_hello_application-1.0.2.tar.gz

---
+++ packages/mateusz-hello-application/mateusz_hello_application-1.0.2.tar.gz/mateusz_hello_application-1.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='mateusz_hello_application',
+    version='1.0.2',
+    packages=find_packages(),
+    include_package_data= True,
+    package_dir={'':'.'},
+    description='Application Hello by Mateusz'
+)
