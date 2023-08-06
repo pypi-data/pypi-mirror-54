bf221ecf2e6dc0840537164e615b9cd83f1c0557
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:38 +0000

    Add package gaitpy/1.5.3/gaitpy-1.5.3.tar.gz

---
+++ packages/gaitpy/gaitpy-1.5.3.tar.gz/gaitpy-1.5.3/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+def requirements():
+    with open('requirements.txt', "r") as fh:
+        return [x for x in fh.read().split('\n') if x]
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='gaitpy',
+                 version='1.5.3',
+                 description='Read and process raw vertical accelerometry data from a lumbar sensor during gait; calculate clinical gait characteristics.',
+                 long_description=long_description,
+                 long_description_content_type="text/markdown",
+                 url='http://github.com/matt002/gaitpy',
+                 packages=setuptools.find_packages(),
+                 author='Matthew Czech',
+                 author_email='czech1117@gmail.com',
+                 keywords=['gait', 'lumbar', 'sensor', 'digital', 'wearable', 'python', 'inverted pendulum', 'czech'],
+                 classifiers=["Programming Language :: Python :: 3.6",
+                              "License :: OSI Approved :: MIT License"],
+                 license='MIT',
+                 zip_safe=False,
+                 install_requires=requirements(),
+                 setup_requires=['numpy==1.13.3'],
+                 include_package_data=True)
