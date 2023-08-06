e7ec158afa13aabf4ea653bc2230337d33bd0a8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:03 +0000

    Add package hydraseq/0.0.27/hydraseq-0.0.27-py3-none-any.whl

---
+++ packages/hydraseq/hydraseq-0.0.27-py3-none-any.whl/hydraseq/__init__.py
@@ -0,0 +1,6 @@
+name = "hydraseq"
+__version__ = '0.0.27'
+from hydraseq.hydraseq import Node
+from hydraseq.hydraseq import Hydraseq
+from hydraseq.minicolumn import MiniColumn
+from hydraseq.automata import DFAstate
