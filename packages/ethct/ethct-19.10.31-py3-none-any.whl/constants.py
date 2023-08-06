eb5133a40ff24f85c7aabfbeea010be38809f9c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:53 +0000

    Add package ethct/19.10.31/ethct-19.10.31-py3-none-any.whl

---
+++ packages/ethct/ethct-19.10.31-py3-none-any.whl/constants.py
@@ -0,0 +1,17 @@
+import os
+import json
+
+default_config_path = os.environ['HOME'] + '/.ethct.json'
+
+key = json.load(open(default_config_path))
+
+URL = {
+    'mainnet': "https://mainnet.infura.io/v3/" + key['infurakey'],
+    'ropsten': "https://ropsten.infura.io/v3/" + key['infurakey'],
+    'rinkeby': "https://rinkeby.infura.io/v3/" + key['infurakey'],
+    'kovan': "https://kovan.infura.io/v3/" + key['infurakey'],
+    'local': "http://localhost:8545",
+}
+
+PRIVATE_KEY = key['privkey']
+NETWORK = key['network']
