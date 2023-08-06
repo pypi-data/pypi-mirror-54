213620dff9e1a77abf94cd419743bb133714bd29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:00 +0000

    Add package kiwitcms/7.1/kiwitcms-7.1-py3-none-any.whl

---
+++ packages/kiwitcms/kiwitcms-7.1-py3-none-any.whl/tcms/rpc/forms.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from django.forms.widgets import CheckboxInput
+
+from tcms.rpc.utils import parse_bool_value
+
+
+class XMLRPCCheckboxInput(CheckboxInput):
+    def value_from_datadict(self, data, files, name):
+        if name not in data:
+            return False
+        value = parse_bool_value(data.get(name))
+        return value
