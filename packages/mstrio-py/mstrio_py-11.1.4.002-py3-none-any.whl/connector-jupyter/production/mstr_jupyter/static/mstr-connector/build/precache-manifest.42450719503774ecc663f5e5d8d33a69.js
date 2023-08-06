71bd9a67a2b93c7ac4c8090e20a6d4ef891f2089
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:22 +0000

    Add package mstrio-py/11.1.4.002/mstrio_py-11.1.4.002-py3-none-any.whl

---
+++ packages/mstrio-py/mstrio_py-11.1.4.002-py3-none-any.whl/connector-jupyter/production/mstr_jupyter/static/mstr-connector/build/precache-manifest.42450719503774ecc663f5e5d8d33a69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfc8fbf023e5754b5d489dc5d4f0c10f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c0916129ae8dfd313219",
+    "url": "./static/css/2.48df7f21.chunk.css"
+  },
+  {
+    "revision": "df72609b6878e1d50599",
+    "url": "./static/css/main.9a5f28e4.chunk.css"
+  },
+  {
+    "revision": "c0916129ae8dfd313219",
+    "url": "./static/js/2.e3a6d109.chunk.js"
+  },
+  {
+    "revision": "df72609b6878e1d50599",
+    "url": "./static/js/main.abda98ca.chunk.js"
+  },
+  {
+    "revision": "5eb9fe67b5985a0ab537",
+    "url": "./static/js/runtime-main.6c141fb0.js"
+  }
+]);
