3234beae418a103778aa7a026c00e90e5133bc33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:04 +0000

    Add package openradar/1.0.0/openradar-1.0.0.tar.gz

---
+++ packages/openradar/openradar-1.0.0.tar.gz/openradar-1.0.0/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="openradar",
+    version="1.0.0",
+    author="Edwin Pan, Jingning Tang, Dashiell Kosaka, Arjun Gupta, Ruihao Yao",
+    author_email="presenseradar@gmail.com",
+    description="A mmWave radar data processing library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
