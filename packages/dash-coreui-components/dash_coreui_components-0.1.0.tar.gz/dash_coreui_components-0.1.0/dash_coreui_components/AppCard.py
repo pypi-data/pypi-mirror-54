cda98ebefd64259d4eebf796c3675974c3ca419f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:08 +0000

    Add package dash-coreui-components/0.1.0/dash_coreui_components-0.1.0.tar.gz

---
+++ packages/dash-coreui-components/dash_coreui_components-0.1.0.tar.gz/dash_coreui_components-0.1.0/dash_coreui_components/AppCard.py
@@ -0,0 +1,34 @@
+# AUTO GENERATED FILE - DO NOT EDIT
+
+from dash.development.base_component import Component, _explicitize_args
+
+
+class AppCard(Component):
+    """An AppCard component.
+CoreUI appcard component.
+
+Keyword arguments:
+- children (a list of or a singular dash component, string or number; optional): The children
+- id (string; default 'card'): The ID used to identify this component in Dash callbacks, defaults to `AppSidebar`.
+- className (string; optional): The CSS class name, defaults to `appcard`.
+- tag (string; default 'div'): The HTML tag.
+- style (dict; optional): The CSS style."""
+    @_explicitize_args
+    def __init__(self, children=None, id=Component.UNDEFINED, className=Component.UNDEFINED, tag=Component.UNDEFINED, style=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['children', 'id', 'className', 'tag', 'style']
+        self._type = 'AppCard'
+        self._namespace = 'dash_coreui_components'
+        self._valid_wildcard_attributes =            []
+        self.available_properties = ['children', 'id', 'className', 'tag', 'style']
+        self.available_wildcard_properties =            []
+
+        _explicit_args = kwargs.pop('_explicit_args')
+        _locals = locals()
+        _locals.update(kwargs)  # For wildcard attrs
+        args = {k: _locals[k] for k in _explicit_args if k != 'children'}
+
+        for k in []:
+            if k not in args:
+                raise TypeError(
+                    'Required argument `' + k + '` was not specified.')
+        super(AppCard, self).__init__(children=children, **args)
---
+++ packages/dash-coreui-components/dash_coreui_components-0.1.0.tar.gz/dash_coreui_components-0.1.0/dash_coreui_components/_imports_.py
@@ -0,0 +1,35 @@
+from .AppAsideToggler import AppAsideToggler
+from .AppBreadcrumb import AppBreadcrumb
+from .AppCard import AppCard
+from .AppFooter import AppFooter
+from .AppHeader import AppHeader
+from .AppHeaderDropdown import AppHeaderDropdown
+from .AppNavbarBrand import AppNavbarBrand
+from .AppRouteConditional import AppRouteConditional
+from .AppSidebar import AppSidebar
+from .AppSidebarFooter import AppSidebarFooter
+from .AppSidebarForm import AppSidebarForm
+from .AppSidebarHeader import AppSidebarHeader
+from .AppSidebarNav import AppSidebarNav
+from .AppSidebarToggler import AppSidebarToggler
+from .AppTableBody import AppTableBody
+from .AppTableHeader import AppTableHeader
+
+__all__ = [
+    "AppAsideToggler",
+    "AppBreadcrumb",
+    "AppCard",
+    "AppFooter",
+    "AppHeader",
+    "AppHeaderDropdown",
+    "AppNavbarBrand",
+    "AppRouteConditional",
+    "AppSidebar",
+    "AppSidebarFooter",
+    "AppSidebarForm",
+    "AppSidebarHeader",
+    "AppSidebarNav",
+    "AppSidebarToggler",
+    "AppTableBody",
+    "AppTableHeader"
+]
