2f44fcf96aba1d7a1036fe086650c247a3c50b2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:48 +0000

    Add package polytropos/0.2.10/polytropos-0.2.10.tar.gz

---
+++ packages/polytropos/polytropos-0.2.10.tar.gz/polytropos-0.2.10/polytropos/ontology/variable/__init__.py
@@ -0,0 +1,9 @@
+import polytropos.ontology.variable.__variable
+from polytropos.ontology.variable.__variable import (
+    Variable, Folder, List, KeyedList, Primitive, Container, GenericList,
+    Validator, Text, Decimal, Integer, Binary, VariableId,
+    Unary, Currency, Phone, Email, URL, Date, MultipleText, Ratio
+)
+
+
+
