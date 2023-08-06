e49c5586a2514391fbdf1184605b0f2c97397a37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:38 +0000

    Add package pypkgfreeze/0.1.3/pypkgfreeze-0.1.3.tar.gz

---
+++ packages/pypkgfreeze/pypkgfreeze-0.1.3.tar.gz/pypkgfreeze-0.1.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+test_req = ["pytest==4.1.0"]
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pypkgfreeze",
+    version="0.1.3",
+    packages=["src"],
+    author="Adam Goldschmidt",
+    author_email="adamgold7@gmail.com",
+    description="Automatically inserts your currently used package versions to setup.py.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/adamgold/pypkgfreeze",
+    install_requires=["Click==7.0"],
+    tests_require=test_req,
+    setup_requires=["pytest-runner==4.2"],
+    extras_require={"test": test_req},
+    entry_points={"console_scripts": "pkgfreeze=src.pypkgfreeze:main"},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Environment :: Console",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
