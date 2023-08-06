8ab4bb70a45f7a9dc096e60c63530dcb50fa3917
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:58 +0000

    Add package prestring/0.6.0/prestring-0.6.0-py2.py3-none-any.whl

---
+++ packages/prestring/prestring-0.6.0-py2.py3-none-any.whl/prestring/naming.py
@@ -0,0 +1,42 @@
+import re
+
+
+def snakecase(
+    name,
+    rx0=re.compile(r"(.)([A-Z][a-z]+)"),
+    rx1=re.compile(r"([a-z0-9])([A-Z])"),
+    separator="_",
+):
+    pattern = r"\1{}\2".format(separator)
+    return rx1.sub(pattern, rx0.sub(pattern, name)).lower()
+
+
+def kebabcase(
+    name,
+    rx0=re.compile(r"(.)([A-Z][a-z]+)"),
+    rx1=re.compile(r"([a-z0-9])([A-Z])"),
+    separator="-",
+):
+    pattern = r"\1{}\2".format(separator)
+    return rx1.sub(pattern, rx0.sub(pattern, name)).lower()
+
+
+def camelcase(name):
+    return untitleize(pascalcase(name))
+
+
+def pascalcase(name, rx=re.compile(r"[\-_ ]")):
+    return "".join(titleize(x) for x in rx.split(name))
+
+
+def titleize(name):
+    if not name:
+        return name
+    name = str(name)
+    return "{}{}".format(name[0].upper(), name[1:])
+
+
+def untitleize(name):
+    if not name:
+        return name
+    return "{}{}".format(name[0].lower(), name[1:])
