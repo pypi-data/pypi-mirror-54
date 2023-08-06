0f8e35c0bc1c643840cc02302542ee4d7ea6179e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:23 +0000

    Add package neqsim/1.2.0/neqsim-1.2.0.tar.gz

---
+++ packages/neqsim/neqsim-1.2.0.tar.gz/neqsim-1.2.0/src/neqsim/__init__.py
@@ -0,0 +1,11 @@
+name = "neqsim_pkg"
+
+from neqsim import javaGateway
+
+java_gateway = javaGateway.start_server()
+
+def methods(checkClass):
+    methods = checkClass.getClass().getMethods()
+    for method in methods:
+        print(method.getName())
+            
