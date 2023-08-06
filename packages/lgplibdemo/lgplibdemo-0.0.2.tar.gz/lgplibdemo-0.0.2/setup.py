8d2f3e8791357c5de1c5b677054f0bbf241b1cb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:07 +0000

    Add package lgplibdemo/0.0.2/lgplibdemo-0.0.2.tar.gz

---
+++ packages/lgplibdemo/lgplibdemo-0.0.2.tar.gz/lgplibdemo-0.0.2/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# import os
+# import sys
+# import re
+# from os.path import abspath, dirname, join
+from setuptools import setup, find_packages
+# from macpath import curdir
+# curPath = os.path.abspath(os.path.dirname(__file__))
+# parentPath = os.path.split(curPath)[0]
+# rootPath = os.path.split(parentPath)[0]
+# sys.path.append(rootPath)
+
+#CURDIR = dirname(abspath(__file__))
+#print(CURDIR)
+# with open(join(CURDIR, 'requirements.txt')) as f:
+#     REQUIREMENTS = f.read().splitlines()
+setup(
+    name             = 'lgplibdemo',
+    version          = '0.0.2',
+    description      = 'library for Robot Framework',
+    long_description = 'library for Robot Framework',
+    author           = 'ligaopan',
+    author_email     = 'ligaopan1984@163.com',
+    url              = 'https://github.com/ligaopan/lgp-library',
+    license          = 'MIT Licence',
+    keywords         = 'robotframework testing testautomation',
+    platforms        = 'any',
+    python_requires  = '>=3.7.*',
+    install_requires = [],
+    package_dir      = {'': 'src'},
+    packages         = find_packages('src')
+    )
