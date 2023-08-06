7fcd7600a25ce1af93ec7f360a36606a433e7355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:59 +0000

    Add package igeBullet/0.1.2/igeBullet-0.1.2.tar.gz

---
+++ packages/igeBullet/igeBullet-0.1.2.tar.gz/igeBullet-0.1.2/igeBullet_doc_en.h
@@ -0,0 +1,33 @@
+
+
+PyDoc_STRVAR(shape_doc,
+	"3d physics shape\n");
+
+PyDoc_STRVAR(rigidbody_doc,
+	"3d physics rigid body\n");
+
+PyDoc_STRVAR(rigidbody_position_doc,
+	"position of rigid body\n");
+
+PyDoc_STRVAR(rigidbody_rotation_doc,
+	"rotation of rigid body\n");
+
+PyDoc_STRVAR(dynworld_doc,
+	"3d physics world\n");
+
+PyDoc_STRVAR(add_doc,
+	"Add rigidbody or constarain to world\n");
+PyDoc_STRVAR(remove_doc,
+	"Remove rigidbody or constarain from world\n");
+PyDoc_STRVAR(clear_doc,
+	"Clear rigidbody and constarain\n");
+PyDoc_STRVAR(step_doc,
+	"step simulation\n");
+PyDoc_STRVAR(wait_doc,
+	"wait until async simultion is end\n");
+PyDoc_STRVAR(gravity_doc,
+	"gravity vector\n");
+
+
+
+
