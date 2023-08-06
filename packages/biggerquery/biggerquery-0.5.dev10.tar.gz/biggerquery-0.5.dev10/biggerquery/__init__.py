8e1074b7ad7c32a4c7f0010a789452197b0b80db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:13 +0000

    Add package biggerquery/0.5.dev10/biggerquery-0.5.dev10.tar.gz

---
+++ packages/biggerquery/biggerquery-0.5.dev10.tar.gz/biggerquery-0.5.dev10/biggerquery/__init__.py
@@ -0,0 +1,42 @@
+from __future__ import absolute_import
+
+__all__ = [
+    'create_dataset_manager',
+    'create_dataflow_manager',
+
+    'DatasetConfig',
+    'DataflowConfig',
+
+    'build_dag_from_notebook',
+    'build_dag',
+    'workflow_to_dag',
+
+    'component',
+    'Dataset',
+
+    'Workflow',
+
+    'Job',
+
+    'fastai_tabular_prediction_component'
+]
+
+from .utils import secure_create_dataflow_manager_import
+from .dataset_manager import create_dataset_manager
+create_dataflow_manager = secure_create_dataflow_manager_import()
+
+from .configuration import DatasetConfig
+from .configuration import DataflowConfig
+
+from .deployment import build_dag_from_notebook
+from .deployment import build_dag
+from .deployment import workflow_to_dag
+
+from .interactive import interactive_component as component
+from .interactive import InteractiveDatasetManager as Dataset
+
+from .workflow import Workflow
+
+from .job import Job
+
+from .user_commons.fastai.predict_component import fastai_tabular_prediction_component
