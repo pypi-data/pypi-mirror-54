b9190b7aba8b4095fb59c8309867d6cdcc6ea02b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:14 +0000

    Add package FileEncryption/0.1.4/FileEncryption-0.1.4.tar.gz

---
+++ packages/FileEncryption/FileEncryption-0.1.4.tar.gz/FileEncryption-0.1.4/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+  name = 'FileEncryption',
+  packages = ['FileEncryption'],
+  version = '0.1.4',
+  license='MIT',
+  description = 'Encrypt you .py or .pyw files to unreadable code', 
+  author = 'Simon Köck',
+  author_email = 'simply.studios.business@gmail.com',
+  url = 'https://github.com/simonkoeck/FileEncryption',
+  download_url = 'https://github.com/simonkoeck/FileEncryption/archive/v_01.tar.gz',
+  keywords = ['File', 'Encryption', 'Unreadable'],
+  install_requires=[
+          'cryptography',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
