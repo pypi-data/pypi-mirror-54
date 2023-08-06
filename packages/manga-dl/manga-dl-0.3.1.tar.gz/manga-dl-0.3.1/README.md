98f34d9a42dd52f92f467ab8cff12f1e1e48e4d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:18:07 +0000

    Add package manga-dl/0.3.1/manga-dl-0.3.1.tar.gz

---
+++ packages/manga-dl/manga-dl-0.3.1.tar.gz/manga-dl-0.3.1/README.md
@@ -0,0 +1,18 @@
+# Manga Downloader
+
+|master|develop|
+|:---:|:---:|
+|[![build status](https://gitlab.namibsun.net/namibsun/python/manga-dl/badges/master/build.svg)](https://gitlab.namibsun.net/namibsun/python/manga-dl/commits/master)|[![build status](https://gitlab.namibsun.net/namibsun/python/manga-dl/badges/develop/build.svg)](https://gitlab.namibsun.net/namibsun/python/manga-dl/commits/develop)|
+
+![Logo](resources/logo/logo-readme.png "Logo")
+
+This is a Manga Downloader that can download managa series from various sources.
+
+## Further Information
+
+* [Changelog](CHANGELOG)
+* [License (GPLv3)](LICENSE)
+* [Gitlab](https://gitlab.namibsun.net/namibsun/python/manga-dl)
+* [Github](https://github.com/namboy94/manga-dl)
+* [Progstats](https://progstats.namibsun.net/projects/manga-dl)
+* [PyPi](https://pypi.org/project/manga-dl)
---
+++ packages/manga-dl/manga-dl-0.3.1.tar.gz/manga-dl-0.3.1/manga_dl/__init__.py
@@ -0,0 +1,24 @@
+"""LICENSE
+Copyright 2015 Hermann Krumrey <hermann@krumreyh.com>
+
+This file is part of manga-dl.
+
+manga-dl is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+manga-dl is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with manga-dl.  If not, see <http://www.gnu.org/licenses/>.
+LICENSE"""
+
+
+sentry_dsn = "https://0c1dcd24a5c346e09115ffebdb780772@sentry.namibsun.net/9"
+"""
+Sentry DSN used for exception logging
+"""
