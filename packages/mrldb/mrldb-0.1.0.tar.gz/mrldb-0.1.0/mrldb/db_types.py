b3f4114bc4d3d28f8c10798c3b0251f97006317c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:59 +0000

    Add package mrldb/0.1.0/mrldb-0.1.0.tar.gz

---
+++ packages/mrldb/mrldb-0.1.0.tar.gz/mrldb-0.1.0/mrldb/db_types.py
@@ -0,0 +1,7 @@
+__doc__=f"""mrldb by Rémi "Mr e-RL" LANGDORPH
+Copyright (c) 2019 Rémi LANGDORPH - mrerl@warlegend.net
+under MIT License (https://github.com/merlleu/mrldb/blob/master/LICENSE)"""
+__all__=["MrlDBMsql", "MrlDBCassandra", "MrlDBSqlite"]
+from .type_mysql import MrlDBMsql
+from .type_cassandra import MrlDBCassandra
+from .type_sqlite import MrlDBSqlite
