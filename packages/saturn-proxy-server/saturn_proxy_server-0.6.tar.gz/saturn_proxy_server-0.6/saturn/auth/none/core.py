7b48123cc3af44a1fd8ff7dd3cb852a60bc085eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:17:28 +0000

    Add package saturn-proxy-server/0.6/saturn_proxy_server-0.6.tar.gz

---
+++ packages/saturn-proxy-server/saturn_proxy_server-0.6.tar.gz/saturn_proxy_server-0.6/saturn/auth/none/core.py
@@ -0,0 +1,13 @@
+class Authenticator:
+    method = 0
+
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super().__new__(cls)
+        return cls.instance
+
+    def __init__(self, **data):
+        pass
+
+    async def authenticate(self, data):
+        return True
