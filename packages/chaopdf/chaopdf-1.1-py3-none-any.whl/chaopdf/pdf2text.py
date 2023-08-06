10444e8d70ad87f01e7369610b7f848fcc0d5cfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:32 +0000

    Add package chaopdf/1.0/chaopdf-1.1-py3-none-any.whl

---
+++ packages/chaopdf/chaopdf-1.1-py3-none-any.whl/chaopdf/pdf2text.py
@@ -0,0 +1,14 @@
+""" This module provides functions to convert a PDF to text."""
+
+
+def convert(path):
+    """
+    Convert the given PDF to text.
+
+    Parameters:
+    path(str): The path to a PDF file.
+
+    Returns:
+    str: The content of the PDF file as text.
+    """
+    print("pdf2text")
