bbfa491863d004ba76b3fdb08a92d444d8380f19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:08 +0000

    Add package pyfps/1.0.2/pyfps-1.0.2-py2-none-any.whl

---
+++ packages/pyfps/pyfps-1.0.2-py2-none-any.whl/pyfps/__init__.py
@@ -0,0 +1,33 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import time
+
+
+def _print_fps(fps):
+    print('FPS: {}'.format(fps))
+
+
+class Fps:
+    def __init__(self, func=_print_fps):
+        self.func = func
+        self.last = 0
+        self.reset()
+        self.second = 1.0
+
+    def reset(self):
+        self.fps = 0
+        self.tick = time.time()
+
+    def update(self, *argc, **args):
+        dur = time.time() - self.tick
+        if dur >= self.second:
+            self.last = self.fps
+            if self.func is not None:
+                self.func(self.fps, *argc, **args)
+            self.reset()
+        else:
+            self.fps += 1
+
