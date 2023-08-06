6fa461320df11ba00d5c9cedd61c0134a06bbf98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:15 +0000

    Add package claripy/8.19.10.29/claripy-8.19.10.29.tar.gz

---
+++ packages/claripy/claripy-8.19.10.29.tar.gz/claripy-8.19.10.29/setup.py
@@ -0,0 +1,26 @@
+try:
+    from setuptools import setup
+    from setuptools import find_packages
+    packages = find_packages()
+except ImportError:
+    from distutils.core import setup
+    import os
+    packages = [x.strip('./').replace('/','.') for x in os.popen('find -name "__init__.py" | xargs -n1 dirname').read().strip().split('\n')]
+
+if bytes is str:
+    raise Exception("This module is designed for python 3 only. Please install an older version to use python 2.")
+
+setup(
+    name='claripy',
+    version='8.19.10.29',
+    python_requires='>=3.5',
+    packages=packages,
+    install_requires=[
+        'z3-solver>=4.8.5.0',
+        'future',
+        'cachetools',
+        'pysmt',
+    ],
+    description='An abstraction layer for constraint solvers',
+    url='https://github.com/angr/claripy',
+)
