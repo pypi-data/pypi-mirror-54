e01833fcd0d4aca82a5992769a0ec50efb6f5da8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:44 +0000

    Add package cle/8.19.10.30/cle-8.19.10.30.tar.gz

---
+++ packages/cle/cle-8.19.10.30.tar.gz/cle-8.19.10.30/setup.py
@@ -0,0 +1,27 @@
+try:
+    from setuptools import setup
+    from setuptools import find_packages
+    packages = find_packages()
+except ImportError:
+    from distutils.core import setup
+    import os
+    packages = [x.strip('./').replace('/','.') for x in os.popen('find -name "__init__.py" | xargs -n1 dirname').read().strip().split('\n')]
+
+if bytes is str:
+    raise Exception("This module is designed for python 3 only. Please install an older version to use python 2.")
+
+setup(
+    name='cle',
+    description='CLE Loads Everything (at least, many binary formats!) and provides a pythonic interface to analyze what they are and what they would look like in memory.',
+    version='8.19.10.30',
+    python_requires='>=3.5',
+    packages=packages,
+    install_requires=[
+        'pyelftools>=0.25',
+        'cffi',
+        'archinfo==8.19.10.30',
+        'pyvex==8.19.10.30',
+        'pefile',
+        'sortedcontainers>=2.0',
+    ]
+)
