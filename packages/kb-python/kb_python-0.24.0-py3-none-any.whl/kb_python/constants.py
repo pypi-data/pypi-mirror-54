42b11f5c3763bcccd07eaa6c9de344429b2bd3e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:27 +0000

    Add package kb-python/0.24.0/kb_python-0.24.0-py3-none-any.whl

---
+++ packages/kb-python/kb_python-0.24.0-py3-none-any.whl/kb_python/constants.py
@@ -0,0 +1,27 @@
+INFO_FILENAME = 'info.txt'
+
+# Default filenames
+CDNA_FILENAME = 'cdna.fa'
+INTRON_FILENAME = 'introns.fa'
+SORTED_FASTA_FILENAME = 'sorted.fa'
+SORTED_GTF_FILENAME = 'sorted.gtf'
+COMBINED_FILENAME = 'combined.fa'
+INDEX_FILENAME = 'transcriptome.idx'
+WHITELIST_FILENAME = 'whitelist.txt'
+FILTER_WHITELIST_FILENAME = 'filter_whitelist.txt'
+INSPECT_FILENAME = 'inspect.json'
+
+BUS_FILENAME = 'output.bus'
+BUS_S_FILENAME = 'output.s.bus'
+BUS_SC_FILENAME = 'output.s.c.bus'
+BUS_SCS_FILENAME = 'output.s.c.s.bus'
+BUS_CDNA_PREFIX = 'spliced'
+BUS_INTRON_PREFIX = 'unspliced'
+ECMAP_FILENAME = 'matrix.ec'
+TXNAMES_FILENAME = 'transcripts.txt'
+COUNTS_DIR = 'counts'
+COUNTS_PREFIX = 'cells_x_genes'
+ADATA_PREFIX = 'adata'
+
+FILTERED_DIR = 'filtered'
+UNFILTERED_DIR = 'unfiltered'
