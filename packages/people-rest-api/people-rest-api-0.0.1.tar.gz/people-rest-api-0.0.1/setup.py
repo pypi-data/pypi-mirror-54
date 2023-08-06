1e3ada54cbad3ebe2af36f9b7617cf9e78e85a75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:29 +0000

    Add package people-rest-api/0.0.1/people-rest-api-0.0.1.tar.gz

---
+++ packages/people-rest-api/people-rest-api-0.0.1.tar.gz/people-rest-api-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='people-rest-api',
+      version='0.0.1',
+      description='Python REST APIs With Flask, Connexion, and SQLAlchemy',
+      url='https://github.com/radcheb/book-store',
+      author='radcheb',
+      author_email='radcheb@example.com',
+      license='MIT',
+      packages=['people_rest_api'],
+      zip_safe=False)
