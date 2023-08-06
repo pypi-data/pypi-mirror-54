66e710e7f328302f5872e70112124b21ed541ab4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:17:39 +0000

    Add package saucs/0.0.1/saucs-0.0.1.tar.gz

---
+++ packages/saucs/saucs-0.0.1.tar.gz/saucs-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="saucs",
+    version="0.0.1",
+    author="Nicolas Crocfer",
+    author_email="ncrocfer@gmail.com",
+    url="http://github.com/ncrocfer/saucs",
+    description="Vulnerability Alerting Application",
+    long_description=open('README.rst').read()
+)
