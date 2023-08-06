23f8a8ce12ffeea4714dfcd7181753972d2bb2f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:45 +0000

    Add package statsapiclient/0.1.2/statsapiclient-0.1.2-py3-none-any.whl

---
+++ packages/statsapiclient/statsapiclient-0.1.2-py3-none-any.whl/statsapiclient/utils.py
@@ -0,0 +1,27 @@
+from requests import HTTPError, get
+
+from .constants import API_HOST, HEADERS
+
+
+def fetch_json(endpoint, params=None):
+    """
+    Helper method for json fetching.
+    Args:
+        endpoint (str): resource endpoint
+        params (dict): query parameters
+    Raises:
+        HTTPError: if requests does not return 200
+    Returns:
+        json: json object for selected API call
+    """
+    try:
+        headers = dict(HEADERS)
+        response = get(
+            f"{API_HOST}/{endpoint}",
+            params=params, headers=headers
+        )
+        response.raise_for_status()
+
+        return response.json()
+    except HTTPError as error:
+        raise HTTPError
