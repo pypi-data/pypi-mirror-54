aae16a119513deb5652dbcd3f2865936b75e3017
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:24 +0000

    Add package py-foldadt/0.1.13/py-foldadt-0.1.13.tar.gz

---
+++ packages/py-foldadt/py-foldadt-0.1.13.tar.gz/py-foldadt-0.1.13/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+setup(
+    name="py-foldadt",
+    version="0.1.13",
+    packages=find_packages(),
+    author="beezee",
+    description="Zero dependency, mypy friendly algebraic data types",
+    keywords="algebraic data types, adt",
+    url="https://github.com/beezee/py-foldadt",
+    project_urls={
+        "Source Code": "https://github.com/beezee/py-foldadt",
+    }
+)
