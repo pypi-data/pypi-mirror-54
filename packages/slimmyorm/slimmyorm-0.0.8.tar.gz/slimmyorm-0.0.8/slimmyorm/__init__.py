2f6eeadb0aaca95c51fea6f56ec797aaf0b613e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:06 +0000

    Add package slimmyorm/0.0.8/slimmyorm-0.0.8.tar.gz

---
+++ packages/slimmyorm/slimmyorm-0.0.8.tar.gz/slimmyorm-0.0.8/slimmyorm/__init__.py
@@ -0,0 +1,6 @@
+from .base_data import BaseData  # noqa: F401
+from .mysql_connection import ORM, MysqlConnection  # noqa: F401
+from .select import Select  # noqa: F401
+from .search import search, search_high_low  # noqa: F401
+from .insert import dict_to_table_insert, insert_row  # noqa: F401
+from .slimmy_json import SlimMyJson  # noqa: F401
