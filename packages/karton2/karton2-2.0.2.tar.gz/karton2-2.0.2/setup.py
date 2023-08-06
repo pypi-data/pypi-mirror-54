d064727e312864d458193b6db17e01c57a853a0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:57 +0000

    Add package karton2/2.0.2/karton2-2.0.2.tar.gz

---
+++ packages/karton2/karton2-2.0.2.tar.gz/karton2-2.0.2/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name="karton2",
+    version="2.0.2",
+    description="Base library for karton subsystems",
+    package_dir={"karton2": "karton"},
+    packages=["karton2"],
+    package_data={"": ["karton/templates/*"]},
+    include_package_data=True,
+    install_requires=open("requirements.txt").read().splitlines(),
+    scripts=["karton/kpm"],
+    classifiers=[
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+    ],
+)
