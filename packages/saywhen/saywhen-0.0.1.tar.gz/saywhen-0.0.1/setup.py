78ccbe9c2f4078430dc0eff2f428f4fba2e0d6c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:51 +0000

    Add package saywhen/0.0.1/saywhen-0.0.1.tar.gz

---
+++ packages/saywhen/saywhen-0.0.1.tar.gz/saywhen-0.0.1/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="saywhen",
+    description="Send notifications when command line job finishes",
+    version="0.0.1",
+    author="Yunqing Gong",
+    author_email="gongyq10@gmail.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yunqing/saywhen",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=['saywhen'],
+    package_data={
+        'saywhen': [
+            'audio/*'
+            ]
+        },
+    include_package_data=True,
+    scripts=['scripts/saywhen'],
+    python_requires='>=3.5',
+)
