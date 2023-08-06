e8f8151883601ba32290fe1a2ec26cd6e0e3aa83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:15 +0000

    Add package colour-hdri/0.1.6/colour_hdri-0.1.6-py2.py3-none-any.whl

---
+++ packages/colour-hdri/colour_hdri-0.1.6-py2.py3-none-any.whl/colour_hdri/utilities/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from .common import vivification, vivified_to_dict, path_exists, filter_files
+from .exif import (EXIF_EXECUTABLE, ExifTag, parse_exif_string,
+                   parse_exif_numeric, parse_exif_fraction, parse_exif_array,
+                   parse_exif_data, read_exif_tags, copy_exif_tags,
+                   update_exif_tags, delete_exif_tags, read_exif_tag,
+                   write_exif_tag)
+from .exposure import (average_luminance, average_illuminance, exposure_value,
+                       adjust_exposure)
+from .image import Metadata, Image, ImageStack
+
+__all__ = []
+__all__ += ['vivification', 'vivified_to_dict', 'path_exists', 'filter_files']
+__all__ += [
+    'EXIF_EXECUTABLE', 'ExifTag', 'parse_exif_string', 'parse_exif_numeric',
+    'parse_exif_fraction', 'parse_exif_array', 'parse_exif_data',
+    'read_exif_tags', 'copy_exif_tags', 'update_exif_tags', 'delete_exif_tags',
+    'read_exif_tag', 'write_exif_tag'
+]
+__all__ += [
+    'average_luminance', 'average_illuminance', 'exposure_value',
+    'adjust_exposure'
+]
+__all__ += ['Metadata', 'Image', 'ImageStack']
