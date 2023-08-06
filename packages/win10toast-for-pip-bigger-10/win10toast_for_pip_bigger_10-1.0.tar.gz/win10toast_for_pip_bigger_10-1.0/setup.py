640e4daef679a83a045b0816f59c8fcf3a26c549
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:44 +0000

    Add package win10toast-for-pip-bigger-10/1.0/win10toast_for_pip_bigger_10-1.0.tar.gz

---
+++ packages/win10toast-for-pip-bigger-10/win10toast_for_pip_bigger_10-1.0.tar.gz/win10toast_for_pip_bigger_10-1.0/setup.py
@@ -0,0 +1,50 @@
+
+from operator import attrgetter
+from os import path
+
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
+from setuptools import setup
+
+def read(fname):
+    return open(path.join(path.dirname(__file__), fname)).read()
+
+
+def from_here(relative_path):
+    return path.join(path.dirname(__file__), relative_path)
+
+
+requirements_txt = list(map(str, map(
+    attrgetter("req"),
+    parse_requirements(from_here("requirements.txt"), session="")
+)))
+
+setup(
+    name="win10toast_for_pip_bigger_10",
+    version="1.0",
+    install_requires=requirements_txt,
+    packages=["win10toast_for_pip_bigger_10"],
+    license="BSD",
+    url="https://github.com/jithurjacob/Windows-10-Toast-Notifications",
+    download_url = 'https://github.com/jithurjacob/Windows-10-Toast-Notifications/tarball/0.9',
+    description=(
+        "An easy-to-use Python library for displaying "
+        "Windows 10 Toast Notifications"
+    ),
+    include_package_data=True,
+    package_data={
+        '': ['*.txt'],
+        'win10toast': ['data/*.ico'],
+    },
+    author="Jithu R Jacob",
+    author_email="jithurjacob@gmail.com",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        'Operating System :: Microsoft',
+        'Environment :: Win32 (MS Windows)',
+        "License :: OSI Approved :: MIT License",
+    ],
+)
