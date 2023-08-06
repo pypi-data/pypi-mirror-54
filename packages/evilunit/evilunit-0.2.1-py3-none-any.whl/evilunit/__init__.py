b05d37dc524afde2729d5c8ea509c09669bf87a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:41 +0000

    Add package evilunit/0.2.1/evilunit-0.2.1-py3-none-any.whl

---
+++ packages/evilunit/evilunit-0.2.1-py3-none-any.whl/evilunit/__init__.py
@@ -0,0 +1,10 @@
+from evilunit.nestedtest import (  # noqa F401
+    NestedTest,
+    NestedTestCompiler,
+    nested_test_compile,
+)
+from evilunit.shortcuts import test_target, test_function  # noqa F401
+from evilunit.parameterized import parameterized  # noqa F401
+
+# typo. TODO:remove near feature.
+paramaterized = parameterized
