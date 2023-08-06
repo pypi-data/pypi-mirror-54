8aee0b37e33fc0d8140510664b5cbe5bc1db4cc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:18 +0000

    Add package muhaftab-demo-package/0.0.2/muhaftab_demo_package-0.0.2.tar.gz

---
+++ packages/muhaftab-demo-package/muhaftab_demo_package-0.0.2.tar.gz/muhaftab_demo_package-0.0.2/README.md
@@ -0,0 +1,3 @@
+## Demo Package
+
+Test package for learning how to create and publish a python package
---
+++ packages/muhaftab-demo-package/muhaftab_demo_package-0.0.2.tar.gz/muhaftab_demo_package-0.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="muhaftab_demo_package",
+    version="0.0.2",
+    author="Muhammad Aftab",
+    author_email="muhaftabkhan@gmail.com",
+    description="A demo package for learning purpose",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6"
+)
