9a360e5874cfbdbb182e93713ac9d506d0065142
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:42 +0000

    Add package hca-cli/0.1.1/hca-cli-0.1.1.tar.gz

---
+++ packages/hca-cli/hca-cli-0.1.1.tar.gz/hca-cli-0.1.1/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name="hca-cli",
+    version="0.1.1",
+    url='https://pypi.org/project/hca',
+    license='Apache Software License',
+    author='Please see https://pypi.org/project/hca',
+    author_email='akislyuk@chanzuckerberg.com',
+    description='Please see https://pypi.org/project/hca',
+    long_description='Please see https://pypi.org/project/hca'
+)
