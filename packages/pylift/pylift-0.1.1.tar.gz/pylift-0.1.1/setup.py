1f710ea252caaa1b2a887d021463fa29921859a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:56 +0000

    Add package pylift/0.1.1/pylift-0.1.1.tar.gz

---
+++ packages/pylift/pylift-0.1.1.tar.gz/pylift-0.1.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+setup(name='pylift',
+      version='0.1.1',
+      description='Python implementation of uplift modeling.',
+      author='Robert Yi, Will Frost',
+      author_email='robert@ryi.me',
+      url='https://github.com/pylift/pylift',
+      install_requires=[
+            'numpy',
+            'matplotlib',
+            'scikit-learn',
+            'scipy',
+            'seaborn',
+            'xgboost'
+          ],
+      packages=find_packages(),
+      zip_safe=False)
