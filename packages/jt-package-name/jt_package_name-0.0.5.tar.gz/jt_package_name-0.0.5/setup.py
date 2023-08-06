73a3bb52cb1e07bc2a8dea3ccebf74ae5a261212
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:05 +0000

    Add package jt-package-name/0.0.5/jt_package_name-0.0.5.tar.gz

---
+++ packages/jt-package-name/jt_package_name-0.0.5.tar.gz/jt_package_name-0.0.5/setup.py
@@ -0,0 +1,33 @@
+"""
+jt_package_name Python package is for Joe Tilsed to learn how to write a python package and
+upload it onto PyPi. Also to show and teach others how to do it themselves.
+---
+setup.py is the build script for setuptools.
+It tells setuptools about your package (such as the name and version) as well as which code files to include.
+"""
+
+import setuptools
+
+with open('README.md', 'r') as readme:
+    long_description = readme.read()
+
+setuptools.setup(
+    name='jt_package_name',
+    version="0.0.5",
+    author="Joe Tilsed",
+    author_email="Joe@Tilsed.com",
+    description="Python Package Template",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://linkedin.com/in/joetilsed/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+	keywords="jt_package_name python"
+)
+
+
+# That's all folks...
