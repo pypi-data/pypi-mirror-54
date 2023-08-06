084715a9c2e43fe18b088ddeaa9fa713218fd717
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:53 +0000

    Add package go-proto-validators/0.0.2/go-proto-validators-0.0.2.tar.gz

---
+++ packages/go-proto-validators/go-proto-validators-0.0.2.tar.gz/go-proto-validators-0.0.2/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='go-proto-validators',
+    version='0.0.2',
+    author='supernan1994',
+    author_email='supernan1994@gmail.com',
+    url='http://supernan1994.github.io/',
+    description=u'python compile files for https://github.com/mwitkow/go-proto-validators',
+    packages=['github'],
+)
