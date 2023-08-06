0e491a6169d1de59569eac1c5e08da71b7591d38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:19 +0000

    Add package dirtrace/0.0.2/dirtrace-0.0.2.tar.gz

---
+++ packages/dirtrace/dirtrace-0.0.2.tar.gz/dirtrace-0.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="dirtrace",
+    version="0.0.2",
+    description="generate chromium's trace event format json file from directory content file size",
+    url="https://github.com/everettjf/dirtrace",
+    author="everettjf",
+    author_email="everettjf@live.com",
+    license='MIT',
+    packages=["dirtrace"],
+    entry_points="""
+         [console_scripts]
+         dirtrace = dirtrace.dirtrace:main
+    """,
+    install_requires=[
+    ],
+    zip_safe=False
+)
