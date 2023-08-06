8f781d3f1d207c213686ce6988dea050dc76b89f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:50 +0000

    Add package arcade/2.1.5/arcade-2.1.5-py2.py3-none-any.whl

---
+++ packages/arcade/arcade-2.1.5-py2.py3-none-any.whl/arcade/arcade_types.py
@@ -0,0 +1,15 @@
+"""
+Module specifying data custom types used for type hinting.
+"""
+from typing import Tuple
+from typing import List
+from typing import Union
+
+RGB = Union[Tuple[int, int, int], List[int]]
+RGBA = Union[Tuple[int, int, int, int], List[int]]
+Color = Union[RGB, RGBA]
+Point = Union[Tuple[float, float], List[float]]
+Vector = Point
+PointList = Union[Tuple[Point, ...], List[Point]]
+Rect = Union[Tuple[float, float, float, float], List[float]]  # x, y, width, height
+RectList = Union[Tuple[Rect, ...], List[Rect]]
