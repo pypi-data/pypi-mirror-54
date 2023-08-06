d116d302428a9a7febbd4322c1984f11a81bc75d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:53 +0000

    Add package qiitacli/0.1.1/qiitacli-0.1.1.tar.gz

---
+++ packages/qiitacli/qiitacli-0.1.1.tar.gz/qiitacli-0.1.1/qiitacli/__init__.py
@@ -0,0 +1,10 @@
+import os
+from pathlib import Path
+
+PRODUCT_NAME = 'qiitacli'
+ACCESSTOKEN_PATH = os.environ.get(
+    "QIITACLI_ACCESSTOKEN_PATH",
+    os.path.join(Path.home(), ".{}.secret".format(PRODUCT_NAME)))
+
+__version__ = '0.1.1'
+__name__ = PRODUCT_NAME
