a32c8e6f03b6b61dff50fd350c69a762595438bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:36 +0000

    Add package familytree/0.0.5/familytree-0.0.5.tar.gz

---
+++ packages/familytree/familytree-0.0.5.tar.gz/familytree-0.0.5/setup.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+import setuptools
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="familytree",
+    version="0.0.5",
+    author="Bodhi Wang",
+    author_email="jyxz5@hotmail.com",
+    description="This program creates family tree graphs from a simple text files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tjnh05/familytreemaker",
+    packages=setuptools.find_packages(),
+    package_data={
+        'familytree': ['*.svg','*.txt','README.md'],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
