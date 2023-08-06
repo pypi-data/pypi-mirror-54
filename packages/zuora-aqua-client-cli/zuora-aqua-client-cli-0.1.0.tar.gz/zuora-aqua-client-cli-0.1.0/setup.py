3516a7c64b3de4d3335e23954e742b2f42cd70d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:10 +0000

    Add package zuora-aqua-client-cli/0.1.0/zuora-aqua-client-cli-0.1.0.tar.gz

---
+++ packages/zuora-aqua-client-cli/zuora-aqua-client-cli-0.1.0.tar.gz/zuora-aqua-client-cli-0.1.0/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['zuora_aqua_client_cli']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=7.0,<8.0', 'requests>=2.22,<3.0']
+
+entry_points = \
+{'console_scripts': ['zacc = zuora_aqua_client_cli.run_zooql:main']}
+
+setup_kwargs = {
+    'name': 'zuora-aqua-client-cli',
+    'version': '0.1.0',
+    'description': 'Run ZOQL queries through AQuA from the command line',
+    'long_description': None,
+    'author': 'Janos Molnar',
+    'author_email': 'janosmolnar1001@gmail.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
