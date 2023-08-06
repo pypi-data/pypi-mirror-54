7666a8aaa19ca072c8dfbe06229266cef9c6605f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:57 +0000

    Add package makey/0.0.15/makey-0.0.15-py3-none-any.whl

---
+++ packages/makey/makey-0.0.15-py3-none-any.whl/makey/__init__.py
@@ -0,0 +1,186 @@
+import getpass
+import logging
+import os
+import re
+from contextlib import contextmanager
+from pathlib import Path
+from urllib.parse import urlparse, urlunparse
+from urllib.request import urlopen
+
+import plumbum
+from plumbum import local, cmd, ProcessExecutionError
+
+logging.basicConfig(level=os.environ.get("MAKEY_LOGLEVEL", "WARNING"))
+
+CMAKE_PROJECT_NAME_PATTERN = re.compile(r"project\((.*)\)", re.IGNORECASE)
+PACKAGE_NAME_PATTERN = re.compile(r"package: (.*?) generated")
+VERSION_STRING_PATTERN = re.compile(r"v(\d+)[\.-](\d+)[\.-](\d+)")
+HTTP_SCHEMES = {"http", "https"}
+
+
+@contextmanager
+def track_new_files(path):
+    files = set(path)
+    changed = set()
+    yield changed
+    changed.update(set(path) - files)
+
+
+def load_source(url_or_path: str) -> plumbum.Path:
+    parsed_url = urlparse(url_or_path)
+
+    with track_new_files(local.cwd) as new_files:
+        # Load data
+        if parsed_url.scheme.startswith("git+"):
+            original_scheme = parsed_url.scheme[len("git+") :]
+            repo_url = urlunparse(
+                (
+                    original_scheme,
+                    parsed_url.netloc,
+                    parsed_url.path,
+                    parsed_url.params,
+                    parsed_url.query,
+                    "",
+                )
+            )
+            args = ["clone", repo_url, "--depth=1"]
+            if parsed_url.fragment:
+                args.append(f"--branch={parsed_url.fragment}")
+            cmd.git(*args)
+
+        elif parsed_url.scheme in HTTP_SCHEMES:
+            (cmd.wget["-qO-", url_or_path] | cmd.tar["xvz"])()
+
+        else:
+            url_path = local.cwd / url_or_path
+            if url_path.is_dir():
+                cmd.cp("-r", url_path, local.cwd)
+            else:
+                cmd.tar("-xvf", url_or_path)
+
+    project_path, = new_files
+    return project_path
+
+
+def find_version_from_git() -> str:
+    version_string = cmd.git("describe", "--tags", "--abbrev=0")
+    major, minor, patch = VERSION_STRING_PATTERN.search(version_string).groups()
+    return f"{major}.{minor}.{patch}"
+
+
+def load_cmake_project_name(cmakelists_contents: str) -> str:
+    library_name_match = CMAKE_PROJECT_NAME_PATTERN.search(cmakelists_contents)
+    if library_name_match is None:
+        raise ValueError("Unable to find package name from CMakeLists.txt")
+    return library_name_match.group(1).strip()
+
+
+def build_with_cpack(verbose: bool = False) -> plumbum.LocalPath:
+    author = getpass.getuser()
+    result = run_command(
+        cmd.cpack["-G", "DEB", "-D", f'CPACK_PACKAGE_CONTACT="{author}"'], verbose
+    )
+    return local.cwd / PACKAGE_NAME_PATTERN.search(result).group(1).strip()
+
+
+def build_with_checkinstall(
+    name: str, version: str, verbose: bool = False
+) -> plumbum.LocalPath:
+    # Checkinstall returns filename on stderr, let's just use this approach
+    with track_new_files(local.cwd) as new_files:
+        run_command(
+            cmd.checkinstall[
+                f"--pkgname={name}",
+                f"--pkgversion={version}",
+                "--install=no",
+                "--fstrans=yes",
+            ],
+            verbose,
+        )
+    return next(p for p in new_files if p.suffix == ".deb")
+
+
+def run_command(command, verbose: bool):
+    if verbose:
+        retcode, stdout, stderr = command & plumbum.TEE
+        return stdout
+    else:
+        return command()
+
+
+def apply_patch(patch: str):
+    parsed_url = urlparse(patch)
+    if parsed_url.scheme in HTTP_SCHEMES:
+        response = urlopen(patch)
+        contents = response.read().decode()
+    else:
+        contents = Path(patch).read_text()
+
+    (cmd.git["apply", "-"] << contents)()
+
+
+def makey(
+    url_or_path: str,
+    jobs: int = 1,
+    version: str = None,
+    verbose: bool = False,
+    patch: str = None,
+    cmake_args: list = None,
+    dpkg_args: list = None,
+    force_checkinstall: bool = False,
+    install_package: bool = True,
+):
+    # Parse file from URL or local tarball
+    print(f"Loading source from {url_or_path}")
+    project_path = load_source(url_or_path)
+
+    # Place source inside the project directory
+    source_path = project_path.move(local.cwd / f"{project_path.name}_source").move(
+        project_path / "source"
+    )
+    build_path = project_path / "build"
+    build_path.mkdir()
+
+    # Support patching files
+    if patch is not None:
+        print("Applying patch")
+        with local.cwd(source_path):
+            apply_patch(patch)
+
+    # Make package with CMAKE
+    with local.cwd(build_path):
+        print("Running CMake")
+        run_command(cmd.cmake[(source_path, *cmake_args)], verbose)
+
+        print("Running Make")
+        run_command(cmd.make[f"-j{jobs}"], verbose)
+
+        # Try CPack, otherwise use checkinstall
+        if (local.cwd / "CPackConfig.cmake").exists() and not force_checkinstall:
+            print("Installing with CPack")
+            deb_path = build_with_cpack(verbose=verbose)
+        else:
+            print("Installing with checkinstall")
+            # Find library name
+            library_name = load_cmake_project_name(
+                (source_path / "CMakeLists.txt").read("utf8")
+            )
+            if version is None:
+                with local.cwd(source_path):
+                    try:
+                        version = find_version_from_git()
+                        print(f"Using version {version} from Git")
+                    except ProcessExecutionError:
+                        version = input(
+                            "Could not load version from Git, please enter version string (major.minor.patch):"
+                        )
+
+            deb_path = build_with_checkinstall(library_name, version, verbose=verbose)
+
+        print(f"Created deb file: {deb_path}")
+
+        if install_package:
+            print("Installing deb file ...")
+            run_command(cmd.sudo[cmd.dpkg][(*dpkg_args, "-i", deb_path)], verbose)
+
+    return deb_path
---
+++ packages/makey/makey-0.0.15-py3-none-any.whl/makey/__main__.py
@@ -0,0 +1,46 @@
+from argparse import ArgumentParser
+from . import makey
+
+
+def make_arguments(opts, flags):
+    return [f"-{o}" for o in opts] + [f"--{f}" for f in flags]
+
+
+def main(args=None):
+    parser = ArgumentParser()
+    parser.add_argument(
+        "url_or_path",
+        type=str,
+        help="URL/local path of tar file, or url of git repository prefixed with git+.",
+    )
+    parser.add_argument(
+        "-j", "--jobs", type=str, help="Number of jobs for make command.", default=1
+    )
+    parser.add_argument("-v", "--version", type=str, help="Project version.")
+    parser.add_argument(
+        "-c", "--force_checkinstall", action="store_true", help="Force checkinstall."
+    )
+    parser.add_argument("-p", "--patch", help="Apply patch to source (URL or filepath).")
+    parser.add_argument("-V", "--verbose", action="store_true", help="Turn on verbose mode.")
+    parser.add_argument("-b", "--build_only", action="store_true", help="Only build package.")
+    parser.add_argument("--cflag", action="append", nargs="+", help="CMake flags pass-through.", default=[])
+    parser.add_argument("--copt", action="append", nargs="+", help="CMake options pass-through.", default=[])
+    parser.add_argument("--dflag", action="append", nargs="+", help="dpkg flags pass-through.", default=[])
+    parser.add_argument("--dopt", action="append", nargs="+", help="dpkg options pass-through.", default=[])
+    args = parser.parse_args(args)
+
+    makey(
+        args.url_or_path,
+        args.jobs,
+        args.version,
+        args.verbose,
+        patch=args.patch,
+        cmake_args=make_arguments(sum(args.copt, []), sum(args.cflag, [])),
+        dpkg_args=make_arguments(sum(args.dopt, []), sum(args.dflag, [])),
+        force_checkinstall=args.force_checkinstall,
+        install_package=not args.build_only,
+    )
+
+
+if __name__ == "__main__":
+    main()
