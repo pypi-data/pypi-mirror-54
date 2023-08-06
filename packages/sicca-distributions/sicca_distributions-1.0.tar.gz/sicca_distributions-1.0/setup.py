6733fdc965993b747f6616ca47022a63e4d95ee4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:58 +0000

    Add package sicca-distributions/1.0/sicca_distributions-1.0.tar.gz

---
+++ packages/sicca-distributions/sicca_distributions-1.0.tar.gz/sicca_distributions-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='sicca_distributions',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['sicca_distributions'],
+      author="Eduardo Siqueira",
+      author_email="eduardochagasds@gmail.com",
+      zip_safe=False)
