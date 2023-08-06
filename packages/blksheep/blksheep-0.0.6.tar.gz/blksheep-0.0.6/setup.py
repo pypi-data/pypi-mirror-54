279275a68bc653188b7d418ac4538f281e2f6b6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:54 +0000

    Add package blksheep/0.0.6/blksheep-0.0.6.tar.gz

---
+++ packages/blksheep/blksheep-0.0.6.tar.gz/blksheep-0.0.6/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="blksheep",
+    version="0.0.6",
+    author="Ruggles Lab",
+    author_email="ruggleslab@gmail.com",
+    description="A package for differential extreme values analysis",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ruggleslab/blackSheep/",
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS",
+        "Operating System :: Unix",
+    ],
+    install_requires=[
+        "pandas >= 0.24.2",
+        "numpy >= 1.16.4",
+        "scipy >= 1.2.1",
+        "matplotlib >= 3.1.0",
+        "seaborn >= 0.9.0",
+        "statsmodels >= 0.10.0"
+    ],
+    packages=setuptools.find_packages(
+        exclude=["*.tests", "*.tests.*", "tests.*", "tests"]
+    ),
+    entry_points={"console_scripts": ["blacksheep = blacksheep.cli:_main"]},
+)
