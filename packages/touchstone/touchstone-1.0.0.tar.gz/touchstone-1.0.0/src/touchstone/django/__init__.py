2fbbe20f8b60a816896a5a12002ce1ad9b1c68de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:13 +0000

    Add package touchstone/1.0.0/touchstone-1.0.0.tar.gz

---
+++ packages/touchstone/touchstone-1.0.0.tar.gz/touchstone-1.0.0/src/touchstone/django/__init__.py
@@ -0,0 +1,4 @@
+from .middleware import InjectViewsMiddleware
+from .properties import MagicInjectedProperties, get_container
+
+__all__ = ["InjectViewsMiddleware", "MagicInjectedProperties", "get_container"]
