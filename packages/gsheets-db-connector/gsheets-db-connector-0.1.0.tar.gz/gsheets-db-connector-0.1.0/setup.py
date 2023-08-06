169838c24a729091bdd958281669e0e33f336b19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:28 +0000

    Add package gsheets-db-connector/0.1.0/gsheets-db-connector-0.1.0.tar.gz

---
+++ packages/gsheets-db-connector/gsheets-db-connector-0.1.0.tar.gz/gsheets-db-connector-0.1.0/setup.py
@@ -0,0 +1,34 @@
+"""
+Build the PyPi package.
+"""
+
+import setuptools
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='gsheets-db-connector',
+    version='0.1.0',
+    author='Shwetabh Kumar',
+    author_email='shwetabh.kumar@fyle.in',
+    description='Connects Google sheets to a database connector to transfer information to and fro.',
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords=['fyle', 'db', 'python', 'sdk', 'sqlite'],
+    url='https://github.com/fylein/gsheets-db-connector',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'logger==1.4',
+        'pandas==0.25.2',
+        'typing==3.7.4.1'
+    ],
+    classifiers=[
+        'Topic :: Internet :: WWW/HTTP',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
