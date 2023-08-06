9f3acf1d179d9facdb97fa0a8b725daaccdf7937
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:32 +0000

    Add package inicheck/0.7.2/inicheck-0.7.2.tar.gz

---
+++ packages/inicheck/inicheck-0.7.2.tar.gz/inicheck-0.7.2/inicheck/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import os
+__author__ = """Micah Johnson"""
+__email__ = 'micah.johnson150@gmail.com'
+__version__ = '0.7.2'
+
+# Keywords for conditional statements in a master config file
+__trigger_keywords__ = ['trigger']
+
+# Key words expected in the section name of a recipe
+__recipe_keywords__ = ['recipe']
+
+# Add in core configs for test purposes
+__core_config__ = os.path.abspath(os.path.dirname(__file__)+'/../tests/test_configs/CoreConfig.ini')
+__recipes__ = os.path.abspath(os.path.dirname(__file__)+'/../tests/test_configs/recipes.ini')
