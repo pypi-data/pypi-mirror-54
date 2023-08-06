dddf00646ed01873db4ea107a2a34783912823fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:33 +0000

    Add package supersql/2019.0.2/supersql-2019.0.2.tar.gz

---
+++ packages/supersql/supersql-2019.0.2.tar.gz/supersql-2019.0.2/README.md
@@ -0,0 +1,75 @@
+Supersql Library
+================
+
+Supersql is a `very thin wrapper` on top of SQL that enables you write SQL code in python easily.
+
+&nbsp;
+
+## Why?
+Let's be honest, writing sql templates using string formatting is really painful.
+SQLAlchemy is great, but sometimes an ORM is not what you need, and whilst the new
+`f strings` in python solve a lot of problems, complex SQL templating is not of
+them.
+
+Supersql makes it super simple to connect to and start querying a database in python.
+
+Let the code do the explanation:
+```py
+
+from supersql import Connection, Query
+
+
+connection = Connection('postgres:localhost:5432', user='postgres', password='postgres')
+
+query = Query()
+
+
+results = query.SELECT(
+        'first_name', 'last_name', 'email'
+    ).FROM(
+        'employees'
+    ).WHERE('email', equals='someone@example.com').run()
+
+
+for result in results:
+    print(result)
+
+```
+
+&nbsp;
+
+Too many magic literals? I agree. Let's try that again with a Schema
+
+```py
+# Schemas help you remove all those magic literals e.g. 'email' string typed twice
+# from your code
+from supersql import Schema, String, Date, Integer
+
+class Employee(Schema):
+    __pk__ = ('email', 'identifier')
+
+    identifier = UUID(pg='uuid_version1', mysql=None)  # mysql included for examples sake
+    email = String(required=True, unique=None, length=25)
+    age = Integer()
+    first_name = String(required=True)
+    last_name = String(25)
+    created_on = Date()
+
+
+# Now lets try again
+emp = Employee()
+results = query.SELECT(
+    emp.first_name, emp.last_name, emp.email
+).FROM(
+    emp
+).WHERE(
+    emp.email, equals='someone@example.com'
+).run()
+```
+
+
+&nbsp;
+
+# NOTE: Still Very Much In Development -- Expected Launch Date (November 11 2019)
+
+Supersql is not installable until launch
---
+++ packages/supersql/supersql-2019.0.2.tar.gz/supersql-2019.0.2/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['src', 'src.containers']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['supersql = supersql:main']}
+
+setup_kwargs = {
+    'name': 'supersql',
+    'version': '2019.0.2',
+    'description': 'Thin wrapper on top of SQL that enables you write SQL code in python easily',
+    'long_description': "Supersql Library\n================\n\nSupersql is a `very thin wrapper` on top of SQL that enables you write SQL code in python easily.\n\n&nbsp;\n\n## Why?\nLet's be honest, writing sql templates using string formatting is really painful.\nSQLAlchemy is great, but sometimes an ORM is not what you need, and whilst the new\n`f strings` in python solve a lot of problems, complex SQL templating is not of\nthem.\n\nSupersql makes it super simple to connect to and start querying a database in python.\n\nLet the code do the explanation:\n```py\n\nfrom supersql import Connection, Query\n\n\nconnection = Connection('postgres:localhost:5432', user='postgres', password='postgres')\n\nquery = Query()\n\n\nresults = query.SELECT(\n        'first_name', 'last_name', 'email'\n    ).FROM(\n        'employees'\n    ).WHERE('email', equals='someone@example.com').run()\n\n\nfor result in results:\n    print(result)\n\n```\n\n&nbsp;\n\nToo many magic literals? I agree. Let's try that again with a Schema\n\n```py\n# Schemas help you remove all those magic literals e.g. 'email' string typed twice\n# from your code\nfrom supersql import Schema, String, Date, Integer\n\nclass Employee(Schema):\n    __pk__ = ('email', 'identifier')\n\n    identifier = UUID(pg='uuid_version1', mysql=None)  # mysql included for examples sake\n    email = String(required=True, unique=None, length=25)\n    age = Integer()\n    first_name = String(required=True)\n    last_name = String(25)\n    created_on = Date()\n\n\n# Now lets try again\nemp = Employee()\nresults = query.SELECT(\n    emp.first_name, emp.last_name, emp.email\n).FROM(\n    emp\n).WHERE(\n    emp.email, equals='someone@example.com'\n).run()\n```\n\n\n&nbsp;\n\n# NOTE: Still Very Much In Development -- Expected Launch Date (November 11 2019)\n\nSupersql is not installable until launch\n",
+    'author': 'Raymond Ortserga',
+    'author_email': 'codesage@live.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
