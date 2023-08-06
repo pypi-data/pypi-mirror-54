1692b8f4f10cd0546ba86ec57020f79bfee32ba6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:31 +0000

    Add package mb-distributions/0.4/mb_distributions-0.4.tar.gz

---
+++ packages/mb-distributions/mb_distributions-0.4.tar.gz/mb_distributions-0.4/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='mb_distributions',
+      version='0.4',
+      description='Gaussian and Binomial distributions',
+      packages=['mb_distributions'],
+      author='Mani Bhushan',
+      author_email='manibhushan.cs@gmail.com',
+      zip_safe=False)
