6e660679d1ab6e7e212f82b32c385bd07540b674
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:27 +0000

    Add package tramway/0.4.2/tramway-0.4.2.tar.gz

---
+++ packages/tramway/tramway-0.4.2.tar.gz/tramway-0.4.2/tramway/helper/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+# Copyright © 2017, Institut Pasteur
+#   Contributor: François Laurent
+
+# This file is part of the TRamWAy software available at
+# "https://github.com/DecBayComp/TRamWAy" and is distributed under
+# the terms of the CeCILL license as circulated at the following URL
+# "http://www.cecill.info/licenses.en.html".
+
+# The fact that you are presently reading this means that you have had
+# knowledge of the CeCILL license and that you accept its terms.
+
+
+#from .analysis import *
+from .tessellation import *
+from .inference import *
+#from .feature import *
+
+#__all__ = ['UseCaseWarning', 'IgnoredInputWarning', 'hdf_extensions', 'imt_extensions', 'fig_formats', \
+#	'tessellate', 'cell_plot', 'find_imt', \
+#	'infer', 'map_plot']
+
