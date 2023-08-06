e48a9e6f21fb457cd4d558124bb0a6ac83be6f66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:03 +0000

    Add package doreah/1.1.7/doreah-1.1.7-py3-none-any.whl

---
+++ packages/doreah/doreah-1.1.7-py3-none-any.whl/doreah/__init__.py
@@ -0,0 +1,44 @@
+name = "doreah"
+version = 1,1,7
+versionstr = ".".join(str(n) for n in version)
+author = {
+	"name": "Johannes Krattenmacher",
+	"email": "python@krateng.dev",
+	"github": "krateng"
+}
+requires = [
+	"lxml",
+	"mechanicalsoup",
+	"requests",
+	"pyyaml"
+]
+
+
+__all__ = [
+	"caching",
+	"io",
+	"logging",
+	"persistence",
+	"pyhp",
+	"regular",
+	"scraping"
+	"settings",
+	"timing",
+	"tsv"
+
+]
+
+
+
+
+# useful things for everyone
+
+
+def deprecated(func):
+	"""Function decorator to deprecate a function"""
+
+	def newfunc(*args,**kwargs):
+		print("\033[93m" + "Function " + func.__name__ + " is deprecated!" + "\033[0m")
+		return func(*args,**kwargs)
+
+	return newfunc
