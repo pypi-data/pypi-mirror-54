6bf6c47a29512687df12ab5eac6730ba60471f99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:04 +0000

    Add package BittyTax/0.2.0/BittyTax-0.2.0-py2.py3-none-any.whl

---
+++ packages/BittyTax/BittyTax-0.2.0-py2.py3-none-any.whl/bittytax/holdings.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+# (c) Nano Nano Ltd 2019
+
+import logging
+
+from decimal import Decimal
+
+from .config import config
+
+log = logging.getLogger()
+
+class Holdings(object):
+    def __init__(self, asset):
+        self.asset = asset
+        self.quantity = Decimal(0)
+        self.cost = Decimal(0)
+
+    def add_tokens(self, quantity, cost):
+        self.quantity += quantity
+        self.cost += cost
+
+        if config.args.debug:
+            log.debug("%s=%s (+%s) %s%s %s (+%s%s %s)",
+                      self.asset,
+                      self.format_quantity(),
+                      '{:0,f}'.format(quantity.normalize()),
+                      config.sym(), self._format_cost(), config.CCY,
+                      config.sym(), '{:0,.2f}'.format(cost), config.CCY)
+    def subtract_tokens(self, quantity, cost):
+        self.quantity -= quantity
+        self.cost -= cost
+
+        if config.args.debug:
+            log.debug("%s=%s (-%s) %s%s %s (-%s%s %s)",
+                      self.asset,
+                      self.format_quantity(),
+                      '{:0,f}'.format(quantity.normalize()),
+                      config.sym(), self._format_cost(), config.CCY,
+                      config.sym(), '{:0,.2f}'.format(cost), config.CCY)
+
+    def format_quantity(self):
+        return '{:0,f}'.format(self.quantity.normalize())
+
+    def _format_cost(self):
+        return '{:0,.2f}'.format(self.cost)
