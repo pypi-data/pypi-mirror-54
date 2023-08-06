1e7d1d4ddd0c35a763c17efb393dc69246bdb532
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:28 +0000

    Add package distributions-rawat170594/0.1/distributions_rawat170594-0.1.tar.gz

---
+++ packages/distributions-rawat170594/distributions_rawat170594-0.1.tar.gz/distributions_rawat170594-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_rawat170594',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_rawat170594'],
+      zip_safe=False)
