d61aa8e566576a1b5d40a210d11e8ad82b50c057
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:54 +0000

    Add package show-time/0.0.1/show-time-0.0.1.tar.gz

---
+++ packages/show-time/show-time-0.0.1.tar.gz/show-time-0.0.1/setup.py
@@ -0,0 +1,30 @@
+#-*- coding:utf8 -*-
+# Copyright (c) 2019 barriery
+# Python release: 3.7.0
+import re
+from setuptools import setup, find_packages
+
+setup(
+    name = 'show-time',
+    version = '0.0.1',
+    description = 'A spider that crawls the time of various shows.',
+    url = 'https://github.com/barrierye/showtime',
+    keywords = 'show play spider',
+    
+    author = 'barriery',
+    author_email = 'barriery@qq.com',
+    maintainer = 'barriery',
+    maintainer_email = 'barriery@qq.com',
+
+    include_package_data=True,
+
+    python_requires='>=3.5',
+
+    install_requires=[
+        'aiohttp==3.6.2',
+        'requests==2.22.0',
+        'protobuf==3.10.0',
+    ],
+
+    packages = find_packages(where='.'),
+)
