c15532444a06d51dc1a52e0aa857bf7d2f475050
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:33 +0000

    Add package OpenDataTools/0.9.9/OpenDataTools-0.9.9-py3-none-any.whl

---
+++ packages/OpenDataTools/OpenDataTools-0.9.9-py3-none-any.whl/opendatatools/__init__.py
@@ -0,0 +1,14 @@
+# encoding: utf-8
+"""
+    Open Data
+    ~~~~
+    Open source data tools
+    copyright: (c) 2017 quantOS-org.
+    license: Apache 2.0, see LICENSE for details.
+"""
+
+import os
+
+__version__ = '0.9.9'
+SOURCE_ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
