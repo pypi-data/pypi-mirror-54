9e89871a7785a15c15635d79a860b45d11f3395e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:39 +0000

    Add package jaroken-distributions/0.1/jaroken_distributions-0.1.tar.gz

---
+++ packages/jaroken-distributions/jaroken_distributions-0.1.tar.gz/jaroken_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='jaroken_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['jaroken_distributions'],
+      zip_safe=False)
