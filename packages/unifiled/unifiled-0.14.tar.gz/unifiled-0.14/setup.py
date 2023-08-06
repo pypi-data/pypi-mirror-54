4ab7fe53ba0bc0ad3b058257e7d42fda4e8ff20c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:16:28 +0000

    Add package unifiled/0.14/unifiled-0.14.tar.gz

---
+++ packages/unifiled/unifiled-0.14.tar.gz/unifiled-0.14/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+  name = 'unifiled',
+  packages = ['unifiled'],
+  version = '0.14',
+  license='MIT',
+  description = 'Easily connect to Ubiquiti Unifi led devices',
+  author = 'Floris Van der krieken',
+  author_email = 'git@florisvdk.net',
+  url = 'https://github.com/florisvdk/unifiled',
+  download_url = 'https://github.com/florisvdk/unifiled/archive/v_01.tar.gz',
+  keywords = ['UNIFI', 'LED', 'CONTROLLER', 'UBIQUITI'],
+  install_requires=[
+          'requests',
+          'urllib3',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
