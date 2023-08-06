ded6957b0961764b035965664b96b8c6deb33170
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:11 +0000

    Add package jintian-architecture-code/3.6/jintian_architecture_code-3.6-py3-none-any.whl

---
+++ packages/jintian-architecture-code/jintian_architecture_code-3.6-py3-none-any.whl/com/fw/db/redis_db.py
@@ -0,0 +1,37 @@
+import redis
+
+from com.fw.base.base_exception import BaseException
+from com.fw.base.base_log import logger
+from com.fw.system.red_conf import system_conf
+
+
+class RedisDB(object):
+
+    def __init__(self):
+        self.init_pool()
+
+    def init_pool(self):
+        if not system_conf.has_group('environment'):
+            raise BaseException("没有配置数据库环境...")
+        version = system_conf.get_value('environment', 'version')
+
+        key = 'redis' + "_" + version
+
+        if not system_conf.has_group(key):
+            logger.warn("-----WARN：没有配置redis -------")
+            return
+
+        self.redis_pool = redis.ConnectionPool(host=system_conf.get_value(key, 'host'), port=system_conf.get_value(key, 'port'),max_connections=30,socket_connect_timeout=3)
+
+        self.connection_client = redis.Redis(connection_pool=self.redis_pool)
+
+        self.connection_client.get_str = self.get_str
+        logger.info("------ redis 初始化成功 ------")
+
+    def get_str(self, name):
+        return str(self.connection_client.get(name), encoding="utf8")
+
+redis = RedisDB()
+
+redis_db = redis.connection_client
+
