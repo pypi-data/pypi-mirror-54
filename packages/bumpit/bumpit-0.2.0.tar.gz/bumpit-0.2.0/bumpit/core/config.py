07945b06cdcd12cd395bd81f0693a7e8b9807614
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:03 +0000

    Add package bumpit/0.2.0/bumpit-0.2.0.tar.gz

---
+++ packages/bumpit/bumpit-0.2.0.tar.gz/bumpit-0.2.0/bumpit/core/config.py
@@ -0,0 +1,45 @@
+from dataclasses import dataclass
+import os
+import yaml
+
+
+@dataclass
+class Configuration:
+    current_version: str
+    strategy: str
+    tag: bool
+    tag_format: str
+    auto_remote_push: bool
+    tracked_files: list
+
+    @staticmethod
+    def parse(file):
+        contents = Configuration._load_config(file)
+
+        mandatory_fields = [
+            "current_version",
+            "strategy",
+            "tag",
+            "tag_format",
+            "tracked_files",
+        ]
+        for field in mandatory_fields:
+            if contents.get(field) is None:
+                raise ValueError(f"Configuration field is missing '{field}'")
+
+        if contents["tag"] not in [True, False]:
+            raise ValueError(
+                f"Invalid tag value '{contents['tag']}'. It should be a bool."
+            )
+
+        return Configuration(**contents)
+
+    @staticmethod
+    def _load_config(file):
+        if not os.path.isfile(file):
+            raise ValueError(f"Invalid config file. Path '{file}' does not exist.")
+
+        with open(file, "rb") as fh:
+            contents = yaml.safe_load(fh)
+
+        return contents or {}
