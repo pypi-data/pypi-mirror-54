db472128448510a0542102aac2ff8c5bc6d4a3cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:50 +0000

    Add package tf-models-official/0.0.2.dev0/tf-models-official-0.0.2.dev0.tar.gz

---
+++ packages/tf-models-official/tf-models-official-0.0.2.dev0.tar.gz/tf-models-official-0.0.2.dev0/setup.py
@@ -0,0 +1,22 @@
+"""Install TensorFlow Official Models."""
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='tf-models-official',
+    version='0.0.2dev',
+    description='TensorFlow Official Models',
+    author='Google Inc.',
+    author_email='no-reply@google.com',
+    url='https://github.com/tensorflow/models',
+    license='Apache 2.0',
+    packages=find_packages(include=['official*'], exclude=[]),
+    install_requires=[
+        'six',
+    ],
+    extras_require={
+        'tensorflow': ['tensorflow>=2.0.0'],
+        'tensorflow_gpu': ['tensorflow-gpu>=2.0.0'],
+        'tensorflow-hub': ['tensorflow-hub>=0.6.0'],
+    },
+)
