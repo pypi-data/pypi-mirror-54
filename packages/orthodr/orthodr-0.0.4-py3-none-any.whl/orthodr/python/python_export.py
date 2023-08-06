b127b882bb02e618e22a01a2f0a23cc9fbc6c92e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:05 +0000

    Add package orthodr/0.0.4/orthodr-0.0.4-py3-none-any.whl

---
+++ packages/orthodr/orthodr-0.0.4-py3-none-any.whl/orthodr/python/python_export.py
@@ -0,0 +1,10 @@
+from .pycpp_export import _add, _demoprint, _get_dict, _test_fn, _test_numpy
+add = _add
+demoprint = _demoprint
+get_dict = _get_dict
+test_fn = _test_fn
+test_numpy = _test_numpy
+
+
+def haha():
+    return 3
