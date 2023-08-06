6f68097d2b0d4a9042f31b6ada7fe272f6efc5db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:07 +0000

    Add package pydataapi-postgres/0.1.3/pydataapi_postgres-0.1.3.tar.gz

---
+++ packages/pydataapi-postgres/pydataapi_postgres-0.1.3.tar.gz/pydataapi_postgres-0.1.3/setup.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+
+from pathlib import Path
+
+from setuptools import setup
+from setuptools.config import read_configuration
+
+config = read_configuration(Path(__file__).parent.joinpath('setup.cfg'))
+
+extras_require = {
+    'setup': config['options']['setup_requires'],
+    **config['options']['extras_require'],
+}
+
+extras_require['all'] = [*extras_require.values()]
+# use_scm_version = {'write_to': Path(config['metadata']['name'].replace('-', ''), 'version.py')}
+
+entry_points = {
+    'sqlalchemy.dialects': [
+        'mysql.pydataapipostgres = pydataapipostgres.dialect:MySQLDataAPIDialect',
+        'postgresql.pydataapipostgres = pydataapipostgres.dialect:PostgreSQLDataAPIDialect',
+    ],
+}
+
+setup(extras_require=extras_require, version='0.1.3', entry_points=entry_points)
