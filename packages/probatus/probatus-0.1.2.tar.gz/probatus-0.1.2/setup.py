e244b094316e83c639eb5ef8f301b5808a01b8a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:13 +0000

    Add package probatus/0.1.2/probatus-0.1.2.tar.gz

---
+++ packages/probatus/probatus-0.1.2.tar.gz/probatus-0.1.2/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+import os
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+base_packages = ["scikit-learn>=0.20.2",
+                 "pandas>=0.25",
+                 "matplotlib==3.1.1",
+                 "seaborn>=0.9.0",
+                 "shap>=0.29",
+		        "joblib>=0.13.2"]
+
+try:
+    if os.environ.get('CI_COMMIT_TAG'):
+        version = os.environ['CI_COMMIT_TAG']
+    else:
+        version = os.environ['CI_JOB_ID']
+except:
+    version = 'local'
+
+
+setuptools.setup(
+    name='probatus',
+    version=version,
+    description='Tools for machine learning model validation',
+    long_description=read('README.md'),
+    author='RPAA ING',
+    author_email='ml_risk_and_pricing_aa@ing.com',
+    license='ING Open Source',
+    packages=setuptools.find_packages(),
+    package_data={'probatus': ['datasets/data/*.pkl']},
+    install_requires=base_packages,
+    url='https://gitlab.com/ing_rpaa/probatus',
+    zip_safe=False
+)
