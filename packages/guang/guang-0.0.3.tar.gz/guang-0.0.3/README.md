cd16a9e8d97342ee9578622c4caaeeb3c8b968b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:50 +0000

    Add package guang/0.0.3/guang-0.0.3.tar.gz

---
+++ packages/guang/guang-0.0.3.tar.gz/guang-0.0.3/README.md
@@ -0,0 +1,6 @@
+## Some tools function.
+
+*This is the first edition*
+
+*Stay tuned for future releases！*
+
---
+++ packages/guang/guang-0.0.3.tar.gz/guang-0.0.3/guang/__init__.py
@@ -0,0 +1,8 @@
+'''
+This is a universal function library.
+'''
+
+__version__ = '0.0.3'
+__author__ = 'K.y'
+
+from . import *
