795fceb0b174df34732f6a2fa98af0e88ebcd1e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:07 +0000

    Add package SmartDjango/3.6.0.alpha/SmartDjango-3.6.0.alpha.tar.gz

---
+++ packages/SmartDjango/SmartDjango-3.6.0.alpha.tar.gz/SmartDjango-3.6.0.alpha/SmartDjango/__init__.py
@@ -0,0 +1,10 @@
+from .excp import Excp
+from .error import E
+from .analyse import Analyse, AnalyseError
+from .http_code import HttpCode, HttpCode
+
+__all__ = [
+    Excp, E,
+    Analyse, AnalyseError,
+    HttpCode, HttpCode,
+]
---
+++ packages/SmartDjango/SmartDjango-3.6.0.alpha.tar.gz/SmartDjango-3.6.0.alpha/SmartDjango/middleware.py
@@ -0,0 +1,29 @@
+from .excp import Excp
+from django.http import HttpResponse
+from smartify import E
+
+
+class HttpPackMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, r, *args, **kwargs):
+        try:
+            e = self.get_response(r, *args, **kwargs)
+            if isinstance(e, HttpResponse):
+                if e.content.decode().find(
+                        "t return an HttpResponse object. It returned None instead.") == -1:
+                    return e
+                e = None
+            if isinstance(e, E):
+                raise e
+        except E as err:
+            e = err
+        return Excp.http_response(e)
+
+    def process_exception(self, r, excp):
+        from .excp import Excp
+        if isinstance(excp, Excp):
+            return Excp.http_response(excp)
+        else:
+            return None
