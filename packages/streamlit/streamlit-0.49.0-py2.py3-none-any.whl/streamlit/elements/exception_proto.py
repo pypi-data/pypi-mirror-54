96c6c6a84fd110847a439de043a3efa47cb81b90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:45 +0000

    Add package streamlit/0.49.0/streamlit-0.49.0-py2.py3-none-any.whl

---
+++ packages/streamlit/streamlit-0.49.0-py2.py3-none-any.whl/streamlit/elements/exception_proto.py
@@ -0,0 +1,147 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018-2019 Streamlit Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import sys
+import traceback
+
+from streamlit.logger import get_logger
+
+LOGGER = get_logger(__name__)
+
+
+def _format_syntax_error_message(exception):
+    """Returns a nicely formatted SyntaxError message that emulates
+    what the Python interpreter outputs, e.g.:
+
+    > File "raven.py", line 3
+    >   st.write('Hello world!!'))
+    >                            ^
+    > SyntaxError: invalid syntax
+
+    Parameters
+    ----------
+    exception : SyntaxError
+
+    Returns
+    -------
+    str
+
+    """
+    if exception.text:
+        return (
+            'File "%(filename)s", line %(lineno)d\n'
+            "  %(text)s\n"
+            "  %(caret_indent)s^\n"
+            "%(errname)s: %(msg)s"
+            % {
+                "filename": exception.filename,
+                "lineno": exception.lineno,
+                "text": exception.text.rstrip(),
+                "caret_indent": " " * max(exception.offset - 1, 0),
+                "errname": type(exception).__name__,
+                "msg": exception.msg,
+            }
+        )
+    # If a few edge cases, SyntaxErrors don't have all these nice fields. So we
+    # have a fall back here.
+    # Example edge case error message: encoding declaration in Unicode string
+    return str(exception)
+
+
+def marshall(exception_proto, exception, exception_traceback=None):
+    """Marshalls an Exception.proto message.
+
+    Parameters
+    ----------
+    exception_proto : Exception.proto
+        The Exception protobuf to fill out
+
+    exception : BaseException
+        The exception whose data we're extracting
+
+    exception_traceback : Exception Traceback or None
+        If None or False, does not show display the trace. If True,
+        tries to capture a trace automatically. If a Traceback object,
+        displays the given traceback.
+    """
+    exception_proto.type = type(exception).__name__
+
+    stack_trace = get_stack_trace(exception, exception_traceback)
+    exception_proto.stack_trace.extend(stack_trace)
+
+    try:
+        if isinstance(exception, SyntaxError):
+            # SyntaxErrors have additional fields (filename, text, lineno,
+            # offset) that we can use for a nicely-formatted message telling
+            # the user what to fix.
+            exception_proto.message = _format_syntax_error_message(exception)
+        else:
+            exception_proto.message = str(exception)
+    except Exception as str_exception:
+        # Sometimes the exception's __str__/__unicode__ method itself
+        # raises an error.
+        exception_proto.message = ""
+        LOGGER.warning(
+            """
+
+Streamlit was unable to parse the data from an exception in the user's script.
+This is usually due to a bug in the Exception object itself. Here is some info
+about that Exception object, so you can report a bug to the original author:
+
+Exception type:
+  %(etype)s
+
+Problem:
+  %(str_exception)s
+
+Traceback:
+%(str_exception_tb)s
+
+        """
+            % {
+                "etype": type(exception).__name__,
+                "str_exception": str_exception,
+                "str_exception_tb": "\n".join(get_stack_trace(str_exception)),
+            }
+        )
+
+
+def get_stack_trace(exception, exception_traceback=None):
+    # Get and extract the traceback for the exception.
+    if exception_traceback is not None:
+        extracted_traceback = traceback.extract_tb(exception_traceback)
+    elif hasattr(exception, "__traceback__"):
+        # This is the Python 3 way to get the traceback.
+        extracted_traceback = traceback.extract_tb(exception.__traceback__)
+    else:
+        # Hack for Python 2 which will extract the traceback as long as this
+        # method was called on the exception as it was caught, which is
+        # likely what the user would do.
+        _, live_exception, live_traceback = sys.exc_info()
+        if exception == live_exception:
+            extracted_traceback = traceback.extract_tb(live_traceback)
+        else:
+            extracted_traceback = None
+
+    # Format the extracted traceback and add it to the protobuf element.
+    if extracted_traceback is None:
+        stack_trace = [
+            "Cannot extract the stack trace for this exception. "
+            "Try calling exception() within the `catch` block."
+        ]
+    else:
+        stack_trace = traceback.format_list(extracted_traceback)
+
+    return stack_trace
---
+++ packages/streamlit/streamlit-0.49.0-py2.py3-none-any.whl/streamlit/static/precache-manifest.09ad101dae5d0dca117de703455816fc.js
@@ -0,0 +1,258 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aaa9c4268ffb0541969b10c1885622b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2734f70c2a005355218b",
+    "url": "./static/css/17.e4ca7388.chunk.css"
+  },
+  {
+    "revision": "7b3d6c5726a4e4ad284c",
+    "url": "./static/css/19.22c9deab.chunk.css"
+  },
+  {
+    "revision": "2cdc8bd68b6f456b7786",
+    "url": "./static/css/20.bc94e10a.chunk.css"
+  },
+  {
+    "revision": "aea3d6c1fa858937c3d0",
+    "url": "./static/css/22.87c198ab.chunk.css"
+  },
+  {
+    "revision": "df2f7abdbe90c1d8047d",
+    "url": "./static/css/23.aa88cdb7.chunk.css"
+  },
+  {
+    "revision": "deedbdd32c23200ac82a",
+    "url": "./static/css/24.4597e414.chunk.css"
+  },
+  {
+    "revision": "399a1812aa7a401acc9d",
+    "url": "./static/css/25.863b1de9.chunk.css"
+  },
+  {
+    "revision": "84fe382e158b7efb9215",
+    "url": "./static/css/26.0a21c670.chunk.css"
+  },
+  {
+    "revision": "79cdc5b95d21dd3d0fdc",
+    "url": "./static/css/8.e71d8db5.chunk.css"
+  },
+  {
+    "revision": "603b803191e6b846d246",
+    "url": "./static/css/main.f247db0f.chunk.css"
+  },
+  {
+    "revision": "3aef1a8335fca6378883",
+    "url": "./static/js/0.c3008862.chunk.js"
+  },
+  {
+    "revision": "c02381123821bd9ba894",
+    "url": "./static/js/1.30560ad0.chunk.js"
+  },
+  {
+    "revision": "3d964d0c61322d75384d",
+    "url": "./static/js/10.9763c65e.chunk.js"
+  },
+  {
+    "revision": "1c25446a3651a1f76340",
+    "url": "./static/js/11.9e7ae8ed.chunk.js"
+  },
+  {
+    "revision": "63679e18f0b2088b72f0",
+    "url": "./static/js/12.fd51051a.chunk.js"
+  },
+  {
+    "revision": "77b04efefc17064f7918",
+    "url": "./static/js/13.05b5f814.chunk.js"
+  },
+  {
+    "revision": "24df135b43d43df31f44",
+    "url": "./static/js/14.675264f3.chunk.js"
+  },
+  {
+    "revision": "7dc81d09e35aaa98c815",
+    "url": "./static/js/15.1a8cb67d.chunk.js"
+  },
+  {
+    "revision": "69c626f1dad3c9ccce32",
+    "url": "./static/js/16.a7772a0d.chunk.js"
+  },
+  {
+    "revision": "2734f70c2a005355218b",
+    "url": "./static/js/17.5bbba147.chunk.js"
+  },
+  {
+    "revision": "8b20d7cc3511295c9c28",
+    "url": "./static/js/18.895693f6.chunk.js"
+  },
+  {
+    "revision": "7b3d6c5726a4e4ad284c",
+    "url": "./static/js/19.e373f1f3.chunk.js"
+  },
+  {
+    "revision": "61bd358a975ead0e51ca",
+    "url": "./static/js/2.aef66d43.chunk.js"
+  },
+  {
+    "revision": "2cdc8bd68b6f456b7786",
+    "url": "./static/js/20.70bab883.chunk.js"
+  },
+  {
+    "revision": "55b8c1fad80f9718b3f3",
+    "url": "./static/js/21.ac462713.chunk.js"
+  },
+  {
+    "revision": "aea3d6c1fa858937c3d0",
+    "url": "./static/js/22.e701d1f3.chunk.js"
+  },
+  {
+    "revision": "df2f7abdbe90c1d8047d",
+    "url": "./static/js/23.9fc80ebd.chunk.js"
+  },
+  {
+    "revision": "deedbdd32c23200ac82a",
+    "url": "./static/js/24.97e3ca3e.chunk.js"
+  },
+  {
+    "revision": "399a1812aa7a401acc9d",
+    "url": "./static/js/25.2b1171d3.chunk.js"
+  },
+  {
+    "revision": "84fe382e158b7efb9215",
+    "url": "./static/js/26.020c53ee.chunk.js"
+  },
+  {
+    "revision": "30401cbae3f39a33eb8a",
+    "url": "./static/js/27.5ed72d5a.chunk.js"
+  },
+  {
+    "revision": "71594702f5521fb8a44d",
+    "url": "./static/js/28.05b93145.chunk.js"
+  },
+  {
+    "revision": "c674134e69758cd5babe",
+    "url": "./static/js/29.7bece431.chunk.js"
+  },
+  {
+    "revision": "867c7dda00dec741b3e3",
+    "url": "./static/js/3.353d9ad7.chunk.js"
+  },
+  {
+    "revision": "a4bc5ee33bddafc97890",
+    "url": "./static/js/30.809c7ddc.chunk.js"
+  },
+  {
+    "revision": "f3df74acc71227f0a472",
+    "url": "./static/js/31.a71aaaff.chunk.js"
+  },
+  {
+    "revision": "8d3ec9f0abeddc463602",
+    "url": "./static/js/32.27216313.chunk.js"
+  },
+  {
+    "revision": "7e623e779c4f0699647d",
+    "url": "./static/js/33.f73ee457.chunk.js"
+  },
+  {
+    "revision": "a9695f98db5797e80a11",
+    "url": "./static/js/34.864f01f0.chunk.js"
+  },
+  {
+    "revision": "3e89908099c7775545bc",
+    "url": "./static/js/35.940a4611.chunk.js"
+  },
+  {
+    "revision": "0bba3d0d2859ec7b48cb",
+    "url": "./static/js/36.37b59e2b.chunk.js"
+  },
+  {
+    "revision": "07363c2c112f57675617",
+    "url": "./static/js/37.7f10b39a.chunk.js"
+  },
+  {
+    "revision": "8a85e6da706bd77ef4de",
+    "url": "./static/js/38.52457743.chunk.js"
+  },
+  {
+    "revision": "a73e174c20722ee7eb7e",
+    "url": "./static/js/4.e9faaa17.chunk.js"
+  },
+  {
+    "revision": "619a14b9764c9b3a48ca",
+    "url": "./static/js/7.5e207e5f.chunk.js"
+  },
+  {
+    "revision": "79cdc5b95d21dd3d0fdc",
+    "url": "./static/js/8.60b32396.chunk.js"
+  },
+  {
+    "revision": "13b012a09730437549f1",
+    "url": "./static/js/9.d87ea438.chunk.js"
+  },
+  {
+    "revision": "603b803191e6b846d246",
+    "url": "./static/js/main.9bc3200d.chunk.js"
+  },
+  {
+    "revision": "9266c3fa29e837096ce4",
+    "url": "./static/js/runtime~main.1917facb.js"
+  },
+  {
+    "revision": "ef8b866bb24c36af1314c962aca2c200",
+    "url": "./static/media/IBMPlexMono-Regular.ef8b866b.ttf"
+  },
+  {
+    "revision": "adfad4378a705d3e9adac6711e73bea0",
+    "url": "./static/media/IBMPlexSans-Bold.adfad437.ttf"
+  },
+  {
+    "revision": "911ddd3f80feaa2ab861308a1d0894f5",
+    "url": "./static/media/IBMPlexSans-BoldItalic.911ddd3f.ttf"
+  },
+  {
+    "revision": "ac30c36f719f3ae8c1fe13bfcafbe41b",
+    "url": "./static/media/IBMPlexSans-Italic.ac30c36f.ttf"
+  },
+  {
+    "revision": "00c43140ca125fc3354ca196756f3324",
+    "url": "./static/media/IBMPlexSans-Light.00c43140.ttf"
+  },
+  {
+    "revision": "2c590a033739a20c877dd5d249969076",
+    "url": "./static/media/IBMPlexSans-LightItalic.2c590a03.ttf"
+  },
+  {
+    "revision": "70907017295be7941e33142244889e2d",
+    "url": "./static/media/IBMPlexSans-Medium.70907017.ttf"
+  },
+  {
+    "revision": "1cdcaa7780056f8ec0a77be7c06d0cc5",
+    "url": "./static/media/IBMPlexSans-MediumItalic.1cdcaa77.ttf"
+  },
+  {
+    "revision": "2526ba235bc18f7446bd10201ece394c",
+    "url": "./static/media/IBMPlexSans-Regular.2526ba23.ttf"
+  },
+  {
+    "revision": "3cf97837524dd7445e9d1462e3c4afe2",
+    "url": "./static/media/open-iconic.3cf97837.woff"
+  },
+  {
+    "revision": "47151e87c5a8666791a91007de4962da",
+    "url": "./static/media/open-iconic.47151e87.eot"
+  },
+  {
+    "revision": "527eff645d8ead16666afb6931041279",
+    "url": "./static/media/open-iconic.527eff64.otf"
+  },
+  {
+    "revision": "93cc7aa654ae36b0828716f5ec3159cd",
+    "url": "./static/media/open-iconic.93cc7aa6.ttf"
+  },
+  {
+    "revision": "eca2c26759a009a4a9361151efe99e74",
+    "url": "./static/media/open-iconic.eca2c267.svg"
+  }
+]);
