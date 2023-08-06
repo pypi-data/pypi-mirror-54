12b6b11d1d2fcdca7d78e3d96b59edc081e3fe2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:08 +0000

    Add package salure-helpers/9.1.4/salure_helpers-9.1.4.tar.gz

---
+++ packages/salure-helpers/salure_helpers-9.1.4.tar.gz/salure_helpers-9.1.4/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name='salure_helpers',
+    version='9.1.4',
+    description='Files with helpful code, developed by Salure',
+    url='https://bitbucket.org/salurebi/salure_helpers/',
+    author='Salure',
+    author_email='bi@salure.nl',
+    license='Salure License',
+    packages=['salure_helpers'],
+    package_data={'salure_helpers': ['templates/*', 'datasets/*']},
+    install_requires=[
+        'pandas',
+        'mandrill',
+        'psycopg2',
+        'pymysql',
+        'requests',
+        'pysftp',
+        'twine',
+        'clickhouse-driver',
+        'pymonetdb',
+        'fs',
+        'gnupg',
+        'xmltodict',
+        'zeep'
+    ],
+    zip_safe=False
+)
