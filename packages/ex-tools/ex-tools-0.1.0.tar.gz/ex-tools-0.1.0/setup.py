f8f8bab367baf11d3f533bfe309eed4f52913527
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:15 +0000

    Add package ex-tools/0.1.0/ex-tools-0.1.0.tar.gz

---
+++ packages/ex-tools/ex-tools-0.1.0.tar.gz/ex-tools-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ex-tools',
+    version='0.1.0',
+    description='Tools that include/extend functools/itertools +alpha',
+    url='https://github.com/yu-ichiro/ex-tools',
+    author='Yuichiro Luke Smith',
+    author_email='yuichiro.luke@gmail.com',
+    license='MIT',
+    packages=['ex_tools'],
+    zip_safe=False
+)
