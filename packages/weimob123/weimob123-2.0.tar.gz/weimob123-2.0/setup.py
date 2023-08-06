b2a8f78061bb81cab07edac071b35919cb794124
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:25 +0000

    Add package weimob123/2.0/weimob123-2.0.tar.gz

---
+++ packages/weimob123/weimob123-2.0.tar.gz/weimob123-2.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='weimob123',
+      version='v2.0',
+      description='weimob123 for world',
+      url='https://github.com/zhucaixiangtest/test123',
+      author='cx123',
+      author_email='zcx@zcx-t.top',
+      license='MIT',
+      packages=['weimob123'],
+      zip_safe=False)
---
+++ packages/weimob123/weimob123-2.0.tar.gz/weimob123-2.0/weimob123/__init__.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+
+def caixiang():
+    return (""
+            "hello weimob"
+            )
