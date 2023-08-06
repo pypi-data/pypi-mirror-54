7de757a316b5d2e0d6ec24b11d44cf714e0091c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:26 +0000

    Add package gym-jsbsim/0.1.1/gym_jsbsim-0.1.1-cp36-cp36m-macosx_10_13_x86_64.whl

---
+++ packages/gym-jsbsim/gym_jsbsim-0.1.1-cp36-cp36m-macosx_10_13_x86_64.whl/gym_jsbsim/__init__.py
@@ -0,0 +1,29 @@
+import os
+from gym.envs.registration import registry, register, make, spec
+from gym_jsbsim.envs import TASKS
+from gym_jsbsim.catalogs import Catalog
+
+"""
+
+This script registers JSBSimEnv
+
+with OpenAI Gym so that they can be instantiated with a gym.make(id)
+
+ command.
+
+
+ To use do:
+
+       env = gym.make('GymJsbsim-{task}-v0')
+
+"""
+
+if 'JSBSIM_ROOT_DIR' not in os.environ:
+    os.environ['JSBSIM_ROOT_DIR'] = os.path.join(os.path.dirname(__file__), 'jsbsim')
+
+for task_name in TASKS:
+    register(
+        id=f'GymJsbsim-{task_name}-v0',
+        entry_point='gym_jsbsim.jsbsim_env:JSBSimEnv',
+        kwargs=dict(task=TASKS[task_name])
+    )
