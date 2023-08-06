05daa84f542dc6cefc947d1e81bebd7dc1c86051
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:30 +0000

    Add package Assembly/0.0.0/assembly-0.0.0-py2-none-any.whl

---
+++ packages/Assembly/assembly-0.0.0-py2-none-any.whl/flask_assembly/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from .core import *
+from .functions import *
+from . import (response,
+               request,
+               signals,
+               extensions,
+               plugins)
