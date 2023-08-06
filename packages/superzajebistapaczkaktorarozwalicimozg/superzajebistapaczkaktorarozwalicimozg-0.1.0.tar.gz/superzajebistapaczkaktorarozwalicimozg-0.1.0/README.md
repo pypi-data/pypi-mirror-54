ed464f370d5e5b838b7b2d93274ad8acb25525a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:37 +0000

    Add package superzajebistapaczkaktorarozwalicimozg/0.1.0/superzajebistapaczkaktorarozwalicimozg-0.1.0.tar.gz

---
+++ packages/superzajebistapaczkaktorarozwalicimozg/superzajebistapaczkaktorarozwalicimozg-0.1.0.tar.gz/superzajebistapaczkaktorarozwalicimozg-0.1.0/README.md
@@ -0,0 +1,2 @@
+hello user pls donate to my acc - link below 
+https://www.catfacts.co/
---
+++ packages/superzajebistapaczkaktorarozwalicimozg/superzajebistapaczkaktorarozwalicimozg-0.1.0.tar.gz/superzajebistapaczkaktorarozwalicimozg-0.1.0/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+from setuptools import find_packages, setup
+
+setup(
+    name='superzajebistapaczkaktorarozwalicimozg',
+    version='0.1.0',
+    author='Frank Sinatra',
+    author_email='lew_11@wp.pl',
+    packages=find_packages(),
+    include_package_data=True,
+    description='Pokochasz tą paczke od pierwszych chwil'
+)
+
