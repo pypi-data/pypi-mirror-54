46442d8eead33389f5619019465bdf0616639e6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:35 +0000

    Add package nfl-insights/0.2.2/nfl_insights-0.2.2.tar.gz

---
+++ packages/nfl-insights/nfl_insights-0.2.2.tar.gz/nfl_insights-0.2.2/nfl_insights/__init__.py
@@ -0,0 +1,6 @@
+#
+# setting configuration constants
+from .nfl_insights_config import *
+from .nfl_get_data import NFLGetData
+from .nfl_stats_queries import NFLStatsQueries
+from .nfl_offense_team_analytics import OffenseTeamData
