23d23ffdcd5f024c5ab3efd3cfdb7d495403ee4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:18 +0000

    Add package parsewhen/0.2.5/parsewhen-0.2.5.tar.gz

---
+++ packages/parsewhen/parsewhen-0.2.5.tar.gz/parsewhen-0.2.5/parsewhen/__init__.py
@@ -0,0 +1,10 @@
+'''Python package `parsewhen` parses arbitrary text and attempts to convert sections
+to datetime or timedelta objects. If nothing can be parsed the original text is returned.
+
+.. include:: ../docs/usage.md
+'''
+
+from . import lexer, parser, errors, walker, eval
+
+from .language import Language
+from .parsewhen import generate, replace, parse, range, tree
