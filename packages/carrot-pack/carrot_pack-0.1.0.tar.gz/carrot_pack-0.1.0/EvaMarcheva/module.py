4fd25e2da5333613b7eeae56f99f3273cfddb526
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:37 +0000

    Add package carrot-pack/0.1.0/carrot_pack-0.1.0.tar.gz

---
+++ packages/carrot-pack/carrot_pack-0.1.0.tar.gz/carrot_pack-0.1.0/EvaMarcheva/module.py
@@ -0,0 +1,5 @@
+def carrot():
+    for i in 'Marchewka':
+        print(i)
+
+carrot()
---
+++ packages/carrot-pack/carrot_pack-0.1.0.tar.gz/carrot_pack-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='carrot_pack',
+    version='0.1.0',
+    author='Evka Marchevka',
+    author_email='karasewa@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    description='This package includes carrots only')
