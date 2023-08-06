5955123222aabd99d8e6607a8a71dbc6fcac019e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:07 +0000

    Add package dnnnlp/1.1.4/dnnnlp-1.1.4-py3-none-any.whl

---
+++ packages/dnnnlp/dnnnlp-1.1.4-py3-none-any.whl/dnnnlp/__init__.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+PyTorch - Deep Neural Network - Natural Language Processing
+Ubuntu 16.04 & PyTorch 1.2
+Last update: KzXuan, 2019.10.29
+"""
+__name__ = "dnnnlp"
+__version__ = "1.1.4"
+
+
+# verbose for output portion
+class verbose():
+    @classmethod
+    def config(cls, level):
+        """Set verbose level for the whole package.
+
+        Args:
+            level [int]: verbose level, needs 0/1/2, higher level means more output
+        """
+        cls.level = level
+
+    @staticmethod
+    def check(level):
+        """Check a set level of one output.
+
+        Args:
+            level [int]: verbose level of a function, needs 0/1/2
+
+        Returns:
+            True or False
+        """
+        if verbose.level >= level:
+            return True
+        else:
+            return False
+
+
+# set default verbose level
+verbose.config(2)
