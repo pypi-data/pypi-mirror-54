c1d16e0a5a1d8471b1d980e3af959da98f6fbdb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:50 +0000

    Add package b2luigi/0.4.1/b2luigi-0.4.1.tar.gz

---
+++ packages/b2luigi/b2luigi-0.4.1.tar.gz/b2luigi-0.4.1/tests/helpers.py
@@ -0,0 +1,28 @@
+import subprocess
+import sys
+
+import b2luigi
+
+from unittest import TestCase
+import tempfile
+import os
+import shutil
+
+
+class B2LuigiTestCase(TestCase):
+    def setUp(self):
+        self.test_dir = tempfile.mkdtemp()
+        self.cwd = os.getcwd()
+        os.chdir(self.test_dir)
+
+        b2luigi.set_setting("result_path", "results")
+
+    def tearDown(self):
+        os.chdir(self.cwd)
+        shutil.rmtree(self.test_dir)
+
+    def call_file(self, file_name, **kwargs):
+        test_file_name = os.path.join(os.path.dirname(__file__), file_name)
+        tmp_file_name = os.path.join(self.test_dir, os.path.basename(file_name))
+        shutil.copy(test_file_name, tmp_file_name)
+        return subprocess.check_output([sys.executable, tmp_file_name], cwd=self.test_dir, **kwargs)
