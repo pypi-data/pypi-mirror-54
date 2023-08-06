bc929d7b77886f186df908a0782c00515d6b1fdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:55 +0000

    Add package ergo/0.3.24a0/ergo-0.3.24a0.tar.gz

---
+++ packages/ergo/ergo-0.3.24a0.tar.gz/ergo-0.3.24a0/src/version.py
@@ -0,0 +1,21 @@
+"""Summary.
+
+Attributes:
+    VERSION (str): Description
+
+"""
+VERSION = '0.3.24-alpha'
+
+
+def get_version() -> str:
+    """Summary.
+
+    Returns:
+        str: Description
+
+    """
+    return VERSION
+
+
+if __name__ == '__main__':
+    print(get_version())
