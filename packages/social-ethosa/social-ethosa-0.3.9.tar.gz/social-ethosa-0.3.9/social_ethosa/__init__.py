e20cda757781ef0728abe3b162aa05b13e332eeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:15 +0000

    Add package social-ethosa/0.3.9/social-ethosa-0.3.9.tar.gz

---
+++ packages/social-ethosa/social-ethosa-0.3.9.tar.gz/social-ethosa-0.3.9/social_ethosa/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# author: ethosa
+
+from .net import *
+from .botwrapper import *
+from .UtilsManager import *
+
+__version__ = "0.3.9"
+__authors__ = ["Ethosa"]
+__copyright__ = "2019"
+__changelog__ = ["social_ethosa.net.pr0n", "social_ethosa.net.hentasis", "social_ethosa.botwrapper"]
