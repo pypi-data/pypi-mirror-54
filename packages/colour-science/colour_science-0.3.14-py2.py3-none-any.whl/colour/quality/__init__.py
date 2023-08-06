f517b82a620ce2f9433ca8b5d0f0a7546dd5c53e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:46 +0000

    Add package colour-science/0.3.14/colour_science-0.3.14-py2.py3-none-any.whl

---
+++ packages/colour-science/colour_science-0.3.14-py2.py3-none-any.whl/colour/quality/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+
+from .datasets import *  # noqa
+from . import datasets
+from .cri import CRI_Specification, colour_rendering_index
+from .cqs import (CQS_Specification, COLOUR_QUALITY_SCALE_METHODS,
+                  colour_quality_scale)
+
+__all__ = []
+__all__ += datasets.__all__
+__all__ += ['CRI_Specification', 'colour_rendering_index']
+__all__ += [
+    'CQS_Specification', 'COLOUR_QUALITY_SCALE_METHODS', 'colour_quality_scale'
+]
