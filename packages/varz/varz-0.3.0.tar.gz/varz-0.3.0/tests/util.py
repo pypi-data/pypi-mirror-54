4f3a61d0aa4e7cbb3750900efcbf035935b2cb6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:54 +0000

    Add package varz/0.3.0/varz-0.3.0.tar.gz

---
+++ packages/varz/varz-0.3.0.tar.gz/varz-0.3.0/tests/util.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+import lab as B
+from numpy.testing import assert_allclose, assert_array_almost_equal
+from plum import Dispatcher
+
+__all__ = ['allclose', 'approx', 'Value']
+
+_dispatch = Dispatcher()
+
+
+class Value(object):
+    def __init__(self, val):
+        self.val = val
+
+
+@_dispatch({B.Number, B.NP, list})
+def _to_numpy(x):
+    return x
+
+
+@_dispatch({B.Torch, B.TF})
+def _to_numpy(x):
+    return x.numpy()
+
+
+def approx(x, y, digits=7):
+    x = _to_numpy(x)
+    y = _to_numpy(y)
+    assert_array_almost_equal(x, y, decimal=digits)
+
+
+def allclose(x, y):
+    x = _to_numpy(x)
+    y = _to_numpy(y)
+    assert_allclose(x, y)
