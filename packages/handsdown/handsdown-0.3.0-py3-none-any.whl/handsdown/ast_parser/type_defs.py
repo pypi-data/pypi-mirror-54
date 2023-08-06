94177189320e0e08cbce7bf76a9297e50544f722
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:44 +0000

    Add package handsdown/0.3.0/handsdown-0.3.0-py3-none-any.whl

---
+++ packages/handsdown/handsdown-0.3.0-py3-none-any.whl/handsdown/ast_parser/type_defs.py
@@ -0,0 +1,19 @@
+from typing import Union, Text
+from handsdown.ast_parser.node_records.node_record import NodeRecord
+from handsdown.sentinel import Sentinel
+import handsdown.ast_parser.smart_ast as ast
+
+# Ready for render expression
+RenderExpr = Union[NodeRecord, Text, Sentinel]
+
+# AST node or text
+Node = Union[Text, ast.AST]
+
+# Not ready for render expression, AST has to be wrapped
+DirtyRenderExpr = Union[ast.AST, Text, Sentinel]
+
+# Iterable AST types
+ASTIterable = Union[ast.List, ast.Set, ast.Tuple]
+
+# AST import node
+ASTImport = Union[ast.Import, ast.ImportFrom]
