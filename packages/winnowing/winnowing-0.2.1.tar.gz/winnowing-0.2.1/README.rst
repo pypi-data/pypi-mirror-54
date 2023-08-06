1f1a8407ae4b65ea92a5ebbba67525c739e98a35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:52 +0000

    Add package winnowing/0.2.1/winnowing-0.2.1.tar.gz

---
+++ packages/winnowing/winnowing-0.2.1.tar.gz/winnowing-0.2.1/README.rst
@@ -0,0 +1,73 @@
+Winnowing
+=========
+
+A Python implementation of the Winnowing (local algorithms for document
+fingerprinting)
+
+Original Work
+=============
+
+The original research paper can be found at
+http://dl.acm.org/citation.cfm?id=872770.
+
+Installation
+============
+
+You may install ``winnowing`` package via ``pip`` as follows:
+
+::
+
+    pip install winnowing
+    
+Alternatively, you may also install the package by cloning this repository.
+
+::
+
+    git clone https://github.com/suminb/winnowing.git
+    cd winnowing && python setup.py install
+
+Usage
+=====
+
+.. code:: python
+
+    >>> from winnowing import winnow
+
+    >>> winnow('A do run run run, a do run run')
+    set([(5, 23942), (14, 2887), (2, 1966), (9, 23942), (20, 1966)])
+
+    >>> winnow('run run')
+    set([(0, 23942)]) # match found!
+
+Default Hash Function
+~~~~~~~~~~~~~~~~~~~~~
+
+Quite honestly, I did not know what hash function to use. The paper did
+not talk about it. So I decided to use a part of SHA-1; more precisely,
+the last 16 bits of the digest.
+
+Custom Hash Function
+~~~~~~~~~~~~~~~~~~~~
+
+You may use your own hash function as demonstrated below.
+
+.. code:: python
+
+    def hash_md5(text):
+        import hashlib
+
+        hs = hashlib.md5(text)
+        hs = hs.hexdigest()
+        hs = int(hs, 16)
+
+        return hs
+
+    # Override the hash function
+    winnow.hash_function = hash_md5
+
+    winnow('The cake was a lie')
+
+Lower Bound of Fingerprint Density
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+(TODO: Write this section)
---
+++ packages/winnowing/winnowing-0.2.1.tar.gz/winnowing-0.2.1/winnowing/__init__.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "Sumin Byeon"
+
+# DEFINITION 1 (WINNOWING). In each window select the minimum hash value. If
+# there is more than one hash with the minimum value, select the rightmost
+# occurrence. Now save all selected hashes as the fingerprints of the document.
+
+# Note the record function must compute the global position using the relative
+# position, min. Saving this position, together with the selected hash, creates
+# a fingerprint.
+
+
+def sanitize(text):
+    """Removes irrelevant features such as spaces and commas.
+
+    :param text: A string of (index, character) tuples.
+    """
+
+    import re
+
+    # NOTE: \p{L} or \p{Letter}: any kind of letter from any language.
+    # http://www.regular-expressions.info/unicode.html
+    p = re.compile(r"\w", re.UNICODE)
+
+    def f(c):
+        return p.match(c[1]) is not None
+
+    return filter(f, map(lambda x: (x[0], x[1].lower()), text))
+
+
+def kgrams(text, k=5):
+    """Derives k-grams from text."""
+
+    text = list(text)
+    n = len(text)
+
+    if n < k:
+        yield text
+    else:
+        for i in range(n - k + 1):
+            yield text[i : i + k]
+
+
+def winnowing_hash(kgram):
+    """
+    :param kgram: e.g., [(0, 'a'), (2, 'd'), (3, 'o'), (5, 'r'), (6, 'u')]
+    """
+    kgram = zip(*kgram)
+    kgram = list(kgram)
+
+    # FIXME: What should we do when kgram is shorter than k?
+    text = "".join(kgram[1]) if len(kgram) > 1 else ""
+
+    hs = hash_function(text)
+
+    # FIXME: What should we do when kgram is shorter than k?
+    return (kgram[0][0] if len(kgram) > 1 else -1, hs)
+
+
+def default_hash(text):
+    import hashlib
+
+    hs = hashlib.sha1(text.encode("utf-8"))
+    hs = hs.hexdigest()[-4:]
+    hs = int(hs, 16)
+
+    return hs
+
+
+def select_min(window):
+    """In each window select the minimum hash value. If there is more than one
+    hash with the minimum value, select the rightmost occurrence. Now save all
+    selected hashes as the fingerprints of the document.
+
+    :param window: A list of (index, hash) tuples.
+    """
+
+    # print window, min(window, key=lambda x: x[1])
+
+    return min(window, key=lambda x: x[1])
+
+
+def winnow(text, k=5):
+    n = len(list(text))
+
+    text = zip(range(n), text)
+    text = sanitize(text)
+
+    hashes = map(lambda x: winnowing_hash(x), kgrams(text, k))
+
+    windows = kgrams(hashes, 4)
+
+    return set(map(select_min, windows))
+
+
+# Specified a hash function. You may override this.
+hash_function = default_hash
