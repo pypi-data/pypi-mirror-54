dd909deccc0958a9953f2a5c8db9cb76704bdfc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:12 +0000

    Add package deepobs/1.2.0b0/deepobs-1.2.0b0.tar.gz

---
+++ packages/deepobs/deepobs-1.2.0b0.tar.gz/deepobs-1.2.0b0/setup.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""Setup for the DeepOBS package"""
+
+import setuptools
+
+install_requires_list = [
+    "argparse",
+    "numpy",
+    "pandas",
+    "matplotlib",
+    "tikzplotlib",
+    "seaborn",
+    "bayesian-optimization",
+]
+
+
+def readme():
+    with open("README.md") as f:
+        return f.read()
+
+
+setuptools.setup(
+    name="deepobs",
+    version="1.2.0-beta0",
+    description="Deep Learning Optimizer Benchmark Suite",
+    long_description=readme(),
+    author="Frank Schneider, Aaron Bahde, Lukas Balles, and Philipp Hennig",
+    author_email="frank.schneider@tue.mpg.de",
+    license="MIT",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+    install_requires=install_requires_list,
+    scripts=[
+        "deepobs/scripts/deepobs_prepare_data.sh",
+        "deepobs/scripts/deepobs_get_baselines.sh",
+        "deepobs/scripts/deepobs_plot_results.py",
+    ],
+    zip_safe=False,
+)
