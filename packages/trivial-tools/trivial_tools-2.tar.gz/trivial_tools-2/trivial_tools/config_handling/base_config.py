1f27cceb437366857d98775b1eda288d09d2a139
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:18:30 +0000

    Add package trivial-tools/2/trivial_tools-2.tar.gz

---
+++ packages/trivial-tools/trivial_tools-2.tar.gz/trivial_tools-2/trivial_tools/config_handling/base_config.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+
+    Базовый класс для управления конфигурациями
+
+"""
+# модули проекта
+from trivial_tools.files import json_config_load
+
+
+class BaseConfig:
+    """
+    Базовый класс для хранения параметров
+    """
+    def __init__(self, **kwargs):
+        """
+        Автоматическая инициация
+        """
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    @classmethod
+    def from_json(cls, file_name: str, config_name: str):
+        """
+        Загрузить настройки из json файла
+        """
+        parameters = json_config_load(file_name, config_name)
+        instance = cls(**parameters)
+        return instance
