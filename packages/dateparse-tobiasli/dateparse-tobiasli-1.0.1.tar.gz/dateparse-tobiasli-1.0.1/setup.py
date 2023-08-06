c70ea100fc3f7cb5d5f37c782e54f2b99f4c294e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:06 +0000

    Add package dateparse-tobiasli/1.0.1/dateparse-tobiasli-1.0.1.tar.gz

---
+++ packages/dateparse-tobiasli/dateparse-tobiasli-1.0.1.tar.gz/dateparse-tobiasli-1.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='dateparse-tobiasli',
+                 version='1.0.1',
+                 description='Package for converting string timestamps into datetimes.',
+                 author='Tobias Litherland',
+                 author_email='tobiaslland@gmail.com',
+                 url='https://github.com/tobiasli/dateparse',
+                 packages=setuptools.find_packages(),
+                 long_description=long_description,
+                 long_description_content_type="text/markdown",
+                 classifiers=[
+                     "Programming Language :: Python :: 3",
+                     "License :: OSI Approved :: MIT License",
+                     "Operating System :: OS Independent",
+                 ],
+                 install_requires=['tregex-tobiasli']
+                 )
