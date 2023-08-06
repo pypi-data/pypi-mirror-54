2a7bc2b986c3abefdb38890d87b2f353aa37c459
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:53 +0000

    Add package jhkim-probability/0.1/jhkim_probability-0.1.tar.gz

---
+++ packages/jhkim-probability/jhkim_probability-0.1.tar.gz/jhkim_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='jhkim_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['jhkim_probability'],
+      author= 'Ji-Hoon Kim',
+      author_email = 'genesis717.jhk@gmail.com',
+      zip_safe=False)
