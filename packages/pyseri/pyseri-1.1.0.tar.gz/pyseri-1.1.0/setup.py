04d6b39fc307da4697da5e2f633e91494efb2a3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:17:56 +0000

    Add package pyseri/1.1.0/pyseri-1.1.0.tar.gz

---
+++ packages/pyseri/pyseri-1.1.0.tar.gz/pyseri-1.1.0/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="pyseri",
+    version="1.1.0",
+    author="TaylorHere",
+    author_email="taylorherelee@gmail.com",
+    description="Python Serializer tool",
+    # long_description=long_description,
+    # long_description_content_type="text/markdown",
+    url="https://github.com/TaylorHere",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
