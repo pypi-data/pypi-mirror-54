e554f7ae32e614a962e346c3acc8db261aae178d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:44 +0000

    Add package dival/0.3.4/dival-0.3.4.tar.gz

---
+++ packages/dival/dival-0.3.4.tar.gz/dival-0.3.4/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+import os
+
+VERSION = '0.3.4'
+with open(os.path.join(os.path.split(__file__)[0], 'dival',
+                       'VERSION'), 'w') as version_f:
+    version_f.write(VERSION)
+
+setup(name='dival',
+      version=VERSION,
+      description='Deep Inversion Validation Library',
+      url='https://github.com/jleuschn/dival',
+      author='Johannes Leuschner',
+      author_email='jleuschn@uni-bremen.de',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+          'numpy>=1.10',
+          'pandas',
+          'odl',
+          'scikit-image',
+          'scikit-learn',
+          'hyperopt',
+          'pydicom',
+          'tqdm',
+          'matplotlib'
+      ],
+      include_package_data=True,
+      zip_safe=False)
