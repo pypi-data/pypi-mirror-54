d0f4724ad56c88a19d5f9f526335566e489264ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:30 +0000

    Add package olr/1.1.1/olr-1.1.1.tar.gz

---
+++ packages/olr/olr-1.1.1.tar.gz/olr-1.1.1/setup.py
@@ -0,0 +1,31 @@
+import os
+import setuptools
+from setuptools import setup, find_packages
+
+datadir = os.path.join('share','data')
+datafiles = [(d, [os.path.join(d,f) for f in files])
+    for d, folders, files in os.walk(datadir)]
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="olr",
+    version="1.1.1",
+    author="Mathew Fok",
+    author_email="mfok@stevens.edu",
+    description="olr: Optimal Linear Regression",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://www.github.com/MatHatter",
+    packages=setuptools.find_packages(),
+    install_requires=['pandas', 'numpy'],
+    data_files = datafiles,
+    py_modules=['__init__', 'olr_function'],
+    classifers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License",
+        "Operating System :: OS Independent",
+    ],
+)
+    
