24ee2b09fa778fc677d63bed0f87ebfe805907af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:06 +0000

    Add package miniful/0.0.6/miniful-0.0.6.tar.gz

---
+++ packages/miniful/miniful-0.0.6.tar.gz/miniful-0.0.6/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+setup(
+  name = 'miniful',
+  packages = ['miniful'], # this must be the same as the name above
+  version = '0.0.6',
+  description = 'Minimal Fuzzy Library',
+  author = 'Marco S. Nobile',
+  author_email = 'nobile@disco.unimib.it',
+  url = 'https://github.com/aresio/stfu', # use the URL to the github repo
+  keywords = ['fuzzy logic', 'sugeno', 'reasoner'], # arbitrary keywords
+  license='LICENSE.txt',
+  install_requires=[
+        "numpy >= 1.12.0",
+        "scipy >= 1.0.0"
+    ],
+  classifiers = ['Programming Language :: Python :: 2.7', 'Programming Language :: Python :: 3.6'],
+)
