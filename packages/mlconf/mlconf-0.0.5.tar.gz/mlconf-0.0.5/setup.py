c253d013e95bfa968df18ecefe3595bcc620c38a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:42 +0000

    Add package mlconf/0.0.5/mlconf-0.0.5.tar.gz

---
+++ packages/mlconf/mlconf-0.0.5.tar.gz/mlconf-0.0.5/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+      name='mlconf',
+      packages=['mlconf'],
+      version='0.0.5',
+      author='Andreas Grivas',
+      author_email='andreasgrv@gmail.com',
+      url = 'https://github.com/andreasgrv/mlconf',
+      download_url = 'https://github.com/andreasgrv/mlconf/archive/0.0.5.tar.gz',
+      license='BSD',
+      keywords='config argparse yaml machine-learning',
+      install_requires=['pyyaml', 'argparse'],
+      tests_require=['pytest'],
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Programming Language :: Python :: 2',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.4',
+                   'Programming Language :: Python :: 3.5',
+                   'Programming Language :: Python :: 3.6',
+                   'Programming Language :: Python :: 3.7'
+                   ],
+      )
