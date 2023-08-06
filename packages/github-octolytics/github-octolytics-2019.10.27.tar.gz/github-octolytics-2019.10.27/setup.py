c7caee56589e67fff8b95e0865075c1fe078147c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:16:55 +0000

    Add package github-octolytics/2019.10.27/github-octolytics-2019.10.27.tar.gz

---
+++ packages/github-octolytics/github-octolytics-2019.10.27.tar.gz/github-octolytics-2019.10.27/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='github-octolytics',
+    version='2019.10.27',
+    install_requires=[
+        'Requests',
+        'public',
+        'setuptools',
+    ],
+    packages=[
+        'github_octolytics',
+    ],
+)
