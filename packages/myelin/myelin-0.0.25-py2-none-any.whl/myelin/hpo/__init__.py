c11d9416d209aa1a1ad4eecdeba5905f1bf95057
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:21 +0000

    Add package myelin/0.0.25/myelin-0.0.25-py2-none-any.whl

---
+++ packages/myelin/myelin-0.0.25-py2-none-any.whl/myelin/hpo/__init__.py
@@ -0,0 +1,32 @@
+import os
+import json
+
+from myelin.metric import MetricClient
+
+__HPO_CLIENT__ = MetricClient()
+
+
+def get_hpo_params():
+    hpo_params = os.environ.get('HPO_PARAMS')
+    if hpo_params is None:
+        return None
+    return extract_hpo_params(json.loads(hpo_params))
+
+
+def get_loss_metric():
+    return os.environ["LOSS_METRIC"]
+
+
+def publish_result(loss_value, loss_metric):
+    info_map = {'loss value': loss_value}
+    config_map = get_hpo_params()
+    config_id = config_map['config-id']
+    budget = config_map['budget']
+    config = extract_hpo_params(config_map)
+
+    __HPO_CLIENT__.post_update(loss_metric, loss_value)
+    __HPO_CLIENT__.post_result(config_id, config, budget, loss_value, info_map)
+
+
+def extract_hpo_params(config_map):
+    return json.loads(config_map["hpo-conf"])
