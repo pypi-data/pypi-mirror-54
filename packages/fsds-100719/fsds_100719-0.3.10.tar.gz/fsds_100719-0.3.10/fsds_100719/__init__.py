acdb1835ce2b19da62b4f004c1d72875d9d1f0b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:58 +0000

    Add package fsds-100719/0.3.10/fsds_100719-0.3.10.tar.gz

---
+++ packages/fsds-100719/fsds_100719-0.3.10.tar.gz/fsds_100719-0.3.10/fsds_100719/__init__.py
@@ -0,0 +1,8 @@
+__version__ = '0.3.10'
+# from .imports import *
+# from .ft import *
+# from .pt import * 
+print(f"fsds_1007219  v{__version__} loaded.  Read the docs: https://fsds.readthedocs.io/en/latest/ ")
+print(f"> For convenient loading of standard modules use: `>> from fsds_100719.imports import *`\n")
+
+from .ds import ihelp, ihelp_menu,quick_ref
