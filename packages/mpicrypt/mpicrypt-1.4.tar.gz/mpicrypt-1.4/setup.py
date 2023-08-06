1bacc9a5e4332545f4d301bd4ca9c9305eb509e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:23 +0000

    Add package mpicrypt/1.4/mpicrypt-1.4.tar.gz

---
+++ packages/mpicrypt/mpicrypt-1.4.tar.gz/mpicrypt-1.4/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+try:
+    from setuptools import setup, Extension
+except:
+    from distutils.core import setup, Extension
+
+MODULES = [
+    Extension(
+        "_mpicrypt",
+        ["_mpicrypt.c","mpi.c","AES.c","sha1.c","md5.c"],
+        extra_objects=["jsalloc.h","AES.h","bn_asm.h","md5.h","platform.h","starimpl.h","sha1.h"],
+        include_dirs=[],
+        library_dirs=[],
+        libraries=[],
+        )]
+
+# build!
+setup(
+    name="mpicrypt",
+    version="1.4",
+    author="Jeff Senn",
+    author_email= "jeffsenn@gmail.com",
+    description="Python _mpicrypt helper",
+    packages=[""],
+    ext_modules = MODULES,
+    )
