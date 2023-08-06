47b7e117a2ce6e360bd574701d4a6f887af07461
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:48 +0000

    Add package dbmig/1.5.1/dbmig-1.5.1.tar.gz

---
+++ packages/dbmig/dbmig-1.5.1.tar.gz/dbmig-1.5.1/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+
+setup(
+	name = 'dbmig',
+	version='1.5.1',
+	author = 'Julien Demoor',
+	author_email = 'julien@jdemoor.com',
+    url = 'https://bitbucket.org/jdkx/dbmig/',
+	license = 'MIT',
+	description = 'Simple tool to run SQL migration scripts against an RDBMS. dbmig supports PosgreSQL and should be easy to adapt to any backend supported by SQLAlchemy.',
+	packages = ['dbmig'],
+	entry_points = {
+		'console_scripts': [
+		    'dbmig = dbmig.main:main',
+		]
+    },
+	install_requires = [
+		'SQLAlchemy',
+	],
+	zip_safe = False,
+	include_package_data = True,
+	classifiers=[
+	    "License :: OSI Approved :: MIT License",
+	    "Programming Language :: Python :: 2",
+	    "Programming Language :: Python :: 2.7",
+        "Topic :: Database"
+    ],
+)
