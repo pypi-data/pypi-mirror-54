12106d639e1276431999109e222d5e194f207a99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:56 +0000

    Add package dbt-postgres/0.15.0b3/dbt-postgres-0.15.0b3.tar.gz

---
+++ packages/dbt-postgres/dbt-postgres-0.15.0b3.tar.gz/dbt-postgres-0.15.0b3/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+from setuptools import find_namespace_packages
+from setuptools import setup
+import os
+
+package_name = "dbt-postgres"
+package_version = "0.15.0b3"
+description = """The postgres adpter plugin for dbt (data build tool)"""
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md')) as f:
+    long_description = f.read()
+
+setup(
+    name=package_name,
+    version=package_version,
+    description=description,
+    long_description=description,
+    long_description_content_type='text/markdown',
+    author="Fishtown Analytics",
+    author_email="info@fishtownanalytics.com",
+    url="https://github.com/fishtown-analytics/dbt",
+    packages=find_namespace_packages(include=['dbt', 'dbt.*']),
+    package_data={
+        'dbt': [
+            'include/postgres/dbt_project.yml',
+            'include/postgres/macros/*.sql',
+            'include/postgres/macros/**/*.sql',
+        ]
+    },
+    install_requires=[
+        'dbt-core=={}'.format(package_version),
+        'psycopg2>=2.7.5,<2.8',
+    ],
+    zip_safe=False,
+)
