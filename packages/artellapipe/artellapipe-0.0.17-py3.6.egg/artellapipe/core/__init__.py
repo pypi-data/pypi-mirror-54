08ed869137b419c8f42e5769181c5ee942de1ae3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:48 +0000

    Add package artellapipe/0.0.17/artellapipe-0.0.17-py3.6.egg

---
+++ packages/artellapipe/artellapipe-0.0.17-py3.6.egg/artellapipe/core/__init__.py
@@ -0,0 +1,13 @@
+order = [
+    'artellapipe.core.defines',
+    'artellapipe.core.abstract',
+    'artellapipe.core.config',
+    'artellapipe.core.artellaclasses',
+    'artellapipe.core.artellalib',
+    'artellapipe.core.assetinfo',
+    'artellapipe.core.assetfile',
+    'artellapipe.core.asset',
+    'artellapipe.core.syncdialog',
+    'artellapipe.core.sequence',
+    'artellapipe.core.shot'
+]
