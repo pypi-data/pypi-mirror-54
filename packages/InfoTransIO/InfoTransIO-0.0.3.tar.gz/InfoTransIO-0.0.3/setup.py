5d7dbfbb1f5c1e8e596bbea0080cc45a533842b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:35 +0000

    Add package InfoTransIO/0.0.3/InfoTransIO-0.0.3.tar.gz

---
+++ packages/InfoTransIO/InfoTransIO-0.0.3.tar.gz/InfoTransIO-0.0.3/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='InfoTransIO',
+    version='0.0.3',
+    author='Rafael Rayes',
+    author_email='rafa@rayes.com.br',
+    description='Transmit data using python with InfoTransIO',
+    long_description='''No description yet''',
+    long_description_content_type="text/markdown",
+    url='https://sites.google.com/rayes.com.br/rafael-rayes/',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "praw"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"]
+    
+)
