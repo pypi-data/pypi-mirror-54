c49c66f493b192680b0637899929c97241c41ff0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:33 +0000

    Add package floaty/2.0.0/floaty-2.0.0.tar.gz

---
+++ packages/floaty/floaty-2.0.0.tar.gz/floaty-2.0.0/floaty/__init__.py
@@ -0,0 +1,47 @@
+from decimal import Decimal
+from numbers import Number
+
+
+def no_decimal(obj):
+    _replace_floating_point(obj, float)
+
+
+def no_float(obj):
+    _replace_floating_point(obj, lambda x: Decimal(str(x)))
+
+
+def read_item(query_return):
+    item = query_return['Item'] if 'Item' in query_return else None
+    no_decimal(item)
+
+    return item
+
+
+def read_items(query_return):
+    items = query_return['Items'] if 'Items' in query_return else []
+
+    for i in items:
+        no_decimal(i)
+
+    return items
+
+
+def no_empty_string(obj):
+    for k in list(obj.keys()):
+        v = obj[k]
+
+        if isinstance(v, dict):
+            no_empty_string(v)
+
+        if v == '':
+            del obj[k]
+
+
+def _replace_floating_point(obj, replace):
+    for k, v in obj.items():
+
+        if isinstance(v, dict):
+            _replace_floating_point(v, replace)
+
+        elif isinstance(v, Number) and v % 1:
+            obj[k] = replace(v)
