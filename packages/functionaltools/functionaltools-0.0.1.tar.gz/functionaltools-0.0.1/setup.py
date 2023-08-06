4b7e5095e03d7d0031fd3202b8478bcd6116b39f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:16:48 +0000

    Add package functionaltools/0.0.1/functionaltools-0.0.1.tar.gz

---
+++ packages/functionaltools/functionaltools-0.0.1.tar.gz/functionaltools-0.0.1/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+
+setuptools.setup(
+    name="functionaltools",
+    version="0.0.1",
+    author="Lewis Rodgers",
+    author_email="contact@lewisrodgers.com",
+    description="Python functions to facilitate functional programming.",
+    url="https://github.com/lewisrodgers/pyfunctionaltools",
+    packages=setuptools.find_packages()
+)
