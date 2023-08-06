c01ec2f2d88a14611c59554037a89654235c040d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:03 +0000

    Add package EXCELerator/0.1/EXCELerator-0.1-py3-none-any.whl

---
+++ packages/EXCELerator/EXCELerator-0.1-py3-none-any.whl/excelerator/__init__.py
@@ -0,0 +1,9 @@
+"""
+Library of functions with utilities for the popular xlsx Python module openpyxl
+"""
+
+__version__ = "0.1"
+
+
+from excelerator.table_reader import TableReader
+from excelerator import exceptions
